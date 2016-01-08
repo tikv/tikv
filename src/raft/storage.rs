b474cbf9a27d21d8a583e6945a4f5b597b20e2b7
ngaut <ngaut@126.com>
2016-01-08 18:05:32 +0800

    raft: Add skeleton for MemStorage

--- src/raft/storage.rs
+++ src/raft/storage.rs
@@ -1,5 +1,4 @@
 use raft::raftpb::*;
-
 use raft::errors::Result;
 
 
@@ -16,4 +15,41 @@ pub trait Storage {
     fn first_index() -> Result<u64>;
     fn last_index() -> Result<u64>;
     fn snapshot() -> Result<Snapshot>;
+}
+
+pub struct MemStorage {
+    hard_state: HardState,
+    snapshot: Snapshot,
+    entries: Vec<Entry>,
+}
+
+// pub fn new() -> MemStorage {
+// MemStorage { entries: Vec::new() }
+// }
+//
+
+impl Storage for MemStorage {
+    fn initial_state() -> Result<RaftState> {
+        None
+    }
+
+    fn entries(low: u64, high: u64, max_size: u64) -> Vec<Entry> {
+        None
+    }
+
+    fn term(idx: u64) -> Result<u64> {
+        None
+    }
+
+    fn first_index() -> Result<u64> {
+        None
+    }
+
+    fn last_index() -> Result<u64> {
+        None
+    }
+
+    fn snapshot() -> Result<Snapshot> {
+        None
+    }
 }
