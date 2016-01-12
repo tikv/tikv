4fdfd4be9f257cb1b0fe13a69f98f6c3076be686
disksing <i@disksing.com>
2016-01-12 12:40:57 +0800

    storage: mvcc
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,8 +1,6 @@
-use std::error;
-use std::result;
-use std::fmt::{self, Display, Formatter};
 use self::engine::Engine;
 pub use self::engine::Dsn;
+use self::mvcc::{Error, Result};
 
 mod engine;
 mod mvcc;
@@ -13,18 +11,17 @@ pub struct Storage {
 
 impl Storage {
     pub fn new(desc: Dsn) -> Result<Storage> {
-        engine::new_engine(desc).map(|e| Storage { engine: e }).map_err(|e| Error::from(e))
+        let eng = try!(engine::new_engine(desc));
+        Ok(Storage{engine: eng})
     }
 
     pub fn get(&self, key: &[u8], version: u64) -> Result<Option<Vec<u8>>> {
         trace!("storage: get {:?}@{}", key, version);
-        // TODO (disksing)
-        self.engine.get(key).map_err(|e| Error::from(e))
+        mvcc::get(&*self.engine, key, version)
     }
 
     pub fn put(&mut self, key: &[u8], value: &[u8], version: u64) -> Result<()> {
         trace!("storage: put {:?}@{}", key, version);
-        // TODO (disksing)
-        self.engine.put(key, value).map_err(|e| Error::from(e))
+        mvcc::put(&mut*self.engine, key, value, version)
     }
 }
