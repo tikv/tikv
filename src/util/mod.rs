65531d1b69267b00d90c2e45229e4c7ccfcd0b9f
Jay Lee <busyjaylee@gmail.com>
2016-01-22 20:21:39 +0800

    address comment

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,6 @@
+use std::cell::UnsafeCell;
+use std::ops::Deref;
+
 pub use log::LogLevelFilter;
 use log::{self, Log, LogMetadata, LogRecord, SetLoggerError};
 
@@ -25,3 +28,33 @@ impl Log for StdOutLogger {
         }
     }
 }
+
+/// A simple general wrapper for struct that is thread-safe for interior mutability already.
+pub struct ThreadSafeCell<T: Sync>(UnsafeCell<T>);
+
+impl<T: Sync> ThreadSafeCell<T> {
+    pub fn new(t: T) -> ThreadSafeCell<T> {
+        ThreadSafeCell(UnsafeCell::new(t))
+    }
+
+    pub fn into_inner(self) -> T {
+        let ThreadSafeCell(c) = self;
+        unsafe { c.into_inner() }
+    }
+
+    pub fn borrow_mut(&self) -> &mut T {
+        let &ThreadSafeCell(ref c) = self;
+        unsafe { &mut *c.get() }
+    }
+}
+
+impl<T: Sync> Deref for ThreadSafeCell<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        let &ThreadSafeCell(ref c) = self;
+        unsafe { &*c.get() }
+    }
+}
+
+unsafe impl<T: Sync> Sync for ThreadSafeCell<T> {}
