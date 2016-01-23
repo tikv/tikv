7fdbe448dc209387d877044976bf6f6012f0d5e2
disksing <i@disksing.com>
2016-01-23 19:11:18 +0800

    storage: tiny simplify

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -50,13 +50,7 @@ impl StorageSender {
                      key: Key,
                      version: u64,
                      callback: Box<FnBox(Result<Option<Value>>) + Send>) {
-        self.inner_sender
-            .send(Message::Get {
-                key: key,
-                version: version,
-                callback: callback,
-            })
-            .unwrap();
+        self.inner_sender.send(Message::Get(Cmd((key, version), callback))).unwrap();
     }
 
     pub fn async_scan(&self,
@@ -64,14 +58,7 @@ impl StorageSender {
                       limit: usize,
                       version: u64,
                       callback: Box<FnBox(Result<Vec<KvPair>>) + Send>) {
-        self.inner_sender
-            .send(Message::Scan {
-                start_key: start_key,
-                limit: limit,
-                version: version,
-                callback: callback,
-            })
-            .unwrap();
+        self.inner_sender.send(Message::Scan(Cmd((start_key, limit, version), callback))).unwrap();
     }
 
     pub fn async_commit(&self,
@@ -81,13 +68,7 @@ impl StorageSender {
                         version: u64,
                         callback: Box<FnBox(Result<()>) + Send>) {
         self.inner_sender
-            .send(Message::Commit {
-                puts: puts,
-                deletes: deletes,
-                locks: locks,
-                version: version,
-                callback: callback,
-            })
+            .send(Message::Commit(Cmd((puts, deletes, locks, version), callback)))
             .unwrap();
     }
 
@@ -96,42 +77,34 @@ impl StorageSender {
     }
 }
 
+type Version = u64;
+type Limit = usize;
+type Puts = Vec<KvPair>;
+type Deletes = Vec<Key>;
+type Locks = Vec<Key>;
+
+struct Cmd<T, V>(T, Box<FnBox(Result<V>) + Send>);
 enum Message {
-    Get {
-        key: Key,
-        version: u64,
-        callback: Box<FnBox(Result<Option<Value>>) + Send>,
-    },
-    Scan {
-        start_key: Key,
-        limit: usize,
-        version: u64,
-        callback: Box<FnBox(Result<Vec<KvPair>>) + Send>,
-    },
-    Commit {
-        puts: Vec<KvPair>,
-        deletes: Vec<Key>,
-        locks: Vec<Key>,
-        version: u64,
-        callback: Box<FnBox(Result<()>) + Send>,
-    },
+    Get(Cmd<(Key, Version), Option<Value>>),
+    Scan(Cmd<(Key, Limit, Version), Vec<KvPair>>),
+    Commit(Cmd<(Puts, Deletes, Locks, Version), ()>),
     Close,
 }
 
 impl fmt::Debug for Message {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Message::Get{ref key, version, ..} => {
+            Message::Get(Cmd((ref key, version), _)) => {
                 write!(f, "storage::message::get {:?} @ {}", key, version)
             }
-            Message::Scan{ref start_key, limit, version, ..} => {
+            Message::Scan(Cmd((ref start_key, limit, version), _)) => {
                 write!(f,
                        "storage::message::scan {:?}({}) @ {}",
                        start_key,
                        limit,
                        version)
             }
-            Message::Commit{ref puts, ref deletes, ref locks, version, ..} => {
+            Message::Commit(Cmd((ref puts, ref deletes, ref locks, version), _)) => {
                 write!(f,
                        "storage::message::commit puts({}), deletes({}), locks({}) @ {}",
                        puts.len(),
@@ -153,9 +126,9 @@ impl Handler for StorageHandler {
     fn notify(&mut self, event_loop: &mut EventLoop<StorageHandler>, msg: Message) {
         debug!("recv message: {:?}", msg);
         match msg {
-            Message::Get{callback, ..} => callback(Ok(None)),
-            Message::Scan{callback, ..} => callback(Ok(vec![])),
-            Message::Commit{callback, ..} => callback(Ok(())),
+            Message::Get(Cmd(_, callback)) => callback(Ok(None)),
+            Message::Scan(Cmd(_, callback)) => callback(Ok(vec![])),
+            Message::Commit(Cmd(_, callback)) => callback(Ok(())),
             Message::Close => event_loop.shutdown(),
         }
     }
