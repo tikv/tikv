bc683136b7b1d920957c43855199c6aea5d64872
siddontang <siddontang@gmail.com>
2016-01-29 13:21:22 +0800

    raftserver: unify db and snapshot to simplify code.

--- src/raftserver/store/engine.rs
+++ src/raftserver/store/engine.rs
@@ -1,6 +1,7 @@
 use std::option::Option;
+use std::ops::Deref;
 
-use rocksdb::{DB, Writable, DBIterator, Direction, IteratorMode};
+use rocksdb::{DB, Writable, DBIterator, Direction, IteratorMode, DBVector};
 use rocksdb::rocksdb::Snapshot;
 use protobuf;
 use byteorder::{ByteOrder, BigEndian};
@@ -13,10 +14,58 @@ pub fn new_engine(path: &str) -> Result<DB> {
     Ok(db)
 }
 
-pub fn get_msg<M>(db: &DB, key: &[u8]) -> Result<Option<M>>
-    where M: protobuf::Message + protobuf::MessageStatic
+pub enum DBValue {
+    DBVector(DBVector),
+    Box(Box<[u8]>),
+}
+
+impl Deref for DBValue {
+    type Target = [u8];
+    fn deref(&self) -> &[u8] {
+        match *self {
+            DBValue::DBVector(ref v) => return v,
+            DBValue::Box(ref v) => return v,
+        }
+    }
+}
+
+pub trait Readable {
+    fn get_value(&self, key: &[u8]) -> Result<Option<DBValue>>;
+    fn new_iterator(&self, start_key: &[u8]) -> DBIterator;
+}
+
+impl Readable for DB {
+    fn get_value(&self, key: &[u8]) -> Result<Option<DBValue>> {
+        let v = try!(self.get(key));
+        Ok(v.map(|e| DBValue::DBVector(e)))
+    }
+
+    fn new_iterator(&self, start_key: &[u8]) -> DBIterator {
+        self.iterator(IteratorMode::From(start_key, Direction::forward))
+    }
+}
+
+impl<'a> Readable for Snapshot<'a> {
+    fn get_value(&self, key: &[u8]) -> Result<Option<DBValue>> {
+        let mut it = self.iterator(IteratorMode::From(key, Direction::forward));
+        if let Some((seek_key, value)) = it.next() {
+            if seek_key.as_ref() == key {
+                return Ok(Some(DBValue::Box(value)));
+            }
+        }
+        Ok(None)
+    }
+
+    fn new_iterator(&self, start_key: &[u8]) -> DBIterator {
+        self.iterator(IteratorMode::From(start_key, Direction::forward))
+    }
+}
+
+pub fn get_msg<M, T>(db: &T, key: &[u8]) -> Result<Option<M>>
+    where T: Readable,
+          M: protobuf::Message + protobuf::MessageStatic
 {
-    let value = try!(db.get(key));
+    let value = try!(db.get_value(key));
 
     if value.is_none() {
         return Ok(None);
@@ -33,8 +82,8 @@ pub fn put_msg<T: Writable, M: protobuf::Message>(w: &T, key: &[u8], m: &M) -> R
     Ok(())
 }
 
-pub fn get_u64(db: &DB, key: &[u8]) -> Result<Option<u64>> {
-    let value = try!(db.get(key));
+pub fn get_u64<T: Readable>(db: &T, key: &[u8]) -> Result<Option<u64>> {
+    let value = try!(db.get_value(key));
 
     if value.is_none() {
         return Ok(None);
@@ -49,7 +98,7 @@ pub fn get_u64(db: &DB, key: &[u8]) -> Result<Option<u64>> {
     Ok(Some(n))
 }
 
-pub fn get_i64(db: &DB, key: &[u8]) -> Result<Option<i64>> {
+pub fn get_i64<T: Readable>(db: &T, key: &[u8]) -> Result<Option<i64>> {
     let r = try!(get_u64(db, key));
     match r {
         None => Ok(None),
@@ -68,80 +117,14 @@ pub fn put_i64<T: Writable>(w: &T, key: &[u8], n: i64) -> Result<()> {
     put_u64(w, key, n as u64)
 }
 
-fn snap_get(snap: &Snapshot, key: &[u8]) -> Result<Option<Box<[u8]>>> {
-    // Snapshot now doesn't have get method, we use iterator instead.
-    let mut it = snap.iterator(IteratorMode::From(key, Direction::forward));
-    if let Some((seek_key, value)) = it.next() {
-        if seek_key.as_ref() == key {
-            return Ok(Some(value));
-        }
-    }
-
-    Ok(None)
-}
-
-// Now snapshot doesn't support get function, so it is not easy to combine DB and Snapshot operations.
-// If origin rust rocksdb supports snapshot get, we will discard these functions.
-pub fn snap_get_msg<M>(snap: &Snapshot, key: &[u8]) -> Result<Option<M>>
-    where M: protobuf::Message + protobuf::MessageStatic
-{
-    let value = try!(snap_get(snap, key));
-
-    if value.is_none() {
-        return Ok(None);
-    }
-
-    let mut m = M::new();
-    try!(m.merge_from_bytes(&value.unwrap()));
-    Ok(Some(m))
-}
-
-pub fn snap_get_u64(snap: &Snapshot, key: &[u8]) -> Result<Option<u64>> {
-    let value = try!(snap_get(snap, key));
-
-    if value.is_none() {
-        return Ok(None);
-    }
-
-    let value = value.unwrap();
-    if value.len() != 8 {
-        return Err(other(format!("need 8 bytes, but only got {}", value.len())));
-    }
-
-    let n = BigEndian::read_u64(&value);
-    Ok(Some(n))
-}
-
-pub fn snap_get_i64(snap: &Snapshot, key: &[u8]) -> Result<Option<i64>> {
-    let r = try!(snap_get_u64(snap, key));
-    match r {
-        None => Ok(None),
-        Some(n) => Ok(Some(n as i64)),
-    }
-}
-
 // scan scans database using an iterator in range [start_key, end_key), calls function f for
 // each iteration, if f returns false, terminates this scan.
-pub fn scan<F>(db: &DB, start_key: &[u8], end_key: &[u8], f: &mut F) -> Result<()>
-    where F: FnMut(&[u8], &[u8]) -> Result<bool>
+pub fn scan<T, F>(db: &T, start_key: &[u8], end_key: &[u8], f: &mut F) -> Result<()>
+    where T: Readable,
+          F: FnMut(&[u8], &[u8]) -> Result<bool>
 {
-    let mut it = db.iterator(IteratorMode::From(start_key, Direction::forward));
+    let mut it = db.new_iterator(start_key);
 
-    scan_inner(&mut it, end_key, f)
-}
-
-// snap_scan like above scan, scans the data in a snapshot.
-pub fn snap_scan<F>(snap: &Snapshot, start_key: &[u8], end_key: &[u8], f: &mut F) -> Result<()>
-    where F: FnMut(&[u8], &[u8]) -> Result<bool>
-{
-    let mut it = snap.iterator(IteratorMode::From(start_key, Direction::forward));
-
-    scan_inner(&mut it, end_key, f)
-}
-
-fn scan_inner<F>(it: &mut DBIterator, end_key: &[u8], f: &mut F) -> Result<()>
-    where F: FnMut(&[u8], &[u8]) -> Result<bool>
-{
     while let Some((key, value)) = it.next() {
         if key.as_ref() >= end_key {
             break;
@@ -155,6 +138,7 @@ fn scan_inner<F>(it: &mut DBIterator, end_key: &[u8], f: &mut F) -> Result<()>
 
     Ok(())
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -178,19 +162,19 @@ mod tests {
 
         let snap = engine.snapshot();
 
-        let mut r1 = get_msg::<Region>(&engine, key).unwrap().unwrap();
+        let mut r1: Region = get_msg(&engine, key).unwrap().unwrap();
         assert_eq!(r, r1);
 
-        let mut r2 = snap_get_msg::<Region>(&snap, key).unwrap().unwrap();
+        let mut r2: Region = get_msg(&snap, key).unwrap().unwrap();
         assert_eq!(r, r2);
 
         r.set_region_id(11);
         put_msg(&engine, key, &r).unwrap();
-        r1 = get_msg::<Region>(&engine, key).unwrap().unwrap();
-        r2 = snap_get_msg::<Region>(&snap, key).unwrap().unwrap();
+        r1 = get_msg(&engine, key).unwrap().unwrap();
+        r2 = get_msg(&snap, key).unwrap().unwrap();
         assert!(r1 != r2);
 
-        let b = get_msg::<Region>(&engine, b"missing_key").unwrap();
+        let b: Option<Region> = get_msg(&engine, b"missing_key").unwrap();
         assert!(b.is_none());
 
         put_i64(&engine, key, -1).unwrap();
@@ -242,13 +226,13 @@ mod tests {
 
         data.clear();
 
-        snap_scan(&snap,
-                  b"",
-                  &[0xFF, 0xFF],
-                  &mut |key, value| -> Result<bool> {
-                      data.push((key.to_vec(), value.to_vec()));
-                      Ok(true)
-                  })
+        scan(&snap,
+             b"",
+             &[0xFF, 0xFF],
+             &mut |key, value| -> Result<bool> {
+                 data.push((key.to_vec(), value.to_vec()));
+                 Ok(true)
+             })
             .unwrap();
 
         assert_eq!(data.len(), 2);
