d74baac9091cbea2193afb294a386489f5dcddf9
Jay Lee <busyjaylee@gmail.com>
2016-02-02 15:20:42 +0800

    reformat code

--- tests/test_raft_flow_control.rs
+++ tests/test_raft_flow_control.rs
@@ -9,7 +9,7 @@ fn test_msg_app_flow_control_full() {
     let mut r = new_test_raft(1, vec![1, 2], 5, 1, new_storage());
     r.become_candidate();
     r.become_leader();
-    
+
     // force the progress to be in replicate state
     r.prs.get_mut(&2).unwrap().become_replicate();
     // fill in the inflights window
@@ -20,10 +20,10 @@ fn test_msg_app_flow_control_full() {
             panic!("#{}: ms count = {}, want 1", i, ms.len());
         }
     }
-    
+
     // ensure 1
     assert!(r.prs[&2].ins.full());
-    
+
     // ensure 2
     for i in 0..10 {
         r.step(new_message(1, 1, MessageType::MsgPropose, 1)).expect("");
@@ -43,7 +43,7 @@ fn test_msg_app_flow_control_move_forward() {
     let mut r = new_test_raft(1, vec![1, 2], 5, 1, new_storage());
     r.become_candidate();
     r.become_leader();
-    
+
     // force the progress to be in replicate state
     r.prs.get_mut(&2).unwrap().become_replicate();
     // fill in the inflights window
@@ -51,7 +51,7 @@ fn test_msg_app_flow_control_move_forward() {
         r.step(new_message(1, 1, MessageType::MsgPropose, 1)).expect("");
         r.read_messages();
     }
-    
+
     // 1 is noop, 2 is the first proposal we just sent.
     // so we start with 2.
     for tt in 2..r.max_inflight {
@@ -60,24 +60,26 @@ fn test_msg_app_flow_control_move_forward() {
         m.set_index(tt as u64);
         r.step(m).expect("");
         r.read_messages();
-        
+
         // fill in the inflights window again
         r.step(new_message(1, 1, MessageType::MsgPropose, 1)).expect("");
         let ms = r.read_messages();
         if ms.len() != 1 {
             panic!("#{}: ms count = {}, want 1", tt, ms.len());
         }
-        
+
         // ensure 1
         assert!(r.prs[&2].ins.full());
-        
+
         // ensure 2
         for i in 0..tt {
             let mut m = new_message(2, 1, MessageType::MsgAppendResponse, 0);
             m.set_index(i as u64);
             r.step(m).expect("");
             if !r.prs[&2].ins.full() {
-                panic!("#{}: inflights.full = {}, want true", tt, r.prs[&2].ins.full());
+                panic!("#{}: inflights.full = {}, want true",
+                       tt,
+                       r.prs[&2].ins.full());
             }
         }
     }
@@ -90,7 +92,7 @@ fn test_msg_app_flow_control_recv_heartbeat() {
     let mut r = new_test_raft(1, vec![1, 2], 5, 1, new_storage());
     r.become_candidate();
     r.become_leader();
-    
+
     // force the progress to be in replicate state
     r.prs.get_mut(&2).unwrap().become_replicate();
     // fill in the inflights window
@@ -98,28 +100,33 @@ fn test_msg_app_flow_control_recv_heartbeat() {
         r.step(new_message(1, 1, MessageType::MsgPropose, 1)).expect("");
         r.read_messages();
     }
-    
+
     for tt in 1..5 {
         if !r.prs[&2].ins.full() {
-            panic!("#{}: inflights.full = {}, want true", tt, r.prs[&2].ins.full());
+            panic!("#{}: inflights.full = {}, want true",
+                   tt,
+                   r.prs[&2].ins.full());
         }
-        
+
         // recv tt MsgHeartbeatResp and expect one free slot
         for i in 0..tt {
             r.step(new_message(2, 1, MessageType::MsgHeartbeatResponse, 0)).expect("");
             r.read_messages();
             if r.prs[&2].ins.full() {
-                panic!("#{}.{}: inflights.full = {}, want false", tt, i, r.prs[&2].ins.full());
+                panic!("#{}.{}: inflights.full = {}, want false",
+                       tt,
+                       i,
+                       r.prs[&2].ins.full());
             }
         }
-        
+
         // one slot
         r.step(new_message(1, 1, MessageType::MsgPropose, 1)).expect("");
         let ms = r.read_messages();
         if ms.len() != 1 {
             panic!("#{}: free slot = 0, want 1", tt);
         }
-        
+
         // and just one slot
         for i in 0..10 {
             r.step(new_message(1, 1, MessageType::MsgPropose, 1)).expect("");
@@ -128,7 +135,7 @@ fn test_msg_app_flow_control_recv_heartbeat() {
                 panic!("#{}.{}, ms1 should be empty.", tt, i);
             }
         }
-        
+
         // clear all pending messages
         r.step(new_message(2, 1, MessageType::MsgHeartbeatResponse, 0)).expect("");
         r.read_messages();
