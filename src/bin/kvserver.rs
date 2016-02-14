2c13a39c8a7174d66e234ccd2760eb7c34ba8769
morefreeze <morefreeze@gmail.com>
2016-02-14 16:35:12 +0800

    kvrpc:
    1. add log level flag
    2. address comment

--- src/bin/kvserver.rs
+++ src/bin/kvserver.rs
@@ -15,29 +15,40 @@ fn print_usage(program: &str, opts: Options) {
 }
 
 fn main() {
-    util::init_log(LogLevelFilter::Debug).unwrap();
-
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
     let mut opts = Options::new();
     opts.optopt("H", "host", "set host:port", "host:port");
+    opts.optopt("L",
+                "log",
+                "set log level",
+                "log level: trace, debug, info, warn, error, off");
     opts.optflag("h", "help", "print this help menu");
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(why) => {
-            panic!(why.to_string());
-        }
-    };
+    let matches = opts.parse(&args[1..]).expect("opts parse failed");
     if matches.opt_present("h") {
         print_usage(&program, opts);
         return;
     }
-    let default_host: String = "127.0.0.1:61234".to_string();
-    let host: String = match matches.opt_str("H") {
-        Some(x) => x,
-        None => default_host,
+    let log_filter = match matches.opt_str("L") {
+        Some(level) => get_level_by_string(&level),
+        None => LogLevelFilter::Info,
     };
+    util::init_log(log_filter).unwrap();
+    let default_host: String = "127.0.0.1:61234".to_string();
+    let host: String = matches.opt_str("H").unwrap_or(default_host);
     let store: Storage = Storage::new(Dsn::Memory).unwrap();
     info!("Start listenning on {}...", host);
     run(&host, store);
+}
+
+fn get_level_by_string(lv: &str) -> LogLevelFilter {
+    match lv {
+        "trace" | "TRACE" => LogLevelFilter::Trace,
+        "debug" | "DEBUG" => LogLevelFilter::Debug,
+        "info" | "INFO" => LogLevelFilter::Info,
+        "warn" | "WARN" => LogLevelFilter::Warn,
+        "error" | "ERROR" => LogLevelFilter::Error,
+        "off" | "OFF" => LogLevelFilter::Off,
+        _ => LogLevelFilter::Info,
+    }
 }
