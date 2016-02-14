fd2421abc6ede0b7a7386e2fe8d3dd3909839693
morefreeze <morefreeze@gmail.com>
2016-02-14 15:30:05 +0800

    kvrpc: add loop for reading data

--- src/util/codec/rpc.rs
+++ src/util/codec/rpc.rs
@@ -38,7 +38,11 @@ pub fn encode_msg<T: io::Write, M: protobuf::Message>(w: &mut T,
 
 // Decodes encoded message, returns message ID.
 pub fn decode_msg<T: io::Read, M: protobuf::Message>(r: &mut T, m: &mut M) -> Result<u64> {
-    let (message_id, payload) = try!(decode_data(r));
+    // Try read, if read UnexpectedEof, then stop
+    let (message_id, payload) = match decode_data(r) {
+        Err(e) => return Err(Error::from(e)),
+        Ok((msg_id, pl)) => (msg_id, pl),
+    };
     try!(m.merge_from_bytes(&payload));
 
     Ok(message_id)
@@ -54,18 +58,6 @@ pub fn encode_data<T: io::Write>(w: &mut T, msg_id: u64, data: &[u8]) -> Result<
     Ok(())
 }
 
-// Decodes encoded data, returns message ID and body.
-pub fn decode_data<T: io::Read>(r: &mut T) -> Result<(u64, Vec<u8>)> {
-    let mut header = vec![0;MSG_HEADER_LEN];
-    try!(r.read_exact(&mut header));
-
-    let (msg_id, payload_len) = try!(decode_msg_header(&mut header));
-    let mut payload = vec![0;payload_len];
-    try!(r.read_exact(&mut payload));
-
-    Ok((msg_id, payload))
-}
-
 // Encodes msg header to a 16 bytes header buffer.
 pub fn encode_msg_header(msg_id: u64, payload_len: usize) -> Vec<u8> {
     let mut buf = vec![0;MSG_HEADER_LEN];
@@ -76,6 +68,23 @@ pub fn encode_msg_header(msg_id: u64, payload_len: usize) -> Vec<u8> {
     BigEndian::write_u64(&mut buf[8..16], msg_id);
 
     return buf;
+}
+
+// Decodes encoded data, returns message ID and body.
+pub fn decode_data<T: io::Read>(r: &mut T) -> Result<(u64, Vec<u8>)> {
+    let mut header = vec![0;MSG_HEADER_LEN];
+    let _ = r.read_exact(&mut header).map_err(|e| {
+        match e.kind() {
+            io::ErrorKind::UnexpectedEof => return Error::Io(e),
+            _ => Error::from(e),
+        }
+    });
+
+    let (msg_id, payload_len) = try!(decode_msg_header(&mut header));
+    let mut payload = vec![0;payload_len];
+    try!(r.read_exact(&mut payload));
+
+    Ok((msg_id, payload))
 }
 
 // Decodes msg header in header buffer, the buffer length size must be equal MSG_HEADER_LEN;
