e3405762618a80dc8b3d4abc3c8448db717cc9f4
siddontang <siddontang@gmail.com>
2016-02-18 11:12:51 +0800

    raftserver: do some public.
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/raftserver/store/mod.rs
+++ src/raftserver/store/mod.rs
@@ -12,4 +12,9 @@ mod peer_storage;
 mod region;
 mod util;
 
-pub use self::msg::{Msg, Sender};
+pub use self::msg::{Msg, Sender, Callback};
+pub use self::store::Store;
+pub use self::config::Config;
+pub use self::transport::Transport;
+pub use self::peer::Peer;
+pub use self::bootstrap::{bootstrap_store, bootstrap_region, bootstrap_cluster};
