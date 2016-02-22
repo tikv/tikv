c39c1feaccc4cbb612d2c37b1336644c01af89ca
disksing <i@disksing.com>
2016-02-22 17:39:53 +0800

    *: update clippy
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -17,6 +17,7 @@ pub type Value = Vec<u8>;
 pub type KvPair = (Key, Value);
 pub type Callback<T> = Box<FnBox(Result<T>) + Send>;
 
+#[allow(type_complexity)]
 pub enum Command {
     Get {
         key: Key,
