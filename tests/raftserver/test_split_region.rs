72284460d666036cd63efa0208d769e5d278bd0d
siddontang <siddontang@gmail.com>
2016-03-04 17:24:22 +0800

    raftserver: add split region test.
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

---
+++ tests/raftserver/test_split_region.rs
@@ -0,0 +1,51 @@
+use super::cluster::{Cluster, Simulator};
+use super::node::new_node_cluster;
+use super::server::new_server_cluster;
+use tikv::pd::Client;
+
+fn test_base_split_region<T: Simulator>(cluster: &mut Cluster<T>) {
+    // init_env_log();
+
+    cluster.bootstrap_region().expect("");
+    cluster.start();
+
+    let pd_client = cluster.pd_client.clone();
+    let cluster_id = cluster.id;
+
+    let region = pd_client.read().unwrap().get_region(cluster_id, b"").unwrap();
+
+    cluster.put(b"a1", b"v1");
+
+    cluster.put(b"a3", b"v3");
+
+    // Split with a2, so a1 must in left, and a3 in right.
+    cluster.split_region(region.get_region_id(), Some(b"a2".to_vec()));
+
+    let left = pd_client.read().unwrap().get_region(cluster_id, b"a1").unwrap();
+    let right = pd_client.read().unwrap().get_region(cluster_id, b"a3").unwrap();
+
+    assert_eq!(region.get_region_id(), left.get_region_id());
+    assert_eq!(region.get_start_key(), left.get_start_key());
+    assert_eq!(left.get_end_key(), right.get_start_key());
+    assert_eq!(region.get_end_key(), right.get_end_key());
+
+    cluster.put(b"a1", b"vv1");
+    assert_eq!(cluster.get(b"a1").unwrap(), b"vv1".to_vec());
+
+    cluster.put(b"a3", b"vv3");
+    assert_eq!(cluster.get(b"a3").unwrap(), b"vv3".to_vec());
+}
+
+#[test]
+fn test_node_base_split_region() {
+    let count = 5;
+    let mut cluster = new_node_cluster(0, count);
+    test_base_split_region(&mut cluster);
+}
+
+#[test]
+fn test_server_base_split_region() {
+    let count = 5;
+    let mut cluster = new_server_cluster(0, count);
+    test_base_split_region(&mut cluster);
+}
