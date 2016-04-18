c0eb0487601d65d1e8e31a8db91e2295dd53cbb4
disksing <i@disksing.com>
2016-04-18 10:34:49 +0800

    *: fix warnings

--- tests/tests.rs
+++ tests/tests.rs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(std_panic, recover)]
 #![feature(plugin)]
 #![cfg_attr(feature = "dev", plugin(clippy))]
 #![cfg_attr(not(feature = "dev"), allow(unknown_lints))]
