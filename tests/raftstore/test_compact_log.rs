5bd529c3a414469e17190eab70b069702d70c8e4
Jay <busyjaylee@gmail.com>
2016-04-21 11:20:42 +0800

    raftstore: only compact replicated log (#433)
    - raftstore: only compact replicated log
    - close #424
    
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- tests/raftstore/test_compact_log.rs
+++ tests/raftstore/test_compact_log.rs
@@ -68,6 +68,78 @@ fn test_compact_log<T: Simulator>(cluster: &mut Cluster<T>) {
     }
 }
 
+fn test_compact_limit<T: Simulator>(cluster: &mut Cluster<T>) {
+    cluster.cfg.store_cfg.raft_log_gc_limit = 1000;
+    cluster.bootstrap_region().expect("");
+    cluster.start();
+
+    cluster.put(b"a1", b"v1");
+
+    let mut before_states = HashMap::new();
+
+    for (&id, engine) in &cluster.engines {
+        let state: RaftTruncatedState = engine.get_msg(&keys::raft_truncated_state_key(1))
+                                              .unwrap()
+                                              .unwrap_or_default();
+        before_states.insert(id, state);
+    }
+
+    let leader = cluster.leader_of_region(1).unwrap().get_id();
+    let to_stop = leader % cluster.engines.len() as u64 + 1;
+    cluster.stop_node(to_stop);
+
+    for i in 1..300 {
+        let k = i.to_string().into_bytes();
+        let v = k.clone();
+        cluster.put(&k, &v);
+        let v2 = cluster.get(&k);
+        assert_eq!(v2, Some(v));
+    }
+
+    // wait log gc.
+    sleep_ms(500);
+
+    // limit has not reached, should not gc.
+    for (&id, engine) in &cluster.engines {
+        let after_state: RaftTruncatedState = engine.get_msg(&keys::raft_truncated_state_key(1))
+                                                    .unwrap()
+                                                    .unwrap_or_default();
+
+        let before_state = before_states.get(&id).unwrap();
+        let idx = after_state.get_index();
+        assert_eq!(idx, before_state.get_index());
+    }
+
+    for i in 300..600 {
+        let k = i.to_string().into_bytes();
+        let v = k.clone();
+        cluster.put(&k, &v);
+        let v2 = cluster.get(&k);
+        assert_eq!(v2, Some(v));
+    }
+
+    sleep_ms(500);
+
+    // Every peer must have compacted logs, so the truncate log state index/term must > than before.
+    for (&id, engine) in &cluster.engines {
+        if id == to_stop {
+            continue;
+        }
+        let after_state: RaftTruncatedState = engine.get_msg(&keys::raft_truncated_state_key(1))
+                                                    .unwrap()
+                                                    .unwrap_or_default();
+
+        let before_state = before_states.get(&id).unwrap();
+        let idx = after_state.get_index();
+        assert!(idx > before_state.get_index());
+
+        for i in 0..idx {
+            let key = keys::raft_log_key(1, i);
+            assert!(engine.get(&key).unwrap().is_none());
+        }
+    }
+}
+
 #[test]
 fn test_node_compact_log() {
     let count = 5;
@@ -80,4 +152,18 @@ fn test_server_compact_log() {
     let count = 5;
     let mut cluster = new_server_cluster(0, count);
     test_compact_log(&mut cluster);
+}
+
+#[test]
+fn test_node_compact_limit() {
+    let count = 5;
+    let mut cluster = new_node_cluster(0, count);
+    test_compact_limit(&mut cluster);
+}
+
+#[test]
+fn test_server_compact_limit() {
+    let count = 5;
+    let mut cluster = new_server_cluster(0, count);
+    test_compact_limit(&mut cluster);
 }
