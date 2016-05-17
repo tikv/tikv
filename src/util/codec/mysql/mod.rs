598c00d10fed5fa4295519a35f05ea3cd95b60f7
Jay <busyjaylee@gmail.com>
2016-05-17 19:34:43 +0800

    corp: add decimal support (#582)
    close #535 
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/util/codec/mysql/mod.rs
+++ src/util/codec/mysql/mod.rs
@@ -12,7 +12,9 @@
 // limitations under the License.
 
 mod duration;
+mod decimal;
 mod types;
 
 pub use self::duration::{Duration, MAX_FSP, DEFAULT_FSP};
+pub use self::decimal::{Decimal, DecimalEncoder, DecimalDecoder};
 pub use self::types::{has_unsigned_flag, has_not_null_flag};
