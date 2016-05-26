308237a1abf66733cc161e213cc34154ec4f520f
siddontang <siddontang@gmail.com>
2016-05-26 16:11:56 +0800

    raftstore: fix get_region after split. (#627)

--- tests/raftstore/cluster.rs
+++ tests/raftstore/cluster.rs
@@ -27,7 +27,7 @@ use kvproto::raft_cmdpb::*;
 use kvproto::metapb::{self, RegionEpoch};
 use kvproto::raft_serverpb::RaftMessage;
 use tikv::pd::PdClient;
-use tikv::util::HandyRwLock;
+use tikv::util::{HandyRwLock, escape};
 use tikv::server::Config as ServerConfig;
 use super::pd::TestPdClient;
 use tikv::raftstore::store::keys::data_key;
@@ -380,9 +380,19 @@ impl<T: Simulator> Cluster<T> {
     }
 
     pub fn get_region(&self, key: &[u8]) -> metapb::Region {
-        self.pd_client
-            .get_region(key)
-            .unwrap()
+        for _ in 0..100 {
+            match self.pd_client.get_region(key) {
+                Ok(region) => return region,
+                Err(_) => {
+                    // We may meet range gap after split, so here we will
+                    // retry to get the region again.
+                    sleep_ms(20);
+                    continue;
+                }
+            };
+        }
+
+        panic!("find no region for {:?}", escape(key));
     }
 
     pub fn get_region_id(&self, key: &[u8]) -> u64 {
