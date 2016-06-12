5ee8f76e0adfc5faa60bfbf96675c00894ab26da
siddontang <siddontang@gmail.com>
2016-06-12 18:02:41 +0800

    store: Address comment. [ci skip]

--- src/raftstore/store/store.rs
+++ src/raftstore/store/store.rs
@@ -368,7 +368,7 @@ impl<T: Transport, C: PdClient> Store<T, C> {
                 return true;
             }
 
-            // we receive another different message (not heartbeat), and we can think
+            // we receive another different message (not heartbeat), and it indicates
             // the leader terminates snapshot pause state and enter probe state again.
             peer.rejecting_snapshot = false;
         }
