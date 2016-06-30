c4cdf76e4c51490e916f9afb71578f6ec2d2275e
siddontang <siddontang@gmail.com>
2016-06-30 17:22:58 +0800

    scripts: check sudo exist.

--- scripts/build_rocksdb.sh
+++ scripts/build_rocksdb.sh
@@ -6,18 +6,23 @@ cd $DEPS_PATH
 
 case "$OSTYPE" in 
     linux*) 
+        SUDO=
+        if which sudo; then 
+            SUDO=sudo
+        fi
+
         if [ ! -d rocksdb-rocksdb-4.6.1 ]; then
-            sudo apt-get update 
-            sudo apt-get install -y --no-install-recommends zlib1g-dev libbz2-dev libsnappy-dev libgflags-dev liblz4-dev 
+            ${SUDO} apt-get update 
+            ${SUDO} apt-get install -y --no-install-recommends zlib1g-dev libbz2-dev libsnappy-dev libgflags-dev liblz4-dev 
             curl -L https://github.com/facebook/rocksdb/archive/rocksdb-4.6.1.tar.gz -o rocksdb.tar.gz 
             tar xf rocksdb.tar.gz 
         fi
         
         cd rocksdb-rocksdb-4.6.1 
         make shared_lib 
-        sudo make install-shared 
+        ${SUDO} make install-shared 
         # guarantee tikv can find rocksdb.
-        sudo ldconfig
+        ${SUDO} ldconfig
     ;;
     darwin*) 
         brew update 
