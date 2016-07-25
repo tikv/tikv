06e4293de4b997c87ec0b39a56e97afabe14e8a8
Jay Lee <busyjaylee@gmail.com>
2016-07-25 22:47:40 +0800

    address comment

--- docker/release.sh
+++ docker/release.sh
@@ -1,28 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
-
-error() {
-    echo $@ >&2
-    return 1
-}
-
-if [[ $# -ne 1 ]]; then
-    error $0 [hash\|tag\|branch]
+if [[ "$(ls -A . 2>/dev/null)" = "" ]]; then
+   echo please mount tikv source to /tikv first. >&2
+   exit 1
 fi
-
-if [[ -d tikv ]]; then
-    cd tikv
-    if [[ ! -d .git ]]; then
-        git init .
-        git remote add origin https://github.com/pingcap/tikv.git
-    fi
-    git fetch origin
-else
-    git clone https://github.com/pingcap/tikv.git
-    cd tikv
-fi
-git checkout $1
-git merge origin/$1
 
 scl enable devtoolset-4 python27 "ROCKSDB_SYS_STATIC=1 ROCKSDB_SYS_PORTABLE=1 make clean release" 
