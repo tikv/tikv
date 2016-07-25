11444da63fded4ae99793248c2471b9e4b98c936
Jay Lee <busyjaylee@gmail.com>
2016-07-25 14:07:01 +0800

    copr: async snap

--- tests/coprocessor/test_select.rs
+++ tests/coprocessor/test_select.rs
@@ -449,7 +449,7 @@ impl ProductTable {
 // This function will create a Product table and initialize with the specified data.
 fn init_with_data(tbl: &ProductTable,
                   vals: &[(i64, Option<&str>, i64)])
-                  -> (Store, Worker<RequestTask>) {
+                  -> (Store, Worker<EndPointTask>) {
     let engine = Arc::new(engine::new_engine(Dsn::RocksDBPath(TEMP_DIR), DEFAULT_CFS).unwrap());
     let mut store = Store::new(engine.clone());
 
@@ -463,8 +463,8 @@ fn init_with_data(tbl: &ProductTable,
     }
     store.commit();
 
-    let runner = EndPointHost::new(engine);
     let mut end_point = Worker::new("test select worker");
+    let runner = EndPointHost::new(engine, end_point.scheduler());
     end_point.start_batch(runner, 5).unwrap();
 
     (store, end_point)
@@ -780,14 +780,11 @@ fn test_reverse() {
     end_point.stop().unwrap().join().unwrap();
 }
 
-fn handle_select(end_point: &Worker<RequestTask>, req: Request) -> SelectResponse {
+fn handle_select(end_point: &Worker<EndPointTask>, req: Request) -> SelectResponse {
     let finish = Event::new();
     let finish_clone = finish.clone();
-    end_point.schedule(RequestTask::new(req,
-                                   box move |r| {
-                                       finish_clone.set(r);
-                                   }))
-        .unwrap();
+    let req = RequestTask::new(req, box move |r| finish_clone.set(r));
+    end_point.schedule(EndPointTask::Request(req)).unwrap();
     finish.wait_timeout(None);
     let resp = finish.take().unwrap().take_cop_resp();
     assert!(resp.has_data(), format!("{:?}", resp));
