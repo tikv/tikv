00bfde4a0493d2cd5ed99d6512a3e0c4a4ed932e
Jay <busyjaylee@gmail.com>
2016-07-25 14:57:23 +0800

    copr: async snap (#868)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/server/coprocessor/mod.rs
+++ src/server/coprocessor/mod.rs
@@ -57,4 +57,4 @@ impl From<txn::Error> for Error {
 }
 
 pub use self::endpoint::{Host as EndPointHost, RequestTask, SelectContext, SINGLE_GROUP,
-                         REQ_TYPE_SELECT, REQ_TYPE_INDEX};
+                         REQ_TYPE_SELECT, REQ_TYPE_INDEX, Task as EndPointTask};
