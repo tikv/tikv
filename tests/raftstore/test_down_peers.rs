9123aef8058e6c9125813964584a5a306db07e46
Huachao Huang <huachao.huang@gmail.com>
2016-07-29 11:08:25 +0800

    tests: fix test_down_peers.

--- tests/raftstore/test_down_peers.rs
+++ tests/raftstore/test_down_peers.rs
@@ -1,47 +1,80 @@
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Instant, Duration};
 
 use rand::random;
+use kvproto::pdpb;
 
 use super::node::new_node_cluster;
+use super::server::new_server_cluster;
+use super::cluster::{Cluster, Simulator};
 
-#[test]
-fn test_down_peers() {
-    let count = 5 as u64;
-    let mut cluster = new_node_cluster(0, count as usize);
+fn wait_down_peers<T: Simulator>(cluster: &Cluster<T>, count: u64) -> u64 {
+    let begin = Instant::now();
+    loop {
+        if cluster.get_down_peers().len() != count as usize {
+            sleep(Duration::from_millis(100));
+        } else {
+            break;
+        }
+    }
+    begin.elapsed().as_secs()
+}
+
+fn check_down_seconds(peer: &pdpb::PeerStats, secs: u64) {
+    assert!(peer.get_down_seconds() == secs || secs > 0 && peer.get_down_seconds() == secs - 1);
+}
+
+fn test_down_peers<T: Simulator>(cluster: &mut Cluster<T>, count: u64) {
     cluster.cfg.raft_store.max_peer_down_duration = Duration::from_millis(100);
     cluster.run();
 
+    // Kill 1, 3
     cluster.stop_node(1);
     cluster.stop_node(3);
-    sleep(Duration::from_millis(2500));
+    let secs = wait_down_peers(&cluster, 2);
+
+    // Check 1, 3 are down.
     let down_peers = cluster.get_down_peers();
-    let p1 = down_peers.get(&1).unwrap();
-    assert!(p1.get_down_seconds() == 1 || p1.get_down_seconds() == 2);
-    let p3 = down_peers.get(&3).unwrap();
-    assert!(p3.get_down_seconds() == 1 || p3.get_down_seconds() == 2);
+    check_down_seconds(down_peers.get(&1).unwrap(), secs);
+    check_down_seconds(down_peers.get(&3).unwrap(), secs);
+
+    // Restart 1, 3
     cluster.run_node(1);
     cluster.run_node(3);
-    sleep(Duration::from_millis(500));
+    wait_down_peers(&cluster, 0);
 
     for _ in 0..3 {
-        let id = random::<u64>() % count + 1;
-        cluster.stop_node(id);
-        sleep(Duration::from_millis(500));
+        let n = random::<u64>() % count + 1;
 
-        let down_peers = cluster.get_down_peers();
-        for n in 1..(count + 1) {
-            match down_peers.get(&n) {
+        // kill n.
+        cluster.stop_node(n);
+        let secs = wait_down_peers(&cluster, 1);
+
+        // Check i is down and others are not down.
+        for i in 1..(count + 1) {
+            match cluster.get_down_peers().get(&i) {
                 Some(peer) => {
-                    assert_eq!(n, id);
-                    assert_eq!(peer.get_down_seconds(), 0);
+                    assert_eq!(i, n);
+                    check_down_seconds(peer, secs);
                 }
-                None => assert!(n != id),
+                None => assert!(i != n),
             }
         }
 
-        cluster.run_node(id);
-        sleep(Duration::from_millis(500));
+        // Restart n.
+        cluster.run_node(n);
+        wait_down_peers(&cluster, 0);
     }
+}
 
+#[test]
+fn test_node_down_peers() {
+    let mut cluster = new_node_cluster(0, 5);
+    test_down_peers(&mut cluster, 5);
+}
+
+#[test]
+fn test_server_down_peers() {
+    let mut cluster = new_server_cluster(0, 5);
+    test_down_peers(&mut cluster, 5);
 }
