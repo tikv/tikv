f6453c7e7290f6f38ff83069ec005d4fc8cd0628
Huachao <huachao.huang@gmail.com>
2016-08-25 18:39:15 +0800

    pd: remove get leader addr

--- src/pd/client.rs
+++ src/pd/client.rs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::time::{Duration, Instant};
 use std::sync::Mutex;
@@ -20,21 +20,25 @@ use std::thread;
 use util::codec::rpc;
 use util::make_std_tcp_conn;
 
-use hyper::Url;
+use rand::{self, Rng};
+use hyper::{Url, Client};
+use rustc_serialize::json::Json;
 
 use kvproto::pdpb::{Request, Response};
 use kvproto::msgpb::{Message, MessageType};
 
 use super::Result;
-use super::etcd::EtcdPdClient;
 
 const MAX_PD_SEND_RETRY_COUNT: usize = 100;
 const SOCKET_READ_TIMEOUT: u64 = 3;
 const SOCKET_WRITE_TIMEOUT: u64 = 3;
 
+const PD_RPC_PREFIX: &'static str = "/pd/rpc";
+const PD_API_PREFIX: &'static str = "/pd/api/v1";
+
 #[derive(Debug)]
 struct RpcClientCore {
-    client: EtcdPdClient,
+    endpoints: String,
     stream: Option<TcpStream>,
 }
 
@@ -72,17 +76,81 @@ fn parse_urls(addr: &str) -> Result<Vec<String>> {
     Ok(hosts)
 }
 
-fn rpc_connect(addr: &str) -> Result<TcpStream> {
-    let hosts = try!(parse_urls(addr));
+fn parse_pd_members(json: &Json) -> Result<Vec<String>> {
+    let mut hosts = Vec::new();
 
-    for host in &hosts {
+    if let Some(&Json::Array(ref members)) = json.find("members") {
+        for m in members {
+            if let Some(&Json::Array(ref client_urls)) = m.find("client-urls") {
+                for url in client_urls {
+                    if let &Json::String(ref u) = url {
+                        hosts.extend(try!(parse_urls(u)))
+                    }
+                }
+            }
+        }
+    }
+
+    if hosts.len() > 0 {
+        Ok(hosts)
+    } else {
+        Err(box_err!("invalid pd members object"))
+    }
+}
+
+fn get_pd_members(endpoints: &str) -> Result<Vec<String>> {
+    let mut c = Client::new();
+    c.set_read_timeout(Some(Duration::from_secs(SOCKET_READ_TIMEOUT)));
+    c.set_write_timeout(Some(Duration::from_secs(SOCKET_WRITE_TIMEOUT)));
+
+    for host in endpoints.split(',') {
+        // Request for pd members.
+        let url = format!("http://{}/{}/members", host, PD_API_PREFIX);
+        let req = c.get(&url);
+        let mut resp = match req.send() {
+            Ok(resp) => resp,
+            Err(e) => {
+                warn!("request pd members failed: {:?}", e);
+                continue;
+            }
+        };
+
+        // Read response body.
+        let mut data = String::new();
+        if let Err(e) = resp.read_to_string(&mut data) {
+            warn!("read pd members failed: {:?}", e);
+            continue;
+        }
+
+        // Parse response body as JSON.
+        let json = match Json::from_str(&data) {
+            Ok(json) => json,
+            Err(e) => {
+                warn!("parse pd members failed: {:?}", e);
+                continue;
+            }
+        };
+
+        return parse_pd_members(&json);
+    }
+
+    Err(box_err!("get pd members failed"))
+}
+
+fn rpc_connect(hosts: &[String]) -> Result<TcpStream> {
+    // Randomize hosts.
+    let mut random_hosts = hosts.to_owned();
+    rand::thread_rng().shuffle(&mut random_hosts);
+
+    for host in &random_hosts {
         let mut stream = match make_std_tcp_conn(host.as_str()) {
             Ok(stream) => stream,
             Err(_) => continue,
         };
 
         // Send a HTTP header to tell PD to hijack this connection for RPC.
-        let header = b"GET /pd/rpc HTTP/1.0\r\n\r\n";
+        let header_str = format!("GET {} HTTP/1.0\r\n\r\n", PD_RPC_PREFIX);
+        let header = header_str.as_bytes();
         try!(stream.set_write_timeout(Some(Duration::from_secs(SOCKET_WRITE_TIMEOUT))));
         match stream.write(header) {
             Ok(n) if n == header.len() => {
@@ -99,22 +167,20 @@ fn rpc_connect(addr: &str) -> Result<TcpStream> {
         }
     }
 
-    Err(box_err!("connect to {} failed", addr))
+    Err(box_err!("failed connect to {:?}", hosts))
 }
 
 impl RpcClientCore {
-    fn new(client: EtcdPdClient) -> RpcClientCore {
+    fn new(endpoints: &str) -> RpcClientCore {
         RpcClientCore {
-            client: client,
+            endpoints: endpoints.into(),
             stream: None,
         }
     }
 
     fn try_connect(&mut self) -> Result<()> {
-        let addr = box_try!(self.client.get_leader_addr());
-        info!("get pd leader {}", addr);
-
-        let stream = try!(rpc_connect(&addr));
+        let hosts = try!(get_pd_members(&self.endpoints));
+        let stream = try!(rpc_connect(&hosts));
         self.stream = Some(stream);
         Ok(())
     }
@@ -166,10 +232,10 @@ pub struct RpcClient {
 }
 
 impl RpcClient {
-    pub fn new(client: EtcdPdClient, cluster_id: u64) -> Result<RpcClient> {
+    pub fn new(endpoints: &str, cluster_id: u64) -> Result<RpcClient> {
         Ok(RpcClient {
             msg_id: AtomicUsize::new(0),
-            core: Mutex::new(RpcClientCore::new(client)),
+            core: Mutex::new(RpcClientCore::new(endpoints)),
             cluster_id: cluster_id,
         })
     }
@@ -187,7 +253,13 @@ impl RpcClient {
 
 #[cfg(test)]
 mod test {
+    use std::env;
+
+    use rustc_serialize::json::Json;
+
     use super::parse_urls;
+    use super::parse_pd_members;
+    use super::get_pd_members;
 
     #[test]
     fn test_parse_urls() {
@@ -216,5 +288,68 @@ mod test {
         assert_eq!(hosts[0], "127.0.0.1:8080");
         assert_eq!(hosts[1], "example.com:2379");
         assert_eq!(hosts[2], "example.com:443");
+    }
+
+    #[test]
+    fn test_parse_pd_members() {
+        let valid_case = r#"{
+            "members": [{
+                "name": "pd1",
+                "peer-urls": ["http://localhost:1001"],
+                "client-urls": ["http://localhost:1001"]
+            }, {
+                "name": "pd2",
+                "peer-urls": ["http://localhost:2001"],
+                "client-urls": ["http://localhost:2001","http://localhost:2002"]
+            }, {
+                "name": "pd3",
+                "peer-urls": ["http://localhost:3001"],
+                "client-urls": ["http://localhost:3001","http://localhost:3002","http://localhost:3003"]
+            }]}"#;
+
+        let json = Json::from_str(valid_case).unwrap();
+        let hosts = parse_pd_members(&json).unwrap();
+        assert_eq!(hosts.len(), 6);
+        assert_eq!(hosts[0], "localhost:1001");
+        assert_eq!(hosts[1], "localhost:2001");
+        assert_eq!(hosts[2], "localhost:2002");
+        assert_eq!(hosts[3], "localhost:3001");
+        assert_eq!(hosts[4], "localhost:3002");
+        assert_eq!(hosts[5], "localhost:3003");
+
+        let invalid_cases = vec![
+            r#"{}"#,
+            r#"{"example": ""}"#,
+            r#"{"members": ""}"#,
+            r#"{"members": []}"#,
+            r#"{"members": [{}]}"#,
+            r#"{"members": [{"client-urls": []}]}"#,
+        ];
+
+        for s in &invalid_cases {
+            let json = Json::from_str(s).unwrap();
+            assert!(parse_pd_members(&json).is_err());
+        }
+    }
+
+    #[test]
+    fn test_get_pd_members() {
+        // We need to set all members in PD_ENDPOINTS to pass this test.
+        let endpoints = match env::var("PD_ENDPOINTS") {
+            Ok(v) => v,
+            Err(_) => return,
+        };
+
+        let mut members = get_pd_members(&endpoints).unwrap();
+        members.sort();
+        println!("get pd members: {:?}", members);
+
+        let mut hosts: Vec<_> = endpoints.split(',').collect();
+        hosts.sort();
+
+        assert_eq!(hosts.len(), members.len());
+        for i in 0..hosts.len() {
+            assert_eq!(hosts[i], members[i]);
+        }
     }
 }
