5e0de95b2b8fff1acc88dbc93cea68e805461bc6
overvenus <overvenus@gmail.com>
2016-09-06 14:40:14 +0800

    a readable escape

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::ascii;
 use std::ops::Deref;
 use std::ops::DerefMut;
 use std::io::{self, Write};
@@ -175,21 +176,16 @@ pub fn escape(data: &[u8]) -> String {
     let mut escaped = Vec::with_capacity(data.len() * 4);
     for &c in data {
         match c {
-            b'\n' => escaped.extend_from_slice(br"\n"),
-            b'\r' => escaped.extend_from_slice(br"\r"),
-            b'\t' => escaped.extend_from_slice(br"\t"),
             b'"' => escaped.extend_from_slice(b"\\\""),
             b'\\' => escaped.extend_from_slice(br"\\"),
-            b'\x20' |
-            b'\x21' |
-            b'\x23'...b'\x5b' |
-            b'\x5d'...b'\x7e' => escaped.push(c),
-            _ => {
+            b'\'' => escaped.push(b'\''),
+            _ if c > b'\x7e' => {
                 escaped.push(b'\\');
                 escaped.push(b'0' + (c >> 6));
                 escaped.push(b'0' + ((c >> 3) & 7));
                 escaped.push(b'0' + (c & 7));
             }
+            _ => escaped.extend(ascii::escape_default(c)),
         }
     }
     escaped.shrink_to_fit();
