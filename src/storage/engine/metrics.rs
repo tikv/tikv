aa0a26ff40d1842fc8c69f432053dd5eee46e124
siddontang <siddontang@gmail.com>
2016-09-20 16:14:33 +0800

    engine: remove some metrics. (#1089)

--- src/storage/engine/metrics.rs
+++ src/storage/engine/metrics.rs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use prometheus::{CounterVec, HistogramVec, exponential_buckets};
+use prometheus::{Counter, CounterVec, HistogramVec, exponential_buckets};
 
 lazy_static! {
     pub static ref ASYNC_REQUESTS_COUNTER_VEC: CounterVec =
@@ -30,24 +30,9 @@ lazy_static! {
             &["type"]
         ).unwrap();
 
-    pub static ref ENGINE_OP_COUNTER_VEC: CounterVec =
-        register_counter_vec!(
-            "tikv_storage_engine_op_total",
-            "Total number of engine operators",
-            &["type", "cf"]
-        ).unwrap();
-
-    pub static ref SNAPSHOT_OP_COUNTER_VEC: CounterVec =
-        register_counter_vec!(
-            "tikv_storage_engine_snapshot_op_total",
-            "Total number of snapshot operators",
-            &["type", "cf"]
-        ).unwrap();
-
-    pub static ref CURSOR_OP_COUNTER_VEC: CounterVec =
-        register_counter_vec!(
-            "tikv_storage_engine_cursor_op_total",
-            "Total number of cursor operators",
-            &["type"]
+    pub static ref CURSOR_OVER_SEEK_BOUND_COUNTER: Counter =
+        register_counter!(
+            "tikv_storage_engine_cursor_over_seek_bound_total",
+            "Total number of cursor next over seek bound number"
         ).unwrap();
 }
