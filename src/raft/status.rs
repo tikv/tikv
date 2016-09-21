ce8e9ac51c9b5ecf96fb897acf2be1b916c61457
Dylan.Wen <hhkbp2@gmail.com>
2016-09-21 16:35:01 +0800

    raft: update LICENSE headers

--- src/raft/status.rs
+++ src/raft/status.rs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright 2015 CoreOS, Inc.
+// Copyright 2015 The etcd Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
