23364df3e6fe05c28dae2225f7d435aea62151de
Jay <busyjaylee@gmail.com>
2016-10-15 20:59:51 +0800

    docker: speedup release build (#1175)

--- docker/release.sh
+++ docker/release.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ "$(ls -A . 2>/dev/null)" = "" ]]; then
-   echo please mount tikv source to /tikv first. >&2
-   exit 1
+clean_target=clean
+
+if [[ $1 == "--use-cache" ]]; then
+    clean_target=
 fi
 
-scl enable devtoolset-4 python27 "ROCKSDB_SYS_STATIC=1 ROCKSDB_SYS_PORTABLE=1 make clean release" 
+if [[ "$(ls -A . 2>/dev/null)" = "" ]]; then
+    echo please mount tikv source to /tikv first. >&2
+    exit 1
+fi
+
+scl enable devtoolset-4 python27 "ROCKSDB_SYS_STATIC=1 ROCKSDB_SYS_PORTABLE=1 make $clean_target release" 
+
