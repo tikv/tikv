7d64d915cb57f654324b405c800e5eb62a85342b
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2016-10-28 13:48:05 +0800

    add tests

--- tests/raftstore/test_compact_log.rs
+++ tests/raftstore/test_compact_log.rs
@@ -144,6 +144,58 @@ fn test_compact_limit<T: Simulator>(cluster: &mut Cluster<T>) {
     }
 }
 
+fn test_compact_many_times<T: Simulator>(cluster: &mut Cluster<T>) {
+    let gc_limit: u64 = 100;
+    cluster.cfg.raft_store.raft_log_gc_limit = gc_limit;
+    cluster.cfg.raft_store.raft_log_gc_threshold = 50;
+    cluster.cfg.raft_store.raft_log_gc_tick_interval = 100; // 100 ms
+    cluster.run();
+
+    cluster.must_put(b"k1", b"v1");
+
+    let mut before_states = HashMap::new();
+
+    for (&id, engine) in &cluster.engines {
+        must_get_equal(engine, b"k1", b"v1");
+        let mut state: RaftApplyState =
+        get_msg_cf_or_default(engine, CF_RAFT, &keys::apply_state_key(1));
+        let state = state.take_truncated_state();
+        // compact should not start
+        assert_eq!(RAFT_INIT_LOG_INDEX, state.get_index());
+        assert_eq!(RAFT_INIT_LOG_TERM, state.get_term());
+        before_states.insert(id, state);
+    }
+
+    for i in 1..500 {
+        let k = i.to_string().into_bytes();
+        let v = k.clone();
+        cluster.must_put(&k, &v);
+        let v2 = cluster.get(&k);
+        assert_eq!(v2, Some(v));
+    }
+
+    // wait log gc.
+    sleep_ms(1000);
+
+    // Every peer must have compacted logs, so the truncate log state index/term must > than before.
+    for (&id, engine) in &cluster.engines {
+        let mut state: RaftApplyState =
+        get_msg_cf_or_default(engine, CF_RAFT, &keys::apply_state_key(1));
+        let after_state = state.take_truncated_state();
+
+        let before_state = before_states.get(&id).unwrap();
+        let idx = after_state.get_index();
+        // Compact raft log at least 2 times.
+        assert!(idx - before_state.get_index() >= gc_limit * 2);
+
+        let handle = get_cf_handle(engine, CF_RAFT).unwrap();
+        for i in 0..idx {
+            let key = keys::raft_log_key(1, i);
+            assert!(engine.get_cf(handle, &key).unwrap().is_none());
+        }
+    }
+}
+
 #[test]
 fn test_node_compact_log() {
     let count = 5;
@@ -170,4 +222,18 @@ fn test_server_compact_limit() {
     let count = 5;
     let mut cluster = new_server_cluster(0, count);
     test_compact_limit(&mut cluster);
+}
+
+#[test]
+fn test_node_compact_many_times() {
+    let count = 5;
+    let mut cluster = new_node_cluster(0, count);
+    test_compact_many_times(&mut cluster);
+}
+
+#[test]
+fn test_server_compact_many_times() {
+    let count = 5;
+    let mut cluster = new_server_cluster(0, count);
+    test_compact_many_times(&mut cluster);
 }
