12cb9bdeacf646eedf741ddb36c9f925bb5e9aa4
overvenus <overvenus@gmail.com>
2016-11-07 16:20:20 +0800

    util: add validate_addrs

--- src/util/config.rs
+++ src/util/config.rs
@@ -11,17 +11,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::str::FromStr;
+use std::net::{SocketAddrV4, SocketAddrV6};
+
 use rocksdb::{DBCompressionType, DBRecoveryMode};
 
 quick_error! {
     #[derive(Debug)]
-    pub enum ParseConfigError {
+    pub enum ConfigError {
         RocksDB
         ReadableNumber
+        PDList(msg: String) {
+            description(&msg)
+            display("{}", msg)
+        }
     }
 }
 
-pub fn parse_rocksdb_compression(tp: &str) -> Result<DBCompressionType, ParseConfigError> {
+pub fn parse_rocksdb_compression(tp: &str) -> Result<DBCompressionType, ConfigError> {
     match &*tp.to_lowercase() {
         "no" => Ok(DBCompressionType::DBNo),
         "snappy" => Ok(DBCompressionType::DBSnappy),
@@ -29,12 +36,12 @@ pub fn parse_rocksdb_compression(tp: &str) -> Result<DBCompressionType, ParseCon
         "bzip2" => Ok(DBCompressionType::DBBz2),
         "lz4" => Ok(DBCompressionType::DBLz4),
         "lz4hc" => Ok(DBCompressionType::DBLz4hc),
-        _ => Err(ParseConfigError::RocksDB),
+        _ => Err(ConfigError::RocksDB),
     }
 }
 
 pub fn parse_rocksdb_per_level_compression(tp: &str)
-                                           -> Result<Vec<DBCompressionType>, ParseConfigError> {
+                                           -> Result<Vec<DBCompressionType>, ConfigError> {
     let mut result: Vec<DBCompressionType> = vec![];
     let v: Vec<&str> = tp.split(':').collect();
     for i in &v {
@@ -45,24 +52,24 @@ pub fn parse_rocksdb_per_level_compression(tp: &str)
             "bzip2" => result.push(DBCompressionType::DBBz2),
             "lz4" => result.push(DBCompressionType::DBLz4),
             "lz4hc" => result.push(DBCompressionType::DBLz4hc),
-            _ => return Err(ParseConfigError::RocksDB),
+            _ => return Err(ConfigError::RocksDB),
         }
     }
 
     Ok(result)
 }
 
-pub fn parse_rocksdb_wal_recovery_mode(mode: i64) -> Result<DBRecoveryMode, ParseConfigError> {
+pub fn parse_rocksdb_wal_recovery_mode(mode: i64) -> Result<DBRecoveryMode, ConfigError> {
     match mode {
         0 => Ok(DBRecoveryMode::TolerateCorruptedTailRecords),
         1 => Ok(DBRecoveryMode::AbsoluteConsistency),
         2 => Ok(DBRecoveryMode::PointInTime),
         3 => Ok(DBRecoveryMode::SkipAnyCorruptedRecords),
-        _ => Err(ParseConfigError::RocksDB),
+        _ => Err(ConfigError::RocksDB),
     }
 }
 
-fn split_property(property: &str) -> Result<(f64, &str), ParseConfigError> {
+fn split_property(property: &str) -> Result<(f64, &str), ConfigError> {
     let mut indx = 0;
     for s in property.chars() {
         match s {
@@ -76,7 +83,7 @@ fn split_property(property: &str) -> Result<(f64, &str), ParseConfigError> {
     }
 
     let (num, unit) = property.split_at(indx);
-    num.parse::<f64>().map(|f| (f, unit)).or(Err(ParseConfigError::ReadableNumber))
+    num.parse::<f64>().map(|f| (f, unit)).or(Err(ConfigError::ReadableNumber))
 }
 
 const UNIT: usize = 1;
@@ -96,7 +103,7 @@ const SECOND: usize = MS * TIME_MAGNITUDE_1;
 const MINTUE: usize = SECOND * TIME_MAGNITUDE_2;
 const HOUR: usize = MINTUE * TIME_MAGNITUDE_2;
 
-pub fn parse_readable_int(size: &str) -> Result<i64, ParseConfigError> {
+pub fn parse_readable_int(size: &str) -> Result<i64, ConfigError> {
     let (num, unit) = try!(split_property(size));
 
     match &*unit.to_lowercase() {
@@ -113,8 +120,65 @@ pub fn parse_readable_int(size: &str) -> Result<i64, ParseConfigError> {
         "m" => Ok((num * (MINTUE as f64)) as i64),
         "h" => Ok((num * (HOUR as f64)) as i64),
 
-        _ => Err(ParseConfigError::ReadableNumber),
+        _ => Err(ConfigError::ReadableNumber),
     }
+}
+
+/// `validate_addrs` validates a list of addresses. Addresses are formed like "Host:Port".
+/// More details about **Host** and **Port** can be found in RFC3986 section 3.2.2 and 3.2.3.
+pub fn validate_addrs(addrs: &[&str]) -> Result<(), ConfigError> {
+    fn new_err(addr: &str) -> ConfigError {
+        ConfigError::PDList(format!("invalid addr: {}", addr))
+    }
+
+    for addr in addrs {
+        if let (Err(_), Err(_)) = (SocketAddrV4::from_str(addr), SocketAddrV6::from_str(addr)) {
+            let mut colons = 0;
+            for c in addr.chars() {
+                match c as u8 {
+                    b':' => {
+                        colons += 1;
+                    }
+
+                    // No scheme, path, query and fragment.
+                    //
+                    // gen-delims = ":" / "/" / "?" / "#" / "[" / "]" / "@"
+                    b'/' | b'?' | b'=' | b'#' |
+
+                    // Other reserved characters.
+                    //
+                    // sub-delims = "!" / "$" / "&" / "'" / "(" / ")"
+                    //              / "*" / "+" / "," / ";" / "="
+                    b'!' | b'$' | b'&' | b'\'' | b'(' | b')' | b'*' | b'+' | b',' | b';' |
+                    b'[' | b']' | b'@' => return Err(new_err(addr)),
+
+                    _ => (),
+                }
+            }
+
+            // Check port.
+            if colons != 1 {
+                return Err(new_err(addr));
+            }
+
+            if let Some(idx) = addr.find(':') {
+                // Leading or trailing colons are invalid.
+                if idx == 0 || idx == addr.len() - 1 {
+                    return Err(new_err(addr));
+                }
+
+                // Port range from 1 to 65535.
+                let port: u32 = try!(addr[(idx + 1)..]
+                    .parse()
+                    .map_err(|_| ConfigError::PDList(format!("invalid addr: {}", addr))));
+                if port > 65535 || port == 0 {
+                    return Err(new_err(addr));
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -189,5 +253,47 @@ mod test {
                 parse_rocksdb_wal_recovery_mode(3).unwrap());
 
         assert!(parse_rocksdb_wal_recovery_mode(4).is_err());
+    }
+
+    #[test]
+    fn test_validate_addrs() {
+        let table = vec![
+            (vec!["127.0.0.1:8080",], true),
+            (vec!["[::1]:8080",], true),
+            (vec!["localhost:8080",], true),
+            (vec!["pingcap.com:8080",], true),
+            (vec!["funnydomain:8080",], true),
+
+            (vec!["127.0.0.1",], false),
+            (vec!["[::1]",], false),
+            (vec!["localhost",], false),
+            (vec!["pingcap.com",], false),
+            (vec!["funnydomain",], false),
+            (vec!["funnydomain:",], false),
+
+            (vec!["root@google.com:8080",], false),
+            (vec!["http://google.com:8080",], false),
+            (vec!["google.com:8080/path",], false),
+            (vec!["http://google.com:8080/path",], false),
+            (vec!["http://google.com:8080/path?lang=en",], false),
+            (vec!["http://google.com:8080/path?lang=en#top",], false),
+
+            (vec!["ftp://ftp.is.co.za/rfc/rfc1808.txt",], false),
+            (vec!["http://www.ietf.org/rfc/rfc2396.txt",], false),
+            (vec!["ldap://[2001:db8::7]/c=GB?objectClass?one",], false),
+            (vec!["mailto:John.Doe@example.com",], false),
+            (vec!["news:comp.infosystems.www.servers.unix",], false),
+            (vec!["tel:+1-816-555-1212",], false),
+            (vec!["telnet://192.0.2.16:80/",], false),
+            (vec!["urn:oasis:names:specification:docbook:dtd:xml:4.1.2",], false),
+
+            (vec![":8080",], false),
+            (vec!["8080",], false),
+            (vec!["8080:",], false),
+        ];
+
+        for (addrs, is_ok) in table {
+            assert_eq!(validate_addrs(&addrs).is_ok(), is_ok);
+        }
     }
 }
