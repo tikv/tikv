2165ec1cf610b56baebf951a30458458e620e579
siddontang <siddontang@gmail.com>
2016-11-10 12:03:23 +0800

    store: use 16MB as the max log size metric.

--- src/raftstore/store/metrics.rs
+++ src/raftstore/store/metrics.rs
@@ -89,7 +89,7 @@ lazy_static! {
                 "tikv_raftstore_propose_log_size",
                 "Bucketed histogram of peer proposing log size",
                 [ vec![256.0, 512.0, 1024.0, 4096.0, 65536.0, 262144.0, 524288.0, 1048576.0,
-                       2097152.0, 4194304.0, 16777216.0, 67108864.0] ]
+                       2097152.0, 4194304.0, 8388608.0, 16777216.0] ]
             }
         ).unwrap();
 }
