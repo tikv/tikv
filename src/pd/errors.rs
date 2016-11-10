81fccf7760d7c6110a071db3e0ac59f3fa824d21
Huachao Huang <huachao.huang@gmail.com>
2016-11-10 17:32:32 +0800

    pd: add region is shutdown error

--- src/pd/errors.rs
+++ src/pd/errors.rs
@@ -36,6 +36,9 @@ quick_error!{
             description("cluster not bootstrap error")
             display("cluster {} is not bootstrapped", cluster_id)
         }
+        RegionIsShutdown {
+            description("region is merged and shutdown")
+        }
         Other(err: Box<error::Error + Sync + Send>) {
             from()
             cause(err.as_ref())
