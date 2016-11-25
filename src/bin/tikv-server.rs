ca30e2b9fd80b61b65a41d9918497177801993ef
Neil Shen <overvenus@gmail.com>
2016-11-25 20:48:47 +0800

    Strict check advertise address (#1345)
    * bin: check advertise address more strictly

--- src/bin/tikv-server.rs
+++ src/bin/tikv-server.rs
@@ -182,27 +182,22 @@ fn check_system_config(config: &toml::Value) {
     }
 }
 
-fn listen_address(matches: &Matches, config: &toml::Value) -> String {
-    let addr = get_flag_string(matches, "A").unwrap_or_else(|| {
-        get_toml_string(config,
-                        "server.addr",
-                        Some(DEFAULT_LISTENING_ADDR.to_owned()))
-    });
-    util::config::check_addr(&addr)
-        .map_err(|e| exit_with_err(format!("{:?}", e)))
-        .unwrap();
-
-    let adv_addr = get_flag_string(matches, "advertise-addr")
-        .unwrap_or_else(|| get_toml_string(config, "server.advertise-addr", Some(addr.clone())));
-    util::config::check_addr(&adv_addr)
-        .map_err(|e| exit_with_err(format!("{:?}", e)))
-        .unwrap();
-
-    if let Some(_) = adv_addr.find("0.0.0.0") {
-        exit_with_err("0.0.0.0 is not allowed in advertise-addr".to_owned());
+fn check_advertise_address(addr: &str) {
+    if let Err(e) = util::config::check_addr(addr) {
+        exit_with_err(format!("{:?}", e));
     }
 
-    addr
+    // FIXME: Forbidden addresses ending in 0 or 255? Those are not always invalid.
+    // See more: https://en.wikipedia.org/wiki/IPv4#Addresses_ending_in_0_or_255
+    let invalid_patterns = [("0.", 0)]; // Current network is not allowed.
+
+    for &(pat, pos) in &invalid_patterns {
+        if let Some(idx) = addr.find(pat) {
+            if pos == idx {
+                exit_with_err(format!("invalid advertise-addr: {:?}", addr));
+            }
+        }
+    }
 }
 
 fn get_rocksdb_db_option(config: &toml::Value) -> RocksdbOptions {
@@ -386,6 +381,7 @@ fn build_cfg(matches: &Matches, config: &toml::Value, cluster_id: u64, addr: &st
     // If no advertise listening address set, use the associated listening address.
     cfg.advertise_addr = get_flag_string(matches, "advertise-addr")
         .unwrap_or_else(|| get_toml_string(config, "server.advertise-addr", Some(addr.to_owned())));
+    check_advertise_address(&cfg.advertise_addr);
 
     cfg.send_buffer_size =
         get_toml_int(config, "server.send-buffer-size", Some(128 * 1024)) as usize;
@@ -683,7 +679,15 @@ fn main() {
     // Before any startup, check system configuration.
     check_system_config(&config);
 
-    let addr = listen_address(&matches, &config);
+    let addr = get_flag_string(&matches, "A").unwrap_or_else(|| {
+        let addr = get_toml_string(&config,
+                                   "server.addr",
+                                   Some(DEFAULT_LISTENING_ADDR.to_owned()));
+        if let Err(e) = util::config::check_addr(&addr) {
+            exit_with_err(format!("{:?}", e));
+        }
+        addr
+    });
     info!("Start listening on {}...", addr);
     let listener = bind(&addr).unwrap();
 
