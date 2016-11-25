b78205a32094568e61ad63e122ad04bbe6b8c8b2
Jay Lee <busyjaylee@gmail.com>
2016-11-25 14:27:42 +0800

    copr: add running tasks upper bound

--- src/server/coprocessor/metrics.rs
+++ src/server/coprocessor/metrics.rs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use prometheus::HistogramVec;
+use prometheus::{HistogramVec, CounterVec};
 
 lazy_static! {
     pub static ref COPR_REQ_HISTOGRAM_VEC: HistogramVec =
@@ -26,5 +26,12 @@ lazy_static! {
             "tikv_coprocessor_outdated_request_wait_seconds",
             "Bucketed histogram of outdated coprocessor request wait duration",
             &["type", "req"]
+        ).unwrap();
+
+    pub static ref COPR_REQ_ERROR: CounterVec =
+        register_counter_vec!(
+            "tikv_coprocessor_request_error",
+            "Total number of push down request error.",
+            &["type", "reason"]
         ).unwrap();
 }
