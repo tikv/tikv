369cc8dc4384d619974e3a32f80da0eb7b5ff561
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2016-11-30 14:30:29 +0800

    add rate limiter for rocksdb

--- src/bin/tikv-server.rs
+++ src/bin/tikv-server.rs
@@ -232,6 +232,11 @@ fn get_rocksdb_db_option(config: &toml::Value) -> RocksdbOptions {
         opts.set_stats_dump_period_sec(stats_dump_period_sec as usize);
     }
 
+    let rate_bytes_per_sec = get_toml_int(config, "rocksdb.rate-bytes-per-sec", Some(0));
+    if rate_bytes_per_sec > 0 {
+        opts.set_ratelimiter(rate_bytes_per_sec as i64);
+    }
+
     opts
 }
 
