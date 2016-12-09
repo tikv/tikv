b27423877e94c5a3afd9a20dcb8ff0534a6502fe
Jay Lee <busyjaylee@gmail.com>
2016-12-09 15:30:22 +0800

    fix ci

--- tests/raftstore/test_region_heartbeat.rs
+++ tests/raftstore/test_region_heartbeat.rs
@@ -2,8 +2,6 @@ use std::thread::sleep;
 use std::sync::mpsc;
 use std::time::{Instant, Duration};
 
-use rand::random;
-use kvproto::pdpb;
 use tikv::util::HandyRwLock;
 
 use super::util;
@@ -23,130 +21,63 @@ fn wait_down_peers<T: Simulator>(cluster: &Cluster<T>, count: u64) {
     }
 }
 
-fn check_down_seconds(peer: &pdpb::PeerStats, secs: u64) {
-    debug!("down {} secs {}", peer.get_down_seconds(), secs);
-    assert!(peer.get_down_seconds() <= secs);
-}
-
-fn test_leader_down_and_become_leader_again<T: Simulator>(cluster: &mut Cluster<T>, count: u64) {
-    // Isolate node.
-    let node = cluster.leader_of_region(1).unwrap();
-    let node_id = node.get_id();
-    cluster.add_send_filter(IsolationFilterFactory::new(node_id));
-    debug!("node: {:?}", node);
-
-    // Kill another node.
-    let next_id = if node_id < count { node_id + 1 } else { 1 };
-    cluster.stop_node(next_id);
-
-    // Wait other node to become leader.
-    let begin = Instant::now();
-    for _ in 1..100 {
-        cluster.reset_leader_of_region(1);
-        if let Some(peer) = cluster.leader_of_region(1) {
-            if peer.get_id() != node_id {
-                break;
-            }
-        }
-        sleep(Duration::from_millis(100));
-    }
-    assert!(node_id != cluster.leader_of_region(1).unwrap().get_id());
-    wait_down_peers(cluster, 2);
-
-    // Check node and another are down.
-    let down_secs = begin.elapsed().as_secs();
-    let down_peers = cluster.get_down_peers();
-    debug!("down_secs: {} down_peers: {:?}", down_secs, down_peers);
-    check_down_seconds(down_peers.get(&node_id).unwrap(), down_secs);
-    check_down_seconds(down_peers.get(&next_id).unwrap(), down_secs);
-
-    // Restart node and sleep a few seconds.
-    let sleep_secs = 3;
-    cluster.clear_send_filters();
-    wait_down_peers(cluster, 1);
-    sleep(Duration::from_secs(sleep_secs));
-
-    // Wait node to become leader again.
-    for _ in 1..100 {
-        cluster.transfer_leader(1, node.clone());
-        if let Some(peer) = cluster.leader_of_region(1) {
-            if peer.get_id() == node_id {
-                break;
-            }
-        }
-        sleep(Duration::from_millis(100));
-    }
-    assert!(node_id == cluster.leader_of_region(1).unwrap().get_id());
-    wait_down_peers(cluster, 1);
-
-    // Ensure that node will not reuse the previous peer heartbeats.
-    let prev_secs = cluster.get_down_peers().get(&next_id).unwrap().get_down_seconds();
-    for _ in 1..100 {
-        let down_secs = cluster.get_down_peers().get(&next_id).unwrap().get_down_seconds();
-        if down_secs != prev_secs {
-            assert!(down_secs < sleep_secs);
-            break;
-        }
-        sleep(Duration::from_millis(100));
-    }
-}
-
-fn test_down_peers<T: Simulator>(cluster: &mut Cluster<T>, count: u64) {
+fn test_down_peers<T: Simulator>(cluster: &mut Cluster<T>) {
     cluster.cfg.raft_store.max_peer_down_duration = Duration::from_millis(500);
-    let t = Instant::now();
     cluster.run();
 
-    // Kill 1, 3
-    cluster.stop_node(1);
-    cluster.stop_node(3);
-    wait_down_peers(cluster, 2);
-
-    // Check 1, 3 are down.
-    let down_peers = cluster.get_down_peers();
-    check_down_seconds(down_peers.get(&1).unwrap(), t.elapsed().as_secs() + 1);
-    check_down_seconds(down_peers.get(&3).unwrap(), t.elapsed().as_secs() + 1);
-
-    // Restart 1, 3
-    cluster.run_node(1);
-    cluster.run_node(3);
-    wait_down_peers(cluster, 0);
-
-    for _ in 0..3 {
-        let n = random::<u64>() % count + 1;
-
-        let t = Instant::now();
-        cluster.must_put(b"k1", b"v1");
-        util::must_get_equal(&cluster.get_engine(n), b"k1", b"v1");
-
-        // kill n.
-        cluster.stop_node(n);
-
-        wait_down_peers(cluster, 1);
-
-        // Check i is down and others are not down.
+    // Kill 1, 2
+    for len in 1..3 {
+        let id = len;
+        cluster.stop_node(id);
+        wait_down_peers(cluster, len);
         let down_peers = cluster.get_down_peers();
-        assert_eq!(down_peers.len(), 1);
-        let peer = down_peers.get(&n).unwrap();
-        check_down_seconds(peer, t.elapsed().as_secs() + 1);
-
-        // Restart n.
-        cluster.run_node(n);
-        wait_down_peers(cluster, 0);
+        assert!(down_peers.contains_key(&id));
     }
 
-    test_leader_down_and_become_leader_again(cluster, count);
+    // Restart 1, 2
+    cluster.run_node(1);
+    cluster.run_node(2);
+    wait_down_peers(cluster, 0);
+
+    cluster.stop_node(1);
+
+    cluster.must_put(b"k1", b"v1");
+    // max peer down duration is 500 millis, but we only report down time in seconds,
+    // so sleep 1 second to make the old down second is always larger than new down second
+    // by at lease 1 second.
+    util::sleep_ms(1000);
+
+    wait_down_peers(cluster, 1);
+    let down_secs = cluster.get_down_peers()[&1].get_down_seconds();
+    let timer = Instant::now();
+    let leader = cluster.leader_of_region(1).unwrap();
+    let new_leader = if leader.get_id() == 2 {
+        util::new_peer(3, 3)
+    } else {
+        util::new_peer(2, 2)
+    };
+
+    cluster.must_transfer_leader(1, new_leader);
+    wait_down_peers(cluster, 1);
+    assert!(cluster.get_down_peers()[&1].get_down_seconds() <
+            down_secs + timer.elapsed().as_secs());
+
+    // Ensure that node will not reuse the previous peer heartbeats.
+    cluster.must_transfer_leader(1, leader);
+    wait_down_peers(cluster, 1);
+    assert!(cluster.get_down_peers()[&1].get_down_seconds() < timer.elapsed().as_secs() + 1);
 }
 
 #[test]
 fn test_node_down_peers() {
     let mut cluster = new_node_cluster(0, 5);
-    test_down_peers(&mut cluster, 5);
+    test_down_peers(&mut cluster);
 }
 
 #[test]
 fn test_server_down_peers() {
     let mut cluster = new_server_cluster(0, 5);
-    test_down_peers(&mut cluster, 5);
+    test_down_peers(&mut cluster);
 }
 
 fn test_pending_peers<T: Simulator>(cluster: &mut Cluster<T>) {
