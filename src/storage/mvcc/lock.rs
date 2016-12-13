19713983a070fbca39405c24718808ea5bb4413e
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2016-12-13 10:38:24 +0800

    address comments && enlarge short threshold from 32 to 64

--- src/storage/mvcc/lock.rs
+++ src/storage/mvcc/lock.rs
@@ -83,7 +83,7 @@ impl Lock {
 
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut b = Vec::with_capacity(1 + MAX_VAR_U64_LEN + self.primary.len() + MAX_VAR_U64_LEN +
-                                       SHORT_VALUE_MAX_LEN);
+                                       SHORT_VALUE_MAX_LEN + 1);
         b.push(self.lock_type.to_u8());
         b.encode_compact_bytes(&self.primary).unwrap();
         b.encode_var_u64(self.ts).unwrap();
