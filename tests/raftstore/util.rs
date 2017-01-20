bfc378898eb7371101496f2dad077e196005b34e
Dylan.Wen <hhkbp2@gmail.com>
2017-01-20 17:04:09 +0800

    tune ticks default value

--- tests/raftstore/util.rs
+++ tests/raftstore/util.rs
@@ -74,10 +74,11 @@ pub fn must_get_cf_none(engine: &Arc<DB>, cf: &str, key: &[u8]) {
 }
 
 pub fn new_store_cfg() -> Config {
+    let election_timeout_ticks = 25;
     Config {
         raft_base_tick_interval: 10,
         raft_heartbeat_ticks: 2,
-        raft_election_timeout_ticks: 25,
+        raft_election_timeout_ticks: election_timeout_ticks,
         raft_log_gc_tick_interval: 100,
         raft_log_gc_threshold: 1,
         pd_heartbeat_tick_interval: 20,
@@ -87,7 +88,7 @@ pub fn new_store_cfg() -> Config {
         // should be configured far beyond the election timeout.
         max_leader_missing_duration: Duration::from_secs(3),
         report_region_flow_interval: 100, // 100ms
-        leader_accelerate_campaign_after_split_ticks: 20,
+        leader_accelerate_campaign_after_split_ticks: election_timeout_ticks - 1,
         ..Config::default()
     }
 }
