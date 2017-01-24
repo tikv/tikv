084418946b8388d9578836cd78a595fe8fa07dff
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-01-24 13:54:24 +0800

    no need to overwrite data in prewrite (#1500)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/storage/mvcc/txn.rs
+++ src/storage/mvcc/txn.rs
@@ -122,6 +122,11 @@ impl<'a> MvccTxn<'a> {
                     ttl: lock.ttl,
                 });
             }
+            // No need to overwrite the lock and data.
+            // If we use single delete, we can't put a key multiple times.
+            info!("duplicated prewrite with start_ts {}, ignore it.",
+                  self.start_ts);
+            return Ok(());
         }
 
         let short_value = if let Mutation::Put((_, ref value)) = mutation {
