687a9e8b778a7ff8a429e3b26a33b4322ddb3c0f
Dylan Wen <hhkbp2@gmail.com>
2017-01-26 15:01:24 +0800

    raftstore: add request wait time and process time metrics (#1563)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/raftstore/store/store.rs
+++ src/raftstore/store/store.rs
@@ -1270,10 +1270,7 @@ impl<T: Transport, C: PdClient> Store<T, C> {
             // Create a compact log request and notify directly.
             let request = new_compact_log_request(region_id, peer.peer.clone(), compact_idx, term);
 
-            if let Err(e) = self.sendch.try_send(Msg::RaftCmd {
-                request: request,
-                callback: Box::new(|_| {}),
-            }) {
+            if let Err(e) = self.sendch.try_send(Msg::new_raft_cmd(request, Box::new(|_| {}))) {
                 error!("{} send compact log {} err {:?}", peer.tag, compact_idx, e);
             }
         }
@@ -1727,10 +1724,8 @@ impl<T: Transport, C: PdClient> Store<T, C> {
             let peer = &self.region_peers[&candidate_id];
 
             info!("{} scheduling consistent check", peer.tag);
-            let msg = Msg::RaftCmd {
-                request: new_compute_hash_request(candidate_id, peer.peer.clone()),
-                callback: Box::new(|_| {}),
-            };
+            let msg = Msg::new_raft_cmd(new_compute_hash_request(candidate_id, peer.peer.clone()),
+                                        Box::new(|_| {}));
 
             if let Err(e) = self.sendch.send(msg) {
                 error!("{} failed to schedule consistent check: {:?}", peer.tag, e);
@@ -1783,10 +1778,8 @@ impl<T: Transport, C: PdClient> Store<T, C> {
             return;
         }
 
-        let msg = Msg::RaftCmd {
-            request: new_verify_hash_request(region_id, peer.clone(), state),
-            callback: Box::new(|_| {}),
-        };
+        let msg = Msg::new_raft_cmd(new_verify_hash_request(region_id, peer.clone(), state),
+                                    Box::new(|_| {}));
         if let Err(e) = self.sendch.send(msg) {
             error!("[region {}] failed to schedule verify command for index {}: {:?}",
                    region_id,
@@ -1869,7 +1862,13 @@ impl<T: Transport, C: PdClient> mio::Handler for Store<T, C> {
                     error!("{} handle raft message err: {:?}", self.tag, e);
                 }
             }
-            Msg::RaftCmd { request, callback } => self.propose_raft_command(request, callback),
+            Msg::RaftCmd { send_time, request, callback } => {
+                self.raft_metrics
+                    .propose
+                    .request_wait_time
+                    .observe(duration_to_sec(send_time.elapsed()) as f64);
+                self.propose_raft_command(request, callback)
+            }
             Msg::Quit => {
                 info!("{} receive quit message", self.tag);
                 event_loop.shutdown();
