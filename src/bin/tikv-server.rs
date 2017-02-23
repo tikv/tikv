1921afc95d8ebe486af385cddc69d5d76cdbb0cc
Dylan.Wen <hhkbp2@gmail.com>
2017-02-23 10:56:25 +0800

    fix typo

--- src/bin/tikv-server.rs
+++ src/bin/tikv-server.rs
@@ -585,7 +585,7 @@ fn build_cfg(matches: &Matches,
             config,
             "raftstore.consistency-check-interval");
     cfg.raft_store.use_sst_file_snapshot =
-        get_toml_boolean(config, "raftstore.use_sst_file_snapshot", Some(false));
+        get_toml_boolean(config, "raftstore.use-sst-file-snapshot", Some(false));
     cfg_usize(&mut cfg.storage.sched_notify_capacity,
               config,
               "storage.scheduler-notify-capacity");
