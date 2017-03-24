285f6f7f2c0d354fcf4a97fe193ad57d3169ef56
wuxuelian <AndreMouche@126.com>
2017-03-24 18:43:41 +0800

    coprocessor:adjust code

--- src/server/coprocessor/endpoint.rs
+++ src/server/coprocessor/endpoint.rs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use std::usize;
 use std::collections::BinaryHeap;
 use std::collections::hash_map::Entry;
