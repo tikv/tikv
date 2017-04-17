abe3f654137e06ac39ff9360fc17556158c39d40
lishihai9017 <lishihai@pingcap.com>
2017-04-17 19:22:21 +0800

    update writebatch bench logic

--- benches/writebatch/bench_writebatch.rs
+++ benches/writebatch/bench_writebatch.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 PingCAP, Inc.
+// Copyright 2017 PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,121 +11,85 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::time::*;
 use test::Bencher;
 use tempdir::TempDir;
 use rocksdb::{DB, WriteBatch, Writable};
 
-fn bench_writebatch_impl(round: usize, batch_keys: usize) {
-    let path = TempDir::new("/tmp/rocksdb_write_batch_bench").unwrap();
-    let db = DB::open_default(path.path().to_str().unwrap()).unwrap();
+#[allow(dead_code)]
+fn writebatch(db: &DB, round: usize, batch_keys: usize) {
     let v = b"operators are syntactic sugar for calls to methods of built-in traits";
     for r in 0..round {
         let batch = WriteBatch::new();
         for i in 0..batch_keys {
             let k = format!("key_round{}_key{}", r, i);
-            batch.put(k.as_bytes(), v);
+            batch.put(k.as_bytes(), v).unwrap();
         }
         db.write(batch).unwrap()
     }
 }
 
-#[bench]
-fn bench_batch_1(b: &mut Bencher) {
+#[allow(dead_code)]
+fn bench_writebatch_impl(b: &mut Bencher, batch_keys: usize) {
+    let path = TempDir::new("/tmp/rocksdb_write_batch_bench").unwrap();
+    let db = DB::open_default(path.path().to_str().unwrap()).unwrap();
     let key_count = 1<<13;
-    let batch_keys = 1;
     let round = key_count / batch_keys;
     b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
+        writebatch(&db, round, batch_keys);
     });
 }
 
 #[bench]
-fn bench_batch_2(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 2;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_1(b: &mut Bencher) {
+    bench_writebatch_impl(b, 1);
 }
 
 #[bench]
-fn bench_batch_4(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 4;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_2(b: &mut Bencher) {
+    bench_writebatch_impl(b, 2);
 }
 
 #[bench]
-fn bench_batch_8(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 8;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_4(b: &mut Bencher) {
+    bench_writebatch_impl(b, 4);
 }
 
 #[bench]
-fn bench_batch_16(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 16;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_8(b: &mut Bencher) {
+    bench_writebatch_impl(b, 8);
 }
 
 #[bench]
-fn bench_batch_32(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 32;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_16(b: &mut Bencher) {
+    bench_writebatch_impl(b, 16);
 }
 
 #[bench]
-fn bench_batch_64(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 64;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_32(b: &mut Bencher) {
+    bench_writebatch_impl(b, 32);
 }
 
 #[bench]
-fn bench_batch_128(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 128;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_64(b: &mut Bencher) {
+    bench_writebatch_impl(b, 64);
 }
 
 #[bench]
-fn bench_batch_256(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 256;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_128(b: &mut Bencher) {
+    bench_writebatch_impl(b, 128);
 }
 
 #[bench]
-fn bench_batch_512(b: &mut Bencher) {
-    let key_count = 1<<13;
-    let batch_keys = 512;
-    let round = key_count / batch_keys;
-    b.iter(|| {
-        bench_writebatch_impl(round, batch_keys);
-    });
+fn bench_writebatch_256(b: &mut Bencher) {
+    bench_writebatch_impl(b, 256);
+}
+
+#[bench]
+fn bench_writebatch_512(b: &mut Bencher) {
+    bench_writebatch_impl(b, 512);
+}
+
+#[bench]
+fn bench_writebatch_1024(b: &mut Bencher) {
+    bench_writebatch_impl(b, 1024);
 }
