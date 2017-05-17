92558a2c7528a83230f3854c085e171a0951307a
disksing <i@disksing.com>
2017-05-17 11:35:04 +0800

    Update bench-tikv.rs

--- benches/bin/bench-tikv.rs
+++ benches/bin/bench-tikv.rs
@@ -17,7 +17,6 @@
 #![feature(test)]
 #![feature(fnbox)]
 #![feature(box_syntax)]
-#![feature(const_fn)]
 #![cfg_attr(feature = "dev", plugin(clippy))]
 #![cfg_attr(not(feature = "dev"), allow(unknown_lints))]
 #![feature(btree_range, collections_bound)]
