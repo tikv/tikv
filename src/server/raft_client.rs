ae849f927a6c6db891eacf3af74c7bef659efece
disksing <i@disksing.com>
2017-05-25 16:27:49 +0800

    fix bugs.

--- src/server/raft_client.rs
+++ src/server/raft_client.rs
@@ -15,6 +15,7 @@ use std::sync::Arc;
 use std::net::SocketAddr;
 
 use futures::sync::mpsc::{self, UnboundedSender};
+use futures::sync::oneshot::{self, Sender};
 use futures::{Future, Sink, Stream};
 use grpc::{Environment, ChannelBuilder};
 use kvproto::raft_serverpb::RaftMessage;
@@ -26,6 +27,7 @@ use super::{Error, Result};
 struct Conn {
     _client: TikvClient,
     stream: UnboundedSender<RaftMessage>,
+    _close: Sender<()>,
 }
 
 impl Conn {
@@ -35,14 +37,19 @@ impl Conn {
         let channel = ChannelBuilder::new(env).connect(&format!("{}", addr));
         let client = TikvClient::new(channel);
         let (tx, rx) = mpsc::unbounded();
+        let (tx_close, rx_close) = oneshot::channel();
         let (sink, _) = client.raft();
-        client.spawn(sink.sink_map_err(Error::from)
-            .send_all(rx.map_err(|_| Error::Sink))
+        client.spawn(rx_close.map_err(|_| ())
+            .select(sink.sink_map_err(Error::from)
+                .send_all(rx.map_err(|_| Error::Sink))
+                .map(|_| ())
+                .map_err(move |e| warn!("send raftmessage to {} failed: {:?}", addr, e)))
             .map(|_| ())
-            .map_err(move |e| warn!("send raftmessage to {} failed: {:?}", addr, e)));
+            .map_err(|_| ()));
         Conn {
             _client: client,
             stream: tx,
+            _close: tx_close,
         }
     }
 }
@@ -69,8 +76,24 @@ impl RaftClient {
     }
 
     pub fn send(&mut self, addr: SocketAddr, msg: RaftMessage) -> Result<()> {
-        let conn = self.get_conn(addr);
-        box_try!(UnboundedSender::send(&conn.stream, msg));
+        let res = {
+            let conn = self.get_conn(addr);
+            UnboundedSender::send(&conn.stream, msg)
+        };
+        if let Err(e) = res {
+            warn!("server: drop conn with tikv endpoint {} error: {:?}",
+                  addr,
+                  e);
+            self.conns.remove(&addr);
+            return Err(box_err!(e));
+        }
         Ok(())
+    }
+}
+
+impl Drop for RaftClient {
+    fn drop(&mut self) {
+        // Drop conns here to make sure all streams are dropped before Environment.
+        self.conns.clear();
     }
 }
