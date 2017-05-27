e021d2851e365cc0aa36165a96a2a3a04d9e1eee
lishihai9017 <lishihai@pingcap.com>
2017-05-27 14:00:05 +0800

    add some test & bench

--- src/raftstore/coprocessor/txn_observer.rs
+++ src/raftstore/coprocessor/txn_observer.rs
@@ -54,7 +54,7 @@ impl RegionObserver for TxnObserver {
                 CmdType::Commit => {
                     let mut commit = reqs[i].take_commit();
                     let key = commit.take_key();
-                    let lock_key = Key::from_encoded(key.clone())
+                    let unlock_key = Key::from_encoded(key.clone())
                         .truncate_ts()
                         .unwrap()
                         .into_encoded();
@@ -70,7 +70,7 @@ impl RegionObserver for TxnObserver {
 
                     let mut del = DeleteRequest::new();
                     del.set_cf(CF_LOCK.to_owned());
-                    del.set_key(lock_key);
+                    del.set_key(unlock_key);
                     let mut new_req = Request::new();
                     new_req.set_cmd_type(CmdType::Delete);
                     new_req.set_delete(del);
@@ -88,11 +88,13 @@ mod tests {
     use storage::engine::Modify;
     use storage::types::make_key;
     use storage::{CF_DEFAULT, CF_LOCK, Options};
-    use kvproto::raft_cmdpb::{CmdType, Request, PutRequest, DeleteRequest, PrewriteRequest};
+    use kvproto::raft_cmdpb::{CmdType, Request, PutRequest, DeleteRequest, PrewriteRequest,
+                              CommitRequest};
     use kvproto::metapb::Region;
     use storage::mvcc::{LockType, Lock};
+    use storage::mvcc::{Write, WriteType};
 
-    fn gen_request(modifies: Vec<Modify>) -> Vec<Request> {
+    fn gen_requests(modifies: Vec<Modify>) -> Vec<Request> {
         let mut reqs = Vec::with_capacity(modifies.len());
         for m in modifies {
             let mut req = Request::new();
@@ -124,44 +126,72 @@ mod tests {
                     req.set_cmd_type(CmdType::Prewrite);
                     req.set_prewrite(prewrite);
                 }
+                Modify::Commit(k, v) => {
+                    let mut commit = CommitRequest::new();
+                    commit.set_key(k.into_encoded());
+                    commit.set_value(v);
+                    req.set_cmd_type(CmdType::Commit);
+                    req.set_commit(commit);
+                }
             }
             reqs.push(req);
         }
         reqs
     }
 
-    fn gen_put_req(key: &[u8], value: &[u8], pk: &[u8], ts: u64) -> Vec<Request> {
-        let mut modifies = Vec::new();
+    fn gen_prewrite_req(key: &[u8],
+                        value: &[u8],
+                        pk: &[u8],
+                        ts: u64)
+                        -> (Vec<Request>, Vec<Request>) {
         let opt = Options::default();
         let key = make_key(key);
         let lock = Lock::new(LockType::Put, pk.to_vec(), ts, opt.lock_ttl, None);
-        modifies.push(Modify::Put(CF_LOCK, key.clone(), lock.to_bytes()));
-        modifies.push(Modify::Put(CF_DEFAULT, key.append_ts(ts), value.to_vec()));
-        gen_request(modifies)
+
+        let mut org_pw_modifies = Vec::new();
+        org_pw_modifies.push(Modify::Put(CF_LOCK, key.clone(), lock.to_bytes()));
+        org_pw_modifies.push(Modify::Put(CF_DEFAULT, key.append_ts(ts), value.to_vec()));
+        let mut new_pw_modifies = Vec::new();
+        new_pw_modifies.push(Modify::Prewrite(key.append_ts(ts), value.to_vec(), lock));
+        (gen_requests(org_pw_modifies), gen_requests(new_pw_modifies))
     }
 
-    fn gen_prewrite_req(key: &[u8], value: &[u8], pk: &[u8], ts: u64) -> Vec<Request> {
-        let mut modifies = Vec::new();
-        let opt = Options::default();
+    fn gen_commit_req(key: &[u8], ts: u64) -> (Vec<Request>, Vec<Request>) {
         let key = make_key(key);
-        let lock = Lock::new(LockType::Put, pk.to_vec(), ts, opt.lock_ttl, None);
-        modifies.push(Modify::Prewrite(key.append_ts(ts), value.to_vec(), lock));
-        gen_request(modifies)
+        let write = Write::new(WriteType::Put, ts, None);
+
+        let mut org_commit_modifies = Vec::new();
+        org_commit_modifies.push(Modify::Put(CF_WRITE, key.append_ts(ts), write.to_bytes()));
+        org_commit_modifies.push(Modify::Delete(CF_LOCK, key.clone()));
+        let mut new_commit_modifies = Vec::new();
+        new_commit_modifies.push(Modify::Commit(key.append_ts(ts), write.to_bytes()));
+        (gen_requests(org_commit_modifies), gen_requests(new_commit_modifies))
     }
 
-    fn test_pre_apply_query_impl(key: &[u8], value: &[u8], pk: &[u8], ts: u64) {
-        let put_req = RepeatedField::from_vec(gen_put_req(key, value, pk, ts));
-        let mut prewrite_req = RepeatedField::from_vec(gen_prewrite_req(key, value, pk, ts));
+    fn test_resolve_prewrite_request(key: &[u8], value: &[u8], pk: &[u8], ts: u64) {
+        let (org_pw_req, new_pw_req) = gen_prewrite_req(key, value, pk, ts);
         let observer = TxnObserver;
         let region = Region::new();
         let mut ctx = ObserverContext::new(&region);
-        observer.pre_apply_query(&mut ctx, &mut prewrite_req);
-        assert_eq!(prewrite_req, put_req);
+        let mut new_pw_req = RepeatedField::from_vec(new_pw_req);
+        observer.pre_apply_query(&mut ctx, &mut new_pw_req);
+        assert_eq!(new_pw_req.into_vec(), org_pw_req);
+    }
+
+    fn test_resolve_commit_request(key: &[u8], ts: u64) {
+        let (org_commit_req, new_commit_req) = gen_commit_req(key, ts);
+        let observer = TxnObserver;
+        let region = Region::new();
+        let mut ctx = ObserverContext::new(&region);
+        let mut new_commit_req = RepeatedField::from_vec(new_commit_req);
+        observer.pre_apply_query(&mut ctx, &mut new_commit_req);
+        assert_eq!(new_commit_req.into_vec(), org_commit_req);
     }
 
     #[test]
     fn test_pre_apply_query() {
-        test_pre_apply_query_impl(b"k1", b"v1", b"k1", 5);
+        test_resolve_prewrite_request(b"k1", b"v1", b"k1", 5);
+        test_resolve_commit_request(b"k1", 5);
     }
 
     use test::Bencher;
@@ -176,37 +206,45 @@ mod tests {
         value
     }
 
-    fn gen_prewrite_reqs(cnt: u32, key: &[u8], value: &[u8], pk: &[u8], ts: u64) -> Vec<Request> {
-        let mut modifies = Vec::new();
-        let opt = Options::default();
-        let key = make_key(key);
-        let lock = Lock::new(LockType::Put, pk.to_vec(), ts, opt.lock_ttl, None);
-        for _ in 0..cnt {
-            modifies.push(Modify::Prewrite(key.append_ts(ts), value.to_vec(), lock.clone()));
-        }
-        gen_request(modifies)
-    }
-
     #[bench]
-    fn bench_gen_prewrite_reqs(b: &mut Bencher) {
+    fn bench_gen_prewrite_req(b: &mut Bencher) {
         let value = gen_value(b'v', SHORT_VALUE_MAX_LEN + 1);
         let (key, pk, ts) = (b"k1", b"k1", 5);
         b.iter(|| {
-            RepeatedField::from_vec(gen_prewrite_reqs(4, key, &value, pk, ts));
+            RepeatedField::from_vec(gen_prewrite_req(key, &value, pk, ts).1);
         });
     }
 
     #[bench]
-    fn bench_pre_apply_query(b: &mut Bencher) {
+    fn bench_resolve_prewrite(b: &mut Bencher) {
         let value = gen_value(b'v', SHORT_VALUE_MAX_LEN + 1);
         let (key, pk, ts) = (b"k1", b"k1", 5);
         let observer = TxnObserver;
         let region = Region::new();
         let mut ctx = ObserverContext::new(&region);
         b.iter(|| {
-            let mut prewrite_reqs =
-                RepeatedField::from_vec(gen_prewrite_reqs(4, key, &value, pk, ts));
+            let mut prewrite_reqs = RepeatedField::from_vec(gen_prewrite_req(key, &value, pk, ts).1);
             observer.pre_apply_query(&mut ctx, &mut prewrite_reqs);
+        });
+    }
+
+    #[bench]
+    fn bench_gen_commit_req(b: &mut Bencher) {
+        let (key, ts) = (b"k1", 5);
+        b.iter(|| {
+            RepeatedField::from_vec(gen_commit_req(key, ts).1);
+        });
+    }
+
+    #[bench]
+    fn bench_resolve_commit(b: &mut Bencher) {
+        let (key, ts) = (b"k1", 5);
+        let observer = TxnObserver;
+        let region = Region::new();
+        let mut ctx = ObserverContext::new(&region);
+        b.iter(|| {
+            let mut commit_req = RepeatedField::from_vec(gen_commit_req(key, ts).1);
+            observer.pre_apply_query(&mut ctx, &mut commit_req);
         });
     }
 }
