289c34c8ff79320015b386b62c8c5083de276ad9
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-06-03 10:57:07 +0800

    address comments

--- src/bin/tikv-server.rs
+++ src/bin/tikv-server.rs
@@ -580,6 +580,7 @@ fn get_rocksdb_lock_cf_option(config: &toml::Value) -> RocksdbOptions {
     default_values.whole_key_filtering = true;
     default_values.compression_per_level = String::from("no:no:no:no:no:no:no");
     default_values.level_zero_file_num_compaction_trigger = 1;
+    default_values.max_bytes_for_level_base = 128 * MB as i64;
 
     get_rocksdb_cf_option(config, "lockcf", default_values)
 }
