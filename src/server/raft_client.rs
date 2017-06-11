92fdaa43ac556f1c18d7a6f8444b8e98355a0b6b
siddontang <siddontang@gmail.com>
2017-06-11 20:41:53 +0800

    use region id to calculate index

--- src/server/raft_client.rs
+++ src/server/raft_client.rs
@@ -65,7 +65,6 @@ pub struct RaftClient {
     env: Arc<Environment>,
     conns: HashMap<(SocketAddr, usize), Conn>,
     conn_size: usize,
-    conn_index: usize,
 }
 
 impl RaftClient {
@@ -74,7 +73,6 @@ impl RaftClient {
             env: env,
             conns: HashMap::default(),
             conn_size: conn_size,
-            conn_index: 0,
         }
     }
 
@@ -86,8 +84,7 @@ impl RaftClient {
     }
 
     pub fn send(&mut self, addr: SocketAddr, msg: RaftMessage) -> Result<()> {
-        self.conn_index = (self.conn_index + 1) % self.conn_size;
-        let index = self.conn_index;
+        let index = msg.get_region_id() as usize % self.conn_size;
         let res = {
             let conn = self.get_conn(addr, index);
             UnboundedSender::send(&conn.stream, (msg, WriteFlags::default()))
