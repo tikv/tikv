6490c8c152fc87b896cae93e41c9e094c68a0da5
wuxuelian <AndreMouche@126.com>
2017-06-12 19:40:46 +0800

    json:address comments

--- src/util/codec/mysql/json.rs
+++ src/util/codec/mysql/json.rs
@@ -187,7 +187,7 @@ struct JsonVisitor;
 impl<'de> Visitor<'de> for JsonVisitor {
     type Value = Json;
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "may be any string")
+        write!(formatter, "a json value")
     }
 
     fn visit_unit<E>(self) -> std::result::Result<Self::Value, E>
