e850e6ed5790c41f513ebfc4e4860978f96ddf8e
Neil Shen <overvenus@gmail.com>
2017-06-13 23:57:28 +0800

    tests: now tests cmpiles

--- tests/raftstore/test_lease_read.rs
+++ tests/raftstore/test_lease_read.rs
@@ -73,13 +73,13 @@ fn read_on_peer<T: Simulator>(cluster: &mut Cluster<T>,
     request.mut_header().set_peer(peer);
     let mut resp = try!(cluster.call_command(request, timeout));
     if resp.get_header().has_error() {
-        return Err(Error::Other(box_err!(resp.mut_header().take_error().take_message())));
+        return Err(Error::Other(box_err!((&*resp.mut_header().take_error().take_message()).to_owned())));
     }
     assert_eq!(resp.get_responses().len(), 1);
     assert_eq!(resp.get_responses()[0].get_cmd_type(), CmdType::Get);
     let mut get = resp.mut_responses()[0].take_get();
     assert!(get.has_value());
-    Ok(get.take_value())
+    Ok((*get.take_value()).into())
 }
 
 fn must_read_on_peer<T: Simulator>(cluster: &mut Cluster<T>,
