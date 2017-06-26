91992362779e63ee1ffd23a94e466abf580e942a
Dylan.Wen <hhkbp2@gmail.com>
2017-06-26 14:44:28 +0800

    add test case

--- src/util/codec/mysql/json/json_modify.rs
+++ src/util/codec/mysql/json/json_modify.rs
@@ -54,10 +54,10 @@ impl Json {
 // `set_json` is used in Json::modify().
 fn set_json(j: Json, path_legs: &[PathLeg], value: Json, mt: ModifyType) -> Json {
     if path_legs.is_empty() {
-        if let ModifyType::Replace = mt {
-            return value;
+        match mt {
+            ModifyType::Replace | ModifyType::Set => return value,
+            _ => return j,
         }
-        return j;
     }
     let (current_leg, sub_path_legs) = (&path_legs[0], &path_legs[1..]);
     match *current_leg {
@@ -81,9 +81,12 @@ fn set_json(j: Json, path_legs: &[PathLeg], value: Json, mt: ModifyType) -> Json
                     array.append(&mut right);
                 }
             } else if sub_path_legs.is_empty() {
-                if let ModifyType::Insert = mt {
-                    // e.g. json_insert('[1, 2, 3]', '$[3]', "x") => '[1, 2, 3, "x"]'
-                    array.push(value)
+                match mt {
+                    ModifyType::Insert | ModifyType::Set => {
+                        // e.g. json_insert('[1, 2, 3]', '$[3]', "x") => '[1, 2, 3, "x"]'
+                        array.push(value)
+                    }
+                    _ => {}
                 }
             }
             if (array.len() == 1) && wrapped {
@@ -98,9 +101,12 @@ fn set_json(j: Json, path_legs: &[PathLeg], value: Json, mt: ModifyType) -> Json
                     // e.g. json_replace('{"a": 1}', '$.a', 2) => '{"a": 2}'
                     map.insert(key.clone(), set_json(v, sub_path_legs, value, mt));
                 } else if sub_path_legs.is_empty() {
-                    if let ModifyType::Insert = mt {
-                        // e.g. json_insert('{"a": 1}', '$.b', 2) => '{"a": 1, "b": 2}'
-                        map.insert(key.clone(), value);
+                    match mt {
+                        ModifyType::Insert | ModifyType::Set => {
+                            // e.g. json_insert('{"a": 1}', '$.b', 2) => '{"a": 1, "b": 2}'
+                            map.insert(key.clone(), value);
+                        }
+                        _ => {}
                     }
                 }
                 return Json::Object(map);
@@ -109,4 +115,61 @@ fn set_json(j: Json, path_legs: &[PathLeg], value: Json, mt: ModifyType) -> Json
         _ => {}
     }
     j
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use super::super::path_expr::parse_json_path_expr;
+
+    #[test]
+    fn test_json_modify() {
+        let mut test_cases = vec![
+            (r#"null"#, "$", r#"{}"#, ModifyType::Set, r#"{}"#, true),
+            (r#"{}"#, "$.a", r#"3"#, ModifyType::Set, r#"{"a": 3}"#, true),
+            (r#"{"a": 3}"#, "$.a", r#"[]"#, ModifyType::Replace, r#"{"a": []}"#, true),
+            (r#"{"a": []}"#, "$.a[0]", r#"3"#, ModifyType::Set, r#"{"a": [3]}"#, true),
+            (r#"{"a": [3]}"#, "$.a[1]", r#"4"#, ModifyType::Insert, r#"{"a": [3, 4]}"#, true),
+            (r#"{"a": [3]}"#, "$[0]", r#"4"#, ModifyType::Set, r#"4"#, true),
+            (r#"{"a": [3]}"#, "$[1]", r#"4"#, ModifyType::Set, r#"[{"a": [3]}, 4]"#, true),
+
+            // Nothing changed because the path is empty and we want to insert.
+            (r#"{}"#, "$", r#"1"#, ModifyType::Insert, r#"{}"#, true),
+            // Nothing changed because the path without last leg doesn't exist.
+            (r#"{"a": [3, 4]}"#, "$.b[1]", r#"3"#, ModifyType::Set, r#"{"a": [3, 4]}"#, true),
+            // Nothing changed because the path without last leg doesn't exist.
+            (r#"{"a": [3, 4]}"#, "$.a[2].b", r#"3"#, ModifyType::Set, r#"{"a": [3, 4]}"#, true),
+            // Nothing changed because we want to insert but the full path exists.
+            (r#"{"a": [3, 4]}"#, "$.a[0]", r#"30"#, ModifyType::Insert, r#"{"a": [3, 4]}"#, true),
+            // Nothing changed because we want to replace but the full path doesn't exist.
+            (r#"{"a": [3, 4]}"#, "$.a[2]", r#"30"#, ModifyType::Replace, r#"{"a": [3, 4]}"#, true),
+
+            // Bad path expression.
+            (r#"null"#, "$.*", r#"{}"#, ModifyType::Set, r#"null"#, false),
+            (r#"null"#, "$[*]", r#"{}"#, ModifyType::Set, r#"null"#, false),
+            (r#"null"#, "$**.a", r#"{}"#, ModifyType::Set, r#"null"#, false),
+            (r#"null"#, "$**[3]", r#"{}"#, ModifyType::Set, r#"null"#, false),
+        ];
+        for (i, (json, path, value, mt, expected, success)) in test_cases.drain(..).enumerate() {
+            let j: Result<Json> = json.parse();
+            assert!(j.is_ok(), "#{} expect json parse ok but got {:?}", i, j);
+            let p = parse_json_path_expr(path);
+            assert!(p.is_ok(), "#{} expect path parse ok but got {:?}", i, p);
+            let v = value.parse();
+            assert!(v.is_ok(), "#{} expect value parse ok but got {:?}", i, v);
+            let e: Result<Json> = expected.parse();
+            assert!(e.is_ok(),
+                    "#{} expect expected value parse ok but got {:?}",
+                    i,
+                    e);
+            let (j, p, v, e) = (j.unwrap(), p.unwrap(), v.unwrap(), e.unwrap());
+            let (m, r) = j.modify(vec![p].as_slice(), vec![v], mt);
+            if success {
+                assert!(r.is_ok(), "#{} expect modify ok but got {:?}", i, r);
+            } else {
+                assert!(r.is_err(), "#{} expect modify error but got {:?}", i, r);
+            }
+            assert_eq!(e, m, "#{} expect modified json {:?} == {:?}", i, m, e);
+        }
+    }
 }
