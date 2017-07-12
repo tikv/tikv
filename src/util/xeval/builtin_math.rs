42cc15de0e4002b71ddc3652335c0d3a4091d699
Dylan.Wen <hhkbp2@gmail.com>
2017-07-12 11:56:56 +0800

    remove impl

--- src/util/xeval/builtin_math.rs
+++ src/util/xeval/builtin_math.rs
@@ -23,20 +23,9 @@ pub fn invalid_type_error(datum: &Datum, expected_type: &str) -> Result<Datum> {
 }
 
 impl Evaluator {
-    pub fn abs_int(&mut self, ctx: &EvalContext, expr: &Expr) -> Result<Datum> {
-        let child = try!(self.get_one_child(expr));
-        let d = try!(self.eval(ctx, child));
-        match d {
-            Datum::I64(i) => {
-                if i >= 0 {
-                    Ok(Datum::I64(i))
-                } else {
-                    Ok(Datum::I64(-i))
-                }
-            }
-            Datum::U64(_) => Ok(d),
-            _ => invalid_type_error(&d, TYPE_INT),
-        }
+    pub fn abs_int(&mut self, _ctx: &EvalContext, _expr: &Expr) -> Result<Datum> {
+        // TODO add impl
+        Err(Error::Eval(ERROR_UNIMPLEMENTED.to_owned()))
     }
 
     pub fn abs_real(&mut self, _ctx: &EvalContext, _expr: &Expr) -> Result<Datum> {
@@ -63,51 +52,4 @@ impl Evaluator {
         // TODO add impl
         Err(Error::Eval(ERROR_UNIMPLEMENTED.to_owned()))
     }
-}
-
-#[cfg(test)]
-mod test {
-    use tipb::expression::{ExprType, ScalarFuncSig};
-    use util::codec::datum::Datum;
-    use super::super::Evaluator;
-    use super::super::evaluator::test::build_expr_with_sig;
-
-    macro_rules! test_eval {
-        ($tag:ident, $cases:expr) => {
-            #[test]
-            fn $tag() {
-                let mut test_cases = $cases;
-                let mut evaluator = Evaluator::default();
-                for (i, (expr, expected)) in test_cases.drain(..).enumerate() {
-                    let res = evaluator.eval(&Default::default(), &expr);
-                    assert!(res.is_ok(),
-                            "#{} expect eval expr {:?} ok but got {:?}",
-                            i,
-                            expr,
-                            res);
-                    let res = res.unwrap();
-                    assert_eq!(res,
-                               expected,
-                               "#{} expect {:?} but got {:?}",
-                               i,
-                               expected,
-                               res);
-                }
-            }
-        };
-    }
-
-    test_eval!(test_abs_int,
-               vec![(build_expr_with_sig(vec![Datum::I64(-1)],
-                                         ExprType::ScalarFunc,
-                                         ScalarFuncSig::AbsInt),
-                     Datum::I64(1)),
-                    (build_expr_with_sig(vec![Datum::I64(1)],
-                                         ExprType::ScalarFunc,
-                                         ScalarFuncSig::AbsInt),
-                     Datum::I64(1)),
-                    (build_expr_with_sig(vec![Datum::U64(1)],
-                                         ExprType::ScalarFunc,
-                                         ScalarFuncSig::AbsInt),
-                     Datum::U64(1))]);
 }
