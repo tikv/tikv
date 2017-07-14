be67e31be88fe40960624601d42c9f341f40f7d8
Cholerae Hu <huyingqian@pingcap.com>
2017-07-14 19:17:23 +0800

    server: MAX_GRPC_SEND_MSG_LEN
    Signed-off-by: Cholerae Hu <huyingqian@pingcap.com>

--- src/server/server.rs
+++ src/server/server.rs
@@ -22,6 +22,7 @@ use kvproto::tikvpb_grpc::*;
 use util::worker::Worker;
 use storage::Storage;
 use raftstore::store::{SnapshotStatusMsg, SnapManager};
+use raftstore::store::config::REGION_SPLIT_SIZE;
 
 use super::{Result, Config};
 use coprocessor::{EndPointHost, EndPointTask};
@@ -33,7 +34,7 @@ use super::raft_client::RaftClient;
 
 const DEFAULT_COPROCESSOR_BATCH: usize = 50;
 const MAX_GRPC_RECV_MSG_LEN: usize = 10 * 1024 * 1024;
-const MAX_GRPC_SEND_MSG_LEN: usize = 8 * 1024 * 1024 * 1024;
+const MAX_GRPC_SEND_MSG_LEN: usize = REGION_SPLIT_SIZE as usize * 1024 * 1024;
 
 pub struct Server<T: RaftStoreRouter + 'static, S: StoreAddrResolver + 'static> {
     env: Arc<Environment>,
