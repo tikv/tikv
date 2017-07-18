ec4abaf793bb97e90a5b901ade49c42400bced1b
ShuYu Wang <andelf@gmail.com>
2017-07-18 17:27:56 +0800

    *: remove unnecessary use (#2049)

--- src/raftstore/store/cmd_resp.rs
+++ src/raftstore/store/cmd_resp.rs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::boxed::Box;
 use std::error;
 
 use kvproto::raft_cmdpb::RaftCmdResponse;
