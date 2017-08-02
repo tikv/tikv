fdd92449cc6719436a4e4832c2fdf52f62b7b190
Jay Lee <busyjaylee@gmail.com>
2017-08-02 16:09:14 +0800

    delete more codes

--- src/util/config.rs
+++ src/util/config.rs
@@ -22,13 +22,10 @@ use std::net::{SocketAddrV4, SocketAddrV6};
 use std::ops::{Mul, Div};
 
 use url;
-use regex::Regex;
 use serde::{Serialize, Deserialize, Serializer, Deserializer};
 use serde::de::{self, Unexpected, Visitor};
 
-use util::collections::HashMap;
 use util;
-use rocksdb::{DBCompressionType, DBRecoveryMode, CompactionPriority};
 
 quick_error! {
     #[derive(Debug)]
@@ -49,36 +46,6 @@ quick_error! {
             description(msg)
             display("config value error: {}", msg)
         }
-    }
-}
-
-pub fn parse_rocksdb_compression(tp: &str) -> Result<DBCompressionType, ConfigError> {
-    match &*tp.trim().to_lowercase() {
-        "no" => Ok(DBCompressionType::No),
-        "snappy" => Ok(DBCompressionType::Snappy),
-        "zlib" => Ok(DBCompressionType::Zlib),
-        "bzip2" => Ok(DBCompressionType::Bz2),
-        "lz4" => Ok(DBCompressionType::Lz4),
-        "lz4hc" => Ok(DBCompressionType::Lz4hc),
-        "zstd" => Ok(DBCompressionType::Zstd),
-        _ => Err(ConfigError::Value(format!("invalid compression type {:?}", tp))),
-    }
-}
-
-pub fn parse_rocksdb_per_level_compression(tp: &str)
-                                           -> Result<Vec<DBCompressionType>, ConfigError> {
-    tp.split(':')
-        .map(parse_rocksdb_compression)
-        .collect()
-}
-
-pub fn parse_rocksdb_wal_recovery_mode(mode: i64) -> Result<DBRecoveryMode, ConfigError> {
-    match mode {
-        0 => Ok(DBRecoveryMode::TolerateCorruptedTailRecords),
-        1 => Ok(DBRecoveryMode::AbsoluteConsistency),
-        2 => Ok(DBRecoveryMode::PointInTime),
-        3 => Ok(DBRecoveryMode::SkipAnyCorruptedRecords),
-        _ => Err(ConfigError::Value(format!("invalid recovery mode: {:?}", mode))),
     }
 }
 
@@ -302,35 +269,6 @@ numeric_enum_mod!{recovery_mode_serde DBRecoveryMode {
     SkipAnyCorruptedRecords = 3,
 }}
 
-pub fn parse_rocksdb_compaction_pri(priority: i64) -> Result<CompactionPriority, ConfigError> {
-    match priority {
-        0 => Ok(CompactionPriority::ByCompensatedSize),
-        1 => Ok(CompactionPriority::OldestLargestSeqFirst),
-        2 => Ok(CompactionPriority::OldestSmallestSeqFirst),
-        3 => Ok(CompactionPriority::MinOverlappingRatio),
-        _ => Err(ConfigError::Value(format!("invalid Compaction priority: {:?}", priority))),
-    }
-}
-
-fn split_property(property: &str) -> Result<(f64, &str), ConfigError> {
-    let mut indx = 0;
-    for s in property.chars() {
-        match s {
-            '0'...'9' | '.' => {
-                indx += 1;
-            }
-            _ => {
-                break;
-            }
-        }
-    }
-
-    let (num, unit) = property.split_at(indx);
-    num.parse::<f64>()
-        .map(|f| (f, unit))
-        .or_else(|_| Err(ConfigError::Value(format!("cannot parse {:?} as f64", num))))
-}
-
 const UNIT: u64 = 1;
 const DATA_MAGNITUDE: u64 = 1024;
 pub const KB: u64 = UNIT * DATA_MAGNITUDE;
@@ -347,55 +285,6 @@ const MS: u64 = UNIT;
 const SECOND: u64 = MS * TIME_MAGNITUDE_1;
 const MINUTE: u64 = SECOND * TIME_MAGNITUDE_2;
 const HOUR: u64 = MINUTE * TIME_MAGNITUDE_2;
-
-pub fn parse_readable_int(size: &str) -> Result<i64, ConfigError> {
-    let (num, unit) = try!(split_property(size));
-
-    match &*unit.to_lowercase() {
-        // file size
-        "kb" => Ok((num * (KB as f64)) as i64),
-        "mb" => Ok((num * (MB as f64)) as i64),
-        "gb" => Ok((num * (GB as f64)) as i64),
-        "tb" => Ok((num * (TB as f64)) as i64),
-        "pb" => Ok((num * (PB as f64)) as i64),
-
-        // time
-        "ms" => Ok((num * (MS as f64)) as i64),
-        "s" => Ok((num * (SECOND as f64)) as i64),
-        "m" => Ok((num * (MINUTE as f64)) as i64),
-        "h" => Ok((num * (HOUR as f64)) as i64),
-
-        _ => Err(ConfigError::Value(format!("invalid unit {:?}", unit))),
-    }
-}
-
-pub fn parse_store_labels(labels: &str) -> Result<HashMap<String, String>, ConfigError> {
-    let mut map = HashMap::default();
-
-    let re = Regex::new(r"^[a-z0-9]([a-z0-9-._]*[a-z0-9])?$").unwrap();
-    for label in labels.split(',') {
-        if label.is_empty() {
-            continue;
-        }
-        let label = label.to_lowercase();
-        let kv: Vec<_> = label.split('=').collect();
-        match kv[..] {
-            [k, v] => {
-                if !re.is_match(k) || !re.is_match(v) {
-                    return Err(ConfigError::StoreLabels(format!("invalid label {:?}", label)));
-                }
-                if map.insert(k.to_owned(), v.to_owned()).is_some() {
-                    return Err(ConfigError::StoreLabels(format!("duplicated label {:?}", label)));
-                }
-            }
-            _ => {
-                return Err(ConfigError::StoreLabels(format!("invalid label {:?}", label)));
-            }
-        }
-    }
-
-    Ok(map)
-}
 
 #[derive(Clone, Debug, Copy)]
 pub struct ReadableSize(pub u64);
@@ -831,9 +720,9 @@ mod test {
 
     use super::*;
 
+    use rocksdb::DBCompressionType;
     use tempdir::TempDir;
     use toml;
-    use rocksdb::DBRecoveryMode;
 
     use util::collections::HashMap;
 
@@ -1011,75 +900,6 @@ mod test {
     }
 
     #[test]
-    fn test_parse_readable_int() {
-        // file size
-        assert_eq!(1_024, parse_readable_int("1KB").unwrap());
-        assert_eq!(1_048_576, parse_readable_int("1MB").unwrap());
-        assert_eq!(1_073_741_824, parse_readable_int("1GB").unwrap());
-        assert_eq!(1_099_511_627_776, parse_readable_int("1TB").unwrap());
-        assert_eq!(1_125_899_906_842_624, parse_readable_int("1PB").unwrap());
-
-        assert_eq!(1_024, parse_readable_int("1kb").unwrap());
-        assert_eq!(1_048_576, parse_readable_int("1mb").unwrap());
-        assert_eq!(1_073_741_824, parse_readable_int("1gb").unwrap());
-        assert_eq!(1_099_511_627_776, parse_readable_int("1tb").unwrap());
-        assert_eq!(1_125_899_906_842_624, parse_readable_int("1pb").unwrap());
-
-        assert_eq!(1_536, parse_readable_int("1.5KB").unwrap());
-        assert_eq!(1_572_864, parse_readable_int("1.5MB").unwrap());
-        assert_eq!(1_610_612_736, parse_readable_int("1.5GB").unwrap());
-        assert_eq!(1_649_267_441_664, parse_readable_int("1.5TB").unwrap());
-        assert_eq!(1_688_849_860_263_936, parse_readable_int("1.5PB").unwrap());
-
-        assert_eq!(100_663_296, parse_readable_int("96MB").unwrap());
-
-        assert_eq!(1_429_365_116_108, parse_readable_int("1.3TB").unwrap());
-        assert_eq!(1_463_669_878_895_411, parse_readable_int("1.3PB").unwrap());
-
-        assert!(parse_readable_int("KB").is_err());
-        assert!(parse_readable_int("MB").is_err());
-        assert!(parse_readable_int("GB").is_err());
-        assert!(parse_readable_int("TB").is_err());
-        assert!(parse_readable_int("PB").is_err());
-
-        // time
-        assert_eq!(1, parse_readable_int("1ms").unwrap());
-        assert_eq!(1_000, parse_readable_int("1s").unwrap());
-        assert_eq!(60_000, parse_readable_int("1m").unwrap());
-        assert_eq!(3_600_000, parse_readable_int("1h").unwrap());
-
-        assert_eq!(1, parse_readable_int("1.3ms").unwrap());
-        assert_eq!(1_000, parse_readable_int("1000ms").unwrap());
-        assert_eq!(1_300, parse_readable_int("1.3s").unwrap());
-        assert_eq!(1_500, parse_readable_int("1.5s").unwrap());
-        assert_eq!(10_000, parse_readable_int("10s").unwrap());
-        assert_eq!(78_000, parse_readable_int("1.3m").unwrap());
-        assert_eq!(90_000, parse_readable_int("1.5m").unwrap());
-        assert_eq!(4_680_000, parse_readable_int("1.3h").unwrap());
-        assert_eq!(5_400_000, parse_readable_int("1.5h").unwrap());
-
-        assert!(parse_readable_int("ms").is_err());
-        assert!(parse_readable_int("s").is_err());
-        assert!(parse_readable_int("m").is_err());
-        assert!(parse_readable_int("h").is_err());
-
-        assert!(parse_readable_int("1").is_err());
-        assert!(parse_readable_int("foo").is_err());
-    }
-
-    #[test]
-    fn test_parse_rocksdb_wal_recovery_mode() {
-        assert!(DBRecoveryMode::TolerateCorruptedTailRecords ==
-                parse_rocksdb_wal_recovery_mode(0).unwrap());
-        assert!(DBRecoveryMode::AbsoluteConsistency == parse_rocksdb_wal_recovery_mode(1).unwrap());
-        assert!(DBRecoveryMode::PointInTime == parse_rocksdb_wal_recovery_mode(2).unwrap());
-        assert!(DBRecoveryMode::SkipAnyCorruptedRecords ==
-                parse_rocksdb_wal_recovery_mode(3).unwrap());
-
-        assert!(parse_rocksdb_wal_recovery_mode(4).is_err());
-    }
-
-    #[test]
     fn test_parse_compression_type() {
         #[derive(Serialize, Deserialize)]
         struct CompressionTypeHolder {
@@ -1124,19 +944,6 @@ mod test {
             "no", "no", "no", "no", "no", "no", "yes"
         ]"#)
             .is_err());
-    }
-
-    #[test]
-    fn test_parse_rocksdb_compaction_pri() {
-        assert!(CompactionPriority::ByCompensatedSize == parse_rocksdb_compaction_pri(0).unwrap());
-        assert!(CompactionPriority::OldestLargestSeqFirst ==
-                parse_rocksdb_compaction_pri(1).unwrap());
-        assert!(CompactionPriority::OldestSmallestSeqFirst ==
-                parse_rocksdb_compaction_pri(2).unwrap());
-        assert!(CompactionPriority::MinOverlappingRatio ==
-                parse_rocksdb_compaction_pri(3).unwrap());
-
-        assert!(parse_rocksdb_compaction_pri(4).is_err());
     }
 
     #[test]
@@ -1216,41 +1023,5 @@ mod test {
         for (addr, is_ok) in table {
             assert_eq!(check_addr(addr).is_ok(), is_ok);
         }
-    }
-
-    #[test]
-    fn test_store_labels() {
-        let cases = vec![
-            "abc",
-            "abc=",
-            "abc.012",
-            "abc,012",
-            "abc=123*",
-            ".123=-abc",
-            "abc,123=123.abc",
-            "abc=123,abc=abc",
-        ];
-
-        for case in cases {
-            assert!(parse_store_labels(case).is_err());
-        }
-
-        let map = parse_store_labels("").unwrap();
-        assert!(map.is_empty());
-
-        let map = parse_store_labels("a=0").unwrap();
-        assert_eq!(map.get("a").unwrap(), "0");
-
-        let map = parse_store_labels("a.1-2=b_1.2").unwrap();
-        assert_eq!(map.get("a.1-2").unwrap(), "b_1.2");
-
-        let map = parse_store_labels("a.1-2=b_1.2,cab-012=3ac.8b2").unwrap();
-        assert_eq!(map.get("a.1-2").unwrap(), "b_1.2");
-        assert_eq!(map.get("cab-012").unwrap(), "3ac.8b2");
-
-        let map = parse_store_labels("zone=us-west-1,disk=ssd,Test=Test").unwrap();
-        assert_eq!(map.get("zone").unwrap(), "us-west-1");
-        assert_eq!(map.get("disk").unwrap(), "ssd");
-        assert_eq!(map.get("test").unwrap(), "test");
     }
 }
