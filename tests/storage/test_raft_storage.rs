35b945c13ff269ac828c00c23aff91a78c531575
Neil Shen <overvenus@gmail.com>
2017-08-03 00:52:17 +0800

    tests: testing get after leader lease

--- tests/storage/test_raft_storage.rs
+++ tests/storage/test_raft_storage.rs
@@ -99,6 +99,22 @@ fn test_raft_storage_batch_snapshot() {
 }
 
 #[test]
+fn test_raft_storage_get_after_lease() {
+    let (_cluster, storage, ctx) = new_raft_storage();
+    let key = b"key";
+    let value = b"value";
+    assert_eq!(storage.raw_get(ctx.clone(), key.to_vec()).unwrap(), None);
+    storage.raw_put(ctx.clone(), key.to_vec(), value.to_vec()).unwrap();
+    assert_eq!(storage.raw_get(ctx.clone(), key.to_vec()).unwrap().unwrap(),
+               value.to_vec());
+
+    // Sleep until the leader lease is expired.
+    thread::sleep(Duration::from_millis(MAX_LEADER_LEASE));
+    assert_eq!(storage.raw_get(ctx.clone(), key.to_vec()).unwrap().unwrap(),
+               value.to_vec());
+}
+
+#[test]
 fn test_raft_storage_rollback_before_prewrite() {
     let (_cluster, storage, ctx) = new_raft_storage();
     let ret = storage.rollback(ctx.clone(), vec![make_key(b"key")], 10);
