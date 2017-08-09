1ad21b81220b8fe1ee2ae051b68bcc79e65aa336
Neil Shen <overvenus@gmail.com>
2017-08-09 15:58:44 +0800

    Fix type

--- src/util/time.rs
+++ src/util/time.rs
@@ -200,23 +200,23 @@ mod inner {
     use libc;
 
     pub fn monotonic_raw_now() -> Timespec {
-        get_time(libc::CLOCK_MONOTONIC_RAW as _)
+        get_time(libc::CLOCK_MONOTONIC_RAW)
     }
 
     pub fn monotonic_now() -> Timespec {
-        get_time(libc::CLOCK_MONOTONIC as _)
+        get_time(libc::CLOCK_MONOTONIC)
     }
 
     pub fn monotonic_coarse_now() -> Timespec {
-        get_time(libc::CLOCK_MONOTONIC_COARSE as _)
+        get_time(libc::CLOCK_MONOTONIC_COARSE)
     }
 
-    fn get_time(clock: libc::clock_t) -> Timespec {
+    fn get_time(clock: libc::clockid_t) -> Timespec {
         let mut t = libc::timespec {
             tv_sec: 0,
             tv_nsec: 0,
         };
-        let errno = unsafe { libc::clock_gettime(clock as _, &mut t) };
+        let errno = unsafe { libc::clock_gettime(clock, &mut t) };
         if errno != 0 {
             panic!("failed to get monotonic raw locktime, err {}",
                    io::Error::last_os_error());
