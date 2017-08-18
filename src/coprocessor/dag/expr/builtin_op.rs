4bc449ff5ac2a8598a377cb28b22ae8cc73394ca
Cholerae Hu <huyingqian@pingcap.com>
2017-08-18 15:57:04 +0800

    dag: add more builtin
    Signed-off-by: Cholerae Hu <huyingqian@pingcap.com>

--- src/coprocessor/dag/expr/builtin_op.rs
+++ src/coprocessor/dag/expr/builtin_op.rs
@@ -29,35 +29,113 @@ impl FnCall {
     }
 
     pub fn logic_or(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let arg0 = try!(self.children[0].eval_int(ctx, row));
+        let arg1 = try!(self.children[1].eval_int(ctx, row));
+        match (arg0, arg1) {
+            (None, None) => Ok(None),
+            (None, Some(0)) => Ok(Some(0)),
+            (Some(0), None) => Ok(Some(0)),
+            (Some(0), Some(0)) => Ok(Some(0)),
+            _ => Ok(Some(1)),
+        }
     }
 
     pub fn logic_xor(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
+        let arg0 = try!(self.children[0].eval_int(ctx, row));
+        let arg1 = try!(self.children[1].eval_int(ctx, row));
+        match (arg0, arg1) {
+            (None, _) => Ok(None),
+            (_, None) => Ok(None),
+            (Some(0), Some(0)) => Ok(Some(0)),
+            (Some(0), _) => Ok(Some(1)),
+            (_, Some(0)) => Ok(Some(1)),
+            _ => Ok(Some(0)),
+        }
+    }
+
+    pub fn bit_and(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
+        unimplemented!()
+    }
+
+    pub fn bit_or(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
+        unimplemented!()
+    }
+
+    pub fn bit_xor(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
+        unimplemented!()
+    }
+
+    pub fn left_shift(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
+        unimplemented!()
+    }
+
+    pub fn right_shift(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
+        unimplemented!()
+    }
+
+    pub fn is_true_or_false(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
         unimplemented!()
     }
 
     pub fn real_is_true(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let input = try!(self.children[0].eval_int(ctx, row));
+        match input {
+            None => Ok(Some(0)),
+            Some(0) => Ok(Some(0)),
+            _ => Ok(Some(1)),
+        }
     }
 
     pub fn decimal_is_true(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let input = try!(self.children[0].eval_decimal(ctx, row));
+        if input.is_none() || input.unwrap.is_zero() {
+            Ok(Some(0))
+        }
+        Ok(Some(1))
     }
 
     pub fn int_is_true(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let input = try!(self.children[0].eval_int(ctx, row));
+        match input {
+            None => Ok(Some(0)),
+            Some(0) => Ok(Some(0)),
+            _ => Ok(Some(1)),
+        }
     }
 
     pub fn real_is_false(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let input = try!(self.children[0].eval_real(ctx, row));
+        match input {
+            None => Ok(Some(0)),
+            Some(0) => Ok(Some(1)),
+            _ => Ok(Some(0)),
+        }
     }
 
     pub fn decimal_is_false(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let input = try!(self.children[0].eval_decimal(ctx, row));
+        if input.is_none() || !input.unwrap.is_zero() {
+            Ok(Some(0))
+        }
+        Ok(Some(1))
     }
 
     pub fn int_is_false(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let input = try!(self.children[0].eval_int(ctx, row));
+        match input {
+            None => Ok(Some(0)),
+            Some(0) => Ok(Some(1)),
+            _ => Ok(Some(0)),
+        }
+    }
+
+    pub fn unary_not(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
+        let arg = try!(self.children[0].eval_int(ctx, row));
+        match arg {
+            None => Ok(None),
+            Some(0) => Ok(Some(1)),
+            _ => Ok(Some(0)),
+        }
     }
 
     pub fn unary_minus_int(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
@@ -65,26 +143,38 @@ impl FnCall {
     }
 
     pub fn decimal_is_null(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let arg = try!(self.children[0].eval_decimal(ctx, row));
+        eval_is_null(arg)
     }
 
     pub fn int_is_null(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let arg = try!(self.children[0].eval_int(ctx, row));
+        eval_is_null(arg)
     }
 
     pub fn real_is_null(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let arg = try!(self.children[0].eval_real(ctx, row));
+        eval_is_null(arg)
     }
 
     pub fn string_is_null(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let arg = try!(self.children[0].eval_string(ctx, row));
+        eval_is_null(arg)
     }
 
     pub fn time_is_null(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
-        unimplemented!()
+        let arg = try!(self.children[0].eval_time(ctx, row));
+        eval_is_null(arg)
     }
 
-    pub fn unary_not(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
+    pub fn bit_neg(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
         unimplemented!()
+    }
+}
+
+fn eval_is_null<T>(arg: Option<T>) -> Result<Option<T>> {
+    match arg {
+        None => Ok(Some(1)),
+        _ => Ok(Some(0)),
     }
 }
