5b9021f47381401edcb8a35bf67c7772ed29cdd4
Jay <BusyJay@users.noreply.github.com>
2017-08-18 12:24:59 +0800

    ci: enable clippy check in circle ci (#2186)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/coprocessor/dag/expr/constant.rs
+++ src/coprocessor/dag/expr/constant.rs
@@ -143,7 +143,7 @@ mod test {
     fn test_constant_eval() {
         let dec = "1.1".parse::<Decimal>().unwrap();
         let s = "你好".as_bytes().to_owned();
-        let dur = Duration::parse("01:00:00".as_bytes(), 0).unwrap();
+        let dur = Duration::parse(b"01:00:00", 0).unwrap();
 
         let tests = vec![
             datum_expr(Datum::Null),
