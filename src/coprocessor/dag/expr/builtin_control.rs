aec63fe6837c57c98718c7ead07101b00ec69b07
Cholerae Hu <huyingqian@pingcap.com>
2017-08-19 23:39:28 +0800

    dag: fix some stupid bugs
    Signed-off-by: Cholerae Hu <huyingqian@pingcap.com>

--- src/coprocessor/dag/expr/builtin_control.rs
+++ src/coprocessor/dag/expr/builtin_control.rs
@@ -21,19 +21,19 @@ use coprocessor::codec::mysql::{Decimal, Duration, Time};
 impl FnCall {
     pub fn if_null_int(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
         let arg0 = try!(self.children[0].eval_int(ctx, row));
-        if !arg0.is_some() {
+        if !arg0.is_none() {
             return Ok(arg0);
         }
-        let arg1 = try!(self.children[0].eval_int(ctx, row));
+        let arg1 = try!(self.children[1].eval_int(ctx, row));
         Ok(arg1)
     }
 
     pub fn if_null_real(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<f64>> {
         let arg0 = try!(self.children[0].eval_real(ctx, row));
-        if !arg0.is_some() {
+        if !arg0.is_none() {
             return Ok(arg0);
         }
-        let arg1 = try!(self.children[0].eval_real(ctx, row));
+        let arg1 = try!(self.children[1].eval_real(ctx, row));
         Ok(arg1)
     }
 
@@ -43,28 +43,28 @@ impl FnCall {
         row: &[Datum],
     ) -> Result<Option<Decimal>> {
         let arg0 = try!(self.children[0].eval_decimal(ctx, row));
-        if !arg0.is_some() {
+        if !arg0.is_none() {
             return Ok(arg0.map(|x| x.into_owned()));
         }
-        let arg1 = try!(self.children[0].eval_decimal(ctx, row));
+        let arg1 = try!(self.children[1].eval_decimal(ctx, row));
         Ok(arg1.map(|x| x.into_owned()))
     }
 
     pub fn if_null_string(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<Vec<u8>>> {
         let arg0 = try!(self.children[0].eval_string(ctx, row));
-        if !arg0.is_some() {
+        if !arg0.is_none() {
             return Ok(arg0.map(|x| x.into_owned()));
         }
-        let arg1 = try!(self.children[0].eval_string(ctx, row));
+        let arg1 = try!(self.children[1].eval_string(ctx, row));
         Ok(arg1.map(|x| x.into_owned()))
     }
 
     pub fn if_null_time(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<Time>> {
         let arg0 = try!(self.children[0].eval_time(ctx, row));
-        if !arg0.is_some() {
+        if !arg0.is_none() {
             return Ok(arg0.map(|x| x.into_owned()));
         }
-        let arg1 = try!(self.children[0].eval_time(ctx, row));
+        let arg1 = try!(self.children[1].eval_time(ctx, row));
         Ok(arg1.map(|x| x.into_owned()))
     }
 
@@ -74,17 +74,17 @@ impl FnCall {
         row: &[Datum],
     ) -> Result<Option<Duration>> {
         let arg0 = try!(self.children[0].eval_duration(ctx, row));
-        if !arg0.is_some() {
+        if !arg0.is_none() {
             return Ok(arg0.map(|x| x.into_owned()));
         }
-        let arg1 = try!(self.children[0].eval_duration(ctx, row));
+        let arg1 = try!(self.children[1].eval_duration(ctx, row));
         Ok(arg1.map(|x| x.into_owned()))
     }
 
     pub fn if_int(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
         let arg0 = try!(self.children[0].eval_int(ctx, row));
-        let arg1 = try!(self.children[0].eval_int(ctx, row));
-        let arg2 = try!(self.children[0].eval_int(ctx, row));
+        let arg1 = try!(self.children[1].eval_int(ctx, row));
+        let arg2 = try!(self.children[2].eval_int(ctx, row));
         match arg0 {
             None | Some(0) => Ok(arg2),
             _ => Ok(arg1),
@@ -93,8 +93,8 @@ impl FnCall {
 
     pub fn if_real(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<f64>> {
         let arg0 = try!(self.children[0].eval_int(ctx, row));
-        let arg1 = try!(self.children[0].eval_real(ctx, row));
-        let arg2 = try!(self.children[0].eval_real(ctx, row));
+        let arg1 = try!(self.children[1].eval_real(ctx, row));
+        let arg2 = try!(self.children[2].eval_real(ctx, row));
         match arg0 {
             None | Some(0) => Ok(arg2),
             _ => Ok(arg1),
@@ -103,8 +103,8 @@ impl FnCall {
 
     pub fn if_decimal(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<Decimal>> {
         let arg0 = try!(self.children[0].eval_int(ctx, row));
-        let arg1 = try!(self.children[0].eval_decimal(ctx, row));
-        let arg2 = try!(self.children[0].eval_decimal(ctx, row));
+        let arg1 = try!(self.children[1].eval_decimal(ctx, row));
+        let arg2 = try!(self.children[2].eval_decimal(ctx, row));
         match arg0 {
             None | Some(0) => Ok(arg2.map(|x| x.into_owned())),
             _ => Ok(arg1.map(|x| x.into_owned())),
@@ -113,8 +113,8 @@ impl FnCall {
 
     pub fn if_string(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<Vec<u8>>> {
         let arg0 = try!(self.children[0].eval_int(ctx, row));
-        let arg1 = try!(self.children[0].eval_string(ctx, row));
-        let arg2 = try!(self.children[0].eval_string(ctx, row));
+        let arg1 = try!(self.children[1].eval_string(ctx, row));
+        let arg2 = try!(self.children[2].eval_string(ctx, row));
         match arg0 {
             None | Some(0) => Ok(arg2.map(|x| x.into_owned())),
             _ => Ok(arg1.map(|x| x.into_owned())),
@@ -123,8 +123,8 @@ impl FnCall {
 
     pub fn if_time(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<Time>> {
         let arg0 = try!(self.children[0].eval_int(ctx, row));
-        let arg1 = try!(self.children[0].eval_time(ctx, row));
-        let arg2 = try!(self.children[0].eval_time(ctx, row));
+        let arg1 = try!(self.children[1].eval_time(ctx, row));
+        let arg2 = try!(self.children[2].eval_time(ctx, row));
         match arg0 {
             None | Some(0) => Ok(arg2.map(|x| x.into_owned())),
             _ => Ok(arg1.map(|x| x.into_owned())),
@@ -133,8 +133,8 @@ impl FnCall {
 
     pub fn if_duration(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<Duration>> {
         let arg0 = try!(self.children[0].eval_int(ctx, row));
-        let arg1 = try!(self.children[0].eval_duration(ctx, row));
-        let arg2 = try!(self.children[0].eval_duration(ctx, row));
+        let arg1 = try!(self.children[1].eval_duration(ctx, row));
+        let arg2 = try!(self.children[2].eval_duration(ctx, row));
         match arg0 {
             None | Some(0) => Ok(arg2.map(|x| x.into_owned())),
             _ => Ok(arg1.map(|x| x.into_owned())),
