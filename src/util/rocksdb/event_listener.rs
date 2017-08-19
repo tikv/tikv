aecb3dfad2552d00b21bd08a8c8e88777702e3d3
gladiator <lishuaihenu@gmail.com>
2017-08-19 11:29:27 +0800

    add missing db_name for event_listener

--- src/util/rocksdb/event_listener.rs
+++ src/util/rocksdb/event_listener.rs
@@ -38,10 +38,10 @@ impl rocksdb::EventListener for EventListener {
             .with_label_values(&[&self.db_name, info.cf_name(), "compaction"])
             .inc();
         STORE_ENGINE_COMPACTION_DURATIONS_VEC
-            .with_label_values(&[info.cf_name()])
+            .with_label_values(&[&self.db_name, info.cf_name()])
             .observe(info.elapsed_micros() as f64 / 1_000_000.0);
         STORE_ENGINE_COMPACTION_NUM_CORRUPT_KEYS_VEC
-            .with_label_values(&[info.cf_name()])
+            .with_label_values(&[&self.db_name, info.cf_name()])
             .inc_by(info.num_corrupt_keys() as f64)
             .unwrap();
     }
