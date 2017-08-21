22e15274baf465a4457b99470ac3c70bfe76b4c4
qupeng <onlyqupeng@gmail.com>
2017-08-21 11:38:24 +0800

    fix CI.

--- src/coprocessor/dag/expr/fncall.rs
+++ src/coprocessor/dag/expr/fncall.rs
@@ -17,38 +17,75 @@ use super::{Error, FnCall, Result};
 impl FnCall {
     pub fn check_args(sig: ScalarFuncSig, args: usize) -> Result<()> {
         let (min_args, max_args) = match sig {
-            ScalarFuncSig::LTInt => (2, 2),
-            ScalarFuncSig::LEInt => (2, 2),
-            ScalarFuncSig::GTInt => (2, 2),
-            ScalarFuncSig::GEInt => (2, 2),
-            ScalarFuncSig::EQInt => (2, 2),
-            ScalarFuncSig::NEInt => (2, 2),
-            ScalarFuncSig::NullEQInt => (2, 2),
-
-            ScalarFuncSig::LTReal => (2, 2),
-            ScalarFuncSig::LEReal => (2, 2),
-            ScalarFuncSig::GTReal => (2, 2),
-            ScalarFuncSig::GEReal => (2, 2),
-            ScalarFuncSig::EQReal => (2, 2),
-            ScalarFuncSig::NEReal => (2, 2),
-            ScalarFuncSig::NullEQReal => (2, 2),
-
-            ScalarFuncSig::PlusReal => (2, 2),
-            ScalarFuncSig::PlusDecimal => (2, 2),
-            ScalarFuncSig::PlusIntUnsigned => (2, 2),
-            ScalarFuncSig::PlusInt => (2, 2),
-
-            ScalarFuncSig::MinusReal => (2, 2),
-            ScalarFuncSig::MinusDecimal => (2, 2),
-            ScalarFuncSig::MinusIntUnsigned => (2, 2),
-            ScalarFuncSig::MinusInt => (2, 2),
-
-            ScalarFuncSig::MultiplyReal => (2, 2),
-            ScalarFuncSig::MultiplyDecimal => (2, 2),
-            ScalarFuncSig::MultiplyIntUnsigned => (2, 2),
+            ScalarFuncSig::LTInt |
+            ScalarFuncSig::LEInt |
+            ScalarFuncSig::GTInt |
+            ScalarFuncSig::GEInt |
+            ScalarFuncSig::EQInt |
+            ScalarFuncSig::NEInt |
+            ScalarFuncSig::NullEQInt |
+            ScalarFuncSig::LTReal |
+            ScalarFuncSig::LEReal |
+            ScalarFuncSig::GTReal |
+            ScalarFuncSig::GEReal |
+            ScalarFuncSig::EQReal |
+            ScalarFuncSig::NEReal |
+            ScalarFuncSig::NullEQReal |
+            ScalarFuncSig::LTDecimal |
+            ScalarFuncSig::LEDecimal |
+            ScalarFuncSig::GTDecimal |
+            ScalarFuncSig::GEDecimal |
+            ScalarFuncSig::EQDecimal |
+            ScalarFuncSig::NEDecimal |
+            ScalarFuncSig::NullEQDecimal |
+            ScalarFuncSig::LTString |
+            ScalarFuncSig::LEString |
+            ScalarFuncSig::GTString |
+            ScalarFuncSig::GEString |
+            ScalarFuncSig::EQString |
+            ScalarFuncSig::NEString |
+            ScalarFuncSig::NullEQString |
+            ScalarFuncSig::LTTime |
+            ScalarFuncSig::LETime |
+            ScalarFuncSig::GTTime |
+            ScalarFuncSig::GETime |
+            ScalarFuncSig::EQTime |
+            ScalarFuncSig::NETime |
+            ScalarFuncSig::NullEQTime |
+            ScalarFuncSig::LTDuration |
+            ScalarFuncSig::LEDuration |
+            ScalarFuncSig::GTDuration |
+            ScalarFuncSig::GEDuration |
+            ScalarFuncSig::EQDuration |
+            ScalarFuncSig::NEDuration |
+            ScalarFuncSig::NullEQDuration |
+            ScalarFuncSig::LTJson |
+            ScalarFuncSig::LEJson |
+            ScalarFuncSig::GTJson |
+            ScalarFuncSig::GEJson |
+            ScalarFuncSig::EQJson |
+            ScalarFuncSig::NEJson |
+            ScalarFuncSig::NullEQJson |
+            ScalarFuncSig::PlusReal |
+            ScalarFuncSig::PlusDecimal |
+            ScalarFuncSig::PlusIntUnsigned |
+            ScalarFuncSig::PlusInt |
+            ScalarFuncSig::MinusReal |
+            ScalarFuncSig::MinusDecimal |
+            ScalarFuncSig::MinusIntUnsigned |
+            ScalarFuncSig::MinusInt |
+            ScalarFuncSig::MultiplyReal |
+            ScalarFuncSig::MultiplyDecimal |
+            ScalarFuncSig::MultiplyIntUnsigned |
             ScalarFuncSig::MultiplyInt => (2, 2),
 
-            ScalarFuncSig::CastIntAsInt => (1, 1),
+            ScalarFuncSig::CastIntAsInt |
+            ScalarFuncSig::CastIntAsReal |
+            ScalarFuncSig::CastIntAsString |
+            ScalarFuncSig::CastIntAsDecimal |
+            ScalarFuncSig::CastIntAsTime |
+            ScalarFuncSig::CastIntAsDuration |
+            ScalarFuncSig::CastIntAsJson => (1, 1),
             _ => unimplemented!(),
         };
         if args < min_args || args > max_args {
