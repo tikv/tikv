516379628069e0d4918451ea6b388b9ebd8569ba
Cholerae Hu <huyingqian@pingcap.com>
2017-08-22 16:26:43 +0800

    dag: address comment
    Signed-off-by: Cholerae Hu <huyingqian@pingcap.com>

--- src/coprocessor/dag/expr/builtin_op.rs
+++ src/coprocessor/dag/expr/builtin_op.rs
@@ -36,10 +36,10 @@ impl FnCall {
             return Ok(Some(1));
         }
         let arg1 = try_opt!(self.children[1].eval_int(ctx, row));
-        match (arg0, arg1) {
-            (None, 0) => Ok(None),
-            (Some(0), 0) => Ok(Some(0)),
-            _ => Ok(Some(1)),
+        if arg1 != 0 {
+            Ok(Some(1))
+        } else {
+            Ok(arg0)
         }
     }
 
@@ -57,7 +57,7 @@ impl FnCall {
     pub fn decimal_is_true(&self, ctx: &StatementContext, row: &[Datum]) -> Result<Option<i64>> {
         let input = try!(self.children[0].eval_decimal(ctx, row));
         Ok(Some(
-            input.map_or(0, |dec| !dec.into_owned().is_zero() as i64),
+            input.map_or(0, |dec| !dec.is_zero() as i64),
         ))
     }
 
@@ -134,216 +134,172 @@ mod test {
                 ScalarFuncSig::LogicalAnd,
                 Datum::I64(1),
                 Datum::I64(1),
-                Datum::I64(1),
+                Some(1),
             ),
             (
                 ScalarFuncSig::LogicalAnd,
                 Datum::I64(1),
                 Datum::I64(0),
-                Datum::I64(0),
-            ),
-            (
-                ScalarFuncSig::LogicalAnd,
-                Datum::I64(0),
-                Datum::I64(1),
-                Datum::I64(0),
+                Some(0),
             ),
             (
                 ScalarFuncSig::LogicalAnd,
                 Datum::I64(0),
                 Datum::I64(0),
-                Datum::I64(0),
+                Some(0),
             ),
             (
                 ScalarFuncSig::LogicalAnd,
                 Datum::I64(2),
                 Datum::I64(-1),
-                Datum::I64(1),
+                Some(1),
             ),
             (
                 ScalarFuncSig::LogicalAnd,
                 Datum::I64(0),
                 Datum::Null,
-                Datum::I64(0),
-            ),
-            (
-                ScalarFuncSig::LogicalAnd,
-                Datum::Null,
-                Datum::I64(0),
-                Datum::I64(0),
+                Some(0),
             ),
             (
                 ScalarFuncSig::LogicalAnd,
                 Datum::Null,
                 Datum::I64(1),
-                Datum::Null,
+                None,
             ),
             (
                 ScalarFuncSig::LogicalOr,
                 Datum::I64(1),
                 Datum::I64(1),
-                Datum::I64(1),
+                Some(1),
             ),
             (
                 ScalarFuncSig::LogicalOr,
                 Datum::I64(1),
                 Datum::I64(0),
-                Datum::I64(1),
-            ),
-            (
-                ScalarFuncSig::LogicalOr,
-                Datum::I64(0),
-                Datum::I64(1),
-                Datum::I64(1),
+                Some(1),
             ),
             (
                 ScalarFuncSig::LogicalOr,
                 Datum::I64(0),
                 Datum::I64(0),
-                Datum::I64(0),
+                Some(0),
             ),
             (
                 ScalarFuncSig::LogicalOr,
                 Datum::I64(2),
                 Datum::I64(-1),
-                Datum::I64(1),
+                Some(1),
             ),
             (
                 ScalarFuncSig::LogicalOr,
                 Datum::I64(1),
                 Datum::Null,
-                Datum::I64(1),
-            ),
-            (
-                ScalarFuncSig::LogicalOr,
-                Datum::Null,
-                Datum::I64(1),
-                Datum::I64(1),
+                Some(1),
             ),
             (
                 ScalarFuncSig::LogicalOr,
                 Datum::Null,
                 Datum::I64(0),
-                Datum::Null,
+                None,
             ),
             (
                 ScalarFuncSig::LogicalXor,
                 Datum::I64(1),
                 Datum::I64(1),
-                Datum::I64(0),
+                Some(0),
             ),
             (
                 ScalarFuncSig::LogicalXor,
                 Datum::I64(1),
                 Datum::I64(0),
-                Datum::I64(1),
-            ),
-            (
-                ScalarFuncSig::LogicalXor,
-                Datum::I64(0),
-                Datum::I64(1),
-                Datum::I64(1),
+                Some(1),
             ),
             (
                 ScalarFuncSig::LogicalXor,
                 Datum::I64(0),
                 Datum::I64(0),
-                Datum::I64(0),
+                Some(0),
             ),
             (
                 ScalarFuncSig::LogicalXor,
                 Datum::I64(2),
                 Datum::I64(-1),
-                Datum::I64(0),
+                Some(0),
             ),
             (
                 ScalarFuncSig::LogicalXor,
                 Datum::I64(0),
                 Datum::Null,
-                Datum::Null,
-            ),
-            (
-                ScalarFuncSig::LogicalXor,
-                Datum::Null,
-                Datum::I64(0),
-                Datum::Null,
+                None,
             ),
             (
                 ScalarFuncSig::LogicalXor,
                 Datum::Null,
                 Datum::I64(1),
-                Datum::Null,
+                None,
             ),
         ];
         let ctx = StatementContext::default();
-        for tt in tests {
-            let arg1 = datum_expr(tt.1);
-            let arg2 = datum_expr(tt.2);
-            let expected = Expression::build(datum_expr(tt.3), 0).unwrap();
-            let op = Expression::build(fncall_expr(tt.0, &[arg1, arg2]), 0).unwrap();
-            let lhs = op.eval_int(&ctx, &[]).unwrap();
-            let rhs = expected.eval_int(&ctx, &[]).unwrap();
-            assert_eq!(lhs, rhs);
+        for (op, lhs, rhs, exp) in tests {
+            let arg1 = datum_expr(lhs);
+            let arg2 = datum_expr(rhs);
+            {
+                let op = Expression::build(fncall_expr(op, &[arg1.clone(), arg2.clone()]), 0).unwrap();
+                let res = op.eval_int(&ctx, &[]).unwrap();
+                assert_eq!(res, exp);
+            }
+            {
+                let op = Expression::build(fncall_expr(op, &[arg2, arg1]), 0).unwrap();
+                let res = op.eval_int(&ctx, &[]).unwrap();
+                assert_eq!(res, exp);
+            }
         }
     }
 
     #[test]
     fn test_unary_op() {
         let tests = vec![
-            (ScalarFuncSig::UnaryNot, Datum::I64(1), Datum::I64(0)),
-            (ScalarFuncSig::UnaryNot, Datum::I64(0), Datum::I64(1)),
-            (ScalarFuncSig::UnaryNot, Datum::I64(123), Datum::I64(0)),
-            (ScalarFuncSig::UnaryNot, Datum::I64(-123), Datum::I64(0)),
-            (ScalarFuncSig::UnaryNot, Datum::Null, Datum::Null),
-            (ScalarFuncSig::RealIsTrue, Datum::F64(0.25), Datum::I64(1)),
-            (ScalarFuncSig::RealIsTrue, Datum::F64(0.0), Datum::I64(0)),
-            (ScalarFuncSig::RealIsNull, Datum::F64(1.25), Datum::I64(0)),
-            (ScalarFuncSig::RealIsNull, Datum::Null, Datum::I64(1)),
-            (ScalarFuncSig::DecimalIsTrue, str2dec("1.1"), Datum::I64(1)),
-            (ScalarFuncSig::DecimalIsTrue, str2dec("0"), Datum::I64(0)),
-            (ScalarFuncSig::DecimalIsNull, str2dec("1.1"), Datum::I64(0)),
-            (ScalarFuncSig::DecimalIsNull, Datum::Null, Datum::I64(1)),
-            (ScalarFuncSig::IntIsFalse, Datum::I64(0), Datum::I64(1)),
-            (ScalarFuncSig::IntIsFalse, Datum::I64(1), Datum::I64(0)),
-            (ScalarFuncSig::IntIsNull, Datum::I64(1), Datum::I64(0)),
-            (ScalarFuncSig::IntIsNull, Datum::Null, Datum::I64(1)),
-            (ScalarFuncSig::StringIsNull, Datum::Null, Datum::I64(1)),
+            (ScalarFuncSig::UnaryNot, Datum::I64(1), Some(0)),
+            (ScalarFuncSig::UnaryNot, Datum::I64(0), Some(1)),
+            (ScalarFuncSig::UnaryNot, Datum::I64(123), Some(0)),
+            (ScalarFuncSig::UnaryNot, Datum::I64(-123), Some(0)),
+            (ScalarFuncSig::UnaryNot, Datum::Null, None),
+            (ScalarFuncSig::RealIsTrue, Datum::F64(0.25), Some(1)),
+            (ScalarFuncSig::RealIsTrue, Datum::F64(0.0), Some(0)),
+            (ScalarFuncSig::RealIsTrue, Datum::Null, Some(0)),
+            (ScalarFuncSig::RealIsNull, Datum::F64(1.25), Some(0)),
+            (ScalarFuncSig::RealIsNull, Datum::Null, Some(1)),
+            (ScalarFuncSig::DecimalIsTrue, str2dec("1.1"), Some(1)),
+            (ScalarFuncSig::DecimalIsTrue, str2dec("0"), Some(0)),
+            (ScalarFuncSig::DecimalIsTrue, Datum::Null, Some(0)),
+            (ScalarFuncSig::DecimalIsNull, str2dec("1.1"), Some(0)),
+            (ScalarFuncSig::DecimalIsNull, Datum::Null, Some(1)),
+            (ScalarFuncSig::IntIsFalse, Datum::I64(0), Some(1)),
+            (ScalarFuncSig::IntIsFalse, Datum::I64(1), Some(0)),
+            (ScalarFuncSig::IntIsFalse, Datum::Null, Some(0)),
+            (ScalarFuncSig::IntIsNull, Datum::I64(1), Some(0)),
+            (ScalarFuncSig::IntIsNull, Datum::Null, Some(1)),
+            (ScalarFuncSig::StringIsNull, Datum::Null, Some(1)),
             (
                 ScalarFuncSig::StringIsNull,
                 Datum::Bytes(b"abc".to_vec()),
-                Datum::I64(0),
+                Some(0),
             ),
-            (ScalarFuncSig::TimeIsNull, Datum::Null, Datum::I64(1)),
+            (ScalarFuncSig::TimeIsNull, Datum::Null, Some(1)),
             // TODO: add Time related tests after Time is implemented in Expression::build
             (
                 ScalarFuncSig::DurationIsNull,
                 Datum::Dur(Duration::zero()),
-                Datum::I64(0),
+                Some(0),
             ),
-            (ScalarFuncSig::DurationIsNull, Datum::Null, Datum::I64(1)),
+            (ScalarFuncSig::DurationIsNull, Datum::Null, Some(1)),
         ];
         let ctx = StatementContext::default();
-        for tt in tests {
-            let arg1 = datum_expr(tt.1);
-            let expected = Expression::build(datum_expr(tt.2), 0).unwrap();
-            let op = Expression::build(fncall_expr(tt.0, &[arg1]), 0).unwrap();
-            match tt.0 {
-                ScalarFuncSig::UnaryNot |
-                ScalarFuncSig::IntIsNull |
-                ScalarFuncSig::IntIsFalse |
-                ScalarFuncSig::RealIsTrue |
-                ScalarFuncSig::RealIsNull |
-                ScalarFuncSig::DecimalIsNull |
-                ScalarFuncSig::DecimalIsTrue |
-                ScalarFuncSig::StringIsNull |
-                ScalarFuncSig::TimeIsNull |
-                ScalarFuncSig::DurationIsNull => {
-                    let lhs = op.eval_int(&ctx, &[]).unwrap();
-                    let rhs = expected.eval_int(&ctx, &[]).unwrap();
-                    assert_eq!(lhs, rhs);
-                }
-                _ => unreachable!(),
-            }
+        for (op, arg, exp) in tests {
+            let arg1 = datum_expr(arg);
+            let op = Expression::build(fncall_expr(op, &[arg1]), 0).unwrap();
+            let res = op.eval_int(&ctx, &[]).unwrap();
+            assert_eq!(res, exp);
         }
     }
 }
