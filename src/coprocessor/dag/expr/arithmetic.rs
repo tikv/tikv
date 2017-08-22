c3bdb9fd9c217f515f22be551a1491c538475d40
qupeng <onlyqupeng@gmail.com>
2017-08-22 15:24:53 +0800

    fix

--- src/coprocessor/dag/expr/arithmetic.rs
+++ src/coprocessor/dag/expr/arithmetic.rs
@@ -173,7 +173,7 @@ fn check_integer_overflow(us: bool, res: i64, res_us: bool) -> Result<Option<i64
 
 #[cfg(test)]
 mod test {
-    use std::{f64, i64};
+    use std::{f64, i64, u64};
     use tipb::expression::ScalarFuncSig;
     use coprocessor::codec::{mysql, Datum};
     use coprocessor::codec::mysql::types;
@@ -189,7 +189,7 @@ mod test {
     }
 
     #[test]
-    fn test_arithmetic() {
+    fn test_arithmetic_int() {
         let tests = vec![
             (
                 ScalarFuncSig::PlusInt,
@@ -210,22 +210,49 @@ mod test {
                 Datum::I64(13),
             ),
             (
-                ScalarFuncSig::PlusReal,
-                Datum::F64(1.01001),
-                Datum::F64(-0.01),
-                Datum::F64(1.00001),
-            ),
-            (
-                ScalarFuncSig::PlusDecimal,
-                str2dec("1.1"),
-                str2dec("2.2"),
-                str2dec("3.3"),
-            ),
-            (
                 ScalarFuncSig::MinusInt,
                 Datum::I64(12),
                 Datum::I64(1),
                 Datum::I64(11),
+            ),
+            (
+                ScalarFuncSig::MultiplyInt,
+                Datum::I64(12),
+                Datum::I64(1),
+                Datum::I64(12),
+            ),
+        ];
+        let ctx = StatementContext::default();
+        for tt in tests {
+            let lhs = datum_expr(tt.1);
+            let rhs = datum_expr(tt.2);
+
+            let lus = mysql::has_unsigned_flag(lhs.get_field_type().get_flag());
+            let rus = mysql::has_unsigned_flag(rhs.get_field_type().get_flag());
+            let unsigned = lus | rus;
+
+            let mut op = Expression::build(fncall_expr(tt.0, &[lhs, rhs]), 0).unwrap();
+            if unsigned {
+                // According to TiDB, the result is unsigned if any of arguments is unsigned.
+                op.mut_tp().set_flag(types::UNSIGNED_FLAG as u32);
+            }
+
+            let expected = Expression::build(datum_expr(tt.3), 0).unwrap();
+
+            let got = op.eval_int(&ctx, &[]).unwrap();
+            let exp = expected.eval_int(&ctx, &[]).unwrap();
+            assert_eq!(got, exp);
+        }
+    }
+
+    #[test]
+    fn test_arithmetic_real() {
+        let tests = vec![
+            (
+                ScalarFuncSig::PlusReal,
+                Datum::F64(1.01001),
+                Datum::F64(-0.01),
+                Datum::F64(1.00001),
             ),
             (
                 ScalarFuncSig::MinusReal,
@@ -234,22 +261,40 @@ mod test {
                 Datum::F64(1.02001),
             ),
             (
-                ScalarFuncSig::MinusDecimal,
-                str2dec("1.1"),
-                str2dec("2.2"),
-                str2dec("-1.1"),
-            ),
-            (
-                ScalarFuncSig::MultiplyInt,
-                Datum::I64(12),
-                Datum::I64(1),
-                Datum::I64(12),
-            ),
-            (
                 ScalarFuncSig::MultiplyReal,
                 Datum::F64(1.01001),
                 Datum::F64(-0.01),
                 Datum::F64(-0.0101001),
+            ),
+        ];
+        let ctx = StatementContext::default();
+        for tt in tests {
+            let lhs = datum_expr(tt.1);
+            let rhs = datum_expr(tt.2);
+
+            let op = Expression::build(fncall_expr(tt.0, &[lhs, rhs]), 0).unwrap();
+            let expected = Expression::build(datum_expr(tt.3), 0).unwrap();
+
+            let got = op.eval_real(&ctx, &[]).unwrap();
+            let exp = expected.eval_real(&ctx, &[]).unwrap();
+            assert_eq!(got, exp);
+        }
+    }
+
+    #[test]
+    fn test_arithmetic_decimal() {
+        let tests = vec![
+            (
+                ScalarFuncSig::PlusDecimal,
+                str2dec("1.1"),
+                str2dec("2.2"),
+                str2dec("3.3"),
+            ),
+            (
+                ScalarFuncSig::MinusDecimal,
+                str2dec("1.1"),
+                str2dec("2.2"),
+                str2dec("-1.1"),
             ),
             (
                 ScalarFuncSig::MultiplyDecimal,
@@ -258,33 +303,17 @@ mod test {
                 str2dec("2.42"),
             ),
         ];
-
         let ctx = StatementContext::default();
         for tt in tests {
             let lhs = datum_expr(tt.1);
             let rhs = datum_expr(tt.2);
-            let expected = Expression::build(datum_expr(tt.3), 0).unwrap();
+
             let op = Expression::build(fncall_expr(tt.0, &[lhs, rhs]), 0).unwrap();
-            match tt.0 {
-                ScalarFuncSig::PlusInt | ScalarFuncSig::MinusInt | ScalarFuncSig::MultiplyInt => {
-                    let lhs = op.eval_int(&ctx, &[]).unwrap();
-                    let rhs = expected.eval_int(&ctx, &[]).unwrap();
-                    assert_eq!(lhs, rhs);
-                }
-                ScalarFuncSig::PlusReal | ScalarFuncSig::MinusReal | ScalarFuncSig::MultiplyReal => {
-                    let lhs = op.eval_real(&ctx, &[]).unwrap();
-                    let rhs = expected.eval_real(&ctx, &[]).unwrap();
-                    assert_eq!(lhs, rhs);
-                }
-                ScalarFuncSig::PlusDecimal |
-                ScalarFuncSig::MinusDecimal |
-                ScalarFuncSig::MultiplyDecimal => {
-                    let lhs = op.eval_decimal(&ctx, &[]).unwrap();
-                    let rhs = expected.eval_decimal(&ctx, &[]).unwrap();
-                    assert_eq!(lhs, rhs);
-                }
-                _ => unreachable!(),
-            }
+            let expected = Expression::build(datum_expr(tt.3), 0).unwrap();
+
+            let got = op.eval_decimal(&ctx, &[]).unwrap();
+            let exp = expected.eval_decimal(&ctx, &[]).unwrap();
+            assert_eq!(got, exp);
         }
     }
 
@@ -303,6 +332,28 @@ mod test {
             ),
             (ScalarFuncSig::PlusInt, Datum::I64(-2), Datum::U64(1)),
             (ScalarFuncSig::PlusInt, Datum::U64(1), Datum::I64(-2)),
+            (
+                ScalarFuncSig::MinusInt,
+                Datum::I64(i64::MIN),
+                Datum::I64(i64::MAX),
+            ),
+            (
+                ScalarFuncSig::MinusInt,
+                Datum::I64(i64::MAX),
+                Datum::I64(i64::MIN),
+            ),
+            (ScalarFuncSig::MinusInt, Datum::I64(-1), Datum::U64(2)),
+            (ScalarFuncSig::MinusInt, Datum::U64(1), Datum::I64(2)),
+            (
+                ScalarFuncSig::MultiplyInt,
+                Datum::I64(i64::MIN),
+                Datum::I64(i64::MAX),
+            ),
+            (
+                ScalarFuncSig::MultiplyInt,
+                Datum::U64(u64::MAX),
+                Datum::I64(i64::MAX),
+            ),
         ];
         let ctx = StatementContext::default();
         for tt in tests {
@@ -319,28 +370,13 @@ mod test {
                 op.mut_tp().set_flag(types::UNSIGNED_FLAG as u32);
             }
 
-            match tt.0 {
-                ScalarFuncSig::PlusInt | ScalarFuncSig::MinusInt | ScalarFuncSig::MultiplyInt => {
-                    let lhs = op.eval_int(&ctx, &[]).unwrap_err();
-                    assert!(check_overflow(lhs).is_ok());
-                }
-                ScalarFuncSig::PlusReal | ScalarFuncSig::MinusReal | ScalarFuncSig::MultiplyReal => {
-                    let lhs = op.eval_real(&ctx, &[]).unwrap_err();
-                    assert!(check_overflow(lhs).is_ok());
-                }
-                ScalarFuncSig::PlusDecimal |
-                ScalarFuncSig::MinusDecimal |
-                ScalarFuncSig::MultiplyDecimal => {
-                    let lhs = op.eval_decimal(&ctx, &[]).unwrap_err();
-                    assert!(check_overflow(lhs).is_ok());
-                }
-                _ => unreachable!(),
-            }
+            let got = op.eval_int(&ctx, &[]).unwrap_err();
+            assert!(check_overflow(got).is_ok());
         }
     }
 
     #[test]
-    fn test_arithmetic_overflow() {
+    fn test_arithmetic_real_overflow() {
         let tests = vec![
             (
                 ScalarFuncSig::PlusReal,
@@ -348,24 +384,9 @@ mod test {
                 Datum::F64(f64::MAX),
             ),
             (
-                ScalarFuncSig::MinusInt,
-                Datum::I64(i64::MIN),
-                Datum::I64(i64::MAX),
-            ),
-            (
-                ScalarFuncSig::MinusInt,
-                Datum::I64(i64::MAX),
-                Datum::I64(i64::MIN),
-            ),
-            (
                 ScalarFuncSig::MinusReal,
                 Datum::F64(f64::MIN),
                 Datum::F64(f64::MAX),
-            ),
-            (
-                ScalarFuncSig::MultiplyInt,
-                Datum::I64(i64::MIN),
-                Datum::I64(i64::MAX),
             ),
             (
                 ScalarFuncSig::MultiplyReal,
@@ -373,32 +394,14 @@ mod test {
                 Datum::F64(f64::MAX),
             ),
         ];
-
         let ctx = StatementContext::default();
-        let mut i = 0;
         for tt in tests {
-            println!("i: {}", i);
-            i += 1;
             let lhs = datum_expr(tt.1);
             let rhs = datum_expr(tt.2);
+
             let op = Expression::build(fncall_expr(tt.0, &[lhs, rhs]), 0).unwrap();
-            match tt.0 {
-                ScalarFuncSig::PlusInt | ScalarFuncSig::MinusInt | ScalarFuncSig::MultiplyInt => {
-                    let lhs = op.eval_int(&ctx, &[]).unwrap_err();
-                    assert!(check_overflow(lhs).is_ok());
-                }
-                ScalarFuncSig::PlusReal | ScalarFuncSig::MinusReal | ScalarFuncSig::MultiplyReal => {
-                    let lhs = op.eval_real(&ctx, &[]).unwrap_err();
-                    assert!(check_overflow(lhs).is_ok());
-                }
-                ScalarFuncSig::PlusDecimal |
-                ScalarFuncSig::MinusDecimal |
-                ScalarFuncSig::MultiplyDecimal => {
-                    let lhs = op.eval_decimal(&ctx, &[]).unwrap_err();
-                    assert!(check_overflow(lhs).is_ok());
-                }
-                _ => unreachable!(),
-            }
+            let got = op.eval_real(&ctx, &[]).unwrap_err();
+            assert!(check_overflow(got).is_ok());
         }
     }
 }
