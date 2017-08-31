44ed9e3cf5d2cfd26c8f3d27639d9c714b61d28d
Neil Shen <overvenus@gmail.com>
2017-09-01 01:14:13 +0800

    tests: add tests for MVCC/TXN servicecs, take 2

--- tests/raftstore/test_service.rs
+++ tests/raftstore/test_service.rs
@@ -85,26 +85,18 @@ fn test_grpc_service() {
         let mut ts = 0;
 
         ts += 1;
-        let start_version = ts;
-        let mut prewrite_req = PrewriteRequest::new();
-        prewrite_req.set_context(ctx.clone());
-        let mut muts = Mutation::new();
-        muts.op = Op::Put;
-        muts.key = k.clone();
-        muts.value = v.clone();
-        prewrite_req.set_mutations(vec![muts].into_iter().collect());
-        prewrite_req.primary_lock = k.clone();
-        prewrite_req.start_version = start_version;
-        prewrite_req.lock_ttl = prewrite_req.start_version + 1;
-        let prewrite_resp = client.kv_prewrite(prewrite_req).unwrap();
-        assert!(!prewrite_resp.has_region_error());
-        assert!(prewrite_resp.errors.is_empty());
+        let prewrite_start_version = ts;
+        let mut mutation = Mutation::new();
+        mutation.op = Op::Put;
+        mutation.key = k.clone();
+        mutation.value = v.clone();
+        must_prewrite(&client, ctx.clone(), vec![mutation], k.clone(), prewrite_start_version);
 
         ts += 1;
         let commit_version = ts;
         let mut commit_req = CommitRequest::new();
         commit_req.set_context(ctx.clone());
-        commit_req.start_version = start_version;
+        commit_req.start_version = prewrite_start_version;
         commit_req.set_keys(vec![k.clone()].into_iter().collect());
         commit_req.commit_version = commit_version;
         let commit_resp = client.kv_commit(commit_req).unwrap();
@@ -137,5 +129,78 @@ fn test_grpc_service() {
             assert_eq!(kv.key, k);
             assert_eq!(kv.value, v);
         }
+
+        ts += 1;
+        let batch_get_version = ts;
+        let mut batch_get_req = BatchGetRequest::new();
+        batch_get_req.set_context(ctx.clone());
+        batch_get_req.set_keys(vec![k.clone()].into_iter().collect());
+        batch_get_req.version = batch_get_version;
+        let batch_get_resp = client.kv_batch_get(batch_get_req).unwrap();
+        assert_eq!(batch_get_resp.pairs.len(), 1);
+        for kv in batch_get_resp.pairs.into_iter() {
+            assert!(!kv.has_error());
+            assert_eq!(kv.key, k);
+            assert_eq!(kv.value, v);
+        }
+
+        ts += 1;
+        let prewrite_start_version2 = ts;
+        let (k2, v2) = (b"key2".to_vec(), b"value2".to_vec());
+        let mut mut_pri = Mutation::new();
+        mut_pri.op = Op::Put;
+        mut_pri.key = k2.clone();
+        mut_pri.value = v2.clone();
+        let mut mut_sec = Mutation::new();
+        mut_sec.op = Op::Put;
+        mut_sec.key = k.clone();
+        mut_sec.value = b"foo".to_vec();
+        must_prewrite(&client, ctx.clone(), vec![mut_pri, mut_sec], k2.clone(), prewrite_start_version2);
+
+        ts += 1;
+        let scan_lock_max_version = ts;
+        let mut scan_lock_req = ScanLockRequest::new();
+        scan_lock_req.set_context(ctx.clone());
+        scan_lock_req.max_version = scan_lock_max_version;
+        let scan_lock_resp = client.kv_scan_lock(scan_lock_req).unwrap();
+        assert!(!scan_lock_resp.has_region_error());
+        assert_eq!(scan_lock_resp.locks.len(), 2);
+        for (lock, key) in scan_lock_resp.locks.into_iter().zip(vec![k.clone(), k2.clone()]) {
+            assert_eq!(lock.primary_lock, k2);
+            assert_eq!(lock.key, key);
+            assert_eq!(lock.lock_version, prewrite_start_version2);
+        }
+
+        ts += 1;
+        let rollback_start_version = ts;
+        let mut rollback_req = BatchRollbackRequest::new();
+        rollback_req.set_context(ctx.clone());
+        rollback_req.start_version = rollback_start_version;
+        rollback_req.set_keys(vec![k2.clone()].into_iter().collect());
+        let rollback_resp = client.kv_batch_rollback(rollback_req).unwrap();
+        assert!(!rollback_resp.has_region_error());
+        assert!(!rollback_resp.has_error());
+
+        ts += 1;
+        let cleanup_start_version = ts;
+        let mut cleanup_req = CleanupRequest::new();
+        cleanup_req.set_context(ctx.clone());
+        cleanup_req.start_version = cleanup_start_version;
+        cleanup_req.set_key(k2.clone());
+        let cleanup_resp = client.kv_cleanup(cleanup_req).unwrap();
+        assert!(!cleanup_resp.has_region_error());
+        assert!(!cleanup_resp.has_error());
     }
+}
+
+fn must_prewrite(client: &TikvClient, ctx: Context, muts: Vec<Mutation>, pk: Vec<u8>, ts: u64) {
+    let mut prewrite_req = PrewriteRequest::new();
+    prewrite_req.set_context(ctx.clone());
+    prewrite_req.set_mutations(muts.into_iter().collect());
+    prewrite_req.primary_lock = pk;
+    prewrite_req.start_version = ts;
+    prewrite_req.lock_ttl = prewrite_req.start_version + 1;
+    let prewrite_resp = client.kv_prewrite(prewrite_req).unwrap();
+    assert!(!prewrite_resp.has_region_error());
+    assert!(prewrite_resp.errors.is_empty());
 }
