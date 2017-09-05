3cb8726ef8ead96c0f93fdd2e4840e7580c17ddd
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-09-05 21:29:17 +0800

    address comments

--- src/raftstore/store/util.rs
+++ src/raftstore/store/util.rs
@@ -112,7 +112,7 @@ pub fn delete_all_in_range(db: &DB, start_key: &[u8], end_key: &[u8]) -> Result<
 
 pub fn delete_in_range_cf(db: &DB, cf: &str, start_key: &[u8], end_key: &[u8]) -> Result<()> {
     let handle = try!(rocksdb_util::get_cf_handle(db, cf));
-    //    try!(db.delete_file_in_range_cf(handle, start_key, end_key));
+    try!(db.delete_file_in_range_cf(handle, start_key, end_key));
 
     let iter_opt = IterOption::new(Some(end_key.to_vec()), false);
     let mut it = try!(db.new_iterator_cf(cf, iter_opt));
