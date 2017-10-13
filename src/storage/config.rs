196c5ba759e7e82ed2543b09d9ce299f3b51d27a
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-10-13 17:09:22 +0800

    address comments

--- src/storage/config.rs
+++ src/storage/config.rs
@@ -24,9 +24,9 @@ const DEFAULT_SCHED_CAPACITY: usize = 10240;
 const DEFAULT_SCHED_MSG_PER_TICK: usize = 1024;
 const DEFAULT_SCHED_CONCURRENCY: usize = 102400;
 
-// Assume one TiKV node can write 100_000 kvs per second,
-// and write requests' average process time is about 100ms,
-// so here use 10_000 as the default value.
+// According to "Little's law", assuming you can write 100_000 KVs
+// per second, and it takes about 100ms to process the write requests
+// on average, hence using the 10_000 as the default value here.
 const DEFAULT_SCHED_TOO_BUSY_THRESHOLD: usize = 10_000;
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
