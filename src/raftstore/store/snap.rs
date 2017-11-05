bfdf932e104974b75a0e11382f0e3433ae951694
UncP <uncp.xu@gmail.com>
2017-11-05 15:19:59 +0800

    fix compilation error
    Signed-off-by: UncP <uncp.xu@gmail.com>

--- src/raftstore/store/snap.rs
+++ src/raftstore/store/snap.rs
@@ -1335,7 +1335,7 @@ impl SnapManager {
     }
 
     pub fn get_limiter(&mut self) -> &mut RateLimiter {
-        *Arc::get_mut(&mut self.limiter).unrap()
+        *Arc::get_mut(&mut self.limiter).unwrap()
     }
 }
 
