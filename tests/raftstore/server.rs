97e8406b6055bdbb092f730b50626d00496f4287
UncP <uncp.xu@gmail.com>
2017-11-09 22:52:54 +0800

    fix compilation error

--- tests/raftstore/server.rs
+++ tests/raftstore/server.rs
@@ -121,7 +121,7 @@ impl Simulator for ServerCluster {
 
         // Create pd client, snapshot manager, server.
         let (worker, resolver) = resolve::new_resolver(self.pd_client.clone()).unwrap();
-        let snap_mgr = SnapManager::new(tmp_str, Some(store_sendch));
+        let snap_mgr = SnapManager::new(tmp_str, Some(store_sendch), 1024 * 1024, 10 * 1024 * 1024);
         let pd_worker = FutureWorker::new("test-pd-worker");
         let mut server = Server::new(
             &cfg.server,
