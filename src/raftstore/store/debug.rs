d93bf319bc2473d48311760a17af8a57bbd0a254
Neil Shen <overvenus@gmail.com>
2017-11-13 20:37:34 +0800

    store/debug: check mvcc range (#2464)

--- src/raftstore/store/debug.rs
+++ src/raftstore/store/debug.rs
@@ -287,6 +287,12 @@ pub struct MvccInfoIterator {
 
 impl MvccInfoIterator {
     fn new(db: &Arc<DB>, from: &[u8], to: &[u8], limit: u64) -> Result<Self> {
+        if !keys::validate_data_key(from) {
+            return Err(Error::InvalidArgument(
+                format!("from non-mvcc area {:?}", from),
+            ));
+        }
+
         let gen_iter = |cf: &str| -> Result<_> {
             let to = if to.is_empty() { None } else { Some(to) };
             let readopts = IterOption::new(to.map(Vec::from), false).build_read_opts();
