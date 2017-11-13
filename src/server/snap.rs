378fded37173ec7a175b42a2fa28e7a9dee4ed0a
UncP <uncp.xu@gmail.com>
2017-11-13 12:46:58 +0800

    fix compilation error

--- src/server/snap.rs
+++ src/server/snap.rs
@@ -25,7 +25,7 @@ use kvproto::raft_serverpb::SnapshotChunk;
 use kvproto::raft_serverpb::RaftMessage;
 use kvproto::tikvpb_grpc::TikvClient;
 
-use raftstore::store::{SnapEntry, SnapKey, SnapManager, Snapshot, SnapshotIOLimiter};
+use raftstore::store::{SnapEntry, SnapKey, SnapManager, Snapshot};
 use util::threadpool::{DefaultContext, ThreadPool, ThreadPoolBuilder};
 use util::worker::Runnable;
 use util::buf::PipeBuffer;
