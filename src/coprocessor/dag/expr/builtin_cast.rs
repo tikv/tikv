3cadfb7dccd4b0931963ffad25be483bf4eef977
Breezewish <me@breeswish.org>
2017-12-06 15:28:40 +0800

    grumble

--- src/coprocessor/dag/expr/builtin_cast.rs
+++ src/coprocessor/dag/expr/builtin_cast.rs
@@ -592,7 +592,7 @@ impl FnCall {
 
     /// `produce_str_with_specified_tp`(`ProduceStrWithSpecifiedTp` in tidb) produces
     /// a new string according to `flen` and `chs`.
-    pub fn produce_str_with_specified_tp<'a, 'b: 'a>(
+    fn produce_str_with_specified_tp<'a, 'b: 'a>(
         &'b self,
         ctx: &StatementContext,
         s: Cow<'a, [u8]>,
