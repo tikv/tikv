2507994d6163be482c0e801166257b9113fd1c0f
qupeng <onlyqupeng@gmail.com>
2017-12-11 20:59:17 +0800

    address comments.

--- src/util/timer.rs
+++ src/util/timer.rs
@@ -11,32 +11,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::cmp::Ordering;
-use std::time::{Duration, Instant};
+use std::cmp::{max, Ordering};
+use std::time::Duration;
+use util::time::Instant;
 use std::collections::BinaryHeap;
 
-struct Timeout<T> {
-    tick: Instant,
+struct TimeoutTask<T> {
+    next_tick: Instant,
     timeout: Duration,
     task: T,
 }
 
-impl<T> PartialEq for Timeout<T> {
-    fn eq(&self, other: &Timeout<T>) -> bool {
-        if self.tick == other.tick {
+impl<T> PartialEq for TimeoutTask<T> {
+    fn eq(&self, other: &TimeoutTask<T>) -> bool {
+        if self.next_tick == other.next_tick {
             return true;
         }
         false
     }
 }
 
-impl<T> Eq for Timeout<T> {}
+impl<T> Eq for TimeoutTask<T> {}
 
-impl<T> PartialOrd for Timeout<T> {
-    fn partial_cmp(&self, other: &Timeout<T>) -> Option<Ordering> {
-        Some(if self.tick > other.tick {
+impl<T> PartialOrd for TimeoutTask<T> {
+    fn partial_cmp(&self, other: &TimeoutTask<T>) -> Option<Ordering> {
+        Some(if self.next_tick > other.next_tick {
             Ordering::Less
-        } else if self.tick < other.tick {
+        } else if self.next_tick < other.next_tick {
             Ordering::Greater
         } else {
             Ordering::Equal
@@ -44,52 +45,81 @@ impl<T> PartialOrd for Timeout<T> {
     }
 }
 
-impl<T> Ord for Timeout<T> {
-    fn cmp(&self, other: &Timeout<T>) -> Ordering {
+impl<T> Ord for TimeoutTask<T> {
+    fn cmp(&self, other: &TimeoutTask<T>) -> Ordering {
         self.partial_cmp(other).unwrap()
     }
 }
 
-pub struct Timer<T: Copy> {
-    timers: BinaryHeap<Timeout<T>>,
+pub struct Timer<T> {
+    now: Instant,
+    tick: Duration,
+    pending: BinaryHeap<TimeoutTask<T>>,
+    delivered: Vec<TimeoutTask<T>>,
 }
 
-impl<T: Copy> Timer<T> {
-    pub fn new() -> Self {
-        Timer {
-            timers: BinaryHeap::new(),
+impl<T> Timer<T> {
+    fn align_instant(&self, ins: Instant) -> Instant {
+        let mut next = self.now + self.tick;
+        loop {
+            if next < ins {
+                next += self.tick;
+                continue;
+            }
+            break next;
         }
     }
 
-    pub fn add_timeout(&mut self, timeout: Duration, task: T) {
-        let timeout = Timeout {
-            tick: Instant::now() + timeout,
+    /// Create a new timer with a specified `tick` which means
+    /// the min timeout when the `Timer` calls `next_timeout`.
+    pub fn new(tick: Duration) -> Self {
+        Timer {
+            now: Instant::now_coarse(),
+            tick: tick,
+            pending: BinaryHeap::new(),
+            delivered: Vec::new(),
+        }
+    }
+
+    /// Add a periodic task into the `Timer`. The `timeout` will
+    /// be aligned up to `tick` of the `Timer` if need.
+    pub fn add_task(&mut self, mut timeout: Duration, task: T) {
+        timeout = max(timeout, self.tick);
+        let task = TimeoutTask {
+            next_tick: self.now + timeout,
             timeout: timeout,
             task: task,
         };
-        self.timers.push(timeout);
+        self.pending.push(task);
     }
 
-    pub fn next_timeout<F>(&mut self, mut keep_tick: F) -> Option<T>
-    where
-        F: FnMut(&T) -> bool,
-    {
-        let now = Instant::now();
-        let could_pop = match self.timers.peek() {
-            Some(t) => t.tick <= now,
-            None => return None,
-        };
-        if could_pop {
-            let mut timeout = self.timers.pop().unwrap();
-            let task = timeout.task;
-            if keep_tick(&task) {
-                while timeout.tick <= now {
-                    timeout.tick += timeout.timeout;
+    /// Get the next `timeout` from the timer. After it's called and
+    /// the `timeout` elapsed, we should call `run_delivered_tasks` to
+    /// consume triggered tasks.
+    pub fn next_timeout(&mut self) -> Option<Duration> {
+        assert!(self.delivered.is_empty());
+        if let Some(timeout_task) = self.pending.pop() {
+            let next_tick = self.align_instant(timeout_task.next_tick);
+            self.delivered.push(timeout_task);
+            while let Some(timeout_task) = self.pending.pop() {
+                if timeout_task.next_tick > next_tick {
+                    break;
                 }
-                self.timers.push(timeout);
+                self.delivered.push(timeout_task);
             }
-            return Some(task);
+            return Some(next_tick - Instant::now_coarse());
         }
         None
+    }
+
+    /// Run all delivered tasks. if the `run` returns true,
+    /// the task will be put into the `Timer` again.
+    pub fn run_delivered_tasks<F: FnMut(&mut T) -> bool>(&mut self, run: &mut F) {
+        for mut timeout_task in self.delivered.drain(..) {
+            if run(&mut timeout_task.task) {
+                timeout_task.next_tick += timeout_task.timeout;
+                self.pending.push(timeout_task);
+            }
+        }
     }
 }
