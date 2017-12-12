53bc4d9d6d515741391d64794b2d035184b991e5
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2017-12-12 17:08:53 +0800

    only support linux

--- src/util/sys/mod.rs
+++ src/util/sys/mod.rs
@@ -1,3 +1,4 @@
+#[cfg(target_os = "linux")]
 use libc;
 
 pub enum AdjustPriType {
@@ -6,16 +7,18 @@ pub enum AdjustPriType {
     Desc,
 }
 
+#[cfg(target_os = "linux")]
 pub fn adjust_priority(t: AdjustPriType) {
-    if cfg!(target_os = "unix") {
-        unsafe {
-            let pid = libc::pthread_self();
-            let pri = match t {
-                AdjustPriType::Incr => -1,
-                AdjustPriType::Normal => 0,
-                AdjustPriType::Desc => 1,
-            };
-            libc::setpriority(libc::PRIO_PROCESS as u32, pid as u32, pri);
-        }
+    unsafe {
+        let pid = libc::pthread_self();
+        let pri = match t {
+            AdjustPriType::Incr => -1,
+            AdjustPriType::Normal => 0,
+            AdjustPriType::Desc => 1,
+        };
+        libc::setpriority(libc::PRIO_PROCESS as u32, pid as u32, pri);
     }
 }
+
+#[cfg(not(target_os = "linux"))]
+pub fn adjust_priority(_: AdjustPriType) {}
