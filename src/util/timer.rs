363a69cd341818ae59df6d072987b3889d839dbc
qupeng <onlyqupeng@gmail.com>
2017-12-12 21:09:41 +0800

    fix

--- src/util/timer.rs
+++ src/util/timer.rs
@@ -11,15 +11,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::cmp::{max, Ordering};
+use std::cmp::Ordering;
 use std::time::Duration;
 use util::time::Instant;
 use std::collections::BinaryHeap;
 
-struct TimeoutTask<T> {
+pub struct TimeoutTask<T> {
     next_tick: Instant,
     timeout: Duration,
     task: T,
+}
+
+impl<T> TimeoutTask<T> {
+    pub fn restore(self, timer: &mut Timer<T>) {
+        timer.pending.push(self);
+    }
+}
+
+impl<T> AsRef<T> for TimeoutTask<T> {
+    fn as_ref(&self) -> &T {
+        &self.task
+    }
+}
+
+impl<T> AsMut<T> for TimeoutTask<T> {
+    fn as_mut(&mut self) -> &mut T {
+        &mut self.task
+    }
 }
 
 impl<T> PartialEq for TimeoutTask<T> {
@@ -55,19 +73,24 @@ pub struct Timer<T> {
     now: Instant,
     tick: Duration,
     pending: BinaryHeap<TimeoutTask<T>>,
-    delivered: Vec<TimeoutTask<T>>,
 }
 
 impl<T> Timer<T> {
-    fn align_instant(&self, ins: Instant) -> Instant {
+    fn align_up_instant(&self, ins: Instant) -> Instant {
         let mut next = self.now + self.tick;
+        let now = Instant::now_coarse();
         loop {
-            if next < ins {
+            if next < ins || next < now {
                 next += self.tick;
                 continue;
             }
             break next;
         }
+    }
+
+    fn deliver(&self, mut task: TimeoutTask<T>, buffer: &mut Vec<TimeoutTask<T>>) {
+        task.next_tick = self.align_up_instant(task.next_tick + task.timeout);
+        buffer.push(task);
     }
 
     /// Create a new timer with a specified `tick` which means
@@ -77,14 +100,12 @@ impl<T> Timer<T> {
             now: Instant::now_coarse(),
             tick: tick,
             pending: BinaryHeap::new(),
-            delivered: Vec::new(),
         }
     }
 
     /// Add a periodic task into the `Timer`. The `timeout` will
     /// be aligned up to `tick` of the `Timer` if need.
-    pub fn add_task(&mut self, mut timeout: Duration, task: T) {
-        timeout = max(timeout, self.tick);
+    pub fn add_task(&mut self, timeout: Duration, task: T) {
         let task = TimeoutTask {
             next_tick: self.now + timeout,
             timeout: timeout,
@@ -93,33 +114,61 @@ impl<T> Timer<T> {
         self.pending.push(task);
     }
 
-    /// Get the next `timeout` from the timer. After it's called and
-    /// the `timeout` elapsed, we should call `run_delivered_tasks` to
-    /// consume triggered tasks.
-    pub fn next_timeout(&mut self) -> Option<Duration> {
-        assert!(self.delivered.is_empty());
+    /// Get the next `timeout` from the timer, and fill `delivered`
+    /// with tasks will be triggered after `timeout` elapsed.
+    /// If there is no pending tasks, `delivered` won't be changed.
+    pub fn next_timeout(&mut self, delivered: &mut Vec<TimeoutTask<T>>) -> Option<Duration> {
         if let Some(timeout_task) = self.pending.pop() {
-            let next_tick = self.align_instant(timeout_task.next_tick);
-            self.delivered.push(timeout_task);
+            let next_tick = self.align_up_instant(timeout_task.next_tick);
+            self.deliver(timeout_task, delivered);
             while let Some(timeout_task) = self.pending.pop() {
                 if timeout_task.next_tick > next_tick {
+                    self.pending.push(timeout_task);
                     break;
                 }
-                self.delivered.push(timeout_task);
+                self.deliver(timeout_task, delivered);
             }
+            self.now = next_tick;
+            // TODO: Should use checked sub.
             return Some(next_tick - Instant::now_coarse());
         }
         None
     }
+}
 
-    /// Run all delivered tasks. if the `run` returns true,
-    /// the task will be put into the `Timer` again.
-    pub fn run_delivered_tasks<F: FnMut(&mut T) -> bool>(&mut self, run: &mut F) {
-        for mut timeout_task in self.delivered.drain(..) {
-            if run(&mut timeout_task.task) {
-                timeout_task.next_tick += timeout_task.timeout;
-                self.pending.push(timeout_task);
-            }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, PartialEq, Eq)]
+    enum Task {
+        A,
+        B,
+        C,
+    }
+
+    #[test]
+    fn test_timer() {
+        let mut timer = Timer::new(Duration::from_millis(100));
+        timer.add_task(Duration::from_millis(20), Task::A);
+        timer.add_task(Duration::from_millis(150), Task::C);
+        timer.add_task(Duration::from_millis(100), Task::B);
+        assert_eq!(timer.pending.len(), 3);
+
+        let mut events = Vec::new();
+        assert!(timer.next_timeout(&mut events).is_some());
+        assert_eq!(events.len(), 2);
+        assert_eq!(events[0].as_ref(), &Task::A);
+        assert_eq!(events[1].as_ref(), &Task::B);
+        for event in events {
+            event.restore(&mut timer);
         }
+
+        let mut events = Vec::new();
+        assert!(timer.next_timeout(&mut events).is_some());
+        assert_eq!(events.len(), 3);
+        assert_eq!(events[0].as_ref(), &Task::A);
+        assert_eq!(events[1].as_ref(), &Task::C);
+        assert_eq!(events[2].as_ref(), &Task::B);
     }
 }
