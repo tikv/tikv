cd3a29239f02b37832ad716371d89463be5c9f1d
MyonKeminta <mk@mkmkm.me>
2017-12-17 00:15:18 +0800

    Eliminated all warnings

--- benches/bin/engine.rs
+++ benches/bin/engine.rs
@@ -1,19 +1,19 @@
 extern crate rand;
 
-//use std::sync::mpsc::{channel, Sender};
+// use std::sync::mpsc::{channel, Sender};
 use tikv::storage::{new_local_engine, CFStatistics, Engine, Key, Modify, ScanMode, Snapshot,
                     Value, ALL_CFS, CF_DEFAULT, CF_LOCK, CF_WRITE, TEMP_DIR};
 use kvproto::kvrpcpb::Context;
 use tikv::raftstore::store::engine::IterOption;
-use tempdir::TempDir;
+// use tempdir::TempDir;
 
 use rand::Rng;
 use test::BenchSamples;
 use super::print_result;
 
-//use tikv::util::codec::number::NumberEncoder;
+// use tikv::util::codec::number::NumberEncoder;
 
-//use test_util::{KvGenerator, generate_random_kvs};
+// use test_util::{KvGenerator, generate_random_kvs};
 
 // TODO: Bench multithreading
 
@@ -35,8 +35,7 @@ fn next_ts() -> u64 {
 
 fn create_engine() -> Box<Engine> {
     //    let dir = TempDir::new("engine_bench").unwrap();
-    let e = new_local_engine(TEMP_DIR, ALL_CFS).unwrap();
-    e
+    new_local_engine(TEMP_DIR, ALL_CFS).unwrap()
 }
 
 #[inline]
@@ -53,9 +52,9 @@ fn create_row_key(tableid: u64, rowid: u64) -> Key {
     let mut result = to_bytes(tableid);
     result.extend(b"_r".iter());
     result.extend(to_bytes(rowid).iter());
-    let result = Key::from_raw(&result[..]);
+    Key::from_raw(&result[..])
     // result.append_ts(ts)
-    result
+    // result
 }
 
 fn generate_row_data(rows: usize, ts_count: usize, value_length: usize) -> Vec<BenchDataItem> {
@@ -74,17 +73,17 @@ fn generate_row_data(rows: usize, ts_count: usize, value_length: usize) -> Vec<B
     }
 
     for _ in 0..ts_count {
-        for i in 0..rows {
-            result[i].ts.push(next_ts());
+        for mut item in &mut result {
+            item.ts.push(next_ts());
         }
     }
 
     result
 }
 
-fn load_test_data(e: &Box<Engine>, data: &Vec<BenchDataItem>) {
+fn load_test_data(e: &Engine, data: &[BenchDataItem]) {
     for ts in 0..data[0].ts.len() {
-        for ref item in data {
+        for item in data {
             let modify_default = Modify::Put(
                 CF_DEFAULT,
                 item.key.append_ts(item.ts[ts]),
@@ -101,7 +100,7 @@ fn load_test_data(e: &Box<Engine>, data: &Vec<BenchDataItem>) {
 
 // Simulated load_lock method
 #[inline]
-fn load_lock(snapshot: &Box<Snapshot>, key: &Key, statistics: &mut CFStatistics) {
+fn load_lock(snapshot: &Snapshot, key: &Key, statistics: &mut CFStatistics) {
     let option = IterOption::new(None, None, false);
     let mut cursor = snapshot.iter_cf(CF_LOCK, option, ScanMode::Mixed).unwrap();
     cursor.get(key, statistics).unwrap();
@@ -109,7 +108,7 @@ fn load_lock(snapshot: &Box<Snapshot>, key: &Key, statistics: &mut CFStatistics)
 
 // Simulated seek_write method
 #[inline]
-fn seek_write(snapshot: &Box<Snapshot>, key: &Key, statistics: &mut CFStatistics) {
+fn seek_write(snapshot: &Snapshot, key: &Key, statistics: &mut CFStatistics) {
     let option = IterOption::default()
         .use_prefix_seek()
         .set_prefix_same_as_start(true);
@@ -118,9 +117,10 @@ fn seek_write(snapshot: &Box<Snapshot>, key: &Key, statistics: &mut CFStatistics
 }
 
 // Simulated load_data method
+#[allow(unused_variables)]
 #[inline]
 fn load_data(
-    snapshot: &Box<Snapshot>,
+    snapshot: &Snapshot,
     key: &Key,
     expected_value: &Option<&[u8]>,
     statistics: &mut CFStatistics,
@@ -133,9 +133,10 @@ fn load_data(
     //    println!("{:?}", expected_value);
 }
 
+#[allow(ptr_arg)]
 #[inline]
 fn prewrite(
-    e: &Box<Engine>,
+    e: &Engine,
     key: &Key,
     value: &Value,
     is_delete: bool,
@@ -143,8 +144,8 @@ fn prewrite(
     statistics: &mut CFStatistics,
 ) {
     let snapshot = e.snapshot(&Context::new()).unwrap();
-    seek_write(&snapshot, &key.append_ts(ts), statistics);
-    load_lock(&snapshot, &key, statistics);
+    seek_write(&*snapshot, &key.append_ts(ts), statistics);
+    load_lock(&*snapshot, key, statistics);
     // Dummy value in CF_LOCK
     let mut modifies = Vec::new();
     let lock_key = Modify::Put(CF_LOCK, key.clone(), vec![0u8; 25]);
@@ -158,9 +159,9 @@ fn prewrite(
 }
 
 #[inline]
-fn commit(e: &Box<Engine>, key: &Key, commit_ts: u64, statistics: &mut CFStatistics) {
+fn commit(e: &Engine, key: &Key, commit_ts: u64, statistics: &mut CFStatistics) {
     let snapshot = e.snapshot(&Context::new()).unwrap();
-    load_lock(&snapshot, &key, statistics);
+    load_lock(&*snapshot, key, statistics);
     // Dummy value in CF_WRITE
     let put_write = Modify::Put(CF_WRITE, key.append_ts(commit_ts), vec![0u8; 9]);
     let unlock_key = Modify::Delete(CF_LOCK, key.clone());
@@ -177,46 +178,46 @@ fn commit(e: &Box<Engine>, key: &Key, commit_ts: u64, statistics: &mut CFStatist
 //fn resolve_lock() {}
 
 
-fn bench_get(e: &Box<Engine>, data: &Vec<BenchDataItem>) -> BenchSamples {
+fn bench_get(e: &Engine, data: &[BenchDataItem]) -> BenchSamples {
     let snapshot = e.snapshot(&Context::new()).unwrap();
     let mut rng = rand::thread_rng();
     let mut fake_statistics = CFStatistics::default();
     bench!{
         let item = &data[rng.gen_range(0, data.len())];
         let ts = item.ts[rng.gen_range(0, item.ts.len())];
-        load_lock(&snapshot, &item.key, &mut fake_statistics);
-        seek_write(&snapshot, &item.key.append_ts(ts), &mut fake_statistics);
+        load_lock(&*snapshot, &item.key, &mut fake_statistics);
+        seek_write(&*snapshot, &item.key.append_ts(ts), &mut fake_statistics);
         // Not calculating ts in CF_DEFAULT but simply reuse the ts, for only benching purpose
-        load_data(&snapshot, &item.key.append_ts(ts), &Some(&item.value[..]), &mut fake_statistics)
+        load_data(&*snapshot, &item.key.append_ts(ts), &Some(&item.value[..]), &mut fake_statistics)
     }
 }
 
 // Any difference between update and insert?
-fn bench_set(e: &Box<Engine>, data: &Vec<BenchDataItem>) -> BenchSamples {
+fn bench_set(e: &Engine, data: &[BenchDataItem]) -> BenchSamples {
     //    let snapshot = e.snapshot(&Context::new()).unwrap();
     let mut rng = rand::thread_rng();
     let mut fake_statistics = CFStatistics::default();
 
     bench!{
         let item = &data[rng.gen_range(0, data.len())];
-        prewrite(&e, &item.key, &item.value, false, next_ts(), &mut fake_statistics);
-        commit(&e, &item.key, next_ts(), &mut fake_statistics)
+        prewrite(&*e, &item.key, &item.value, false, next_ts(), &mut fake_statistics);
+        commit(&*e, &item.key, next_ts(), &mut fake_statistics)
     }
 }
 
-fn bench_delete(e: &Box<Engine>, data: &Vec<BenchDataItem>) -> BenchSamples {
+fn bench_delete(e: &Engine, data: &[BenchDataItem]) -> BenchSamples {
     //    let snapshot = e.snapshot(&Context::new()).unwrap();
     let mut rng = rand::thread_rng();
     let mut fake_statistics = CFStatistics::default();
 
     bench!{
         let item = &data[rng.gen_range(0, data.len())];
-        prewrite(&e, &item.key, &Vec::new(), true, next_ts(), &mut fake_statistics);
-        commit(&e, &item.key, next_ts(), &mut fake_statistics)
+        prewrite(&*e, &item.key, &Vec::new(), true, next_ts(), &mut fake_statistics);
+        commit(&*e, &item.key, next_ts(), &mut fake_statistics)
     }
 }
 
-fn bench_single_gc(e: &Box<Engine>, data: &Vec<BenchDataItem>) -> BenchSamples {
+fn bench_single_gc(e: &Engine, data: &[BenchDataItem]) -> BenchSamples {
     let snapshot = e.snapshot(&Context::new()).unwrap();
     //    let mut rng = rand::thread_rng();
     let mut fake_statistics = CFStatistics::default();
@@ -227,13 +228,13 @@ fn bench_single_gc(e: &Box<Engine>, data: &Vec<BenchDataItem>) -> BenchSamples {
         let item = it.next().unwrap();
         // Simulating the last write is Put, so the last version is kept after gc
         let ts = u64::max_value();
-        seek_write(&snapshot, &item.key.append_ts(ts), &mut fake_statistics);
+        seek_write(&*snapshot, &item.key.append_ts(ts), &mut fake_statistics);
 
         let mut modifies = Vec::new();
 
         for ts in item.ts[0..item.ts.len()-1].iter().rev() {
             let key = item.key.append_ts(*ts);
-            seek_write(&snapshot, &key, &mut fake_statistics);
+            seek_write(&*snapshot, &key, &mut fake_statistics);
             modifies.push(Modify::Delete(CF_WRITE, key.clone()));
             modifies.push(Modify::Delete(CF_DEFAULT, key));
         };
@@ -243,13 +244,13 @@ fn bench_single_gc(e: &Box<Engine>, data: &Vec<BenchDataItem>) -> BenchSamples {
 
 
 pub fn bench_engine() {
-    for table_size in [1_000, 10_000, 100_000].iter() {
-        for version_count in [1, 5, 50].iter() {
-            for value_length in [32, 128, 1024].iter() {
+    for table_size in &[1_000, 10_000, 100_000] {
+        for version_count in &[1, 5, 50] {
+            for value_length in &[32, 128, 1024] {
                 {
                     let e = create_engine();
                     let data = generate_row_data(*table_size, *version_count, *value_length);
-                    load_test_data(&e, &data);
+                    load_test_data(&*e, &data);
 
                     printf!(
                         "benching txn get on local engine\trows:{} versions:{} value len:{}\t...",
@@ -257,7 +258,7 @@ pub fn bench_engine() {
                         version_count,
                         value_length
                     );
-                    print_result(bench_get(&e, &data));
+                    print_result(bench_get(&*e, &data));
 
                     printf!(
                         "benching txn set on local engine\trows:{} versions:{} value len:{}\t...",
@@ -265,7 +266,7 @@ pub fn bench_engine() {
                         version_count,
                         value_length
                     );
-                    print_result(bench_set(&e, &data));
+                    print_result(bench_set(&*e, &data));
                 } // Dispose e and data, regenerate new ones
 
                 {
@@ -273,7 +274,7 @@ pub fn bench_engine() {
                     let e = create_engine();
                     let data = generate_row_data(*table_size, *version_count, *value_length);
 
-                    load_test_data(&e, &data);
+                    load_test_data(&*e, &data);
 
                     printf!(
                         "benching txn del on local engine\trows:{} versions:{} value len:{}\t...",
@@ -281,19 +282,19 @@ pub fn bench_engine() {
                         version_count,
                         value_length
                     );
-                    print_result(bench_delete(&e, &data));
+                    print_result(bench_delete(&*e, &data));
                 }
             }
         }
     }
 
 
-    for version_count in [1, 5, 50].iter() {
-        for value_length in [32, 128, 1024].iter() {
+    for version_count in &[1, 5, 50] {
+        for value_length in &[32, 128, 1024] {
             let e = create_engine();
             let data = generate_row_data(100_000, *version_count, *value_length);
 
-            load_test_data(&e, &data);
+            load_test_data(&*e, &data);
 
             printf!(
                 "benching gc on local engine\trows:{} versions:{} value len:{}\t...",
@@ -301,7 +302,7 @@ pub fn bench_engine() {
                 version_count,
                 value_length
             );
-            print_result(bench_single_gc(&e, &data));
+            print_result(bench_single_gc(&*e, &data));
         }
     }
 
