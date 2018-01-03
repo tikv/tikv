091581961c07d6a0b12660b834c6745286a51119
Jay Lee <busyjaylee@gmail.com>
2018-01-03 20:36:24 +0800

    add admin entry case

--- tests/raftstore_cases/test_merge.rs
+++ tests/raftstore_cases/test_merge.rs
@@ -12,16 +12,18 @@
 // limitations under the License.
 
 use std::time::Duration;
+use std::thread;
 
 use kvproto::metapb;
 use kvproto::raft_serverpb::{PeerState, RegionLocalState};
-
-use super::node::new_node_cluster;
-use super::util;
 use tikv::pd::PdClient;
 use tikv::raftstore::store::keys;
 use tikv::storage::CF_RAFT;
 use tikv::raftstore::store::Peekable;
+
+use super::node::new_node_cluster;
+use super::util;
+use raftstore::util::*;
 
 #[test]
 fn test_node_base_merge() {
@@ -81,16 +83,62 @@ fn test_node_base_merge() {
     assert!(!resp.get_header().has_error(), "{:?}", resp);
     assert_eq!(resp.get_responses()[0].get_get().get_value(), b"v1");
 
-    for i in 1..4 {
+    'outer: for i in 1..4 {
         let state_key = keys::region_state_key(left.get_id());
-        let state: RegionLocalState = cluster
-            .get_engine(i)
-            .get_msg_cf(CF_RAFT, &state_key)
-            .unwrap()
-            .unwrap();
-        assert_eq!(state.get_state(), PeerState::Tombstone);
-        assert_eq!(*state.get_region(), left);
+        let mut state = RegionLocalState::default();
+        for _ in 0..3 {
+            state = cluster
+                .get_engine(i)
+                .get_msg_cf(CF_RAFT, &state_key)
+                .unwrap()
+                .unwrap();
+            if state.get_state() == PeerState::Tombstone && *state.get_region() == left {
+                continue 'outer;
+            }
+            thread::sleep(Duration::from_millis(500));
+        }
+        panic!("store {} is still not merged: {:?}", i, state);
     }
 
     cluster.must_put(b"k4", b"v4");
+}
+
+#[test]
+fn test_node_merge_with_admin_entries() {
+    let mut cluster = new_node_cluster(0, 3);
+    let pd_client = cluster.pd_client.clone();
+    pd_client.disable_default_rule();
+
+    cluster.run_conf_change();
+
+    cluster.must_put(b"k1", b"v1");
+    cluster.must_put(b"k3", b"v3");
+
+    let region = pd_client.get_region(b"k1").unwrap();
+    cluster.must_split(&region, b"k2");
+    let left = pd_client.get_region(b"k1").unwrap();
+    let right = pd_client.get_region(b"k2").unwrap();
+
+    pd_client.must_add_peer(left.get_id(), new_peer(2, 2));
+    pd_client.must_add_peer(right.get_id(), new_peer(2, 4));
+
+    pd_client.must_merge(left.get_id(), metapb::MergeDirection::Forward);
+
+    let region = pd_client.get_region(b"k1").unwrap();
+    assert_eq!(region.get_id(), right.get_id());
+    assert_eq!(region.get_start_key(), left.get_start_key());
+    assert_eq!(region.get_end_key(), right.get_end_key());
+    let new_epoch = region.get_region_epoch();
+    let get = util::new_request(
+        region.get_id(),
+        new_epoch.to_owned(),
+        vec![util::new_get_cmd(b"k1")],
+        false,
+    );
+    debug!("requesting {:?}", get);
+    let resp = cluster
+        .call_command_on_leader(get, Duration::from_secs(5))
+        .unwrap();
+    assert!(!resp.get_header().has_error(), "{:?}", resp);
+    assert_eq!(resp.get_responses()[0].get_get().get_value(), b"v1");
 }
