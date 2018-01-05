68d05c9a4972742dab74983819595356797ad259
MyonKeminta <mk@mkmkm.me>
2018-01-05 18:38:51 +0800

    bench/mvcctxn: Remove change in src/storage and tests/
    These modifications were to make it possible to disable WAL.
    Now the Engine interface is no longer used in the benchmark code. So these modifications are not necessary now.

--- src/storage/engine/rocksdb.rs
+++ src/storage/engine/rocksdb.rs
@@ -27,7 +27,7 @@ use super::{BatchCallback, Callback, CbContext, Cursor, Engine, Error, Iterator 
 use tempdir::TempDir;
 
 enum Task {
-    Write(Vec<Modify>, Callback<()>, bool),
+    Write(Vec<Modify>, Callback<()>),
     Snapshot(Callback<Box<Snapshot>>),
     SnapshotBath(usize, BatchCallback<Box<Snapshot>>),
 }
@@ -47,10 +47,7 @@ struct Runner(Arc<DB>);
 impl Runnable<Task> for Runner {
     fn run(&mut self, t: Task) {
         match t {
-            Task::Write(modifies, cb, with_wal) => cb((
-                CbContext::new(),
-                write_modifies(&self.0, modifies, with_wal),
-            )),
+            Task::Write(modifies, cb) => cb((CbContext::new(), write_modifies(&self.0, modifies))),
             Task::Snapshot(cb) => cb((
                 CbContext::new(),
                 Ok(box RocksSnapshot::new(self.0.clone())),
@@ -87,7 +84,6 @@ impl Drop for EngineRocksdbCore {
 pub struct EngineRocksdb {
     core: Arc<Mutex<EngineRocksdbCore>>,
     sched: Scheduler<Task>,
-    with_wal: bool,
 }
 
 impl EngineRocksdb {
@@ -95,7 +91,6 @@ impl EngineRocksdb {
         path: &str,
         cfs: &[CfName],
         cfs_opts: Option<Vec<CFOptions>>,
-        with_wal: bool,
     ) -> Result<EngineRocksdb> {
         info!("EngineRocksdb: creating for path {}", path);
         let (path, temp_dir) = match path {
@@ -114,7 +109,6 @@ impl EngineRocksdb {
                 temp_dir: temp_dir,
                 worker: worker,
             })),
-            with_wal,
         })
     }
 
@@ -136,7 +130,7 @@ impl Debug for EngineRocksdb {
     }
 }
 
-fn write_modifies(db: &DB, modifies: Vec<Modify>, with_wal: bool) -> Result<()> {
+fn write_modifies(db: &DB, modifies: Vec<Modify>) -> Result<()> {
     let wb = WriteBatch::new();
     for rev in modifies {
         let res = match rev {
@@ -171,12 +165,7 @@ fn write_modifies(db: &DB, modifies: Vec<Modify>, with_wal: bool) -> Result<()> 
             return Err(Error::RocksDb(msg));
         }
     }
-    let result = if with_wal {
-        db.write(wb)
-    } else {
-        db.write_without_wal(wb)
-    };
-    if let Err(msg) = result {
+    if let Err(msg) = db.write(wb) {
         return Err(Error::RocksDb(msg));
     }
     Ok(())
@@ -184,10 +173,7 @@ fn write_modifies(db: &DB, modifies: Vec<Modify>, with_wal: bool) -> Result<()> 
 
 impl Engine for EngineRocksdb {
     fn async_write(&self, _: &Context, modifies: Vec<Modify>, cb: Callback<()>) -> Result<()> {
-        box_try!(
-            self.sched
-                .schedule(Task::Write(modifies, cb, self.with_wal))
-        );
+        box_try!(self.sched.schedule(Task::Write(modifies, cb)));
         Ok(())
     }
 
@@ -212,7 +198,6 @@ impl Engine for EngineRocksdb {
         box EngineRocksdb {
             core: self.core.clone(),
             sched: self.sched.clone(),
-            with_wal: self.with_wal,
         }
     }
 }
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -32,9 +32,9 @@ pub mod types;
 mod metrics;
 
 pub use self::config::{Config, DEFAULT_DATA_DIR, DEFAULT_ROCKSDB_SUB_DIR};
-pub use self::engine::{new_local_engine, new_local_engine_with_config, CFStatistics, Cursor,
-                       Engine, Error as EngineError, FlowStatistics, Iterator, Modify, ScanMode,
-                       Snapshot, Statistics, StatisticsSummary, TEMP_DIR};
+pub use self::engine::{new_local_engine, CFStatistics, Cursor, Engine, Error as EngineError,
+                       FlowStatistics, Iterator, Modify, ScanMode, Snapshot, Statistics,
+                       StatisticsSummary, TEMP_DIR};
 pub use self::engine::raftkv::RaftKv;
 use self::mvcc::Lock;
 pub use self::txn::{Msg, Scheduler, SnapshotStore, StoreScanner};
