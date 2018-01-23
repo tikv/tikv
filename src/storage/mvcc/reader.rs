979944247731af6cf02320fe61cda38af99ea99a
XuWentao <30992981+wentaoxu@users.noreply.github.com>
2018-01-23 09:20:55 +0800

    Limit data size for scan lock (#2664)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/storage/mvcc/reader.rs
+++ src/storage/mvcc/reader.rs
@@ -408,7 +408,7 @@ impl MvccReader {
         &mut self,
         start: Option<Key>,
         filter: F,
-        limit: Option<usize>,
+        limit: usize,
     ) -> Result<(Vec<(Key, Lock)>, Option<Key>)>
     where
         F: Fn(&Lock) -> bool,
@@ -428,10 +428,8 @@ impl MvccReader {
             let lock = Lock::parse(cursor.value())?;
             if filter(&lock) {
                 locks.push((key.clone(), lock));
-                if let Some(limit) = limit {
-                    if locks.len() >= limit {
-                        return Ok((locks, Some(key)));
-                    }
+                if limit > 0 && locks.len() >= limit {
+                    return Ok((locks, Some(key)));
                 }
             }
             cursor.next(&mut self.statistics.lock);
