72e4f5733c8e2f0dfec2dc4e42d3b19f10ee8b15
wuxuelian <AndreMouche@126.com>
2018-01-25 15:37:34 +0800

    engine/mod.rs:refactor last_iter_op from bool to enum

--- src/storage/engine/mod.rs
+++ src/storage/engine/mod.rs
@@ -298,7 +298,17 @@ pub struct Cursor {
     // the data cursor can be seen will be
     min_key: Option<Vec<u8>>,
     max_key: Option<Vec<u8>>,
-    last_op_is_seek: bool,
+    last_iter_op: IterOp,
+}
+
+enum IterOp {
+    Null,
+    Seek,
+    SeekForPrev,
+    SeekToFirst,
+    SeekToLast,
+    Next,
+    Prev,
 }
 
 impl Cursor {
@@ -308,7 +318,7 @@ impl Cursor {
             scan_mode: mode,
             min_key: None,
             max_key: None,
-            last_op_is_seek: false,
+            last_iter_op: IterOp::Null,
         }
     }
 
@@ -326,7 +336,7 @@ impl Cursor {
         }
 
         statistics.seek += 1;
-        self.last_op_is_seek = true;
+        self.last_iter_op = IterOp::Seek;
         if !self.iter.seek(key)? {
             self.max_key = Some(key.encoded().to_owned());
             return Ok(false);
@@ -419,7 +429,10 @@ impl Cursor {
 
     /// Whether it used iter.seek or iter.seek_prev last time.
     pub fn last_op_is_seek(&self) -> bool {
-        self.last_op_is_seek
+        match self.last_iter_op {
+            IterOp::Seek | IterOp::SeekForPrev | IterOp::SeekToFirst | IterOp::SeekToLast => true,
+            _ => false,
+        }
     }
 
     fn seek_for_prev(&mut self, key: &Key, statistics: &mut CFStatistics) -> Result<bool> {
@@ -436,7 +449,7 @@ impl Cursor {
         }
 
         statistics.seek_for_prev += 1;
-        self.last_op_is_seek = true;
+        self.last_iter_op = IterOp::SeekForPrev;
         if !self.iter.seek_for_prev(key)? {
             self.min_key = Some(key.encoded().to_owned());
             return Ok(false);
@@ -533,28 +546,28 @@ impl Cursor {
     #[inline]
     pub fn seek_to_first(&mut self, statistics: &mut CFStatistics) -> bool {
         statistics.seek += 1;
-        self.last_op_is_seek = true;
+        self.last_iter_op = IterOp::SeekToFirst;
         self.iter.seek_to_first()
     }
 
     #[inline]
     pub fn seek_to_last(&mut self, statistics: &mut CFStatistics) -> bool {
         statistics.seek += 1;
-        self.last_op_is_seek = true;
+        self.last_iter_op = IterOp::SeekToLast;
         self.iter.seek_to_last()
     }
 
     #[inline]
     pub fn next(&mut self, statistics: &mut CFStatistics) -> bool {
         statistics.next += 1;
-        self.last_op_is_seek = false;
+        self.last_iter_op = IterOp::Next;
         self.iter.next()
     }
 
     #[inline]
     pub fn prev(&mut self, statistics: &mut CFStatistics) -> bool {
         statistics.prev += 1;
-        self.last_op_is_seek = false;
+        self.last_iter_op = IterOp::Prev;
         self.iter.prev()
     }
 
