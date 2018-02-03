39497331e46654aeea90025c4ce0da0f4712aece
Huachao Huang <huachao.huang@gmail.com>
2018-02-03 10:34:46 +0800

    Fix format

--- tests/raftstore_cases/test_clear_stale_data.rs
+++ tests/raftstore_cases/test_clear_stale_data.rs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rocksdb::{Writable, DB, CompactOptions};
+use rocksdb::{CompactOptions, Writable, DB};
 
 use tikv::storage::types::Key;
 use tikv::raftstore::store::keys;
