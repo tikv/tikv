0ccde012eebf9379e9332325ac30905b4d8e8d4f
shirly <AndreMouche@126.com>
2018-02-08 21:42:52 +0800

    address comments

--- src/coprocessor/codec/table.rs
+++ src/coprocessor/codec/table.rs
@@ -70,7 +70,7 @@ pub fn check_table_ranges(ranges: &[KeyRange]) -> Result<()> {
     for range in ranges {
         extract_table_prefix(range.get_start())?;
         extract_table_prefix(range.get_end())?;
-        if range.get_start() > range.get_end() {
+        if range.get_start() >= range.get_end() {
             return Err(invalid_type!(
                 "invalid range,range.start should be smaller than range.end, but got [{:?},{:?})",
                 range.get_start(),
@@ -622,15 +622,15 @@ mod test {
     #[test]
     fn test_check_table_range() {
         let small_key = b"t\x80\x00\x00\x00\x00\x00\x00\x01a".to_vec();
-        let big_key = b"t\x80\x00\x00\x00\x00\x00\x00\x01b".to_vec();
+        let large_key = b"t\x80\x00\x00\x00\x00\x00\x00\x01b".to_vec();
         let mut range = KeyRange::new();
         range.set_start(small_key.clone());
-        range.set_end(big_key.clone());
+        range.set_end(large_key.clone());
         assert!(check_table_ranges(&[range]).is_ok());
         //test range.start > range.end
         let mut range = KeyRange::new();
         range.set_end(small_key.clone());
-        range.set_start(big_key);
+        range.set_start(large_key);
         assert!(check_table_ranges(&[range]).is_err());
 
         // test invalid end
