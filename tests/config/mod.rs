5012eb97b7235b9840f861402167633ceaea7152
qupeng <onlyqupeng@gmail.com>
2018-02-09 17:18:38 +0800

    raftstore: limit max snapshot total size. (#2632)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- tests/config/mod.rs
+++ tests/config/mod.rs
@@ -72,6 +72,7 @@ fn test_serde_custom_tikv_config() {
         end_point_recursion_limit: 100,
         end_point_batch_row_limit: 64,
         snap_max_write_bytes_per_sec: ReadableSize::mb(10),
+        snap_max_total_size: ReadableSize::gb(10),
     };
     value.readpool = ReadPoolConfig {
         high_concurrency: 1,
