07d446df795bf8d673a55f8fa9da14919e7a1d25
Neil Shen <overvenus@gmail.com>
2018-03-08 12:15:53 +0800

    pd: cancel unused region heartbeat rpc (#2784) (#2798)

--- src/pd/util.rs
+++ src/pd/util.rs
@@ -65,16 +65,15 @@ impl Stream for HeartbeatReceiver {
                 }
             }
 
-            warn!("heartbeat receiver is stale, refreshing..");
             self.receiver.take();
 
             let mut inner = self.inner.wl();
             let mut receiver = None;
             if let Either::Left(ref mut recv) = inner.hb_receiver {
                 receiver = recv.take();
-                info!("heartbeat receiver is refreshed.");
             }
             if receiver.is_some() {
+                info!("heartbeat receiver is refreshed.");
                 self.receiver = receiver;
             } else {
                 inner.hb_receiver = Either::Right(task::current());
@@ -168,6 +167,13 @@ impl LeaderClient {
         {
             let mut inner = self.inner.wl();
             let (tx, rx) = client.region_heartbeat();
+            warn!("heartbeat sender and receiver are stale, refreshing..");
+
+            // Try to cancel an unused heartbeat sender.
+            if let Either::Left(Some(ref mut r)) = inner.hb_sender {
+                info!("cancel region heartbeat sender");
+                r.cancel();
+            }
             inner.hb_sender = Either::Left(Some(tx));
             if let Either::Right(ref mut task) = inner.hb_receiver {
                 task.notify();
@@ -363,7 +369,7 @@ pub fn validate_endpoints(
 }
 
 fn connect(env: Arc<Environment>, addr: &str) -> Result<(PdClient, GetMembersResponse)> {
-    debug!("connect to PD endpoint: {:?}", addr);
+    info!("connect to PD endpoint: {:?}", addr);
     let addr = addr.trim_left_matches("http://");
     let channel = ChannelBuilder::new(env).connect(addr);
     let client = PdClient::new(channel);
