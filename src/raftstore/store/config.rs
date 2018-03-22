bad929d48f97db764c92e0d8172d87cfad19d5cd
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2018-03-22 22:37:31 +0800

    use delete range by default (#2806)

--- src/raftstore/store/config.rs
+++ src/raftstore/store/config.rs
@@ -157,7 +157,7 @@ impl Default for Config {
             allow_remove_leader: false,
             merge_max_log_gap: 10,
             merge_check_tick_interval: ReadableDuration::secs(10),
-            use_delete_range: false,
+            use_delete_range: true,
 
             // They are preserved for compatibility check.
             region_max_size: ReadableSize(0),
