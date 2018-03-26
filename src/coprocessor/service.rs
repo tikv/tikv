bbe8fd34e24bfc4da325900362a6bf6047a0c47a
Breezewish <me@breeswish.org>
2018-03-26 14:39:48 +0800

    fix the bug that handler is not running on CpuPool and add some tests

--- src/coprocessor/service.rs
+++ src/coprocessor/service.rs
@@ -12,6 +12,7 @@
 // limitations under the License.
 
 use futures::{future, stream, Future, Stream};
+use futures::sync::mpsc;
 use protobuf::{CodedInputStream, Message};
 
 use kvproto::{coprocessor as coppb, errorpb, kvrpcpb};
@@ -159,83 +160,109 @@ impl Service {
             .map_err(Error::from)
     }
 
-    fn handle_request(
+    fn run_handler(
+        handler: Box<RequestHandler>,
+        is_streaming: bool,
+    ) -> impl Stream<Item = coppb::Response, Error = Error> {
+        stream::unfold((handler, false), move |(mut handler, finished)| {
+            if finished {
+                return None;
+            }
+            if is_streaming {
+                match handler.handle_streaming_request() {
+                    Ok((None, _)) => None,
+                    Ok((Some(resp), finished)) => {
+                        let yielded = resp;
+                        let next_state = (handler, finished);
+                        Some(Ok((yielded, next_state)))
+                    }
+                    Err(e) => Some(Err(e)),
+                }
+            } else {
+                match handler.handle_request() {
+                    Ok(resp) => {
+                        let yielded = resp;
+                        let next_state = (handler, true);
+                        Some(Ok((yielded, next_state)))
+                    }
+                    Err(e) => Some(Err(e)),
+                }
+            }
+        })
+    }
+
+    fn handle_request_by_custom_handler(
         &self,
-        req: coppb::Request,
+        context: kvrpcpb::Context,
+        handler_builder: RequestHandlerBuilder,
         is_streaming: bool,
     ) -> impl Stream<Item = coppb::Response, Error = ()> {
-        let request_handler_builder = self.new_request_handler_builder(&req, is_streaming);
+        let (tx, rx) = mpsc::channel::<Result<coppb::Response>>(8);
         let engine = self.engine.clone();
-        let priority = readpool::Priority::from(req.get_context().get_priority());
+        let priority = readpool::Priority::from(context.get_priority());
+
+        let (tx1, tx2) = (tx.clone(), tx.clone());
         let result = self.read_pool.future_execute(priority, move |_ctxd| {
-            Service::async_snapshot(engine, req.get_context()).and_then(move |snapshot| {
-                let handler = match request_handler_builder(snapshot) {
-                    Ok(handler) => handler,
-                    Err(e) => cop_util::ErrorRequestHandler::new(e).into_boxed(),
-                };
-                handler.check_if_outdated()?;
-                let resp_stream =
-                    stream::unfold((handler, false), move |(mut handler, finished)| {
-                        if finished {
-                            return None;
-                        }
-                        if is_streaming {
-                            match handler.handle_streaming_request() {
-                                Ok((None, _)) => None,
-                                Ok((Some(resp), finished)) => {
-                                    let yielded = resp;
-                                    let next_state = (handler, finished);
-                                    Some(Ok((yielded, next_state)))
-                                }
-                                Err(e) => Some(Err(e)),
-                            }
-                        } else {
-                            match handler.handle_request() {
-                                Ok(resp) => {
-                                    let yielded = resp;
-                                    let next_state = (handler, true);
-                                    Some(Ok((yielded, next_state)))
-                                }
-                                Err(e) => Some(Err(e)),
-                            }
-                        }
-                    });
-                Ok(resp_stream)
-            })
+            Service::async_snapshot(engine, &context)
+                .and_then(move |snapshot| {
+                    let handler = match handler_builder(snapshot) {
+                        Ok(handler) => handler,
+                        Err(e) => cop_util::ErrorRequestHandler::new(e).into_boxed(),
+                    };
+                    future::result(handler.check_if_outdated()).and_then(move |_| {
+                        Service::run_handler(handler, is_streaming)
+                            .then(Ok::<_, mpsc::SendError<_>>)
+                            .forward(tx1)
+                            .map_err(|_| Error::Other(box_err!("sink send failed")))
+                            .map(|_| ())
+                    })
+                })
+                .map_err(move |e| {
+                    stream::once::<_, mpsc::SendError<_>>(Ok(Err(e)))
+                        .forward(tx2)
+                        .wait()
+                        .unwrap();
+                })
         });
-        let future_of_stream = future::result(result).map_err(|_| Error::Full).flatten();
-        stream::unfold(Some(future_of_stream), |some_future_of_stream| {
-            // TODO: Can be simplified using `stream::once(future_of_stream)` in futures 0.2.
-            match some_future_of_stream {
-                None => None,
-                Some(future_of_stream) => Some(future_of_stream.map(|stream| (stream, None))),
+
+        match result {
+            Err(_) => {
+                stream::once::<_, mpsc::SendError<_>>(Ok(Err(Error::Full)))
+                    .forward(tx)
+                    .wait()
+                    .unwrap();
             }
-        }).flatten()
-            .then(|result| {
-                // TODO: Can be simplified using `recover()` in futures 0.2.
-                let resp = match result {
-                    Ok(resp) => resp,
-                    Err(e) => {
-                        let mut metrics = BasicLocalMetrics::default();
-                        make_error_response(e, &mut metrics)
-                    }
-                };
-                Ok::<_, ()>(resp)
-            })
+            Ok(cpu_future) => {
+                // keep running on the FuturePool
+                cpu_future.forget();
+            }
+        }
+
+        rx.map(|result| match result {
+            Ok(resp) => resp,
+            Err(e) => {
+                let mut metrics = BasicLocalMetrics::default();
+                make_error_response(e, &mut metrics)
+            }
+        })
     }
 
-    pub fn handle_stream_request(
+    /// Specifying custom request handler is useful in tests.
+    fn handle_stream_request_by_custom_handler(
         &self,
-        req: coppb::Request,
+        context: kvrpcpb::Context,
+        handler_builder: RequestHandlerBuilder,
     ) -> impl Stream<Item = coppb::Response, Error = ()> {
-        self.handle_request(req, true)
+        self.handle_request_by_custom_handler(context, handler_builder, true)
     }
 
-    pub fn handle_unary_request(
+    /// Specifying custom request handler is useful in tests.
+    fn handle_unary_request_by_custom_handler(
         &self,
-        req: coppb::Request,
+        context: kvrpcpb::Context,
+        handler_builder: RequestHandlerBuilder,
     ) -> impl Future<Item = coppb::Response, Error = ()> {
-        self.handle_request(req, false)
+        self.handle_request_by_custom_handler(context, handler_builder, false)
             .take(1)
             .fold(None, |_, resp| Ok::<_, ()>(Some(resp)))
             .then(|result| {
@@ -246,6 +273,22 @@ impl Service {
                 };
                 Ok::<_, ()>(resp)
             })
+    }
+
+    pub fn handle_stream_request(
+        &self,
+        mut req: coppb::Request,
+    ) -> impl Stream<Item = coppb::Response, Error = ()> {
+        let handler_builder = self.new_request_handler_builder(&req, true);
+        self.handle_stream_request_by_custom_handler(req.take_context(), handler_builder)
+    }
+
+    pub fn handle_unary_request(
+        &self,
+        mut req: coppb::Request,
+    ) -> impl Future<Item = coppb::Response, Error = ()> {
+        let handler_builder = self.new_request_handler_builder(&req, false);
+        self.handle_unary_request_by_custom_handler(req.take_context(), handler_builder)
     }
 }
 
@@ -284,4 +327,199 @@ fn make_error_response(e: Error, metrics: &mut BasicLocalMetrics) -> coppb::Resp
     };
     metrics.error_cnt.with_label_values(&[tag]).inc();
     resp
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::sync::mpsc;
+    use std::thread;
+
+    use storage::engine::{self, TEMP_DIR};
+
+    struct DummyReqHandler {
+        is_outdated: bool,
+        delay_millis: u64,
+        unary_result: Option<Result<coppb::Response>>,
+    }
+
+    impl DummyReqHandler {
+        pub fn new_outdated() -> DummyReqHandler {
+            DummyReqHandler {
+                is_outdated: true,
+                delay_millis: 0,
+                unary_result: None,
+            }
+        }
+
+        pub fn new_unary_fixture(result: Result<coppb::Response>) -> DummyReqHandler {
+            DummyReqHandler {
+                is_outdated: false,
+                delay_millis: 0,
+                unary_result: Some(result),
+            }
+        }
+
+        pub fn new_delayed_unary_fixture(
+            result: Result<coppb::Response>,
+            delay_millis: u64,
+        ) -> DummyReqHandler {
+            DummyReqHandler {
+                is_outdated: false,
+                delay_millis,
+                unary_result: Some(result),
+            }
+        }
+    }
+
+    impl RequestHandler for DummyReqHandler {
+        fn handle_request(&mut self) -> Result<coppb::Response> {
+            if self.is_outdated {
+                unreachable!();
+            }
+            if self.delay_millis > 0 {
+                thread::sleep(Duration::from_millis(self.delay_millis))
+            }
+            self.unary_result.take().unwrap()
+        }
+
+        fn check_if_outdated(&self) -> Result<()> {
+            if self.is_outdated {
+                Err(Error::Outdated(Duration::from_secs(1), "select"))
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    #[test]
+    fn test_outdated_request() {
+        let engine = engine::new_local_engine(TEMP_DIR, &[]).unwrap();
+        let read_pool = ReadPool::new("readpool", &readpool::Config::default_for_test(), || {
+            || ReadPoolContext::new(None)
+        });
+        let service = Service::new(&Config::default(), engine, read_pool);
+
+        // a normal request
+        let handler_builder =
+            box |_| Ok(DummyReqHandler::new_unary_fixture(Ok(coppb::Response::new())).into_boxed());
+        let resp = service
+            .handle_unary_request_by_custom_handler(kvrpcpb::Context::new(), handler_builder)
+            .wait()
+            .unwrap();
+        assert!(resp.get_other_error().is_empty());
+
+        // an outdated request
+        let handler_builder = box |_| Ok(DummyReqHandler::new_outdated().into_boxed());
+        let resp = service
+            .handle_unary_request_by_custom_handler(kvrpcpb::Context::new(), handler_builder)
+            .wait()
+            .unwrap();
+        assert_eq!(resp.get_other_error(), OUTDATED_ERROR_MSG);
+    }
+
+    #[test]
+    fn test_invalid_req_type() {
+        let engine = engine::new_local_engine(TEMP_DIR, &[]).unwrap();
+        let read_pool = ReadPool::new("readpool", &readpool::Config::default_for_test(), || {
+            || ReadPoolContext::new(None)
+        });
+        let service = Service::new(&Config::default(), engine, read_pool);
+
+        let mut req = coppb::Request::new();
+        req.set_tp(9999);
+
+        let resp: coppb::Response = service.handle_unary_request(req).wait().unwrap();
+        assert!(!resp.get_other_error().is_empty());
+    }
+
+    #[test]
+    fn test_invalid_req_body() {
+        let engine = engine::new_local_engine(TEMP_DIR, &[]).unwrap();
+        let read_pool = ReadPool::new("readpool", &readpool::Config::default_for_test(), || {
+            || ReadPoolContext::new(None)
+        });
+        let service = Service::new(&Config::default(), engine, read_pool);
+
+        let mut req = coppb::Request::new();
+        req.set_tp(REQ_TYPE_DAG);
+        req.set_data(vec![1, 2, 3]);
+
+        let resp = service.handle_unary_request(req).wait().unwrap();
+        assert!(!resp.get_other_error().is_empty());
+    }
+
+    #[test]
+    fn test_full() {
+        let engine = engine::new_local_engine(TEMP_DIR, &[]).unwrap();
+        let read_pool = ReadPool::new(
+            "readpool",
+            &readpool::Config {
+                normal_concurrency: 1,
+                max_tasks_normal: 2,
+                ..readpool::Config::default_for_test()
+            },
+            || || ReadPoolContext::new(None),
+        );
+        let service = Service::new(&Config::default(), engine, read_pool);
+
+        let (tx, rx) = mpsc::channel();
+
+        // first 2 requests are processed as normal and laters are returned as errors
+        for i in 0..5 {
+            let mut response = coppb::Response::new();
+            response.set_data(vec![1, 2, i]);
+
+            let mut context = kvrpcpb::Context::new();
+            context.set_priority(kvrpcpb::CommandPri::Normal);
+
+            let handler_builder = box |_| {
+                Ok(DummyReqHandler::new_delayed_unary_fixture(Ok(response), 1000).into_boxed())
+            };
+            let future = service.handle_unary_request_by_custom_handler(context, handler_builder);
+            let tx = tx.clone();
+            thread::spawn(move || tx.send(future.wait().unwrap()));
+            thread::sleep(Duration::from_millis(100));
+        }
+
+        // verify
+        for _ in 2..5 {
+            let resp: coppb::Response = rx.recv().unwrap();
+            assert_eq!(resp.get_data().len(), 0);
+            assert!(resp.has_region_error());
+            assert!(resp.get_region_error().has_server_is_busy());
+            assert_eq!(
+                resp.get_region_error().get_server_is_busy().get_reason(),
+                COPROCESSOR_BUSY_ERROR_MSG
+            );
+        }
+        for i in 0..2 {
+            let resp = rx.recv().unwrap();
+            assert_eq!(resp.get_data(), [1, 2, i]);
+            assert!(!resp.has_region_error());
+        }
+    }
+
+    #[test]
+    fn test_error_unary_response() {
+        let engine = engine::new_local_engine(TEMP_DIR, &[]).unwrap();
+        let read_pool = ReadPool::new("readpool", &readpool::Config::default_for_test(), || {
+            || ReadPoolContext::new(None)
+        });
+        let service = Service::new(&Config::default(), engine, read_pool);
+
+        let handler_builder = box |_| {
+            Ok(
+                DummyReqHandler::new_unary_fixture(Err(Error::Other(box_err!("foobar"))))
+                    .into_boxed(),
+            )
+        };
+        let resp = service
+            .handle_unary_request_by_custom_handler(kvrpcpb::Context::new(), handler_builder)
+            .wait()
+            .unwrap();
+        assert_eq!(resp.get_data().len(), 0);
+        assert!(!resp.get_other_error().is_empty());
+    }
 }
