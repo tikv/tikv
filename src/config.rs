203bb01d1da9cb4554466355b8bed741de39aeb1
UncP <uncp@pingcap.com>
2018-04-24 10:16:08 +0800

    use DeleteFilesInRange to delete region (#2736)
    * use DeleteFilesInRange to destroy region

--- src/config.rs
+++ src/config.rs
@@ -877,6 +877,11 @@ impl TiKvConfig {
             );
             self.readpool.coprocessor.stack_size = self.server.end_point_stack_size.unwrap();
         }
+        if self.raft_store.clean_stale_peer_delay.as_secs() > 0 {
+            let delay_secs = self.raft_store.clean_stale_peer_delay.as_secs()
+                + self.server.end_point_request_max_handle_duration.as_secs();
+            self.raft_store.clean_stale_peer_delay = ReadableDuration::secs(delay_secs);
+        }
     }
 
     pub fn tune_for_import_mode(&mut self) {
