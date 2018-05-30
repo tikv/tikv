be7759b0cef348858bbad35a4a5d3f6c3875446a
Shirly <AndreMouche@126.com>
2018-05-30 16:13:20 +0800

    codec/number: remove trait NumberDecoder (#3108)

--- src/coprocessor/dag/expr/mod.rs
+++ src/coprocessor/dag/expr/mod.rs
@@ -35,7 +35,7 @@ use tipb::expression::{Expr, ExprType, FieldType, ScalarFuncSig};
 use coprocessor::codec::mysql::{Decimal, Duration, Json, Time, MAX_FSP};
 use coprocessor::codec::mysql::{charset, types};
 use coprocessor::codec::{self, Datum};
-use util::codec::number::{self, NumberDecoder};
+use util::codec::number;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Expression {
@@ -221,13 +221,11 @@ impl Expression {
         let tp = expr.take_field_type();
         match expr.get_tp() {
             ExprType::Null => Ok(Expression::new_const(Datum::Null, tp)),
-            ExprType::Int64 => expr.get_val()
-                .decode_i64()
+            ExprType::Int64 => number::decode_i64(&mut expr.get_val())
                 .map(Datum::I64)
                 .map(|e| Expression::new_const(e, tp))
                 .map_err(Error::from),
-            ExprType::Uint64 => expr.get_val()
-                .decode_u64()
+            ExprType::Uint64 => number::decode_u64(&mut expr.get_val())
                 .map(Datum::U64)
                 .map(|e| Expression::new_const(e, tp))
                 .map_err(Error::from),
@@ -238,8 +236,7 @@ impl Expression {
                 .map(Datum::F64)
                 .map(|e| Expression::new_const(e, tp))
                 .map_err(Error::from),
-            ExprType::MysqlTime => expr.get_val()
-                .decode_u64()
+            ExprType::MysqlTime => number::decode_u64(&mut expr.get_val())
                 .and_then(|i| {
                     let fsp = tp.get_decimal() as i8;
                     let t = tp.get_tp() as u8;
@@ -247,8 +244,7 @@ impl Expression {
                 })
                 .map(|t| Expression::new_const(Datum::Time(t), tp))
                 .map_err(Error::from),
-            ExprType::MysqlDuration => expr.get_val()
-                .decode_i64()
+            ExprType::MysqlDuration => number::decode_i64(&mut expr.get_val())
                 .and_then(|n| Duration::from_nanos(n, MAX_FSP))
                 .map(Datum::Dur)
                 .map(|e| Expression::new_const(e, tp))
@@ -276,7 +272,7 @@ impl Expression {
                     })
             }
             ExprType::ColumnRef => {
-                let offset = expr.get_val().decode_i64().map_err(Error::from)? as usize;
+                let offset = number::decode_i64(&mut expr.get_val()).map_err(Error::from)? as usize;
                 let column = Column { offset, tp };
                 Ok(Expression::ColumnRef(column))
             }
