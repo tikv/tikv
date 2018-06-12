43b29e9688c8c218d387e14e566b61c50b06af31
siddontang <siddontang@gmail.com>
2018-06-12 14:16:35 +0800

    *: remove unnecessary configs

--- tests/config/mod.rs
+++ tests/config/mod.rs
@@ -60,8 +60,6 @@ fn test_serde_custom_tikv_config() {
         addr: "example.com:443".to_owned(),
         labels: map!{ "a".to_owned() => "b".to_owned() },
         advertise_addr: "example.com:443".to_owned(),
-        notify_capacity: 12_345,
-        messages_per_tick: 123,
         concurrent_send_snap_limit: 4,
         concurrent_recv_snap_limit: 4,
         grpc_compression_type: GrpcCompressionType::Gzip,
