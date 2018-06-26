def42eb81ab853acaec09f470603b3a80ed3c460
Hoverbear <andrew@hoverbear.org>
2018-06-25 21:01:42 -0700

    tests: Add additional prevote test

--- tests/raftstore_cases/test_prevote.rs
+++ tests/raftstore_cases/test_prevote.rs
@@ -97,3 +97,33 @@ fn test_server_no_prevote() {
     let mut cluster = new_server_cluster(0, 3);
     test_prevote(&mut cluster, false);
 }
+
+// Test isolating a minority of the cluster and make sure that the remove themselves.
+fn test_pair_isolated<T: Simulator>(cluster: &mut Cluster<T>) {
+    let region = 1;
+    let pd_client = Arc::clone(&cluster.pd_client);
+
+    // We must start the cluster before adding send filters, otherwise it panics.
+    cluster.run();
+
+    cluster.must_transfer_leader(region, new_peer(1, 1));
+
+    cluster.must_put(b"k1", b"v1");
+
+    // Split the nodes and ensure they will eventually remove themselves if isolated.
+    cluster.partition(vec![1, 2, 3], vec![4, 5]);
+
+    // Verify that pd has removed them.
+    pd_client.must_remove_peer(region, new_peer(4, 4));
+    pd_client.must_remove_peer(region, new_peer(5, 5));
+
+    // Verify the nodes have self removed.
+    cluster.must_remove_region(4, region);
+    cluster.must_remove_region(5, region);
+}
+
+#[test]
+fn test_server_pair_isolated() {
+    let mut cluster = new_server_cluster(0, 5);
+    test_pair_isolated(&mut cluster);
+}
