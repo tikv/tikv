48a4345d37b4e20d6fc85d85445ceae501cdf5b2
Breezewish <me@breeswish.org>
2018-06-27 18:01:36 +0800

    address comments

--- src/storage/engine/perf_context.rs
+++ src/storage/engine/perf_context.rs
@@ -11,86 +11,53 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::fmt;
-use std::marker::PhantomData;
-
 use rocksdb::PerfContext;
 
-// TODO: Can be simplified by const generics once RFC #2000 is landed.
-
-#[derive(Clone, Debug, Copy)]
-pub struct TypeDelta;
-#[derive(Clone, Debug, Copy)]
-pub struct TypeInstant;
-
-pub trait Type: fmt::Debug {}
-impl Type for TypeDelta {}
-impl Type for TypeInstant {}
-
-/// Store statistics we need. Data comes from RocksDB's `PerfContext`.
-///
-/// Depends on different contexts, it may either store instant values (i.e. absolute values
-/// at a specific time) or delta values (i.e. changed values during some time).
-#[derive(Clone, Debug, Copy)]
-pub struct PerfStatistics<T: Type> {
-    _phantom: PhantomData<T>,
-    pub internal_key_skipped_count: usize,
-    pub internal_delete_skipped_count: usize,
-    pub block_cache_hit_count: usize,
-    pub block_read_count: usize,
-    pub block_read_byte: usize,
-}
-
-/// Indicate that this statistics store delta values between two instant statistics.
-pub type PerfStatisticsDelta = PerfStatistics<TypeDelta>;
-
-/// Indicate that this statistics store instant values.
-pub type PerfStatisticsInstant = PerfStatistics<TypeInstant>;
-
-impl PerfStatisticsInstant {
-    /// Create an instance which stores instant statistics values, retrieved at creation.
-    pub fn new() -> Self {
-        let perf_context = PerfContext::get();
-        Self {
-            _phantom: Default::default(),
-            internal_key_skipped_count: perf_context.internal_key_skipped_count() as usize,
-            internal_delete_skipped_count: perf_context.internal_delete_skipped_count() as usize,
-            block_cache_hit_count: perf_context.block_cache_hit_count() as usize,
-            block_read_count: perf_context.block_read_count() as usize,
-            block_read_byte: perf_context.block_read_byte() as usize,
+macro_rules! define_perf_statistics {
+    ($($field: ident),+ $(,)*) => {
+        /// Store statistics we need. Data comes from RocksDB's `PerfContext`.
+        /// This statistics store instant values.
+        #[derive(Debug, Clone, Copy)]
+        pub struct PerfStatisticsInstant {
+            $(pub $field: usize,)+
         }
-    }
 
-    /// Calculates delta values.
-    pub fn delta_between(&self, other: &Self) -> PerfStatisticsDelta {
-        PerfStatisticsDelta {
-            _phantom: Default::default(),
-            internal_key_skipped_count: other.internal_key_skipped_count
-                - self.internal_key_skipped_count,
-            internal_delete_skipped_count: other.internal_delete_skipped_count
-                - self.internal_delete_skipped_count,
-            block_cache_hit_count: other.block_cache_hit_count - self.block_cache_hit_count,
-            block_read_count: other.block_read_count - self.block_read_count,
-            block_read_byte: other.block_read_byte - self.block_read_byte,
+        impl PerfStatisticsInstant {
+            /// Create an instance which stores instant statistics values, retrieved at creation.
+            pub fn new() -> Self {
+                let perf_context = PerfContext::get();
+                Self {
+                    $($field: perf_context.$field() as usize,)+
+                }
+            }
+
+            /// Calculates delta values.
+            pub fn delta_between(&self, other: &Self) -> PerfStatisticsDelta {
+                PerfStatisticsDelta {
+                    $($field: other.$field - self.$field,)+
+                }
+            }
+
+            /// Calculate delta values until now.
+            pub fn delta(&self) -> PerfStatisticsDelta {
+                let now = Self::new();
+                self.delta_between(&now)
+            }
         }
-    }
 
-    /// Calculate delta values until now.
-    pub fn delta(&self) -> PerfStatisticsDelta {
-        let now = Self::new();
-        self.delta_between(&now)
+        /// Store statistics we need. Data comes from RocksDB's `PerfContext`.
+        /// This this statistics store delta values between two instant statistics.
+        #[derive(Default, Debug, Clone, Copy)]
+        pub struct PerfStatisticsDelta {
+            $(pub $field: usize,)+
+        }
     }
 }
 
-impl Default for PerfStatisticsDelta {
-    fn default() -> Self {
-        Self {
-            _phantom: Default::default(),
-            internal_key_skipped_count: 0,
-            internal_delete_skipped_count: 0,
-            block_cache_hit_count: 0,
-            block_read_count: 0,
-            block_read_byte: 0,
-        }
-    }
+define_perf_statistics!{
+    internal_key_skipped_count,
+    internal_delete_skipped_count,
+    block_cache_hit_count,
+    block_read_count,
+    block_read_byte,
 }
