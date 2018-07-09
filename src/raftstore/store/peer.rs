182245313b452b558d11fbaa8988ba932d1ff875
Jay Lee <busyjaylee@gmail.com>
2018-07-09 16:00:37 +0800

    fix warnings

--- src/raftstore/store/peer.rs
+++ src/raftstore/store/peer.rs
@@ -2157,7 +2157,7 @@ mod tests {
                 self.applied_to_index_term
             }
             fn inspect_lease(&mut self) -> LeaseState {
-                self.lease_state.clone()
+                self.lease_state
             }
         }
 
