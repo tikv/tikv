c611772c6f68e916064e32949eca67cdee8553db
bb7133 <bb7133@gmail.com>
2018-07-20 22:56:25 +0800

    add the original unit test case
    Signed-off-by: bb7133 <bb7133@gmail.com>

--- src/coprocessor/dag/expr/builtin_arithmetic.rs
+++ src/coprocessor/dag/expr/builtin_arithmetic.rs
@@ -792,6 +792,12 @@ mod test {
             ),
             (
                 ScalarFuncSig::ModDecimal,
+                str2dec("-33"),
+                str2dec("-11"),
+                str2dec("0"),
+            ),
+            (
+                ScalarFuncSig::ModDecimal,
                 str2dec("0.0000000001"),
                 str2dec("1.0"),
                 str2dec("0.0000000001"),
