c268b6c1d0f6518c95ecfe43504fd527a3aeaaaf
Wenxuan Shi <hi@breeswish.org>
2018-07-31 17:42:58 +0800

    Generate build info from environment variable (#3368)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/bin/util/mod.rs
+++ src/bin/util/mod.rs
@@ -3,3 +3,25 @@ pub(crate) mod profiling;
 #[macro_use]
 pub(crate) mod setup;
 pub(crate) mod signal_handler;
+
+/// Returns the tikv version information.
+pub fn tikv_version_info() -> String {
+    let fallback = "Unknown (env var does not exist when building)";
+    format!(
+        "\nRelease Version:   {}\
+         \nGit Commit Hash:   {}\
+         \nGit Commit Branch: {}\
+         \nUTC Build Time:    {}\
+         \nRust Version:      {}",
+        env!("CARGO_PKG_VERSION"),
+        option_env!("TIKV_BUILD_GIT_HASH").unwrap_or(fallback),
+        option_env!("TIKV_BUILD_GIT_BRANCH").unwrap_or(fallback),
+        option_env!("TIKV_BUILD_TIME").unwrap_or(fallback),
+        option_env!("TIKV_BUILD_RUSTC_VERSION").unwrap_or(fallback),
+    )
+}
+
+/// Prints the tikv version information to the standard output.
+pub fn print_tikv_info() {
+    info!("Welcome to TiKV. {}", tikv_version_info());
+}
