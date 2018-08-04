323c76c5329ee177ff49cb78d76caa7f9d3be73e
Wenxuan Shi <hi@breeswish.org>
2018-08-04 11:03:50 +0800

    Support specifying time zone by names (#3281)

--- src/coprocessor/dag/expr/mod.rs
+++ src/coprocessor/dag/expr/mod.rs
@@ -424,7 +424,7 @@ mod test {
 
     #[test]
     fn test_expression_eval() {
-        let mut ctx = EvalContext::new(Arc::new(EvalConfig::new(0, FLAG_IGNORE_TRUNCATE).unwrap()));
+        let mut ctx = EvalContext::new(Arc::new(EvalConfig::new(FLAG_IGNORE_TRUNCATE).unwrap()));
         let cases = vec![
             (
                 ScalarFuncSig::CastStringAsReal,
