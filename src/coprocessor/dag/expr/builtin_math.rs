a7d4e705252e210655d7858585197b973a11a8dd
shizy <shizy@cn.ibm.com>
2018-08-08 15:14:45 +0800

    scalar function RoundInt implementation

--- src/coprocessor/dag/expr/builtin_math.rs
+++ src/coprocessor/dag/expr/builtin_math.rs
@@ -109,6 +109,11 @@ impl ScalarFunc {
     }
 
     #[inline]
+    pub fn round_int(&self, ctx: &mut EvalContext, row: &[Datum]) -> Result<Option<i64>> {
+        self.children[0].eval_int(ctx, row)
+    }
+
+    #[inline]
     pub fn crc32(&self, ctx: &mut EvalContext, row: &[Datum]) -> Result<Option<i64>> {
         let d = try_opt!(self.children[0].eval_string(ctx, row));
         let mut digest = crc32::Digest::new(crc32::IEEE);
@@ -302,6 +307,34 @@ mod test {
             if sig == ScalarFuncSig::FloorIntToDec || sig == ScalarFuncSig::FloorDecToDec {
                 op.mut_tp().set_flen(convert::UNSPECIFIED_LENGTH);
                 op.mut_tp().set_decimal(convert::UNSPECIFIED_LENGTH);
+            }
+            let got = op.eval(&mut ctx, &[]).unwrap();
+            assert_eq!(got, exp);
+        }
+    }
+
+    #[test]
+    fn test_round() {
+        let tests = vec![
+            (
+                ScalarFuncSig::RoundInt,
+                Datum::I64(i64::MIN),
+                Datum::I64(i64::MIN),
+            ),
+            (
+                ScalarFuncSig::RoundInt,
+                Datum::U64(u64::MAX),
+                Datum::U64(u64::MAX),
+            ),
+        ];
+        // for ceil decimal to int.
+        let mut ctx = EvalContext::new(Arc::new(EvalConfig::new(FLAG_IGNORE_TRUNCATE).unwrap()));
+        for (sig, arg, exp) in tests {
+            let arg = datum_expr(arg);
+            let mut op =
+                Expression::build(&mut ctx, scalar_func_expr(sig, &[arg.clone()])).unwrap();
+            if mysql::has_unsigned_flag(arg.get_field_type().get_flag()) {
+                op.mut_tp().set_flag(types::UNSIGNED_FLAG as u32);
             }
             let got = op.eval(&mut ctx, &[]).unwrap();
             assert_eq!(got, exp);
