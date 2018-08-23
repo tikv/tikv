e6fef8d938ee15fb3116759332db6c95903b94a6
laidahe <laidahe2006@126.com>
2018-08-23 20:21:39 +0800

    RouldReal to RoundReal

--- src/coprocessor/dag/expr/scalar_function.rs
+++ src/coprocessor/dag/expr/scalar_function.rs
@@ -183,7 +183,7 @@ impl ScalarFunc {
             | ScalarFuncSig::CeilDecToDec
             | ScalarFuncSig::CeilDecToInt
             | ScalarFuncSig::FloorReal
-            | ScalarFuncSig::RouldReal
+            | ScalarFuncSig::RoundReal
             | ScalarFuncSig::FloorIntToInt
             | ScalarFuncSig::FloorIntToDec
             | ScalarFuncSig::FloorDecToDec
@@ -796,7 +796,7 @@ dispatch_call! {
         AbsReal => abs_real,
         CeilReal => ceil_real,
         FloorReal => floor_real,
-        RouldReal => round_real,
+        RoundReal => round_real,
         PI => pi,
 
         IfNullReal => if_null_real,
@@ -1086,7 +1086,7 @@ mod test {
                     ScalarFuncSig::CeilDecToDec,
                     ScalarFuncSig::CeilDecToInt,
                     ScalarFuncSig::FloorReal,
-                    ScalarFuncSig::RouldReal,
+                    ScalarFuncSig::RoundReal,
                     ScalarFuncSig::FloorIntToInt,
                     ScalarFuncSig::FloorIntToDec,
                     ScalarFuncSig::FloorDecToDec,
