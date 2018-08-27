4bd931d31578073718bfc1e4fc8f97aad6b83f2a
spongedc <duchuangucas@live.com>
2018-08-27 22:55:34 +0800

    Add more test cases
    Signed-off-by: spongedc <duchuangucas@live.com>

--- src/coprocessor/dag/expr/builtin_other.rs
+++ src/coprocessor/dag/expr/builtin_other.rs
@@ -37,10 +37,14 @@ impl ScalarFunc {
 
 #[cfg(test)]
 mod test {
+    use coprocessor::codec::mysql::Decimal;
     use coprocessor::codec::Datum;
     use coprocessor::dag::expr::test::{datum_expr, scalar_func_expr};
-    use coprocessor::dag::expr::{EvalContext, Expression};
+    use coprocessor::dag::expr::{EvalConfig, EvalContext, Expression, FLAG_OVERFLOW_AS_WARNING};
+    use std::str::FromStr;
+    use std::sync::Arc;
     use tipb::expression::ScalarFuncSig;
+
     #[test]
     fn test_bit_count() {
         let cases = vec![
@@ -50,6 +54,12 @@ mod test {
             (Datum::I64(-1), Datum::I64(64)),
             (Datum::I64(-11), Datum::I64(62)),
             (Datum::I64(-1000), Datum::I64(56)),
+            (Datum::I64(9223372036854775807), Datum::I64(63)),
+            (Datum::U64(9223372036854775808), Datum::I64(1)),
+            (Datum::U64(9223372036854775809), Datum::I64(2)),
+            (Datum::U64(11111111112222222222), Datum::I64(37)),
+            (Datum::U64(18446744073709551615), Datum::I64(64)),
+            (Datum::U64(18446744073709551614), Datum::I64(63)),
             (Datum::Null, Datum::Null),
         ];
         let mut ctx = EvalContext::default();
@@ -61,14 +71,72 @@ mod test {
             assert_eq!(res, exp);
         }
 
-        // Test overflow
-        let args = datum_expr(Datum::Bytes(
-            b"111111111111111111111111111111111111111111111111111111111111111".to_vec(),
-        ));
-        let child = scalar_func_expr(ScalarFuncSig::CastStringAsInt, &[args]);
-        let op = scalar_func_expr(ScalarFuncSig::BitCount, &[child]);
-        let op = Expression::build(&mut ctx, op).unwrap();
-        let res = op.eval(&mut ctx, &[]).unwrap();
-        assert_eq!(res, Datum::I64(64));
+        let cases = vec![
+            (
+                Datum::Bytes(
+                    b"111111111111111111111111111111111111111111111111111111111111111".to_vec(),
+                ),
+                Datum::I64(64),
+            ),
+            (
+                Datum::Bytes(b"18446744073709551616".to_vec()),
+                Datum::I64(64),
+            ),
+            (
+                Datum::Bytes(b"18446744073709551615".to_vec()),
+                Datum::I64(64),
+            ),
+            (
+                Datum::Bytes(b"18446744073709551614".to_vec()),
+                Datum::I64(63),
+            ),
+            (
+                Datum::Bytes(b"11111111112222222222".to_vec()),
+                Datum::I64(37),
+            ),
+        ];
+        let mut ctx =
+            EvalContext::new(Arc::new(EvalConfig::new(FLAG_OVERFLOW_AS_WARNING).unwrap()));
+        for (input, exp) in cases {
+            let args = &[datum_expr(input)];
+            let child = scalar_func_expr(ScalarFuncSig::CastStringAsInt, args);
+            let op = scalar_func_expr(ScalarFuncSig::BitCount, &[child]);
+            let op = Expression::build(&mut ctx, op).unwrap();
+            let res = op.eval(&mut ctx, &[]).unwrap();
+            assert_eq!(res, exp);
+        }
+
+        let cases = vec![
+            (
+                Datum::Dec(
+                    Decimal::from_str(
+                        "111111111111111111111111111111111111111111111111111111111111111",
+                    ).unwrap(),
+                ),
+                Datum::I64(63),
+            ),
+            (
+                Datum::Dec(
+                    Decimal::from_str(
+                        "-111111111111111111111111111111111111111111111111111111111111111",
+                    ).unwrap(),
+                ),
+                Datum::I64(1),
+            ),
+            (
+                Datum::Dec(Decimal::from_str("18446744073709551616").unwrap()),
+                Datum::I64(63),
+            ),
+        ];
+        let mut ctx =
+            EvalContext::new(Arc::new(EvalConfig::new(FLAG_OVERFLOW_AS_WARNING).unwrap()));
+        for (input, exp) in cases {
+            let args = &[datum_expr(input)];
+            let child = scalar_func_expr(ScalarFuncSig::CastDecimalAsInt, args);
+            let op = scalar_func_expr(ScalarFuncSig::BitCount, &[child]);
+            let op = Expression::build(&mut ctx, op).unwrap();
+            let res = op.eval(&mut ctx, &[]).unwrap();
+            assert_eq!(res, exp);
+        }
     }
 }
