6283469f27c347355094f876993a661dcba19709
xiangyu0xf <xiangyu0xf@gmail.com>
2018-09-06 00:52:17 +0800

    coprocessor/dag/expr: add some builtin UDFs(Rand, RandWithSeed) (#3415)
    Signed-off-by: xiangyu0xf <xiangyu0xf@gmail.com>

--- src/coprocessor/dag/expr/mod.rs
+++ src/coprocessor/dag/expr/mod.rs
@@ -35,7 +35,9 @@ pub use coprocessor::codec::{Error, Result};
 use coprocessor::codec::mysql::{charset, types};
 use coprocessor::codec::mysql::{Decimal, Duration, Json, Time, MAX_FSP};
 use coprocessor::codec::{self, Datum};
+use rand::XorShiftRng;
 use std::borrow::Cow;
+use std::cell::RefCell;
 use std::str;
 use tipb::expression::{Expr, ExprType, FieldType, ScalarFuncSig};
 use util::codec::number;
@@ -65,6 +67,24 @@ pub struct ScalarFunc {
     sig: ScalarFuncSig,
     children: Vec<Expression>,
     tp: FieldType,
+    cus_rng: CusRng,
+}
+
+#[derive(Clone)]
+struct CusRng {
+    rng: RefCell<Option<XorShiftRng>>,
+}
+
+impl ::std::fmt::Debug for CusRng {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "()")
+    }
+}
+
+impl PartialEq for CusRng {
+    fn eq(&self, other: &CusRng) -> bool {
+        self == other
+    }
 }
 
 impl Expression {
@@ -271,6 +291,9 @@ impl Expression {
                             sig: expr.get_sig(),
                             children,
                             tp,
+                            cus_rng: CusRng {
+                                rng: RefCell::new(None),
+                            },
                         })
                     })
             }
