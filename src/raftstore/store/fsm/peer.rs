effedeffa4c744627a3e063deb6ce3d9f950d034
Connor1996 <zbk602423539@gmail.com>
2018-09-06 16:34:53 +0800

    uniform
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- src/raftstore/store/fsm/peer.rs
+++ src/raftstore/store/fsm/peer.rs
@@ -1259,15 +1259,13 @@ impl<T: Transport, C: PdClient> Store<T, C> {
         let region_id = region.get_id();
         let peer = self.region_peers.get_mut(&region_id).unwrap();
         peer.set_region(region);
+        // make approximate size and keys updated in time.
+        // the reason why follower need to update is that there is a issue that after merge
+        // and then transfer leader, the new leader may have stale size and keys.
+        peer.size_diff_hint = self.cfg.region_split_check_diff.0;
         if peer.is_leader() {
-            // make approximate size and keys updated in time.
-            peer.size_diff_hint = self.cfg.region_split_check_diff.0;
             info!("notify pd with merge {:?} into {:?}", source, peer.region());
             peer.heartbeat_pd(&self.pd_worker);
-        } else {
-            // when follower becomes leader, it will invoke split check to update size and keys.
-            peer.approximate_size = None;
-            peer.approximate_keys = None;
         }
     }
 
