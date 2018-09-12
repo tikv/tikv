9c807bc2d9e0655dfd48ef05dd42d05e0ac02091
Breezewish <breezewish@pingcap.com>
2018-09-12 22:46:27 +0800

    Remove unused import
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- src/coprocessor/dag/dag.rs
+++ src/coprocessor/dag/dag.rs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::marker::PhantomData;
 use std::sync::Arc;
 
 use kvproto::coprocessor::{KeyRange, Response};
