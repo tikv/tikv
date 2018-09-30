7be024b5be8e8ab07fc4d60399cca5cb8520eb25
Connor1996 <zbk602423539@gmail.com>
2018-09-30 15:32:01 +0800

    fix format
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- src/raftstore/store/worker/region.rs
+++ src/raftstore/store/worker/region.rs
@@ -348,7 +348,7 @@ impl SnapContext {
                 continue;
             }
 
-            let handle = match rocksdb::get_cf_handle(&self.engines.kv, cf).unwrap();
+            let handle = rocksdb::get_cf_handle(&self.engines.kv, cf).unwrap();
             if let Some(n) = get_cf_num_files_at_level(&self.engines.kv, handle, 0) {
                 let options = self.engines.kv.get_options_cf(handle);
                 if n + 1 >= u64::from(options.get_level_zero_slowdown_writes_trigger()) {
