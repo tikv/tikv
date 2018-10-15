19f42d9557c1647537e708ede92ff6f462e96fc1
MyonKeminta <MyonKeminta@users.noreply.github.com>
2018-10-15 20:01:09 +0800

    Add outdated flag
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- src/raftstore/coprocessor/mod.rs
+++ src/raftstore/coprocessor/mod.rs
@@ -29,7 +29,7 @@ pub mod split_observer;
 pub use self::config::Config;
 pub use self::dispatcher::{CoprocessorHost, Registry};
 pub use self::error::{Error, Result};
-pub use self::region_collection::RegionCollection;
+pub use self::region_collection::{RegionCollection, RegionInfo};
 pub use self::split_check::{
     HalfCheckObserver, Host as SplitCheckerHost, KeysCheckObserver, SizeCheckObserver,
     TableCheckObserver,
