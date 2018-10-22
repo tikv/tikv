aa5639872572b9341885296329d3438993286788
Max <xiahaijiao@hotmail.com>
2018-10-22 11:38:35 +0800

    fuzz: add licenses & use macros.
    Signed-off-by: Max <xiahaijiao@hotmail.com>

--- fuzz/src/util/number_maker.rs
+++ fuzz/src/util/number_maker.rs
@@ -1,89 +1,42 @@
-use std::mem;
+// Copyright 2016 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-pub fn make_u64<I>(iter: &mut I) -> u64
-where
-    I: Iterator<Item = u8>,
-{
-    let mut bytes = [0u8; 8];
-    for byte in &mut bytes {
-        *byte = iter.next().unwrap();
-    }
-    unsafe { mem::transmute(bytes) }
+macro_rules! make_numbers {
+    ($name:ident, $size:expr, $ret:ty) => {
+        pub fn $name<I>(iter: &mut I) -> $ret
+        where
+            I: Iterator<Item = u8>,
+        {
+            let mut bytes = [0u8; $size];
+            for byte in &mut bytes {
+                *byte = iter.next().unwrap();
+            }
+            unsafe { ::std::mem::transmute(bytes) }
+        }
+    };
 }
 
-pub fn make_i64<I>(iter: &mut I) -> i64
-where
-    I: Iterator<Item = u8>,
-{
-    let mut bytes = [0u8; 8];
-    for byte in &mut bytes {
-        *byte = iter.next().unwrap();
-    }
-    unsafe { mem::transmute(bytes) }
-}
-
-pub fn make_f64<I>(iter: &mut I) -> f64
-where
-    I: Iterator<Item = u8>,
-{
-    let mut bytes = [0u8; 8];
-    for byte in &mut bytes {
-        *byte = iter.next().unwrap();
-    }
-    unsafe { mem::transmute(bytes) }
-}
-
-pub fn make_u32<I>(iter: &mut I) -> u32
-where
-    I: Iterator<Item = u8>,
-{
-    let mut bytes = [0u8; 4];
-    for byte in &mut bytes {
-        *byte = iter.next().unwrap();
-    }
-    unsafe { mem::transmute(bytes) }
-}
-
-pub fn make_i32<I>(iter: &mut I) -> i32
-where
-    I: Iterator<Item = u8>,
-{
-    let mut bytes = [0u8; 4];
-    for byte in &mut bytes {
-        *byte = iter.next().unwrap();
-    }
-    unsafe { mem::transmute(bytes) }
-}
-
-pub fn make_u16<I>(iter: &mut I) -> u16
-where
-    I: Iterator<Item = u8>,
-{
-    let mut bytes = [0u8; 2];
-    for byte in &mut bytes {
-        *byte = iter.next().unwrap();
-    }
-    unsafe { mem::transmute(bytes) }
-}
-
-pub fn make_i8<I>(iter: &mut I) -> i8
-where
-    I: Iterator<Item = u8>,
-{
-    let mut bytes = [0u8; 1];
-    for byte in &mut bytes {
-        *byte = iter.next().unwrap();
-    }
-    unsafe { mem::transmute(bytes) }
-}
+make_numbers!{make_u64, 8, u64}
+make_numbers!{make_i64, 8, i64}
+make_numbers!{make_f64, 8, f64}
+make_numbers!{make_u32, 4, u32}
+make_numbers!{make_i32, 4, i32}
+make_numbers!{make_u16, 2, u16}
+make_numbers!{make_i8, 1, i8}
 
 pub fn make_bool<I>(iter: &mut I) -> bool
 where
     I: Iterator<Item = u8>,
 {
-    let mut bytes = [0u8; 1];
-    for byte in &mut bytes {
-        *byte = iter.next().unwrap();
-    }
-    unsafe { mem::transmute(bytes) }
+    make_i8(iter) % 2 == 0
 }
