7db8e5b17c9084b5ddf950ae8547b568d144ca90
ShuNing <nolouch@gmail.com>
2018-12-08 14:06:37 +0800

    raftstore: reject transfer leader to the recently added peers (#3878) (#3897)
    Signed-off-by: nolouch <nolouch@gmail.com>

--- tests/config/mod.rs
+++ tests/config/mod.rs
@@ -123,6 +123,7 @@ fn test_serde_custom_tikv_config() {
         raft_log_gc_count_limit: 12,
         raft_log_gc_size_limit: ReadableSize::kb(1),
         raft_entry_cache_life_time: ReadableDuration::secs(12),
+        raft_reject_transfer_leader_duration: ReadableDuration::secs(3),
         split_region_check_tick_interval: ReadableDuration::secs(12),
         region_split_check_diff: ReadableSize::mb(6),
         region_compact_check_interval: ReadableDuration::secs(12),
