fb56d9ee8d2c8b802ddf129390be72fc9f73b082
Wenxuan Shi <hi@breeswish.org>
2018-12-09 10:45:14 +0800

    Make test_handle_time more stable (#3821)
    * make tests more stable
    
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- src/lib.rs
+++ src/lib.rs
@@ -113,13 +113,15 @@ extern crate uuid;
 extern crate zipf;
 #[macro_use]
 extern crate derive_more;
+extern crate safemem;
 extern crate smallvec;
-
+#[macro_use]
+extern crate more_asserts;
 extern crate base64;
+
 extern crate cop_datatype;
 extern crate flate2;
 extern crate panic_hook;
-extern crate safemem;
 
 #[macro_use]
 pub mod util;
