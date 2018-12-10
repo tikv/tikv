e546bbc0a1193f2866d8d6a4a34598f3c4b293d3
qupeng <onlyqupeng@gmail.com>
2018-12-10 17:46:18 +0800

    Add a batch mpsc (#3738)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- benches/misc/channel/bench_channel.rs
+++ benches/misc/channel/bench_channel.rs
@@ -12,10 +12,11 @@
 // limitations under the License.
 
 use std::sync::mpsc::channel;
-use std::thread;
+use std::{thread, usize};
 use test::Bencher;
 
 use crossbeam::channel;
+use futures::{Future, Stream};
 use mio::{EventLoop, Handler, Sender};
 use tikv::util::mpsc;
 
@@ -172,4 +173,77 @@ fn bench_crossbeam_channel(b: &mut Bencher) {
     tx.send(0).unwrap();
     let n2 = t.join().unwrap();
     assert_eq!(n1, n2);
+}
+
+#[bench]
+fn bench_receiver_stream_batch(b: &mut Bencher) {
+    let (tx, rx) = mpsc::batch::bounded::<i32>(128, 8);
+    for _ in 0..1 {
+        let tx1 = tx.clone();
+        thread::spawn(move || {
+            (0..usize::MAX)
+                .take_while(|i| tx1.send(*i as i32).is_ok())
+                .count();
+        });
+    }
+
+    let mut rx = Some(mpsc::batch::BatchReceiver::new(rx, 32, Vec::new, |v, t| {
+        v.push(t)
+    }));
+
+    b.iter(|| {
+        let mut count = 0;
+        let mut rx1 = rx.take().unwrap();
+        loop {
+            let (item, s) = rx1
+                .into_future()
+                .wait()
+                .map_err(|_| unreachable!())
+                .unwrap();
+            rx1 = s;
+            if let Some(v) = item {
+                count += v.len();
+                if count < 10000 {
+                    continue;
+                }
+            }
+            break;
+        }
+        rx = Some(rx1);
+    })
+}
+
+#[bench]
+fn bench_receiver_stream(b: &mut Bencher) {
+    let (tx, rx) = mpsc::batch::bounded::<i32>(128, 1);
+    for _ in 0..1 {
+        let tx1 = tx.clone();
+        thread::spawn(move || {
+            (0..usize::MAX)
+                .take_while(|i| tx1.send(*i as i32).is_ok())
+                .count();
+        });
+    }
+
+    let mut rx = Some(rx);
+    b.iter(|| {
+        let mut count = 0;
+        let mut rx1 = rx.take().unwrap();
+        loop {
+            let (item, s) = rx1
+                .into_future()
+                .wait()
+                .map_err(|_| unreachable!())
+                .unwrap();
+            rx1 = s;
+            if item.is_some() {
+                count += 1;
+                if count < 10000 {
+                    continue;
+                }
+            }
+            break;
+        }
+        rx = Some(rx1);
+    })
 }
