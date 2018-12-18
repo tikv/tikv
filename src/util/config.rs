8db461104e486c7d0fab3f25fe8762994d9eb30e
Connor <zbk602423539@gmail.com>
2018-12-18 10:03:38 +0800

    Supports day format in ReadableDuration (#3687) (#3931)
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- src/util/config.rs
+++ src/util/config.rs
@@ -239,10 +239,12 @@ const PB: u64 = (TB as u64) * (DATA_MAGNITUDE as u64);
 
 const TIME_MAGNITUDE_1: u64 = 1000;
 const TIME_MAGNITUDE_2: u64 = 60;
+const TIME_MAGNITUDE_3: u64 = 24;
 const MS: u64 = UNIT;
 const SECOND: u64 = MS * TIME_MAGNITUDE_1;
 const MINUTE: u64 = SECOND * TIME_MAGNITUDE_2;
 const HOUR: u64 = MINUTE * TIME_MAGNITUDE_2;
+const DAY: u64 = HOUR * TIME_MAGNITUDE_3;
 
 #[derive(Clone, Debug, Copy, PartialEq)]
 pub struct ReadableSize(pub u64);
@@ -452,6 +454,11 @@ impl Serialize for ReadableDuration {
     {
         let mut dur = util::time::duration_to_ms(self.0);
         let mut buffer = String::new();
+        // For backward compatibility, do not render config that old version cannot parse.
+        // if dur >= DAY {
+        //     write!(buffer, "{}d", dur / DAY).unwrap();
+        //     dur %= DAY;
+        // }
         if dur >= HOUR {
             write!(buffer, "{}h", dur / HOUR).unwrap();
             dur %= HOUR;
@@ -496,17 +503,18 @@ impl<'de> Deserialize<'de> for ReadableDuration {
                 if !dur_str.is_ascii() {
                     return Err(E::invalid_value(Unexpected::Str(dur_str), &"ascii string"));
                 }
-                let err_msg = "valid duration, only h, m, s, ms are supported.";
+                let err_msg = "valid duration, only d, h, m, s, ms are supported.";
                 let mut left = dur_str.as_bytes();
-                let mut last_unit = HOUR + 1;
+                let mut last_unit = DAY + 1;
                 let mut dur = 0f64;
-                while let Some(idx) = left.iter().position(|c| b"hms".contains(c)) {
+                while let Some(idx) = left.iter().position(|c| b"dhms".contains(c)) {
                     let (first, second) = left.split_at(idx);
                     let unit = if second.starts_with(b"ms") {
                         left = &left[idx + 2..];
                         MS
                     } else {
                         let u = match second[0] {
+                            b'd' => DAY,
                             b'h' => HOUR,
                             b'm' => MINUTE,
                             b's' => SECOND,
@@ -518,7 +526,7 @@ impl<'de> Deserialize<'de> for ReadableDuration {
                     if unit >= last_unit {
                         return Err(E::invalid_value(
                             Unexpected::Str(dur_str),
-                            &"h, m, s, ms should occur in giving order.",
+                            &"d, h, m, s, ms should occur in given order.",
                         ));
                     }
                     // do we need to check 12h360m?
@@ -1089,6 +1097,8 @@ mod test {
             (0, 0, "0s"),
             (0, 1, "1ms"),
             (2, 0, "2s"),
+            (24 * 3600, 0, "24h"),
+            (36 * 3600, 0, "36h"),
             (4 * 60, 0, "4m"),
             (5 * 3600, 0, "5h"),
             (3600 + 2 * 60, 0, "1h2m"),
@@ -1105,7 +1115,12 @@ mod test {
             assert_eq!(res_dur.d.0, d.d.0);
         }
 
-        let decode_cases = vec![(" 0.5 h2m ", 3600 / 2 + 2 * 60, 0)];
+        let decode_cases = vec![
+            (" 0.5 h2m ", 3600 / 2 + 2 * 60, 0),
+            ("24h", 24 * 3600, 0),
+            ("1.5d", 36 * 3600, 0),
+            ("36h", 36 * 3600, 0),
+        ];
         for (src, secs, ms) in decode_cases {
             let src = format!("d = {:?}", src);
             let res: DurHolder = toml::from_str(&src).unwrap();
