b156a0524bf1c6cfc4cf1bc9051928817a776bcd
Breezewish <breezewish@pingcap.com>
2018-12-24 16:45:32 +0800

    Support profiling sections of code
    Signed-off-by: Breezewish <breezewish@pingcap.com>

---
+++ components/cpu_profiler/src/profiler_dummy.rs
@@ -0,0 +1,27 @@
+// Copyright 2018 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[inline]
+pub fn start(_fname: impl Into<Vec<u8>>) {
+    // Do nothing
+}
+
+#[inline]
+pub fn stop() {
+    // Do nothing
+}
+
+#[inline]
+pub fn is_enabled() -> bool {
+    false
+}
