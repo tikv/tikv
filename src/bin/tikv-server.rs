cde52a2cbb5f594ebd08ef4f786d53e182eab040
Neil Shen <overvenus@gmail.com>
2018-12-25 17:10:45 +0800

    *: rearrange loggers
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- src/bin/tikv-server.rs
+++ src/bin/tikv-server.rs
@@ -22,21 +22,19 @@ extern crate jemallocator;
 extern crate libc;
 #[macro_use]
 extern crate log;
-#[macro_use(slog_o, slog_kv)]
-extern crate slog;
+extern crate hyper;
 #[cfg(unix)]
 extern crate nix;
 extern crate rocksdb;
 extern crate serde_json;
 #[cfg(unix)]
 extern crate signal;
+extern crate slog;
 extern crate slog_async;
 extern crate slog_scope;
 extern crate slog_stdlog;
 extern crate slog_term;
-#[macro_use]
 extern crate tikv;
-extern crate hyper;
 extern crate toml;
 
 #[cfg(unix)]
@@ -411,8 +409,8 @@ fn main() {
 
     // Sets the global logger ASAP.
     // It is okay to use the config w/o `validata()`,
-    // because `init_log()` handles various conditions.
-    let guard = init_log(&config);
+    // because `initial_logger()` handles various conditions.
+    let guard = initial_logger(&config);
     tikv_util::set_exit_hook(false, Some(guard), &config.storage.data_dir);
 
     // Print version information.
