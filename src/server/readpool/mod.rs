009cadce32f82037d5056fbf5ae7f116f9deaefd
Hetian Zhu <fullstop1005@gmail.com>
2018-12-30 10:27:35 +0800

    use closure directly since RFC 2132 is completed (#3995)
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/server/readpool/mod.rs
+++ src/server/readpool/mod.rs
@@ -53,12 +53,9 @@ impl<T: futurepool::Context + 'static> Clone for ReadPool<T> {
 }
 
 impl<T: futurepool::Context + 'static> ReadPool<T> {
-    // Rust does not support copying closures (RFC 2132) so that we need a closure builder.
-    // TODO: Use a single closure once RFC 2132 is implemented.
-    pub fn new<F, CF>(name_prefix: &str, config: &Config, context_factory_builder: F) -> Self
+    pub fn new<F>(name_prefix: &str, config: &Config, context_factory: F) -> Self
     where
-        F: futurepool::Factory<CF>,
-        CF: futurepool::Factory<T>,
+        F: Fn() -> T,
     {
         let tick_interval = Duration::from_secs(TICK_INTERVAL_SEC);
 
@@ -68,21 +65,21 @@ impl<T: futurepool::Context + 'static> ReadPool<T> {
                 config.stack_size.0 as usize,
                 &format!("{}-high", name_prefix),
                 tick_interval,
-                context_factory_builder.build(),
+                &context_factory,
             ),
             pool_normal: FuturePool::new(
                 config.normal_concurrency,
                 config.stack_size.0 as usize,
                 &format!("{}-normal", name_prefix),
                 tick_interval,
-                context_factory_builder.build(),
+                &context_factory,
             ),
             pool_low: FuturePool::new(
                 config.low_concurrency,
                 config.stack_size.0 as usize,
                 &format!("{}-low", name_prefix),
                 tick_interval,
-                context_factory_builder.build(),
+                &context_factory,
             ),
             max_tasks_high: config.max_tasks_per_worker_high * config.high_concurrency,
             max_tasks_normal: config.max_tasks_per_worker_normal * config.normal_concurrency,
@@ -194,7 +191,7 @@ mod tests {
 
     #[test]
     fn test_future_execute() {
-        let read_pool = ReadPool::new("readpool", &Config::default_for_test(), || || Context {});
+        let read_pool = ReadPool::new("readpool", &Config::default_for_test(), || Context {});
 
         expect_val(
             vec![1, 2, 4],
@@ -251,7 +248,7 @@ mod tests {
                 max_tasks_per_worker_high: 2,
                 ..Config::default_for_test()
             },
-            || || Context {},
+            || Context {},
         );
 
         wait_on_new_thread(
