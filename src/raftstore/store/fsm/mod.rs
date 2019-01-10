3050ccd2e120292ea511b2fb60797546455dbf4f
Connor1996 <zbk602423539@gmail.com>
2019-01-10 17:12:24 +0800

    address comment
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- src/raftstore/store/fsm/mod.rs
+++ src/raftstore/store/fsm/mod.rs
@@ -59,20 +59,20 @@ type Key = Vec<u8>;
 pub struct Store<T, C: 'static> {
     /// store_tag, "[store <store_id>]"
     tag: String,
-    /// the store meta information of the store
+    /// The store meta information of the store.
     store: metapb::Store,
-    /// the config of TiKV
+    /// The config of TiKV.
     cfg: Rc<Config>,
-    /// the local engine
+    /// The local engine.
     engines: Engines,
-    /// the start time of this TiKV
+    /// The start time of this TiKV instance.
     start_time: Timespec,
-    /// whether the process of handling Raft ready is slow
+    /// Whether the process of handling Raft ready is slow.
     is_busy: bool,
 
     /// It is used to transport messages between different Raft peers.
     trans: T,
-    /// the sender pointing to itself, used to send some periodic tasks
+    /// The sender pointing to itself, used to send some periodic tasks.
     sendch: SendCh<Msg>,
     /// Unreachable reports can be ignored if the event loop's channel is full,
     /// which may lead to abandoned follower. So use a special receiver to handle
@@ -86,59 +86,59 @@ pub struct Store<T, C: 'static> {
     /// the Regions in Merging state
     merging_regions: Option<Vec<metapb::Region>>,
 
-    /// the Regions for which there will probably be some readiness which
+    /// The Regions for which there will probably be some readiness which
     /// needs to be handled between two mio ticks.
     pending_raft_groups: HashSet<u64>,
-    /// the Regions with pending snapshots between two mio ticks.
+    /// The Regions with pending snapshots between two mio ticks.
     pending_snapshot_regions: Vec<metapb::Region>,
-    /// A marker used to indicate if the peer of a region is going to apply a snapshot
-    /// with different range.
-    /// It assumes that when a peer is going to accept snapshot, it can never
+    /// A marker used to indicate whether the peer of a Region is going to apply a snapshot
+    /// with a different range.
+    /// It assumes that when a peer is going to accept the snapshot, it can never
     /// catch up by normal log replication.
     pending_cross_snap: HashMap<u64, metapb::RegionEpoch>,
-    /// `MsgRequestPreVote` or `MsgRequestVote` messages from new split regions shouldn't be dropped
-    /// if there is no such region in this store now. So the messages are recorded temporarily and will
+    /// `MsgRequestPreVote` or `MsgRequestVote` messages from newly split Regions shouldn't be dropped
+    /// if there is no such Region in this store now. So the messages are recorded temporarily and will
     /// be handled later.
     pending_votes: RingQueue<RaftMessage>,
 
-    /// the worker to gc Raft logs asynchronously
+    /// The worker to perform a GC (Garbage Collection) on Raft logs asynchronously.
     raftlog_gc_worker: Worker<RaftlogGcTask>,
-    /// the worker to do consistency check asynchronously
+    /// The worker to do the consistency check asynchronously.
     consistency_check_worker: Worker<ConsistencyCheckTask>,
-    /// the worker to do read requests asynchronously to avoid the blocking of write requests
+    /// The worker to do read requests asynchronously to avoid the blocking of write requests.
     local_reader: Worker<ReadTask>,
 
-    /// the worker to split Regions asynchronously
+    /// The worker to split Regions asynchronously.
     split_check_worker: Worker<SplitCheckTask>,
     /// It is a host for some observers which is used to do some information collection works.
     coprocessor_host: Arc<CoprocessorHost>,
 
-    /// the worker to clean up already ingested SST files asynchronously, mainly used for importer
+    /// The worker to clean up already ingested SST files asynchronously, mainly used for importer.
     cleanup_sst_worker: Worker<CleanupSSTTask>,
     /// It manages SST files that are waiting for ingesting.
     importer: Arc<SSTImporter>,
 
-    /// the worker to check and compact RocksDB asynchronously to speed up disk space reclamation
+    /// The worker to check and compact RocksDB asynchronously to speed up disk space reclamation.
     compact_worker: Worker<CompactTask>,
-    /// the last checked key of the last task for compact worker
+    /// The last checked key of the last task for the compact worker.
     last_compact_checked_key: Key,
 
-    /// the worker to schedule PD related tasks asynchronously
+    /// The worker to schedule PD related tasks asynchronously.
     pd_worker: FutureWorker<PdTask>,
-    /// the actual client to send PD requests and receive responses
+    /// The actual client to send PD requests and receive responses.
     pd_client: Arc<C>,
 
-    /// the worker to apply raft logs asynchronously
+    /// The worker to apply Raft logs asynchronously.
     apply_worker: Worker<ApplyTask>,
-    /// the receiver to receive the apply results from apply worker
+    /// The receiver to receive the `apply` results from the `apply` worker.
     apply_res_receiver: Option<StdReceiver<ApplyTaskRes>>,
 
-    /// the worker to do snapshot related works asynchronously
+    /// The worker to do snapshot related works asynchronously.
     region_worker: Worker<RegionTask>,
     /// It is used to manage all the snapshots, including generation and deletion.
     snap_mgr: SnapManager,
 
-    // some local metrics
+    // Some local metrics.
     entry_cache_metrics: Rc<RefCell<CacheQueryStats>>,
     raft_metrics: RaftMetrics,
     store_stat: StoreStat,
