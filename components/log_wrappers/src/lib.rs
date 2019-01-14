fc658464e9c3f1cfef61bf4d2afa8b46a0c916c3
Breezewish <breezewish@pingcap.com>
2019-01-15 01:04:44 +0800

    Add unit tests
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- components/log_wrappers/src/lib.rs
+++ components/log_wrappers/src/lib.rs
@@ -15,7 +15,11 @@
 
 extern crate hex;
 extern crate kvproto as lib_kvproto;
+#[macro_use]
 extern crate slog;
+extern crate slog_term;
+
+pub mod test_util;
 
 // We don't introduce namespace std for std functions.
 
@@ -32,6 +36,37 @@ pub mod time {
         ) -> ::slog::Result {
             serializer.emit_arguments(key, &format_args!("{:?}", self.0))
         }
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_duration() {
+        let buffer = ::test_util::SyncLoggerBuffer::new();
+        let logger = buffer.build_logger();
+
+        slog_info!(logger, "foo"; "bar" => Duration(&::std::time::Duration::from_millis(2500)));
+        assert_eq!(&buffer.as_string(), "TIME INFO foo, bar: 2.5s\n");
+
+        buffer.clear();
+        slog_info!(logger, "foo"; "bar" => Duration(&::std::time::Duration::from_millis(23)));
+        assert_eq!(&buffer.as_string(), "TIME INFO foo, bar: 23ms\n");
+
+        buffer.clear();
+        slog_info!(logger, "foo"; "bar" => Duration(&::std::time::Duration::from_millis(0)));
+        assert_eq!(&buffer.as_string(), "TIME INFO foo, bar: 0ns\n");
+
+        buffer.clear();
+        slog_info!(logger, "foo"; "bar" => Duration(&::std::time::Duration::from_secs(1000)));
+        assert_eq!(&buffer.as_string(), "TIME INFO foo, bar: 1000s\n");
+
+        buffer.clear();
+        slog_info!(logger, "foo"; "bar" => Some(Duration(&::std::time::Duration::from_secs(1))));
+        assert_eq!(&buffer.as_string(), "TIME INFO foo, bar: 1s\n");
+
+        buffer.clear();
+        let v: Option<Duration> = None;
+        slog_info!(logger, "foo"; "bar" => v);
+        assert_eq!(&buffer.as_string(), "TIME INFO foo, bar: None\n");
     }
 }
 
@@ -57,11 +92,40 @@ pub mod kvproto {
                 )
             }
         }
+
+        #[cfg(test)]
+        #[test]
+        fn test_key_range() {
+            let buffer = ::test_util::SyncLoggerBuffer::new();
+            let logger = buffer.build_logger();
+
+            let mut range = ::lib_kvproto::kvrpcpb::KeyRange::new();
+            range.set_start_key(b"\x20\x00".to_vec());
+            range.set_end_key(b"\x31\xFF\x12a".to_vec());
+            slog_info!(logger, "foo"; "bar" => KeyRange(&range));
+            assert_eq!(
+                &buffer.as_string(),
+                "TIME INFO foo, bar: [2000, 31FF1261)\n"
+            );
+
+            buffer.clear();
+            let mut range = ::lib_kvproto::kvrpcpb::KeyRange::new();
+            range.set_end_key(b"\x31".to_vec());
+            slog_info!(logger, "foo"; "bar" => KeyRange(&range));
+            assert_eq!(&buffer.as_string(), "TIME INFO foo, bar: [, 31)\n");
+
+            buffer.clear();
+            let mut range = ::lib_kvproto::kvrpcpb::KeyRange::new();
+            range.set_start_key(b"\xC0".to_vec());
+            slog_info!(logger, "foo"; "bar" => KeyRange(&range));
+            assert_eq!(&buffer.as_string(), "TIME INFO foo, bar: [C0, )\n");
+        }
     }
 
     pub mod coprocessor {
         pub struct KeyRange<'a>(pub &'a ::lib_kvproto::coprocessor::KeyRange);
 
+        // Similar to `kvrpcpb::KeyRange`. Tests are ignored.
         impl<'a> ::slog::Value for KeyRange<'a> {
             #[inline]
             fn serialize(
