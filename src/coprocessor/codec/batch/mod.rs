db1fef9bbbdabb18e1a64dc4fea2ad9f80c3d32c
Wenxuan Shi <hi@breeswish.org>
2019-02-17 23:32:10 +0800

    Extract LazyBatchColumn (#4211)
    Signed-off-by: Breezewish <breezewish@pingcap.com>
    
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- src/coprocessor/codec/batch/mod.rs
+++ src/coprocessor/codec/batch/mod.rs
@@ -11,8 +11,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod lazy_column;
 mod rows;
 mod vector;
 
+pub use self::lazy_column::LazyBatchColumn;
 pub use self::rows::BatchRows;
 pub use self::vector::VectorValue;
