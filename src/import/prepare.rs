97facf89c1c67a805868e248db0cd9217f8d48e2
kennytm <kennytm@gmail.com>
2019-02-25 17:59:23 +0800

    import: wait after split before scatter
    Signed-off-by: kennytm <kennytm@gmail.com>

--- src/import/prepare.rs
+++ src/import/prepare.rs
@@ -30,6 +30,9 @@ use super::{Config, Error, Result};
 const MAX_RETRY_TIMES: u64 = 3;
 const RETRY_INTERVAL_SECS: u64 = 1;
 
+const SCATTER_WAIT_MAX_RETRY_TIMES: u64 = 125;
+const SCATTER_WAIT_INTERVAL_MILLIS: u64 = 8;
+
 /// PrepareJob is responsible for improving cluster data balance
 ///
 /// The main job is:
@@ -180,6 +183,23 @@ impl<Client: ImportClient> PrepareRangeJob<Client> {
         }
         match self.split_region(&region) {
             Ok(new_region) => {
+                // We need to wait for a few milliseconds, because PD may have
+                // not received any heartbeat from the new split region, such
+                // that PD cannot create scatter operator for the new split
+                // region because it doesn't have the meta data of the new split
+                // region.
+                for i in 0..SCATTER_WAIT_MAX_RETRY_TIMES {
+                    if self.client.has_region_id(new_region.region.id)? {
+                        if i > 0 {
+                            debug!("waited between split and scatter"; "retry times" => %i);
+                        }
+                        break;
+                    } else if i == SCATTER_WAIT_MAX_RETRY_TIMES - 1 {
+                        warn!("split region still failed after exhausting all retries");
+                    } else {
+                        thread::sleep(Duration::from_millis(SCATTER_WAIT_INTERVAL_MILLIS));
+                    }
+                }
                 self.scatter_region(&new_region)?;
                 Ok(true)
             }
