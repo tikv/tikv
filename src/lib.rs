55432329184e1744ca8e39e9a81fabc3c97eef29
Nick Cameron <nrc@ncameron.org>
2019-03-01 11:40:02 +1300

    Tidy up lib.rs
    Signed-off-by: Nick Cameron <nrc@ncameron.org>

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@
 
 #![crate_type = "lib"]
 #![cfg_attr(test, feature(test))]
+#![recursion_limit = "200"]
+
 #![feature(label_break_value)]
 #![feature(fnbox)]
 #![feature(alloc)]
@@ -21,11 +23,11 @@
 #![feature(integer_atomics)]
 #![feature(proc_macro_hygiene)]
 #![feature(cell_update)]
-#![recursion_limit = "200"]
 #![feature(range_contains)]
+
 // Currently this raises some false positives, so we allow it:
 // https://github.com/rust-lang-nursery/rust-clippy/issues/2638
-#![cfg_attr(feature = "cargo-clippy", allow(clippy::nonminimal_bool))]
+#![allow(clippy::nonminimal_bool)]
 
 extern crate alloc;
 extern crate backtrace;
