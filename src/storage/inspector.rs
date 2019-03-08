f4e0998353a2990395114e55d5bb573782a6daa6
qupeng <qupeng@pingcap.com>
2019-03-08 16:18:23 +0800

    use futures to fetch ts.

--- src/storage/inspector.rs
+++ src/storage/inspector.rs
@@ -11,34 +11,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
-use std::sync::atomic::{self, AtomicUsize};
-use std::sync::{Arc, Mutex, RwLock};
+use std::sync::{Arc, RwLock};
 
 //use super::metrics::*;
-use futures::Future;
+use futures::{Future, Async, Poll, Stream};
+use futures::sync::mpsc;
+use tokio_threadpool::{ThreadPool, Builder as ThreadPoolBuilder};
 //use raft::StateRole;
 
 use crate::pd::PdClient;
 use crate::raftstore::coprocessor::CoprocessorHost;
 use crate::util::collections::HashMap;
-use crate::util::worker::{Builder as WorkerBuilder, Runnable, Scheduler, Worker};
 use crate::util::HandyRwLock;
 
 //const KEY_BUCKET_SIZE: usize = 1024;
-
-#[derive(Debug, Clone)]
-enum InspectorRunnerTask {
-    //    UpdateRegion { region_id: u64, version: u64 },
-    // RemoveRegion { region_id: u64 },
-    UpdateTsForRegion { region_id: u64, version: u64 },
-}
-
-impl Display for InspectorRunnerTask {
-    fn fmt(&self, fmt: &mut Formatter) -> FmtResult {
-        Debug::fmt(self, fmt)
-    }
-}
+const INSPECTOR_NAME_PREFIX: &str = "mvcc-inspector";
 
 //#[derive(Clone)]
 //struct LeaderChangeObserver {
@@ -96,6 +83,12 @@ impl Display for InspectorRunnerTask {
 //    }
 //}
 
+#[derive(Debug, Clone, Copy)]
+struct UpdateTsTask { 
+    region_id: u64,
+    version: u64,
+}
+
 #[derive(Default)]
 struct RegionMaxTsRecord {
     is_ready: bool,
@@ -106,45 +99,67 @@ struct RegionMaxTsRecord {
 // region_id -> (is_ready, epoch_version, max_read_ts)
 type TsMap = HashMap<u64, Arc<RwLock<RegionMaxTsRecord>>>;
 
+#[derive(Clone)]
 pub struct MvccInspector {
-    max_read_ts_map: Arc<RwLock<TsMap>>,
-    worker: Arc<Mutex<Worker<InspectorRunnerTask>>>,
-    scheduler: Scheduler<InspectorRunnerTask>,
-    refs: Arc<AtomicUsize>,
+    inner: Arc<Inner>,
+}
+
+pub struct Inner {
+    max_read_ts_map: RwLock<TsMap>,
+    worker: ThreadPool,
+    sender: mpsc::UnboundedSender<UpdateTsTask>,
 }
 
 impl MvccInspector {
     pub fn new<C: PdClient + 'static>(pd_client: Arc<C>) -> Self {
-        let max_read_ts_map = Arc::new(RwLock::new(TsMap::default()));
+        let worker = ThreadPoolBuilder::new()
+            .pool_size(1)
+            .name_prefix(INSPECTOR_NAME_PREFIX)
+            .build();
+        let (tx, rx) = mpsc::unbounded();
+        let inner = Arc::new(Inner {
+            max_read_ts_map: Default::default(),
+            worker,
+            sender: tx,
+        });
 
-        let update_runner = MvccInspectorUpdateRunner {
-            max_read_ts_map: max_read_ts_map.clone(),
-            pd_client,
-        };
-
-        let mut worker = WorkerBuilder::new("mvcc-inspector-update-runner").create();
-        worker.start(update_runner).unwrap();
-        let scheduler = worker.scheduler();
-
-        Self {
-            max_read_ts_map,
-            worker: Arc::new(Mutex::new(worker)),
-            scheduler,
-            refs: Arc::new(AtomicUsize::new(1)),
-        }
+        let inner1 = Arc::clone(&inner);
+        inner.worker.spawn(TaskCollector(rx).for_each(move |map|{
+            let inner2 = Arc::clone(&inner1);
+            pd_client.get_timestamp()
+                .map_err(|e| error!("get timestamp fail"; "err" => ?e))
+                .map(move |tso| (tso, map))
+                .map(move |(tso, map)| {
+                    for (region_id, version) in map {
+                        let global_map = inner2.max_read_ts_map.rl();  
+                        let e = Arc::clone(global_map.get(&region_id).unwrap());
+                        drop(global_map);
+                        let mut e = e.wl();
+                        if e.version != version {
+                            continue;
+                        }
+                        e.max_read_ts = tso;
+                        e.is_ready = true;
+                        info!("max_read_ts is ready"; "region_id" => region_id, "version" => version, "max_read_ts" => tso);
+                    }
+                })
+        }));
+        Self { inner }
     }
 
     // Quickly make the code buildable
     pub fn new_mock() -> Self {
-        let worker = WorkerBuilder::new("mvcc-inspector-update-runner").create();
-        let scheduler = worker.scheduler();
-
-        Self {
+        let worker = ThreadPoolBuilder::new()
+            .pool_size(1)
+            .name_prefix(INSPECTOR_NAME_PREFIX)
+            .build();
+        let (tx, _) = mpsc::unbounded(); // TODO: real mock.
+        let inner = Arc::new(Inner {
             max_read_ts_map: Default::default(),
-            worker: Arc::new(Mutex::new(worker)),
-            scheduler,
-            refs: Arc::new(AtomicUsize::new(1)),
-        }
+            worker,
+            sender: tx,
+        });
+        Self { inner }
     }
 
     pub fn register_observer(&self, _host: &mut CoprocessorHost) {
@@ -157,10 +172,6 @@ impl MvccInspector {
         //            .register_region_change_observer(1, box observer);
     }
 
-    fn stop(&self) {
-        self.worker.lock().unwrap().stop().unwrap().join().unwrap();
-    }
-
     pub fn report_read_ts(&self, region_id: u64, version: u64, ts: u64, _from: &str) {
         if ts == u64::max_value() {
             return;
@@ -170,8 +181,8 @@ impl MvccInspector {
         if lock.version < version {
             lock.is_ready = false;
             lock.version = version;
-            self.scheduler
-                .schedule(InspectorRunnerTask::UpdateTsForRegion { region_id, version })
+            self.inner.sender
+                .unbounded_send(UpdateTsTask { region_id, version })
                 .unwrap();
         }
         if lock.max_read_ts < ts {
@@ -189,10 +200,10 @@ impl MvccInspector {
     }
 
     fn get_entry(&self, region_id: u64, version: u64) -> Arc<RwLock<RegionMaxTsRecord>> {
-        if let Some(entry) = self.max_read_ts_map.rl().get(&region_id) {
+        if let Some(entry) = self.inner.max_read_ts_map.rl().get(&region_id) {
             return entry.clone();
         }
-        let mut map = self.max_read_ts_map.wl();
+        let mut map = self.inner.max_read_ts_map.wl();
         let mut not_exist = false;
         let entry = map.entry(region_id).or_insert_with(|| {
             not_exist = true;
@@ -202,63 +213,33 @@ impl MvccInspector {
                 max_read_ts: 0,
             }))
         });
-        self.scheduler
-            .schedule(InspectorRunnerTask::UpdateTsForRegion { region_id, version })
+        self.inner.sender
+            .unbounded_send(UpdateTsTask { region_id, version })
             .unwrap();
         entry.clone()
     }
 }
 
-impl Clone for MvccInspector {
-    fn clone(&self) -> Self {
-        self.refs.fetch_add(1, atomic::Ordering::SeqCst);
-
-        Self {
-            max_read_ts_map: self.max_read_ts_map.clone(),
-            worker: self.worker.clone(),
-            scheduler: self.scheduler.clone(),
-            refs: self.refs.clone(),
-        }
-    }
-}
-
-impl Drop for MvccInspector {
-    fn drop(&mut self) {
-        let refs = self.refs.fetch_sub(1, atomic::Ordering::SeqCst);
-        if refs == 1 {
-            self.stop();
-        }
-    }
-}
-
-#[derive(Clone)]
-struct MvccInspectorUpdateRunner<C: PdClient> {
-    max_read_ts_map: Arc<RwLock<TsMap>>,
-    pd_client: Arc<C>,
-}
-
-impl<C: PdClient> MvccInspectorUpdateRunner<C> {
-    fn update_ts_for_region(&self, region_id: u64, version: u64) {
-        let ts = self.pd_client.get_timestamp().wait().unwrap();
-        if let Some(mut entry) = self.max_read_ts_map.rl().get(&region_id).map(|e| e.wl()) {
-            if entry.version != version {
-                return;
-            }
-            if entry.max_read_ts < ts {
-                entry.max_read_ts = ts;
-            }
-            entry.is_ready = true;
-            info!("max_read_ts is ready"; "region_id" => region_id, "version" => version, "max_read_ts" => ts);
-        }
-    }
-}
-
-impl<C: PdClient> Runnable<InspectorRunnerTask> for MvccInspectorUpdateRunner<C> {
-    fn run(&mut self, event: InspectorRunnerTask) {
-        match event {
-            InspectorRunnerTask::UpdateTsForRegion { region_id, version } => {
-                self.update_ts_for_region(region_id, version)
+struct TaskCollector(mpsc::UnboundedReceiver<UpdateTsTask>);
+impl Stream for TaskCollector {
+    type Error = ();
+    type Item = HashMap<u64, u64>;
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        let mut items = HashMap::new();
+        loop {
+            match self.0.poll() {
+                Ok(Async::Ready(Some(UpdateTsTask {region_id, version}))) => {
+                    if items.get(&region_id).unwrap_or(&0) < &version {
+                        items.insert(region_id, version);
+                    }
+                },
+                Ok(Async::NotReady) => break,
+                _ => unreachable!(),
             }
         }
+        if items.is_empty() {
+            return Ok(Async::NotReady);
+        }
+        Ok(Async::Ready(Some(items)))
     }
 }
