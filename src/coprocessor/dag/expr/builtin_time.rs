5b3a9278dec2beb342f05864ae9a892306ded2d8
Seact <seact_l@outlook.com>
2019-03-24 23:41:25 +0800

    PeriodAdd and PeriodDiff
    Signed-off-by: Seact <seact_l@outlook.com>

--- src/coprocessor/dag/expr/builtin_time.rs
+++ src/coprocessor/dag/expr/builtin_time.rs
@@ -322,6 +322,52 @@ impl ScalarFunc {
         Ok(None)
     }
 
+    fn period_to_month(period: u64) -> u64 {
+        if period == 0 {
+            return 0;
+        }
+        let (year, month) = (period / 100, period % 100);
+        if year < 70 {
+            year * 12 + month - 1 + 24000 // 2000*12
+        } else if year < 100 {
+            year * 12 + month - 1 + 22800 // 1900*12
+        } else {
+            year * 12 + month - 1
+        }
+    }
+
+    fn month_to_period(month: u64) -> u64 {
+        if month == 0 {
+            return 0;
+        }
+        let year = month / 12;
+        if year < 70 {
+            year * 100 + month % 12 + 1 + 200000 // 2000*100
+        } else if year < 100 {
+            year * 100 + month % 12 + 1 + 190000 // 1900*100
+        } else {
+            year * 100 + month % 12 + 1
+        }
+    }
+
+    pub fn period_add(&self, ctx: &mut EvalContext, row: &[Datum]) -> Result<Option<i64>> {
+        let p = try_opt!(self.children[0].eval_int(ctx, row));
+        if p == 0 {
+            return Ok(Some(0));
+        }
+        let n = try_opt!(self.children[1].eval_int(ctx, row));
+        let (month, _) = (Self::period_to_month(p as u64) as i32 as i64).overflowing_add(n);
+        Ok(Some(Self::month_to_period(month as u32 as u64) as i64))
+    }
+
+    pub fn period_diff(&self, ctx: &mut EvalContext, row: &[Datum]) -> Result<Option<i64>> {
+        let p1 = try_opt!(self.children[0].eval_int(ctx, row));
+        let p2 = try_opt!(self.children[1].eval_int(ctx, row));
+        Ok(Some(
+            Self::period_to_month(p1 as u64) as i64 - Self::period_to_month(p2 as u64) as i64,
+        ))
+    }
+
     #[inline]
     pub fn to_days(&self, ctx: &mut EvalContext, row: &[Datum]) -> Result<Option<i64>> {
         let t: Cow<'_, Time> = try_opt!(self.children[0].eval_time(ctx, row));
@@ -1339,6 +1385,51 @@ mod tests {
         let mut ctx = EvalContext::default();
         test_ok_case_zero_arg(&mut ctx, ScalarFuncSig::AddTimeDateTimeNull, Datum::Null);
         test_ok_case_zero_arg(&mut ctx, ScalarFuncSig::SubTimeDateTimeNull, Datum::Null);
+    }
+
+    #[test]
+    fn test_period_add() {
+        let cases = vec![
+            (2, 222, 201808),
+            (0, 222, 0),
+            (196802, 14, 196904),
+            (6901, 13, 207002),
+            (7001, 13, 197102),
+            (200212, 9223372036854775807, 200211),
+            (9223372036854775807, 0, 27201459511),
+            (9223372036854775807, 9223372036854775807, 27201459510),
+        ];
+        let mut ctx = EvalContext::default();
+        for (arg1, arg2, exp) in cases {
+            test_ok_case_two_arg(
+                &mut ctx,
+                ScalarFuncSig::PeriodAdd,
+                Datum::I64(arg1),
+                Datum::I64(arg2),
+                Datum::I64(exp),
+            );
+        }
+    }
+
+    #[test]
+    fn test_period_diff() {
+        let cases = vec![
+            (213002, 7010, 1912),
+            (213002, 215810, -344),
+            (2202, 9601, 313),
+            (202202, 9601, 313),
+            (200806, 6907, -733),
+        ];
+        let mut ctx = EvalContext::default();
+        for (arg1, arg2, exp) in cases {
+            test_ok_case_two_arg(
+                &mut ctx,
+                ScalarFuncSig::PeriodDiff,
+                Datum::I64(arg1),
+                Datum::I64(arg2),
+                Datum::I64(exp),
+            );
+        }
     }
 
     #[test]
