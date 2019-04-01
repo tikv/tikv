36aca3e2be35314f4926b6e52caf6c18406e365a
MyonKeminta <MyonKeminta@users.noreply.github.com>
2019-04-01 15:20:00 +0800

    Add bench for rollback
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- benches/hierarchy/mvcc/mod.rs
+++ benches/hierarchy/mvcc/mod.rs
@@ -20,6 +20,36 @@ use tikv::storage::{Key, Mutation, Options};
 
 use super::{BenchConfig, EngineFactory, DEFAULT_ITERATIONS, DEFAULT_KV_GENERATOR_SEED};
 
+fn setup_prewrite<E, F>(engine: &E, config: &BenchConfig<F>, start_ts: u64) -> (E::Snap, Vec<Key>)
+where
+    E: Engine,
+    F: EngineFactory<E>,
+{
+    let ctx = Context::new();
+    let snapshot = engine.snapshot(&ctx).unwrap();
+    let mut txn = MvccTxn::new(snapshot, start_ts, true).unwrap();
+
+    let kvs = KvGenerator::with_seed(
+        config.key_length,
+        config.value_length,
+        DEFAULT_KV_GENERATOR_SEED,
+    )
+    .generate(DEFAULT_ITERATIONS);
+    for (k, v) in &kvs {
+        txn.prewrite(
+            Mutation::Put((Key::from_raw(&k), v.clone())),
+            &k.clone(),
+            &Options::default(),
+        )
+        .unwrap();
+    }
+    let modifies = txn.into_modifies();
+    let _ = engine.async_write(&ctx, modifies, Box::new(move |(_, _)| {}));
+    let keys: Vec<Key> = kvs.iter().map(|(k, _)| Key::from_raw(&k)).collect();
+    let snapshot = engine.snapshot(&ctx).unwrap();
+    (snapshot, keys)
+}
+
 fn mvcc_prewrite<E: Engine, F: EngineFactory<E>>(b: &mut Bencher, config: &BenchConfig<F>) {
     let engine = config.engine_factory.build();
     let ctx = Context::new();
@@ -49,29 +79,63 @@ fn mvcc_prewrite<E: Engine, F: EngineFactory<E>>(b: &mut Bencher, config: &Bench
 
 fn mvcc_commit<E: Engine, F: EngineFactory<E>>(b: &mut Bencher, config: &BenchConfig<F>) {
     let engine = config.engine_factory.build();
+    b.iter_with_setup(
+        || setup_prewrite(&engine, &config, 1),
+        |(snapshot, keys)| {
+            for key in keys {
+                let mut txn = MvccTxn::new(snapshot.clone(), 1, true).unwrap();
+                black_box(txn.commit(key, 2)).unwrap();
+            }
+        },
+    );
+}
+
+fn mvcc_rollback_prewrote<E: Engine, F: EngineFactory<E>>(
+    b: &mut Bencher,
+    config: &BenchConfig<F>,
+) {
+    let engine = config.engine_factory.build();
+    b.iter_with_setup(
+        || setup_prewrite(&engine, &config, 1),
+        |(snapshot, keys)| {
+            for key in keys {
+                let mut txn = MvccTxn::new(snapshot.clone(), 1, true).unwrap();
+                black_box(txn.rollback(key)).unwrap();
+            }
+        },
+    )
+}
+
+fn mvcc_rollback_conflict<E: Engine, F: EngineFactory<E>>(
+    b: &mut Bencher,
+    config: &BenchConfig<F>,
+) {
+    let engine = config.engine_factory.build();
+    b.iter_with_setup(
+        || setup_prewrite(&engine, &config, 2),
+        |(snapshot, keys)| {
+            for key in keys {
+                let mut txn = MvccTxn::new(snapshot.clone(), 1, true).unwrap();
+                black_box(txn.rollback(key)).unwrap();
+            }
+        },
+    )
+}
+
+fn mvcc_rollback_non_prewrote<E: Engine, F: EngineFactory<E>>(
+    b: &mut Bencher,
+    config: &BenchConfig<F>,
+) {
+    let engine = config.engine_factory.build();
     let ctx = Context::new();
-    let snapshot = engine.snapshot(&ctx).unwrap();
-    let option = Options::default();
     b.iter_with_setup(
         || {
-            let mut txn = MvccTxn::new(snapshot.clone(), 1, true).unwrap();
-
             let kvs = KvGenerator::with_seed(
                 config.key_length,
                 config.value_length,
                 DEFAULT_KV_GENERATOR_SEED,
             )
             .generate(DEFAULT_ITERATIONS);
-            for (k, v) in &kvs {
-                txn.prewrite(
-                    Mutation::Put((Key::from_raw(&k), v.clone())),
-                    &k.clone(),
-                    &option,
-                )
-                .unwrap();
-            }
-            let modifies = txn.into_modifies();
-            let _ = engine.async_write(&ctx, modifies, Box::new(move |(_, _)| {}));
             let keys: Vec<Key> = kvs.iter().map(|(k, _)| Key::from_raw(&k)).collect();
             let snapshot = engine.snapshot(&ctx).unwrap();
             (snapshot, keys)
@@ -79,10 +143,10 @@ fn mvcc_commit<E: Engine, F: EngineFactory<E>>(b: &mut Bencher, config: &BenchCo
         |(snapshot, keys)| {
             for key in keys {
                 let mut txn = MvccTxn::new(snapshot.clone(), 1, true).unwrap();
-                black_box(txn.commit(key, 1)).unwrap();
+                black_box(txn.rollback(key)).unwrap();
             }
         },
-    );
+    )
 }
 
 fn mvcc_reader_load_lock<E: Engine, F: EngineFactory<E>>(b: &mut Bencher, config: &BenchConfig<F>) {
@@ -158,6 +222,21 @@ fn mvcc_reader_seek_write<E: Engine, F: EngineFactory<E>>(
 pub fn bench_mvcc<E: Engine, F: EngineFactory<E>>(c: &mut Criterion, configs: &[BenchConfig<F>]) {
     c.bench_function_over_inputs("mvcc_prewrite", mvcc_prewrite, configs.to_owned());
     c.bench_function_over_inputs("mvcc_commit", mvcc_commit, configs.to_owned());
+    c.bench_function_over_inputs(
+        "mvcc_rollback_prewrote",
+        mvcc_rollback_prewrote,
+        configs.to_owned(),
+    );
+    c.bench_function_over_inputs(
+        "mvcc_rollback_conflict",
+        mvcc_rollback_conflict,
+        configs.to_owned(),
+    );
+    c.bench_function_over_inputs(
+        "mvcc_rollback_non_prewrote",
+        mvcc_rollback_non_prewrote,
+        configs.to_owned(),
+    );
     c.bench_function_over_inputs("mvcc_load_lock", mvcc_reader_load_lock, configs.to_owned());
     c.bench_function_over_inputs(
         "mvcc_seek_write",
