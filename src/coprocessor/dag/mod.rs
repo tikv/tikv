37ed967a5b090821d45f38e2a1796d43becaa2df
Breezewish <breezewish@pingcap.com>
2019-04-08 16:14:53 +0800

    Move executors into the same directory
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- src/coprocessor/dag/mod.rs
+++ src/coprocessor/dag/mod.rs
@@ -33,7 +33,7 @@
 //!
 //!   Obviously, this kind of executor must not be the first executor in the pipeline.
 
-pub mod batch_executor;
+pub mod batch;
 pub mod batch_handler;
 mod builder;
 pub mod executor;
