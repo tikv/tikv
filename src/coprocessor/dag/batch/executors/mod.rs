96bbcb744523113c56343da6054f9bbff21044ff
Shirly <AndreMouche@126.com>
2019-04-11 22:35:21 +0800

    Copr: implement batch_executor/limit (#4469)

--- src/coprocessor/dag/batch/executors/mod.rs
+++ src/coprocessor/dag/batch/executors/mod.rs
@@ -12,8 +12,10 @@
 // limitations under the License.
 
 mod index_scan_executor;
+mod limit;
 mod table_scan_executor;
 mod util;
 
 pub use self::index_scan_executor::BatchIndexScanExecutor;
+pub use self::limit::BatchLimitExecutor;
 pub use self::table_scan_executor::BatchTableScanExecutor;
