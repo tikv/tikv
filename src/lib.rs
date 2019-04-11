fa0e70df7d7e83bb222569d1ddf1b261b7f17aec
Brian Anderson <andersrb@gmail.com>
2019-04-11 02:33:31 +0000

    tikv: add simple crate docs
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
 // Copyright 2016 TiKV Project Authors. Licensed under Apache-2.0.
 
+//! TiKV - A distributed key/value database
+//!
+//! TiKV ("Ti" stands for Titanium) is an open source distributed
+//! transactional key-value database. Unlike other traditional NoSQL
+//! systems, TiKV not only provides classical Key-Value APIs, but also
+//! transactional APIs with ACID compliance. TiKV was originally
+//! created to complement [TiDB], a distributed HTAP database
+//! compatible with the MySQL protocol.
+//!
+//! [TiDB]: https://github.com/pingcap/tidb
+//!
+//! The design of TiKV is inspired by some great distributed systems
+//! from Google, such as BigTable, Spanner, and Percolator, and some
+//! of the latest achievements in academia in recent years, such as
+//! the Raft consensus algorithm.
+
 #![crate_type = "lib"]
 #![cfg_attr(test, feature(test))]
 #![recursion_limit = "200"]
