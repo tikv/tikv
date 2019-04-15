25d79e012fc771eb5b2a94eeec5c776449d85516
Ana Hobden <operator@hoverbear.org>
2019-04-15 13:43:24 -0700

    Fmt
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- src/pd/config.rs
+++ src/pd/config.rs
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::error::Error;
 use crate::util::config::ReadableDuration;
+use std::error::Error;
 
 /// The configuration for a PD Client.
 ///
