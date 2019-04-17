765149dc4bd72399afab454c7df66dd0d1608e80
youjiali1995 <zlwgx1023@gmail.com>
2019-04-17 14:18:57 +0800

    fuzz: add tests for new codecs
    Signed-off-by: youjiali1995 <zlwgx1023@gmail.com>

--- fuzz/targets/mod.rs
+++ fuzz/targets/mod.rs
@@ -9,7 +9,7 @@ use failure::Error;
 use std::io::Cursor;
 
 #[inline(always)]
-pub fn fuzz_codec_bytes(data: &[u8]) -> Result<(), Error> {
+pub fn fuzz_util_codec_bytes(data: &[u8]) -> Result<(), Error> {
     let _ = tikv_util::codec::bytes::encode_bytes(data);
     let _ = tikv_util::codec::bytes::encode_bytes_desc(data);
     let _ = tikv_util::codec::bytes::encoded_bytes_len(data, true);
@@ -18,7 +18,7 @@ pub fn fuzz_codec_bytes(data: &[u8]) -> Result<(), Error> {
 }
 
 #[inline(always)]
-pub fn fuzz_codec_number(data: &[u8]) -> Result<(), Error> {
+pub fn fuzz_util_codec_number(data: &[u8]) -> Result<(), Error> {
     use tikv_util::codec::number::NumberEncoder;
     {
         let mut cursor = Cursor::new(data);
@@ -196,4 +196,86 @@ pub fn fuzz_coprocessor_codec_time_from_u64(data: &[u8]) -> Result<(), Error> {
     let fsp = cursor.read_as_i8()?;
     let t = Time::from_packed_u64(u, time_type, fsp, &tz)?;
     fuzz_time(t, cursor)
+}
+
+#[inline(always)]
+pub fn fuzz_codec_bytes(data: &[u8]) -> Result<(), Error> {
+    use codec::MemComparableByteCodec;
+    let mut buf = vec![0; MemComparableByteCodec::encoded_len(data.len())];
+    let _ = MemComparableByteCodec::encode_all(data, buf.as_mut_slice());
+    let _ = MemComparableByteCodec::encode_all_desc(data, buf.as_mut_slice());
+    let _ = MemComparableByteCodec::try_decode_first(data, buf.as_mut_slice());
+    let _ = MemComparableByteCodec::try_decode_first_desc(data, buf.as_mut_slice());
+    let mut data = data.to_owned();
+    let _ = MemComparableByteCodec::try_decode_first_in_place(data.as_mut_slice());
+    let _ = MemComparableByteCodec::try_decode_first_in_place_desc(data.as_mut_slice());
+    Ok(())
+}
+
+#[inline(always)]
+pub fn fuzz_codec_number(data: &[u8]) -> Result<(), Error> {
+    use codec::NumberCodec;
+    {
+        let mut cursor = Cursor::new(data);
+        let n = cursor.read_as_u64()?;
+        let mut buf = vec![0; 8];
+        NumberCodec::encode_u64(buf.as_mut_slice(), n);
+        NumberCodec::encode_u64_le(buf.as_mut_slice(), n);
+        NumberCodec::encode_u64_desc(buf.as_mut_slice(), n);
+        NumberCodec::encode_var_u64(buf.as_mut_slice(), n);
+    }
+    {
+        let mut cursor = Cursor::new(data);
+        let n = cursor.read_as_i64()?;
+        let mut buf = vec![0; 8];
+        NumberCodec::encode_i64(buf.as_mut_slice(), n);
+        NumberCodec::encode_i64_le(buf.as_mut_slice(), n);
+        NumberCodec::encode_i64_desc(buf.as_mut_slice(), n);
+        NumberCodec::encode_var_i64(buf.as_mut_slice(), n);
+    }
+    {
+        let mut cursor = Cursor::new(data);
+        let n = cursor.read_as_f64()?;
+        let mut buf = vec![0; 8];
+        NumberCodec::encode_f64(buf.as_mut_slice(), n);
+        NumberCodec::encode_f64_le(buf.as_mut_slice(), n);
+        NumberCodec::encode_f64_desc(buf.as_mut_slice(), n);
+    }
+    {
+        let mut cursor = Cursor::new(data);
+        let n = cursor.read_as_u32()?;
+        let mut buf = vec![0; 4];
+        NumberCodec::encode_u32(buf.as_mut_slice(), n);
+        NumberCodec::encode_u32_le(buf.as_mut_slice(), n);
+    }
+    {
+        let mut cursor = Cursor::new(data);
+        let n = cursor.read_as_i32()?;
+        let mut buf = vec![0; 4];
+        NumberCodec::encode_i32_le(buf.as_mut_slice(), n);
+    }
+    {
+        let mut cursor = Cursor::new(data);
+        let n = cursor.read_as_u16()?;
+        let mut buf = vec![0; 2];
+        NumberCodec::encode_u16(buf.as_mut_slice(), n);
+        NumberCodec::encode_u16_le(buf.as_mut_slice(), n);
+    }
+    {
+        let _ = NumberCodec::decode_u64(data);
+        let _ = NumberCodec::decode_u64_desc(data);
+        let _ = NumberCodec::decode_u64_le(data);
+        let _ = NumberCodec::decode_i64(data);
+        let _ = NumberCodec::decode_i64_desc(data);
+        let _ = NumberCodec::decode_i64_le(data);
+        let _ = NumberCodec::decode_f64(data);
+        let _ = NumberCodec::decode_f64_desc(data);
+        let _ = NumberCodec::decode_f64_le(data);
+        let _ = NumberCodec::decode_u32(data);
+        let _ = NumberCodec::decode_u32_le(data);
+        let _ = NumberCodec::decode_i32_le(data);
+        let _ = NumberCodec::decode_u16(data);
+        let _ = NumberCodec::decode_u16_le(data);
+    }
+    Ok(())
 }
