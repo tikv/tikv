272ce391b92797fb07a943cd809ac04e4f09f706
Breezewish <breezewish@pingcap.com>
2019-04-25 17:53:10 +0800

    Allow unused
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- src/coprocessor/dag/rpn_expr/types/test_util.rs
+++ src/coprocessor/dag/rpn_expr/types/test_util.rs
@@ -1,5 +1,7 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
+#![allow(unused)]
+
 use tipb::expression::FieldType;
 
 use crate::coprocessor::codec::batch::LazyBatchColumnVec;
