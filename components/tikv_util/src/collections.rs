d41b94115438ec2d375600bf933ef2035d915d65
Yilin Chen <sticnarf@gmail.com>
2019-04-25 14:59:10 +0800

    util: Change to use std::collections::HashMap/HashSet
    The HashMap implementation in the latest Rust std points to hashbrown
    which we are using now. So it's useless to add hashbrown to our
    dependencies.
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- components/tikv_util/src/collections.rs
+++ components/tikv_util/src/collections.rs
@@ -1,6 +1,4 @@
 // Copyright 2017 TiKV Project Authors. Licensed under Apache-2.0.
 
-pub use std::collections::hash_map::Entry as HashMapEntry;
-
 pub use indexmap::map::Entry as OrderMapEntry;
 pub use indexmap::IndexMap as OrderMap;
