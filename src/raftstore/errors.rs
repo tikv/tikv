f502422c0b435f79b4ee320c441c99f2901de7a6
Brian Anderson <andersrb@gmail.com>
2019-04-26 18:05:07 +0800

    raftstore: Remove unused error type

--- src/raftstore/errors.rs
+++ src/raftstore/errors.rs
@@ -1,23 +1,3 @@
 // Copyright 2016 TiKV Project Authors. Licensed under Apache-2.0.
 
-use crate::pd;
-
 pub use raftstore2::errors::{Error, Result, DiscardReason, RAFTSTORE_IS_BUSY};
-
-quick_error! {
-    #[derive(Debug)]
-    pub enum Error2 {
-        Inner(err: raftstore2::errors::Error) {
-            from()
-            cause(err)
-            description(err.description())
-            display("Raftstore {}", err)
-        }
-        Pd(err: pd::Error) {
-            from()
-            cause(err)
-            description(err.description())
-            display("Pd {}", err)
-        }
-    }
-}
