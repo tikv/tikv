0f062cbfbcea0d242499774eea5247646ebf6f0b
Brian Anderson <andersrb@gmail.com>
2019-04-27 13:45:43 +0800

    pd: replace local imports with external

--- src/pd/pd.rs
+++ src/pd/pd.rs
@@ -24,7 +24,7 @@ use crate::raftstore::store::cmd_resp::new_error;
 use tikv_misc::store_util::is_epoch_stale;
 use tikv_misc::store_util::KeysInfoFormatter;
 use crate::raftstore::store::Callback;
-use crate::raftstore::store::StoreInfo;
+use tikv_misc::store_info::StoreInfo;
 use crate::raftstore::store::{CasualMessage, PeerMsg, RaftCommand, RaftRouter};
 use tikv_misc::flow_stats::FlowStatistics;
 use tikv_util::collections::HashMap;
