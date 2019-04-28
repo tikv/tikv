fcdf33bb6675a245ad7e5629deca5294b5b1c009
Brian Anderson <andersrb@gmail.com>
2019-04-28 16:04:28 +0800

    A few cleanup commits (#4513)
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,6 @@
 #![feature(fnbox)]
 #![feature(proc_macro_hygiene)]
 #![feature(specialization)]
-// Currently this raises some false positives, so we allow it:
-// https://github.com/rust-lang-nursery/rust-clippy/issues/2638
-#![allow(clippy::nonminimal_bool)]
 
 #[macro_use]
 extern crate bitflags;
@@ -51,7 +48,6 @@ extern crate vlog;
 extern crate tikv_util;
 #[cfg(test)]
 extern crate test;
-use grpcio as grpc;
 
 pub mod config;
 pub mod coprocessor;
@@ -60,5 +56,3 @@ pub mod pd;
 pub mod raftstore;
 pub mod server;
 pub mod storage;
-
-pub use crate::storage::Storage;
