5e2d9eb4604e32976ee8e7e630aa82bd3b36f04f
Yang Keao <keao.yang@yahoo.com>
2019-04-29 18:36:27 +0800

    server/status_server: Add new entry "/jeprof" to get jemalloc profile
    Add new entry "/jeprof" to status_server and start jemalloc profiler for
    several seconds (according to `seconds` in query), and return the profile.
    
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- src/server/status_server.rs
+++ src/server/status_server.rs
@@ -1,17 +1,21 @@
 // Copyright 2018 TiKV Project Authors. Licensed under Apache-2.0.
 
-use futures::future::{ok, FutureResult};
+use futures::future::ok;
 use futures::sync::oneshot::{Receiver, Sender};
 use futures::{self, Future};
 use hyper::service::service_fn;
 use hyper::{self, Body, Method, Request, Response, Server, StatusCode};
+use tempdir::TempDir;
 use tokio_threadpool::{Builder, ThreadPool};
 
 use std::net::SocketAddr;
 use std::str::FromStr;
 
 use super::Result;
+use tikv_alloc;
+use tikv_util::collections::HashMap;
 use tikv_util::metrics::dump;
+use tikv_util::timer::GLOBAL_TIMER_HANDLE;
 
 pub struct StatusServer {
     thread_pool: ThreadPool,
@@ -48,19 +52,92 @@ impl StatusServer {
         let builder = Server::try_bind(&addr)?;
 
         // Create a status service.
-        let service = |req: Request<Body>| -> FutureResult<Response<Body>, hyper::Error> {
-            let mut response = Response::new(Body::empty());
-
+        let service = |req: Request<Body>| -> Box<dyn Future<Item=Response<Body>, Error=hyper::Error> + Send> {
             match (req.method(), req.uri().path()) {
                 (&Method::GET, "/metrics") => {
-                    *response.body_mut() = Body::from(dump());
+                    let response = Response::builder().body(Body::from(dump())).unwrap();
+                    Box::new(ok(response))
                 }
-                (&Method::GET, "/status") => return ok(response),
+                (&Method::GET, "/jeprof") => {
+                    let url = url::Url::parse(&format!("http://host{}", req.uri().to_string())).unwrap(); // Add scheme and host to parse query
+                    let query_pairs: HashMap<_, _> = url.query_pairs().collect();
+                    let seconds: u64 = match query_pairs.get("seconds") {
+                        Some(val) => {
+                            match val.parse() {
+                                Ok(val) => val,
+                                Err(_) => {
+                                    let response = Response::builder().status(StatusCode::BAD_REQUEST).body(Body::empty()).unwrap();
+                                    return Box::new(ok(response));
+                                }
+                            }
+                        }
+                        None => {
+                            let response = Response::builder().status(StatusCode::BAD_REQUEST).body(Body::empty()).unwrap();
+                            return Box::new(ok(response));
+                        }
+                    };
+                    if let Err(e) = tikv_alloc::activate_prof() {
+                        match e {
+                            tikv_alloc::error::ProfError::MemProfilingNotEnabled => {
+                                let response = Response::builder().status(StatusCode::NOT_FOUND).body(Body::from("feature mem-profiling is not enabled")).unwrap();
+                                return Box::new(ok(response));
+                            }
+                            tikv_alloc::error::ProfError::JemallocError(e) => {
+                                let response = Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::from(format!("jemalloc error {}", e))).unwrap();
+                                return Box::new(ok(response));
+                            }
+                            tikv_alloc::error::ProfError::JemallocNotEnabled => {
+                                let response = Response::builder().status(StatusCode::NOT_FOUND).body(Body::from("feature jemalloc is not enabled")).unwrap();
+                                return Box::new(ok(response));
+                            }
+                        }
+                    }
+                    info!("Start profiling {} seconds", seconds);
+
+                    let timer = GLOBAL_TIMER_HANDLE.clone();
+                    Box::new(timer.delay(std::time::Instant::now() + std::time::Duration::from_secs(seconds))
+                        .then(|_| {
+                            if let Err(e) = tikv_alloc::deactivate_prof() {
+                                match e {
+                                    tikv_alloc::error::ProfError::JemallocError(e) => {
+                                        error!("jemalloc error {}", e); // TODO: return error through http
+                                    }
+                                    _ => {
+                                        unreachable!()
+                                    }
+                                }
+                            }
+
+                            let tmp_dir = TempDir::new("").unwrap();
+                            let os_path = tmp_dir.path().join("tikv_dump_profile").into_os_string();
+                            let path = os_path.into_string().unwrap();
+
+                            tikv_alloc::dump_prof(Some(&path));
+                            tokio_fs::file::File::open(path)
+                                .and_then(|file| {
+                                    let buf: Vec<u8> = Vec::new();
+                                    tokio_io::io::read_to_end(file, buf)
+                                })
+                                .and_then(move |(_, buf)| {
+                                    let response = Response::builder().body(buf.into()).unwrap();
+                                    drop(tmp_dir); // Drop here manually to extend life of tmp_dir.
+                                    ok(response)
+                                })
+                        })
+                        .or_else(|_| {
+                            let response = Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::empty()).unwrap();
+                            ok(response)
+                        }))
+                }
+                (&Method::GET, "/status") => {
+                    let response = Response::builder().body(Body::empty()).unwrap();
+                    Box::new(ok(response))
+                },
                 _ => {
-                    *response.status_mut() = StatusCode::NOT_FOUND;
+                    let response = Response::builder().status(StatusCode::NOT_FOUND).body(Body::empty()).unwrap();
+                    Box::new(ok(response))
                 }
-            };
-            ok(response)
+            }
         };
 
         // Start to serve.
