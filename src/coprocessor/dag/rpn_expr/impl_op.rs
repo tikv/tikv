64dcb35ac84dc949988b174731ce7a552e45ffc3
Breezewish <breezewish@pingcap.com>
2019-04-29 18:49:38 +0800

    Add logical or
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- src/coprocessor/dag/rpn_expr/impl_op.rs
+++ src/coprocessor/dag/rpn_expr/impl_op.rs
@@ -26,9 +26,41 @@ impl RpnFnLogicalAnd {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub struct RpnFnLogicalOr;
+
+impl_template_fn! { 2 arg @ RpnFnLogicalOr }
+
+impl RpnFnLogicalOr {
+    #[inline]
+    fn call(
+        _ctx: &mut EvalContext,
+        _payload: RpnFnCallPayload<'_>,
+        arg0: &Option<i64>,
+        arg1: &Option<i64>,
+    ) -> Result<Option<i64>> {
+        // Rule 1. As long as there is `Some(x)` where x is not 0, the result is `1`.
+        // Rule 2. As long as there is `None`, the result is `None`.
+        // Rule 3. For the rest, the result is `0`.
+        Ok(match arg0 {
+            None => match arg1 {
+                None => None,
+                Some(0) => None,
+                Some(_) => Some(1),
+            },
+            Some(0) => match arg1 {
+                None => None,
+                Some(0) => Some(0),
+                Some(_) => Some(1),
+            },
+            Some(_) => Some(1),
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use tipb::expression::ScalarFuncSig;
 
     use crate::coprocessor::dag::rpn_expr::types::test_util::RpnFnScalarEvaluator;
 
@@ -46,7 +78,27 @@ mod tests {
             let output = RpnFnScalarEvaluator::new()
                 .push_param(arg0)
                 .push_param(arg1)
-                .evaluate(RpnFnLogicalAnd)
+                .evaluate(ScalarFuncSig::LogicalAnd)
+                .unwrap();
+            assert_eq!(output, expect_output);
+        }
+    }
+
+    #[test]
+    fn test_logical_or() {
+        let test_cases = vec![
+            (Some(1), Some(1), Some(1)),
+            (Some(1), Some(0), Some(1)),
+            (Some(0), Some(0), Some(0)),
+            (Some(2), Some(-1), Some(1)),
+            (Some(1), None, Some(1)),
+            (None, Some(0), None),
+        ];
+        for (arg0, arg1, expect_output) in test_cases {
+            let output = RpnFnScalarEvaluator::new()
+                .push_param(arg0)
+                .push_param(arg1)
+                .evaluate(ScalarFuncSig::LogicalOr)
                 .unwrap();
             assert_eq!(output, expect_output);
         }
