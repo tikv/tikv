0374d1e6551068fc9244021e011223776d59b15c
Breezewish <breezewish@pingcap.com>
2019-05-05 15:25:40 +0800

    Introduce cop_codegen
    Signed-off-by: Breezewish <breezewish@pingcap.com>

---
+++ components/cop_codegen/src/rpn_function.rs
@@ -0,0 +1,65 @@
+// Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
+
+#[derive(FromDeriveInput, Debug)]
+#[darling(attributes(rpn_function))]
+pub struct RpnFunctionOpts {
+    ident: syn::Ident,
+    generics: syn::Generics,
+    #[darling(default)]
+    args: usize,
+}
+
+impl RpnFunctionOpts {
+    pub fn generate_tokens(self) -> proc_macro2::TokenStream {
+        let ident = &self.ident;
+        let name = ident.to_string();
+        let args = self.args;
+        let helper_fn_name = match self.args {
+            0 => "eval_0_arg",
+            1 => "eval_1_arg",
+            2 => "eval_2_args",
+            3 => "eval_3_args",
+            _ => panic!("Unsupported argument length {}", self.args),
+        };
+        let helper_fn_ident =
+            proc_macro2::Ident::new(helper_fn_name, proc_macro2::Span::call_site());
+
+        let (impl_generics, ty_generics, where_clause) = self.generics.split_for_impl();
+        quote! {
+            impl #impl_generics tikv_util::AssertCopy for #ident #ty_generics #where_clause {}
+
+            impl #impl_generics crate::coprocessor::dag::rpn_expr::RpnFunction for #ident #ty_generics #where_clause {
+                #[inline]
+                fn name(&self) -> &'static str {
+                    #name
+                }
+
+                #[inline]
+                fn args_len(&self) -> usize {
+                    #args
+                }
+
+                #[inline]
+                fn eval(
+                    &self,
+                    rows: usize,
+                    context: &mut crate::coprocessor::dag::expr::EvalContext,
+                    payload: crate::coprocessor::dag::rpn_expr::types::RpnFnCallPayload<'_>,
+                ) -> crate::coprocessor::Result<crate::coprocessor::codec::data_type::VectorValue>
+                {
+                    crate::coprocessor::dag::rpn_expr::function::Helper::#helper_fn_ident(
+                        rows,
+                        Self::call,
+                        context,
+                        payload,
+                    )
+                }
+
+                #[inline]
+                fn box_clone(&self) -> Box<dyn crate::coprocessor::dag::rpn_expr::RpnFunction> {
+                    Box::new(*self)
+                }
+            }
+        }
+    }
+}
--- src/coprocessor/dag/rpn_expr/types/expr_eval.rs
+++ src/coprocessor/dag/rpn_expr/types/expr_eval.rs
@@ -132,14 +132,16 @@ impl RpnExpression {
 
         let mut stack = Vec::with_capacity(self.len());
 
-        // First loop: ensure referred columns are decoded.
+        // We iterate two times. The first time we decode all referred columns. The second time
+        // we evaluate. This is to make Rust's borrow checker happy because there will be
+        // mutable reference during the first iteration and we can't keep these references.
+
         for node in self.as_ref() {
             if let RpnExpressionNode::ColumnRef { ref offset, .. } = node {
                 columns.ensure_column_decoded(*offset, &context.cfg.tz, &schema[*offset])?;
             }
         }
 
-        // Second loop: evaluate RPN expressions.
         for node in self.as_ref() {
             match node {
                 RpnExpressionNode::Constant {
@@ -232,6 +234,7 @@ mod tests {
 
     use super::*;
 
+    use cop_codegen::RpnFunction;
     use cop_datatype::{EvalType, FieldTypeAccessor, FieldTypeTp};
     use tipb::expression::FieldType;
 
@@ -351,10 +354,8 @@ mod tests {
     /// Single function call node (i.e. nullary function)
     #[test]
     fn test_eval_single_fn_call_node() {
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
         struct FnFoo;
-
-        impl_template_fn! { 0 arg @ FnFoo }
 
         impl FnFoo {
             fn call(_ctx: &mut EvalContext, _payload: RpnFnCallPayload<'_>) -> Result<Option<i64>> {
@@ -381,10 +382,9 @@ mod tests {
     #[test]
     fn test_eval_unary_function_scalar() {
         /// FnFoo(v) performs v * 2.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 1)]
         struct FnFoo;
-
-        impl_template_fn! { 1 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -416,10 +416,9 @@ mod tests {
     #[test]
     fn test_eval_unary_function_vector() {
         /// FnFoo(v) performs v + 5.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 1)]
         struct FnFoo;
-
-        impl_template_fn! { 1 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -464,10 +463,9 @@ mod tests {
     #[test]
     fn test_eval_unary_function_raw_column() {
         /// FnFoo(v) performs v + 5.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 1)]
         struct FnFoo;
-
-        impl_template_fn! { 1 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -522,10 +520,9 @@ mod tests {
     #[test]
     fn test_eval_binary_function_scalar_scalar() {
         /// FnFoo(v) performs v1 + float(v2) - 1.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 2)]
         struct FnFoo;
-
-        impl_template_fn! { 2 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -559,10 +556,9 @@ mod tests {
     #[test]
     fn test_eval_binary_function_vector_scalar() {
         /// FnFoo(v) performs v1 - v2.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 2)]
         struct FnFoo;
-
-        impl_template_fn! { 2 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -609,10 +605,9 @@ mod tests {
     #[test]
     fn test_eval_binary_function_scalar_vector() {
         /// FnFoo(v) performs v1 - float(v2).
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 2)]
         struct FnFoo;
-
-        impl_template_fn! { 2 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -659,10 +654,9 @@ mod tests {
     #[test]
     fn test_eval_binary_function_vector_vector() {
         /// FnFoo(v) performs int(v1*2.5 - float(v2)*3.5).
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 2)]
         struct FnFoo;
-
-        impl_template_fn! { 2 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -729,10 +723,9 @@ mod tests {
     #[test]
     fn test_eval_binary_function_raw_column() {
         /// FnFoo(v1, v2) performs v1 * v2.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 2)]
         struct FnFoo;
-
-        impl_template_fn! { 2 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -796,10 +789,9 @@ mod tests {
     #[test]
     fn test_eval_ternary_function() {
         /// FnFoo(v) performs v1 - v2 * v3.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 3)]
         struct FnFoo;
-
-        impl_template_fn! { 3 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -854,10 +846,9 @@ mod tests {
     #[test]
     fn test_eval_comprehensive() {
         /// FnA(v1, v2, v3) performs v1 * v2 - v3.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 3)]
         struct FnA;
-
-        impl_template_fn! { 3 arg @ FnA }
 
         impl FnA {
             fn call(
@@ -872,10 +863,8 @@ mod tests {
         }
 
         /// FnB() returns 42.0.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
         struct FnB;
-
-        impl_template_fn! { 0 arg @ FnB }
 
         impl FnB {
             fn call(_ctx: &mut EvalContext, _payload: RpnFnCallPayload<'_>) -> Result<Option<f64>> {
@@ -884,10 +873,9 @@ mod tests {
         }
 
         /// FnC(v1, v2) performs float(v2 - v1).
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 2)]
         struct FnC;
-
-        impl_template_fn! { 2 arg @ FnC }
 
         impl FnC {
             fn call(
@@ -901,10 +889,9 @@ mod tests {
         }
 
         /// FnD(v1, v2) performs v1 + v2 * 2.
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 2)]
         struct FnD;
-
-        impl_template_fn! { 2 arg @ FnD }
 
         impl FnD {
             fn call(
@@ -983,10 +970,9 @@ mod tests {
     /// Unary function, but supplied zero arguments. Should panic.
     #[test]
     fn test_eval_fail_1() {
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 1)]
         struct FnFoo;
-
-        impl_template_fn! { 1 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -1014,10 +1000,9 @@ mod tests {
     fn test_eval_fail_2() {
         /// FnFoo(v) performs v * 2.
 
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 1)]
         struct FnFoo;
-
-        impl_template_fn! { 1 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -1050,10 +1035,9 @@ mod tests {
     fn test_eval_fail_3() {
         // Expects real argument, receives int argument.
 
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 1)]
         struct FnFoo;
-
-        impl_template_fn! { 1 arg @ FnFoo }
 
         impl FnFoo {
             fn call(
@@ -1095,10 +1079,9 @@ mod tests {
         //      )
 
         /// FnA(a: int, b: float, c: int) performs: float(a) - b * float(c)
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 3)]
         struct FnA;
-
-        impl_template_fn! { 3 arg @ FnA }
 
         impl FnA {
             fn call(
@@ -1113,10 +1096,9 @@ mod tests {
         }
 
         /// FnB(a: float, b: int) performs: a * (float(b) - 1.5)
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 2)]
         struct FnB;
-
-        impl_template_fn! { 2 arg @ FnB }
 
         impl FnB {
             fn call(
@@ -1130,10 +1112,8 @@ mod tests {
         }
 
         /// FnC() returns: int(42)
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
         struct FnC;
-
-        impl_template_fn! { 0 arg @ FnC }
 
         impl FnC {
             fn call(_ctx: &mut EvalContext, _payload: RpnFnCallPayload<'_>) -> Result<Option<i64>> {
@@ -1142,10 +1122,9 @@ mod tests {
         }
 
         /// FnD(a: float) performs: int(a)
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Debug, Clone, Copy, RpnFunction)]
+        #[rpn_function(args = 1)]
         struct FnD;
-
-        impl_template_fn! { 1 arg @ FnD }
 
         impl FnD {
             fn call(
