4277ce55d3649cf3cbd08f8a2a1ecc7aa053c0ee
Breezewish <breezewish@pingcap.com>
2019-05-05 16:14:25 +0800

    Add compare functions for other data types
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- src/coprocessor/dag/rpn_expr/types/expr_eval.rs
+++ src/coprocessor/dag/rpn_expr/types/expr_eval.rs
@@ -1136,7 +1136,7 @@ mod tests {
             }
         }
 
-        fn fn_mapper(value: ScalarFuncSig) -> Result<Box<dyn RpnFunction>> {
+        fn fn_mapper(value: ScalarFuncSig, _children: &[Expr]) -> Result<Box<dyn RpnFunction>> {
             // FnA: CastIntAsInt
             // FnB: CastIntAsReal
             // FnC: CastIntAsString
