232e189b04fe822ac0a308fabf9d807602fcaece
Dr.Who <example@gmail.com>
2019-07-04 19:47:45 +0800

    zhouyuanhui@pingcap.com
    Signed-off-by: Dr.Who <example@gmail.com>

--- tests/integrations/server/raft_client.rs
+++ tests/integrations/server/raft_client.rs
@@ -210,3 +210,90 @@ fn test_batch_raft_server_drop() {
     }
  pool.shutdown().wait().unwrap();
 }
+
+#[test]
+fn test_batch_raft_network_isolation() {
+    #[derive(Clone)]
+    struct MockKvForRaft(Arc<AtomicUsize>);
+
+    impl MockKvService for MockKvForRaft {
+        fn raft(
+            &mut self,
+            ctx: RpcContext<'_>,
+            stream: RequestStream<RaftMessage>,
+            sink: ClientStreamingSink<Done>,
+        ) {
+            let counter = Arc::clone(&self.0);
+            ctx.spawn(
+                stream
+                    .for_each(move |_| {
+                        counter.fetch_add(1, Ordering::SeqCst);
+                        Ok(())
+                    })
+                    .map_err(|_| drop(sink)),
+            );
+        }
+
+        fn batch_raft(
+            &mut self,
+            ctx: RpcContext<'_>,
+            stream: RequestStream<BatchRaftMessage>,
+            sink: ClientStreamingSink<Done>,
+        ) {
+            // let status = RpcStatus::new(RpcStatusCode::Unimplemented, None);
+            // ctx.spawn(sink.fail(status).map_err(|_| ()));
+
+            let counter = Arc::clone(&self.0);
+            ctx.spawn(
+                stream
+                    .for_each(move |msgs| {
+                        let len = msgs.msgs.len();
+                        counter.fetch_add(len, Ordering::SeqCst);
+                        Ok(())
+                    })
+                    .map_err(|_| drop(sink)),
+            );
+        }
+    }
+
+    let pool = tokio_threadpool::Builder::new().pool_size(1).build();
+    let mut raft_client = get_raft_client(&pool);
+    let counter = Arc::new(AtomicUsize::new(0));
+
+    // Try to bind the mock server on a TCP port, and then do test.
+    for i in 0..100 {
+        let kv_service = MockKv(MockKvForRaft(Arc::clone(&counter)));
+        let port = 60000 + i;
+        let mut mock_server = match mock_kv_service(kv_service, "localhost", port) {
+            Ok(s) => s,
+            Err(_) => continue,
+        };
+        println!("port is {}",port);
+        mock_server.start();
+
+        let addr = format!("localhost:{}", port);
+
+        (0..50).for_each(|_| {
+            raft_client.send(1, &addr, RaftMessage::new()).unwrap();
+            thread::sleep(time::Duration::from_millis(10));
+            raft_client.flush();
+        });
+
+        println!("counter {:?}",counter.load(Ordering::SeqCst));
+        println!("please close the port {}",port);
+        // Write 60 seconds
+        // Shut down the port during this time, simulate network isolation
+        thread::sleep(time::Duration::from_secs(60));
+        println!("work go on"); 
+        (50..100).for_each(|_| {
+             raft_client.send(1, &addr, RaftMessage::new()).unwrap(); 
+            thread::sleep(time::Duration::from_millis(10));
+            raft_client.flush();
+        }); 
+
+        // Counter must be 50.
+        assert!(counter.load(Ordering::SeqCst) > 49);
+        break;
+    }
+    pool.shutdown().wait().unwrap();
+}
