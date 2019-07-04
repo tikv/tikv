855cda867eae4aea3b5840d817f1769187eaf2db
mapleFU <1506118561@qq.com>
2019-07-04 10:14:25 +0800

    [FMT] tikv/coprocessor: Using make format and clippy to check and format the file
    Signed-off-by: mapleFU <1506118561@qq.com>

--- src/coprocessor/dag/rpn_expr/impl_math.rs
+++ src/coprocessor/dag/rpn_expr/impl_math.rs
@@ -114,7 +114,13 @@ mod tests {
                 .push_param(arg.clone())
                 .evaluate(ScalarFuncSig::AbsDecimal)
                 .unwrap();
-            assert_eq!(output, expect_output, "{:?} {:?}", arg, ScalarFuncSig::AbsDecimal);
+            assert_eq!(
+                output,
+                expect_output,
+                "{:?} {:?}",
+                arg,
+                ScalarFuncSig::AbsDecimal
+            );
         }
     }
 }
