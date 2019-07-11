b0f33c1fc4f0d31a07f45e1f080dc050f37d58d3
5kbpers <tangminghua@pingcap.com>
2019-07-11 10:45:32 +0800

    fix test_wait_for_apply_index
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- tests/failpoints/cases/test_follower_read.rs
+++ tests/failpoints/cases/test_follower_read.rs
@@ -1,12 +1,15 @@
 use std::sync::Arc;
 
 use fail;
-
+use kvproto::kvrpcpb::Context;
 use test_raftstore::*;
+use tikv::storage::kv::*;
+use tikv::storage::Key;
+use tikv_util::HandyRwLock;
 
 #[test]
 fn test_wait_for_apply_index() {
-    let mut cluster = new_node_cluster(0, 3);
+    let mut cluster = new_server_cluster(0, 3);
 
     // Increase the election tick to make this test case running reliably.
     configure_for_lease_read(&mut cluster, Some(50), Some(10_000));
@@ -21,20 +24,35 @@ fn test_wait_for_apply_index() {
     cluster.pd_client.must_add_peer(r1, p3.clone());
     must_get_equal(&cluster.get_engine(3), b"k0", b"v0");
 
-    let r1 = cluster.get_region(b"k1");
-    // Ensure peer 3 is not leader to use fail point 'on_raft_ready'
-    if cluster.leader_of_region(r1.get_id()) == Some(p3) {
-        cluster.must_transfer_leader(r1.get_id(), p2);
-    }
+    let r1 = cluster.get_region(b"k0");
+    cluster.must_transfer_leader(r1.get_id(), p2.clone());
+
+    let mut ctx = Context::new();
+    ctx.set_region_id(r1.get_id());
+    ctx.set_region_epoch(r1.get_region_epoch().clone());
+    ctx.set_follower_read(true);
 
     fail::cfg("on_raft_ready", "pause").unwrap();
     cluster.must_put(b"k1", b"v1");
     cluster.must_put(b"k2", b"v2");
     cluster.must_put(b"k3", b"v3");
     cluster.must_put(b"k4", b"v4");
-    cluster.must_put(b"k5", b"v5");
+    must_get_equal(&cluster.get_engine(2), b"k4", b"v4");
     fail::cfg("on_raft_ready", "off").unwrap();
-    // Peer 3 does not apply the value of 'k5' right now
-    must_get_none(&cluster.get_engine(3), b"k5");
-    assert_eq!(cluster.must_get(b"k5").unwrap(), b"v5");
+    // Peer 3 does not apply the value of 'k4' right now
+    let p3_storage = cluster.sim.rl().storages[&p3.get_id()].clone();
+    ctx.set_peer(p3.clone());
+    must_get_none(&cluster.get_engine(3), b"k4");
+    assert_has(&ctx, &p3_storage, b"k4", b"v4");
+}
+
+fn assert_has<E: Engine>(ctx: &Context, engine: &E, key: &[u8], value: &[u8]) {
+    let snapshot = engine.snapshot(ctx).unwrap();
+    assert_eq!(
+        snapshot
+            .get(&Key::from_encoded_slice(key))
+            .unwrap()
+            .unwrap(),
+        value
+    );
 }
