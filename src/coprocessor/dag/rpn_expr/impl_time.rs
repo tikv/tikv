61c3cc23949feeea4286d510a3911751972cf153
mapleFU <1506118561@qq.com>
2019-07-16 16:32:38 +0800

    [ADD] tikv/coprocessor: migrate implement for `date_format` from coprocessor::dag::expr to coprocessor::dag::rpn_expr
    Signed-off-by: mapleFU <1506118561@qq.com>

--- src/coprocessor/dag/rpn_expr/impl_time.rs
+++ src/coprocessor/dag/rpn_expr/impl_time.rs
@@ -1,11 +1,9 @@
-use std::borrow::Cow;
-
 use cop_codegen::rpn_fn;
 
 use super::super::expr::EvalContext;
 
 use crate::coprocessor::codec::data_type::*;
-use crate::coprocessor::codec::{self, Error};
+use crate::coprocessor::codec::Error;
 use crate::coprocessor::Result;
 
 #[rpn_fn(capture = [ctx])]
@@ -15,7 +13,24 @@ pub fn date_format(
     t: &Option<DateTime>,
     layout: &Option<Bytes>,
 ) -> Result<Option<Bytes>> {
-    unimplemented!()
+    use std::str::from_utf8;
+
+    if t.is_none() || layout.is_none() {
+        return Ok(None);
+    }
+    let (t, layout) = (t.as_ref().unwrap(), layout.as_ref().unwrap());
+    if t.invalid_zero() {
+        return ctx
+            .handle_invalid_time_error(Error::incorrect_datetime_value(&format!("{}", t)))
+            .map(|_| Ok(None))?;
+    }
+
+    let t = t.date_format(from_utf8(layout.as_slice()).map_err(Error::Encoding)?);
+    if let Err(err) = t {
+        return ctx.handle_invalid_time_error(err).map(|_| Ok(None))?;
+    }
+
+    Ok(Some(t.unwrap().into_bytes()))
 }
 
 #[cfg(test)]
