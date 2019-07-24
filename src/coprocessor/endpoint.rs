435724623c72168206a6214009d2f9d7661f9abf
Wenxuan <breezewish@pingcap.com>
2019-07-24 10:46:25 +0800

    Separate coprocessor statistics interfaces (#5121)
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- src/coprocessor/endpoint.rs
+++ src/coprocessor/endpoint.rs
@@ -19,7 +19,6 @@ use crate::storage::kv::with_tls_engine;
 use crate::storage::{self, Engine, SnapshotStore};
 use tikv_util::Either;
 
-use crate::coprocessor::dag::executor::ExecutorMetrics;
 use crate::coprocessor::metrics::*;
 use crate::coprocessor::tracker::Tracker;
 use crate::coprocessor::*;
@@ -241,13 +240,10 @@ impl<E: Engine> Endpoint<E> {
                 // There might be errors when handling requests. In this case, we still need its
                 // execution metrics.
                 let result = handler.handle_request();
-                let exec_metrics = {
-                    let mut metrics = ExecutorMetrics::default();
-                    handler.collect_metrics_into(&mut metrics);
-                    metrics
-                };
+                let mut storage_stats = Statistics::default();
+                handler.collect_scan_statistics(&mut storage_stats);
 
-                tracker.on_finish_item(Some(exec_metrics));
+                tracker.on_finish_item(Some(storage_stats));
                 let exec_details = tracker.get_item_exec_details();
 
                 tracker.on_finish_all_items();
@@ -356,13 +352,10 @@ impl<E: Engine> Endpoint<E> {
                             tracker.on_begin_item();
 
                             let result = handler.handle_streaming_request();
-                            let exec_metrics = {
-                                let mut metrics = ExecutorMetrics::default();
-                                handler.collect_metrics_into(&mut metrics);
-                                metrics
-                            };
+                            let mut storage_stats = Statistics::default();
+                            handler.collect_scan_statistics(&mut storage_stats);
 
-                            tracker.on_finish_item(Some(exec_metrics));
+                            tracker.on_finish_item(Some(storage_stats));
                             let exec_details = tracker.get_item_exec_details();
 
                             let (mut resp, finished) = match result {
