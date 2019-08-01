45c32a36e4644742fb5c776ab977c60be37cde94
Iosmanthus Teng <myosmanthustree@gmail.com>
2019-08-01 16:23:05 +0800

    Add `evaluate_ctx` which returns EvaluContext
    Signed-off-by: Iosmanthus Teng <myosmanthustree@gmail.com>

--- src/coprocessor/dag/rpn_expr/types/test_util.rs
+++ src/coprocessor/dag/rpn_expr/types/test_util.rs
@@ -73,7 +73,11 @@ impl RpnFnScalarEvaluator {
     }
 
     /// Evaluates the given function by using collected parameters.
-    pub fn evaluate<T: Evaluable>(self, sig: ScalarFuncSig) -> Result<Option<T>> {
+    /// Then return the evaluation result and the inner `EvalContext`
+    pub fn evaluate_ctx<T: Evaluable>(
+        self,
+        sig: ScalarFuncSig,
+    ) -> (EvalContext, Result<Option<T>>) {
         let return_field_type = match self.return_field_type {
             Some(ft) => ft,
             None => T::EVAL_TYPE.into_certain_field_type_tp_for_test().into(),
@@ -102,14 +106,25 @@ impl RpnFnScalarEvaluator {
             .build();
 
         let mut columns = LazyBatchColumnVec::empty();
-        let ret = expr.eval(&mut context, &[], &mut columns, &[0], 1)?;
-        match ret {
-            // Only used in tests, so clone is fine.
-            RpnStackNode::Scalar { value, .. } => Ok(T::borrow_scalar_value(value).clone()),
-            RpnStackNode::Vector { value, .. } => {
-                assert_eq!(value.as_ref().len(), 1);
-                Ok(T::borrow_vector_value(value.as_ref())[0].clone())
-            }
-        }
+        let ret = expr.eval(&mut context, &[], &mut columns, &[0], 1);
+
+        (
+            context,
+            ret.map(|ret| {
+                match ret {
+                    // Only used in tests, so clone is fine.
+                    RpnStackNode::Scalar { value, .. } => T::borrow_scalar_value(value).clone(),
+                    RpnStackNode::Vector { value, .. } => {
+                        assert_eq!(value.as_ref().len(), 1);
+                        T::borrow_vector_value(value.as_ref())[0].clone()
+                    }
+                }
+            }),
+        )
+    }
+
+    /// Evaluates the given function by using collected parameters.
+    pub fn evaluate<T: Evaluable>(self, sig: ScalarFuncSig) -> Result<Option<T>> {
+        self.evaluate_ctx(sig).1
     }
 }
