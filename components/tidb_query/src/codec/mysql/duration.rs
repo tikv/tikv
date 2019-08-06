595dc4bec6d130cb1f6cb104b23d41022fbe8930
Iosmanthus Teng <myosmanthustree@gmail.com>
2019-08-06 18:20:53 +0800

    Change error type return by `Duration::*`
    Signed-off-by: Iosmanthus Teng <myosmanthustree@gmail.com>

--- components/tidb_query/src/codec/mysql/duration.rs
+++ components/tidb_query/src/codec/mysql/duration.rs
@@ -10,6 +10,7 @@ use tikv_util::codec::BytesSlice;
 
 use super::{check_fsp, Decimal};
 use crate::codec::convert::ConvertTo;
+use crate::codec::error::Error;
 use crate::codec::mysql::MAX_FSP;
 use crate::codec::{Result, TEN_POW};
 use crate::expr::EvalContext;
@@ -29,14 +30,14 @@ const MAX_MINUTES: u32 = 59;
 const MAX_SECONDS: u32 = 59;
 const MAX_MICROS: u32 = 999_999;
 
+/// `MAX_TIME_IN_SECS` is the maximum for mysql time type.
+const MAX_TIME_IN_SECS: i64 =
+    (MAX_HOURS * SECS_PER_HOUR + MAX_MINUTES * SECS_PER_MINUTE + MAX_SECONDS) as i64;
+
 #[inline]
 fn check_hour(hour: u32) -> Result<u32> {
     if hour > MAX_HOURS {
-        Err(invalid_type!(
-            "invalid hour value: {} larger than {}",
-            hour,
-            MAX_HOURS
-        ))
+        Err(Error::overflow(hour, MAX_HOURS))
     } else {
         Ok(hour)
     }
@@ -45,11 +46,7 @@ fn check_hour(hour: u32) -> Result<u32> {
 #[inline]
 fn check_minute(minute: u32) -> Result<u32> {
     if minute > MAX_MINUTES {
-        Err(invalid_type!(
-            "invalid minute value: {} larger than {}",
-            minute,
-            MAX_MINUTES
-        ))
+        Err(Error::truncated_wrong_val("MINUTES", minute))
     } else {
         Ok(minute)
     }
@@ -58,11 +55,7 @@ fn check_minute(minute: u32) -> Result<u32> {
 #[inline]
 fn check_second(second: u32) -> Result<u32> {
     if second > MAX_SECONDS {
-        Err(invalid_type!(
-            "invalid second value: {} larger than {}",
-            second,
-            MAX_SECONDS
-        ))
+        Err(Error::truncated_wrong_val("SECONDS", second))
     } else {
         Ok(second)
     }
@@ -71,18 +64,14 @@ fn check_second(second: u32) -> Result<u32> {
 #[inline]
 fn check_micros(micros: u32) -> Result<u32> {
     if micros > MAX_MICROS {
-        Err(invalid_type!(
-            "invalid fractional value: {} larger than {}",
-            micros,
-            MAX_MICROS
-        ))
+        Err(Error::truncated_wrong_val("MICROS", micros))
     } else {
         Ok(micros)
     }
 }
 
 mod parser {
-    use super::{check_hour, check_minute, check_second, MICRO_WIDTH, TEN_POW};
+    use super::{check_hour, check_minute, check_second, Error, MICRO_WIDTH, TEN_POW};
     use nom::character::complete::{digit1, multispace0, multispace1};
     use nom::{
         alt, call, char, complete, cond, do_parse, eof, map, map_res, opt, peek, preceded, tag,
@@ -110,7 +99,7 @@ mod parser {
             if buf.len() <= 7 {
                 Ok(buf_to_int(buf))
             } else {
-                Err(invalid_type!("invalid time value, more than {} digits", 7))
+                Err(Error::truncated_wrong_val("TIME DIGITS", 7))
             }
         })
     }
@@ -449,7 +438,7 @@ impl Duration {
         Duration::from_micros(
             millis
                 .checked_mul(1000)
-                .ok_or(invalid_type!("micros overflow"))?,
+                .ok_or(Error::overflow(millis, MAX_TIME_IN_SECS * 1000))?,
             fsp,
         )
     }
@@ -480,7 +469,7 @@ impl Duration {
 
         let (mut neg, [mut day, mut hour, mut minute, mut second, micros]) =
             self::parser::parse(input, fsp)
-                .map_err(|_| invalid_type!("invalid time format"))?
+                .map_err(|_| Error::truncated_wrong_val("time", format!("{:?}", input)))?
                 .1;
 
         if day.is_some() && hour.is_none() {
@@ -1067,10 +1056,6 @@ mod tests {
 
     #[test]
     fn test_checked_add_and_sub_duration() {
-        /// `MAX_TIME_IN_SECS` is the maximum for mysql time type.
-        const MAX_TIME_IN_SECS: i64 =
-            (MAX_HOURS * SECS_PER_HOUR + MAX_MINUTES * SECS_PER_MINUTE + MAX_SECONDS) as i64;
-
         let cases = vec![
             ("11:30:45.123456", "00:00:14.876545", "11:31:00.000001"),
             ("11:30:45.123456", "00:30:00", "12:00:45.123456"),
