c2a3d4e47eb49aed117df7e6067a70425951db6d
5kbpers <tangminghua@pingcap.com>
2019-08-09 10:07:52 +0800

    derive Clone for KeyBuilder
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/engine/src/cf.rs
+++ components/engine/src/cf.rs
@@ -1,11 +1,11 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
-pub type CFName = &'static str;
-pub const CF_DEFAULT: CFName = "default";
-pub const CF_LOCK: CFName = "lock";
-pub const CF_WRITE: CFName = "write";
-pub const CF_RAFT: CFName = "raft";
+pub type CfName = &'static str;
+pub const CF_DEFAULT: CfName = "default";
+pub const CF_LOCK: CfName = "lock";
+pub const CF_WRITE: CfName = "write";
+pub const CF_RAFT: CfName = "raft";
 // Cfs that should be very large generally.
-pub const LARGE_CFS: &[CFName] = &[CF_DEFAULT, CF_WRITE];
-pub const ALL_CFS: &[CFName] = &[CF_DEFAULT, CF_LOCK, CF_WRITE, CF_RAFT];
-pub const DATA_CFS: &[CFName] = &[CF_DEFAULT, CF_LOCK, CF_WRITE];
+pub const LARGE_CFS: &[CfName] = &[CF_DEFAULT, CF_WRITE];
+pub const ALL_CFS: &[CfName] = &[CF_DEFAULT, CF_LOCK, CF_WRITE, CF_RAFT];
+pub const DATA_CFS: &[CfName] = &[CF_DEFAULT, CF_LOCK, CF_WRITE];
