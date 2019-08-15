2225ff52ab5f42999d46258be60a91c6fbbbb5f7
mapleFU <1506118561@qq.com>
2019-08-15 14:45:19 +0800

    [Migrate] Migrate tests for `json_modify`
    Signed-off-by: mapleFU <1506118561@qq.com>

--- components/tidb_query/src/rpn_expr/impl_json.rs
+++ components/tidb_query/src/rpn_expr/impl_json.rs
@@ -32,16 +32,56 @@ fn json_replace(args: &[ScalarValueRef]) -> Result<Option<Json>> {
     json_modify(args, ModifyType::Replace)
 }
 
-/// TODO: set min_args for json functions.
 /// TODO: think about can macros be used in this function.
 /// TODO: implement the functions.
+/// TODO: make clear how json handles None.
 #[inline]
 fn json_modify(args: &[ScalarValueRef], mt: ModifyType) -> Result<Option<Json>> {
-    unimplemented!()
+    // args >= 2
+    // base Json argument
+    let base: &Option<Json> = Evaluable::borrow_scalar_value_ref(&args[0]);
+    let base = base.as_ref().map_or(Json::None, |json| json.to_owned());
+
+    // args.len() / 2
+    let sz = args.len() / 2;
+
+    let mut path_expr_list = Vec::with_capacity(sz);
+    let mut values = Vec::with_capacity(sz);
+
+    for chunk in args[1..].chunks(2) {
+        let path: &Option<Bytes> = Evaluable::borrow_scalar_value_ref(&chunk[0]);
+        let value: &Option<Json> = Evaluable::borrow_scalar_value_ref(&chunk[1]);
+        if path.is_none() {
+            return Ok(None);
+        }
+        let path = path.unwrap();
+        let value = value.as_ref().map_or(Json::None, |json| json.to_owned());
+
+        // TODO: check path with
+        let path = PathExpression::try_from(path);
+
+        path_expr_list.push(try_opt!(path));
+        values.push(value);
+    }
+    base.modify(&path_expr_list, values, mt)?;
+
+    base
 }
 
+/// validate the arguments are `(&Option<Json>, &[(Option<Bytes>, Option<Json>)])`
 fn json_modify_validator(expr: &tipb::Expr) -> Result<()> {
-    unimplemented!()
+    let children = expr.get_children();
+    // min_args will be validated before extra_validator, so expr.get_children() must be larger than 2
+    super::function::validate_expr_return_type(&children[0], Json::EVAL_TYPE)?;
+    if chunk.len() == 1 {
+        return Err(other_err!(
+                "Incorrect parameter count in the call to native function 'JSON_OBJECT'"
+            ));
+    } else {
+        super::function::validate_expr_return_type(&chunk[0], Bytes::EVAL_TYPE)?;
+        super::function::validate_expr_return_type(&chunk[1], Json::EVAL_TYPE)?;
+    }
+    Ok(())
 }
 
 #[rpn_fn]
@@ -89,9 +129,59 @@ mod tests {
 
     #[test]
     fn test_json_modify() {
-        unimplemented!()
+        let cases: Vec<(_, Vec<ScalarValue>, _)> = vec![
+            (
+                ScalarFuncSig::JsonSetSig,
+                vec![None::<Json>.into(), None::<Bytes>.into(), None::<Json>.into()],
+                None::<Json>,
+            ),
+            (
+                ScalarFuncSig::JsonSetSig,
+                vec![
+                    Some(Json::I64(9)).into(),
+                    Some(Bytes::from(b"$[1]".to_vec())).into(),
+                    Some(Json::U64(3)).into(),
+                ],
+                Some(Json::from(r#"[9,3]"#.parse().unwrap())),
+            ),
+            (
+                ScalarFuncSig::JsonInsertSig,
+                vec![
+                    Some(Json::I64(9)).into(),
+                    Some(Bytes::from(b"$[1]".to_vec())).into(),
+                    Some(Json::U64(3)).into(),
+                ],
+                Some(Json::from(r#"[9,3]"#.parse().unwrap())),
+            ),
+            (
+                ScalarFuncSig::JsonReplaceSig,
+                vec![
+                    Some(Json::I64(9)).into(),
+                    Some(Bytes::from(b"$[1]".to_vec())).into(),
+                    Some(Json::U64(3)).into(),
+                ],
+                Some(Json::from(r#"9"#.parse().unwrap())),
+            ),
+            (
+                ScalarFuncSig::JsonSetSig,
+                vec![
+                    Some(Json::from(r#"{"a":"x"}"#.parse().unwrap())).into(),
+                    Some(Bytes::from(b"$.a".to_vec())).into(),
+                    None::<Json>.into(),
+                ],
+                Some(Json::from(r#"{"a":null}"#.parse().unwrap())),
+            ),
+        ];
+        for (sig, args, expect_output) in cases {
+            let output: Option<Json> = RpnFnScalarEvaluator::new()
+                .push_params(args.clone())
+                .evaluate(sig)
+                .unwrap();
+            assert_eq!(output, expect_output, "{:?}", args);
+        }
     }
 
+    #[test]
     fn test_json_unquote() {
         let cases = vec![
             (None, false, None),
