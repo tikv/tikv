1b5b44e1c3a9c9d32d0316b77e402aba3b254cd9
Huang Zexin <hzx20112012@gmail.com>
2019-08-15 15:57:11 +0800

    cop: refactor rpn cast uint to * (#5179)
    * cop: refactor rpn `cast uint to *` according to TiDB's impl(expression/builtin_cast.go)
    
    Signed-off-by: H-ZeX <hzx20112012@gmail.com>

--- components/tidb_query/src/codec/mysql/decimal.rs
+++ components/tidb_query/src/codec/mysql/decimal.rs
@@ -1108,6 +1108,8 @@ impl Decimal {
         self.word_buf[buf_from] /= TEN_POW[shift];
     }
 
+    // TODO, remove this after merge the `refactor ScalarFunc::builtin_cast`
+    //
     /// convert_to(ProduceDecWithSpecifiedTp in tidb)
     /// produces a new decimal according to `flen` and `decimal`.
     pub fn convert_to(self, ctx: &mut EvalContext, flen: u8, decimal: u8) -> Result<Decimal> {
