0dbebfa16f4fe1cad4670e13b439d4bc036e99c1
Wenxuan <breezewish@pingcap.com>
2019-08-22 11:15:50 +0800

    Move task limit from read pool to future pool (#5301)

--- components/tikv_util/src/future_pool/mod.rs
+++ components/tikv_util/src/future_pool/mod.rs
@@ -13,7 +13,7 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use futures::{lazy, Future};
-use prometheus::*;
+use prometheus::{IntCounter, IntGauge};
 use tokio_threadpool::{SpawnHandle, ThreadPool};
 
 use crate::time::Instant;
@@ -34,11 +34,12 @@ struct Env {
 pub struct FuturePool {
     pool: Arc<ThreadPool>,
     env: Arc<Env>,
+    max_tasks: usize,
 }
 
 impl std::fmt::Debug for FuturePool {
     fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        fmt.debug_struct("FuturePool").finish()
+        write!(fmt, "FuturePool")
     }
 }
 
@@ -52,6 +53,27 @@ impl FuturePool {
         // As long as different future pool has different name prefix, we can safely use the value
         // in metrics.
         self.env.metrics_running_task_count.get() as usize
+    }
+
+    fn gate_spawn(&self) -> Result<(), Full> {
+        fail_point!("future_pool_spawn_full", |_| Err(Full {
+            current_tasks: 100,
+            max_tasks: 100,
+        }));
+
+        if self.max_tasks == std::usize::MAX {
+            return Ok(());
+        }
+
+        let current_tasks = self.get_running_task_count();
+        if current_tasks >= self.max_tasks {
+            Err(Full {
+                current_tasks,
+                max_tasks: self.max_tasks,
+            })
+        } else {
+            Ok(())
+        }
     }
 
     /// Wraps a user provided future to support features of the `FuturePool`.
@@ -78,30 +100,53 @@ impl FuturePool {
     }
 
     /// Spawns a future in the pool.
-    pub fn spawn<F, R>(&self, future_fn: F)
+    pub fn spawn<F, R>(&self, future_fn: F) -> Result<(), Full>
     where
         F: FnOnce() -> R + Send + 'static,
         R: Future + Send + 'static,
         R::Item: Send + 'static,
         R::Error: Send + 'static,
     {
+        self.gate_spawn()?;
+
         let future = self.wrap_user_future(future_fn);
         self.pool.spawn(future.then(|_| Ok(())));
+        Ok(())
     }
 
     /// Spawns a future in the pool and returns a handle to the result of the future.
     ///
     /// The future will not be executed if the handle is not polled.
     #[must_use]
-    pub fn spawn_handle<F, R>(&self, future_fn: F) -> SpawnHandle<R::Item, R::Error>
+    pub fn spawn_handle<F, R>(&self, future_fn: F) -> Result<SpawnHandle<R::Item, R::Error>, Full>
     where
         F: FnOnce() -> R + Send + 'static,
         R: Future + Send + 'static,
         R::Item: Send + 'static,
         R::Error: Send + 'static,
     {
+        self.gate_spawn()?;
+
         let future = self.wrap_user_future(future_fn);
-        self.pool.spawn_handle(future)
+        Ok(self.pool.spawn_handle(future))
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct Full {
+    pub current_tasks: usize,
+    pub max_tasks: usize,
+}
+
+impl std::fmt::Display for Full {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(fmt, "future pool is full")
+    }
+}
+
+impl std::error::Error for Full {
+    fn description(&self) -> &str {
+        "future pool is full"
     }
 }
 
@@ -139,6 +184,7 @@ mod tests {
             thread::sleep(duration);
             future::ok::<_, ()>(())
         })
+        .unwrap()
         .wait()
         .unwrap();
     }
@@ -147,7 +193,8 @@ mod tests {
         pool.spawn(move || {
             thread::sleep(duration);
             future::ok::<_, ()>(())
-        });
+        })
+        .unwrap();
     }
 
     #[test]
@@ -257,13 +304,17 @@ mod tests {
             thread::sleep(Duration::from_millis(200));
             tx2.send(11).unwrap();
             future::ok::<_, ()>(())
-        });
+        })
+        .unwrap();
 
         let tx2 = tx.clone();
-        drop(pool.spawn_handle(move || {
-            tx2.send(7).unwrap();
-            future::ok::<_, ()>(())
-        }));
+        drop(
+            pool.spawn_handle(move || {
+                tx2.send(7).unwrap();
+                future::ok::<_, ()>(())
+            })
+            .unwrap(),
+        );
 
         thread::sleep(Duration::from_millis(500));
 
@@ -277,7 +328,7 @@ mod tests {
 
         let handle = pool.spawn_handle(move || future::ok::<_, ()>(42));
 
-        assert_eq!(handle.wait().unwrap(), 42);
+        assert_eq!(handle.unwrap().wait().unwrap(), 42);
     }
 
     #[test]
@@ -306,5 +357,93 @@ mod tests {
 
         thread::sleep(Duration::from_millis(2700));
         assert_eq!(pool.get_running_task_count(), 0);
+    }
+
+    fn spawn_long_time_future(
+        pool: &FuturePool,
+        id: u64,
+        future_duration_ms: u64,
+    ) -> Result<SpawnHandle<u64, ()>, Full> {
+        pool.spawn_handle(move || {
+            thread::sleep(Duration::from_millis(future_duration_ms));
+            future::ok::<u64, ()>(id)
+        })
+    }
+
+    fn wait_on_new_thread<F>(
+        sender: mpsc::Sender<std::result::Result<F::Item, F::Error>>,
+        future: F,
+    ) where
+        F: Future + Send + 'static,
+        F::Item: Send + 'static,
+        F::Error: Send + 'static,
+    {
+        thread::spawn(move || {
+            let r = future.wait();
+            sender.send(r).unwrap();
+        });
+    }
+
+    #[test]
+    fn test_full() {
+        let (tx, rx) = mpsc::channel();
+
+        let read_pool = Builder::new()
+            .name_prefix("future_pool_test_full")
+            .pool_size(2)
+            .max_tasks(4)
+            .build();
+
+        wait_on_new_thread(
+            tx.clone(),
+            spawn_long_time_future(&read_pool, 0, 5).unwrap(),
+        );
+        // not full
+        assert_eq!(rx.recv().unwrap(), Ok(0));
+
+        wait_on_new_thread(
+            tx.clone(),
+            spawn_long_time_future(&read_pool, 1, 100).unwrap(),
+        );
+        wait_on_new_thread(
+            tx.clone(),
+            spawn_long_time_future(&read_pool, 2, 200).unwrap(),
+        );
+        wait_on_new_thread(
+            tx.clone(),
+            spawn_long_time_future(&read_pool, 3, 300).unwrap(),
+        );
+        wait_on_new_thread(
+            tx.clone(),
+            spawn_long_time_future(&read_pool, 4, 400).unwrap(),
+        );
+        // no available results (running = 4)
+        assert!(rx.recv_timeout(Duration::from_millis(50)).is_err());
+
+        // full
+        assert!(spawn_long_time_future(&read_pool, 5, 100).is_err());
+
+        // full
+        assert!(spawn_long_time_future(&read_pool, 6, 100).is_err());
+
+        // wait a future completes (running = 3)
+        assert_eq!(rx.recv().unwrap(), Ok(1));
+
+        // add new (running = 4)
+        wait_on_new_thread(
+            tx.clone(),
+            spawn_long_time_future(&read_pool, 7, 5).unwrap(),
+        );
+
+        // full
+        assert!(spawn_long_time_future(&read_pool, 8, 100).is_err());
+
+        assert!(rx.recv().is_ok());
+        assert!(rx.recv().is_ok());
+        assert!(rx.recv().is_ok());
+        assert!(rx.recv().is_ok());
+
+        // no more results
+        assert!(rx.recv_timeout(Duration::from_millis(500)).is_err());
     }
 }
