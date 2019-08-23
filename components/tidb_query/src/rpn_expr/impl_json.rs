0e57db840263630389da799ea592dc66b815fe9b
mapleFU <1506118561@qq.com>
2019-08-23 11:40:14 +0800

    [Split] Split json_array from branch 'dev-json-parse'
    Signed-off-by: mapleFU <1506118561@qq.com>

--- components/tidb_query/src/rpn_expr/impl_json.rs
+++ components/tidb_query/src/rpn_expr/impl_json.rs
@@ -1,9 +1,6 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
-use std::collections::BTreeMap;
-
 use tidb_query_codegen::rpn_fn;
-use tidb_query_datatype::EvalType;
 
 use crate::codec::data_type::*;
 use crate::codec::mysql::json::*;
@@ -28,70 +25,6 @@ fn json_array(args: &[&Option<Json>]) -> Result<Option<Json>> {
             })
             .collect(),
     )))
-}
-
-fn json_object_validator(expr: &tipb::Expr) -> Result<()> {
-    for chunk in expr.get_children().chunks(2) {
-        if chunk.len() == 1 {
-            return Err(other_err!(
-                "Incorrect parameter count in the call to native function 'JSON_OBJECT'"
-            ));
-        } else {
-            super::function::validate_expr_return_type(&chunk[0], EvalType::Bytes)?;
-            super::function::validate_expr_return_type(&chunk[1], EvalType::Json)?;
-        }
-    }
-    Ok(())
-}
-
-/// Required args like `&[(&Option<Byte>, &Option<Json>)]`.
-#[rpn_fn(raw_varg, extra_validator = json_object_validator)]
-#[inline]
-fn json_object(raw_args: &[ScalarValueRef]) -> Result<Option<Json>> {
-    let mut pairs = BTreeMap::new();
-    for chunk in raw_args.chunks(2) {
-        // chunk.len() must be 1 or 2 here.
-        let key = Evaluable::borrow_scalar_value_ref(&chunk[0])
-            .as_ref()
-            .map_or_else(
-                || {
-                    Err(other_err!(
-                        "Data truncation: JSON documents may not contain NULL member names."
-                    ))
-                },
-                |v: &Bytes| {
-                    String::from_utf8(v.to_owned()).map_err(|e| crate::codec::Error::from(e).into())
-                },
-            )?;
-
-        let value = Evaluable::borrow_scalar_value_ref(&chunk[1])
-            .as_ref()
-            .map_or(Json::None, |v: &Json| v.to_owned());
-
-        pairs.insert(key, value);
-    }
-    Ok(Some(Json::Object(pairs)))
-}
-
-// According to mysql 5.7,
-// arguments of json_merge should not be less than 2.
-#[rpn_fn(varg, min_args = 2)]
-#[inline]
-pub fn json_merge(args: &[&Option<Json>]) -> Result<Option<Json>> {
-    // min_args = 2, so it's ok to call args[0]
-    let mut base_json = match args[0] {
-        None => return Ok(None),
-        Some(json) => json.to_owned(),
-    };
-
-    for json_to_merge in &args[1..] {
-        let json_to_merge = match json_to_merge {
-            None => return Ok(None),
-            Some(json) => json.to_owned(),
-        };
-        base_json = base_json.merge(json_to_merge);
-    }
-    Ok(Some(base_json))
 }
 
 #[rpn_fn]
@@ -166,94 +99,6 @@ mod tests {
                 .evaluate(ScalarFuncSig::JsonArraySig)
                 .unwrap();
             assert_eq!(output, expected, "{:?}", vargs);
-        }
-    }
-
-    #[test]
-    fn test_json_merge() {
-        let cases = vec![
-            (vec![None, None], None),
-            (vec![Some("{}"), Some("[]")], Some("[{}]")),
-            (
-                vec![Some(r#"{}"#), Some(r#"[]"#), Some(r#"3"#), Some(r#""4""#)],
-                Some(r#"[{}, 3, "4"]"#),
-            ),
-            (
-                vec![Some("[1, 2]"), Some("[3, 4]")],
-                Some(r#"[1, 2, 3, 4]"#),
-            ),
-        ];
-
-        for (vargs, expected) in cases {
-            let vargs = vargs
-                .into_iter()
-                .map(|input| input.map(|s| Json::from_str(s).unwrap()))
-                .collect::<Vec<_>>();
-            let expected = expected.map(|s| Json::from_str(s).unwrap());
-
-            let output = RpnFnScalarEvaluator::new()
-                .push_params(vargs.clone())
-                .evaluate(ScalarFuncSig::JsonMergeSig)
-                .unwrap();
-            assert_eq!(output, expected, "{:?}", vargs);
-        }
-    }
-
-    #[test]
-    fn test_json_object() {
-        let cases = vec![
-            (vec![], r#"{}"#),
-            (vec![("1", None)], r#"{"1":null}"#),
-            (
-                vec![
-                    ("1", None),
-                    ("2", Some(r#""sdf""#)),
-                    ("k1", Some(r#""v1""#)),
-                ],
-                r#"{"1":null,"2":"sdf","k1":"v1"}"#,
-            ),
-        ];
-
-        for (vargs, expected) in cases {
-            let vargs = vargs
-                .into_iter()
-                .map(|(key, value)| (Bytes::from(key), value.map(|s| Json::from_str(s).unwrap())))
-                .collect::<Vec<_>>();
-
-            let mut new_vargs: Vec<ScalarValue> = vec![];
-            for (key, value) in vargs.into_iter() {
-                new_vargs.push(ScalarValue::from(key));
-                new_vargs.push(ScalarValue::from(value));
-            }
-
-            let expected = Json::from_str(expected).unwrap();
-
-            let output: Json = RpnFnScalarEvaluator::new()
-                .push_params(new_vargs)
-                .evaluate(ScalarFuncSig::JsonObjectSig)
-                .unwrap()
-                .unwrap();
-            assert_eq!(output, expected);
-        }
-
-        let err_cases = vec![
-            vec![
-                ScalarValue::from(Bytes::from("1")),
-                ScalarValue::from(None::<Json>),
-                ScalarValue::from(Bytes::from("1")),
-            ],
-            vec![
-                ScalarValue::from(None::<Bytes>),
-                ScalarValue::from(Json::from_str("1").unwrap()),
-            ],
-        ];
-
-        for err_args in err_cases {
-            let output: Result<Option<Json>> = RpnFnScalarEvaluator::new()
-                .push_params(err_args)
-                .evaluate(ScalarFuncSig::JsonObjectSig);
-
-            assert!(output.is_err());
         }
     }
 
