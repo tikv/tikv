3e190fa4ce5a48b7d002875ab072d363c13416dc
mapleFU <1506118561@qq.com>
2019-08-28 16:05:13 +0800

    [ADD] add `take_scanned_range` in trait `BatchExecutor`.
    - "MockExecutor" just return ("0", "1").into() here.
    - No testing provided now.
    
    Signed-off-by: mapleFU <1506118561@qq.com>

--- components/tidb_query/src/batch/executors/top_n_executor.rs
+++ components/tidb_query/src/batch/executors/top_n_executor.rs
@@ -16,6 +16,7 @@ use crate::expr::EvalWarnings;
 use crate::expr::{EvalConfig, EvalContext};
 use crate::rpn_expr::RpnStackNode;
 use crate::rpn_expr::{RpnExpression, RpnExpressionBuilder};
+use crate::storage::IntervalRange;
 use crate::Result;
 
 pub struct BatchTopNExecutor<Src: BatchExecutor> {
@@ -319,6 +320,11 @@ impl<Src: BatchExecutor> BatchExecutor for BatchTopNExecutor<Src> {
     #[inline]
     fn collect_storage_stats(&mut self, dest: &mut Self::StorageStats) {
         self.src.collect_storage_stats(dest);
+    }
+
+    #[inline]
+    fn take_scanned_range(&mut self) -> IntervalRange {
+        self.src.take_scanned_range()
     }
 }
 
