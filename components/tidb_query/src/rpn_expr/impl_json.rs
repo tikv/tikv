b13557a433f4e33eb82a8ce1a7f06b71be4b0b48
mapleFU <1506118561@qq.com>
2019-08-30 03:21:00 +0800

    [Update] Finish and add tests for json-extract
    Signed-off-by: mapleFU <1506118561@qq.com>

--- components/tidb_query/src/rpn_expr/impl_json.rs
+++ components/tidb_query/src/rpn_expr/impl_json.rs
@@ -1,10 +1,11 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
 use tidb_query_codegen::rpn_fn;
+use tidb_query_datatype::EvalType;
 
 use crate::codec::data_type::*;
 use crate::codec::mysql::json::*;
-use crate::Result;
+use crate::{Error, Result};
 
 #[rpn_fn]
 #[inline]
@@ -56,16 +57,50 @@ fn json_unquote(arg: &Option<Json>) -> Result<Option<Bytes>> {
 }
 
 fn json_extract_validator(expr: &tipb::Expr) -> Result<()> {
-    unimplemented!()
+    let children = expr.get_children();
+    // min_args = 2
+    // args should be like `&Option<Json> , &[&Option<Bytes>]`.
+    super::function::validate_expr_return_type(&children[0], EvalType::Json)?;
+    for i in 1..children.len() {
+        super::function::validate_expr_return_type(&children[i], EvalType::Bytes)?;
+    }
+    Ok(())
 }
 
-/// args should be like `Option<Json> , &[Option<Bytes>]`.
+/// args should be like `&Option<Json> , &[&Option<Bytes>]`.
 #[rpn_fn(raw_varg, min_args = 2, extra_validator = json_extract_validator)]
 #[inline]
 fn json_extract(args: &[ScalarValueRef]) -> Result<Option<Json>> {
     // args should be at least 2
-    let key: &Option<Bytes> = Evaluable::borrow_scalar_value_ref(&args[0]);
-    unimplemented!()
+    let j: &Option<Json> = Evaluable::borrow_scalar_value_ref(&args[0]);
+    let j = match j.as_ref() {
+        None => return Ok(None),
+        Some(j) => j.to_owned(),
+    };
+
+    let mut path_expr_list = vec![];
+    for i in 1..args.len() {
+        let json_path: &Option<Bytes> = Evaluable::borrow_scalar_value_ref(&args[i]);
+
+        let json_path = match json_path.as_ref() {
+            None => return Ok(None),
+            Some(p) => p.to_owned(),
+        };
+        let json_path = eval_string_and_decode(json_path)?;
+
+        let path_expr = parse_json_path_expr(&json_path)?;
+
+        path_expr_list.push(path_expr);
+    }
+
+    Ok(j.extract(&path_expr_list))
+}
+
+#[inline]
+fn eval_string_and_decode(b: Bytes) -> Result<String> {
+    String::from_utf8(b)
+        .map_err(crate::codec::Error::from)
+        .map_err(Error::from)
 }
 
 #[cfg(test)]
@@ -201,6 +236,48 @@ mod tests {
 
     #[test]
     fn test_json_extract() {
-        unimplemented!()
+        let cases: Vec<(Vec<ScalarValue>, _)> = vec![
+            (vec![None::<Json>.into(), None::<Bytes>.into()], None),
+            (
+                vec![
+                    Some(Json::from_str("[10, 20, [30, 40]]").unwrap()).into(),
+                    Some(b"$[1]".to_vec()).into(),
+                ],
+                Some("20"),
+            ),
+            (
+                vec![
+                    Some(Json::from_str("[10, 20, [30, 40]]").unwrap()).into(),
+                    Some(b"$[1]".to_vec()).into(),
+                    Some(b"$[0]".to_vec()).into(),
+                ],
+                Some("[20, 10]"),
+            ),
+            (
+                vec![
+                    Some(Json::from_str("[10, 20, [30, 40]]").unwrap()).into(),
+                    Some(b"$[2][*]".to_vec()).into(),
+                ],
+                Some("[30, 40]"),
+            ),
+            (
+                vec![
+                    Some(Json::from_str("[10, 20, [30, 40]]").unwrap()).into(),
+                    Some(b"$[2][*]".to_vec()).into(),
+                    None::<Bytes>.into(),
+                ],
+                None,
+            ),
+        ];
+
+        for (vargs, expected) in cases {
+            let expected = expected.map(|s| Json::from_str(s).unwrap());
+
+            let output = RpnFnScalarEvaluator::new()
+                .push_params(vargs.clone())
+                .evaluate(ScalarFuncSig::JsonExtractSig)
+                .unwrap();
+            assert_eq!(output, expected, "{:?}", vargs);
+        }
     }
 }
