4ac851bdc1d2305f087b72894ae34c627ef2f8d7
Yang Keao <keao.yang@yahoo.com>
2019-09-05 14:22:48 +0800

    Revert "add `config-check` flag for tikv-server (#5298)"
    This reverts commit 49384732b342123c2305f02ea3a009bb13b5eda8.

--- cmd/src/bin/tikv-server.rs
+++ cmd/src/bin/tikv-server.rs
@@ -6,7 +6,6 @@
 use std::process;
 
 use clap::{crate_authors, crate_version, App, Arg};
-use cmd::setup::validate_and_persist_config;
 use tikv::config::TiKvConfig;
 
 fn main() {
@@ -22,13 +21,6 @@ fn main() {
                 .value_name("FILE")
                 .help("Set the configuration file")
                 .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("config-check")
-                .required(false)
-                .long("config-check")
-                .takes_value(false)
-                .help("Check config file validity and exit"),
         )
         .arg(
             Arg::with_name("log-level")
@@ -147,14 +139,6 @@ fn main() {
         .map_or_else(TiKvConfig::default, |path| TiKvConfig::from_file(&path));
 
     cmd::setup::overwrite_config_with_cmd_args(&mut config, &matches);
-
-    if matches.is_present("config-check") {
-        validate_and_persist_config(&mut config, false);
-        println!("config check successful");
-        process::exit(0)
-    } else {
-        validate_and_persist_config(&mut config, true);
-    }
 
     cmd::server::run_tikv(config);
 }
