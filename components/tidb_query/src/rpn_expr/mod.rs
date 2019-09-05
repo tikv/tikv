3924fbeaa9d5c8413cfd83bed3c8c98eeca58a7c
Yang Keao <keao.yang@yahoo.com>
2019-09-05 14:23:39 +0800

    Revert "tidb_query: Migrate json_type to RPN Function. (#5224)"
    This reverts commit 51f61d345c9019e326a70531b4a0456faa0a6549.

--- components/tidb_query/src/rpn_expr/mod.rs
+++ components/tidb_query/src/rpn_expr/mod.rs
@@ -6,7 +6,6 @@ pub mod impl_arithmetic;
 pub mod impl_cast;
 pub mod impl_compare;
 pub mod impl_control;
-pub mod impl_json;
 pub mod impl_like;
 pub mod impl_math;
 pub mod impl_op;
@@ -23,7 +22,6 @@ use crate::Result;
 use self::impl_arithmetic::*;
 use self::impl_compare::*;
 use self::impl_control::*;
-use self::impl_json::*;
 use self::impl_like::*;
 use self::impl_math::*;
 use self::impl_op::*;
@@ -234,7 +232,6 @@ fn map_pb_sig_to_rpn_func(value: ScalarFuncSig, children: &[Expr]) -> Result<Rpn
         ScalarFuncSig::IfString => if_condition_fn_meta::<Bytes>(),
         ScalarFuncSig::IfTime => if_condition_fn_meta::<DateTime>(),
         ScalarFuncSig::IfDecimal => if_condition_fn_meta::<Decimal>(),
-        ScalarFuncSig::JsonTypeSig => json_type_fn_meta(),
         _ => return Err(other_err!(
             "ScalarFunction {:?} is not supported in batch mode",
             value
