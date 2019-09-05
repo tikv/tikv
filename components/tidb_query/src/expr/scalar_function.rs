0657fc7f5e7eb3a509b13fa7521c8c0d3500a441
Yang Keao <keao.yang@yahoo.com>
2019-09-05 14:23:31 +0800

    Revert "coprocessor: differentiate real/int/decimal for `not` expression (#5005)"
    This reverts commit cc7688ce4679d2fc0520efe5fd77a32a6cf2b868.

--- components/tidb_query/src/expr/scalar_function.rs
+++ components/tidb_query/src/expr/scalar_function.rs
@@ -200,8 +200,8 @@ impl ScalarFunc {
             | ScalarFuncSig::WeekOfYear
             | ScalarFuncSig::Year
             | ScalarFuncSig::UnaryNotInt
-            | ScalarFuncSig::UnaryNotReal
             | ScalarFuncSig::UnaryNotDecimal
+            | ScalarFuncSig::UnaryNotReal
             | ScalarFuncSig::UnaryMinusInt
             | ScalarFuncSig::UnaryMinusReal
             | ScalarFuncSig::UnaryMinusDecimal
@@ -765,9 +765,11 @@ dispatch_call! {
         LogicalOr => logical_or,
         LogicalXor => logical_xor,
 
-        UnaryNotInt => unary_not_int,
-        UnaryNotReal => unary_not_real,
-        UnaryNotDecimal => unary_not_decimal,
+        // FIXME(@lonng)
+        UnaryNotInt => unary_not,
+        UnaryNotDecimal => unary_not,
+        UnaryNotReal => unary_not,
+
         UnaryMinusInt => unary_minus_int,
         IntIsNull => int_is_null,
         IntIsFalse => int_is_false,
@@ -1272,8 +1274,8 @@ mod tests {
                     ScalarFuncSig::WeekOfYear,
                     ScalarFuncSig::Year,
                     ScalarFuncSig::UnaryNotInt,
-                    ScalarFuncSig::UnaryNotReal,
                     ScalarFuncSig::UnaryNotDecimal,
+                    ScalarFuncSig::UnaryNotReal,
                     ScalarFuncSig::UnaryMinusInt,
                     ScalarFuncSig::UnaryMinusReal,
                     ScalarFuncSig::UnaryMinusDecimal,
