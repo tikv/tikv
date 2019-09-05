a09a3c3e56ef2243f132d44becfe669f4fc128ae
Yang Keao <keao.yang@yahoo.com>
2019-09-05 14:23:14 +0800

    Revert "failpoints: setup for ci only once"
    This reverts commit 134c290e54a4a73496489bb1b6a0ec639d950aa9.

--- tests/failpoints/mod.rs
+++ tests/failpoints/mod.rs
@@ -9,19 +9,15 @@ extern crate slog_global;
 
 mod cases;
 
-use std::sync::Once;
-
-static INIT: Once = Once::new();
-
 fn setup<'a>() -> fail::FailScenario<'a> {
-    INIT.call_once(test_util::setup_for_ci);
-    fail::FailScenario::setup()
+    let guard = fail::FailScenario::setup();
+    test_util::setup_for_ci();
+    guard
 }
 
 #[test]
 fn test_setup() {
     let _ = std::thread::spawn(move || {
-        let _ = setup();
         panic_hook::mute();
         let _g = setup();
         panic!("Poison!");
