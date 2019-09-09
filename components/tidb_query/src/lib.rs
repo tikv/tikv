1f28cde6214449c142b119865a03813c012c2d60
Brian Anderson <andersrb@gmail.com>
2019-09-09 09:34:37 -0700

    tidb_query: add more comments about busted redundant_semicolon lint
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/tidb_query/src/lib.rs
+++ components/tidb_query/src/lib.rs
@@ -12,7 +12,8 @@
 #![feature(const_fn)]
 #![feature(test)]
 // FIXME: rustc says there are redundant semicolons here but isn't
-// saying where as of nightly-2019-08-26
+// saying where as of nightly-2019-09-05
+// See https://github.com/rust-lang/rust/issues/63967
 #![allow(redundant_semicolon)]
 // FIXME: ditto. probably a result of the above
 #![allow(clippy::no_effect)]
