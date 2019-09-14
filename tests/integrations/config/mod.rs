e5655160fce4b04226ffed763a9bf0c9fbadd59b
Little-Wallace <bupt2013211450@gmail.com>
2019-09-14 23:54:41 +0800

    fix ut
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- tests/integrations/config/mod.rs
+++ tests/integrations/config/mod.rs
@@ -188,6 +188,8 @@ fn test_serde_custom_tikv_config() {
         writable_file_max_buffer_size: ReadableSize::mb(12),
         use_direct_io_for_flush_and_compaction: true,
         enable_pipelined_write: false,
+        enable_multithread_write: false,
+        memtable_write_pool_size: 2,
         defaultcf: DefaultCfConfig {
             block_size: ReadableSize::kb(12),
             block_cache_size: ReadableSize::gb(12),
@@ -432,6 +434,8 @@ fn test_serde_custom_tikv_config() {
         allow_concurrent_memtable_write: true,
         bytes_per_sync: ReadableSize::mb(1),
         wal_bytes_per_sync: ReadableSize::kb(32),
+        enable_multithread_write: false,
+        memtable_write_pool_size: 2,
         defaultcf: RaftDefaultCfConfig {
             block_size: ReadableSize::kb(12),
             block_cache_size: ReadableSize::gb(12),
