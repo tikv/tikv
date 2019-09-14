abd8d9c28146cddec19b90ea976f456d8b82e9f8
Little-Wallace <bupt2013211450@gmail.com>
2019-09-14 23:38:26 +0800

    fix ut
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- src/raftstore/store/fsm/apply.rs
+++ src/raftstore/store/fsm/apply.rs
@@ -2946,7 +2946,6 @@ mod tests {
     use crate::raftstore::store::msg::WriteResponse;
     use crate::raftstore::store::peer_storage::RAFT_INIT_LOG_INDEX;
     use crate::raftstore::store::util::{new_learner_peer, new_peer};
-    use engine::rocks::Writable;
     use engine::{WriteBatch, DB};
     use kvproto::metapb::{self, RegionEpoch};
     use kvproto::raft_cmdpb::*;
