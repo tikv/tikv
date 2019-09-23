27b48c16e139738ede2d2c1932edf13882e46eeb
Yang Keao <keao.yang@yahoo.com>
2019-09-23 15:28:55 +0800

    benches/coprocessor_executors: use official criterion and use cpb measurement
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- benches/coprocessor_executors/mod.rs
+++ benches/coprocessor_executors/mod.rs
@@ -19,7 +19,7 @@ fn main() {
     nix::sched::sched_setaffinity(nix::unistd::Pid::from_raw(0), &cpu_set).unwrap();
 
     let mut c = criterion::Criterion::default()
-        .with_measurement(criterion_cpu_time::PosixTime::UserAndSystemTime)
+        .with_measurement(criterion_cycles_per_byte::CyclesPerByte)
         .configure_from_args();
 
     util::fixture::bench(&mut c);
