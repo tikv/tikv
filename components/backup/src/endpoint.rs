47c6b35b83a42a0ff2e7c5f43ef8bbb985487478
linning <linningde25@gmail.com>
2019-09-27 21:31:28 +0800

    add test_thread_pool_shutdown_when_idle
    Signed-off-by: linning <linningde25@gmail.com>

--- components/backup/src/endpoint.rs
+++ components/backup/src/endpoint.rs
@@ -176,6 +176,7 @@ type BackupRes = (Vec<File>, Statistics);
 pub struct Endpoint<E: Engine, R: RegionInfoProvider> {
     store_id: u64,
     pool: RefCell<ControlThreadPool>,
+    pool_idle_threshold: u64,
     db: Arc<DB>,
 
     pub(crate) engine: E,
@@ -317,8 +318,8 @@ impl ControlThreadPool {
     }
 
     /// Shutdown the thread pool if it has been idle for a long time.
-    fn check_active(&mut self) {
-        if self.last_active.elapsed() >= Duration::from_millis(IDLE_THREADPOOL_DURATION) {
+    fn check_active(&mut self, idle_threshold: Duration) {
+        if self.last_active.elapsed() >= idle_threshold {
             self.size = 0;
             if let Some(w) = self.workers.take() {
                 w.shutdown();
@@ -334,13 +335,14 @@ impl<E: Engine, R: RegionInfoProvider> Endpoint<E, R> {
             engine,
             region_info,
             pool: RefCell::new(ControlThreadPool::new()),
+            pool_idle_threshold: IDLE_THREADPOOL_DURATION,
             db,
         }
     }
 
     pub fn new_timer(&self) -> Timer<()> {
         let mut timer = Timer::new(1);
-        timer.add_task(Duration::from_millis(IDLE_THREADPOOL_DURATION), ());
+        timer.add_task(Duration::from_millis(self.pool_idle_threshold), ());
         timer
     }
 
@@ -505,8 +507,11 @@ impl<E: Engine, R: RegionInfoProvider> Runnable<Task> for Endpoint<E, R> {
 
 impl<E: Engine, R: RegionInfoProvider> RunnableWithTimer<Task, ()> for Endpoint<E, R> {
     fn on_timeout(&mut self, timer: &mut Timer<()>, _: ()) {
-        self.pool.borrow_mut().check_active();
-        timer.add_task(Duration::from_millis(IDLE_THREADPOOL_DURATION), ());
+        let pool_idle_duration = Duration::from_millis(self.pool_idle_threshold);
+        self.pool
+            .borrow_mut()
+            .check_active(pool_idle_duration.clone());
+        timer.add_task(pool_idle_duration, ());
     }
 }
 
@@ -565,9 +570,10 @@ fn backup_file_name(store_id: u64, region: &Region) -> String {
 pub mod tests {
     use super::*;
     use external_storage::LocalStorage;
-    use futures::{Future, Stream};
+    use futures::{self, Future, Stream};
     use kvproto::metapb;
     use rand;
+    use std::thread;
     use tempfile::TempDir;
     use tikv::raftstore::coprocessor::RegionCollector;
     use tikv::raftstore::coprocessor::SeekRegionCallback;
@@ -576,6 +582,7 @@ pub mod tests {
     use tikv::storage::mvcc::tests::*;
     use tikv::storage::SHORT_VALUE_MAX_LEN;
     use tikv::storage::{RocksEngine, TestEngineBuilder};
+    use tikv_util::time::Instant;
 
     #[derive(Clone)]
     pub struct MockRegionInfoProvider {
@@ -1062,5 +1069,59 @@ pub mod tests {
         assert!(endpoint.pool.borrow().size == 3);
     }
 
+    #[test]
+    fn test_thread_pool_shutdown_when_idle() {
+        let (_, mut endpoint) = new_endpoint();
+
+        // set the idle threshold to 100ms
+        endpoint.pool_idle_threshold = 100;
+        let mut backup_timer = endpoint.new_timer();
+        let endpoint = Arc::new(Mutex::new(endpoint));
+        let scheduler = {
+            let endpoint = endpoint.clone();
+            let (tx, rx) = tikv_util::mpsc::unbounded();
+            thread::spawn(move || loop {
+                let tick_time = backup_timer.next_timeout().unwrap();
+                let timeout = tick_time.checked_sub(Instant::now()).unwrap_or_default();
+                let task = match rx.recv_timeout(timeout) {
+                    Ok(Some(task)) => Some(task),
+                    _ => None,
+                };
+                if let Some(task) = task {
+                    let mut endpoint = endpoint.lock().unwrap();
+                    endpoint.run(task);
+                }
+                endpoint.lock().unwrap().on_timeout(&mut backup_timer, ());
+            });
+            tx
+        };
+
+        let mut req = BackupRequest::new();
+        req.set_start_key(vec![]);
+        req.set_end_key(vec![]);
+        req.set_start_version(1);
+        req.set_end_version(1);
+        req.set_concurrency(10);
+        req.set_path("noop://foo".to_owned());
+
+        let (tx, _) = futures::sync::mpsc::unbounded();
+        let (task, _) = Task::new(req, tx).unwrap();
+
+        // if not task arrive after create the thread pool is empty
+        assert_eq!(endpoint.lock().unwrap().pool.borrow().size, 0);
+
+        scheduler.send(Some(task)).unwrap();
+        // wait the task send to worker
+        thread::sleep(Duration::from_millis(10));
+        assert_eq!(endpoint.lock().unwrap().pool.borrow().size, 10);
+
+        // thread pool not yet shutdown
+        thread::sleep(Duration::from_millis(50));
+        assert_eq!(endpoint.lock().unwrap().pool.borrow().size, 10);
+
+        // thread pool shutdown if not task arrive for 100ms
+        thread::sleep(Duration::from_millis(50));
+        assert_eq!(endpoint.lock().unwrap().pool.borrow().size, 0);
+    }
     // TODO: region err in txn(engine(request))
 }
