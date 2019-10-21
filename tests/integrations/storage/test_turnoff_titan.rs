456ab448cb1a6c8a5a40960f968856059c03168b
tabokie <xy.tao@outlook.com>
2019-10-21 14:46:45 +0800

    fix test
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- tests/integrations/storage/test_turnoff_titan.rs
+++ tests/integrations/storage/test_turnoff_titan.rs
@@ -7,22 +7,22 @@ use tikv_util::config::ReadableSize;
 
 #[test]
 fn test_turnoff_titan() {
-    let mut cluster = new_server_cluster(0, 3);
+    let mut cluster = new_node_cluster(0, 3);
+    cluster.cfg.rocksdb.defaultcf.disable_auto_compactions = true;
     configure_for_enable_titan(&mut cluster, ReadableSize::kb(0));
     cluster.run();
     assert_eq!(cluster.must_get(b"k1"), None);
-    let size = 100;
+    let size = 50;
     for i in 0..size {
         assert!(cluster
             .put(
-                format!("key-{}", i).as_bytes(),
-                format!("value-{}", i).as_bytes(),
+                format!("k{:02}0", i).as_bytes(),
+                format!("v{}", i).as_bytes(),
             )
             .is_ok());
     }
     // make sure data is flushed to disk.
     cluster.must_flush_cf(CF_DEFAULT, true);
-    cluster.compact_data();
     cluster.shutdown();
 
     // try reopen db when titandb dir is non-empty
@@ -32,6 +32,7 @@ fn test_turnoff_titan() {
     configure_for_enable_titan(&mut cluster, ReadableSize::kb(0));
     assert!(cluster.pre_start_check().is_ok());
     cluster.start().unwrap();
+    assert_eq!(cluster.must_get(b"k1"), None);
     for i in cluster.get_node_ids().into_iter() {
         let db = cluster.get_engine(i);
         let handle = get_cf_handle(&db, CF_DEFAULT).unwrap();
@@ -39,11 +40,24 @@ fn test_turnoff_titan() {
         opt.push(("blob_run_mode", "kFallback"));
         assert!(db.set_options_cf(handle, &opt).is_ok());
     }
+    for i in 0..size {
+        assert!(cluster
+            .put(
+                format!("k{:02}1", i).as_bytes(),
+                format!("v{}", i).as_bytes(),
+            )
+            .is_ok());
+    }
     cluster.must_flush_cf(CF_DEFAULT, true);
     cluster.compact_data();
+    cluster.must_flush_cf(CF_DEFAULT, true);
     cluster.shutdown();
+    sleep_ms(2000);
 
     configure_for_disable_titan(&mut cluster);
-    assert!(cluster.pre_start_check().is_ok());
+    assert!(cluster
+        .pre_start_check()
+        .map_err(|e| error!("{}", e))
+        .is_ok());
     cluster.start().unwrap();
 }
