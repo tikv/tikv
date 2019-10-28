8ca85beb5c09377cc8e7d93c7ccff5160d76bc57
linning <linningde25@gmail.com>
2019-10-28 20:29:07 +0800

    reduce wait time
    Signed-off-by: linning <linningde25@gmail.com>

--- tests/integrations/raftstore/test_prevote.rs
+++ tests/integrations/raftstore/test_prevote.rs
@@ -45,7 +45,7 @@ fn test_prevote<T: Simulator>(
     cluster.cfg.raft_store.prevote = true;
     // To stable the test, we use a large election timeout to make
     // leader's readiness get handle within an election timeout
-    configure_for_lease_read(cluster, Some(10), Some(70));
+    configure_for_lease_read(cluster, Some(10), Some(50));
 
     let leader_id = 1;
     let detect_during_failure = detect_during_failure.into();
@@ -77,7 +77,7 @@ fn test_prevote<T: Simulator>(
 
     if let (Some(rx), Some((_, should_detect))) = (rx, detect_during_failure) {
         // Once we see a response on the wire we know a prevote round is happening.
-        let received = rx.recv_timeout(Duration::from_secs(7));
+        let received = rx.recv_timeout(Duration::from_secs(5));
         debug!("Done with failure prevote notifier, got {:?}", received);
         assert_eq!(
             received.is_ok(),
@@ -114,7 +114,7 @@ fn test_prevote<T: Simulator>(
 
     // Once we see a response on the wire we know a prevote round is happening.
     if let (Some(rx), Some((_, should_detect))) = (rx, detect_during_failure) {
-        let received = rx.recv_timeout(Duration::from_secs(7));
+        let received = rx.recv_timeout(Duration::from_secs(5));
         debug!("Done with recovery prevote notifier, got {:?}", received);
 
         assert_eq!(
