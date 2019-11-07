d019ccecefc260ff760a53b7b8742fb84ffca9b5
Lonng <heng@lonng.org>
2019-11-07 19:40:02 +0800

    tidb_query: fix parse number to JSON (#5842)
    Signed-off-by: Lonng <heng@lonng.org>

--- components/tidb_query/src/expr/builtin_json.rs
+++ components/tidb_query/src/expr/builtin_json.rs
@@ -201,8 +201,9 @@ mod tests {
             (Some(r#"true"#), Some("BOOLEAN")),
             (Some(r#"null"#), Some("NULL")),
             (Some(r#"-3"#), Some("INTEGER")),
-            (Some(r#"3"#), Some("UNSIGNED INTEGER")),
+            (Some(r#"3"#), Some("INTEGER")),
             (Some(r#"3.14"#), Some("DOUBLE")),
+            (Some(r#"9223372036854775808"#), Some("DOUBLE")),
             (Some(r#"[1, 2, 3]"#), Some("ARRAY")),
             (Some(r#"{"name": 123}"#), Some("OBJECT")),
         ];
