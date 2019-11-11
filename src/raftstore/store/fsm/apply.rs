617fdeba4717cef9f6a2fd9eb2b8c0550539f786
5kbpers <tangminghua@pingcap.com>
2019-11-11 21:16:00 +0800

    fix for tests
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- src/raftstore/store/fsm/apply.rs
+++ src/raftstore/store/fsm/apply.rs
@@ -1087,7 +1087,7 @@ impl ApplyDelegate {
     ) -> Result<(RaftCmdResponse, ApplyResult)> {
         fail_point!(
             "on_apply_write_cmd",
-            cfg!(not(test)) || self.id() == 3,
+            cfg!(release) || self.id() == 3,
             |_| {
                 unimplemented!();
             }
