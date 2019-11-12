094a56e3ea2abf429cc258ab0317bcd226e74360
tabokie <xy.tao@outlook.com>
2019-11-12 13:49:09 +0800

    limit batch size
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- src/server/service/kv.rs
+++ src/server/service/kv.rs
@@ -51,6 +51,7 @@ const GRPC_MSG_NOTIFY_SIZE: usize = 8;
 const REQUEST_BATCH_LIMITER_SAMPLE_WINDOW: usize = 30;
 const REQUEST_BATCH_LIMITER_LOW_LOAD_RATIO: f32 = 0.3;
 const REQUEST_BATCH_LIMITER_HIGH_LATENCY: f64 = 2.2;
+const REQUEST_BATCH_LIMITER_READ_SIZE_LIMIT: usize = 30;
 
 #[derive(Hash, PartialEq, Eq, Debug)]
 struct RegionVerId {
@@ -145,7 +146,7 @@ impl BatchLimiter {
     /// Whether current batch needs more requests.
     #[inline]
     fn needs_more(&self) -> bool {
-        self.enable_batch
+        self.batch_input < REQUEST_BATCH_LIMITER_READ_SIZE_LIMIT && self.enable_batch
     }
 
     /// Observe a tick from timer guard. Limiter will update statistics at this point.
