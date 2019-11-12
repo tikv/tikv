15de487bd81b09363d5b0392e0c750c19789742d
Brian Anderson <andersrb@gmail.com>
2019-11-12 13:48:46 +0100

    engine_rocks: Add Compat implementation for snapshots
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_rocks/src/compat.rs
+++ components/engine_rocks/src/compat.rs
@@ -3,19 +3,34 @@
 use crate::engine::RocksEngine;
 use engine::DB;
 use std::sync::Arc;
+use engine::Snapshot as RawSnapshot;
+use crate::snapshot::RocksSnapshot;
 
 /// A trait to enter the world of engine traits from a raw `Arc<DB>`
 /// with as little syntax as possible.
 ///
 /// This will be used during the transition from RocksDB to the
 /// `KvEngine` abstraction and then discarded.
-pub trait EngineCompat {
-    fn c(&self) -> &RocksEngine;
+pub trait Compat {
+    type Other;
+
+    fn c(&self) -> &Self::Other;
 }
 
-impl EngineCompat for Arc<DB> {
+impl Compat for Arc<DB> {
+    type Other = RocksEngine;
+
     #[inline]
     fn c(&self) -> &RocksEngine {
         RocksEngine::from_ref(self)
+    }
+}
+
+impl Compat for RawSnapshot {
+    type Other = RocksSnapshot;
+
+    #[inline]
+    fn c(&self) -> &RocksSnapshot {
+        RocksSnapshot::from_ref(self)
     }
 }
