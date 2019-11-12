a33ce4b78966c6ed145c37db4f4c62394eaaf8b1
FateTHarlaown <623239185@qq.com>
2019-11-12 14:53:54 +0800

    update test
    Signed-off-by: FateTHarlaown <623239185@qq.com>

--- components/tidb_query/src/rpn_expr/impl_string.rs
+++ components/tidb_query/src/rpn_expr/impl_string.rs
@@ -16,30 +16,40 @@ pub fn length(arg: &Option<Bytes>) -> Result<Option<i64>> {
 mod tests {
     use tipb::ScalarFuncSig;
 
-    use super::*;
     use crate::rpn_expr::types::test_util::RpnFnScalarEvaluator;
 
     #[test]
     fn test_length() {
         let test_cases = vec![
-            (ScalarFuncSig::Length, "", Some(0i64)),
-            (ScalarFuncSig::Length, "你好", Some(6i64)),
-            (ScalarFuncSig::Length, "TiKV", Some(4i64)),
-            (ScalarFuncSig::Length, "あなたのことが好きです", Some(33i64)),
-            (ScalarFuncSig::Length, "분산 데이터베이스", Some(25i64)),
-            (ScalarFuncSig::Length, "россия в мире  кубок", Some(38i64)),
-            (ScalarFuncSig::Length, "قاعدة البيانات", Some(27i64)),
+            (ScalarFuncSig::Length, None, None),
+            (ScalarFuncSig::Length, Some(""), Some(0i64)),
+            (ScalarFuncSig::Length, Some("你好"), Some(6i64)),
+            (ScalarFuncSig::Length, Some("TiKV"), Some(4i64)),
+            (
+                ScalarFuncSig::Length,
+                Some("あなたのことが好きです"),
+                Some(33i64),
+            ),
+            (
+                ScalarFuncSig::Length,
+                Some("분산 데이터베이스"),
+                Some(25i64),
+            ),
+            (
+                ScalarFuncSig::Length,
+                Some("россия в мире  кубок"),
+                Some(38i64),
+            ),
+            (ScalarFuncSig::Length, Some("قاعدة البيانات"), Some(27i64)),
         ];
 
         for (sig, arg, expect_output) in test_cases {
-            let arg = arg.as_bytes().to_vec();
+            let arg = arg.map(|s| s.as_bytes().to_vec());
             let output = RpnFnScalarEvaluator::new()
                 .push_param(arg)
                 .evaluate(sig)
                 .unwrap();
             assert_eq!(output, expect_output);
         }
-
-        assert_eq!(length(&None).unwrap(), None);
     }
 }
