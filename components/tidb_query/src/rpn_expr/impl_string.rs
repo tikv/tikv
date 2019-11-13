69d56a947dd47e220ba38de74f32acbedf0b82d3
FateTHarlaown <623239185@qq.com>
2019-11-13 09:43:12 +0800

    de-duplicate ScalarFuncSig::Length
    Signed-off-by: FateTHarlaown <623239185@qq.com>

--- components/tidb_query/src/rpn_expr/impl_string.rs
+++ components/tidb_query/src/rpn_expr/impl_string.rs
@@ -18,33 +18,21 @@ mod tests {
     #[test]
     fn test_length() {
         let test_cases = vec![
-            (ScalarFuncSig::Length, None, None),
-            (ScalarFuncSig::Length, Some(""), Some(0i64)),
-            (ScalarFuncSig::Length, Some("你好"), Some(6i64)),
-            (ScalarFuncSig::Length, Some("TiKV"), Some(4i64)),
-            (
-                ScalarFuncSig::Length,
-                Some("あなたのことが好きです"),
-                Some(33i64),
-            ),
-            (
-                ScalarFuncSig::Length,
-                Some("분산 데이터베이스"),
-                Some(25i64),
-            ),
-            (
-                ScalarFuncSig::Length,
-                Some("россия в мире  кубок"),
-                Some(38i64),
-            ),
-            (ScalarFuncSig::Length, Some("قاعدة البيانات"), Some(27i64)),
+            (None, None),
+            (Some(""), Some(0i64)),
+            (Some("你好"), Some(6i64)),
+            (Some("TiKV"), Some(4i64)),
+            (Some("あなたのことが好きです"), Some(33i64)),
+            (Some("분산 데이터베이스"), Some(25i64)),
+            (Some("россия в мире  кубок"), Some(38i64)),
+            (Some("قاعدة البيانات"), Some(27i64)),
         ];
 
-        for (sig, arg, expect_output) in test_cases {
+        for (arg, expect_output) in test_cases {
             let arg = arg.map(|s| s.as_bytes().to_vec());
             let output = RpnFnScalarEvaluator::new()
                 .push_param(arg)
-                .evaluate(sig)
+                .evaluate(ScalarFuncSig::Length)
                 .unwrap();
             assert_eq!(output, expect_output);
         }
