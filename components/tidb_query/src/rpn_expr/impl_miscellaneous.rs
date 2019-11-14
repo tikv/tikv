49b989ab527aa5dbd1816012f7d7b7b3a409171e
Ruihang Xia <waynestxia@gmail.com>
2019-11-14 23:48:19 +0800

    rewrite function
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- components/tidb_query/src/rpn_expr/impl_miscellaneous.rs
+++ components/tidb_query/src/rpn_expr/impl_miscellaneous.rs
@@ -5,24 +5,15 @@ use tidb_query_codegen::rpn_fn;
 use crate::codec::data_type::*;
 use crate::Result;
 
+use std::convert::TryFrom;
 use std::net::Ipv4Addr;
 
 #[rpn_fn]
 #[inline]
 pub fn inet_ntoa(arg: &Option<Int>) -> Result<Option<Bytes>> {
-    Ok(match arg {
-        Some(arg) => {
-            if *arg < 0 || *arg > i64::from(u32::max_value()) {
-                None
-            } else {
-                let v = *arg as u32;
-                let ipv4_addr =
-                    Ipv4Addr::new((v >> 24) as u8, (v >> 16) as u8, (v >> 8) as u8, v as u8);
-                Some(format!("{}", ipv4_addr).into_bytes())
-            }
-        }
-        _ => None,
-    })
+    Ok(arg
+        .and_then(|arg| u32::try_from(arg).ok())
+        .map(|arg| format!("{}", Ipv4Addr::from(arg)).into_bytes()))
 }
 
 #[cfg(test)]
