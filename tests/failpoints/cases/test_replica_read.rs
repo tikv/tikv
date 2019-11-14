1c6c96ae7514134a211cf8783d11eaa246ea9b1a
5kbpers <tangminghua@pingcap.com>
2019-11-14 21:03:56 +0800

    make test more stable
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- tests/failpoints/cases/test_replica_read.rs
+++ tests/failpoints/cases/test_replica_read.rs
@@ -2,9 +2,11 @@
 
 use engine::{Peekable, CF_RAFT};
 use fail;
-use kvproto::raft_serverpb::{PeerState, RegionLocalState};
+use kvproto::raft_serverpb::{PeerState, RaftMessage, RegionLocalState};
+use raft::eraftpb::MessageType;
 use std::mem;
 use std::sync::atomic::AtomicBool;
+use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
@@ -22,12 +24,13 @@ fn test_wait_for_apply_index() {
     pd_client.disable_default_operator();
 
     let r1 = cluster.run_conf_change();
-    cluster.must_put(b"k0", b"v0");
     let p2 = new_peer(2, 2);
     cluster.pd_client.must_add_peer(r1, p2.clone());
     let p3 = new_peer(3, 3);
     cluster.pd_client.must_add_peer(r1, p3.clone());
-    must_get_equal(&cluster.get_engine(3), b"k0", b"v0");
+    cluster.must_put(b"k0", b"v0");
+    cluster.pd_client.must_none_pending_peer(p2.clone());
+    cluster.pd_client.must_none_pending_peer(p3.clone());
 
     let region = cluster.get_region(b"k0");
     cluster.must_transfer_leader(region.get_id(), p2.clone());
@@ -81,24 +84,31 @@ fn test_duplicate_read_index_ctx() {
     // Set region and peers
     let r1 = cluster.run_conf_change();
     let p1 = new_peer(1, 1);
-    cluster.must_put(b"k0", b"v0");
     let p2 = new_peer(2, 2);
     cluster.pd_client.must_add_peer(r1, p2.clone());
     let p3 = new_peer(3, 3);
     cluster.pd_client.must_add_peer(r1, p3.clone());
-    must_get_equal(&cluster.get_engine(3), b"k0", b"v0");
+    cluster.must_put(b"k0", b"v0");
+    cluster.pd_client.must_none_pending_peer(p2.clone());
+    cluster.pd_client.must_none_pending_peer(p3.clone());
     let region = cluster.get_region(b"k0");
     assert_eq!(cluster.leader_of_region(region.get_id()).unwrap(), p1);
 
     // Delay all raft messages to peer 1.
     let dropped_msgs = Arc::new(Mutex::new(Vec::new()));
-    let filter = Box::new(
+    let (sx, rx) = mpsc::sync_channel::<()>(2);
+    let recv_filter = Box::new(
         RegionPacketFilter::new(region.get_id(), 1)
             .direction(Direction::Recv)
             .when(Arc::new(AtomicBool::new(true)))
-            .reserve_dropped(Arc::clone(&dropped_msgs)),
+            .reserve_dropped(Arc::clone(&dropped_msgs))
+            .set_msg_callback(Arc::new(move |msg: &RaftMessage| {
+                if msg.get_message().get_msg_type() == MessageType::MsgReadIndex {
+                    sx.send(()).unwrap();
+                }
+            })),
     );
-    cluster.sim.wl().add_recv_filter(1, filter);
+    cluster.sim.wl().add_recv_filter(1, recv_filter);
 
     // send two read index requests to leader
     let mut request = new_request(
@@ -115,8 +125,9 @@ fn test_duplicate_read_index_ctx() {
         .rl()
         .async_command_on_node(2, request.clone(), cb2)
         .unwrap();
-    thread::sleep(Duration::from_millis(200));
+    rx.recv_timeout(Duration::from_secs(5)).unwrap();
 
+    must_get_equal(&cluster.get_engine(3), b"k0", b"v0");
     request.mut_header().set_peer(p3.clone());
     let (cb3, rx3) = make_cb(&request);
     // send to peer 3
@@ -125,7 +136,7 @@ fn test_duplicate_read_index_ctx() {
         .rl()
         .async_command_on_node(3, request.clone(), cb3)
         .unwrap();
-    thread::sleep(Duration::from_millis(200));
+    rx.recv_timeout(Duration::from_secs(5)).unwrap();
 
     let router = cluster.sim.wl().get_router(1).unwrap();
     fail::cfg("pause_on_peer_collect_message", "pause").unwrap();
@@ -136,8 +147,8 @@ fn test_duplicate_read_index_ctx() {
     fail::cfg("pause_on_peer_collect_message", "off").unwrap();
 
     // read index response must not be dropped
-    assert!(rx2.recv_timeout(Duration::from_millis(500)).is_ok());
-    assert!(rx3.recv_timeout(Duration::from_millis(500)).is_ok());
+    rx2.recv_timeout(Duration::from_secs(5)).unwrap();
+    rx3.recv_timeout(Duration::from_secs(5)).unwrap();
 }
 
 #[test]
@@ -152,9 +163,10 @@ fn test_read_before_init() {
     // Set region and peers
     let r1 = cluster.run_conf_change();
     let p1 = new_peer(1, 1);
-    cluster.must_put(b"k0", b"v0");
     let p2 = new_peer(2, 2);
     cluster.pd_client.must_add_peer(r1, p2.clone());
+    cluster.must_put(b"k0", b"v0");
+    cluster.pd_client.must_none_pending_peer(p2.clone());
     must_get_equal(&cluster.get_engine(2), b"k0", b"v0");
 
     fail::cfg("before_apply_snap_update_region", "return").unwrap();
@@ -179,7 +191,7 @@ fn test_read_before_init() {
         .rl()
         .async_command_on_node(3, request, cb)
         .unwrap();
-    let resp = rx.recv_timeout(Duration::from_millis(500)).unwrap();
+    let resp = rx.recv_timeout(Duration::from_secs(5)).unwrap();
     assert!(
         resp.get_header()
             .get_error()
@@ -202,10 +214,10 @@ fn test_read_applying_snapshot() {
     // Set region and peers
     let r1 = cluster.run_conf_change();
     let p1 = new_peer(1, 1);
-    cluster.must_put(b"k0", b"v0");
     let p2 = new_peer(2, 2);
     cluster.pd_client.must_add_peer(r1, p2.clone());
-    must_get_equal(&cluster.get_engine(2), b"k0", b"v0");
+    cluster.must_put(b"k0", b"v0");
+    cluster.pd_client.must_none_pending_peer(p2.clone());
 
     // Don't apply snapshot to init peer 3
     fail::cfg("region_apply_snap", "pause").unwrap();
@@ -238,8 +250,14 @@ fn test_read_applying_snapshot() {
         .rl()
         .async_command_on_node(3, request, cb)
         .unwrap();
+    let resp = match rx.recv_timeout(Duration::from_secs(5)) {
+        Ok(r) => r,
+        Err(_) => {
+            fail::cfg("region_apply_snap", "off").unwrap();
+            panic!("cannot receive response");
+        }
+    };
     fail::cfg("region_apply_snap", "off").unwrap();
-    let resp = rx.recv_timeout(Duration::from_millis(500)).unwrap();
     assert!(
         resp.get_header()
             .get_error()
