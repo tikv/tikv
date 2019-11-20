41ae120cdcbedb882f5f487bdaeb66f4efb59cf7
Kolbe Kegel <kolbe@kolbekegel.com>
2019-11-20 23:16:12 +0000

    Moved objcopy to external script for better handling on platforms where it does not exist
    Signed-off-by: Kolbe Kegel <kolbe@kolbekegel.com>

--- scripts/objcopy.sh
+++ scripts/objcopy.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-if command -v objcopy >/dev/null 2>&1; then
-    objcopy --compress-debug-sections=zlib-gnu ${CARGO_TARGET_DIR}/release/tikv-server
-    objcopy --compress-debug-sections=zlib-gnu ${CARGO_TARGET_DIR}/release/tikv-ctl
+# This script exists to handle platforms where objcopy is not available.
+# If a platform has some other tool that should be used, a special case should
+# be added to this script.
+
+if [ -z "$CARGO_TARGET_DIR" ]
+then
+    echo "This script expects CARGO_TARGET_DIR to be set in the environment. Aborting." >&2
+    exit 1
+fi
+
+if command -v objcopy >/dev/null 2>&1
+then
+    for file in tikv-server tikv-ctl
+    do
+        objcopy --compress-debug-sections=zlib-gnu "${CARGO_TARGET_DIR}/release/$file"
+    done
 else
     echo 'objcopy executable not found, skipping'
 fi
-
