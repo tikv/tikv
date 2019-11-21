45681f088cee3df9d8a514a4806e63a9c91d97bf
Lonng <heng@lonng.org>
2019-11-21 14:38:44 +0800

    add more test cases
    Signed-off-by: Lonng <heng@lonng.org>

--- src/server/service/diagnostics.rs
+++ src/server/service/diagnostics.rs
@@ -62,7 +62,7 @@ mod log {
     const INVALID_TIMESTAMP: i64 = -1;
     const TIMESTAMP_LENGTH: usize = 30;
 
-    pub struct LogIterator {
+    struct LogIterator {
         search_files: Vec<(i64, File)>,
         currrent_lines: Option<std::io::Lines<BufReader<File>>>,
 
@@ -76,7 +76,7 @@ mod log {
     #[derive(Debug)]
     pub enum Error {
         InvalidRequest(String),
-        ParseError,
+        ParseError(String),
         SearchError(String),
         IOError(std::io::Error),
     }
@@ -88,32 +88,37 @@ mod log {
     }
 
     impl LogIterator {
-        pub fn new(
-            log_file: &str,
+        fn new<P: AsRef<Path>>(
+            log_file: P,
             begin_time: i64,
             end_time: i64,
             level: LogLevel,
             filter: String,
         ) -> Result<Self, Error> {
-            let log_path: &Path = log_file.as_ref();
+            let log_path = log_file.as_ref();
             let log_name = match log_path.file_name() {
                 Some(file_name) => match file_name.to_str() {
                     Some(file_name) => file_name,
-                    None => return Err(Error::SearchError(format!("Invalid utf8: {}", log_file))),
+                    None => {
+                        return Err(Error::SearchError(format!("Invalid utf8: {:?}", file_name)))
+                    }
                 },
                 None => {
                     return Err(Error::SearchError(format!(
-                        "Illegal file name: {}",
-                        log_file
+                        "Illegal file name: {:?}",
+                        log_path
                     )))
                 }
             };
+            if log_name.is_empty() {
+                return Err(Error::InvalidRequest("Empty `log_file` path".to_owned()));
+            }
             let log_dir = match log_path.parent() {
                 Some(dir) => dir,
                 None => {
                     return Err(Error::SearchError(format!(
-                        "Illegal parent dir: {}",
-                        log_file
+                        "Illegal parent dir: {:?}",
+                        log_path
                     )))
                 }
             };
@@ -143,9 +148,8 @@ mod log {
                     Ok((file_start_time, file_end_time)) => (file_start_time, file_end_time),
                     Err(_) => continue,
                 };
-
-                if (begin_time < file_start_time && end_time > file_start_time)
-                    || (end_time > file_end_time && begin_time < file_end_time)
+                if (begin_time <= file_start_time && end_time >= file_start_time)
+                    || (end_time >= file_end_time && begin_time <= file_end_time)
                 {
                     if let Err(err) = file.seek(SeekFrom::Start(0)) {
                         warn!("seek file failed: {}, err: {}", file_name, err);
@@ -199,7 +203,7 @@ mod log {
                             match parse(&input) {
                                 Ok((content, (time, level))) => {
                                     // The remain content timestamp more the end time or this file contains inrecognation formation
-                                    if time == INVALID_TIMESTAMP && time > self.end_time {
+                                    if time == INVALID_TIMESTAMP || time > self.end_time {
                                         break;
                                     }
                                     if time < self.begin_time {
@@ -208,6 +212,7 @@ mod log {
                                     if self.level != LogLevel::All && level != self.level {
                                         continue;
                                     }
+                                    // TODO: do we need to support regular expression search
                                     if self.filter.len() > 0 && !content.contains(&self.filter) {
                                         continue;
                                     }
@@ -267,7 +272,8 @@ mod log {
         let buffer = BufReader::new(file);
         let file_start_time = match buffer.lines().nth(0) {
             Some(Ok(line)) => {
-                let (_, (time, _)) = parse(&line).map_err(|_| Error::ParseError)?;
+                let (_, (time, _)) = parse(&line)
+                    .map_err(|err| Error::ParseError(format!("Parse error: {:?}", err)))?;
                 time
             }
             Some(Err(err)) => {
@@ -280,7 +286,8 @@ mod log {
         let mut rev_lines = rev_lines::RevLines::with_capacity(512, buffer)?;
         let file_end_time = match rev_lines.nth(0) {
             Some(line) => {
-                let (_, (time, _)) = parse(&line).map_err(|_| Error::ParseError)?;
+                let (_, (time, _)) = parse(&line)
+                    .map_err(|err| Error::ParseError(format!("Parse error: {:?}", err)))?;
                 time
             }
             None => INVALID_TIMESTAMP,
@@ -292,9 +299,6 @@ mod log {
         log_file: &str,
         mut req: SearchLogRequest,
     ) -> impl Future<Item = SearchLogResponse, Error = Error> {
-        if log_file.is_empty() {
-            return err(Error::InvalidRequest("Empty `log_file` path".to_owned()));
-        }
         let begin_time = req.get_start_time();
         let end_time = req.get_end_time();
         let level = req.get_level();
@@ -325,6 +329,8 @@ mod log {
     #[cfg(test)]
     mod tests {
         use super::*;
+        use std::io::Write;
+        use tempfile::tempdir;
 
         #[test]
         fn test_parse_time() {
@@ -357,18 +363,19 @@ mod log {
             }
         }
 
+        fn timestamp(time: &str) -> i64 {
+            match DateTime::parse_from_str(time, "%Y/%m/%d %H:%M:%S%.3f %z") {
+                Ok(t) => t.timestamp_millis(),
+                Err(_) => INVALID_TIMESTAMP,
+            }
+        }
+
         #[test]
         fn test_parse() {
             // (input, time, level, message)
             let cs: Vec<(&str, &str, LogLevel, &str)> = vec![
                 (
                     "[2019/08/23 18:09:52.387 +08:00] [INFO] [foo.rs:100] [some message] [key=val]",
-                    "2019/08/23 18:09:52.387 +08:00",
-                    LogLevel::Info,
-                    "[foo.rs:100] [some message] [key=val]",
-                ),
-                (
-                    "[2019/08/23 18:09:52.387 +08:00]           [INFO] [foo.rs:100] [some message] [key=val]",
                     "2019/08/23 18:09:52.387 +08:00",
                     LogLevel::Info,
                     "[foo.rs:100] [some message] [key=val]",
@@ -456,15 +463,228 @@ mod log {
             for (input, time, level, content) in cs.into_iter() {
                 let result = parse(input);
                 assert!(result.is_ok(), "expected OK, but got: {:?}", result);
-                let timestamp = match DateTime::parse_from_str(time, "%Y/%m/%d %H:%M:%S%.3f %z") {
-                    Ok(t) => t.timestamp_millis(),
-                    Err(_) => INVALID_TIMESTAMP,
-                };
+                let timestamp = timestamp(time);
                 let log = result.unwrap();
                 assert_eq!(log.0, content);
                 assert_eq!((log.1).0, timestamp);
                 assert_eq!((log.1).1, level);
             }
+        }
+
+        #[test]
+        fn test_parse_time_range() {
+            let dir = tempdir().unwrap();
+            let log_file = dir.path().join("tikv.log");
+            let mut file = File::create(&log_file).unwrap();
+            write!(
+                file,
+                r#"[2019/08/23 18:09:52.387 +08:00] [warning] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:53.387 +08:00] [INFO] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:54.387 +08:00] [trace] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:55.387 +08:00] [DEBUG] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:56.387 +08:00] [ERROR] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:57.387 +08:00] [CRITICAL] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:58.387 +08:00] [WARN] [foo.rs:100] [some message] [key=val]"#
+            )
+            .unwrap();
+
+            let file = File::open(&log_file).unwrap();
+            let start_time = timestamp("2019/08/23 18:09:52.387 +08:00");
+            let end_time = timestamp("2019/08/23 18:09:58.387 +08:00");
+            assert_eq!(parse_time_range(&file).unwrap(), (start_time, end_time));
+        }
+
+        #[test]
+        fn test_log_iterator() {
+            let dir = tempdir().unwrap();
+            let log_file = dir.path().join("tikv.log");
+            let mut file = File::create(&log_file).unwrap();
+            write!(
+                file,
+                r#"[2019/08/23 18:09:53.387 +08:00] [INFO] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:54.387 +08:00] [trace] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:55.387 +08:00] [DEBUG] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:56.387 +08:00] [ERROR] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:57.387 +08:00] [CRITICAL] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:58.387 +08:00] [WARN] [foo.rs:100] [some message] [key=val] - test-filter
+[2019/08/23 18:09:59.387 +08:00] [warning] [foo.rs:100] [some message] [key=val]"#
+            )
+            .unwrap();
+
+            let log_file2 = dir.path().join("tikv.log.2");
+            let mut file = File::create(&log_file2).unwrap();
+            write!(
+                file,
+                r#"[2019/08/23 18:10:01.387 +08:00] [INFO] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:02.387 +08:00] [trace] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:03.387 +08:00] [DEBUG] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:04.387 +08:00] [ERROR] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:05.387 +08:00] [CRITICAL] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:06.387 +08:00] [WARN] [foo.rs:100] [some message] [key=val]"#
+            )
+            .unwrap();
+
+            // We use the timestamp as the identity of log item in following test cases
+            // all content
+            let log_iter =
+                LogIterator::new(&log_file, 0, std::i64::MAX, LogLevel::All, "".to_string())
+                    .unwrap();
+            let expected = vec![
+                "2019/08/23 18:09:53.387 +08:00",
+                "2019/08/23 18:09:54.387 +08:00",
+                "2019/08/23 18:09:55.387 +08:00",
+                "2019/08/23 18:09:56.387 +08:00",
+                "2019/08/23 18:09:57.387 +08:00",
+                "2019/08/23 18:09:58.387 +08:00",
+                "2019/08/23 18:09:59.387 +08:00",
+                "2019/08/23 18:10:01.387 +08:00",
+                "2019/08/23 18:10:02.387 +08:00",
+                "2019/08/23 18:10:03.387 +08:00",
+                "2019/08/23 18:10:04.387 +08:00",
+                "2019/08/23 18:10:05.387 +08:00",
+                "2019/08/23 18:10:06.387 +08:00",
+            ]
+            .iter()
+            .map(|s| timestamp(s))
+            .collect::<Vec<i64>>();
+            assert_eq!(
+                log_iter.map(|m| m.get_time()).collect::<Vec<i64>>(),
+                expected
+            );
+
+            // filter by time range
+            let log_iter = LogIterator::new(
+                &log_file,
+                timestamp("2019/08/23 18:09:56.387 +08:00"),
+                timestamp("2019/08/23 18:10:03.387 +08:00"),
+                LogLevel::All,
+                "".to_string(),
+            )
+            .unwrap();
+            let expected = vec![
+                "2019/08/23 18:09:56.387 +08:00",
+                "2019/08/23 18:09:57.387 +08:00",
+                "2019/08/23 18:09:58.387 +08:00",
+                "2019/08/23 18:09:59.387 +08:00",
+                "2019/08/23 18:10:01.387 +08:00",
+                "2019/08/23 18:10:02.387 +08:00",
+                "2019/08/23 18:10:03.387 +08:00",
+            ]
+            .iter()
+            .map(|s| timestamp(s))
+            .collect::<Vec<i64>>();
+            assert_eq!(
+                log_iter.map(|m| m.get_time()).collect::<Vec<i64>>(),
+                expected
+            );
+
+            // filter by log level
+            let log_iter = LogIterator::new(
+                &log_file,
+                timestamp("2019/08/23 18:09:53.387 +08:00"),
+                timestamp("2019/08/23 18:09:58.387 +08:00"),
+                LogLevel::Info,
+                "".to_string(),
+            )
+            .unwrap();
+
+            let expected = vec!["2019/08/23 18:09:53.387 +08:00"]
+                .iter()
+                .map(|s| timestamp(s))
+                .collect::<Vec<i64>>();
+            assert_eq!(
+                log_iter.map(|m| m.get_time()).collect::<Vec<i64>>(),
+                expected
+            );
+            let log_iter = LogIterator::new(
+                &log_file,
+                timestamp("2019/08/23 18:09:53.387 +08:00"),
+                std::i64::MAX,
+                LogLevel::Warn,
+                "".to_string(),
+            )
+            .unwrap();
+            let expected = vec![
+                "2019/08/23 18:09:58.387 +08:00",
+                "2019/08/23 18:09:59.387 +08:00",
+                "2019/08/23 18:10:06.387 +08:00",
+            ]
+            .iter()
+            .map(|s| timestamp(s))
+            .collect::<Vec<i64>>();
+            assert_eq!(
+                log_iter.map(|m| m.get_time()).collect::<Vec<i64>>(),
+                expected
+            );
+
+            // filter by pattern
+            let log_iter = LogIterator::new(
+                &log_file,
+                timestamp("2019/08/23 18:09:54.387 +08:00"),
+                std::i64::MAX,
+                LogLevel::Warn,
+                "test-filter".to_string(),
+            )
+            .unwrap();
+            let expected = vec!["2019/08/23 18:09:58.387 +08:00"]
+                .iter()
+                .map(|s| timestamp(s))
+                .collect::<Vec<i64>>();
+            assert_eq!(
+                log_iter.map(|m| m.get_time()).collect::<Vec<i64>>(),
+                expected
+            );
+        }
+
+        #[test]
+        fn test_search() {
+            let dir = tempdir().unwrap();
+            let log_file = dir.path().join("tikv.log");
+            let mut file = File::create(&log_file).unwrap();
+            write!(
+                file,
+                r#"[2019/08/23 18:09:53.387 +08:00] [INFO] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:54.387 +08:00] [trace] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:55.387 +08:00] [DEBUG] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:56.387 +08:00] [ERROR] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:57.387 +08:00] [CRITICAL] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:09:58.387 +08:00] [WARN] [foo.rs:100] [some message] [key=val] - test-filter
+[2019/08/23 18:09:59.387 +08:00] [warning] [foo.rs:100] [some message] [key=val]"#
+            )
+            .unwrap();
+
+            let log_file2 = dir.path().join("tikv.log.2");
+            let mut file = File::create(&log_file2).unwrap();
+            write!(
+                file,
+                r#"[2019/08/23 18:10:01.387 +08:00] [INFO] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:02.387 +08:00] [trace] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:03.387 +08:00] [DEBUG] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:04.387 +08:00] [ERROR] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:05.387 +08:00] [CRITICAL] [foo.rs:100] [some message] [key=val]
+[2019/08/23 18:10:06.387 +08:00] [WARN] [foo.rs:100] [some message] [key=val]"#
+            )
+            .unwrap();
+
+            let mut req = SearchLogRequest::new();
+            req.set_start_time(timestamp("2019/08/23 18:09:54.387 +08:00"));
+            req.set_end_time(std::i64::MAX);
+            req.set_level(LogLevel::Warn);
+            req.set_pattern("test-filter".to_string());
+            let expected = vec!["2019/08/23 18:09:58.387 +08:00"]
+                .iter()
+                .map(|s| timestamp(s))
+                .collect::<Vec<i64>>();
+            assert_eq!(
+                search(log_file.to_str().unwrap(), req)
+                    .wait()
+                    .unwrap()
+                    .take_messages()
+                    .iter()
+                    .map(|m| m.get_time())
+                    .collect::<Vec<i64>>(),
+                expected
+            );
         }
     }
 }
