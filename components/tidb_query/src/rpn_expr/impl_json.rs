4f3d440d4129ae0aef276ca9dc05811361f8a20f
Hawkingrei <hawking.rei@gmail.com>
2019-11-21 20:04:24 +0800

    improve code
    Signed-off-by: Wangweizhen <hawking.rei@gmail.com>

--- components/tidb_query/src/rpn_expr/impl_json.rs
+++ components/tidb_query/src/rpn_expr/impl_json.rs
@@ -213,11 +213,10 @@ fn json_length(args: &[ScalarValueRef]) -> Result<Option<Int>> {
         None => return Ok(None),
         Some(j) => j.to_owned(),
     };
-    Ok(j.json_length(
-        &parse_json_path_list(&args[1..])?
-            .or_else(|| Some(Vec::new()))
-            .unwrap(),
-    ))
+    let path_expr_list = parse_json_path_list(&args[1..])?
+        .or_else(|| Some(Vec::new()))
+        .unwrap();
+    Ok(j.json_length(&path_expr_list))
 }
 
 #[rpn_fn(raw_varg, min_args = 2, extra_validator = json_with_paths_validator)]
