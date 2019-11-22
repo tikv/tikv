1a95c9cdd62bab88b979cc7ac21520b0593543d4
mak <610546069@qq.com>
2019-11-22 18:56:49 +0800

    copr: vectorize InetAton
    Signed-off-by: mak <610546069@qq.com>

--- components/tidb_query/src/rpn_expr/impl_miscellaneous.rs
+++ components/tidb_query/src/rpn_expr/impl_miscellaneous.rs
@@ -25,6 +25,42 @@ pub fn is_ipv4_compat(addr: &Option<Bytes>) -> Result<Option<i64>> {
     }
 }
 
+#[rpn_fn]
+#[inline]
+pub fn inet_aton(addr: &Option<Bytes>) -> Result<Option<i64>> {
+    Ok(addr
+        .as_ref()
+        .map(|addr| String::from_utf8_lossy(addr))
+        .filter(|addr| !(addr.len() == 0 || addr.ends_with('.')))
+        .and_then(|addr| {
+            let (mut byte_result, mut result, mut dot_count): (u64, u64, usize) = (0, 0, 0);
+            for c in addr.chars() {
+                if c >= '0' && c <= '9' {
+                    let digit = c as u64 - '0' as u64;
+                    byte_result = byte_result * 10 + digit;
+                    if byte_result > 255 {
+                        return None;
+                    }
+                } else if c == '.' {
+                    dot_count += 1;
+                    if dot_count > 3 {
+                        return None;
+                    }
+                    result = (result << 8) + byte_result;
+                    byte_result = 0;
+                } else {
+                    return None;
+                }
+            }
+            if dot_count == 1 {
+                result <<= 16;
+            } else if dot_count == 2 {
+                result <<= 8;
+            }
+            Some(((result << 8) + byte_result) as i64)
+        }))
+}
+
 #[cfg(test)]
 mod tests {
     use tipb::ScalarFuncSig;
@@ -61,6 +97,35 @@ mod tests {
             let output = RpnFnScalarEvaluator::new()
                 .push_param(input)
                 .evaluate(ScalarFuncSig::IsIPv4Compat)
+                .unwrap();
+            assert_eq!(output, expect);
+        }
+    }
+
+    #[test]
+    fn test_inet_aton() {
+        let test_cases = vec![
+            (Some(b"0.0.0.0".to_vec()), Some(0)),
+            (Some(b"255.255.255.255".to_vec()), Some(4294967295)),
+            (Some(b"127.0.0.1".to_vec()), Some(2130706433)),
+            (Some(b"113.14.22.3".to_vec()), Some(1896748547)),
+            (Some(b"1".to_vec()), Some(1)),
+            (Some(b"0.1.2".to_vec()), Some(65538)),
+            (Some(b"0.1.2.3.4".to_vec()), None),
+            (Some(b"0.1.2..3".to_vec()), None),
+            (Some(b".0.1.2.3".to_vec()), None),
+            (Some(b"0.1.2.3.".to_vec()), None),
+            (Some(b"1.-2.3.4".to_vec()), None),
+            (Some(b"".to_vec()), None),
+            (Some(b"0.0.0.256".to_vec()), None),
+            (Some(b"127.0.0,1".to_vec()), None),
+            (None, None),
+        ];
+
+        for (input, expect) in test_cases {
+            let output = RpnFnScalarEvaluator::new()
+                .push_param(input)
+                .evaluate(ScalarFuncSig::InetAton)
                 .unwrap();
             assert_eq!(output, expect);
         }
