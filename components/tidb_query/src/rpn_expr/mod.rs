c4976f17897d90913915b14437f760dd4b27a2b2
gauss1314 <gauss1314@gmail.com>
2019-11-26 00:31:33 +0800

    copr: Add vectorize is_ipv4/6 (#6013)
    Signed-off-by: gauss <gauss1314@gmail.com>

--- components/tidb_query/src/rpn_expr/mod.rs
+++ components/tidb_query/src/rpn_expr/mod.rs
@@ -293,7 +293,9 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::JsonRemoveSig => json_remove_fn_meta(),
         ScalarFuncSig::InetNtoa => inet_ntoa_fn_meta(),
         ScalarFuncSig::Bin => bin_fn_meta(),
+        ScalarFuncSig::IsIPv4 => is_ipv4_fn_meta(),
         ScalarFuncSig::IsIPv4Compat => is_ipv4_compat_fn_meta(),
+        ScalarFuncSig::IsIPv6 => is_ipv6_fn_meta(),
         ScalarFuncSig::Inet6Ntoa => inet6_ntoa_fn_meta(),
         ScalarFuncSig::CastIntAsInt |
         ScalarFuncSig::CastIntAsReal |
