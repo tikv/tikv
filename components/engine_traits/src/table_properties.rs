fdcfdcd7e87d5c0cc4c6f30a9c1d01e863bf3577
Brian Anderson <andersrb@gmail.com>
2019-11-25 09:20:44 +0000

    engine_traits: Add get_range_properties_cf to TablePropertiesExt
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_traits/src/table_properties.rs
+++ components/engine_traits/src/table_properties.rs
@@ -1,7 +1,7 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
 use crate::CFHandleExt;
-use crate::errors::Result;
+use crate::errors::{Result, Error};
 use crate::range::Range;
 
 pub trait TablePropertiesExt: CFHandleExt {
@@ -12,6 +12,18 @@ pub trait TablePropertiesExt: CFHandleExt {
         cf: &Self::CFHandle,
         ranges: &[Range],
     ) -> Result<Self::TablePropertiesCollection>;
+
+    fn get_range_properties_cf(
+        &self,
+        cfname: &str,
+        start_key: &[u8],
+        end_key: &[u8],
+    ) -> Result<Self::TablePropertiesCollection> {
+        let cf = self.cf_handle(cfname)
+            .ok_or_else(|| Error::Engine(format!("cf {} not found", cfname)))?;
+        let range = Range::new(start_key, end_key);
+        Ok(self.get_properties_of_tables_in_range(cf, &[range])?)
+    }
 }
 
 pub trait TablePropertiesCollection {
