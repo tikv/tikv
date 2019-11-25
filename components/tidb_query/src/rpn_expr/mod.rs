9d969acd501495f03619e550c23b8c4b660b31f9
Mak <33799668+MaiCw4J@users.noreply.github.com>
2019-11-25 18:32:18 +0800

    copr: vectorize Inet6Ntoa (#6021)
    Signed-off-by: mak <610546069@qq.com>

--- components/tidb_query/src/rpn_expr/mod.rs
+++ components/tidb_query/src/rpn_expr/mod.rs
@@ -294,6 +294,7 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::InetNtoa => inet_ntoa_fn_meta(),
         ScalarFuncSig::Bin => bin_fn_meta(),
         ScalarFuncSig::IsIPv4Compat => is_ipv4_compat_fn_meta(),
+        ScalarFuncSig::Inet6Ntoa => inet6_ntoa_fn_meta(),
         ScalarFuncSig::CastIntAsInt |
         ScalarFuncSig::CastIntAsReal |
         ScalarFuncSig::CastIntAsString |
