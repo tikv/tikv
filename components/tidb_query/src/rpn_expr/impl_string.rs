1afacc5ac2c8748f3be646a0d1308b0efba74dba
Mak <33799668+MaiCw4J@users.noreply.github.com>
2019-11-25 15:03:18 +0800

    copr: vectorize Strcmp (#5998)
    Signed-off-by: mak <610546069@qq.com>

--- components/tidb_query/src/rpn_expr/impl_string.rs
+++ components/tidb_query/src/rpn_expr/impl_string.rs
@@ -201,6 +201,20 @@ pub fn locate_binary_3_args(
     }
 }
 
+#[rpn_fn]
+#[inline]
+pub fn strcmp(left: &Option<Bytes>, right: &Option<Bytes>) -> Result<Option<i64>> {
+    use std::cmp::Ordering::*;
+    Ok(match (left, right) {
+        (Some(left), Some(right)) => Some(match left.cmp(right) {
+            Less => -1,
+            Equal => 0,
+            Greater => 1,
+        }),
+        _ => None,
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -793,6 +807,37 @@ mod tests {
                 .evaluate(ScalarFuncSig::LocateBinary3Args)
                 .unwrap();
             assert_eq!(output, exp)
+        }
+    }
+
+    #[test]
+    fn test_strcmp() {
+        let test_cases = vec![
+            (Some(b"123".to_vec()), Some(b"123".to_vec()), Some(0)),
+            (Some(b"123".to_vec()), Some(b"1".to_vec()), Some(1)),
+            (Some(b"1".to_vec()), Some(b"123".to_vec()), Some(-1)),
+            (Some(b"123".to_vec()), Some(b"45".to_vec()), Some(-1)),
+            (
+                Some("你好".as_bytes().to_vec()),
+                Some(b"hello".to_vec()),
+                Some(1),
+            ),
+            (Some(b"".to_vec()), Some(b"123".to_vec()), Some(-1)),
+            (Some(b"123".to_vec()), Some(b"".to_vec()), Some(1)),
+            (Some(b"".to_vec()), Some(b"".to_vec()), Some(0)),
+            (None, Some(b"123".to_vec()), None),
+            (Some(b"123".to_vec()), None, None),
+            (Some(b"".to_vec()), None, None),
+            (None, Some(b"".to_vec()), None),
+        ];
+
+        for (left, right, expect_output) in test_cases {
+            let output = RpnFnScalarEvaluator::new()
+                .push_param(left)
+                .push_param(right)
+                .evaluate(ScalarFuncSig::Strcmp)
+                .unwrap();
+            assert_eq!(output, expect_output);
         }
     }
 }
