ab0cf94961b0e268182efa6f512b6742b4e2ef0b
taoyu <t1997y@vip.qq.com>
2019-11-27 02:31:58 +0800

    copr: Add vectorize elt
    Signed-off-by: taoyu <t1997y@vip.qq.com>

--- components/tidb_query/src/rpn_expr/mod.rs
+++ components/tidb_query/src/rpn_expr/mod.rs
@@ -392,6 +392,7 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::Right => right_fn_meta(),
         ScalarFuncSig::LocateBinary2Args => locate_binary_2_args_fn_meta(),
         ScalarFuncSig::LocateBinary3Args => locate_binary_3_args_fn_meta(),
+        ScalarFuncSig::Elt => elt_fn_meta(),
         ScalarFuncSig::Strcmp => strcmp_fn_meta(),
         _ => return Err(other_err!(
             "ScalarFunction {:?} is not supported in batch mode",
