c803e2e4048bd8d6495acbda6bc07a1145bfa7a5
koushiro <koushiro.cqx@gmail.com>
2019-11-29 18:24:37 +0800

    Add more tests
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- components/tidb_query/src/rpn_expr/impl_encryption.rs
+++ components/tidb_query/src/rpn_expr/impl_encryption.rs
@@ -56,15 +56,29 @@ mod tests {
     #[test]
     fn test_md5() {
         let test_cases = vec![
-            ("", "d41d8cd98f00b204e9800998ecf8427e"),
-            ("a", "0cc175b9c0f1b6a831c399e269772661"),
-            ("ab", "187ef4436122d1cc2f40dc2b92f0eba0"),
-            ("abc", "900150983cd24fb0d6963f7d28e17f72"),
-            ("123", "202cb962ac59075b964b07152d234b70"),
+            (vec![], "d41d8cd98f00b204e9800998ecf8427e"),
+            ("a".as_bytes().to_vec(), "0cc175b9c0f1b6a831c399e269772661"),
+            ("ab".as_bytes().to_vec(), "187ef4436122d1cc2f40dc2b92f0eba0"),
+            (
+                "abc".as_bytes().to_vec(),
+                "900150983cd24fb0d6963f7d28e17f72",
+            ),
+            (
+                "123".as_bytes().to_vec(),
+                "202cb962ac59075b964b07152d234b70",
+            ),
+            (
+                "你好".as_bytes().to_vec(),
+                "7eca689f0d3389d9dea66ae112e5cfd7",
+            ),
+            (
+                "分布式データベース".as_bytes().to_vec(),
+                "63c0354797bd261e2cbf8581147eeeda",
+            ),
+            (vec![0xc0, 0x80], "b26555f33aedac7b2684438cc5d4d05e"),
+            (vec![0xED, 0xA0, 0x80], "546d3dc8de10fbf8b448f678a47901e4"),
         ];
-
         for (arg, expect_output) in test_cases {
-            let arg = Some(arg.as_bytes().to_vec());
             let expect_output = Some(Bytes::from(expect_output));
 
             let output = RpnFnScalarEvaluator::new()
@@ -79,15 +93,38 @@ mod tests {
     #[test]
     fn test_sha1() {
         let test_cases = vec![
-            ("", "da39a3ee5e6b4b0d3255bfef95601890afd80709"),
-            ("a", "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8"),
-            ("ab", "da23614e02469a0d7c7bd1bdab5c9c474b1904dc"),
-            ("abc", "a9993e364706816aba3e25717850c26c9cd0d89d"),
-            ("123", "40bd001563085fc35165329ea1ff5c5ecbdbbeef"),
+            (vec![], "da39a3ee5e6b4b0d3255bfef95601890afd80709"),
+            (
+                "a".as_bytes().to_vec(),
+                "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8",
+            ),
+            (
+                "ab".as_bytes().to_vec(),
+                "da23614e02469a0d7c7bd1bdab5c9c474b1904dc",
+            ),
+            (
+                "abc".as_bytes().to_vec(),
+                "a9993e364706816aba3e25717850c26c9cd0d89d",
+            ),
+            (
+                "123".as_bytes().to_vec(),
+                "40bd001563085fc35165329ea1ff5c5ecbdbbeef",
+            ),
+            (
+                "你好".as_bytes().to_vec(),
+                "440ee0853ad1e99f962b63e459ef992d7c211722",
+            ),
+            (
+                "分布式データベース".as_bytes().to_vec(),
+                "82aa64080df2ca37550ddfc3419d75ac1df3e0d0",
+            ),
+            (vec![0xc0, 0x80], "8bf4822782a21d7ac68ece130ac36987548003bd"),
+            (
+                vec![0xED, 0xA0, 0x80],
+                "10db70ec072d000c68dd95879f9b831e43a859fd",
+            ),
         ];
-
         for (arg, expect_output) in test_cases {
-            let arg = Some(arg.as_bytes().to_vec());
             let expect_output = Some(Bytes::from(expect_output));
 
             let output = RpnFnScalarEvaluator::new()
