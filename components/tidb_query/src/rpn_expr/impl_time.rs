b992371730bac56628dffbf374439836f5ddb3ae
tw <t888y@qq.com>
2019-11-29 20:44:38 +0800

    remove format
    Signed-off-by: tw <t888y@qq.com>

--- components/tidb_query/src/rpn_expr/impl_time.rs
+++ components/tidb_query/src/rpn_expr/impl_time.rs
@@ -135,7 +135,7 @@ pub fn day_name(ctx: &mut EvalContext, t: &Option<DateTime>) -> Result<Option<By
         Some(t) => {
             if t.invalid_zero() {
                 return ctx
-                    .handle_invalid_time_error(Error::incorrect_datetime_value(&format!("{}", t)))
+                    .handle_invalid_time_error(Error::incorrect_datetime_value(t))
                     .map(|_| Ok(None))?;
             }
             use crate::codec::mysql::time::WeekdayExtension;
