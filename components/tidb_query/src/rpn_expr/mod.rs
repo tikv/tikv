9b3509f37725d93a239a2db2554f83046ebcd50f
Zhongyang Wu <zhongyang.wu@outlook.com>
2019-12-01 22:05:23 -0500

    copr: vectorize is_ipv4_mapped (#6025)
    Signed-off-by: zhongyang.wu <zhongyang.wu@outlook.com>

--- components/tidb_query/src/rpn_expr/mod.rs
+++ components/tidb_query/src/rpn_expr/mod.rs
@@ -332,6 +332,7 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::Bin => bin_fn_meta(),
         ScalarFuncSig::IsIPv4 => is_ipv4_fn_meta(),
         ScalarFuncSig::IsIPv4Compat => is_ipv4_compat_fn_meta(),
+        ScalarFuncSig::IsIPv4Mapped => is_ipv4_mapped_fn_meta(),
         ScalarFuncSig::IsIPv6 => is_ipv6_fn_meta(),
         ScalarFuncSig::Inet6Ntoa => inet6_ntoa_fn_meta(),
         ScalarFuncSig::Inet6Aton => inet6_aton_fn_meta(),
