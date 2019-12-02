9642878bf93731472e3cdd4f3953ec45c204e5ea
koushiro <koushiro.cqx@gmail.com>
2019-12-02 14:29:10 +0800

    Improve miscellaneous impls and tests
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- components/tidb_query/src/rpn_expr/mod.rs
+++ components/tidb_query/src/rpn_expr/mod.rs
@@ -328,15 +328,14 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::JsonUnquoteSig => json_unquote_fn_meta(),
         ScalarFuncSig::JsonExtractSig => json_extract_fn_meta(),
         ScalarFuncSig::JsonRemoveSig => json_remove_fn_meta(),
+        ScalarFuncSig::InetAton => inet_aton_fn_meta(),
         ScalarFuncSig::InetNtoa => inet_ntoa_fn_meta(),
-        ScalarFuncSig::Bin => bin_fn_meta(),
+        ScalarFuncSig::Inet6Aton => inet6_aton_fn_meta(),
+        ScalarFuncSig::Inet6Ntoa => inet6_ntoa_fn_meta(),
         ScalarFuncSig::IsIPv4 => is_ipv4_fn_meta(),
         ScalarFuncSig::IsIPv4Compat => is_ipv4_compat_fn_meta(),
         ScalarFuncSig::IsIPv4Mapped => is_ipv4_mapped_fn_meta(),
         ScalarFuncSig::IsIPv6 => is_ipv6_fn_meta(),
-        ScalarFuncSig::Inet6Ntoa => inet6_ntoa_fn_meta(),
-        ScalarFuncSig::Inet6Aton => inet6_aton_fn_meta(),
-        ScalarFuncSig::InetAton => inet_aton_fn_meta(),
         ScalarFuncSig::CastIntAsInt |
         ScalarFuncSig::CastIntAsReal |
         ScalarFuncSig::CastIntAsString |
@@ -386,6 +385,7 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::CastJsonAsTime |
         ScalarFuncSig::CastJsonAsDuration |
         ScalarFuncSig::CastJsonAsJson => map_cast_func(expr)?,
+        ScalarFuncSig::Bin => bin_fn_meta(),
         ScalarFuncSig::Length => length_fn_meta(),
         ScalarFuncSig::BitLength => bit_length_fn_meta(),
         ScalarFuncSig::Concat => concat_fn_meta(),
