680ef37e76e58d9be26c918382214a8de60f0a31
Neil Shen <overvenus@gmail.com>
2019-12-02 15:57:01 +0800

    lib: output build profile and enabled features
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -64,7 +64,46 @@ pub mod raftstore;
 pub mod server;
 pub mod storage;
 
+// Detect tikv enabled features.
+fn detect_enabled_features() -> String {
+    macro_rules! detect {
+        ($f:expr, $feat:tt) => {
+            if cfg!(feature = $feat) {
+                $f.push_str($feat);
+                $f.push(' ');
+            }
+        };
+    }
+
+    let mut flags = String::new();
+    detect!(flags, "tcmalloc");
+    detect!(flags, "jemalloc");
+    detect!(flags, "mimalloc");
+    detect!(flags, "portable");
+    detect!(flags, "sse");
+    detect!(flags, "mem-profiling");
+    detect!(flags, "failpoints");
+    detect!(flags, "prost-codec");
+    detect!(flags, "protobuf-codec");
+    flags.pop(); // Try to trim the last ' '.
+    flags
+}
+
+// Detect tikv build profile.
+fn detect_build_profile() -> &'static str {
+    // Detect build profile by debug_assertions see more:
+    // https://users.rust-lang.org/t/conditional-compilation-for-debug-release/1098
+    // TODO: Find a better way to detect, since debug_assertions can be enabled
+    //       in a custom profile.
+    if cfg!(debug_assertions) {
+        "debug"
+    } else {
+        "release"
+    }
+}
+
 /// Returns the tikv version information.
+// TODO: Should we remove `UTC Build Time`? It breaks reproducible builds.
 pub fn tikv_version_info() -> String {
     let fallback = "Unknown (env var does not exist when building)";
     format!(
@@ -72,12 +111,16 @@ pub fn tikv_version_info() -> String {
          \nGit Commit Hash:   {}\
          \nGit Commit Branch: {}\
          \nUTC Build Time:    {}\
-         \nRust Version:      {}",
+         \nRust Version:      {}\
+         \nFeatures:          {}\
+         \nBuild Profile:     {}",
         env!("CARGO_PKG_VERSION"),
         option_env!("TIKV_BUILD_GIT_HASH").unwrap_or(fallback),
         option_env!("TIKV_BUILD_GIT_BRANCH").unwrap_or(fallback),
         option_env!("TIKV_BUILD_TIME").unwrap_or(fallback),
         option_env!("TIKV_BUILD_RUSTC_VERSION").unwrap_or(fallback),
+        detect_enabled_features(),
+        detect_build_profile(),
     )
 }
 
