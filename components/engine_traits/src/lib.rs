5e78c2bda8b49271f8cc35fdbcac41e26cf394a3
Brian Anderson <andersrb@gmail.com>
2019-12-06 05:40:41 +0000

    engine_traits: Add comment about recursive associated types
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_traits/src/lib.rs
+++ components/engine_traits/src/lib.rs
@@ -125,6 +125,13 @@
 //!   - https://github.com/rust-lang/rfcs/pull/1598
 //!   - https://github.com/rust-lang/rust/issues/44265
 //!
+//! - Traits can't have mutually-recursive associated types. That is, if
+//!   `KvEngine` has a `Snapshot` associated type, `Snapshot` can't then have a
+//!   `KvEngine` associated type - the compiler will not be able to resolve both
+//!   `KvEngine`s to the name type. In these cases, e.g. `Snapshot` needs to be
+//!   parameterized over its engine type and `impl Snapshot<RocksEngine> for
+//!   RocksSnapshot`.
+//!
 //!
 //! # Refactoring tips
 //!
