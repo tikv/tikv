e7fa36e924c509358949b933578fd6664d675479
Mak <33799668+MaiCw4J@users.noreply.github.com>
2019-12-08 20:09:40 +0800

    copr: vectorize PeriodAdd, PeriodDiff (#6035)
    Signed-off-by: mak <610546069@qq.com>

--- components/tidb_query/src/rpn_expr/impl_time.rs
+++ components/tidb_query/src/rpn_expr/impl_time.rs
@@ -180,6 +180,35 @@ pub fn day_name(ctx: &mut EvalContext, t: &Option<DateTime>) -> Result<Option<By
     }
 }
 
+#[rpn_fn]
+#[inline]
+pub fn period_add(p: &Option<Int>, n: &Option<Int>) -> Result<Option<Int>> {
+    Ok(match (p, n) {
+        (Some(p), Some(n)) => {
+            if *p == 0 {
+                Some(0)
+            } else {
+                let (month, _) =
+                    (i64::from(DateTime::period_to_month(*p as u64) as i32)).overflowing_add(*n);
+                Some(DateTime::month_to_period(u64::from(month as u32)) as i64)
+            }
+        }
+        _ => None,
+    })
+}
+
+#[rpn_fn]
+#[inline]
+pub fn period_diff(p1: &Option<Int>, p2: &Option<Int>) -> Result<Option<Int>> {
+    match (p1, p2) {
+        (Some(p1), Some(p2)) => Ok(Some(
+            DateTime::period_to_month(*p1 as u64) as i64
+                - DateTime::period_to_month(*p2 as u64) as i64,
+        )),
+        _ => Ok(None),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -590,6 +619,68 @@ mod tests {
             if let Some(err_code) = err_code {
                 assert_eq!(ctx.warnings.warnings[0].get_code(), err_code);
             }
+        }
+    }
+
+    #[test]
+    fn test_period_add() {
+        let cases = vec![
+            (2, 222, 201808),
+            (0, 222, 0),
+            (196802, 14, 196904),
+            (6901, 13, 207002),
+            (7001, 13, 197102),
+            (200212, 9223372036854775807, 200211),
+            (9223372036854775807, 0, 27201459511),
+            (9223372036854775807, 9223372036854775807, 27201459510),
+            (201611, 2, 201701),
+            (201611, 3, 201702),
+            (201611, -13, 201510),
+            (1611, 3, 201702),
+            (7011, 3, 197102),
+            (12323, 10, 12509),
+            (0, 3, 0),
+        ];
+        for (arg1, arg2, exp) in cases {
+            let output = RpnFnScalarEvaluator::new()
+                .push_param(Some(arg1))
+                .push_param(Some(arg2))
+                .evaluate(ScalarFuncSig::PeriodAdd)
+                .unwrap();
+            assert_eq!(output, Some(exp));
+        }
+    }
+
+    #[test]
+    fn test_period_diff() {
+        let cases = vec![
+            (213002, 7010, 1912),
+            (213002, 215810, -344),
+            (2202, 9601, 313),
+            (202202, 9601, 313),
+            (200806, 6907, -733),
+            (201611, 201611, 0),
+            (200802, 200703, 11),
+            (0, 999999999, -120000086),
+            (9999999, 0, 1200086),
+            (411, 200413, -2),
+            (197000, 207700, -1284),
+            (201701, 201611, 2),
+            (201702, 201611, 3),
+            (201510, 201611, -13),
+            (201702, 1611, 3),
+            (197102, 7011, 3),
+            (12509, 12323, 10),
+            (12509, 12323, 10),
+        ];
+
+        for (arg1, arg2, exp) in cases {
+            let output = RpnFnScalarEvaluator::new()
+                .push_param(Some(arg1))
+                .push_param(Some(arg2))
+                .evaluate(ScalarFuncSig::PeriodDiff)
+                .unwrap();
+            assert_eq!(output, Some(exp));
         }
     }
 }
