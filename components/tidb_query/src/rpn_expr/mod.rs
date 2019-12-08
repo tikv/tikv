9c03639db93f1d00dcc845c4f10cb61579ffc759
gauss1314 <gauss1314@gmail.com>
2019-12-08 18:34:10 +0800

    copr: Add vectorize to_days (#6007)
    Signed-off-by: gauss <gauss1314@gmail.com>

--- components/tidb_query/src/rpn_expr/mod.rs
+++ components/tidb_query/src/rpn_expr/mod.rs
@@ -258,6 +258,7 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::DateFormatSig => date_format_fn_meta(),
         ScalarFuncSig::DayOfYear => day_of_year_fn_meta(),
         ScalarFuncSig::WeekDay => week_day_fn_meta(),
+        ScalarFuncSig::ToDays => to_days_fn_meta(),
         ScalarFuncSig::FromDays => from_days_fn_meta(),
         ScalarFuncSig::Month => month_fn_meta(),
         ScalarFuncSig::Hour => hour_fn_meta(),
