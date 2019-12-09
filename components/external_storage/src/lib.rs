3f04236400016201dff72edcb51d8e61a206adc9
kennytm <kennytm@gmail.com>
2019-12-10 03:47:56 +0800

    *: use kvproto::backup::StorageBackend in create_storage (#6186)
    Signed-off-by: kennytm <kennytm@gmail.com>

--- components/external_storage/src/lib.rs
+++ components/external_storage/src/lib.rs
@@ -9,6 +9,7 @@
     slog_error,
     slog_info,
     slog_debug,
+    slog_warn,
     slog_log,
     slog_record,
     slog_b,
@@ -25,36 +26,70 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::sync::Arc;
 
-use url::Url;
+use kvproto::backup::StorageBackend_oneof_backend as Backend;
+use kvproto::backup::{Noop, StorageBackend};
 
 mod local;
 pub use local::LocalStorage;
 mod noop;
 pub use noop::NoopStorage;
 
-/// Create a new storage from the given url.
-pub fn create_storage(url: &str) -> io::Result<Arc<dyn ExternalStorage>> {
-    let url = Url::parse(url).map_err(|e| {
-        io::Error::new(
-            io::ErrorKind::InvalidInput,
-            format!("failed to create storage {} {}", e, url),
-        )
-    })?;
-
-    match url.scheme() {
-        LocalStorage::SCHEME => {
-            let p = Path::new(url.path());
+/// Create a new storage from the given storage backend description.
+pub fn create_storage(backend: &StorageBackend) -> io::Result<Arc<dyn ExternalStorage>> {
+    match &backend.backend {
+        Some(Backend::local(local)) => {
+            let p = Path::new(&local.path);
             LocalStorage::new(p).map(|s| Arc::new(s) as _)
         }
-        NoopStorage::SCHEME => Ok(Arc::new(NoopStorage::new()) as _),
-        other => {
-            error!("unknown storage"; "scheme" => other);
+        Some(Backend::noop(_)) => Ok(Arc::new(NoopStorage::new()) as _),
+        _ => {
+            let u = url_of_backend(backend);
+            error!("unknown storage"; "scheme" => u.scheme());
             Err(io::Error::new(
                 io::ErrorKind::Other,
-                format!("unknown storage {}", url),
+                format!("unknown storage {}", u),
             ))
         }
     }
+}
+
+/// Formats the storage backend as a URL.
+pub fn url_of_backend(backend: &StorageBackend) -> url::Url {
+    let mut u = url::Url::parse("unknown:///").unwrap();
+    match &backend.backend {
+        Some(Backend::local(local)) => {
+            u.set_scheme("local").unwrap();
+            u.set_path(&local.path);
+        }
+        Some(Backend::noop(_)) => {
+            u.set_scheme("noop").unwrap();
+        }
+        Some(Backend::s3(s3)) => {
+            u.set_scheme("s3").unwrap();
+            if let Err(e) = u.set_host(Some(&s3.bucket)) {
+                warn!("ignoring invalid S3 bucket name"; "bucket" => &s3.bucket, "error" => %e);
+            }
+            u.set_path(s3.get_prefix());
+        }
+        None => {}
+    }
+    u
+}
+
+/// Creates a local `StorageBackend` to the given path.
+pub fn make_local_backend(path: &Path) -> StorageBackend {
+    let path = path.display().to_string();
+    let mut backend = StorageBackend::default();
+    backend.mut_local().set_path(path);
+    backend
+}
+
+/// Creates a noop `StorageBackend`.
+pub fn make_noop_backend() -> StorageBackend {
+    let noop = Noop::default();
+    let mut backend = StorageBackend::default();
+    backend.set_noop(noop);
+    backend
 }
 
 /// An abstraction of an external storage.
@@ -81,8 +116,33 @@ mod tests {
 
     #[test]
     fn test_create_storage() {
-        create_storage("local:///tmp/a").unwrap();
-        create_storage("noop:///foo").unwrap();
-        assert!(create_storage("invalid").is_err());
+        let backend = make_local_backend(Path::new("/tmp/a"));
+        create_storage(&backend).unwrap();
+
+        let backend = make_noop_backend();
+        create_storage(&backend).unwrap();
+
+        let backend = StorageBackend::default();
+        assert!(create_storage(&backend).is_err());
+    }
+
+    #[test]
+    fn test_url_of_backend() {
+        let backend = make_local_backend(Path::new("/tmp/a"));
+        assert_eq!(url_of_backend(&backend).to_string(), "local:///tmp/a");
+
+        let backend = make_noop_backend();
+        assert_eq!(url_of_backend(&backend).to_string(), "noop:///");
+
+        let mut backend = StorageBackend::default();
+        let s3 = backend.mut_s3();
+        s3.set_bucket("bucket".to_owned());
+        s3.set_prefix("/backup 01/prefix/".to_owned());
+        s3.set_endpoint("http://endpoint.com".to_owned());
+        // ^ only 'bucket' and 'prefix' should be visible in url_of_backend()
+        assert_eq!(
+            url_of_backend(&backend).to_string(),
+            "s3://bucket/backup%2001/prefix/"
+        );
     }
 }
