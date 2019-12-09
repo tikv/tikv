193b12ee2eaf911d77986f24b44969633db58e00
mak <610546069@qq.com>
2019-12-09 11:15:16 +0800

    fix
    Signed-off-by: mak <610546069@qq.com>

--- components/tikv_util/src/collections.rs
+++ components/tikv_util/src/collections.rs
@@ -1,13 +1,13 @@
 // Copyright 2017 TiKV Project Authors. Licensed under Apache-2.0.
 use ahash;
 use std::cmp::Eq;
-use std::hash::Hash;
+use std::hash::{BuildHasherDefault, Hash};
 
-pub type HashMap<K, V> =
-    std::collections::HashMap<K, V, std::hash::BuildHasherDefault<ahash::AHasher>>;
-pub type HashSet<T> = std::collections::HashSet<T, std::hash::BuildHasherDefault<ahash::AHasher>>;
+pub type ABuildHasher = BuildHasherDefault<ahash::AHasher>;
+pub type HashMap<K, V> = std::collections::HashMap<K, V, ABuildHasher>;
+pub type HashSet<T> = std::collections::HashSet<T, ABuildHasher>;
 pub use std::collections::hash_map::Entry as HashMapEntry;
 
 pub fn hash_set_with_capacity<T: Hash + Eq>(capacity: usize) -> HashSet<T> {
-    HashSet::with_capacity_and_hasher(capacity, ahash::ABuildHasher::default())
+    HashSet::with_capacity_and_hasher(capacity, ABuildHasher::default())
 }
