cbc35008059428585dee40dac5c64a321cf7dbb9
5kbpers <tangminghua@pingcap.com>
2019-12-10 15:43:15 +0800

    check lease before renewing
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- src/raftstore/store/fsm/peer.rs
+++ src/raftstore/store/fsm/peer.rs
@@ -50,15 +50,14 @@ use crate::raftstore::store::peer::{
 };
 use crate::raftstore::store::peer_storage::{ApplySnapResult, InvokeContext};
 use crate::raftstore::store::transport::Transport;
-use crate::raftstore::store::util::KeysInfoFormatter;
+use crate::raftstore::store::util::{KeysInfoFormatter, LeaseState};
 use crate::raftstore::store::worker::{
     CleanupSSTTask, CleanupTask, ConsistencyCheckTask, RaftlogGcTask, ReadDelegate, RegionTask,
     SplitCheckTask,
 };
-use crate::raftstore::store::PdTask;
 use crate::raftstore::store::{
-    util, CasualMessage, Config, PeerMsg, PeerTicks, RaftCommand, SignificantMsg, SnapKey,
-    SnapshotDeleter, StoreMsg,
+    util, CasualMessage, Config, PdTask, PeerMsg, PeerTicks, RaftCommand, RequestInspector,
+    SignificantMsg, SnapKey, SnapshotDeleter, StoreMsg,
 };
 use keys::{self, enc_end_key, enc_start_key};
 
@@ -794,7 +793,7 @@ impl<'a, T: Transport, C: PdClient> PeerFsmDelegate<'a, T, C> {
         }
 
         // Send a read index to renew heartbeat
-        if self.fsm.peer.is_leader() {
+        if self.fsm.peer.is_leader() && self.fsm.peer.inspect_lease() != LeaseState::Valid {
             let last_pending_read_count = self.fsm.peer.raft_group.raft.pending_read_count();
             let last_ready_read_count = self.fsm.peer.raft_group.raft.ready_read_count();
 
