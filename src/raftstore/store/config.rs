a65c05ecf7d1cd1254c99cd5e18a3b72a9415083
Jay Lee <busyjaylee@gmail.com>
2019-12-24 21:39:44 +0800

    raftstore: allow disable early apply
    Signed-off-by: Jay Lee <busyjaylee@gmail.com>

--- src/raftstore/store/config.rs
+++ src/raftstore/store/config.rs
@@ -120,6 +120,7 @@ pub struct Config {
     pub store_pool_size: usize,
     pub future_poll_size: usize,
     pub hibernate_regions: bool,
+    pub early_apply: bool,
 
     // Deprecated! These two configuration has been moved to Coprocessor.
     // They are preserved for compatibility check.
@@ -193,6 +194,7 @@ impl Default for Config {
             store_pool_size: 2,
             future_poll_size: 1,
             hibernate_regions: true,
+            early_apply: true,
 
             // They are preserved for compatibility check.
             region_max_size: ReadableSize(0),
