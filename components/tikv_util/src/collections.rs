b44c4b86be683572f06cee788de600b4ee07ddbc
jiannuan <gan.jiannuan@gmail.com>
2020-01-03 14:17:53 +0800

    Use aHash instead of FxHash (#6183)
    Signed-off-by: jiannuan <gan.jiannuan@gmail.com>

--- components/tikv_util/src/collections.rs
+++ components/tikv_util/src/collections.rs
@@ -1,13 +1,13 @@
 // Copyright 2017 TiKV Project Authors. Licensed under Apache-2.0.
-use fxhash;
+use ahash;
 use std::cmp::Eq;
 use std::hash::Hash;
 
 pub type HashMap<K, V> =
-    std::collections::HashMap<K, V, std::hash::BuildHasherDefault<fxhash::FxHasher>>;
-pub type HashSet<T> = std::collections::HashSet<T, std::hash::BuildHasherDefault<fxhash::FxHasher>>;
+    std::collections::HashMap<K, V, ahash::ABuildHasher>;
+pub type HashSet<T> = std::collections::HashSet<T, ahash::ABuildHasher>;
 pub use std::collections::hash_map::Entry as HashMapEntry;
 
 pub fn hash_set_with_capacity<T: Hash + Eq>(capacity: usize) -> HashSet<T> {
-    HashSet::with_capacity_and_hasher(capacity, fxhash::FxBuildHasher::default())
+    HashSet::with_capacity_and_hasher(capacity, ahash::ABuildHasher)
 }
