26d8be481e8e92f75ec3750720bb796870d0325d
Neil Shen <overvenus@gmail.com>
2020-01-14 16:45:06 +0800

    address comment
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- scripts/check-udeps.sh
+++ scripts/check-udeps.sh
@@ -3,4 +3,4 @@
 # This script makes sure that there is unused dependencies in the tikv
 # dependencies tree. It is suitable to run as part of CI.
 
-cargo install cargo-udeps && cargo udeps
+which cargo-udeps &>/dev/null || cargo install cargo-udeps && cargo udeps
