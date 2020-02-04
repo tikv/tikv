e42d6d77925fe88aea18451cd4f0060ef3000b05
Neil Shen <overvenus@gmail.com>
2020-02-04 13:25:22 +0800

    cdc: add CDC delegate
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- src/storage/mvcc/reader/mod.rs
+++ src/storage/mvcc/reader/mod.rs
@@ -6,4 +6,5 @@ mod scanner;
 
 pub use self::point_getter::{PointGetter, PointGetterBuilder};
 pub use self::reader::{check_need_gc, MvccReader};
+pub use self::scanner::test_util;
 pub use self::scanner::{has_data_in_range, DeltaScanner, EntryScanner, Scanner, ScannerBuilder};
