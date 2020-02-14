c26ac6025d9d6779d429ce4907fd2a6f54fb0302
Fullstop000 <fullstop1005@gmail.com>
2020-02-14 13:56:56 +0800

    copr: introduce Binary Json (#6388)
    Signed-off-by: Fullstop000 <fullstop1005@gmail.com>

--- components/tidb_query/src/codec/data_type/mod.rs
+++ components/tidb_query/src/codec/data_type/mod.rs
@@ -10,7 +10,7 @@ mod vector;
 pub type Int = i64;
 pub type Real = ordered_float::NotNan<f64>;
 pub type Bytes = Vec<u8>;
-pub use crate::codec::mysql::{Decimal, Duration, Json, Time as DateTime};
+pub use crate::codec::mysql::{Decimal, Duration, Json, JsonType, Time as DateTime};
 
 // Dynamic eval types.
 pub use self::scalar::{ScalarValue, ScalarValueRef};
