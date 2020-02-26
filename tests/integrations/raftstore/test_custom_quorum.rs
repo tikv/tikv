32282281680b0ae9e69aaa846117af9cfc489c71
qupeng <qupeng@pingcap.com>
2020-02-26 18:12:26 +0800

    address comments
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- tests/integrations/raftstore/test_custom_quorum.rs
+++ tests/integrations/raftstore/test_custom_quorum.rs
@@ -1,7 +1,5 @@
 // Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
 
-use std::time::Duration;
-
 use raftstore::store::QuorumAlgorithm;
 use test_raftstore::*;
 
@@ -16,9 +14,6 @@ fn test_integration_on_half_fail_quorum_fn() {
     // After peer 4 and 5 fail, no new leader could be elected.
     cluster.stop_node(4);
     cluster.stop_node(5);
-    let epoch = cluster.get_region_epoch(1);
-    let cmd = new_admin_request(1, &epoch, new_transfer_leader_cmd(new_peer(2, 2)));
-    let _ = cluster.call_command_on_leader(cmd, Duration::from_secs(5));
     for _ in 0..500 {
         sleep_ms(10);
         if cluster.leader_of_region(1).is_none() {
