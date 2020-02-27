e5ffc4d7aab29f961b61d4e176d1a57e32d0270c
Lei Zhao <zlwgx1023@gmail.com>
2020-02-27 14:48:56 +0800

    txn: pessimistic lock returns multiple values (#6696)
    Signed-off-by: youjiali1995 <zlwgx1023@gmail.com>

--- src/server/lock_manager/waiter_manager.rs
+++ src/server/lock_manager/waiter_manager.rs
@@ -252,9 +252,9 @@ impl Waiter {
     /// Extracts key and primary key from `ProcessResult`.
     fn extract_key_info(&mut self) -> (Vec<u8>, Vec<u8>) {
         match &mut self.pr {
-            ProcessResult::MultiRes { results } => match results.pop().expect("mustn't be empty") {
+            ProcessResult::PessimisticLockRes { res } => match res {
                 Err(StorageError(box StorageErrorInner::Txn(TxnError(
-                    box TxnErrorInner::Mvcc(MvccError(box MvccErrorInner::KeyIsLocked(mut info))),
+                    box TxnErrorInner::Mvcc(MvccError(box MvccErrorInner::KeyIsLocked(info))),
                 )))) => (info.take_key(), info.take_primary_lock()),
                 _ => panic!("unexpected mvcc error"),
             },
@@ -616,6 +616,7 @@ impl FutureRunnable<Task> for WaiterManager {
 #[cfg(test)]
 pub mod tests {
     use super::*;
+    use crate::storage::PessimisticLockRes;
     use tikv_util::future::paired_future_callback;
     use tikv_util::worker::FutureWorker;
 
@@ -705,7 +706,9 @@ pub mod tests {
     pub(crate) type WaiterCtx = (
         Waiter,
         LockInfo,
-        tokio_sync::oneshot::Receiver<Result<Vec<Result<(), StorageError>>, StorageError>>,
+        tokio_sync::oneshot::Receiver<
+            Result<Result<PessimisticLockRes, StorageError>, StorageError>,
+        >,
     );
 
     pub(crate) fn new_test_waiter(
@@ -721,10 +724,10 @@ pub mod tests {
         info.set_primary_lock(primary);
         info.set_lock_ttl(3000);
         info.set_txn_size(16);
-        let pr = ProcessResult::MultiRes {
-            results: vec![Err(StorageError::from(TxnError::from(MvccError::from(
+        let pr = ProcessResult::PessimisticLockRes {
+            res: Err(StorageError::from(TxnError::from(MvccError::from(
                 MvccErrorInner::KeyIsLocked(info.clone()),
-            ))))],
+            )))),
         };
         let lock = Lock {
             ts: lock_ts,
@@ -733,7 +736,7 @@ pub mod tests {
         let (cb, f) = paired_future_callback();
         let waiter = Waiter::new(
             waiter_ts,
-            StorageCallback::Booleans(cb),
+            StorageCallback::PessimisticLock(cb),
             pr,
             lock,
             Instant::now() + Duration::from_millis(3000),
@@ -823,7 +826,7 @@ pub mod tests {
     fn test_waiter_notify() {
         let (waiter, lock_info, f) = new_test_waiter(10.into(), 20.into(), 20);
         waiter.notify();
-        expect_key_is_locked(f.wait().unwrap().unwrap().pop().unwrap(), lock_info);
+        expect_key_is_locked(f.wait().unwrap().unwrap(), lock_info);
 
         // A waiter can conflict with other transactions more than once.
         for conflict_times in 1..=3 {
@@ -1055,7 +1058,7 @@ pub mod tests {
             WaitTimeout::Millis(1000),
         );
         assert_elapsed(
-            || expect_key_is_locked(f.wait().unwrap().unwrap().pop().unwrap(), lock_info),
+            || expect_key_is_locked(f.wait().unwrap().unwrap(), lock_info),
             900,
             1200,
         );
@@ -1070,7 +1073,7 @@ pub mod tests {
             WaitTimeout::Millis(100),
         );
         assert_elapsed(
-            || expect_key_is_locked(f.wait().unwrap().unwrap().pop().unwrap(), lock_info),
+            || expect_key_is_locked(f.wait().unwrap().unwrap(), lock_info),
             50,
             300,
         );
@@ -1085,7 +1088,7 @@ pub mod tests {
             WaitTimeout::Millis(3000),
         );
         assert_elapsed(
-            || expect_key_is_locked(f.wait().unwrap().unwrap().pop().unwrap(), lock_info),
+            || expect_key_is_locked(f.wait().unwrap().unwrap(), lock_info),
             900,
             1200,
         );
@@ -1271,13 +1274,13 @@ pub mod tests {
         );
         // Should notify duplicated waiter immediately.
         assert_elapsed(
-            || expect_key_is_locked(f1.wait().unwrap().unwrap().pop().unwrap(), lock_info1),
+            || expect_key_is_locked(f1.wait().unwrap().unwrap(), lock_info1),
             0,
             200,
         );
         // The new waiter will be wake up after timeout.
         assert_elapsed(
-            || expect_key_is_locked(f2.wait().unwrap().unwrap().pop().unwrap(), lock_info2),
+            || expect_key_is_locked(f2.wait().unwrap().unwrap(), lock_info2),
             900,
             1200,
         );
