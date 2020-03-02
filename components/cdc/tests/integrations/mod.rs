5ee4322ce7b9fe375b77de80e4dfd244782e5eb5
5kbpers <20279863+5kbpers@users.noreply.github.com>
2020-03-02 15:36:41 +0800

    cdc: check leadership before subscribing (#6691)
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/cdc/tests/integrations/mod.rs
+++ components/cdc/tests/integrations/mod.rs
@@ -2,10 +2,28 @@
 
 mod test_cdc;
 
+use std::sync::*;
+
+static INIT: Once = Once::new();
+
 pub fn init() {
-    use std::sync::*;
-    static INIT: Once = Once::new();
-    INIT.call_once(|| {
-        test_util::setup_for_ci();
+    INIT.call_once(test_util::setup_for_ci);
+}
+
+fn setup_fail<'a>() -> fail::FailScenario<'a> {
+    INIT.call_once(test_util::setup_for_ci);
+    fail::FailScenario::setup()
+}
+
+#[test]
+fn test_setup_fail() {
+    let _ = std::thread::spawn(move || {
+        let _ = setup_fail();
+        panic_hook::mute();
+        let _g = setup_fail();
+        panic!("Poison!");
     })
+    .join();
+
+    let _g = setup_fail();
 }
