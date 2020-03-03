8cd8299e26b2b04cb46c54c747a16ab549122204
Brian Anderson <andersrb@gmail.com>
2020-03-03 14:53:00 -0700

    Update components/engine_traits/src/properties.rs
    Co-Authored-By: kennytm <kennytm@gmail.com>
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_traits/src/properties.rs
+++ components/engine_traits/src/properties.rs
@@ -1,4 +1,4 @@
-// Copyright 2017 TiKV Project Authors. Licensed under Apache-2.0.
+// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
 
 use std::collections::BTreeMap;
 use std::io::Read;
