7236f95933d41c4927a67c55ce9c1ad6ce4d2d74
Little-Wallace <bupt2013211450@gmail.com>
2020-03-03 13:06:27 +0800

    fix config test
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- tests/integrations/config/mod.rs
+++ tests/integrations/config/mod.rs
@@ -176,6 +176,7 @@ fn test_serde_custom_tikv_config() {
         store_pool_size: 3,
         future_poll_size: 2,
         hibernate_regions: false,
+        enable_multi_batch_write: true,
     };
     value.pd = PdConfig::new(vec!["example.com:443".to_owned()]);
     let titan_cf_config = TitanCfConfig {
