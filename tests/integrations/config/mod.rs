026e0fabb7f6ef8c423ba5793173c8fe3ed0ef53
Fullstop000 <fullstop1005@gmail.com>
2020-03-04 19:31:30 +0800

    improve test_serde_custom_tikv_config
    Signed-off-by: Fullstop000 <fullstop1005@gmail.com>

--- tests/integrations/config/mod.rs
+++ tests/integrations/config/mod.rs
@@ -50,6 +50,7 @@ fn test_serde_custom_tikv_config() {
     let mut value = TiKvConfig::default();
     value.log_level = Level::Debug;
     value.log_file = "foo".to_owned();
+    value.slow_log_file = "slow_foo".to_owned();
     value.server = ServerConfig {
         cluster_id: 0, // KEEP IT ZERO, it is skipped by serde.
         addr: "example.com:443".to_owned(),
