6c5f4790c5a01dbcc63c6ce1f0160b49dfdf1e6d
Rustin-Liu <rustin.liu@gmail.com>
2020-03-08 13:47:16 +0800

    tikv_alloc: add error
    Signed-off-by: Rustin-Liu <rustin.liu@gmail.com>

--- components/tikv_alloc/src/error.rs
+++ components/tikv_alloc/src/error.rs
@@ -6,7 +6,8 @@ pub enum ProfError {
     MemProfilingNotEnabled,
     IOError(std::io::Error),
     JemallocError(i32),
-    PathError(std::ffi::OsString), // When temp files are in a non-unicode directory, OsString.into_string() will cause this error
+    PathEncodingError(std::ffi::OsString), // When temp files are in a non-unicode directory, OsString.into_string() will cause this error,
+    PtahError(std::ffi::NulError)
 }
 
 pub type ProfResult<T> = std::result::Result<T, ProfError>;
@@ -17,7 +18,8 @@ impl fmt::Display for ProfError {
             ProfError::MemProfilingNotEnabled => write!(f, "mem-profiling was not enabled"),
             ProfError::IOError(e) => write!(f, "io error occurred {:?}", e),
             ProfError::JemallocError(e) => write!(f, "jemalloc error {}", e),
-            ProfError::PathError(path) => write!(f, "Dump target path {:?} is non-unicode ", path),
+            ProfError::PathEncodingError(path) => write!(f, "Dump target path {:?} is non-unicode", path),
+            ProfError::PtahError(path) => write!(f, "Dump target path {:?} is invalid", path),
         }
     }
 }
