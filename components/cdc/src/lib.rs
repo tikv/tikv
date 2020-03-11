56fc203147d3d568b68e825283bb02beb2267d28
5kbpers <20279863+5kbpers@users.noreply.github.com>
2020-03-11 20:29:34 +0800

    cdc: support batch service (#6733)
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/cdc/src/lib.rs
+++ components/cdc/src/lib.rs
@@ -1,9 +1,6 @@
 // Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
 
 #![feature(box_patterns)]
-// TODO remove following allows.
-#![allow(dead_code)]
-#![allow(unused_imports)]
 
 #[macro_use]
 extern crate slog_global;
