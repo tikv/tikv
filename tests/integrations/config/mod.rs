1bd180232f5d4eec0db94f7b10cdee477f1e7e6c
Xintao <hunterlxt@live.com>
2020-03-22 16:41:19 +0800

    add toml config test for cert-allowed-cn
    Signed-off-by: Xintao <hunterlxt@live.com>

--- tests/integrations/config/mod.rs
+++ tests/integrations/config/mod.rs
@@ -533,13 +533,15 @@ fn test_serde_custom_tikv_config() {
         region_max_keys: 100000,
         region_split_keys: 100000,
     };
+    let mut cert_allowed_cn = HashSet::default();
+    cert_allowed_cn.insert("example.tikv.com".to_owned());
     value.security = SecurityConfig {
         ca_path: "invalid path".to_owned(),
         cert_path: "invalid path".to_owned(),
         key_path: "invalid path".to_owned(),
         override_ssl_target: "".to_owned(),
         cipher_file: "invalid path".to_owned(),
-        cert_allowed_cn: HashSet::default(),
+        cert_allowed_cn,
     };
     value.import = ImportConfig {
         num_threads: 123,
