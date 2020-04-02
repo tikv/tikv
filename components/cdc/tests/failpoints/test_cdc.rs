b1db977da8a0457d8dd273276a0c881f4938d95b
5kbpers <tangminghua@pingcap.com>
2020-04-02 18:48:35 +0800

    cdc: observe region changed events
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/cdc/tests/failpoints/test_cdc.rs
+++ components/cdc/tests/failpoints/test_cdc.rs
@@ -113,3 +113,90 @@ fn test_region_ready_after_deregister() {
     event_feed_wrap.as_ref().replace(None);
     suite.stop();
 }
+
+#[test]
+fn test_merge() {
+    let mut suite = TestSuite::new(1);
+    // Split region
+    let region = suite.cluster.get_region(&[]);
+    suite.cluster.must_split(&region, b"k1");
+    // Subscribe source region
+    let source = suite.cluster.get_region(b"k0");
+    let mut req = ChangeDataRequest::default();
+    req.region_id = source.get_id();
+    req.set_region_epoch(source.region_epoch.clone());
+    let (source_tx, source_wrap, source_event) =
+        new_event_feed(suite.get_region_cdc_client(source.get_id()));
+    let source_tx = source_tx
+        .send((req.clone(), WriteFlags::default()))
+        .wait()
+        .unwrap();
+    // Subscribe target region
+    let target = suite.cluster.get_region(b"k2");
+    req.region_id = target.get_id();
+    req.set_region_epoch(target.region_epoch.clone());
+    let (target_tx, target_wrap, target_event) =
+        new_event_feed(suite.get_region_cdc_client(target.get_id()));
+    let target_tx = target_tx
+        .send((req.clone(), WriteFlags::default()))
+        .wait()
+        .unwrap();
+    // Pause before completint commit merge
+    let fp = "before_handle_catch_up_logs_for_merge";
+    fail::cfg(fp, "pause").unwrap();
+    // The call is finished when prepare_merge is applied.
+    cluster.try_merge(source.get_id(), target.get_id());
+    // Epoch not match after prepare_merge
+    let mut events = source_event(false);
+    assert_eq!(events.len(), 1, "{:?}", events);
+    match events.pop().unwrap().event.unwrap() {
+        Event_oneof_event::Error(err) => {
+            assert!(err.has_epoch_not_match(), "{:?}", err);
+        }
+        _ => panic!("unknown event"),
+    }
+    let mut events = target_event(false);
+    assert_eq!(events.len(), 1, "{:?}", events);
+    match events.pop().unwrap().event.unwrap() {
+        Event_oneof_event::Error(err) => {
+            assert!(err.has_epoch_not_match(), "{:?}", err);
+        }
+        _ => panic!("unknown event"),
+    }
+    req.region_id = source.get_id();
+    req.set_region_epoch(source.region_epoch.clone());
+    // Retry to subscribe source region
+    let source = suite.cluster.get_region(b"k0");
+    req.region_id = source.get_id();
+    req.set_region_epoch(source.region_epoch.clone());
+    let _source_tx = source_tx
+        .send((req.clone(), WriteFlags::default()))
+        .wait()
+        .unwrap();
+    // Retry to subscribe target region
+    let target = suite.cluster.get_region(b"k0");
+    req.region_id = target.get_id();
+    req.set_region_epoch(target.region_epoch.clone());
+    let _target_tx = target_tx
+        .send((req.clone(), WriteFlags::default()))
+        .wait()
+        .unwrap();
+    // Continue to commit merge
+    fail::remove(fp);
+    let mut events = source_event(false);
+    assert_eq!(events.len(), 1, "{:?}", events);
+    match events.pop().unwrap().event.unwrap() {
+        Event_oneof_event::Error(err) => {
+            assert!(err.has_epoch_not_match(), "{:?}", err);
+        }
+        _ => panic!("unknown event"),
+    }
+    let mut events = target_event(false);
+    assert_eq!(events.len(), 1, "{:?}", events);
+    match events.pop().unwrap().event.unwrap() {
+        Event_oneof_event::Error(err) => {
+            assert!(err.has_region_not_found(), "{:?}", err);
+        }
+        _ => panic!("unknown event"),
+    }
+}
