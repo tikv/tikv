5e938114cd6a1d29c86bf216fb771ea921fd1fda
zhongyang.wu <zhongyang.wu@outlook.com>
2020-04-01 23:06:12 -0400

    make clippy happy
    Signed-off-by: zhongyang.wu <zhongyang.wu@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -89,7 +89,7 @@ pub fn tikv_version_info() -> String {
 /// Prints the tikv version information to the standard output.
 pub fn log_tikv_info() {
     info!("Welcome to TiKV");
-    for line in tikv_version_info().lines().filter(|s| s.len() > 0) {
+    for line in tikv_version_info().lines().filter(|s| !s.is_empty()) {
         info!("{}", line);
     }
 }
