254ee7d64b7a69dec8f8c58205db8a91ec0409d4
TXXT <hunterlxt@live.com>
2020-04-09 16:21:05 +0800

    protect correct lifetime for lock file
    Signed-off-by: TXXT <hunterlxt@live.com>

--- cmd/src/server.rs
+++ cmd/src/server.rs
@@ -126,6 +126,7 @@ struct TiKVServer {
     region_info_accessor: RegionInfoAccessor,
     coprocessor_host: Option<CoprocessorHost>,
     to_stop: Vec<Box<dyn Stop>>,
+    lock_file: Option<File>,
 }
 
 struct Engines {
@@ -185,6 +186,7 @@ impl TiKVServer {
             region_info_accessor,
             coprocessor_host,
             to_stop: vec![Box::new(resolve_worker)],
+            lock_file: None,
         }
     }
 
@@ -303,6 +305,7 @@ impl TiKVServer {
                 self.store_path.display()
             );
         }
+        self.lock_file = Some(f);
 
         if tikv_util::panic_mark_file_exists(&self.config.storage.data_dir) {
             fatal!(
