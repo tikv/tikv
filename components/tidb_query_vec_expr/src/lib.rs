0ba95e2cad8ad5943fcb7aa2aa49b04168097d96
Qiannan Lyu <lvqiannan@gmail.com>
2020-04-17 16:55:58 +1200

    add builtin function lpad_utf8
    Signed-off-by: Qiannan Lyu <lvqiannan@gmail.com>

--- components/tidb_query_vec_expr/src/lib.rs
+++ components/tidb_query_vec_expr/src/lib.rs
@@ -509,6 +509,7 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::LTrim => ltrim_fn_meta(),
         ScalarFuncSig::RTrim => rtrim_fn_meta(),
         ScalarFuncSig::Lpad => lpad_fn_meta(),
+        ScalarFuncSig::LpadUtf8 => lpad_utf8_fn_meta(),
         ScalarFuncSig::Trim1Arg => trim_1_arg_fn_meta(),
         ScalarFuncSig::Replace => replace_fn_meta(),
         ScalarFuncSig::Left => left_fn_meta(),
