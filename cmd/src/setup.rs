c32d901c250d51fd44ceae2c53043ac90532fe63
Deardrops <passagesfall@gmail.com>
2020-04-20 16:15:59 +0800

    cargo format
    Signed-off-by: Deardrops <passagesfall@gmail.com>

--- cmd/src/setup.rs
+++ cmd/src/setup.rs
@@ -32,14 +32,7 @@ macro_rules! fatal {
 // number while rotate by size.
 fn rename_by_timestamp(path: &Path) -> io::Result<PathBuf> {
     let mut new_path = path.to_path_buf().into_os_string();
-<<<<<<< HEAD
-    new_path.push(format!("{}", Local::now().format("%Y-%m-%d-%H:%M:%S%.f")));
-=======
-    new_path.push(format!(
-        ".{}",
-        Local::now().format(logger::DATETIME_ROTATE_SUFFIX)
-    ));
->>>>>>> 700f23b... tikv_util: fix compatibility issue in tikv rotating log file naming (#7437)
+    new_path.push(format!(".{}", Local::now().format("%Y-%m-%d-%H:%M:%S%.f")));
     Ok(PathBuf::from(new_path))
 }
 
