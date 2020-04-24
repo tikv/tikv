90d0739beb6b2213bb2e3db314226b080995e019
qupeng <qupeng@pingcap.com>
2020-04-24 15:11:53 +0800

    raftstore: remove `SnapshotDeleter` and simplify APIs (#7534)
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- components/raftstore/src/store/fsm/store.rs
+++ components/raftstore/src/store/fsm/store.rs
@@ -52,8 +52,8 @@ use crate::store::worker::{
 };
 use crate::store::PdTask;
 use crate::store::{
-    util, Callback, CasualMessage, PeerMsg, RaftCommand, SignificantMsg, SnapManager,
-    SnapshotDeleter, StoreMsg, StoreTick,
+    util, Callback, CasualMessage, PeerMsg, RaftCommand, SignificantMsg, SnapManager, StoreMsg,
+    StoreTick,
 };
 use crate::Result;
 use engine_rocks::{CompactedEvent, CompactionListener};
