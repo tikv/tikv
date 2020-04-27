7b4344dd59224694622a760530d60a0d74d167dc
Lei Zhao <zlwgx1023@gmail.com>
2020-04-27 14:57:18 +0800

    txn: only wake up waiters when locks are indeed released (#7379) (#7585)
    Signed-off-by: youjiali1995 <zlwgx1023@gmail.com>

---
+++ benches/misc/storage/key.rs
@@ -0,0 +1,31 @@
+// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
+
+use rand::{self, Rng, RngCore};
+use tikv::coprocessor::codec::{datum, table, Datum};
+use tikv::storage::Key;
+
+#[inline]
+fn gen_rand_str(len: usize) -> Vec<u8> {
+    let mut rand_str = vec![0; len];
+    rand::thread_rng().fill_bytes(&mut rand_str);
+    rand_str
+}
+
+#[bench]
+fn bench_row_key_gen_hash(b: &mut test::Bencher) {
+    let id: i64 = rand::thread_rng().gen();
+    let row_key = Key::from_raw(&table::encode_row_key(id, id));
+    b.iter(|| {
+        test::black_box(row_key.gen_hash());
+    });
+}
+
+#[bench]
+fn bench_index_key_gen_hash(b: &mut test::Bencher) {
+    let id: i64 = rand::thread_rng().gen();
+    let encoded_index_val = datum::encode_key(&[Datum::Bytes(gen_rand_str(64))]).unwrap();
+    let index_key = Key::from_raw(&table::encode_index_seek_key(id, id, &encoded_index_val));
+    b.iter(|| {
+        test::black_box(index_key.gen_hash());
+    });
+}
