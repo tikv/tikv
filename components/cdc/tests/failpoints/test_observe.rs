8315f17e09296af3ddb5241a5e8eab0c81ee93c5
5kbpers <tangminghua@pingcap.com>
2020-04-29 01:10:37 +0800

    fix test
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/cdc/tests/failpoints/test_observe.rs
+++ components/cdc/tests/failpoints/test_observe.rs
@@ -14,11 +14,13 @@ use kvproto::kvrpcpb::*;
 use kvproto::raft_serverpb::RaftMessage;
 use pd_client::PdClient;
 use raft::eraftpb::MessageType;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::time::Duration;
 use test_raftstore::*;
 use tikv_util::config::ReadableDuration;
+use tikv_util::HandyRwLock;
 
 #[test]
 fn test_observe_duplicate_cmd() {
@@ -135,21 +137,32 @@ fn test_observe_duplicate_cmd() {
 #[test]
 fn test_delayed_change_cmd() {
     let mut cluster = new_server_cluster(1, 3);
-    cluster.cfg.raft_store.raft_store_max_leader_lease = ReadableDuration::millis(150);
+    configure_for_lease_read(&mut cluster, Some(50), Some(20));
+    cluster.cfg.raft_store.raft_store_max_leader_lease = ReadableDuration::millis(100);
+    cluster.pd_client.disable_default_operator();
     let mut suite = TestSuite::with_cluster(3, cluster);
-    let region = suite.cluster.get_region(&[]);
-    let leader = suite.cluster.leader_of_region(region.get_id()).unwrap();
+    suite.cluster.must_put(b"k1", b"v1");
+    let region = suite.cluster.pd_client.get_region(&[]).unwrap();
+    let leader = new_peer(1, 1);
+    suite
+        .cluster
+        .must_transfer_leader(region.get_id(), leader.clone());
     // Wait util lease expired
-    sleep_ms(200);
+    sleep_ms(300);
 
-    let (read_index_sx, read_index_rx) = mpsc::sync_channel::<RaftMessage>(10);
+    let (sx, rx) = mpsc::sync_channel::<RaftMessage>(1);
+    let send_flag = Arc::new(AtomicBool::new(true));
     let send_read_index_filter = RegionPacketFilter::new(region.get_id(), leader.get_store_id())
         .direction(Direction::Send)
-        .msg_type(MessageType::MsgReadIndex)
+        .msg_type(MessageType::MsgHeartbeat)
         .set_msg_callback(Arc::new(move |msg: &RaftMessage| {
-            read_index_sx.send(msg.clone()).unwrap();
+            if send_flag.compare_and_swap(true, false, Ordering::SeqCst) {
+                sx.send(msg.clone()).unwrap();
+            }
         }));
-    suite.cluster.add_send_filter(CloneFilterFactory(send_read_index_filter));
+    suite
+        .cluster
+        .add_send_filter(CloneFilterFactory(send_read_index_filter));
 
     let mut req = ChangeDataRequest::default();
     req.region_id = region.get_id();
@@ -161,14 +174,7 @@ fn test_delayed_change_cmd() {
         .wait()
         .unwrap();
 
-    read_index_rx.recv_timeout(Duration::from_secs(1)).unwrap();
-
-    let start_ts = suite.cluster.pd_client.get_tso().wait().unwrap();
-    let mut mutation = Mutation::default();
-    mutation.set_op(Op::Put);
-    mutation.key = b"k".to_vec();
-    mutation.value = b"v".to_vec();
-    suite.must_kv_prewrite(region.get_id(), vec![mutation], b"k".to_vec(), start_ts);
+    suite.cluster.must_put(b"k2", b"v2");
 
     let (req_tx, resp_rx) = suite
         .get_region_cdc_client(region.get_id())
@@ -176,32 +182,33 @@ fn test_delayed_change_cmd() {
         .unwrap();
     event_feed_wrap.as_ref().replace(Some(resp_rx));
     let _req_tx = req_tx.send((req, WriteFlags::default())).wait().unwrap();
+    sleep_ms(200);
 
-    // Commit
-    let commit_ts = suite.cluster.pd_client.get_tso().wait().unwrap();
-    suite.must_kv_commit(1, vec![b"k".to_vec()], start_ts, commit_ts);
+    suite
+        .cluster
+        .sim
+        .wl()
+        .clear_send_filters(leader.get_store_id());
+    rx.recv_timeout(Duration::from_secs(1)).unwrap();
 
-    let mut events = receive_event(false);
-    while events.len() < 2 {
-        events.extend(receive_event(false).into_iter());
-    }
-    assert_eq!(events.len(), 2);
-    for event in events {
-        match event.event.unwrap() {
-            Event_oneof_event::Entries(es) => match es.entries.len() {
-                1 => {
-                    assert_eq!(es.entries[0].get_type(), EventLogType::Commit, "{:?}", es);
+    let mut counter = 0;
+    loop {
+        for e in receive_event(true) {
+            match e.event.unwrap() {
+                Event_oneof_event::ResolvedTs(ts) => {
+                    assert_ne!(0, ts);
+                    counter += 1;
                 }
-                2 => {
+                Event_oneof_event::Entries(es) => {
+                    assert!(es.entries.len() == 1, "{:?}", es);
                     let e = &es.entries[0];
-                    assert_eq!(e.get_type(), EventLogType::Prewrite, "{:?}", es);
-                    let e = &es.entries[1];
                     assert_eq!(e.get_type(), EventLogType::Initialized, "{:?}", es);
                 }
-                _ => panic!("{:?}", es),
-            },
-            Event_oneof_event::Error(e) => panic!("{:?}", e),
-            _ => panic!("unknown event"),
+                _ => panic!("unknown event"),
+            }
+        }
+        if counter > 3 {
+            break;
         }
     }
 
