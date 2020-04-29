b4dfe53edb2d2e90908d3e9444c2cf26ded36790
Wenxuan <breezewish@pingcap.com>
2020-04-29 11:28:57 +0800

    [release-3.0] *: reduce sys_getdents syscall (#7306) (#7559)
    Signed-off-by: Breezewish <breezewish@pingcap.com>

--- components/tikv_util/src/metrics/mod.rs
+++ components/tikv_util/src/metrics/mod.rs
@@ -10,10 +10,20 @@ mod threads_linux;
 #[cfg(target_os = "linux")]
 pub use self::threads_linux::{cpu_total, get_thread_ids, monitor_threads};
 
+#[cfg(target_os = "linux")]
+mod memory_linux;
+#[cfg(target_os = "linux")]
+pub use self::memory_linux::monitor_memory;
+
 #[cfg(not(target_os = "linux"))]
 mod threads_dummy;
 #[cfg(not(target_os = "linux"))]
 pub use self::threads_dummy::monitor_threads;
+
+#[cfg(not(target_os = "linux"))]
+mod memory_dummy;
+#[cfg(not(target_os = "linux"))]
+pub use self::memory_dummy::monitor_memory;
 
 pub use self::allocator_metrics::monitor_allocator_stats;
 
