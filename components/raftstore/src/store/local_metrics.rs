8da8194bfe31dee5a2ea18bd43e38656b5d38054
Little-Wallace <bupt2013211450@gmail.com>
2020-05-06 11:45:00 +0800

    fix lint
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- components/raftstore/src/store/local_metrics.rs
+++ components/raftstore/src/store/local_metrics.rs
@@ -303,9 +303,7 @@ impl RaftProposeMetrics {
             self.conf_change = 0;
         }
         if self.batch > 0 {
-            PEER_PROPOSAL_COUNTER
-                .batch
-                .inc_by(self.batch as i64);
+            PEER_PROPOSAL_COUNTER.batch.inc_by(self.batch as i64);
             self.batch = 0;
         }
         self.request_wait_time.flush();
