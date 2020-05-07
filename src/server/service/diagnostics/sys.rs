1d05e85134988aff8c7f2bdbd016b1fab2458264
Nick Cameron <nrc@ncameron.org>
2020-05-08 11:13:51 +1200

    diagnostics: address review comments
    Signed-off-by: Nick Cameron <nrc@ncameron.org>

--- src/server/service/diagnostics/sys.rs
+++ src/server/service/diagnostics/sys.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
+// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
 
 use std::collections::HashMap;
 use std::string::ToString;
