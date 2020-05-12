5d06a7475822e23b91e416f6e0439d1c7ecd0684
Brian Anderson <andersrb@gmail.com>
2020-05-12 00:52:31 +0000

    engine_rocks: Don't rely one engine to build iterator ReadOptions
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_rocks/src/options.rs
+++ components/engine_rocks/src/options.rs
@@ -1,7 +1,7 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
-use engine::IterOptionsExt;
-use rocksdb::{ReadOptions as RawReadOptions, WriteOptions as RawWriteOptions};
+use rocksdb::{ReadOptions as RawReadOptions, WriteOptions as RawWriteOptions, TableFilter, TableProperties};
+use tikv_util::codec::number;
 
 pub struct RocksReadOptions(RawReadOptions);
 
@@ -47,7 +47,85 @@ impl From<&engine_traits::WriteOptions> for RocksWriteOptions {
 
 impl From<engine_traits::IterOptions> for RocksReadOptions {
     fn from(opts: engine_traits::IterOptions) -> Self {
-        let r = opts.build_read_opts();
+        let r = build_read_opts(opts);
         RocksReadOptions(r)
+    }
+}
+
+fn build_read_opts(iter_opts: engine_traits::IterOptions) -> RawReadOptions {
+    let mut opts = RawReadOptions::new();
+    opts.fill_cache(iter_opts.fill_cache());
+    if iter_opts.key_only() {
+        opts.set_titan_key_only(true);
+    }
+    if iter_opts.total_order_seek_used() {
+        opts.set_total_order_seek(true);
+    } else if iter_opts.prefix_same_as_start() {
+        opts.set_prefix_same_as_start(true);
+    }
+
+    if iter_opts.hint_min_ts().is_some() || iter_opts.hint_max_ts().is_some() {
+        let ts_filter = TsFilter::new(iter_opts.hint_min_ts(), iter_opts.hint_max_ts());
+        opts.set_table_filter(Box::new(ts_filter))
+    }
+
+    let (lower, upper) = iter_opts.build_bounds();
+    if let Some(lower) = lower {
+        opts.set_iterate_lower_bound(lower);
+    }
+    if let Some(upper) = upper {
+        opts.set_iterate_upper_bound(upper);
+    }
+
+    opts
+}
+
+struct TsFilter {
+    hint_min_ts: Option<u64>,
+    hint_max_ts: Option<u64>,
+}
+
+impl TsFilter {
+    fn new(hint_min_ts: Option<u64>, hint_max_ts: Option<u64>) -> TsFilter {
+        TsFilter {
+            hint_min_ts,
+            hint_max_ts,
+        }
+    }
+}
+
+impl TableFilter for TsFilter {
+    fn table_filter(&self, props: &TableProperties) -> bool {
+        if self.hint_max_ts.is_none() && self.hint_min_ts.is_none() {
+            return true;
+        }
+
+        let user_props = props.user_collected_properties();
+
+        if let Some(hint_min_ts) = self.hint_min_ts {
+            // TODO avoid hard code after refactor MvccProperties from
+            // tikv/src/raftstore/coprocessor/ into some component about engine.
+            if let Some(mut p) = user_props.get("tikv.max_ts") {
+                if let Ok(get_max) = number::decode_u64(&mut p) {
+                    if get_max < hint_min_ts {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if let Some(hint_max_ts) = self.hint_max_ts {
+            // TODO avoid hard code after refactor MvccProperties from
+            // tikv/src/raftstore/coprocessor/ into some component about engine.
+            if let Some(mut p) = user_props.get("tikv.min_ts") {
+                if let Ok(get_min) = number::decode_u64(&mut p) {
+                    if get_min > hint_max_ts {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        true
     }
 }
