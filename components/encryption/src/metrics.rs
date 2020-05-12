b55e30941dc006f60bae0dd8914b9aa6d6713aa2
TXXT <hunterlxt@live.com>
2020-05-12 11:52:34 +0800

    address comment
    Signed-off-by: TXXT <hunterlxt@live.com>

--- components/encryption/src/metrics.rs
+++ components/encryption/src/metrics.rs
@@ -15,12 +15,12 @@ lazy_static::lazy_static! {
         "tikv_encryption_is_initialized",
         "Flag to indicate if KeyDictionary encryption is initialized"
     ).unwrap();
-    pub static ref ENCRYPT_DECRPTION_FILE_HISTOGRAM: HistogramVec = register_histogram_vec!(
+    pub static ref ENCRYPT_DECRPTION_FILE_HISTOGRAM: GaugeVec = register_gauge_vec!(
         "tikv_encryption_write_read_file_duration_seconds",
         "Histogram of writing or reading file duration",
-        &["type", "operation"],
-        exponential_buckets(0.001, 2.0, 16).unwrap() // Up to 65.5 seconds
+        &["type", "operation"]
     ).unwrap();
+
     pub static ref ENCRYPTION_FILE_SIZE_GAUGE: IntGaugeVec = register_int_gauge_vec!(
         "tikv_encryption_meta_file_size_bytes",
         "Total size of ecryption meta files",
