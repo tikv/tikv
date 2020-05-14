3760143696c8dd11818febac4d32bf2f0d010654
Liqi Geng <gengliqiii@gmail.com>
2020-05-14 22:01:02 +0800

    fix merge test
    Signed-off-by: Liqi Geng <gengliqiii@gmail.com>

--- components/test_raftstore/src/pd.rs
+++ components/test_raftstore/src/pd.rs
@@ -968,10 +968,8 @@ impl TestPdClient {
         if right.get_start_key() != split_key {
             return false;
         }
-
-        assert!(left.get_region_epoch().get_version() > region.get_region_epoch().get_version());
-        assert!(right.get_region_epoch().get_version() > region.get_region_epoch().get_version());
-        true
+        return left.get_region_epoch().get_version() > region.get_region_epoch().get_version() && 
+                right.get_region_epoch().get_version() > region.get_region_epoch().get_version()
     }
 
     pub fn get_store_stats(&self, store_id: u64) -> Option<pdpb::StoreStats> {
