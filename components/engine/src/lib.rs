a317fda3a18e2588fdb0a224f6e9c6d4235fedb9
Brian Anderson <andersrb@gmail.com>
2020-05-22 02:12:12 +0000

    engine: Remove Engines type
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine/src/lib.rs
+++ components/engine/src/lib.rs
@@ -7,34 +7,7 @@ extern crate slog_global;
 #[allow(unused_extern_crates)]
 extern crate tikv_alloc;
 
-use std::sync::Arc;
-
 pub mod rocks;
 pub use crate::rocks::{CFHandle, DBIterator, Env, Range, ReadOptions, WriteOptions, DB};
 mod errors;
 pub use crate::errors::*;
-
-#[derive(Clone, Debug)]
-pub struct Engines {
-    pub kv: Arc<DB>,
-    pub raft: Arc<DB>,
-    pub shared_block_cache: bool,
-}
-
-impl Engines {
-    pub fn new(kv_engine: Arc<DB>, raft_engine: Arc<DB>, shared_block_cache: bool) -> Engines {
-        Engines {
-            kv: kv_engine,
-            raft: raft_engine,
-            shared_block_cache,
-        }
-    }
-
-    pub fn sync_kv(&self) -> Result<()> {
-        self.kv.sync_wal().map_err(Error::Engine)
-    }
-
-    pub fn sync_raft(&self) -> Result<()> {
-        self.raft.sync_wal().map_err(Error::Engine)
-    }
-}
