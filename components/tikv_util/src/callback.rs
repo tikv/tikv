026e640f1ddf3b9ac7a5d7515fdff969a9060b64
Yilin Chen <sticnarf@gmail.com>
2020-05-25 10:44:14 +0800

    add docs to must_call
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- components/tikv_util/src/callback.rs
+++ components/tikv_util/src/callback.rs
@@ -4,6 +4,14 @@ use std::marker::PhantomData;
 
 pub type Callback<T> = Box<dyn FnOnce(T) + Send>;
 
+/// Creates a callback that is automatically called on drop if it's not called
+/// explicitly.
+///
+/// Note that leaking the callback can cause it to be never called but it
+/// rarely happens.
+///
+/// Also note that because `callback` and `arg_on_drop` may be called in the `drop`
+/// method, do not panic inside them or use `safe_panic` instead.
 pub fn must_call<T: Send + 'static>(
     callback: impl FnOnce(T) + Send + 'static,
     arg_on_drop: impl FnOnce() -> T + Send + 'static,
