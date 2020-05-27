75509f8eaa5eb6c20164589efad236ea035b6ece
zhongzc <zhongzc_arch@outlook.com>
2020-05-27 17:56:50 +0800

    fmt
    Signed-off-by: zhongzc <zhongzc_arch@outlook.com>

--- src/coprocessor/endpoint.rs
+++ src/coprocessor/endpoint.rs
@@ -354,7 +354,7 @@ impl<E: Engine> Endpoint<E> {
         tracker.collect_storage_statistics(storage_stats);
         let exec_details = tracker.get_exec_details();
         tracker.on_finish_all_items();
-        
+
         let mut resp = match result {
             Ok(resp) => {
                 COPR_RESP_SIZE.inc_by(resp.data.len() as i64);
