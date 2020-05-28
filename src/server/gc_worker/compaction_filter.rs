df396a0625e4a86c2cb22a2e33b51f9f55c05edc
qupeng <qupeng@pingcap.com>
2020-05-28 15:01:25 +0800

    handle delete marks in compaction filter
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- src/server/gc_worker/compaction_filter.rs
+++ src/server/gc_worker/compaction_filter.rs
@@ -7,12 +7,13 @@ use std::sync::{Arc, Mutex};
 use super::GcWorkerConfigManager;
 use crate::storage::mvcc::{GC_DELETE_VERSIONS_HISTOGRAM, MVCC_VERSIONS_HISTOGRAM};
 use engine::rocks::{
-    new_compaction_filter_raw, CompactionFilter, CompactionFilterContext, CompactionFilterFactory,
-    DBCompactionFilter, WriteOptions, DB,
+    new_compaction_filter_raw, util::get_cf_handle, CompactionFilter, CompactionFilterContext,
+    CompactionFilterFactory, DBCompactionFilter, DBIterator, ReadOptions, SeekKey, WriteOptions,
+    DB,
 };
 use engine_rocks::RocksEngine;
 use engine_rocks::RocksWriteBatch;
-use engine_traits::{Mutable, WriteBatch};
+use engine_traits::{Mutable, WriteBatch, CF_WRITE};
 use txn_types::{Key, WriteRef, WriteType};
 
 const DEFAULT_DELETE_BATCH_SIZE: usize = 256 * 1024;
@@ -46,7 +47,7 @@ pub struct WriteCompactionFilterFactory;
 impl CompactionFilterFactory for WriteCompactionFilterFactory {
     fn create_compaction_filter(
         &self,
-        _context: &CompactionFilterContext,
+        context: &CompactionFilterContext,
     ) -> *mut DBCompactionFilter {
         let gc_context_option = GC_CONTEXT.lock().unwrap();
         let gc_context = match *gc_context_option {
@@ -65,16 +66,18 @@ impl CompactionFilterFactory for WriteCompactionFilterFactory {
         let db = Arc::clone(&gc_context.db);
         let safe_point = Arc::clone(&gc_context.safe_point);
 
-        let filter = Box::new(WriteCompactionFilter::new(db, safe_point));
+        let filter = Box::new(WriteCompactionFilter::new(db, safe_point, context));
         unsafe { new_compaction_filter_raw(name, filter) }
     }
 }
 
 struct WriteCompactionFilter {
+    bottommost_level: bool,
     safe_point: Arc<AtomicU64>,
     db: Arc<DB>,
 
     write_batch: RocksWriteBatch,
+    delete_key_prefix: Vec<u8>,
     key_prefix: Vec<u8>,
     remove_older: bool,
 
@@ -83,15 +86,17 @@ struct WriteCompactionFilter {
 }
 
 impl WriteCompactionFilter {
-    fn new(db: Arc<DB>, safe_point: Arc<AtomicU64>) -> Self {
+    fn new(db: Arc<DB>, safe_point: Arc<AtomicU64>, context: &CompactionFilterContext) -> Self {
         // Safe point must have been initialized.
         assert!(safe_point.load(Ordering::Relaxed) > 0);
 
         let wb = RocksWriteBatch::with_capacity(Arc::clone(&db), DEFAULT_DELETE_BATCH_SIZE);
         WriteCompactionFilter {
+            bottommost_level: context.bottommost_level(),
             safe_point,
             db,
             write_batch: wb,
+            delete_key_prefix: vec![],
             key_prefix: vec![],
             remove_older: false,
 
@@ -102,12 +107,20 @@ impl WriteCompactionFilter {
 
     fn delete_default_key(&mut self, key: &[u8]) {
         self.write_batch.delete(key).unwrap();
+        self.flush_pending_writes_if_need();
+    }
+
+    fn delete_write_key(&mut self, key: &[u8]) {
+        self.write_batch.delete_cf(CF_WRITE, key).unwrap();
+        self.flush_pending_writes_if_need();
+    }
+
+    fn flush_pending_writes_if_need(&mut self) {
         if self.write_batch.data_size() > DEFAULT_DELETE_BATCH_SIZE {
             let mut opts = WriteOptions::new();
             opts.set_sync(false);
-            self.db
-                .write_opt(self.write_batch.as_inner(), &opts)
-                .unwrap();
+            let raw_batch = self.write_batch.as_inner();
+            self.db.write_opt(raw_batch, &opts).unwrap();
             self.write_batch.clear();
         }
     }
@@ -126,12 +139,36 @@ impl WriteCompactionFilter {
 
 impl Drop for WriteCompactionFilter {
     fn drop(&mut self) {
+        if !self.delete_key_prefix.is_empty() && self.delete_key_prefix == self.key_prefix {
+            // In this compaction, the last MVCC version is deleted. However there could be
+            // still some versions for the key which are not included in this compaction.
+            let cf_handle = get_cf_handle(&self.db, CF_WRITE).unwrap();
+            let mut iter = DBIterator::new_cf(self.db.clone(), cf_handle, ReadOptions::new());
+            let mut valid = iter.seek(SeekKey::Key(&self.delete_key_prefix)).unwrap();
+            while valid {
+                let (key, value) = (iter.key(), iter.value());
+                let key_prefix = match Key::split_on_ts_for(key) {
+                    Ok((prefix, _)) if prefix != self.delete_key_prefix.as_slice() => break,
+                    Ok((prefix, _)) => prefix,
+                    Err(_) => break,
+                };
+
+                let write = WriteRef::parse(value).unwrap();
+                let (start_ts, short_value) = (write.start_ts, write.short_value);
+                if short_value.is_none() {
+                    let key = Key::from_encoded_slice(key_prefix).append_ts(start_ts);
+                    self.delete_default_key(key.as_encoded());
+                }
+
+                self.delete_write_key(key);
+                valid = iter.next().unwrap();
+            }
+        }
         if !self.write_batch.is_empty() {
             let mut opts = WriteOptions::new();
             opts.set_sync(true);
-            self.db
-                .write_opt(self.write_batch.as_inner(), &opts)
-                .unwrap();
+            let raw_batch = self.write_batch.as_inner();
+            self.db.write_opt(raw_batch, &opts).unwrap();
         } else {
             self.db.flush(true).unwrap();
         }
@@ -179,6 +216,12 @@ impl CompactionFilter for WriteCompactionFilter {
                 WriteType::Delete => {
                     // Currently `WriteType::Delete` will always be kept.
                     self.remove_older = true;
+                    if self.bottommost_level {
+                        // Handle delete marks if the bottommost level is reached.
+                        filtered = true;
+                        self.delete_key_prefix.clear();
+                        self.delete_key_prefix.extend_from_slice(key_prefix);
+                    }
                 }
                 WriteType::Put => self.remove_older = true,
             }
