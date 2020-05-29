abb63bf369bf1e4ae3217813077b80cea4a043d3
kennytm <kennytm@gmail.com>
2020-05-29 14:51:36 +0800

    cherry pick #7917 to release-4.0
    Signed-off-by: sre-bot <sre-bot@pingcap.com>

--- components/external_storage/src/util.rs
+++ components/external_storage/src/util.rs
@@ -2,7 +2,12 @@
 
 use bytes::Bytes;
 use futures::stream::{self, Stream};
+<<<<<<< HEAD
 use futures_io::AsyncRead;
+=======
+use futures_util::io::AsyncRead;
+use rand::{thread_rng, Rng};
+>>>>>>> 3c667df... Improve robustness of Backup/Restore involving external_storage (#7917)
 use std::{
     future::Future,
     io, iter,
@@ -10,7 +15,9 @@ use std::{
     pin::Pin,
     sync::Mutex,
     task::{Context, Poll},
+    time::Duration,
 };
+use tokio::{runtime::Builder, time::delay_for};
 
 /// Wrapper of an `AsyncRead` instance, exposed as a `Sync` `Stream` of `Bytes`.
 pub struct AsyncReadAsSyncStreamOfBytes<R> {
@@ -67,11 +74,55 @@ pub fn error_stream(e: io::Error) -> impl Stream<Item = io::Result<Bytes>> + Unp
 pub fn block_on_external_io<F: Future>(f: F) -> F::Output {
     // we need a Tokio runtime rather than futures_executor::block_on because
     // Tokio futures require Tokio executor.
-    tokio::runtime::Builder::new()
+    Builder::new()
         .basic_scheduler()
         .enable_io()
         .enable_time()
         .build()
         .expect("failed to create Tokio runtime")
         .block_on(f)
+}
+
+/// Trait for errors which can be retried inside [`retry()`].
+pub trait RetryError {
+    /// Returns a placeholder to indicate an uninitialized error. This function exists only to
+    /// satisfy safety, there is no meaning attached to the returned value.
+    fn placeholder() -> Self;
+
+    /// Returns whether this error can be retried.
+    fn is_retryable(&self) -> bool;
+}
+
+/// Retries a future execution.
+///
+/// This method implements truncated exponential back-off retry strategies outlined in
+/// https://docs.aws.amazon.com/general/latest/gr/api-retries.html and
+/// https://cloud.google.com/storage/docs/exponential-backoff
+/// Since rusoto does not have transparent auto-retry (https://github.com/rusoto/rusoto/issues/234),
+/// we need to implement this manually.
+pub async fn retry<G, T, F, E>(mut action: G) -> Result<T, E>
+where
+    G: FnMut() -> F,
+    F: Future<Output = Result<T, E>>,
+    E: RetryError,
+{
+    const MAX_RETRY_DELAY: Duration = Duration::from_secs(32);
+    const MAX_RETRY_TIMES: usize = 4;
+    let mut retry_wait_dur = Duration::from_secs(1);
+    let mut result = Err(E::placeholder());
+
+    for _ in 0..MAX_RETRY_TIMES {
+        result = action().await;
+        if let Err(e) = &result {
+            if e.is_retryable() {
+                delay_for(retry_wait_dur + Duration::from_millis(thread_rng().gen_range(0, 1000)))
+                    .await;
+                retry_wait_dur = MAX_RETRY_DELAY.min(retry_wait_dur * 2);
+                continue;
+            }
+        }
+        break;
+    }
+
+    result
 }
