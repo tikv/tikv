c60adf9adf8e80418e8224e27f9c376ff39d1534
Yuning Zhang <codeworm96@outlook.com>
2020-05-28 22:44:02 -0400

    sst_importer: add import mode timeout
    Signed-off-by: Yuning Zhang <codeworm96@outlook.com>

--- components/sst_importer/src/import_mode.rs
+++ components/sst_importer/src/import_mode.rs
@@ -1,64 +1,143 @@
 // Copyright 2018 TiKV Project Authors. Licensed under Apache-2.0.
 
-use engine_traits::{ColumnFamilyOptions, DBOptions, KvEngine};
-use kvproto::import_sstpb::*;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use std::time::Instant;
 
+use engine_traits::{ColumnFamilyOptions, DBOptions, KvEngine};
+use futures_cpupool::CpuPool;
+use futures_util::compat::{Compat, Future01CompatExt};
+use futures_util::future::FutureExt;
+use kvproto::import_sstpb::*;
+use tikv_util::timer::GLOBAL_TIMER_HANDLE;
+
+use super::Config;
 use super::Result;
 
 type RocksDBMetricsFn = fn(cf: &str, name: &str, v: f64);
 
-pub struct ImportModeSwitcher {
+struct ImportModeSwitcherInner<T: KvEngine> {
     mode: SwitchMode,
     backup_db_options: ImportModeDBOptions,
     backup_cf_options: Vec<(String, ImportModeCFOptions)>,
+    timeout: Duration,
+    next_check: Instant,
+    db: T,
+    metrics_fn: RocksDBMetricsFn,
 }
 
-impl ImportModeSwitcher {
-    pub fn new() -> ImportModeSwitcher {
-        ImportModeSwitcher {
-            mode: SwitchMode::Normal,
-            backup_db_options: ImportModeDBOptions::new(),
-            backup_cf_options: Vec::new(),
-        }
-    }
-
-    pub fn enter_normal_mode(&mut self, db: &impl KvEngine, mf: RocksDBMetricsFn) -> Result<()> {
+impl<T: KvEngine> ImportModeSwitcherInner<T> {
+    fn enter_normal_mode(&mut self, mf: RocksDBMetricsFn) -> Result<()> {
         if self.mode == SwitchMode::Normal {
             return Ok(());
         }
 
-        self.backup_db_options.set_options(db)?;
+        self.backup_db_options.set_options(&self.db)?;
         for (cf_name, cf_opts) in &self.backup_cf_options {
-            cf_opts.set_options(db, cf_name, mf)?;
+            cf_opts.set_options(&self.db, cf_name, mf)?;
         }
 
         self.mode = SwitchMode::Normal;
         Ok(())
     }
 
-    pub fn enter_import_mode(&mut self, db: &impl KvEngine, mf: RocksDBMetricsFn) -> Result<()> {
+    fn enter_import_mode(&mut self, mf: RocksDBMetricsFn) -> Result<()> {
         if self.mode == SwitchMode::Import {
             return Ok(());
         }
 
-        self.backup_db_options = ImportModeDBOptions::new_options(db);
+        self.backup_db_options = ImportModeDBOptions::new_options(&self.db);
         self.backup_cf_options.clear();
 
         let import_db_options = ImportModeDBOptions::new();
-        import_db_options.set_options(db)?;
+        import_db_options.set_options(&self.db)?;
         let import_cf_options = ImportModeCFOptions::new();
-        for cf_name in db.cf_names() {
-            let cf_opts = ImportModeCFOptions::new_options(db, cf_name);
+        for cf_name in self.db.cf_names() {
+            let cf_opts = ImportModeCFOptions::new_options(&self.db, cf_name);
             self.backup_cf_options.push((cf_name.to_owned(), cf_opts));
-            import_cf_options.set_options(db, cf_name, mf)?;
+            import_cf_options.set_options(&self.db, cf_name, mf)?;
         }
 
         self.mode = SwitchMode::Import;
         Ok(())
     }
 
-    pub fn get_mode(&self) -> SwitchMode {
+    fn get_mode(&self) -> SwitchMode {
         self.mode
+    }
+}
+
+#[derive(Clone)]
+pub struct ImportModeSwitcher<T: KvEngine> {
+    inner: Arc<Mutex<ImportModeSwitcherInner<T>>>,
+}
+
+impl<T: KvEngine> ImportModeSwitcher<T> {
+    pub fn new(cfg: &Config, executor: &CpuPool, db: T) -> ImportModeSwitcher<T> {
+        fn mf(_cf: &str, _name: &str, _v: f64) {}
+
+        let timeout = cfg.import_mode_timeout.0;
+        let inner = Arc::new(Mutex::new(ImportModeSwitcherInner {
+            mode: SwitchMode::Normal,
+            backup_db_options: ImportModeDBOptions::new(),
+            backup_cf_options: Vec::new(),
+            timeout,
+            next_check: Instant::now() + timeout,
+            db,
+            metrics_fn: mf,
+        }));
+
+        // spawn a background future to put TiKV back into normal mode after timeout
+        let switcher = Arc::downgrade(&inner);
+        let timer_loop = async move {
+            loop {
+                let next_check = match switcher.upgrade() {
+                    Some(switcher) => {
+                        let mut switcher = switcher.lock().unwrap();
+                        let now = Instant::now();
+                        if now >= switcher.next_check {
+                            if switcher.mode == SwitchMode::Import {
+                                let mf = switcher.metrics_fn;
+                                if !switcher.enter_normal_mode(mf).is_ok() {
+                                    error!("failed to put TiKV back into normal mode");
+                                }
+                            }
+                            switcher.next_check = now + switcher.timeout
+                        }
+                        switcher.next_check
+                    }
+                    // if the switcher has been dropped, we can stop
+                    None => break,
+                };
+
+                let ok = GLOBAL_TIMER_HANDLE.delay(next_check).compat().await.is_ok();
+
+                if !ok {
+                    warn!("failed to delay with global timer");
+                }
+            }
+        };
+        executor
+            .spawn(Compat::new(timer_loop.unit_error().boxed()))
+            .forget();
+
+        ImportModeSwitcher { inner }
+    }
+
+    pub fn enter_normal_mode(&mut self, mf: RocksDBMetricsFn) -> Result<()> {
+        self.inner.lock().unwrap().enter_normal_mode(mf)
+    }
+
+    pub fn enter_import_mode(&mut self, mf: RocksDBMetricsFn) -> Result<()> {
+        let mut inner = self.inner.lock().unwrap();
+        inner.enter_import_mode(mf)?;
+        inner.next_check = Instant::now() + inner.timeout;
+        inner.metrics_fn = mf;
+        Ok(())
+    }
+
+    pub fn get_mode(&self) -> SwitchMode {
+        self.inner.lock().unwrap().get_mode()
     }
 }
 
@@ -220,15 +299,21 @@ mod tests {
 
         fn mf(_cf: &str, _name: &str, _v: f64) {}
 
-        let mut switcher = ImportModeSwitcher::new();
+        let cfg = Config::default();
+        let threads = futures_cpupool::Builder::new()
+            .name_prefix("sst-importer")
+            .pool_size(cfg.num_threads)
+            .create();
+
+        let mut switcher = ImportModeSwitcher::new(&cfg, &threads, db.clone());
         check_import_options(&db, &normal_db_options, &normal_cf_options);
-        switcher.enter_import_mode(&db, mf).unwrap();
+        switcher.enter_import_mode(mf).unwrap();
         check_import_options(&db, &import_db_options, &import_cf_options);
-        switcher.enter_import_mode(&db, mf).unwrap();
+        switcher.enter_import_mode(mf).unwrap();
         check_import_options(&db, &import_db_options, &import_cf_options);
-        switcher.enter_normal_mode(&db, mf).unwrap();
+        switcher.enter_normal_mode(mf).unwrap();
         check_import_options(&db, &normal_db_options, &normal_cf_options);
-        switcher.enter_normal_mode(&db, mf).unwrap();
+        switcher.enter_normal_mode(mf).unwrap();
         check_import_options(&db, &normal_db_options, &normal_cf_options);
     }
 }
--- src/import/sst_service.rs
+++ src/import/sst_service.rs
@@ -1,7 +1,7 @@
 // Copyright 2018 TiKV Project Authors. Licensed under Apache-2.0.
 
 use std::f64::INFINITY;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use engine_traits::{name_to_cf, CompactExt, MiscExt, CF_DEFAULT, CF_WRITE};
 use futures::sync::mpsc;
@@ -44,7 +44,7 @@ pub struct ImportSSTService<Router> {
     engine: RocksEngine,
     threads: CpuPool,
     importer: Arc<SSTImporter>,
-    switcher: Arc<Mutex<ImportModeSwitcher>>,
+    switcher: ImportModeSwitcher<RocksEngine>,
     limiter: Limiter,
     security_mgr: Arc<SecurityManager>,
 }
@@ -61,13 +61,14 @@ impl<Router: RaftStoreRouter<RocksSnapshot>> ImportSSTService<Router> {
             .name_prefix("sst-importer")
             .pool_size(cfg.num_threads)
             .create();
+        let switcher = ImportModeSwitcher::new(&cfg, &threads, engine.clone());
         ImportSSTService {
             cfg,
             router,
             engine,
             threads,
             importer,
-            switcher: Arc::new(Mutex::new(ImportModeSwitcher::new())),
+            switcher,
             limiter: Limiter::new(INFINITY),
             security_mgr,
         }
@@ -88,14 +89,13 @@ impl<Router: RaftStoreRouter<RocksSnapshot>> ImportSst for ImportSSTService<Rout
         let timer = Instant::now_coarse();
 
         let res = {
-            let mut switcher = self.switcher.lock().unwrap();
             fn mf(cf: &str, name: &str, v: f64) {
                 CONFIG_ROCKSDB_GAUGE.with_label_values(&[cf, name]).set(v);
             }
 
             match req.get_mode() {
-                SwitchMode::Normal => switcher.enter_normal_mode(&self.engine, mf),
-                SwitchMode::Import => switcher.enter_import_mode(&self.engine, mf),
+                SwitchMode::Normal => self.switcher.enter_normal_mode(mf),
+                SwitchMode::Import => self.switcher.enter_import_mode(mf),
             }
         };
         match res {
@@ -231,7 +231,7 @@ impl<Router: RaftStoreRouter<RocksSnapshot>> ImportSst for ImportSSTService<Rout
         let label = "ingest";
         let timer = Instant::now_coarse();
 
-        if self.switcher.lock().unwrap().get_mode() == SwitchMode::Normal
+        if self.switcher.get_mode() == SwitchMode::Normal
             && self
                 .engine
                 .ingest_maybe_slowdown_writes(CF_DEFAULT)
