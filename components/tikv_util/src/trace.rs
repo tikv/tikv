33ff508d1d0eb838f692aefe5770943ddb22c55a
Renkai <gaelookair@gmail.com>
2020-05-31 15:44:29 +0800

    update fill span proto
    Signed-off-by: Renkai <gaelookair@gmail.com>

--- components/tikv_util/src/trace.rs
+++ components/tikv_util/src/trace.rs
@@ -60,7 +60,7 @@ impl Into<spanpb::Event> for TraceEvent {
     }
 }
 
-pub fn encode_spans(mut rx: Collector) -> impl Iterator<Item = spanpb::SpanSet> {
+pub fn encode_spans(rx: Collector) -> impl Iterator<Item = spanpb::SpanSet> {
     /*
     let finished_spans = rx.collect();
     let spans = finished_spans.into_iter().map(|span| {
@@ -87,13 +87,45 @@ pub fn encode_spans(mut rx: Collector) -> impl Iterator<Item = spanpb::SpanSet> 
      */
 
     let span_sets = rx.collect();
-    //TODO finish encode to proto
     span_sets
         .into_iter()
         .map(|span_set| {
-            let mut set = spanpb::SpanSet::default();
+            let mut pb_set = spanpb::SpanSet::default();
+            pb_set.set_start_time_ns(span_set.start_time_ns);
+            pb_set.set_cycles_per_sec(span_set.cycles_per_sec);
 
-            set
+            let spans = span_set.spans.into_iter().map(|span| {
+                let mut s = spanpb::Span::default();
+                s.set_id(span.id);
+                s.set_begin_cycles(span.begin_cycles);
+                s.set_end_cycles(span.end_cycles);
+                s.set_event(TraceEvent::from(span.event).into());
+
+                #[cfg(feature = "prost-codec")]
+                use minitrace::Link;
+
+                #[cfg(feature = "prost-codec")]
+                match span.link {
+                    Link::Root => {
+                        s.link = spanpb::Link::Root;
+                    }
+                    Link::Parent(id) => {
+                        s.link = spanpb::Link::Parent(id);
+                    }
+                    Link::Continue(id) => {
+                        s.link = spanpb::Link::Continue(id);
+                    }
+                }
+
+                #[cfg(feature = "protobuf-codec")]
+                s.set_link(span.link.into());
+
+                s
+            });
+
+            pb_set.set_spans(spans.collect());
+
+            pb_set
         })
         .into_iter()
 }
