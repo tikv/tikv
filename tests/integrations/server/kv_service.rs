e6edbd2fc8cc618d98ffecd5660f02a52cfa6dc2
5kbpers <tangminghua@pingcap.com>
2020-06-02 17:01:49 +0800

    Revert "kv_service: fix batch empty request deadlock (#7535) (#7539)"
    This reverts commit e927870da224126529a900101c9932d456b9cc75.

--- tests/integrations/server/kv_service.rs
+++ tests/integrations/server/kv_service.rs
@@ -2,7 +2,7 @@
 
 use futures::{Future, Sink, Stream};
 use grpcio::*;
-use kvproto::tikvpb::*;
+use kvproto::tikvpb::BatchCommandsRequest;
 use kvproto::tikvpb_grpc::TikvClient;
 use std::sync::{mpsc, Arc};
 use std::thread;
@@ -32,53 +32,6 @@ fn test_batch_commands() {
         match sender.send((batch_req, WriteFlags::default())).wait() {
             Ok(s) => sender = s,
             Err(e) => panic!("tikv cilent send fail: {:?}", e),
-        }
-    }
-
-    let (tx, rx) = mpsc::sync_channel(1);
-    thread::spawn(move || {
-        // We have send 10k requests to the server, so we should get 10k responses.
-        let mut count = 0;
-        for x in receiver
-            .wait()
-            .map(move |b| b.unwrap().get_responses().len())
-        {
-            count += x;
-            if count == 10000 {
-                tx.send(1).unwrap();
-                return;
-            }
-        }
-    });
-    rx.recv_timeout(Duration::from_secs(1)).unwrap();
-}
-
-#[test]
-fn test_empty_commands() {
-    let mut cluster = new_server_cluster(0, 1);
-    cluster.run();
-
-    let leader = cluster.get_region(b"").get_peers()[0].clone();
-    let addr = cluster.sim.rl().get_addr(leader.get_store_id()).to_owned();
-
-    let env = Arc::new(Environment::new(1));
-    let channel = ChannelBuilder::new(env).connect(&addr);
-    let client = TikvClient::new(channel);
-
-    let (mut sender, receiver) = client.batch_commands().unwrap();
-    for _ in 0..1000 {
-        let mut batch_req = BatchCommandsRequest::default();
-        for i in 0..10 {
-            let mut req = BatchCommandsRequest_Request::default();
-            req.cmd = Some(BatchCommandsRequest_Request_oneof_cmd::Empty(
-                Default::default(),
-            ));
-            batch_req.mut_requests().push(req);
-            batch_req.mut_request_ids().push(i);
-        }
-        match sender.send((batch_req, WriteFlags::default())).wait() {
-            Ok(s) => sender = s,
-            Err(e) => panic!("tikv client send fail: {:?}", e),
         }
     }
 
