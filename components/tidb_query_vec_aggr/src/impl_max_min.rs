3c9c497e899ab9dc1f1328e5ec61f3c6d9c54874
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-06-11 15:16:33 +0800

    make DCO-robot happy
    Signed-off-by: Yuanjia Zhang <zhangyuanjia@pingcap.com>

--- components/tidb_query_vec_aggr/src/impl_max_min.rs
+++ components/tidb_query_vec_aggr/src/impl_max_min.rs
@@ -4,10 +4,11 @@ use std::cmp::Ordering;
 use std::convert::TryFrom;
 
 use tidb_query_codegen::AggrFunction;
-use tidb_query_datatype::{EvalType, FieldTypeAccessor};
+use tidb_query_datatype::{Collation, EvalType, FieldTypeAccessor};
 use tipb::{Expr, ExprType, FieldType};
 
 use tidb_query_common::Result;
+use tidb_query_datatype::codec::collation::*;
 use tidb_query_datatype::codec::data_type::*;
 use tidb_query_datatype::expr::EvalContext;
 use tidb_query_vec_expr::{RpnExpression, RpnExpressionBuilder};
@@ -64,6 +65,7 @@ impl<T: Extremum> super::AggrDefinitionParser for AggrFnDefinitionParserExtremum
 
         let out_ft = aggr_def.take_field_type();
         let out_et = box_try!(EvalType::try_from(out_ft.as_accessor().tp()));
+        let out_coll = out_ft.as_accessor().collation();
 
         if out_et != eval_type {
             return Err(other_err!(
@@ -80,11 +82,115 @@ impl<T: Extremum> super::AggrDefinitionParser for AggrFnDefinitionParserExtremum
             src_schema.len(),
         )?);
 
+        if out_et == EvalType::Bytes {
+            return match out_coll {
+                Ok(coll) => {
+                    match_template_collator! {
+                        TT, match coll {
+                            Collation::TT => Ok(Box::new(AggFnExtremum4Bytes::<TT, T>::new()))
+                        }
+                    }
+                }
+                Err(e) => Err(other_err!("get collation information error {}", e)),
+            };
+        }
+
         match_template_evaluable! {
             TT, match eval_type {
                 EvalType::TT => Ok(Box::new(AggFnExtremum::<TT, T>::new()))
             }
         }
+    }
+}
+
+#[derive(Debug, AggrFunction)]
+#[aggr_function(state = AggFnStateExtremum4Bytes::<T>::new(self.ord))]
+pub struct AggFnExtremum4Bytes<T, E>
+where
+    T: Collator,
+    E: Extremum,
+    VectorValue: VectorValueExt<Bytes>,
+{
+    _phantom: std::marker::PhantomData<(T, E)>,
+    ord: Ordering,
+}
+
+impl<T, E> AggFnExtremum4Bytes<T, E>
+where
+    T: Collator,
+    E: Extremum,
+    VectorValue: VectorValueExt<Bytes>,
+{
+    fn new() -> Self {
+        Self {
+            _phantom: std::marker::PhantomData,
+            ord: E::ORD,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AggFnStateExtremum4Bytes<T>
+where
+    VectorValue: VectorValueExt<Bytes>,
+    T: Collator,
+{
+    ord: Ordering,
+    extremum: Option<Bytes>,
+    _phantom: std::marker::PhantomData<T>,
+}
+
+impl<T> AggFnStateExtremum4Bytes<T>
+where
+    VectorValue: VectorValueExt<Bytes>,
+    T: Collator,
+{
+    pub fn new(ord: Ordering) -> Self {
+        Self {
+            ord,
+            extremum: None,
+            _phantom: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<T> super::ConcreteAggrFunctionState for AggFnStateExtremum4Bytes<T>
+where
+    VectorValue: VectorValueExt<Bytes>,
+    T: Collator,
+{
+    type ParameterType = Bytes;
+
+    #[inline]
+    fn update_concrete(
+        &mut self,
+        _ctx: &mut EvalContext,
+        value: &Option<Self::ParameterType>,
+    ) -> Result<()> {
+        if value.is_none() {
+            return Ok(());
+        }
+
+        if self.extremum.is_none() {
+            self.extremum = value.clone();
+            return Ok(());
+        }
+
+        match T::sort_compare(&self.extremum.as_ref().unwrap(), &value.as_ref().unwrap()) {
+            Err(e) => Err(other_err!("compare bytes error {}", e)),
+            Ok(ord) => {
+                if ord == self.ord {
+                    self.extremum = value.clone();
+                }
+                Ok(())
+            }
+        }
+    }
+
+    #[inline]
+    fn push_result(&self, _ctx: &mut EvalContext, target: &mut [VectorValue]) -> Result<()> {
+        target[0].push(self.extremum.clone());
+        Ok(())
     }
 }
 
@@ -279,6 +385,49 @@ mod tests {
         result[0].clear();
         state.push_result(&mut ctx, &mut result).unwrap();
         assert_eq!(result[0].as_int_slice(), &[Some(-1i64)]);
+    }
+
+    #[test]
+    fn test_collation() {
+        let mut ctx = EvalContext::default();
+        let cases = vec![
+            (Collation::Binary, true, vec!["A", "a"], "a"),
+            (Collation::Utf8Mb4Bin, true, vec!["A", "a"], "a"),
+            (Collation::Utf8Mb4GeneralCi, true, vec!["A", "a"], "A"),
+            (Collation::Utf8Mb4BinNoPadding, true, vec!["A", "a"], "a"),
+            (Collation::Binary, false, vec!["a", "A"], "A"),
+            (Collation::Utf8Mb4Bin, false, vec!["a", "A"], "A"),
+            (Collation::Utf8Mb4GeneralCi, false, vec!["a", "A"], "a"),
+            (Collation::Utf8Mb4BinNoPadding, false, vec!["a", "A"], "A"),
+        ];
+        for (coll, is_max, args, expected) in cases {
+            let function = match_template_collator! {
+                TT, match coll {
+                    Collation::TT => {
+                        match is_max {
+                            true => Box::new(AggFnExtremum4Bytes::<TT, Max>::new()) as Box<dyn AggrFunction>,
+                            false => Box::new(AggFnExtremum4Bytes::<TT, Min>::new()) as Box<dyn AggrFunction>
+                        }
+                    }
+                }
+            };
+            let mut state = function.create_state();
+            let mut result = [VectorValue::with_capacity(0, EvalType::Bytes)];
+            state.push_result(&mut ctx, &mut result).unwrap();
+            assert_eq!(result[0].as_bytes_slice(), &[None]);
+
+            for arg in args {
+                state
+                    .update(&mut ctx, &Some(String::from(arg).into_bytes()))
+                    .unwrap();
+            }
+            result[0].clear();
+            state.push_result(&mut ctx, &mut result).unwrap();
+            assert_eq!(
+                result[0].as_bytes_slice(),
+                [Some(String::from(expected).into_bytes())]
+            );
+        }
     }
 
     #[test]
