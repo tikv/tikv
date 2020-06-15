4598d4a7724a444a4061f5733dc1932db33a6e0c
Brian Anderson <andersrb@gmail.com>
2020-06-15 00:05:26 +0000

    engine_rocks: Add raw rocksdb reexports from engine::rocks
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

---
+++ components/engine_rocks/src/raw.rs
@@ -0,0 +1,17 @@
+// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
+
+//! Reexports from the rocksdb crate
+//!
+//! This is a temporary artifact of refactoring. It exists to provide downstream
+//! crates access to the rocksdb API without depending directly on the rocksdb
+//! crate, but only until the engine interface is completely abstracted.
+
+pub use rocksdb::{
+    run_ldb_tool, BlockBasedOptions, CFHandle, Cache, ColumnFamilyOptions, CompactOptions,
+    CompactionJobInfo, CompactionPriority, DBBottommostLevelCompaction, DBCompactionStyle,
+    DBCompressionType, DBEntryType, DBInfoLogLevel, DBIterator, DBOptions, DBRateLimiterMode,
+    DBRecoveryMode, DBStatisticsTickerType, DBTitanDBBlobRunMode, Env, EventListener,
+    IngestExternalFileOptions, LRUCacheOptions, MemoryAllocator, PerfContext, Range, ReadOptions,
+    SeekKey, SliceTransform, TableFilter, TablePropertiesCollector,
+    TablePropertiesCollectorFactory, TitanBlobIndex, TitanDBOptions, Writable, WriteOptions, DB,
+};
