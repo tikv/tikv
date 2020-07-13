51862db03c13ac637a9d00336d44666f51eb4363
Brian Anderson <andersrb@gmail.com>
2020-07-13 01:22:16 +0000

    engine_traits: Stub out get_range_approximate_keys functions
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

---
+++ components/engine_rocks/src/range_properties.rs
@@ -0,0 +1,14 @@
+// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
+
+use engine_traits::{RangePropertiesExt, Result, Range};
+use crate::engine::RocksEngine;
+
+impl RangePropertiesExt for RocksEngine {
+    fn get_range_approximate_keys(&self, range: Range, region_id: u64, large_threshold: u64) -> Result<u64> {
+        panic!()
+    }
+
+    fn get_range_approximate_keys_cf(&self, cfname: &str, range: Range, region_id: u64, large_threshold: u64) -> Result<u64> {
+        panic!()
+    }
+}
---
+++ components/engine_traits/src/range_properties.rs
@@ -0,0 +1,19 @@
+// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
+
+//! Various metrics related to key ranges
+//!
+//! In RocksDB these are typically implemented with user collected properties,
+//! which might require the database to be constructed with specific options.
+
+use crate::Range;
+use crate::errors::Result;
+
+pub trait RangePropertiesExt {
+    /// Gets the number of keys in a range.
+    ///
+    /// The region_id is used only for logging and means nothing internally.
+    fn get_range_approximate_keys(&self, range: Range, region_id: u64, large_threshold: u64) -> Result<u64>;
+
+    fn get_range_approximate_keys_cf(&self, cfname: &str, range: Range, region_id: u64, large_threshold: u64) -> Result<u64>;
+}
+
