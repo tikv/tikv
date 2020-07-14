04598b132307a907b837518f57294056be384593
youjiali1995 <zlwgx1023@gmail.com>
2020-07-14 17:09:04 +0800

    adjust  test_applied_lock_collector
    Signed-off-by: youjiali1995 <zlwgx1023@gmail.com>

--- tests/integrations/server/gc_worker.rs
+++ tests/integrations/server/gc_worker.rs
@@ -93,13 +93,7 @@ fn test_applied_lock_collector() {
     ctx.set_peer(leader_peer.clone());
     ctx.set_region_epoch(cluster.get_region_epoch(region_id));
 
-    let mut safe_point = 10000;
-    // Write 100 locks whose timestamp is less than the safe point.
-    let mutations = (0..100)
-        .map(|i| new_mutation(Op::Put, format!("key{}", i).as_bytes(), b"v"))
-        .collect();
-    must_kv_prewrite(&leader_client, ctx.clone(), mutations, b"key0".to_vec(), 10);
-    // Make sure all stores applies all logs.
+    // It's used to make sure all stores applies all logs.
     let wait_for_apply = |cluster: &mut Cluster<_>, region: &metapb::Region| {
         let cluster = &mut *cluster;
         region.get_peers().iter().for_each(|p| {
@@ -109,16 +103,40 @@ fn test_applied_lock_collector() {
             assert!(!resp.get_header().has_error(), "{:?}", resp);
         });
     };
-    wait_for_apply(&mut cluster, &region);
 
-    // Register lock observer at safe point 10000
+    let check_lock = |lock: &LockInfo, k: &[u8], pk: &[u8], ts| {
+        assert_eq!(lock.get_key(), k);
+        assert_eq!(lock.get_primary_lock(), pk);
+        assert_eq!(lock.get_lock_version(), ts);
+    };
+
+    // Register lock observer at safe point 10000.
+    let mut safe_point = 10000;
     clients.iter().for_each(|(_, c)| {
+        // Should report error when checking non-existent observer.
+        assert!(!check_lock_observer(c, safe_point).get_error().is_empty());
         must_register_lock_observer(c, safe_point);
         assert!(must_check_lock_observer(c, safe_point, true).is_empty());
     });
 
+    // Lock observer should only collect values in lock CF.
+    let key = b"key0";
+    must_kv_prewrite(
+        &leader_client,
+        ctx.clone(),
+        vec![new_mutation(Op::Put, key, &b"v".repeat(1024))],
+        key.to_vec(),
+        1,
+    );
+    must_kv_commit(&leader_client, ctx.clone(), vec![key.to_vec()], 1, 2, 2);
+    wait_for_apply(&mut cluster, &region);
+    clients.iter().for_each(|(_, c)| {
+        let locks = must_check_lock_observer(c, safe_point, true);
+        assert_eq!(locks.len(), 1);
+        check_lock(&locks[0], key, key, 1);
+    });
+
     // Lock observer shouldn't collect locks after the safe point.
-    let key = b"key100";
     must_kv_prewrite(
         &leader_client,
         ctx.clone(),
@@ -129,64 +147,39 @@ fn test_applied_lock_collector() {
     wait_for_apply(&mut cluster, &region);
     clients.iter().for_each(|(_, c)| {
         let locks = must_check_lock_observer(c, safe_point, true);
-        assert!(locks.is_empty(), "{:?}", locks);
-    });
-
-    // Lock observer should collect locks before the safe point.
-    let key = b"key101";
-    must_kv_prewrite(
-        &leader_client,
-        ctx.clone(),
-        vec![new_mutation(Op::Put, key, b"v")],
-        key.to_vec(),
-        safe_point - 1,
-    );
-    wait_for_apply(&mut cluster, &region);
-    clients.iter().for_each(|(_, c)| {
-        let locks = must_check_lock_observer(c, safe_point, true);
         assert_eq!(locks.len(), 1);
-        assert_eq!(locks[0].get_key(), key);
-        assert_eq!(locks[0].get_primary_lock(), key);
-        assert_eq!(locks[0].get_lock_version(), safe_point - 1);
+        check_lock(&locks[0], key, key, 1);
     });
 
-    // Register lock observer at a stale safe point. It shouldn't affect the observer.
-    clients.iter().for_each(|(_, c)| {
-        must_register_lock_observer(c, safe_point - 1);
-    });
-    let key = b"key102";
-    must_kv_prewrite(
-        &leader_client,
-        ctx.clone(),
-        vec![new_mutation(Op::Put, key, b"v")],
-        key.to_vec(),
-        safe_point,
-    );
+    // Write 999 locks whose timestamp is less than the safe point.
+    let mutations = (1..1000)
+        .map(|i| new_mutation(Op::Put, format!("key{}", i).as_bytes(), b"v"))
+        .collect();
+    must_kv_prewrite(&leader_client, ctx.clone(), mutations, b"key1".to_vec(), 10);
     wait_for_apply(&mut cluster, &region);
     clients.iter().for_each(|(_, c)| {
         let locks = must_check_lock_observer(c, safe_point, true);
-        assert_eq!(locks.len(), 2);
+        // Plus the first lock.
+        assert_eq!(locks.len(), 1000);
     });
 
-    // Add a new store.
+    // Add a new store and register lock observer.
     let store_id = cluster.add_new_engine();
     let channel =
         ChannelBuilder::new(Arc::clone(&env)).connect(cluster.sim.rl().get_addr(store_id));
     let client = TikvClient::new(channel);
-    let resp = check_lock_observer(&client, safe_point);
-    assert!(!resp.get_error().is_empty());
     must_register_lock_observer(&client, safe_point);
 
-    // Add a new peer. Lock observer should collect all locks.
+    // Add a new peer. Lock observer should collect all locks from snapshot.
     let peer = new_peer(store_id, store_id);
     cluster.pd_client.must_add_peer(region_id, peer.clone());
     cluster.pd_client.must_none_pending_peer(peer);
     wait_for_apply(&mut cluster, &region);
     let locks = must_check_lock_observer(&client, safe_point, true);
-    assert_eq!(locks.len(), 102);
+    assert_eq!(locks.len(), 999);
 
     // Should be dirty when collects too many locks.
-    let mutations = (1024..2048)
+    let mutations = (1000..1100)
         .map(|i| new_mutation(Op::Put, format!("key{}", i).as_bytes(), b"v"))
         .collect();
     must_kv_prewrite(
@@ -194,11 +187,21 @@ fn test_applied_lock_collector() {
         ctx.clone(),
         mutations,
         b"key1000".to_vec(),
-        10,
+        100,
     );
     wait_for_apply(&mut cluster, &region);
     clients.insert(store_id, client);
     clients.iter().for_each(|(_, c)| {
+        let resp = check_lock_observer(c, safe_point);
+        assert!(resp.get_error().is_empty(), "{:?}", resp.get_error());
+        assert!(!resp.get_is_clean());
+        // MAX_COLLECT_SIZE is 1024.
+        assert_eq!(resp.get_locks().len(), 1024);
+    });
+
+    // Reregister and check. It shouldn't clean up state.
+    clients.iter().for_each(|(_, c)| {
+        must_register_lock_observer(c, safe_point);
         let resp = check_lock_observer(c, safe_point);
         assert!(resp.get_error().is_empty(), "{:?}", resp.get_error());
         assert!(!resp.get_is_clean());
@@ -211,13 +214,18 @@ fn test_applied_lock_collector() {
     clients.iter().for_each(|(_, c)| {
         must_register_lock_observer(c, safe_point);
         assert!(must_check_lock_observer(c, safe_point, true).is_empty());
+        // Can't register observer with smaller max_ts.
+        assert!(!register_lock_observer(&c, safe_point - 1)
+            .get_error()
+            .is_empty());
+        assert!(must_check_lock_observer(c, safe_point, true).is_empty());
     });
     let leader_client = clients.get(&leader_store_id).unwrap();
     must_kv_prewrite(
         &leader_client,
         ctx.clone(),
-        vec![new_mutation(Op::Put, b"key103", b"v")],
-        b"key103".to_vec(),
+        vec![new_mutation(Op::Put, b"key1100", b"v")],
+        b"key1100".to_vec(),
         safe_point,
     );
     wait_for_apply(&mut cluster, &region);
@@ -225,13 +233,14 @@ fn test_applied_lock_collector() {
         // Should collect locks according to the new max ts.
         let locks = must_check_lock_observer(c, safe_point, true);
         assert_eq!(locks.len(), 1, "{:?}", locks);
-        // Should remove it with a wrong max ts.
-        let resp = remove_lock_observer(c, safe_point - 1);
-        assert!(!resp.get_error().is_empty());
+        // Shouldn't remove it with a wrong max ts.
+        assert!(!remove_lock_observer(c, safe_point - 1)
+            .get_error()
+            .is_empty());
         let locks = must_check_lock_observer(c, safe_point, true);
         assert_eq!(locks.len(), 1, "{:?}", locks);
+        // Remove lock observers.
         must_remove_lock_observer(c, safe_point);
-        let resp = check_lock_observer(c, safe_point);
-        assert!(!resp.get_error().is_empty());
+        assert!(!check_lock_observer(c, safe_point).get_error().is_empty());
     });
 }
