519f66aaf222f26d3e78da9133e958ee783fb023
Brian Anderson <andersrb@gmail.com>
2020-07-16 23:02:41 +0000

    raftstore: Convert type to typaram
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/raftstore/src/store/fsm/store.rs
+++ components/raftstore/src/store/fsm/store.rs
@@ -245,7 +245,7 @@ pub struct PollContext<EK, ER, T, C: 'static> where EK: KvEngine, ER: KvEngine {
     pub cleanup_scheduler: Scheduler<CleanupTask>,
     pub raftlog_gc_scheduler: Scheduler<RaftlogGcTask<RocksEngine>>,
     pub region_scheduler: Scheduler<RegionTask<RocksSnapshot>>,
-    pub apply_router: ApplyRouter<RocksEngine>,
+    pub apply_router: ApplyRouter<EK>,
     pub router: RaftRouter<RocksSnapshot>,
     pub importer: Arc<SSTImporter>,
     pub store_meta: Arc<Mutex<StoreMeta>>,
