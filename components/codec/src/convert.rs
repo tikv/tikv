df7392712a75888991f190aca465526c5f8942d8
abbccdda <boyang@confluent.io>
2020-07-23 00:22:39 -0700

    fix comparison lint
    Signed-off-by: abbccdda <boyang@confluent.io>

--- components/codec/src/convert.rs
+++ components/codec/src/convert.rs
@@ -50,6 +50,6 @@ fn test_encode_f64_to_comparable_u64() {
 
 #[test]
 fn test_decode_comparable_u64_to_f64() {
-    assert_eq!(decode_comparable_u64_to_f64(1 << 63), 0.0);
+    assert!(decode_comparable_u64_to_f64(1 << 63).abs() < f64::EPSILON);
     assert_ne!(decode_comparable_u64_to_f64(0), 0.0);
 }
