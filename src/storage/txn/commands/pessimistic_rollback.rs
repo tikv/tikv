5add61a45eba8332be0d78db6557814b15015429
longfangsong <longfangsong@icloud.com>
2020-08-12 06:08:12 +0800

    move pessimistic_rollback and rollback
    Signed-off-by: longfangsong <longfangsong@icloud.com>

--- src/storage/txn/commands/pessimistic_rollback.rs
+++ src/storage/txn/commands/pessimistic_rollback.rs
@@ -2,14 +2,16 @@
 
 use crate::storage::kv::WriteData;
 use crate::storage::lock_manager::LockManager;
-use crate::storage::mvcc::MvccTxn;
+use crate::storage::mvcc::txn::make_txn_error;
+use crate::storage::mvcc::{MvccTxn, ReleasedLock, Result as MvccResult};
 use crate::storage::txn::commands::{
     Command, CommandExt, ReleasedLocks, TypedCommand, WriteCommand, WriteContext, WriteResult,
 };
 use crate::storage::txn::Result;
 use crate::storage::{ProcessResult, Result as StorageResult, Snapshot};
 use pd_client::PdClient;
-use txn_types::{Key, TimeStamp};
+use std::mem;
+use txn_types::{Key, LockType, TimeStamp};
 
 command! {
     /// Rollback pessimistic locks identified by `start_ts` and `for_update_ts`.
@@ -35,10 +37,43 @@ impl CommandExt for PessimisticRollback {
     gen_lock!(keys: multiple);
 }
 
+impl PessimisticRollback {
+    /// Delete any pessimistic lock with small for_update_ts belongs to this transaction.
+    pub fn pessimistic_rollback<S: Snapshot, P: PdClient + 'static>(
+        &mut self,
+        txn: &mut MvccTxn<S, P>,
+        key: Key,
+    ) -> MvccResult<Option<ReleasedLock>> {
+        fail_point!("pessimistic_rollback", |err| Err(make_txn_error(
+            err,
+            &key,
+            txn.start_ts,
+        )
+        .into()));
+
+        if let Some(lock) = txn.reader.load_lock(&key)? {
+            if lock.lock_type == LockType::Pessimistic
+                && lock.ts == txn.start_ts
+                && lock.for_update_ts <= self.for_update_ts
+            {
+                return Ok(txn.unlock_key(key, true));
+            }
+        }
+        Ok(None)
+    }
+}
+
 impl<S: Snapshot, L: LockManager, P: PdClient + 'static> WriteCommand<S, L, P>
     for PessimisticRollback
 {
-    fn process_write(self, snapshot: S, context: WriteContext<'_, L, P>) -> Result<WriteResult> {
+    fn process_write(
+        mut self,
+        snapshot: S,
+        context: WriteContext<'_, L, P>,
+    ) -> Result<WriteResult> {
+        let ctx = mem::take(&mut self.ctx);
+        let keys = mem::take(&mut self.keys);
+
         let mut txn = MvccTxn::new(
             snapshot,
             self.start_ts,
@@ -46,17 +81,17 @@ impl<S: Snapshot, L: LockManager, P: PdClient + 'static> WriteCommand<S, L, P>
             context.pd_client,
         );
 
-        let rows = self.keys.len();
+        let rows = keys.len();
         let mut released_locks = ReleasedLocks::new(self.start_ts, TimeStamp::zero());
-        for k in self.keys {
-            released_locks.push(txn.pessimistic_rollback(k, self.for_update_ts)?);
+        for k in keys {
+            released_locks.push(self.pessimistic_rollback(&mut txn, k)?);
         }
         released_locks.wake_up(context.lock_mgr);
 
         context.statistics.add(&txn.take_statistics());
         let write_data = WriteData::from_modifies(txn.into_modifies());
         Ok(WriteResult {
-            ctx: self.ctx,
+            ctx,
             to_be_write: write_data,
             rows,
             pr: ProcessResult::MultiRes { results: vec![] },
--- src/storage/txn/commands/rollback.rs
+++ src/storage/txn/commands/rollback.rs
@@ -2,13 +2,16 @@
 
 use crate::storage::kv::WriteData;
 use crate::storage::lock_manager::LockManager;
-use crate::storage::mvcc::MvccTxn;
+use crate::storage::mvcc::txn::make_txn_error;
+use crate::storage::mvcc::{MvccTxn, ReleasedLock, Result as MvccResult};
+use crate::storage::txn::commands::cleanup::Cleanup;
 use crate::storage::txn::commands::{
     Command, CommandExt, ReleasedLocks, TypedCommand, WriteCommand, WriteContext, WriteResult,
 };
 use crate::storage::txn::Result;
 use crate::storage::{ProcessResult, Snapshot};
 use pd_client::PdClient;
+use std::mem;
 use txn_types::{Key, TimeStamp};
 
 command! {
@@ -33,8 +36,37 @@ impl CommandExt for Rollback {
     gen_lock!(keys: multiple);
 }
 
+impl Rollback {
+    pub fn rollback<S: Snapshot, P: PdClient + 'static>(
+        &mut self,
+        txn: &mut MvccTxn<S, P>,
+        key: Key,
+    ) -> MvccResult<Option<ReleasedLock>> {
+        fail_point!("rollback", |err| Err(make_txn_error(
+            err,
+            &key,
+            self.start_ts,
+        )
+        .into()));
+        let cleanup_command = Cleanup {
+            key,
+            start_ts: self.start_ts,
+            current_ts: TimeStamp::zero(),
+            ctx: self.ctx.clone(),
+        };
+
+        // Rollback is called only if the transaction is known to fail. Under the circumstances,
+        // the rollback record needn't be protected.
+        cleanup_command.cleanup(txn, false)
+    }
+}
+
 impl<S: Snapshot, L: LockManager, P: PdClient + 'static> WriteCommand<S, L, P> for Rollback {
-    fn process_write(self, snapshot: S, context: WriteContext<'_, L, P>) -> Result<WriteResult> {
+    fn process_write(
+        mut self,
+        snapshot: S,
+        context: WriteContext<'_, L, P>,
+    ) -> Result<WriteResult> {
         let mut txn = MvccTxn::new(
             snapshot,
             self.start_ts,
@@ -44,8 +76,9 @@ impl<S: Snapshot, L: LockManager, P: PdClient + 'static> WriteCommand<S, L, P> f
 
         let rows = self.keys.len();
         let mut released_locks = ReleasedLocks::new(self.start_ts, TimeStamp::zero());
-        for k in self.keys {
-            released_locks.push(txn.rollback(k)?);
+        let keys = mem::take(&mut self.keys);
+        for k in keys {
+            released_locks.push(self.rollback(&mut txn, k)?);
         }
         released_locks.wake_up(context.lock_mgr);
 
