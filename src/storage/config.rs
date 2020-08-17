e030a0aae9622f3774df89c62f21b2171a72a69e
Yilin Chen <sticnarf@gmail.com>
2020-08-17 15:16:17 +0800

    *: disable checking memory locks by default (#8431)
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- src/storage/config.rs
+++ src/storage/config.rs
@@ -45,6 +45,12 @@ pub struct Config {
     #[config(skip)]
     // Reserve disk space to make tikv would have enough space to compact when disk is full.
     pub reserve_space: ReadableSize,
+    // If this option is enabled, prewrite will support async commit and locks in the in-memory
+    // lock table are checked for reading requests.
+    // CAUTION: This feature is not ready for production and this option may be removed in the
+    // future.
+    #[config(skip)]
+    pub enable_async_commit: bool,
     #[config(submodule)]
     pub block_cache: BlockCacheConfig,
 }
@@ -60,6 +66,7 @@ impl Default for Config {
             scheduler_worker_pool_size: if cpu_num >= 16.0 { 8 } else { 4 },
             scheduler_pending_write_threshold: ReadableSize::mb(DEFAULT_SCHED_PENDING_WRITE_MB),
             reserve_space: ReadableSize::gb(DEFAULT_RESERVER_SPACE_SIZE),
+            enable_async_commit: false,
             block_cache: BlockCacheConfig::default(),
         }
     }
