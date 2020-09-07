4572d3541a537be93891df18a2e0dab5bf46ae5b
Yilin Chen <sticnarf@gmail.com>
2020-09-07 16:33:57 +0800

    use git version of crossbeam-skiplist
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- components/crossbeam-skiplist/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# Unreleased
-
-- Initial implementation.
--- components/crossbeam-skiplist/tests/set.rs
+++
@@ -1,9 +0,0 @@
-use crossbeam_skiplist::SkipSet;
-
-#[test]
-fn smoke() {
-    let m = SkipSet::new();
-    m.insert(1);
-    m.insert(5);
-    m.insert(7);
-}
