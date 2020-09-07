f50607a923b2162b64533947aca18ee47ee22cd7
Lei Zhao <zlwgx1023@gmail.com>
2020-09-07 17:22:21 +0800

    pd_client: fix deadlock between reconnect and sync_request (#8611)
    Signed-off-by: youjiali1995 <zlwgx1023@gmail.com>

--- components/test_pd/src/lib.rs
+++ components/test_pd/src/lib.rs
@@ -1,7 +1,13 @@
 // Copyright 2017 TiKV Project Authors. Licensed under Apache-2.0.
 
+#[macro_use]
+extern crate tikv_util;
+#[macro_use]
+extern crate slog_global;
+
 pub mod mocker;
 mod server;
+pub mod util;
 
 pub use self::mocker::PdMocker;
 pub use self::server::Server;
