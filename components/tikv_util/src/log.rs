858fc877a955ef1e1f990c0da38c896c7ef19b05
5kbpers <tangminghua@pingcap.com>
2020-09-08 15:40:44 +0800

    opt format
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/tikv_util/src/log.rs
+++ components/tikv_util/src/log.rs
@@ -5,23 +5,23 @@
 macro_rules! crit( ($($args:tt)+) => {
     ::slog_global::crit!($($args)+)
 };);
-/// Logs a error level message using the slog global logger.
-/// Use '?' to output error in debug format or '%' to ouput error in display format.
+
+/// Logs a error level message using the slog global logger. /// Use '?' to output error in debug format or '%' to ouput error in display format.
 /// As the third and forth rules shown, the last log field should follow a ',' to seperate the 'err' field. eg. `error!(?e, "msg"; "foo" => foo,);`
 /// If you don't want to output error code, just use the common form like other macros.
 /// Require `slog_global` dependency and `#![feature(min_speacilization)]` in all crates.
 #[macro_export]
 macro_rules! error {
     (?$e:expr; $l:literal) => {
-        ::slog_global::error!($l; "err"=>?$e,"err_code"=>%error_code::ErrorCodeExt::error_code(&$e))
+        ::slog_global::error!($l; "err" => ?$e,"err_code" => %error_code::ErrorCodeExt::error_code(&$e))
     };
 
     (%$e:expr; $l:literal) => {
-        ::slog_global::error!($l; "err"=>?$e,"err_code"=>%error_code::ErrorCodeExt::error_code(&$e))
+        ::slog_global::error!($l; "err" => ?$e,"err_code" => %error_code::ErrorCodeExt::error_code(&$e))
     };
 
     (?$e:expr; $($args:tt)+) => {
-        ::slog_global::error!($($args)+ "err"=>?$e,"err_code"=>%error_code::ErrorCodeExt::error_code(&$e))
+        ::slog_global::error!($($args)+ "err" => ?$e,"err_code" => %error_code::ErrorCodeExt::error_code(&$e))
     };
 
     (%$e:expr; $($args:tt)+) => {
@@ -35,21 +35,24 @@ macro_rules! error {
 
 /// Logs a warning level message using the slog global logger.
 #[macro_export]
-macro_rules! warn( ($($args:tt)+) => {
+macro_rules! warn(($($args:tt)+) => {
     ::slog_global::warn!($($args)+)
 };);
+
 /// Logs a info level message using the slog global logger.
 #[macro_export]
-macro_rules! info( ($($args:tt)+) => {
+macro_rules! info(($($args:tt)+) => {
     ::slog_global::info!($($args)+)
 };);
+
 /// Logs a debug level message using the slog global logger.
 #[macro_export]
-macro_rules! debug( ($($args:tt)+) => {
+macro_rules! debug(($($args:tt)+) => {
     ::slog_global::debug!($($args)+)
 };);
+
 /// Logs a trace level message using the slog global logger.
 #[macro_export]
-macro_rules! trace( ($($args:tt)+) => {
+macro_rules! trace(($($args:tt)+) => {
     ::slog_global::trace!($($args)+)
 };);
