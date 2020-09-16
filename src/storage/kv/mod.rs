5714cc662017ecb6fb482379edb7cb3b438b09f5
Nick Cameron <nrc@ncameron.org>
2020-09-17 11:26:18 +1200

    storage: add some top-level docs
    Signed-off-by: Nick Cameron <nrc@ncameron.org>

--- src/storage/kv/mod.rs
+++ src/storage/kv/mod.rs
@@ -1,5 +1,9 @@
 // Copyright 2016 TiKV Project Authors. Licensed under Apache-2.0.
 
+//! There are multiple [`Engine`](kv::Engine) implementations, [`RaftKv`](crate::server::raftkv::RaftKv)
+//! is used by the [`Server`](crate::server::Server). The [`BTreeEngine`](kv::BTreeEngine) and
+//! [`RocksEngine`](RocksEngine) are used for testing only.
+
 mod btree_engine;
 mod cursor;
 mod perf_context;
