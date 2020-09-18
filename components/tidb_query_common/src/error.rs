af9224e2edb96c1cf5c3ff022938a10ae619e617
koushiro <koushiro.cqx@gmail.com>
2020-09-18 13:57:23 +0800

    tidb_query_common: replace failure with anyhow and thiserror
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- components/tidb_query_common/src/error.rs
+++ components/tidb_query_common/src/error.rs
@@ -1,22 +1,22 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
 use error_code::{self, ErrorCode, ErrorCodeExt};
-use failure::Fail;
+use thiserror::Error;
 
-#[derive(Fail, Debug)]
+#[derive(Debug, Error)]
 pub enum EvaluateError {
-    #[fail(display = "Execution terminated due to exceeding the deadline")]
+    #[error("Execution terminated due to exceeding the deadline")]
     DeadlineExceeded,
 
-    #[fail(display = "Invalid {} character string", charset)]
+    #[error("Invalid {charset} character string")]
     InvalidCharacterString { charset: String },
 
     /// This variant is only a compatible layer for existing CodecError.
     /// Ideally each error kind should occupy an enum variant.
-    #[fail(display = "{}", msg)]
+    #[error("{msg}")]
     Custom { code: i32, msg: String },
 
-    #[fail(display = "{}", _0)]
+    #[error("{0}")]
     Other(String),
 }
 
@@ -60,26 +60,26 @@ impl ErrorCodeExt for EvaluateError {
     }
 }
 
-#[derive(Fail, Debug)]
-#[fail(display = "{}", _0)]
-pub struct StorageError(pub failure::Error);
+#[derive(Debug, Error)]
+#[error("{0}")]
+pub struct StorageError(pub anyhow::Error);
 
-impl From<failure::Error> for StorageError {
+impl From<anyhow::Error> for StorageError {
     #[inline]
-    fn from(err: failure::Error) -> Self {
+    fn from(err: anyhow::Error) -> Self {
         StorageError(err)
     }
 }
 
 /// We want to restrict the type of errors to be either a `StorageError` or `EvaluateError`, thus
-/// `failure::Error` is not used. Instead, we introduce our own error enum.
-#[derive(Fail, Debug)]
+/// `anyhow::Error` is not used. Instead, we introduce our own error enum.
+#[derive(Debug, Error)]
 pub enum ErrorInner {
-    #[fail(display = "Storage error: {}", _0)]
-    Storage(#[fail(cause)] StorageError),
+    #[error("Storage error: {0}")]
+    Storage(#[source] StorageError),
 
-    #[fail(display = "Evaluate error: {}", _0)]
-    Evaluate(#[fail(cause)] EvaluateError),
+    #[error("Evaluate error: {0}")]
+    Evaluate(#[source] EvaluateError),
 }
 
 pub struct Error(pub Box<ErrorInner>);
@@ -120,6 +120,8 @@ impl<T: Into<EvaluateError>> From<T> for Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+const_assert!(8 == std::mem::size_of::<Result<()>>());
 
 impl ErrorCodeExt for Error {
     fn error_code(&self) -> ErrorCode {
--- components/tidb_query_common/src/lib.rs
+++ components/tidb_query_common/src/lib.rs
@@ -1,6 +1,9 @@
 #![feature(min_specialization)]
 
 #[macro_use]
+extern crate static_assertions;
+
+#[macro_use]
 pub mod macros;
 
 pub mod error;
