57cefbd2d5d11b4f492e0a27b7e81c91f5405f5e
Ofey Chan <ofey206@gmail.com>
2020-09-27 15:56:28 +0800

    Remove all TODOs
    Signed-off-by: Ofey Chan <ofey206@gmail.com>

--- components/tidb_query_vec_expr/src/lib.rs
+++ components/tidb_query_vec_expr/src/lib.rs
@@ -197,7 +197,6 @@ fn truncate_int_mapper(rhs_is_unsigned: bool) -> RpnFnMeta {
 }
 
 fn truncate_uint_mapper(rhs_is_unsigned: bool) -> RpnFnMeta {
-    // TODO: TruncateUint
     if rhs_is_unsigned {
         truncate_uint_with_uint_fn_meta()
     } else {
@@ -476,7 +475,6 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::RoundInt => round_int_fn_meta(),
         ScalarFuncSig::RoundDec => round_dec_fn_meta(),
         ScalarFuncSig::TruncateInt => map_rhs_int_sig(value, children, truncate_int_mapper)?,
-        // TODO: TruncateUint
         ScalarFuncSig::TruncateUint => map_rhs_int_sig(value, children, truncate_uint_mapper)?,
         ScalarFuncSig::TruncateReal => map_rhs_int_sig(value, children, truncate_real_mapper)?,
         ScalarFuncSig::RoundWithFracInt => round_with_frac_int_fn_meta(),
