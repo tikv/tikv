16d9f63c79e94cb7940fa84c6920c163f581bb8c
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2020-10-28 20:48:56 +0800

    cdc: fix the ordering of batch events (#8629) (#8639)
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/cdc/src/service.rs
+++ components/cdc/src/service.rs
@@ -19,7 +19,7 @@ use crate::endpoint::{Deregister, Task};
 static CONNECTION_ID_ALLOC: AtomicUsize = AtomicUsize::new(0);
 
 const CDC_MSG_NOTIFY_COUNT: usize = 8;
-const CDC_MAX_RESP_SIZE: usize = 6 * 1024 * 1024; // 6MB
+const CDC_MAX_RESP_SIZE: u32 = 6 * 1024 * 1024; // 6MB
 const CDC_MSG_MAX_BATCH_SIZE: usize = 128;
 
 /// A unique identifier of a Connection.
@@ -32,17 +32,77 @@ impl ConnID {
     }
 }
 
+#[derive(Clone, Debug)]
 pub enum CdcEvent {
     ResolvedTs(ResolvedTs),
     Event(Event),
 }
 
 impl CdcEvent {
-    pub fn size(&self) -> usize {
+    pub fn size(&self) -> u32 {
         match self {
-            CdcEvent::ResolvedTs(r) => r.compute_size() as usize,
-            CdcEvent::Event(ref e) => e.compute_size() as usize,
+            CdcEvent::ResolvedTs(_) => 0,
+            CdcEvent::Event(ref e) => e.compute_size(),
         }
+    }
+
+    pub fn event(&self) -> &Event {
+        match self {
+            CdcEvent::ResolvedTs(_) => unreachable!(),
+            CdcEvent::Event(ref e) => e,
+        }
+    }
+
+    pub fn resolved_ts(&self) -> &ResolvedTs {
+        match self {
+            CdcEvent::ResolvedTs(ref r) => r,
+            CdcEvent::Event(_) => unreachable!(),
+        }
+    }
+}
+
+struct EventBatcher {
+    events: Vec<ChangeDataEvent>,
+    last_size: u32,
+}
+
+impl EventBatcher {
+    pub fn with_capacity(cap: usize) -> EventBatcher {
+        EventBatcher {
+            events: Vec::with_capacity(cap),
+            last_size: 0,
+        }
+    }
+
+    // The size of the response should not exceed CDC_MAX_RESP_SIZE.
+    // Split the events into multiple responses by CDC_MAX_RESP_SIZE here.
+    pub fn push(&mut self, event: CdcEvent) {
+        let size = event.size();
+        if self.events.is_empty() || self.last_size + size >= CDC_MAX_RESP_SIZE {
+            self.last_size = 0;
+            self.events.push(ChangeDataEvent::default());
+        }
+        match event {
+            CdcEvent::Event(e) => {
+                self.last_size += size;
+                self.events.last_mut().unwrap().mut_events().push(e);
+            }
+            CdcEvent::ResolvedTs(r) => {
+                let mut change_data_event = ChangeDataEvent::default();
+                change_data_event.set_resolved_ts(r);
+                self.events.push(change_data_event);
+                // Set last_size to MAX-1 for sending resolved ts as an individual event.
+                // '-1' is to avoid empty event when the next event is still resolved ts.
+                self.last_size = CDC_MAX_RESP_SIZE - 1;
+            }
+        }
+    }
+
+    pub fn build(self) -> Vec<ChangeDataEvent> {
+        self.events
+            .into_iter()
+            .filter(|e| e.has_resolved_ts() || !e.events.is_empty())
+            .collect()
     }
 }
 
@@ -171,38 +231,12 @@ impl ChangeData for Service {
         let rx = BatchReceiver::new(rx, CDC_MSG_MAX_BATCH_SIZE, Vec::new, VecCollector);
         let send_resp = sink.send_all(
             rx.map(|events| {
-                // The size of the response should not exceed CDC_MAX_RESP_SIZE.
-                // Split the events into multiple responses by CDC_MAX_RESP_SIZE here.
-                let events_len = events.len();
-                let mut resp_vecs = Vec::with_capacity(events_len);
-                resp_vecs.push(ChangeDataEvent::default());
-                let mut current_events_size = 0;
-                for event in events {
-                    if current_events_size >= CDC_MAX_RESP_SIZE {
-                        resp_vecs.push(ChangeDataEvent::default());
-                        current_events_size = 0;
-                    }
-                    let event_size = event.size();
-                    match event {
-                        CdcEvent::Event(e) => {
-                            resp_vecs.last_mut().unwrap().mut_events().push(e);
-                            current_events_size += event_size;
-                        }
-                        CdcEvent::ResolvedTs(r) => {
-                            // Set resolved ts as an individual event.
-                            let events = resp_vecs.last_mut().unwrap().take_events();
-                            let mut change_data_event = ChangeDataEvent::default();
-                            change_data_event.set_events(events);
-
-                            resp_vecs.last_mut().unwrap().set_resolved_ts(r);
-                            resp_vecs.push(change_data_event);
-                        }
-                    }
-                }
-                let resps = resp_vecs
+                let mut batcher = EventBatcher::with_capacity(events.len());
+                events.into_iter().for_each(|e| batcher.push(e));
+                let resps = batcher
+                    .build()
                     .into_iter()
-                    .filter(|e| e.has_resolved_ts() || !e.events.is_empty())
-                    .map(|resp| (resp, WriteFlags::default()));
+                    .map(|e| (e, WriteFlags::default()));
                 stream::iter_ok(resps)
             })
             .flatten()
@@ -246,5 +280,72 @@ impl ChangeData for Service {
             }
             Ok(())
         }));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "prost-codec")]
+    use kvproto::cdcpb::event::{
+        Entries as EventEntries, Event as Event_oneof_event, Row as EventRow,
+    };
+    use kvproto::cdcpb::{ChangeDataEvent, Event, ResolvedTs};
+    #[cfg(not(feature = "prost-codec"))]
+    use kvproto::cdcpb::{EventEntries, EventRow, Event_oneof_event};
+
+    use crate::service::{CdcEvent, EventBatcher, CDC_MAX_RESP_SIZE};
+
+    #[test]
+    fn test_event_batcher() {
+        let mut batcher = EventBatcher::with_capacity(1024);
+
+        let check_events = |result: Vec<ChangeDataEvent>, expected: Vec<Vec<CdcEvent>>| {
+            assert_eq!(result.len(), expected.len());
+
+            for i in 0..expected.len() {
+                if !result[i].has_resolved_ts() {
+                    assert_eq!(result[i].events.len(), expected[i].len());
+                    for j in 0..expected[i].len() {
+                        assert_eq!(&result[i].events[j], expected[i][j].event());
+                    }
+                } else {
+                    assert_eq!(expected[i].len(), 1);
+                    assert_eq!(result[i].get_resolved_ts(), expected[i][0].resolved_ts());
+                }
+            }
+        };
+
+        let mut event_small = Event::default();
+        let row_small = EventRow::default();
+        let mut event_entries = EventEntries::default();
+        event_entries.entries = vec![row_small].into();
+        event_small.event = Some(Event_oneof_event::Entries(event_entries));
+
+        let mut event_big = Event::default();
+        let mut row_big = EventRow::default();
+        row_big.set_key(vec![0 as u8; CDC_MAX_RESP_SIZE as usize]);
+        let mut event_entries = EventEntries::default();
+        event_entries.entries = vec![row_big].into();
+        event_big.event = Some(Event_oneof_event::Entries(event_entries));
+
+        batcher.push(CdcEvent::Event(event_small.clone()));
+        batcher.push(CdcEvent::ResolvedTs(ResolvedTs::default()));
+        batcher.push(CdcEvent::ResolvedTs(ResolvedTs::default()));
+        batcher.push(CdcEvent::Event(event_big.clone()));
+        batcher.push(CdcEvent::Event(event_small.clone()));
+        batcher.push(CdcEvent::Event(event_small.clone()));
+        batcher.push(CdcEvent::Event(event_big.clone()));
+
+        check_events(
+            batcher.build(),
+            vec![
+                vec![CdcEvent::Event(event_small.clone())],
+                vec![CdcEvent::ResolvedTs(ResolvedTs::default())],
+                vec![CdcEvent::ResolvedTs(ResolvedTs::default())],
+                vec![CdcEvent::Event(event_big.clone())],
+                vec![CdcEvent::Event(event_small); 2],
+                vec![CdcEvent::Event(event_big)],
+            ],
+        );
     }
 }
