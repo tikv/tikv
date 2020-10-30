59b54c1d9812e38a996874be623f5deb6fe710f1
MyonKeminta <MyonKeminta@users.noreply.github.com>
2020-10-30 11:59:10 +0800

    address comments
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- src/storage/txn/commands/prewrite.rs
+++ src/storage/txn/commands/prewrite.rs
@@ -299,7 +299,7 @@ impl<S: Snapshot, L: LockManager> WriteCommand<S, L> for Prewrite {
             };
             (pr, WriteData::default(), 0, self.ctx, None, vec![])
         };
-        // Currently if `try_one_pc` is set, it must have been succeeded here.
+        // Currently if `try_one_pc` is set, it must have succeeded here.
         let response_policy =
             if (!async_commit_ts.is_zero() || self.try_one_pc) && context.async_apply_prewrite {
                 ResponsePolicy::OnCommitted
