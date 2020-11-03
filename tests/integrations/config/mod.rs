b8a440a96b33774c738c520146624e0ab8b70f45
Little-Wallace <bupt2013211450@gmail.com>
2020-11-03 16:39:41 +0800

    fix config
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- tests/integrations/config/mod.rs
+++ tests/integrations/config/mod.rs
@@ -160,7 +160,7 @@ fn test_serde_custom_tikv_config() {
         split_region_check_tick_interval: ReadableDuration::secs(12),
         region_split_check_diff: ReadableSize::mb(6),
         region_compact_check_interval: ReadableDuration::secs(12),
-        clean_stale_peer_delay: ReadableDuration::secs(13),
+        clean_stale_peer_delay: ReadableDuration::minutes(11),
         region_compact_check_step: 1_234,
         region_compact_min_tombstones: 999,
         region_compact_tombstones_percent: 33,
