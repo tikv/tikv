224f659ff99533b26d1cef34635f860621e3907e
Neil Shen <overvenus@gmail.com>
2020-11-07 14:23:00 +0800

    Rollup of CDC commits  (#8969)
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/cdc/tests/failpoints/mod.rs
+++ components/cdc/tests/failpoints/mod.rs
@@ -1,4 +1,12 @@
 // Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
+
+#![feature(custom_test_frameworks)]
+#![test_runner(test_util::run_failpoint_tests)]
+
 mod test_observe;
 mod test_register;
 mod test_resolve;
+
+#[path = "../mod.rs"]
+mod testsuite;
+pub use testsuite::*;
