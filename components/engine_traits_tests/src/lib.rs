74a38e8cb9d1ab079d78ddf9351f5267730cf556
Brian Anderson <andersrb@gmail.com>
2020-11-13 23:00:34 +0000

    engine_traits_tests: Add tests for snapshot read consistency
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_traits_tests/src/lib.rs
+++ components/engine_traits_tests/src/lib.rs
@@ -780,7 +780,72 @@ mod snapshot_basic {
         let value = value.unwrap();
         assert_eq!(value, b"aa");
     }
+}
 
+mod read_consistency {
+    //! Testing iterator and snapshot behavior in the presence of intermixed writes
+
+    use super::{default_engine};
+    use engine_traits::{KvEngine, SyncMutable, Peekable};
+    use engine_traits::{Iterable, Iterator};
+
+    #[test]
+    fn snapshot_with_writes() {
+        let db = default_engine();
+
+        db.engine.put(b"a", b"aa").unwrap();
+
+        let snapshot = db.engine.snapshot();
+
+        assert_eq!(snapshot.get_value(b"a").unwrap().unwrap(), b"aa");
+
+        db.engine.put(b"b", b"bb").unwrap();
+
+        assert!(snapshot.get_value(b"b").unwrap().is_none());
+        assert_eq!(db.engine.get_value(b"b").unwrap().unwrap(), b"bb");
+
+        db.engine.delete(b"a").unwrap();
+
+        assert_eq!(snapshot.get_value(b"a").unwrap().unwrap(), b"aa");
+        assert!(db.engine.get_value(b"a").unwrap().is_none());
+    }
+
+    #[test]
+    fn snapshot_iterator_with_writes() {
+        let db = default_engine();
+
+        db.engine.put(b"a", b"").unwrap();
+        db.engine.put(b"c", b"").unwrap();
+
+        let snapshot = db.engine.snapshot();
+        let mut iter = snapshot.iterator().unwrap();
+
+        assert!(iter.seek_to_first().unwrap());
+        assert_eq!(iter.key(), b"a");
+
+        db.engine.put(b"b", b"").unwrap();
+
+        assert!(iter.next().unwrap());
+        assert_eq!(iter.key(), b"c");
+        assert!(db.engine.get_value(b"b").unwrap().is_some());
+
+        db.engine.put(b"d", b"").unwrap();
+
+        assert!(!iter.next().unwrap());
+        assert!(db.engine.get_value(b"d").unwrap().is_some());
+
+        db.engine.delete(b"a").unwrap();
+        db.engine.delete(b"c").unwrap();
+
+        iter.seek_to_first().unwrap();
+        assert_eq!(iter.key(), b"a");
+        assert!(iter.next().unwrap());
+        assert_eq!(iter.key(), b"c");
+        assert!(!iter.next().unwrap());
+
+        assert!(db.engine.get_value(b"a").unwrap().is_none());
+        assert!(db.engine.get_value(b"c").unwrap().is_none());
+    }
 }
 
 mod misc {
