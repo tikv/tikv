8620e2a9c54dceadaac54021efa9dcccaeea0339
Brian Anderson <andersrb@gmail.com>
2020-11-13 08:28:34 +0000

    engine_traits_tests: Add some snapshot tests
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_traits_tests/src/lib.rs
+++ components/engine_traits_tests/src/lib.rs
@@ -555,3 +555,86 @@ mod engine_iter {
     }
 }
 
+mod misc {
+    use super::{default_engine};
+    use engine_traits::{KvEngine, SyncMutable, Peekable};
+
+    #[test]
+    fn sync_basic() {
+        let db = default_engine();
+        db.engine.put(b"foo", b"bar").unwrap();
+        db.engine.sync().unwrap();
+        let value = db.engine.get_value(b"foo").unwrap();
+        let value = value.expect("value");
+        assert_eq!(b"bar", &*value);
+    }
+}
+
+mod snapshot {
+    use super::{default_engine, engine_cfs};
+    use engine_traits::{KvEngine, SyncMutable, Peekable};
+    use engine_traits::{ALL_CFS, CF_WRITE};
+
+    #[test]
+    fn snapshot_get_value() {
+        let db = default_engine();
+
+        db.engine.put(b"a", b"aa").unwrap();
+
+        let snap = db.engine.snapshot();
+
+        let value = snap.get_value(b"a").unwrap();
+        let value = value.unwrap();
+        assert_eq!(value, b"aa");
+
+        let value = snap.get_value(b"b").unwrap();
+        assert!(value.is_none());
+    }
+
+    #[test]
+    fn snapshot_get_value_after_put() {
+        let db = default_engine();
+
+        db.engine.put(b"a", b"aa").unwrap();
+
+        let snap = db.engine.snapshot();
+
+        db.engine.put(b"a", b"aaa").unwrap();
+
+        let value = snap.get_value(b"a").unwrap();
+        let value = value.unwrap();
+        assert_eq!(value, b"aa");
+    }
+
+    #[test]
+    fn snapshot_get_value_cf() {
+        let db = engine_cfs(ALL_CFS);
+
+        db.engine.put_cf(CF_WRITE, b"a", b"aa").unwrap();
+
+        let snap = db.engine.snapshot();
+
+        let value = snap.get_value_cf(CF_WRITE, b"a").unwrap();
+        let value = value.unwrap();
+        assert_eq!(value, b"aa");
+
+        let value = snap.get_value_cf(CF_WRITE, b"b").unwrap();
+        assert!(value.is_none());
+    }
+
+    #[test]
+    fn snapshot_get_value_cf_after_put() {
+        let db = engine_cfs(ALL_CFS);
+
+        db.engine.put_cf(CF_WRITE, b"a", b"aa").unwrap();
+
+        let snap = db.engine.snapshot();
+
+        db.engine.put_cf(CF_WRITE, b"a", b"aaa").unwrap();
+
+        let value = snap.get_value_cf(CF_WRITE, b"a").unwrap();
+        let value = value.unwrap();
+        assert_eq!(value, b"aa");
+    }
+
+}
