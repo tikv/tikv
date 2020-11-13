94a4d65b917340dc4508f8b9479b286669bc8bb0
Brian Anderson <andersrb@gmail.com>
2020-11-13 21:59:22 +0000

    engine_traits_tests: Add tests for snapshot cf_names
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_traits_tests/src/lib.rs
+++ components/engine_traits_tests/src/lib.rs
@@ -188,7 +188,7 @@ mod basic_read_write {
 
 mod cf_names {
     use super::{default_engine, engine_cfs};
-    use engine_traits::CFNamesExt;
+    use engine_traits::{KvEngine, CFNamesExt, Snapshot};
     use engine_traits::{CF_DEFAULT, ALL_CFS, CF_WRITE};
 
     #[test]
@@ -213,6 +213,35 @@ mod cf_names {
     fn implicit_default_cf() {
         let db = engine_cfs(&[CF_WRITE]);
         let names = db.engine.cf_names();
+        assert_eq!(names.len(), 2);
+        assert!(names.contains(&CF_DEFAULT));
+    }
+
+    #[test]
+    fn default_names_snapshot() {
+        let db = default_engine();
+        let snapshot = db.engine.snapshot();
+        let names = snapshot.cf_names();
+        assert_eq!(names.len(), 1);
+        assert_eq!(names[0], CF_DEFAULT);
+    }
+
+    #[test]
+    fn cf_names_snapshot() {
+        let db = engine_cfs(ALL_CFS);
+        let snapshot = db.engine.snapshot();
+        let names = snapshot.cf_names();
+        assert_eq!(names.len(), ALL_CFS.len());
+        for cf in ALL_CFS {
+            assert!(names.contains(cf));
+        }
+    }
+
+    #[test]
+    fn implicit_default_cf_snapshot() {
+        let db = engine_cfs(&[CF_WRITE]);
+        let snapshot = db.engine.snapshot();
+        let names = snapshot.cf_names();
         assert_eq!(names.len(), 2);
         assert!(names.contains(&CF_DEFAULT));
     }
