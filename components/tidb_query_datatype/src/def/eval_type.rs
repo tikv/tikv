80c0a61cedb4f1c070a106527abd4cc4fb3322dc
Xuanwo <github@xuanwo.io>
2020-11-16 16:49:07 +0800

    Fix tests
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- components/tidb_query_datatype/src/def/eval_type.rs
+++ components/tidb_query_datatype/src/def/eval_type.rs
@@ -72,10 +72,9 @@ impl std::convert::TryFrom<crate::FieldTypeTp> for EvalType {
             | crate::FieldTypeTp::Blob
             | crate::FieldTypeTp::VarString
             | crate::FieldTypeTp::String => EvalType::Bytes,
-            crate::FieldTypeTp::Enum => EvalType::Enum,
-            crate::FieldTypeTp::Set => EvalType::Set,
             _ => {
                 // In TiDB, Bit's eval type is Int, but it is not yet supported in TiKV.
+                // TODO: we need to handle FieldTypeTp::{Enum, Set} after we implement encode and decode.
                 return Err(crate::DataTypeError::UnsupportedType {
                     name: tp.to_string(),
                 });
