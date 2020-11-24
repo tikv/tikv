44b97a038cebfc79226cad1ee887f7b07f958622
Lenshood <7877221+LENSHOOD@users.noreply.github.com>
2020-11-25 01:33:06 +0800

    copr: migrate cast decimal as time expr compatible with non-vec (#15185) (#8737) (#9031)
    Cast decimal to time may get wrong result because of different cast
    policy with mysql. This commit fix it by add a parse_from_deciaml()
    method to tidb_query_datatype::codec::mysql::Time, which deal with
    decimal whole part as int, frac part as str.
    
    The reason is that the parse logic of datetime is different when
    dealing with strings and numbers. In short, when dealing with numbers,
    if number length are not 6, 8, 12, 14, the parse logic should padded
    with leading zeros to the closest length. But when dealing with strings,
    the parse logic will interpreted from left to right to find year, month,
    day, hour... for as many parts as are present in the string.
    
    For more details, see
    https://dev.mysql.com/doc/refman/5.7/en/date-and-time-literals.html.
    Related tidb issue:
    https://github.com/pingcap/tidb/issues/15185
    Signed-off-by: Lenshood <7877221+LENSHOOD@users.noreply.github.com>

--- components/tidb_query/src/expr/builtin_cast.rs
+++ components/tidb_query/src/expr/builtin_cast.rs
@@ -433,8 +433,7 @@ impl ScalarFunc {
         row: &'a [Datum],
     ) -> Result<Option<Cow<'a, Time>>> {
         let val = try_opt!(self.children[0].eval_decimal(ctx, row));
-        let s = val.to_string();
-        Ok(Some(self.produce_time_with_float_str(ctx, &s)?))
+        Ok(Some(self.produce_time_with_float_str(ctx, val)?))
     }
 
     pub fn cast_str_as_time<'a, 'b: 'a>(
@@ -739,8 +738,18 @@ impl ScalarFunc {
         Ok(Cow::Owned(t))
     }
 
-    fn produce_time_with_float_str(&self, ctx: &mut EvalContext, s: &str) -> Result<Cow<'_, Time>> {
-        let mut t = Time::parse_datetime(ctx, s, self.field_type.decimal() as i8, true)?;
+    fn produce_time_with_float_str<'a, 'b: 'a>(
+        &'b self,
+        ctx: &mut EvalContext,
+        val: Cow<'a, Decimal>,
+    ) -> Result<Cow<'_, Time>> {
+        let mut t = Time::parse_from_decimal(
+            ctx,
+            &val.into_owned(),
+            self.field_type.as_accessor().tp().try_into()?,
+            self.field_type.decimal() as i8,
+            true,
+        )?;
         t.set_time_type(self.field_type.as_accessor().tp().try_into()?)?;
         Ok(Cow::Owned(t))
     }
