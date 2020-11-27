36cca064075418f720eecbbf9aafdfe7effb5f69
qupeng <qupeng@pingcap.com>
2020-11-27 21:07:30 +0800

    handles mvcc delete marks correctly
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- components/engine_rocks/src/raw.rs
+++ components/engine_rocks/src/raw.rs
@@ -13,7 +13,8 @@ pub use rocksdb::{
     CompactionJobInfo, CompactionPriority, DBBottommostLevelCompaction, DBCompactionFilter,
     DBCompactionStyle, DBCompressionType, DBEntryType, DBInfoLogLevel, DBIterator, DBOptions,
     DBRateLimiterMode, DBRecoveryMode, DBStatisticsTickerType, DBTitanDBBlobRunMode, Env,
-    EventListener, IngestExternalFileOptions, LRUCacheOptions, MemoryAllocator, PerfContext, Range,
-    ReadOptions, SeekKey, SliceTransform, TableFilter, TablePropertiesCollector,
-    TablePropertiesCollectorFactory, TitanBlobIndex, TitanDBOptions, Writable, WriteOptions, DB,
+    EventListener, FullKey, IngestExternalFileOptions, LRUCacheOptions, MemoryAllocator,
+    PerfContext, Range, ReadOptions, SeekKey, SliceTransform, TableFilter,
+    TablePropertiesCollector, TablePropertiesCollectorFactory, TitanBlobIndex, TitanDBOptions,
+    Writable, WriteOptions, DB,
 };
