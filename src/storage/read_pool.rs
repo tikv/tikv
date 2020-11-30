a5021496682ca422502cde81ad9d1ce821bab5e0
Pramod Biligiri <pramodbiligiri@gmail.com>
2020-11-30 22:26:38 +0530

    A few more docs
    Signed-off-by: Pramod Biligiri <pramodbiligiri@gmail.com>

--- src/storage/read_pool.rs
+++ src/storage/read_pool.rs
@@ -1,5 +1,7 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
+//! Distinct thread pools to handle read commands having different priority levels
+
 use crate::config::StorageReadPoolConfig;
 use crate::storage::kv::{destroy_tls_engine, set_tls_engine, Engine, FlowStatsReporter};
 use crate::storage::metrics;
@@ -17,6 +19,7 @@ impl<R: FlowStatsReporter> PoolTicker for FuturePoolTicker<R> {
     }
 }
 
+/// Create respective thread pools to handle read commands of different priority levels
 pub fn build_read_pool<E: Engine, R: FlowStatsReporter>(
     config: &StorageReadPoolConfig,
     reporter: R,
@@ -45,6 +48,7 @@ pub fn build_read_pool<E: Engine, R: FlowStatsReporter>(
         .collect()
 }
 
+// Thread pools created for test have different behaviour on tick
 pub fn build_read_pool_for_test<E: Engine>(
     config: &StorageReadPoolConfig,
     engine: E,
