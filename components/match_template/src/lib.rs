1563630d3df5aa2fcdae57203335051c0114fb02
Andy Lok <andylokandy@hotmail.com>
2020-11-30 23:42:00 +0800

    temp
    Signed-off-by: Andy Lok <andylokandy@hotmail.com>

--- components/match_template/src/lib.rs
+++ components/match_template/src/lib.rs
@@ -1,66 +1,65 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
-//! This crate provides a macro that can be used to append a match expression with multiple
-//! arms, where the tokens in the first arm, as a template, can be subsitituted and the template
-//! arm will be expanded into multiple arms.
-//!
-//! For example, the following code
-//!
-//! ```ignore
-//! match_template! {
-//!     T = [Int, Real, Double],
-//!     match Foo {
-//!         EvalType::T => { panic!("{}", EvalType::T); },
-//!         EvalType::Other => unreachable!(),
-//!     }
-//! }
-//! ```
-//!
-//! generates
-//!
-//! ```ignore
-//! match Foo {
-//!     EvalType::Int => { panic!("{}", EvalType::Int); },
-//!     EvalType::Real => { panic!("{}", EvalType::Real); },
-//!     EvalType::Double => { panic!("{}", EvalType::Double); },
-//!     EvalType::Other => unreachable!(),
-//! }
-//! ```
-//!
-//! In addition, substitution can vary on two sides of the arms.
-//!
-//! For example,
-//!
-//! ```ignore
-//! match_template! {
-//!     T = [Foo, Bar => Baz],
-//!     match Foo {
-//!         EvalType::T => { panic!("{}", EvalType::T); },
-//!     }
-//! }
-//! ```
-//!
-//! generates
-//!
-//! ```ignore
-//! match Foo {
-//!     EvalType::Foo => { panic!("{}", EvalType::Foo); },
-//!     EvalType::Bar => { panic!("{}", EvalType::Baz); },
-//! }
-//! ```
-//!
-//! Wildcard match arm is also supported (but there will be no substitution).
-
 #[macro_use]
 extern crate quote;
 extern crate proc_macro;
 
-use proc_macro2::{TokenStream, TokenTree};
-use syn::fold::Fold;
+use proc_macro2::{Group, TokenStream, TokenTree};
+use quote::ToTokens;
 use syn::parse::{Parse, ParseStream, Result};
 use syn::punctuated::Punctuated;
 use syn::*;
 
+/// This crate provides a macro that can be used to append a match expression with multiple
+/// arms, where the tokens in the first arm, as a template, can be subsitituted and the template
+/// arm will be expanded into multiple arms.
+///
+/// For example, the following code
+///
+/// ```ignore
+/// match_template! {
+///     T = [Int, Real, Double],
+///     match Foo {
+///         EvalType::T => { panic!("{}", EvalType::T); },
+///         EvalType::Other => unreachable!(),
+///     }
+/// }
+/// ```
+///
+/// generates
+///
+/// ```ignore
+/// match Foo {
+///     EvalType::Int => { panic!("{}", EvalType::Int); },
+///     EvalType::Real => { panic!("{}", EvalType::Real); },
+///     EvalType::Double => { panic!("{}", EvalType::Double); },
+///     EvalType::Other => unreachable!(),
+/// }
+/// ```
+///
+/// In addition, substitution can vary on two sides of the arms.
+///
+/// For example,
+///
+/// ```ignore
+/// match_template! {
+///     T = [Foo, Bar => Baz],
+///     match Foo {
+///         EvalType::T => { panic!("{}", EvalType::T); },
+///     }
+/// }
+/// ```
+///
+/// generates
+///
+/// ```ignore
+/// match Foo {
+///     EvalType::Foo => { panic!("{}", EvalType::Foo); },
+///     EvalType::Bar => { panic!("{}", EvalType::Baz); },
+/// }
+/// ```
+///
+/// Wildcard match arm is also supported (but there will be no substitution).
 #[proc_macro]
 pub fn match_template(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mt = parse_macro_input!(input as MatchTemplate);
@@ -110,19 +109,18 @@ impl MatchTemplate {
             remaining_arms,
         } = self;
         let match_arms = substitutes.into_iter().map(|substitute| {
-            let mut folder = match substitute {
-                Substitution::Identical(ident) => MatchArmIdentFolder {
-                    template_ident: template_ident.clone(),
-                    left_ident: ident.clone(),
-                    right_ident: ident,
-                },
-                Substitution::Map(left_ident, right_ident) => MatchArmIdentFolder {
-                    template_ident: template_ident.clone(),
-                    left_ident,
-                    right_ident,
-                },
+            let mut arm = template_arm.clone();
+            let (left_tokens, right_tokens) = match substitute {
+                Substitution::Identical(ident) => {
+                    (ident.clone().into_token_stream(), ident.into_token_stream())
+                }
+                Substitution::Map(left_ident, right_tokens) => {
+                    (left_ident.into_token_stream(), right_tokens)
+                }
             };
-            folder.fold_arm(template_arm.clone())
+            arm.pat = replace_in_token_stream(arm.pat, &template_ident, &left_tokens);
+            arm.body = replace_in_token_stream(arm.body, &template_ident, &right_tokens);
+            arm
         });
         quote! {
             match #match_exp {
@@ -133,78 +131,53 @@ impl MatchTemplate {
     }
 }
 
+#[derive(Debug)]
 enum Substitution {
     Identical(Ident),
-    Map(Ident, Ident),
+    Map(Ident, TokenStream),
 }
 
 impl Parse for Substitution {
     fn parse(input: ParseStream<'_>) -> Result<Self> {
-        let first_ident = input.parse()?;
+        let template_ident = input.parse()?;
         let fat_arrow: Option<Token![=>]> = input.parse()?;
         if fat_arrow.is_some() {
-            let second_ident = input.parse()?;
-            Ok(Substitution::Map(first_ident, second_ident))
+            let mut tokens: Vec<TokenTree> = vec![];
+            while !input.peek(Token![,]) && !input.is_empty() {
+                tokens.push(input.parse()?);
+            }
+            Ok(Substitution::Map(
+                template_ident,
+                tokens.into_iter().collect(),
+            ))
         } else {
-            Ok(Substitution::Identical(first_ident))
+            Ok(Substitution::Identical(template_ident))
         }
     }
 }
 
-struct MatchArmIdentFolder {
-    template_ident: Ident,
-    left_ident: Ident,
-    right_ident: Ident,
-}
+fn replace_in_token_stream<T: ToTokens + Parse>(
+    input: T,
+    from_ident: &Ident,
+    to_tokens: &TokenStream,
+) -> T {
+    let mut tokens = TokenStream::new();
+    input.to_tokens(&mut tokens);
 
-impl Fold for MatchArmIdentFolder {
-    fn fold_pat(&mut self, i: Pat) -> Pat {
-        ReplaceIdentFolder {
-            from_ident: self.template_ident.clone(),
-            to_ident: self.left_ident.clone(),
-        }
-        .fold_pat(i)
-    }
+    let tokens: TokenStream = tokens
+        .into_iter()
+        .flat_map(|token| match token {
+            TokenTree::Ident(ident) if ident == *from_ident => to_tokens.clone(),
+            TokenTree::Group(group) => Group::new(
+                group.delimiter(),
+                replace_in_token_stream(group.stream(), from_ident, to_tokens),
+            )
+            .into_token_stream(),
+            other => other.into(),
+        })
+        .collect();
 
-    fn fold_expr(&mut self, i: Expr) -> Expr {
-        ReplaceIdentFolder {
-            from_ident: self.template_ident.clone(),
-            to_ident: self.right_ident.clone(),
-        }
-        .fold_expr(i)
-    }
-}
-
-struct ReplaceIdentFolder {
-    from_ident: Ident,
-    to_ident: Ident,
-}
-
-impl Fold for ReplaceIdentFolder {
-    fn fold_macro(&mut self, i: Macro) -> Macro {
-        let mut m = syn::fold::fold_macro(self, i);
-        m.tokens = m
-            .tokens
-            .into_iter()
-            .map(|tt| {
-                if let TokenTree::Ident(i) = &tt {
-                    if i == &self.from_ident {
-                        return TokenTree::Ident(self.to_ident.clone());
-                    }
-                }
-                tt
-            })
-            .collect();
-        m
-    }
-
-    fn fold_ident(&mut self, i: Ident) -> Ident {
-        if i == self.from_ident {
-            self.to_ident.clone()
-        } else {
-            i
-        }
-    }
+    syn::parse2(tokens).unwrap()
 }
 
 #[cfg(test)]
@@ -263,7 +236,7 @@ mod tests {
     #[test]
     fn test_map() {
         let input = r#"
-            TT = [Foo, Bar => Baz],
+            TT = [Foo, Bar => Baz, Bark => <&'static Whooh>()],
             match v {
                 VectorValue::TT => EvalType::TT,
                 EvalType::Other => unreachable!(),
@@ -274,6 +247,7 @@ mod tests {
             match v {
                 VectorValue::Foo => EvalType::Foo,
                 VectorValue::Bar => EvalType::Baz,
+                VectorValue::Bark => EvalType:: < & 'static Whooh>(),
                 EvalType::Other => unreachable!(),
             }
         "#;
