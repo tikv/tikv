1f4cb58f953d17993fb70ddc224cd3dba4e5bd3a
5kbpers <tangminghua@pingcap.com>
2020-12-03 13:24:04 +0800

    *: fix clippy
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/tidb_query_datatype/src/codec/mysql/enums.rs
+++ components/tidb_query_datatype/src/codec/mysql/enums.rs
@@ -140,7 +140,7 @@ mod tests {
     #[test]
     fn test_is_empty() {
         let mut buf = BufferVec::new();
-        for v in vec!["a", "b", "c"] {
+        for v in &["a", "b", "c"] {
             buf.push(v)
         }
 
@@ -152,7 +152,7 @@ mod tests {
         assert!(!s.as_ref().is_empty());
 
         let s = Enum {
-            data: s.data.clone(),
+            data: s.data,
             value: 0,
         };
 
