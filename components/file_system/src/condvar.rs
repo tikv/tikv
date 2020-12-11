9ac75a5233c5a1aef3f77a6ebec5e4433d0f5a2f
tabokie <xy.tao@outlook.com>
2020-12-11 17:25:51 +0800

    use enum dispatch and NonNull
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- components/file_system/src/condvar.rs
+++ components/file_system/src/condvar.rs
@@ -2,75 +2,49 @@
 
 use futures::{future::FutureExt, pin_mut, select};
 use std::cell::Cell;
+use std::ptr::NonNull;
 use std::sync::Condvar as StdCondvar;
 use std::sync::{Mutex, MutexGuard};
 use std::time::Duration;
 use tokio::sync::Semaphore as AsyncSemaphore;
 
-trait LinkedNotifiable {
-    fn notify(&self);
-    fn get_next(&self) -> Option<*mut dyn LinkedNotifiable>;
-    fn set_next(&self, next: Option<*mut dyn LinkedNotifiable>);
+enum LinkedNotifiable {
+    Sync(StdCondvar, Cell<Option<NonNull<LinkedNotifiable>>>),
+    Async(AsyncSemaphore, Cell<Option<NonNull<LinkedNotifiable>>>),
 }
 
-#[derive(Debug)]
-struct SyncCondvarNode {
-    condv: StdCondvar,
-    next: Cell<Option<*mut dyn LinkedNotifiable>>,
-}
+impl LinkedNotifiable {
+    pub fn new_sync() -> LinkedNotifiable {
+        LinkedNotifiable::Sync(StdCondvar::new(), Cell::new(None))
+    }
+    pub fn new_async() -> LinkedNotifiable {
+        LinkedNotifiable::Async(AsyncSemaphore::new(0), Cell::new(None))
+    }
 
-impl SyncCondvarNode {
-    pub fn new() -> SyncCondvarNode {
-        SyncCondvarNode {
-            condv: StdCondvar::new(),
-            next: Cell::new(None),
+    pub fn notify(&self) {
+        match self {
+            &LinkedNotifiable::Sync(ref condv, _) => condv.notify_one(),
+            &LinkedNotifiable::Async(ref sem, _) => sem.add_permits(1),
         }
     }
-}
-
-impl LinkedNotifiable for SyncCondvarNode {
-    fn notify(&self) {
-        self.condv.notify_one();
-    }
-    fn get_next(&self) -> Option<*mut dyn LinkedNotifiable> {
-        self.next.get()
-    }
-    fn set_next(&self, next: Option<*mut dyn LinkedNotifiable>) {
-        self.next.set(next);
-    }
-}
-
-#[derive(Debug)]
-struct AsyncCondvarNode {
-    sem: AsyncSemaphore,
-    next: Cell<Option<*mut dyn LinkedNotifiable>>,
-}
-
-impl AsyncCondvarNode {
-    pub fn new() -> AsyncCondvarNode {
-        AsyncCondvarNode {
-            sem: AsyncSemaphore::new(0),
-            next: Cell::new(None),
+    pub fn get_next(&self) -> Option<NonNull<LinkedNotifiable>> {
+        match self {
+            &LinkedNotifiable::Sync(_, ref ptr) => ptr.get(),
+            &LinkedNotifiable::Async(_, ref ptr) => ptr.get(),
         }
     }
-}
-
-impl LinkedNotifiable for AsyncCondvarNode {
-    fn notify(&self) {
-        self.sem.add_permits(1);
-    }
-    fn get_next(&self) -> Option<*mut dyn LinkedNotifiable> {
-        self.next.get()
-    }
-    fn set_next(&self, next: Option<*mut dyn LinkedNotifiable>) {
-        self.next.set(next);
+    pub fn set_next(&self, next: Option<NonNull<LinkedNotifiable>>) {
+        match self {
+            &LinkedNotifiable::Sync(_, ref ptr) => ptr.set(next),
+            &LinkedNotifiable::Async(_, ref ptr) => ptr.set(next),
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct Condvar {
-    head: Cell<Option<*mut dyn LinkedNotifiable>>,
-    tail: Cell<Option<*mut dyn LinkedNotifiable>>,
+    head: Cell<Option<NonNull<LinkedNotifiable>>>,
+    tail: Cell<Option<NonNull<LinkedNotifiable>>>,
 }
 
 unsafe impl Send for Condvar {}
@@ -89,19 +63,24 @@ impl Condvar {
         guard: MutexGuard<'a, T>,
         timeout: Duration,
     ) -> (MutexGuard<'a, T>, bool) {
-        let mut node = SyncCondvarNode::new();
-        let raw_tail: *mut _ = &mut node;
+        let mut node = LinkedNotifiable::new_sync();
+        let raw_node_ptr: *mut _ = &mut node;
+        let node_ptr = unsafe { NonNull::new_unchecked(raw_node_ptr) };
         if let Some(tail) = self.tail.get() {
             unsafe {
-                (*tail).set_next(Some(raw_tail));
+                tail.as_ref().set_next(Some(node_ptr.clone()));
             }
         } else {
-            self.head.set(Some(raw_tail));
+            self.head.set(Some(node_ptr.clone()));
         }
-        self.tail.set(Some(raw_tail));
+        self.tail.set(Some(node_ptr));
         // alternative: std::thread::park_timeout suffers from spurious wake
-        let (guard, res) = node.condv.wait_timeout(guard, timeout).unwrap();
-        self.notify_before_me(raw_tail);
+        let (guard, res) = match node {
+            LinkedNotifiable::Sync(ref condv, _) => condv.wait_timeout(guard, timeout).unwrap(),
+            _ => unreachable!(),
+        };
+        // let (guard, res) = node.condv.wait_timeout(guard, timeout).unwrap();
+        self.notify_before_me(raw_node_ptr);
         (guard, res.timed_out())
     }
 
@@ -113,34 +92,39 @@ impl Condvar {
     ) -> (MutexGuard<'a, T>, bool) {
         // it's safe to drop early because semaphore is state preserving
         std::mem::drop(guard);
-        let mut node = AsyncCondvarNode::new();
-        let raw_tail: *mut _ = &mut node;
+        let mut node = LinkedNotifiable::new_async();
+        let raw_node_ptr: *mut _ = &mut node;
+        let node_ptr = unsafe { NonNull::new_unchecked(raw_node_ptr) };
         if let Some(tail) = self.tail.get() {
             unsafe {
-                (*tail).set_next(Some(raw_tail));
+                tail.as_ref().set_next(Some(node_ptr));
             }
         } else {
-            self.head.set(Some(raw_tail));
+            self.head.set(Some(node_ptr));
         }
-        self.tail.set(Some(raw_tail));
-        let f = node.sem.acquire().fuse();
+        self.tail.set(Some(node_ptr));
+        let f = match node {
+            LinkedNotifiable::Async(ref sem, _) => sem.acquire().fuse(),
+            _ => unreachable!(),
+        };
+        // let f = node.sem.acquire().fuse();
         pin_mut!(f);
         let timed_out = select! {
             _ = f => false,
             _ = tokio::time::delay_for(timeout).fuse() => true,
         };
         let guard = mu.lock().unwrap();
-        self.notify_before_me(raw_tail);
+        self.notify_before_me(raw_node_ptr);
         (guard, timed_out)
     }
 
-    fn notify_before_me(&self, me: *mut dyn LinkedNotifiable) {
+    fn notify_before_me(&self, me: *mut LinkedNotifiable) {
         let mut ptr = self.head.get();
         loop {
             if let Some(inner) = ptr {
                 unsafe {
-                    let node = &(*inner);
-                    if inner == me {
+                    let node = inner.as_ref();
+                    if inner.as_ptr() == me {
                         self.head.set(node.get_next());
                         if self.head.get().is_none() {
                             self.tail.set(None);
@@ -162,7 +146,7 @@ impl Condvar {
     fn notify_head(&self) {
         if let Some(head) = self.head.get() {
             unsafe {
-                let node = &(*head);
+                let node = head.as_ref();
                 node.notify();
                 self.head.set(node.get_next());
             }
