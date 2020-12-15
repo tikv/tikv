1245d1fbc4e5376f91c8ada4a308c82e8b378ffe
Yi Wu <yiwu@pingcap.com>
2020-12-15 14:48:42 +0800

    add unit test
    Signed-off-by: Yi Wu <yiwu@pingcap.com>

--- components/raftstore/src/store/compaction_guard.rs
+++ components/raftstore/src/store/compaction_guard.rs
@@ -115,6 +115,22 @@ impl SstPartitioner for CompactionGuardGenerator {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::coprocessor::Result as CoprocessorResult;
+    use engine_rocks::{
+        raw::{BlockBasedOptions, ColumnFamilyOptions, DBCompressionType, DBOptions},
+        raw_util::{new_engine_opt, CFOptions},
+        RocksEngine, RocksSstPartitionerFactory, RocksSstReader,
+    };
+    use engine_traits::{
+        CompactExt, Iterator, MiscExt, SeekKey, SstReader, SyncMutable, CF_DEFAULT,
+    };
+    use keys::DATA_PREFIX_KEY;
+    use kvproto::metapb::Region;
+    use std::{
+        str,
+        sync::{Arc, Mutex},
+    };
+    use tempfile::TempDir;
 
     #[test]
     fn test_compaction_guard_should_partition() {
@@ -208,5 +224,138 @@ mod tests {
             SstPartitionerResult::NotRequired
         );
         assert_eq!(guard.pos.get(), 13);
+    }
+
+    struct MockProvider(Mutex<Vec<Region>>);
+
+    impl Clone for MockProvider {
+        fn clone(&self) -> Self {
+            MockProvider(Mutex::new(self.0.lock().unwrap().clone()))
+        }
+    }
+
+    impl RegionInfoProvider for MockProvider {
+        fn get_regions_in_range(
+            &self,
+            _start_key: &[u8],
+            _end_key: &[u8],
+        ) -> CoprocessorResult<Vec<Region>> {
+            Ok(self.0.lock().unwrap().clone())
+        }
+    }
+
+    const MIN_OUTPUT_FILE_SIZE: u64 = 1024;
+    const MAX_OUTPUT_FILE_SIZE: u64 = 4096;
+
+    fn new_test_db(provider: MockProvider) -> (RocksEngine, TempDir) {
+        let temp_dir = TempDir::new().unwrap();
+
+        let mut cf_opts = ColumnFamilyOptions::new();
+        cf_opts.set_target_file_size_base(MAX_OUTPUT_FILE_SIZE);
+        cf_opts.set_sst_partitioner_factory(RocksSstPartitionerFactory(
+            CompactionGuardGeneratorFactory::new(provider, MIN_OUTPUT_FILE_SIZE),
+        ));
+        cf_opts.set_disable_auto_compactions(true);
+        cf_opts.compression_per_level(&[
+            DBCompressionType::No,
+            DBCompressionType::No,
+            DBCompressionType::No,
+            DBCompressionType::No,
+            DBCompressionType::No,
+            DBCompressionType::No,
+            DBCompressionType::No,
+        ]);
+        // Make block size small to make sure current_output_file_size passed to SstPartitioner
+        // is accurate.
+        let mut block_based_opts = BlockBasedOptions::new();
+        block_based_opts.set_block_size(100);
+        cf_opts.set_block_based_table_factory(&block_based_opts);
+
+        let db = RocksEngine::from_db(Arc::new(
+            new_engine_opt(
+                temp_dir.path().to_str().unwrap(),
+                DBOptions::new(),
+                vec![CFOptions::new(CF_DEFAULT, cf_opts)],
+            )
+            .unwrap(),
+        ));
+        (db, temp_dir)
+    }
+
+    fn collect_keys(path: &str) -> Vec<Vec<u8>> {
+        let mut sst_reader = RocksSstReader::open(path).unwrap().iter();
+        let mut valid = sst_reader.seek(SeekKey::Start).unwrap();
+        let mut ret = vec![];
+        while valid {
+            ret.push(sst_reader.key().to_owned());
+            valid = sst_reader.next().unwrap();
+        }
+        ret
+    }
+
+    #[test]
+    fn test_compaction_guard_with_rocks() {
+        let provider = MockProvider(Mutex::new(vec![
+            Region {
+                id: 1,
+                start_key: b"a".to_vec(),
+                end_key: b"b".to_vec(),
+                ..Default::default()
+            },
+            Region {
+                id: 1,
+                start_key: b"b".to_vec(),
+                end_key: b"c".to_vec(),
+                ..Default::default()
+            },
+            Region {
+                id: 1,
+                start_key: b"c".to_vec(),
+                end_key: b"d".to_vec(),
+                ..Default::default()
+            },
+        ]));
+        let (db, dir) = new_test_db(provider);
+
+        // The following test assume data key starts with "z".
+        assert_eq!(b"z", DATA_PREFIX_KEY);
+
+        // Create two overlapping SST files then force compaction.
+        // Region "a" will share a SST file with region "b", since region "a" is too small.
+        // Region "c" will be splitted into two SSTs, since its size is larger than
+        // target_file_size_base.
+        let value = vec![b'v'; 1024];
+        db.put(b"za1", b"").unwrap();
+        db.put(b"zb1", &value).unwrap();
+        db.put(b"zc1", &value).unwrap();
+        db.flush(true /*sync*/).unwrap();
+        db.put(b"zb2", &value).unwrap();
+        db.put(b"zc2", &value).unwrap();
+        db.put(b"zc3", &value).unwrap();
+        db.put(b"zc4", &value).unwrap();
+        db.put(b"zc5", &value).unwrap();
+        db.put(b"zc6", &value).unwrap();
+        db.flush(true /*sync*/).unwrap();
+        db.compact_range(
+            CF_DEFAULT, None,  /*start_key*/
+            None,  /*end_key*/
+            false, /*exclusive_manual*/
+            1,     /*max_subcompactions*/
+        )
+        .unwrap();
+
+        let files = dir.path().read_dir().unwrap();
+        let mut sst_files = files
+            .map(|entry| entry.unwrap().path().to_str().unwrap().to_owned())
+            .filter(|entry| entry.ends_with(".sst"))
+            .collect::<Vec<String>>();
+        sst_files.sort();
+        assert_eq!(3, sst_files.len());
+        assert_eq!(collect_keys(&sst_files[0]), [b"za1", b"zb1", b"zb2"]);
+        assert_eq!(
+            collect_keys(&sst_files[1]),
+            [b"zc1", b"zc2", b"zc3", b"zc4", b"zc5"]
+        );
+        assert_eq!(collect_keys(&sst_files[2]), [b"zc6"]);
     }
 }
