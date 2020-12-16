26419f211ca12cf8bf91ccbe71037273e1373572
yiwu-arbug <yiwu@pingcap.com>
2020-12-16 14:59:34 -0800

    rocksdb: turn on compaction guard by default and add metrics (#9270)
    Signed-off-by: Yi Wu <yiwu@pingcap.com>

--- src/storage/kv/rocksdb_engine.rs
+++ src/storage/kv/rocksdb_engine.rs
@@ -212,9 +212,9 @@ impl TestEngineBuilder {
                 CF_DEFAULT => {
                     CFOptions::new(CF_DEFAULT, cfg_rocksdb.defaultcf.build_opt(&cache, None))
                 }
-                CF_LOCK => CFOptions::new(CF_LOCK, cfg_rocksdb.lockcf.build_opt(&cache, None)),
+                CF_LOCK => CFOptions::new(CF_LOCK, cfg_rocksdb.lockcf.build_opt(&cache)),
                 CF_WRITE => CFOptions::new(CF_WRITE, cfg_rocksdb.writecf.build_opt(&cache, None)),
-                CF_RAFT => CFOptions::new(CF_RAFT, cfg_rocksdb.raftcf.build_opt(&cache, None)),
+                CF_RAFT => CFOptions::new(CF_RAFT, cfg_rocksdb.raftcf.build_opt(&cache)),
                 _ => CFOptions::new(*cf, ColumnFamilyOptions::new()),
             })
             .collect();
