559cf01da06f4ee05c5fa455b763288d5eb0e7cf
qupeng <qupeng@pingcap.com>
2020-12-24 14:53:55 +0800

    server: register consistency check correctly
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- src/server/gc_worker/gc_worker.rs
+++ src/server/gc_worker/gc_worker.rs
@@ -649,9 +649,8 @@ where
     pub fn start_auto_gc<S: GcSafePointProvider, R: RegionInfoProvider>(
         &self,
         cfg: AutoGcConfig<S, R>,
-    ) -> Result<Arc<AtomicU64>> {
-        let safe_point = Arc::new(AtomicU64::new(0));
-
+        safe_point: Arc<AtomicU64>, // Store safe point here.
+    ) -> Result<()> {
         let kvdb = self.engine.kv_engine();
         let cfg_mgr = self.config_manager.clone();
         let feature_gate = self.feature_gate.clone();
@@ -661,14 +660,14 @@ where
         assert!(handle.is_none());
         let new_handle = GcManager::new(
             cfg,
-            safe_point.clone(),
+            safe_point,
             self.worker_scheduler.clone(),
             self.config_manager.clone(),
             self.feature_gate.clone(),
         )
         .start()?;
         *handle = Some(new_handle);
-        Ok(safe_point)
+        Ok(())
     }
 
     pub fn start(&mut self) -> Result<()> {
