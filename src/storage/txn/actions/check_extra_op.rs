a45e2a1572fb7349910092116bb5d42c4b8b0ba1
longfangsong <longfangsong@icloud.com>
2020-12-28 16:25:09 +0800

    add test for get_old_value
    Signed-off-by: longfangsong <longfangsong@icloud.com>

--- src/storage/txn/actions/check_extra_op.rs
+++ src/storage/txn/actions/check_extra_op.rs
@@ -37,15 +37,33 @@ fn get_old_value<S: Snapshot>(
     key: &Key,
     prev_write: Write,
 ) -> MvccResult<Option<OldValue>> {
+    debug_assert_eq!(
+        &Key::from_encoded(
+            txn.reader
+                .write_cursor
+                .as_ref()
+                .unwrap()
+                .key(&mut txn.reader.statistics.write)
+                .to_vec()
+        )
+        .truncate_ts()
+        .unwrap(),
+        key
+    );
+    debug_assert_eq!(
+        txn_types::WriteRef::parse(
+            txn.reader
+                .write_cursor
+                .as_ref()
+                .unwrap()
+                .value(&mut txn.reader.statistics.write)
+        )
+        .unwrap()
+        .to_owned(),
+        prev_write
+    );
     if prev_write.write_type == WriteType::Rollback || prev_write.write_type == WriteType::Lock {
         let write_cursor = txn.reader.write_cursor.as_mut().unwrap();
-        debug_assert_eq!(write_cursor.key(&mut txn.reader.statistics.write), key);
-        debug_assert_eq!(
-            txn_types::WriteRef::parse(cursor.value(&mut txn.reader.statistics.write))
-                .unwrap()
-                .to_owned(),
-            prev_write
-        );
         // Skip the current write record.
         write_cursor.next(&mut txn.reader.statistics.write);
         let write = seek_for_valid_write(
@@ -75,149 +93,123 @@ fn get_old_value<S: Snapshot>(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::storage::kv::WriteData;
-    use crate::storage::mvcc::txn::tests::txn_props;
-    use crate::storage::txn::{acquire_pessimistic_lock, commit, prewrite, CommitKind};
+    use crate::storage::mvcc::tests::write;
     use crate::storage::{Engine, TestEngineBuilder};
     use concurrency_manager::ConcurrencyManager;
     use kvproto::kvrpcpb::Context;
-    use txn_types::{Mutation, TimeStamp};
+    use txn_types::TimeStamp;
+
     #[test]
-    fn test_extra_op_old_value() {
-        let engine = TestEngineBuilder::new().build().unwrap();
-        let key = Key::from_raw(b"key");
-        let ctx = Context::default();
+    fn test_get_old_value() {
+        struct Case {
+            expected: Option<OldValue>,
 
-        let new_old_value = |short_value, start_ts| OldValue {
-            short_value,
-            start_ts,
-        };
-
+            // (write_record, put_ts)
+            // all data to write to the engine
+            // current write_cursor will be on the last record in `written`
+            // which also means prev_write is `Write` in the record
+            written: Vec<(Write, TimeStamp)>,
+        }
         let cases = vec![
-            (
-                Mutation::Put((key.clone(), b"v0".to_vec())),
-                false,
-                5,
-                5,
-                None,
-                true,
-            ),
-            (
-                Mutation::Put((key.clone(), b"v1".to_vec())),
-                false,
-                6,
-                6,
-                Some(new_old_value(Some(b"v0".to_vec()), 5.into())),
-                true,
-            ),
-            (Mutation::Lock(key.clone()), false, 7, 7, None, false),
-            (
-                Mutation::Lock(key.clone()),
-                false,
-                8,
-                8,
-                Some(new_old_value(Some(b"v1".to_vec()), 6.into())),
-                false,
-            ),
-            (
-                Mutation::Put((key.clone(), vec![b'0'; 5120])),
-                false,
-                9,
-                9,
-                Some(new_old_value(Some(b"v1".to_vec()), 6.into())),
-                true,
-            ),
-            (
-                Mutation::Put((key.clone(), b"v3".to_vec())),
-                false,
-                10,
-                10,
-                Some(new_old_value(None, 9.into())),
-                true,
-            ),
-            (
-                Mutation::Put((key.clone(), b"v4".to_vec())),
-                true,
-                11,
-                11,
-                None,
-                true,
-            ),
-        ];
+            // prev_write is Rollback, and there exists a more previous valid write
+            Case {
+                expected: Some(OldValue {
+                    short_value: None,
+                    start_ts: TimeStamp::new(4),
+                }),
 
-        let write = |modifies| {
-            engine.write(&ctx, modifies).unwrap();
-        };
-
-        let new_txn = |start_ts, cm| {
-            let snapshot = engine.snapshot(Default::default()).unwrap();
-            MvccTxn::new(snapshot, start_ts, true, cm)
-        };
-
-        for case in cases {
-            let (mutation, is_pessimistic, start_ts, commit_ts, old_value, check_old_value) = case;
-            let mutation_type = mutation.mutation_type();
-            let cm = ConcurrencyManager::new(start_ts.into());
-            let mut txn = new_txn(start_ts.into(), cm.clone());
-
-            txn.extra_op = ExtraOp::ReadOldValue;
-            if is_pessimistic {
-                acquire_pessimistic_lock(
-                    &mut txn,
-                    key.clone(),
-                    b"key",
-                    false,
-                    0,
-                    start_ts.into(),
-                    false,
-                    TimeStamp::zero(),
-                )
-                .unwrap();
-                write(WriteData::from_modifies(txn.into_modifies()));
-                txn = new_txn(start_ts.into(), cm.clone());
-                txn.extra_op = ExtraOp::ReadOldValue;
-                prewrite(
-                    &mut txn,
-                    &txn_props(
-                        start_ts.into(),
-                        b"key",
-                        CommitKind::TwoPc,
-                        Some(TimeStamp::default()),
-                        0,
-                        false,
+                written: vec![
+                    (
+                        Write::new(WriteType::Put, TimeStamp::new(4), None),
+                        TimeStamp::new(6),
                     ),
-                    mutation,
-                    &None,
-                    true,
-                )
-                .unwrap();
-            } else {
-                prewrite(
-                    &mut txn,
-                    &txn_props(start_ts.into(), b"key", CommitKind::TwoPc, None, 0, false),
-                    mutation,
-                    &None,
-                    false,
-                )
-                .unwrap();
-            }
-            if check_old_value {
-                let extra = txn.take_extra();
-                let ts_key = key.clone().append_ts(start_ts.into());
-                assert!(
-                    extra.old_values.get(&ts_key).is_some(),
-                    "{}@{}",
-                    ts_key,
-                    start_ts
+                    (
+                        Write::new(WriteType::Rollback, TimeStamp::new(5), None),
+                        TimeStamp::new(7),
+                    ),
+                ],
+            },
+            // prev_write is Rollback, and there isn't a more previous valid write
+            Case {
+                expected: None,
+
+                written: vec![(
+                    Write::new(WriteType::Rollback, TimeStamp::new(5), None),
+                    TimeStamp::new(6),
+                )],
+            },
+            // prev_write is Lock, and there exists a more previous valid write
+            Case {
+                expected: Some(OldValue {
+                    short_value: None,
+                    start_ts: TimeStamp::new(3),
+                }),
+
+                written: vec![
+                    (
+                        Write::new(WriteType::Put, TimeStamp::new(3), None),
+                        TimeStamp::new(6),
+                    ),
+                    (
+                        Write::new(WriteType::Lock, TimeStamp::new(5), None),
+                        TimeStamp::new(7),
+                    ),
+                ],
+            },
+            // prev_write is Lock, and there isn't a more previous valid write
+            Case {
+                expected: None,
+
+                written: vec![(
+                    Write::new(WriteType::Lock, TimeStamp::new(5), None),
+                    TimeStamp::new(6),
+                )],
+            },
+            // prev_write is not Rollback or Lock, check_gc_fence_as_latest_version is true
+            Case {
+                expected: Some(OldValue {
+                    short_value: None,
+                    start_ts: TimeStamp::new(7),
+                }),
+                written: vec![(
+                    Write::new(WriteType::Put, TimeStamp::new(7), None)
+                        .set_overlapped_rollback(true, Some(27.into())),
+                    TimeStamp::new(5),
+                )],
+            },
+            // prev_write is not Rollback or Lock, check_gc_fence_as_latest_version is false
+            Case {
+                expected: None,
+                written: vec![(
+                    Write::new(WriteType::Put, TimeStamp::new(4), None)
+                        .set_overlapped_rollback(true, Some(3.into())),
+                    TimeStamp::new(5),
+                )],
+            },
+        ];
+        for case in cases {
+            let engine = TestEngineBuilder::new().build().unwrap();
+            let cm = ConcurrencyManager::new(42.into());
+            let snapshot = engine.snapshot(Default::default()).unwrap();
+            let mut txn = MvccTxn::new(snapshot, TimeStamp::new(10), true, cm.clone());
+            for (write_record, put_ts) in case.written.iter() {
+                txn.put_write(
+                    Key::from_raw(b"a"),
+                    *put_ts,
+                    write_record.as_ref().to_bytes(),
                 );
-                assert_eq!(extra.old_values[&ts_key], (old_value, mutation_type));
             }
-            write(WriteData::from_modifies(txn.into_modifies()));
-            let mut txn = new_txn(start_ts.into(), cm);
-            commit(&mut txn, key.clone(), commit_ts.into()).unwrap();
-            engine
-                .write(&ctx, WriteData::from_modifies(txn.into_modifies()))
-                .unwrap();
+            write(&engine, &Context::default(), txn.into_modifies());
+            let snapshot = engine.snapshot(Default::default()).unwrap();
+            let mut txn = MvccTxn::new(snapshot, TimeStamp::new(25), true, cm);
+            let prev_write = txn
+                .reader
+                .seek_write(&Key::from_raw(b"a"), case.written.last().unwrap().1)
+                .unwrap()
+                .unwrap()
+                .1;
+            let result = get_old_value(&mut txn, &Key::from_raw(b"a"), prev_write).unwrap();
+            assert_eq!(result, case.expected);
         }
     }
 }
