946d36de88d3b34eaf89cd795e1b3b4e2e1f2f2b
tabokie <xy.tao@outlook.com>
2021-01-07 17:40:23 +0800

    fix
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- components/engine_rocks/src/file_system.rs
+++ components/engine_rocks/src/file_system.rs
@@ -52,7 +52,7 @@ mod tests {
 
     fn new_test_db(dir: &str) -> (Arc<DB>, Arc<BytesRecorder>) {
         let recorder = Arc::new(BytesRecorder::new());
-        set_io_rate_limiter(IORateLimiter::new(10000, Some(recorder.clone())));
+        set_io_rate_limiter(Some(Arc::new(IORateLimiter::new(Some(recorder.clone())))));
         let mut db_opts = DBOptions::new();
         db_opts.add_event_listener(RocksEventListener::new("test_db"));
         let env = get_env(Some(Arc::new(EngineFileSystemInspector::new())), None).unwrap();
