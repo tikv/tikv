f2187114c32338f6b284cdcb2107b6728c4302ad
Brian Anderson <andersrb@gmail.com>
2021-01-15 00:47:52 -0600

    Make PerfContext abstract and use it in raftstore (#9243)
    <!--
    Thank you for contributing to TiKV!
    
    If you haven't already, please read TiKV's [CONTRIBUTING](https://github.com/tikv/tikv/blob/master/CONTRIBUTING.md) document.
    
    If you're unsure about anything, just ask; somebody should be along to answer within a day or two.
    
    PR Title Format:
    1. module [, module2, module3]: what's changed
    2. *: what's changed
    
    If you want to open the **Challenge Program** pull request, please use the following template:
    https://raw.githubusercontent.com/tikv/.github/master/.github/PULL_REQUEST_TEMPLATE/challenge-program.md
    You can use it with query parameters: https://github.com/tikv/tikv/compare/master...${you branch}?template=challenge-program.md
    -->
    
    ### What problem does this PR solve?
    
    Problem Summary:
    
    PerfContext is the last concrete rocks type used in raftstore. In order to remove engine_rocks as a dependency of raftstore, it must be abstracted in some way.
    
    This PR makes PerfContext another engine trait. Unlike in rocks, where it is accessed as a global, this patch follows the existing engine_traits pattern, and access PerfContext through accessors on an engine instance. This seems more reasonable as a general-purpose abstraction.
    
    On the other hand, this doesn't make any attempt to make the PerfContext measurements reasonable for any non-rocks engine. Instead, it makes the PerfContext accessor, now  called `get_perf_context` return an Option. For now, other engines are just expected to return None.
    
    This patch then entirely removes engine_rocks::PerfContext and PerfLevel from raftstore. There are a few usages in the tikv crate that I have not touched yet. The next step is to remove engine_rocks as a dependency from raftstore.
    
    ### What is changed and how it works?
    
    
    ### Related changes
    
    
    https://github.com/tikv/tikv/issues/6402
    
    ### Check List <!--REMOVE the items that are not applicable-->
    
    - Unit test
    
    
    ### Release note <!-- bugfixes or new feature need a release note -->
    
    - No release note.

--- tests/integrations/config/mod.rs
+++ tests/integrations/config/mod.rs
@@ -9,10 +9,11 @@ use slog::Level;
 use batch_system::Config as BatchSystemConfig;
 use collections::HashSet;
 use encryption::{EncryptionConfig, FileConfig, MasterKeyConfig};
-use engine_rocks::config::{BlobRunMode, CompressionType, LogLevel, PerfLevel};
+use engine_rocks::config::{BlobRunMode, CompressionType, LogLevel};
 use engine_rocks::raw::{
     CompactionPriority, DBCompactionStyle, DBCompressionType, DBRateLimiterMode, DBRecoveryMode,
 };
+use engine_traits::config::PerfLevel;
 use kvproto::encryptionpb::EncryptionMethod;
 use pd_client::Config as PdConfig;
 use raftstore::coprocessor::{Config as CopConfig, ConsistencyCheckMethod};
