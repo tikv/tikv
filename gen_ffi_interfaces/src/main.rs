811b97355f756a06b03498c9515a4c4dab5dc06f
Tong Zhigao <tongzhigao@pingcap.com>
2021-01-28 16:29:17 +0800

    Refactor `gen_ffi_interfaces`
    Signed-off-by: Tong Zhigao <tongzhigao@pingcap.com>

--- gen_ffi_interfaces/src/main.rs
+++ gen_ffi_interfaces/src/main.rs
@@ -1,15 +1,16 @@
 extern crate bindgen;
 extern crate walkdir;
 
-use bindgen::{Builder, EnumVariation};
+use bindgen::EnumVariation;
+use clap::{App, Arg};
+use std::fs;
 use std::io::Read;
 use std::path::Path;
-use std::{env, fs};
 use walkdir::WalkDir;
 
-fn add_ffi_src_head(dir: String, mut config: Builder) -> Builder {
+fn scan_ffi_src_head(dir: &str) -> Vec<String> {
     let mut headers = Vec::new();
-    for result in WalkDir::new(Path::new(&dir)) {
+    for result in WalkDir::new(Path::new(dir)) {
         let dent = result.expect("Error happened when search headers");
         if !dent.file_type().is_file() {
             continue;
@@ -21,13 +22,33 @@ fn add_ffi_src_head(dir: String, mut config: Builder) -> Builder {
         headers.push(String::from(dent.path().to_str().unwrap()));
     }
     headers.sort();
-    for path in headers {
-        config = config.header(path);
-    }
-    config
+    headers
 }
 
 fn gen_ffi_code() {
+    let matches = App::new("RaftStore Proxy FFI Generator")
+        .author("tongzhigao@pingcap.com")
+        .arg(
+            Arg::with_name("ffi_src_dir")
+                .long("ffi_src_dir")
+                .help("Sets the path of FFI source code directory")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("ffi_tar_file")
+                .long("ffi_tar_file")
+                .help("Sets the target file path of generated FFI rust code")
+                .default_value("components/raftstore/src/tiflash_ffi/interfaces.rs")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let src_dir = matches.value_of("ffi_src_dir").unwrap();
+    let tar_file = matches.value_of("ffi_tar_file").unwrap();
+
+    println!("\nFFI src dir path is {}", src_dir);
+
     let mut builder = bindgen::Builder::default()
         .clang_arg("-std=c++17")
         .clang_arg("-x")
@@ -39,15 +60,22 @@ fn gen_ffi_code() {
             non_exhaustive: false,
         });
 
-    let dir = env::var("RAFT_STORE_PROXY_FFI_SRC_DIR").unwrap();
+    let headers = scan_ffi_src_head(src_dir);
 
-    builder = add_ffi_src_head(dir, builder);
+    {
+        println!("Scan and get src files");
+        for f in &headers {
+            println!("    {}", f);
+        }
+    }
+
+    for path in headers {
+        builder = builder.header(path);
+    }
+
+    println!("Start generate rust code into {}\n", tar_file);
 
     let bindings = builder.generate().unwrap();
-
-    let tar_file = env::var("RAFT_STORE_PROXY_FFI_TAR_FILE").unwrap_or(String::from(
-        "components/raftstore/src/tiflash_ffi/interfaces.rs",
-    ));
 
     bindings.write_to_file(tar_file).unwrap();
 }
