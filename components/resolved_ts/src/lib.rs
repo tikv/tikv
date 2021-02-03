00374abdf9df26740012a50a8018d377ea74a805
5kbpers <tangminghua@pingcap.com>
2021-02-03 17:05:52 +0800

    resolved_ts: add resolved ts advancer and event observer
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/resolved_ts/src/lib.rs
+++ components/resolved_ts/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
+#![feature(box_patterns)]
 
-#[macro_use(debug)]
+#[macro_use]
 extern crate tikv_util;
 
 mod resolver;
@@ -8,3 +9,13 @@ pub use resolver::*;
 
 mod cmd;
 pub use cmd::*;
+mod observer;
+pub use observer::*;
+mod advance;
+pub use advance::*;
+mod sinker;
+pub use sinker::*;
+mod endpoint;
+pub use endpoint::*;
+mod errors;
+pub use errors::*;
