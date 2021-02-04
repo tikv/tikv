f922316c1bd17ea987cbcd18c96e3cafc4478089
Brian Anderson <andersrb@gmail.com>
2021-02-04 23:20:06 +0000

    engine_traits_tests: Update docs
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_traits_tests/src/lib.rs
+++ components/engine_traits_tests/src/lib.rs
@@ -8,38 +8,55 @@
 //! All engine instances are constructed through the `engine_test` crate,
 //! so individual engines can be tested by setting that crate's feature flags.
 //!
-//! e.g. to test the `engine_sled` crate
+//! e.g. to test the `engine_panic` crate
 //!
 //! ```no_test
-//! cargo test -p engine_traits_tests --no-default-features --features=protobuf-codec,test-engines-sled
+//! cargo test -p engine_traits_tests --no-default-features --features=protobuf-codec,test-engines-panic
 //! ```
+//!
+//! As of now this mostly tests the essential features of
+//!
+//! - point get / delete
+//! - range deletes
+//! - snapshots
+//! - write batches
+//! - iterators
+//!
+//! It is intended though to cover essentially all features of storage engines.
+//!
+//! It is poor at testing data consistency and error recovery,
+//! as such tests require hooks into the engine which are not
+//! presented by the engine traits.
+//!
+//! Note that in some cases there are multiple ways to accomplish the same
+//! thing. In particular, engines have methods that operate on the default
+//! column family, and these are effectively the same as using the `_cf` methods
+//! with `CF_DEFAULT`; and writing can be performed directly through the engine,
+//! or through write batches. As such, some tests are parameterized over
+//! multiple APIs. This is particularly true of the `scenario_writes` module,
+//! which contains much of the basic writing tests, and the `iterator` module.
 
 #![cfg(test)]
 
-mod ctor;
 mod basic_read_write;
-mod scenario_writes;
-mod delete_range;
 mod cf_names;
+mod ctor;
+mod delete_range;
 mod iterator;
-mod snapshot_basic;
-mod read_consistency;
-mod write_batch;
 mod misc;
+mod read_consistency;
+mod scenario_writes;
+mod snapshot_basic;
+mod write_batch;
 
-fn tempdir() -> tempfile::TempDir {
-    tempfile::Builder::new()
-        .prefix("tikv-engine-traits-tests")
-        .tempdir()
-        .unwrap()
-}
-
+/// The engine / tempdir pair used in all tests
 struct TempDirEnginePair {
     // NB engine must drop before tempdir
     engine: engine_test::kv::KvTestEngine,
     tempdir: tempfile::TempDir,
 }
 
+/// Create an engine with only CF_DEFAULT
 fn default_engine() -> TempDirEnginePair {
     use engine_traits::CF_DEFAULT;
     use engine_test::kv::KvTestEngine;
@@ -53,6 +70,7 @@ fn default_engine() -> TempDirEnginePair {
     }
 }
 
+/// Create an engine with the specified column families
 fn engine_cfs(cfs: &[&str]) -> TempDirEnginePair {
     use engine_test::kv::KvTestEngine;
     use engine_test::ctor::EngineConstructorExt;
@@ -63,6 +81,13 @@ fn engine_cfs(cfs: &[&str]) -> TempDirEnginePair {
     TempDirEnginePair {
         engine, tempdir: dir,
     }
+}
+
+fn tempdir() -> tempfile::TempDir {
+    tempfile::Builder::new()
+        .prefix("tikv-engine-traits-tests")
+        .tempdir()
+        .unwrap()
 }
 
 fn assert_engine_error<T>(r: engine_traits::Result<T>) {
