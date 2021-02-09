4994a9476792569cba773d994dc9e5822862d05b
wangggong <793160615@qq.com>
2021-02-09 16:03:41 +0800

    feat: add instr
    Signed-off-by: wangggong <793160615@qq.com>

--- components/tidb_query_expr/src/lib.rs
+++ components/tidb_query_expr/src/lib.rs
@@ -632,6 +632,7 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::Space => space_fn_meta(),
         ScalarFuncSig::SubstringIndex => substring_index_fn_meta(),
         ScalarFuncSig::Strcmp => map_strcmp_sig(ft)?,
+        ScalarFuncSig::Instr => instr_fn_meta(),
         ScalarFuncSig::InstrUtf8 => instr_utf8_fn_meta(),
         ScalarFuncSig::OctInt => oct_int_fn_meta(),
         ScalarFuncSig::OctString => oct_string_fn_meta(),
