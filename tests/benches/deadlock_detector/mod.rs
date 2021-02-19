cd29b3af48734347d171a1713fca239271477f0b
Yang Keao <keao.yang@yahoo.com>
2021-02-19 21:39:06 +0800

    remove one range from bench_dense_detect_without_cleanup
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- tests/benches/deadlock_detector/mod.rs
+++ tests/benches/deadlock_detector/mod.rs
@@ -76,7 +76,6 @@ fn bench_dense_detect_without_cleanup(c: &mut Criterion) {
         100,
         1_000,
         10_000,
-        10_000,
         100_000,
         1_000_000,
         10_000_000,
