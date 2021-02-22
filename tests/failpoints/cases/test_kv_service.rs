048fc2dee0aec0431583ecf5df4952b28c7a6e58
Yilin Chen <sticnarf@gmail.com>
2021-02-22 16:06:29 +0800

    add more failpoint tests
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- tests/failpoints/cases/test_kv_service.rs
+++ tests/failpoints/cases/test_kv_service.rs
@@ -129,7 +129,7 @@ fn test_scan_lock_push_async_commit() {
     }
 }
 
-fn test_memory_lock_backoff(f: impl FnOnce(TikvClient, Context) + Send + 'static) {
+fn test_memory_lock_backoff_succeed(f: impl FnOnce(TikvClient, Context) + Send + 'static) {
     let (_cluster, client, ctx) = must_new_cluster_and_kv_client();
 
     let k1 = b"k1";
@@ -137,8 +137,7 @@ fn test_memory_lock_backoff(f: impl FnOnce(TikvClient, Context) + Send + 'static
     let v = b"v";
     let ts = 10;
 
-    fail::cfg("before-set-lock-in-memory", "pause").unwrap();
-    fail::cfg("scheduler_async_write_finish", "sleep(50)").unwrap();
+    fail::cfg("scheduler_async_write_finish", "pause").unwrap();
     let client1 = client.clone();
     let ctx1 = ctx.clone();
     let handle1 = std::thread::spawn(move || {
@@ -156,28 +155,148 @@ fn test_memory_lock_backoff(f: impl FnOnce(TikvClient, Context) + Send + 'static
         prewrite.set_primary_lock(k1.to_vec());
         prewrite.set_start_version(ts);
         prewrite.set_lock_ttl(1000);
+        prewrite.set_use_async_commit(true);
         prewrite.set_try_one_pc(true);
 
         let resp = client1.kv_prewrite(&prewrite).unwrap();
         assert!(!resp.has_region_error());
         assert_eq!(resp.get_errors(), &[]);
-        fail::remove("scheduler_async_write_finish");
     });
 
     // Wait until the prewrite acquires the mem lock
     thread::sleep(Duration::from_millis(200));
 
     let handle2 = thread::spawn(move || f(client, ctx));
-    fail::remove("before-set-lock-in-memory");
+    fail::cfg("scheduler_async_write_finish", "sleep(20)").unwrap();
 
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+
+    fail::remove("scheduler_async_write_finish");
+}
+
+fn test_memory_lock_backoff_succeed_partial(f: impl FnOnce(TikvClient, Context) + Send + 'static) {
+    let (_cluster, client, ctx) = must_new_cluster_and_kv_client();
+
+    let k1 = b"k1";
+    let k2 = b"k2";
+    let v = b"v";
+
+    fail::cfg("scheduler_async_write_finish", "pause").unwrap();
+    let client1 = client.clone();
+    let ctx1 = ctx.clone();
+    let handle1 = std::thread::spawn(move || {
+        let mut prewrite = PrewriteRequest::default();
+        prewrite.set_context(ctx1);
+        let mut m1 = Mutation::default();
+        m1.set_op(Op::Put);
+        m1.set_key(k1.to_vec());
+        m1.set_value(v.to_vec());
+        prewrite.set_mutations(vec![m1].into());
+        prewrite.set_primary_lock(k1.to_vec());
+        prewrite.set_start_version(10);
+        prewrite.set_lock_ttl(1000);
+        prewrite.set_use_async_commit(true);
+        prewrite.set_try_one_pc(true);
+        let resp = client1.kv_prewrite(&prewrite).unwrap();
+        assert!(!resp.has_region_error());
+        assert_eq!(resp.get_errors(), &[]);
+    });
+
+    let client2 = client.clone();
+    let ctx2 = ctx.clone();
+    let handle2 = std::thread::spawn(move || {
+        let mut prewrite = PrewriteRequest::default();
+        prewrite.set_context(ctx2);
+        let mut m2 = Mutation::default();
+        m2.set_op(Op::Put);
+        m2.set_key(k2.to_vec());
+        m2.set_value(v.to_vec());
+        prewrite.set_mutations(vec![m2].into());
+        prewrite.set_primary_lock(k2.to_vec());
+        prewrite.set_start_version(11);
+        prewrite.set_lock_ttl(1000);
+        prewrite.set_use_async_commit(true);
+        prewrite.set_try_one_pc(true);
+        let resp = client2.kv_prewrite(&prewrite).unwrap();
+        assert!(!resp.has_region_error());
+        assert_eq!(resp.get_errors(), &[]);
+    });
+
+    // Wait until the prewrites acquire the mem lock
+    thread::sleep(Duration::from_millis(200));
+
+    let handle3 = thread::spawn(move || f(client, ctx));
+    fail::cfg("scheduler_async_write_finish", "sleep(20)").unwrap();
+    fail::cfg("scheduler_async_write_finish", "pause").unwrap();
+
+    handle3.join().unwrap();
+
+    fail::remove("scheduler_async_write_finish");
     handle1.join().unwrap();
     handle2.join().unwrap();
 }
 
 #[test]
-fn test_get_memory_lock_backoff() {
-    test_memory_lock_backoff(|client, ctx| {
-        let mut get = GetRequest::default();
-        get.set_context(ctx);
+fn test_kv_get_memory_lock_backoff() {
+    test_memory_lock_backoff_succeed(|client, ctx| {
+        let mut req = GetRequest::default();
+        req.set_context(ctx);
+        req.set_key(b"k2".to_vec());
+        req.set_version(u64::MAX);
+        let resp = client.kv_get(&req).unwrap();
+        assert_eq!(resp.get_value(), b"v");
+    });
+}
+
+#[test]
+fn test_kv_batch_get_memory_lock_backoff() {
+    // test_memory_lock_backoff_succeed(|client, ctx| {
+    //     let mut req = BatchGetRequest::default();
+    //     req.set_context(ctx);
+    //     req.set_keys(vec![b"k1".to_vec(), b"k2".to_vec()].into());
+    //     req.set_version(u64::MAX);
+    //     let resp = client.kv_batch_get(&req).unwrap();
+    //     assert_eq!(resp.get_pairs()[0].get_key(), b"k1");
+    //     assert_eq!(resp.get_pairs()[0].get_value(), b"v");
+    //     assert_eq!(resp.get_pairs()[1].get_key(), b"k2");
+    //     assert_eq!(resp.get_pairs()[1].get_value(), b"v");
+    // });
+
+    test_memory_lock_backoff_succeed_partial(|client, ctx| {
+        let mut req = BatchGetRequest::default();
+        req.set_context(ctx);
+        req.set_keys(vec![b"k1".to_vec(), b"k2".to_vec()].into());
+        req.set_version(u64::MAX);
+        let resp = client.kv_batch_get(&req).unwrap();
+        assert!(resp.has_error());
+    });
+}
+
+#[test]
+fn test_kv_scan_memory_lock_backoff() {
+    test_memory_lock_backoff_succeed(|client, ctx| {
+        let mut req = ScanRequest::default();
+        req.set_context(ctx);
+        req.set_start_key(b"k1".to_vec());
+        req.set_end_key(b"z".to_vec());
+        req.set_version(u64::MAX);
+        req.set_limit(2);
+        let resp = client.kv_scan(&req).unwrap();
+        assert_eq!(resp.get_pairs()[0].get_key(), b"k1");
+        assert_eq!(resp.get_pairs()[0].get_value(), b"v");
+        assert_eq!(resp.get_pairs()[1].get_key(), b"k2");
+        assert_eq!(resp.get_pairs()[1].get_value(), b"v");
+    });
+
+    test_memory_lock_backoff_succeed_partial(|client, ctx| {
+        let mut req = ScanRequest::default();
+        req.set_context(ctx);
+        req.set_start_key(b"k1".to_vec());
+        req.set_end_key(b"z".to_vec());
+        req.set_version(u64::MAX);
+        req.set_limit(2);
+        let resp = client.kv_scan(&req).unwrap();
+        assert!(resp.has_error());
     });
 }
