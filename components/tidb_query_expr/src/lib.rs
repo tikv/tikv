993ec6b818e20404cad2234c6314209a19f1074a
wangggong <793160615@qq.com>
2021-03-01 12:42:53 +0800

    copr: add instr (#9646)
    Signed-off-by: wangggong <793160615@qq.com>
    
    <!--
    Thank you for contributing to TiKV!
    
    If you haven't already, please read TiKV's [CONTRIBUTING](https://github.com/tikv/tikv/blob/master/CONTRIBUTING.md) document.
    
    If you're unsure about anything, just ask; somebody should be along to answer within a day or two.
    
    PR Title Format:
    1. module [, module2, module3]: what's changed
    2. *: what's changed
    
    If you want to open the **Challenge Program** pull request, please use the following template:
    https://raw.githubusercontent.com/tikv/.github/master/.github/PULL_REQUEST_TEMPLATE/challenge-program.md
    You can use it with query parameters: https://github.com/tikv/tikv/compare/master...${you branch}?template=challenge-program.md
    -->
    
    ### What problem does this PR solve?
    
    Issue Number: #9016 <!-- REMOVE this line if no issue to close -->
    
    Problem Summary:
    
    ### What is changed and how it works?
    
    What's Changed:
    
    ### Related changes
    
    - PR to update `pingcap/docs`/`pingcap/docs-cn`:
    - PR to update `pingcap/tidb-ansible`:
    - Need to cherry-pick to the release branch
    
    ### Check List <!--REMOVE the items that are not applicable-->
    
    Tests <!-- At least one of them must be included. -->
    
    - Unit test
    
    Side effects
    
    ### Release note <!-- bugfixes or new feature need a release note -->
    
    No release note.

--- components/tidb_query_expr/src/lib.rs
+++ components/tidb_query_expr/src/lib.rs
@@ -648,6 +648,7 @@ fn map_expr_node_to_rpn_func(expr: &Expr) -> Result<RpnFnMeta> {
         ScalarFuncSig::Space => space_fn_meta(),
         ScalarFuncSig::SubstringIndex => substring_index_fn_meta(),
         ScalarFuncSig::Strcmp => map_strcmp_sig(ft)?,
+        ScalarFuncSig::Instr => instr_fn_meta(),
         ScalarFuncSig::InstrUtf8 => instr_utf8_fn_meta(),
         ScalarFuncSig::Quote => quote_fn_meta(),
         ScalarFuncSig::OctInt => oct_int_fn_meta(),
