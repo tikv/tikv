61833845563c9f595d2bacabbca03f4a3d6498cb
Zijie Lu <wslzj40@gmail.com>
2021-03-04 10:23:23 +0800

    fix test
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- components/test_raftstore/src/util.rs
+++ components/test_raftstore/src/util.rs
@@ -680,7 +680,7 @@ pub fn configure_for_lease_read<T: Simulator>(
     let election_timeout = base_tick_interval * election_ticks;
     // Adjust max leader lease.
     cluster.cfg.raft_store.raft_store_max_leader_lease =
-        ReadableDuration(election_timeout - Duration::from_millis(base_tick_ms.unwrap()));
+        ReadableDuration(election_timeout - Duration::from_millis(base_tick_interval));
     // Use large peer check interval, abnormal and max leader missing duration to make a valid config,
     // that is election timeout x 2 < peer stale state check < abnormal < max leader missing duration.
     cluster.cfg.raft_store.peer_stale_state_check_interval = ReadableDuration(election_timeout * 3);
