53f71bcd2d7e06858669aeb5b7d909f529b4f7e2
Andreas Zimmerer <andreas.zimmerer@tum.de>
2021-03-05 13:19:32 +0100

    Got rid of allocations for rpad and lpad functions
    Signed-off-by: Andreas Zimmerer <andreas.zimmerer@tum.de>

--- components/tidb_query_expr/src/impl_string.rs
+++ components/tidb_query_expr/src/impl_string.rs
@@ -23,6 +23,17 @@ const BASE64_INPUT_CHUNK_LENGTH: usize = 3;
 const BASE64_ENCODED_CHUNK_LENGTH: usize = 4;
 const BASE64_LINE_WRAP: u8 = b'\n';
 
+/// Returns the byte index of the char at `char_idx` in `s`.
+/// If `char_idx` is larger then the number of UTF-8 chars in `s`,
+/// the length of `s` in bytes is returned.
+#[inline]
+fn get_utf8_byte_index(s: &str, char_idx: usize) -> usize {
+    s.char_indices()
+        .map(|(i, _)| i)
+        .nth(char_idx)
+        .unwrap_or_else(|| s.len())
+}
+
 #[rpn_fn(writer)]
 #[inline]
 pub fn bin(num: &Int, writer: BytesWriter) -> Result<BytesGuard> {
@@ -241,18 +252,23 @@ pub fn lpad(arg: BytesRef, len: &Int, pad: BytesRef, writer: BytesWriter) -> Res
     match validate_target_len_for_pad(*len < 0, *len, arg.len(), 1, pad.is_empty()) {
         None => Ok(writer.write(None)),
         Some(0) => Ok(writer.write_ref(Some(b""))),
+        Some(target_len) if target_len < arg.len() => {
+            Ok(writer.write_ref(Some(&arg[..target_len])))
+        }
         Some(target_len) => {
-            let r = if let Some(remain) = target_len.checked_sub(arg.len()) {
-                pad.iter()
-                    .cycle()
-                    .take(remain)
-                    .chain(arg.iter())
-                    .copied()
-                    .collect::<Bytes>()
-            } else {
-                arg[..target_len].to_vec()
-            };
-            Ok(writer.write(Some(r)))
+            let mut writer = writer.begin();
+            // Write full pads
+            let num_pads = (target_len - arg.len()) / pad.len();
+            for _ in 0..num_pads {
+                writer.partial_write(&pad);
+            }
+
+            // Write last incomplete pad (might be none)
+            let last_pad_len = (target_len - arg.len()) % pad.len();
+            writer.partial_write(&pad[..last_pad_len]);
+
+            writer.partial_write(&arg);
+            Ok(writer.finish())
         }
     }
 }
@@ -268,20 +284,30 @@ pub fn lpad_utf8(
     let input = str::from_utf8(&*arg)?;
     let pad = str::from_utf8(&*pad)?;
     let input_len = input.chars().count();
+    let pad_len = pad.chars().count();
+
     match validate_target_len_for_pad(*len < 0, *len, input_len, 4, pad.is_empty()) {
         None => Ok(writer.write(None)),
         Some(0) => Ok(writer.write_ref(Some(b""))),
+        Some(target_len) if target_len < input_len => {
+            let utf8_byte_end = get_utf8_byte_index(input, target_len);
+            Ok(writer.write_ref(Some(input[..utf8_byte_end].as_bytes())))
+        }
         Some(target_len) => {
-            let r = if let Some(remain) = target_len.checked_sub(input_len) {
-                pad.chars()
-                    .cycle()
-                    .take(remain)
-                    .chain(input.chars())
-                    .collect::<String>()
-            } else {
-                input.chars().take(target_len).collect::<String>()
-            };
-            Ok(writer.write(Some(r.into_bytes())))
+            let mut writer = writer.begin();
+            // Write full pads
+            let num_pads = (target_len - input_len) / pad_len;
+            for _ in 0..num_pads {
+                writer.partial_write(pad.as_bytes());
+            }
+
+            // Write last incomplete pad (might be none)
+            let last_pad_len = (target_len - input_len) % pad_len;
+            let utf8_byte_end = get_utf8_byte_index(pad, last_pad_len);
+            writer.partial_write(pad[..utf8_byte_end].as_bytes());
+
+            writer.partial_write(input.as_bytes());
+            Ok(writer.finish())
         }
     }
 }
@@ -292,14 +318,23 @@ pub fn rpad(arg: BytesRef, len: &Int, pad: BytesRef, writer: BytesWriter) -> Res
     match validate_target_len_for_pad(*len < 0, *len, arg.len(), 1, pad.is_empty()) {
         None => Ok(writer.write(None)),
         Some(0) => Ok(writer.write_ref(Some(b""))),
+        Some(target_len) if target_len < arg.len() => {
+            Ok(writer.write_ref(Some(&arg[..target_len])))
+        }
         Some(target_len) => {
-            let r = arg
-                .iter()
-                .chain(pad.iter().cycle())
-                .copied()
-                .take(target_len)
-                .collect::<Bytes>();
-            Ok(writer.write(Some(r)))
+            let mut writer = writer.begin();
+            writer.partial_write(&arg);
+
+            // Write full pads
+            let num_pads = (target_len - arg.len()) / pad.len();
+            for _ in 0..num_pads {
+                writer.partial_write(&pad);
+            }
+
+            // Write last incomplete pad (might be none)
+            let last_pad_len = (target_len - arg.len()) % pad.len();
+            writer.partial_write(&pad[..last_pad_len]);
+            Ok(writer.finish())
         }
     }
 }
@@ -315,17 +350,30 @@ pub fn rpad_utf8(
     let input = str::from_utf8(&*arg)?;
     let pad = str::from_utf8(&*pad)?;
     let input_len = input.chars().count();
+    let pad_len = pad.chars().count();
 
     match validate_target_len_for_pad(*len < 0, *len, input_len, 4, pad.is_empty()) {
         None => Ok(writer.write(None)),
         Some(0) => Ok(writer.write_ref(Some(b""))),
+        Some(target_len) if target_len < input_len => {
+            let utf8_byte_end = get_utf8_byte_index(input, target_len);
+            Ok(writer.write_ref(Some(input[..utf8_byte_end].as_bytes())))
+        }
         Some(target_len) => {
-            let r = input
-                .chars()
-                .chain(pad.chars().cycle())
-                .take(target_len)
-                .collect::<String>();
-            Ok(writer.write(Some(r.into_bytes())))
+            let mut writer = writer.begin();
+            writer.partial_write(input.as_bytes());
+
+            // Write full pads
+            let num_pads = (target_len - input_len) / pad_len;
+            for _ in 0..num_pads {
+                writer.partial_write(pad.as_bytes());
+            }
+
+            // Write last incomplete pad (might be none)
+            let last_pad_len = (target_len - input_len) % pad_len;
+            let utf8_byte_end = get_utf8_byte_index(pad, last_pad_len);
+            writer.partial_write(pad[..utf8_byte_end].as_bytes());
+            Ok(writer.finish())
         }
     }
 }
@@ -403,11 +451,7 @@ pub fn left_utf8(lhs: BytesRef, rhs: &Int, writer: BytesWriter) -> Result<BytesG
     let rhs = *rhs as usize;
     let len = s.chars().count();
     let result = if len > rhs {
-        let idx = s
-            .char_indices()
-            .nth(rhs)
-            .map(|(idx, _)| idx)
-            .unwrap_or_else(|| s.len());
+        let idx = get_utf8_byte_index(s, rhs);
         s[..idx].as_bytes()
     } else {
         s.as_bytes()
@@ -470,11 +514,7 @@ pub fn right_utf8(lhs: BytesRef, rhs: &Int, writer: BytesWriter) -> Result<Bytes
     let rhs = *rhs as usize;
     let len = s.chars().count();
     let result = if len > rhs {
-        let idx = s
-            .char_indices()
-            .nth(len - rhs)
-            .map(|(idx, _)| idx)
-            .unwrap_or_else(|| s.len());
+        let idx = get_utf8_byte_index(s, len - rhs);
         s[idx..].as_bytes()
     } else {
         s.as_bytes()
@@ -1006,6 +1046,16 @@ mod tests {
 
     use super::*;
     use crate::types::test_util::RpnFnScalarEvaluator;
+
+    #[test]
+    fn test_get_utf8_byte_index() {
+        let s = "你好世界";
+
+        assert_eq!(&s[..get_utf8_byte_index(s, 0)], "");
+        assert_eq!(&s[..get_utf8_byte_index(s, 2)], "你好");
+        assert_eq!(&s[..get_utf8_byte_index(s, 4)], "你好世界");
+        assert_eq!(&s[..get_utf8_byte_index(s, 9)], "你好世界");
+    }
 
     #[test]
     fn test_bin() {
