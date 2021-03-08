edc0bfc5b7860ee14fb72e9e1c7ec497196b7190
Connor <zbk602423539@gmail.com>
2021-03-08 21:22:02 +0800

    rename
    Signed-off-by: Connor <zbk602423539@gmail.com>

--- components/engine_panic/src/ttl_properties.rs
+++ components/engine_panic/src/ttl_properties.rs
@@ -1,15 +1,15 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
 use crate::engine::PanicEngine;
-use engine_traits::{Result, TTLProperties, TTLPropertiesExt};
+use engine_traits::{Result, TtlProperties, TtlPropertiesExt};
 
-impl TTLPropertiesExt for PanicEngine {
+impl TtlPropertiesExt for PanicEngine {
     fn get_range_ttl_properties_cf(
         &self,
         cf: &str,
         start_key: &[u8],
         end_key: &[u8],
-    ) -> Result<Vec<(String, TTLProperties)>> {
+    ) -> Result<Vec<(String, TtlProperties)>> {
         panic!()
     }
 }
