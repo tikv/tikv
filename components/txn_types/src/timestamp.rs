0d88521b7bcf4da001d806ade538de49ac9374dc
Zijie Lu <wslzj40@gmail.com>
2021-03-15 10:44:11 +0800

    Fix test
    Signed-off-by: Zijie Lu <wslzj40@gmail.com>

--- components/txn_types/src/timestamp.rs
+++ components/txn_types/src/timestamp.rs
@@ -33,24 +33,24 @@ impl TimeStamp {
     }
 
     pub fn next(self) -> TimeStamp {
-        assert!(self.0 + 1 > 0);
+        assert!(self.0 + 1 >= 0);
         TimeStamp(self.0 + 1)
     }
 
     pub fn prev(self) -> TimeStamp {
-        assert!(self.0 - 1 > 0);
+        assert!(self.0 - 1 >= 0);
         TimeStamp(self.0 - 1)
     }
 
     pub fn incr(&mut self) -> &mut TimeStamp {
         self.0 += 1;
-        assert!(self.0 > 0);
+        assert!(self.0 >= 0);
         self
     }
 
     pub fn decr(&mut self) -> &mut TimeStamp {
         self.0 -= 1;
-        assert!(self.0 > 0);
+        assert!(self.0 >= 0);
         self
     }
 
