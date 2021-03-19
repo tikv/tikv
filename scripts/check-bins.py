9240e5a967118a15bd22f2c57545d88f2ad698e2
Andreas Zimmerer <andreas.zimmerer@tum.de>
2021-03-19 22:35:13 +0100

    coprv2: whitelist coprocessor_plugin_api to not contain jemalloc
    Signed-off-by: Andreas Zimmerer <andreas.zimmerer@tum.de>

--- scripts/check-bins.py
+++ scripts/check-bins.py
@@ -13,7 +13,7 @@ import sys, json, os, time, re
 WHITE_LIST = {
     "configuration", "configuration_derive", "match_template", "tidb_query_codegen",
     "panic_hook", "fuzz", "fuzzer_afl", "fuzzer_honggfuzz", "fuzzer_libfuzzer",
-    "rusoto_util",
+    "rusoto_util", "coprocessor_plugin_api",
 }
 
 JEMALLOC_SYMBOL = ["je_arena_boot", " malloc"]
