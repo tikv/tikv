681b9a08d60cc84604d82c225742b46b71a89ccc
lhy1024 <admin@liudos.us>
2021-03-25 20:38:13 +0800

    address comment
    Signed-off-by: lhy1024 <admin@liudos.us>

--- components/raftstore/src/store/hibernate_state.rs
+++ components/raftstore/src/store/hibernate_state.rs
@@ -2,7 +2,7 @@
 
 use kvproto::metapb::Region;
 use pd_client::{Feature, FeatureGate};
-use serde_derive::*;
+use serde_derive::{Serialize, Deserialize};
 
 /// Because negotiation protocol can't be recognized by old version of binaries,
 /// so enabling it directly can cause a lot of connection reset.
