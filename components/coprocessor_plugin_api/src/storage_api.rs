fa3a706a9d308084ce39a1347c6ba474c8025897
Andreas Zimmerer <andreas.zimmerer@tum.de>
2021-03-27 17:59:39 +0100

    coprv2: error transformation from TiKV errors to plugin_api errors
    Signed-off-by: Andreas Zimmerer <andreas.zimmerer@tum.de>

--- components/coprocessor_plugin_api/src/storage_api.rs
+++ components/coprocessor_plugin_api/src/storage_api.rs
@@ -1,8 +1,10 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
 use async_trait::async_trait;
+use std::error::Error;
 use std::fmt;
 use std::ops::Range;
+use std::time::Duration;
 
 /// A raw key in the storage.
 pub type Key = Vec<u8>;
@@ -38,12 +40,31 @@ pub enum StorageError {
         start_key: Key,
         end_key: Key,
     },
-    OtherError(String),
+    Timeout(Duration),
+    Canceled,
+
+    /// Errors that can not be handled by a coprocessor plugin but should instead be returned to the
+    /// client.
+    ///
+    /// If such an error appears, plugins can run some cleanup code and return early from the
+    /// request. The error will be passed to the client and the client might retry the request.
+    /// TODO: we said `Box<dyn Any>`, but isn't `Box<dyn Error>` also fine?
+    Other(Box<dyn Error>),
 }
 
 impl fmt::Display for StorageError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Storage-related error: {:?}", self)
+        match self {
+            StorageError::KeyNotInRegion {
+                key,
+                region,
+                start_key,
+                end_key,
+            } => write!(f, "key {:?} not found in region {:?}", key, region.id),
+            StorageError::Timeout(d) => write!(f, "timeout after {:?}", d),
+            StorageError::Canceled => write!(f, "request canceled"),
+            StorageError::Other(e) => write!(f, "{}", e),
+        }
     }
 }
 
--- src/coprocessor_v2/raw_storage_impl.rs
+++ src/coprocessor_v2/raw_storage_impl.rs
@@ -2,13 +2,14 @@
 
 use async_trait::async_trait;
 use coprocessor_plugin_api::*;
+use futures::channel::oneshot::Canceled;
 use kvproto::kvrpcpb::Context;
 use std::ops::Range;
 use tikv_util::future::paired_future_callback;
 
-use crate::storage::errors::{extract_kv_pairs, extract_region_error};
-use crate::storage::lock_manager::LockManager;
-use crate::storage::{Engine, Storage};
+use crate::storage::errors::extract_kv_pairs;
+use crate::storage::{self, Engine, Storage, lock_manager::LockManager};
+use crate::storage::kv::{Error as EngineError, ErrorInner as EngineErrorInner};
 
 /// Implementation of the [`RawStorage`] trait.
 ///
@@ -35,8 +36,8 @@ impl<E: Engine, L: LockManager> RawStorage for RawStorageImpl<'_, E, L> {
 
         let res = self.storage.raw_get(ctx, cf, key);
 
-        let v = res.await;
-        construct_result(v, |x| x)
+        let value = res.await.map_err(StorageErrorShim::from)?;
+        Ok(value)
     }
 
     async fn batch_get(&self, keys: Vec<Key>) -> StorageResult<Vec<KvPair>> {
@@ -45,13 +46,12 @@ impl<E: Engine, L: LockManager> RawStorage for RawStorageImpl<'_, E, L> {
 
         let res = self.storage.raw_batch_get(ctx, cf, keys);
 
-        let v = res.await;
-        construct_result(v, |x| {
-            extract_kv_pairs(Ok(x))
-                .into_iter()
-                .map(|kv| (kv.key, kv.value))
-                .collect()
-        })
+        let v = res.await.map_err(StorageErrorShim::from)?;
+        let kv_pairs = extract_kv_pairs(Ok(v))
+            .into_iter()
+            .map(|kv| (kv.key, kv.value))
+            .collect();
+        Ok(kv_pairs)
     }
 
     async fn scan(&self, key_range: Range<Key>) -> StorageResult<Vec<Value>> {
@@ -70,13 +70,12 @@ impl<E: Engine, L: LockManager> RawStorage for RawStorageImpl<'_, E, L> {
             reverse,
         );
 
-        let v = res.await;
-        construct_result(v, |x| {
-            extract_kv_pairs(Ok(x))
-                .into_iter()
-                .map(|kv| kv.value)
-                .collect()
-        })
+        let v = res.await.map_err(StorageErrorShim::from)?;
+        let values = extract_kv_pairs(Ok(v))
+            .into_iter()
+            .map(|kv| kv.value)
+            .collect();
+        Ok(values)
     }
 
     async fn put(&self, key: Key, value: Value) -> StorageResult<()> {
@@ -87,11 +86,12 @@ impl<E: Engine, L: LockManager> RawStorage for RawStorageImpl<'_, E, L> {
 
         let res = self.storage.raw_put(ctx, cf, key, value, ttl, cb);
 
-        let v = match res {
+        match res {
             Err(e) => Err(e),
-            Ok(_) => f.await.expect("future got canceled"),
-        };
-        construct_result(v, |x| x)
+            Ok(_) => f.await.map_err(StorageErrorShim::from)?,
+        }
+        .map_err(StorageErrorShim::from)?;
+        Ok(())
     }
 
     async fn batch_put(&self, kv_pairs: Vec<KvPair>) -> StorageResult<()> {
@@ -102,11 +102,12 @@ impl<E: Engine, L: LockManager> RawStorage for RawStorageImpl<'_, E, L> {
 
         let res = self.storage.raw_batch_put(ctx, cf, kv_pairs, ttl, cb);
 
-        let v = match res {
+        match res {
             Err(e) => Err(e),
-            Ok(_) => f.await.expect("future got canceled"),
-        };
-        construct_result(v, |x| x)
+            Ok(_) => f.await.map_err(StorageErrorShim::from)?,
+        }
+        .map_err(StorageErrorShim::from)?;
+        Ok(())
     }
 
     async fn delete(&self, key: Key) -> StorageResult<()> {
@@ -116,11 +117,12 @@ impl<E: Engine, L: LockManager> RawStorage for RawStorageImpl<'_, E, L> {
 
         let res = self.storage.raw_delete(ctx, cf, key, cb);
 
-        let v = match res {
+        match res {
             Err(e) => Err(e),
-            Ok(_) => f.await.expect("future got canceled"),
-        };
-        construct_result(v, |x| x)
+            Ok(_) => f.await.map_err(StorageErrorShim::from)?,
+        }
+        .map_err(StorageErrorShim::from)?;
+        Ok(())
     }
 
     async fn batch_delete(&self, keys: Vec<Key>) -> StorageResult<()> {
@@ -130,11 +132,12 @@ impl<E: Engine, L: LockManager> RawStorage for RawStorageImpl<'_, E, L> {
 
         let res = self.storage.raw_batch_delete(ctx, cf, keys, cb);
 
-        let v = match res {
+        match res {
             Err(e) => Err(e),
-            Ok(_) => f.await.expect("future got canceled"),
-        };
-        construct_result(v, |x| x)
+            Ok(_) => f.await.map_err(StorageErrorShim::from)?,
+        }
+        .map_err(StorageErrorShim::from)?;
+        Ok(())
     }
 
     async fn delete_range(&self, key_range: Range<Key>) -> StorageResult<()> {
@@ -147,28 +150,54 @@ impl<E: Engine, L: LockManager> RawStorage for RawStorageImpl<'_, E, L> {
             .storage
             .raw_delete_range(ctx, cf, key_range.start, key_range.end, cb);
 
-        let v = match res {
+        match res {
             Err(e) => Err(e),
-            Ok(_) => f.await.expect("future got canceled"),
-        };
-        construct_result(v, |x| x)
+            Ok(_) => f.await.map_err(StorageErrorShim::from)?,
+        }
+        .map_err(StorageErrorShim::from)?;
+        Ok(())
     }
 }
 
-fn construct_result<T, R>(
-    storage_result: crate::storage::Result<T>,
-    on_ok: impl FnOnce(T) -> R,
-) -> StorageResult<R> {
-    if let Some(err) = extract_region_error(&storage_result) {
-        // TODO: map specific error to StorageError
-        match err {
-            _ => todo!(),
-        }
-    } else if let Err(e) = storage_result {
-        Err(StorageError::OtherError(format!("{}", e)))
-    } else if let Ok(v) = storage_result {
-        Ok(on_ok(v))
-    } else {
-        unreachable!()
+/// Helper struct for converting between [`storage::errors::Error`] and
+/// [`coprocessor_plugin_api::StorageError`].
+struct StorageErrorShim(StorageError);
+
+impl From<StorageErrorShim> for StorageError {
+    fn from(err_shim: StorageErrorShim) -> Self {
+        err_shim.0
     }
 }
+
+impl From<storage::errors::Error> for StorageErrorShim {
+    fn from(error: storage::errors::Error) -> Self {
+        let inner = match *error.0 {
+            // Key not in region
+            storage::errors::ErrorInner::Engine(EngineError(box EngineErrorInner::Request(
+                ref req_err,
+            ))) if req_err.has_key_not_in_region() => {
+                let key_err = req_err.get_key_not_in_region();
+                StorageError::KeyNotInRegion {
+                    key: key_err.get_key().to_owned(),
+                    region: todo!(), // TODO: how to construct region here? We only have region_id
+                    start_key: key_err.get_start_key().to_owned(),
+                    end_key: key_err.get_end_key().to_owned(),
+                }
+            }
+            // Timeout
+            storage::errors::ErrorInner::Engine(EngineError(box EngineErrorInner::Timeout(
+                duration,
+            ))) => StorageError::Timeout(duration),
+            // Other errors are passed as-is.
+            err => StorageError::Other(Box::new(err)),
+        };
+        StorageErrorShim(inner)
+    }
+}
+
+impl From<Canceled> for StorageErrorShim {
+    fn from(_c: Canceled) -> Self {
+        StorageErrorShim(StorageError::Canceled)
+    }
+}
+
