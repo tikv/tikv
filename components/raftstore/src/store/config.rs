945aa976e21a69f5ded3178732692bba6a8b9c81
5kbpers <tangminghua@pingcap.com>
2021-03-31 22:20:34 +0800

    revert changes of raftstore config
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/raftstore/src/store/config.rs
+++ components/raftstore/src/store/config.rs
@@ -246,9 +246,7 @@ impl Default for Config {
             apply_batch_system: BatchSystemConfig::default(),
             store_batch_system: BatchSystemConfig::default(),
             future_poll_size: 1,
-            // make it compatible with the CDC flow control.
-            // TODO fix flow control to support hibernation.
-            hibernate_regions: false,
+            hibernate_regions: tikv_util::build_on_master_branch(),
             dev_assert: false,
             apply_yield_duration: ReadableDuration::millis(500),
 
