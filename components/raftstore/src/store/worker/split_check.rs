e975f38389b2f3d6e1fa4a067637af82a5528d1a
Connor <zbk602423539@gmail.com>
2021-04-01 19:59:24 +0800

    cherry pick #9897 to release-5.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- components/raftstore/src/store/worker/split_check.rs
+++ components/raftstore/src/store/worker/split_check.rs
@@ -12,6 +12,7 @@ use kvproto::metapb::Region;
 use kvproto::metapb::RegionEpoch;
 use kvproto::pdpb::CheckPolicy;
 
+#[cfg(any(test, feature = "testexport"))]
 use crate::coprocessor::Config;
 use crate::coprocessor::CoprocessorHost;
 use crate::coprocessor::SplitCheckerHost;
@@ -182,7 +183,6 @@ where
     engine: E,
     router: S,
     coprocessor: CoprocessorHost<E>,
-    cfg: Config,
 }
 
 impl<E, S> Runner<E, S>
@@ -190,12 +190,11 @@ where
     E: KvEngine,
     S: CasualRouter<E>,
 {
-    pub fn new(engine: E, router: S, coprocessor: CoprocessorHost<E>, cfg: Config) -> Runner<E, S> {
+    pub fn new(engine: E, router: S, coprocessor: CoprocessorHost<E>) -> Runner<E, S> {
         Runner {
             engine,
             router,
             coprocessor,
-            cfg,
         }
     }
 
@@ -212,13 +211,9 @@ where
         );
         CHECK_SPILT_COUNTER.all.inc();
 
-        let mut host = self.coprocessor.new_split_checker_host(
-            &self.cfg,
-            region,
-            &self.engine,
-            auto_split,
-            policy,
-        );
+        let mut host =
+            self.coprocessor
+                .new_split_checker_host(region, &self.engine, auto_split, policy);
         if host.skip() {
             debug!("skip split check"; "region_id" => region.get_id());
             return;
@@ -328,7 +323,7 @@ where
             "split check config updated";
             "change" => ?change
         );
-        self.cfg.update(change);
+        self.coprocessor.cfg.update(change);
     }
 }
 
@@ -348,7 +343,7 @@ where
             } => self.check_split(&region, auto_split, policy),
             Task::ChangeConfig(c) => self.change_cfg(c),
             #[cfg(any(test, feature = "testexport"))]
-            Task::Validate(f) => f(&self.cfg),
+            Task::Validate(f) => f(&self.coprocessor.cfg),
             Task::GetRegionApproximateSizeAndKeys {
                 region,
                 pending_tasks,
