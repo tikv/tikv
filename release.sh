46971477c22ad17d446e4fcf7b5b71b111cfacab
Zhigao Tong <tongzhigao@pingcap.com>
2021-04-03 12:11:57 +0800

    Update build scripts
    Signed-off-by: Zhigao Tong <tongzhigao@pingcap.com>

--- release.sh
+++ release.sh
@@ -18,6 +18,9 @@ if [[ $(uname -s) == "Darwin" ]]; then
 #  export OPENSSL_LIB_DIR=$(brew --prefix openssl)"/lib"
 #  export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)"/include"
 #  export OPENSSL_NO_VENDOR=1
+  export OPENSSL_ROOT_DIR="/usr/local/opt/openssl"
+  export CARGO_PROFILE_DEV_DEBUG="true"
+  export CARGO_PROFILE_RELEASE_DEBUG="true"
   mkdir -p target/release
   PROXY_LIB_TARGET_COPY_PATH="target/release/lib${ENGINE_LABEL_VALUE}_proxy.dylib" make build
 else
