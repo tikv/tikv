3927fd8edd0bcb40ee9fe50e0c08f041c6ed9625
dongyan <dybest@icloud.com>
2021-04-04 15:18:05 +0800

    fix tests
    Signed-off-by: dongyan <dybest@icloud.com>

--- components/tidb_query_aggr/src/impl_variance.rs
+++ components/tidb_query_aggr/src/impl_variance.rs
@@ -472,18 +472,13 @@ mod tests {
             VectorValue::with_capacity(0, EvalType::Decimal),
         ];
 
-        let mut buf = BufferVec::new();
-        buf.push("我好强啊");
-        buf.push("我太强啦");
-        let buf = Arc::new(buf);
-
         state.push_result(&mut ctx, &mut result[..]).unwrap();
         assert_eq!(result[0].to_int_vec(), &[Some(0)]);
         assert_eq!(result[1].to_decimal_vec(), &[None]);
         assert_eq!(result[2].to_decimal_vec(), &[None]);
 
-        update!(state, &mut ctx, Some(EnumRef::new(&buf, 1))).unwrap();
-        update!(state, &mut ctx, Some(EnumRef::new(&buf, 2))).unwrap();
+        update!(state, &mut ctx, Some(EnumRef::new("bbb".as_bytes(), 1))).unwrap();
+        update!(state, &mut ctx, Some(EnumRef::new("aaa".as_bytes(), 2))).unwrap();
         result[0].clear();
         result[1].clear();
         result[2].clear();
