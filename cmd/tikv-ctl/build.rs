cf89cfaf5b00916758b12cc401ef8de1b44b6774
Yilin Chen <sticnarf@gmail.com>
2021-04-06 14:48:26 +0800

    extract build.rs
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- cmd/tikv-ctl/build.rs
+++ cmd/tikv-ctl/build.rs
@@ -1,10 +1,3 @@
 // Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
 
-// Pay attention to synchronize this file with cmd/tikv-server/build.rs.
-
-fn main() {
-    println!(
-        "cargo:rustc-env=TIKV_BUILD_TIME={}",
-        time::now_utc().strftime("%Y-%m-%d %H:%M:%S").unwrap()
-    );
-}
+include!("../build.rs");
