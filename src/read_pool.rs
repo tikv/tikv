be300c1148666df4ba3582305d320871b2fa3331
Connor <zbk602423539@gmail.com>
2021-04-12 17:38:50 +0800

    shorten read pool thread name
    Signed-off-by: Connor <zbk602423539@gmail.com>

--- src/read_pool.rs
+++ src/read_pool.rs
@@ -170,14 +170,14 @@ fn get_unified_read_pool_name() -> String {
 
     static COUNTER: AtomicU64 = AtomicU64::new(0);
     format!(
-        "unified-read-pool-test-{}",
+        "read-pool-test-{}",
         COUNTER.fetch_add(1, Ordering::Relaxed)
     )
 }
 
 #[cfg(not(test))]
 fn get_unified_read_pool_name() -> String {
-    "unified-read-pool".to_string()
+    "read-pool".to_string()
 }
 
 pub fn build_yatp_read_pool<E: Engine, R: FlowStatsReporter>(
