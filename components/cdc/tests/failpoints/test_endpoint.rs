f3cc18bf48301006560986ec62b690b0f54c3652
Zixiong Liu <liuzixiong@pingcap.com>
2021-04-12 15:38:27 +0800

    fix clippy
    Signed-off-by: Zixiong Liu <liuzixiong@pingcap.com>

--- components/cdc/tests/failpoints/test_endpoint.rs
+++ components/cdc/tests/failpoints/test_endpoint.rs
@@ -21,11 +21,11 @@ fn test_cdc_double_scan_deregister() {
     let mut mutation = Mutation::default();
     mutation.set_op(Op::Put);
     mutation.key = k.clone();
-    mutation.value = v.clone();
+    mutation.value = v;
     suite.must_kv_prewrite(1, vec![mutation], k.clone(), start_ts1);
     // Commit
     let commit_ts1 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
-    suite.must_kv_commit(1, vec![k.clone()], start_ts1, commit_ts1);
+    suite.must_kv_commit(1, vec![k], start_ts1, commit_ts1);
 
     let fp = "cdc_incremental_scan_start";
     fail::cfg(fp, "pause").unwrap();
@@ -55,13 +55,10 @@ fn test_cdc_double_scan_deregister() {
     'outer: loop {
         let event = receive_event_1(false);
         let mut event_vec = event.events.to_vec();
-        while event_vec.len() > 0 {
-            match event_vec.pop().unwrap().event.unwrap() {
-                Event_oneof_event::Error(err) => {
-                    assert!(err.has_region_not_found(), "{:?}", err);
-                    break 'outer;
-                }
-                _ => {}
+        while !event_vec.is_empty() {
+            if let Event_oneof_event::Error(err) = event_vec.pop().unwrap().event.unwrap() {
+                assert!(err.has_region_not_found(), "{:?}", err);
+                break 'outer;
             }
         }
     }
@@ -117,13 +114,10 @@ fn test_cdc_double_scan_io_error() {
             }
         }
         let mut event_vec = event.events.to_vec();
-        while event_vec.len() > 0 {
-            match event_vec.pop().unwrap().event.unwrap() {
-                Event_oneof_event::Error(err) => {
-                    assert!(err.has_region_not_found(), "{:?}", err);
-                    break 'outer;
-                }
-                _ => {}
+        while !event_vec.is_empty() {
+            if let Event_oneof_event::Error(err) = event_vec.pop().unwrap().event.unwrap() {
+                assert!(err.has_region_not_found(), "{:?}", err);
+                break 'outer;
             }
         }
     }
@@ -137,7 +131,7 @@ fn test_cdc_double_scan_io_error() {
             }
         }
         let mut event_vec = event.events.to_vec();
-        while event_vec.len() > 0 {
+        while !event_vec.is_empty() {
             match event_vec.pop().unwrap().event.unwrap() {
                 Event_oneof_event::Error(err) => {
                     assert!(err.has_region_not_found(), "{:?}", err);
