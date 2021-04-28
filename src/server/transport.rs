9290b707dc61d4f1e33077e9f866c50b17b8d77d
Brian Anderson <andersrb@gmail.com>
2021-04-28 07:32:26 -0400

    server: Replace RocksEngine with type parameter
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- src/server/transport.rs
+++ src/server/transport.rs
@@ -4,43 +4,52 @@ use kvproto::raft_serverpb::RaftMessage;
 
 use crate::server::raft_client::RaftClient;
 use crate::server::resolve::StoreAddrResolver;
-use engine_rocks::RocksEngine;
+use engine_traits::KvEngine;
 use raftstore::router::RaftStoreRouter;
 use raftstore::store::Transport;
 use raftstore::Result as RaftStoreResult;
+use std::marker::PhantomData;
 
-pub struct ServerTransport<T, S>
+pub struct ServerTransport<T, S, E>
 where
-    T: RaftStoreRouter<RocksEngine> + 'static,
+    T: RaftStoreRouter<E> + 'static,
     S: StoreAddrResolver + 'static,
+    E: KvEngine,
 {
-    raft_client: RaftClient<S, T, RocksEngine>,
+    raft_client: RaftClient<S, T, E>,
+    engine: PhantomData<E>,
 }
 
-impl<T, S> Clone for ServerTransport<T, S>
+impl<T, S, E> Clone for ServerTransport<T, S, E>
 where
-    T: RaftStoreRouter<RocksEngine> + 'static,
+    T: RaftStoreRouter<E> + 'static,
     S: StoreAddrResolver + 'static,
+    E: KvEngine,
 {
     fn clone(&self) -> Self {
         ServerTransport {
             raft_client: self.raft_client.clone(),
+            engine: PhantomData,
         }
     }
 }
 
-impl<T: RaftStoreRouter<RocksEngine> + 'static, S: StoreAddrResolver + 'static>
-    ServerTransport<T, S>
+impl<T, S, E> ServerTransport<T, S, E>
+where E: KvEngine, T: RaftStoreRouter<E> + 'static, S: StoreAddrResolver + 'static
 {
-    pub fn new(raft_client: RaftClient<S, T, RocksEngine>) -> ServerTransport<T, S> {
-        ServerTransport { raft_client }
+    pub fn new(raft_client: RaftClient<S, T, E>) -> ServerTransport<T, S, E> {
+        ServerTransport {
+            raft_client,
+            engine: PhantomData,
+        }
     }
 }
 
-impl<T, S> Transport for ServerTransport<T, S>
+impl<T, S, E> Transport for ServerTransport<T, S, E>
 where
-    T: RaftStoreRouter<RocksEngine> + Unpin + 'static,
+    T: RaftStoreRouter<E> + Unpin + 'static,
     S: StoreAddrResolver + Unpin + 'static,
+    E: KvEngine + Unpin,
 {
     fn send(&mut self, msg: RaftMessage) -> RaftStoreResult<()> {
         match self.raft_client.send(msg) {
