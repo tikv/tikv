9074d5b243c47480f605c97ebbf410a72a56cd04
Andreas Zimmerer <andreas.zimmerer@tum.de>
2021-05-02 12:11:10 +0200

    coprv2: remove redundant lifetime
    Signed-off-by: Andreas Zimmerer <andreas.zimmerer@tum.de>

--- src/coprocessor_v2/plugin_registry.rs
+++ src/coprocessor_v2/plugin_registry.rs
@@ -450,7 +450,7 @@ mod tests {
     use std::sync::Once;
 
     static INIT: Once = Once::new();
-    static EXAMPLE_PLUGIN: &'static [u8] = include_bytes!(env!("CARGO_DYLIB_FILE_EXAMPLE_PLUGIN"));
+    static EXAMPLE_PLUGIN: &[u8] = include_bytes!(env!("CARGO_DYLIB_FILE_EXAMPLE_PLUGIN"));
 
     fn initialize_library() -> PathBuf {
         let lib_path = std::env::temp_dir().join(&pkgname_to_libname("example-plugin"));
