e0a50675cde1c8c68be3e3231d3f4b4f45425b7d
Little-Wallace <bupt2013211450@gmail.com>
2021-05-05 13:34:02 +0800

    refactor batch get commandt
    Signed-off-by: Little-Wallace <bupt2013211450@gmail.com>

--- src/server/metrics.rs
+++ src/server/metrics.rs
@@ -398,7 +398,7 @@ lazy_static! {
             "tikv_server_request_batch_size",
             "Size of request batch input",
             &["type"],
-            exponential_buckets(1f64, 5f64, 10).unwrap()
+            vec![1.0, 2.0, 4.0, 8.0, 12.0, 16.0, 20.0, 24.0, 28.0, 32.0, 64.0]
         )
         .unwrap();
     pub static ref REQUEST_BATCH_RATIO_HISTOGRAM_VEC: RequestBatchRatioHistogramVec =
