ba3f8a5ce7016a89e978947d1c0e623ddcb06b60
guo-shaoge <shaoge1994@163.com>
2021-05-11 09:11:40 +0800

    add more tests
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- components/tidb_query_aggr/src/impl_max_min.rs
+++ components/tidb_query_aggr/src/impl_max_min.rs
@@ -505,13 +505,13 @@ where
                 if IS_UNSIGNED {
                     let v1 = self.extremum.map(|x| x as u64);
                     let v2 = value.map(|x| *x as u64);
-                    if v1 < v2 {
+                    if v1.cmp(&v2) == E::ORD {
                         self.extremum = value.map(|x| *x);
                     }
                 } else {
                     let v1 = self.extremum.map(|x| x as i64);
                     let v2 = value.map(|x| *x as i64);
-                    if v1 < v2 {
+                    if v1.cmp(&v2) == E::ORD {
                         self.extremum = value.map(|x| *x);
                     }
                 }
@@ -775,20 +775,6 @@ mod tests {
 
     #[test]
     fn test_integration() {
-        let max_parser = AggrFnDefinitionParserExtremum::<Max>::new();
-        let min_parser = AggrFnDefinitionParserExtremum::<Min>::new();
-
-        let max = ExprDefBuilder::aggr_func(ExprType::Max, FieldTypeTp::LongLong)
-            .push_child(ExprDefBuilder::column_ref(0, FieldTypeTp::LongLong))
-            .build();
-        max_parser.check_supported(&max).unwrap();
-
-        let min = ExprDefBuilder::aggr_func(ExprType::Min, FieldTypeTp::LongLong)
-            .push_child(ExprDefBuilder::column_ref(0, FieldTypeTp::LongLong))
-            .build();
-        min_parser.check_supported(&min).unwrap();
-
-        let src_schema = [FieldTypeTp::LongLong.into()];
         let mut columns = LazyBatchColumnVec::from(vec![{
             let mut col = LazyBatchColumn::decoded_with_capacity_and_tp(0, EvalType::Int);
             col.mut_decoded().push_int(Some(10000));
@@ -802,6 +788,77 @@ mod tests {
             col
         }]);
         let logical_rows = vec![3, 2, 6, 5, 1, 7];
+        let expected_res = vec![Some(99), Some(-1i64),];
+        let mut field_type = FieldType::default();
+        let fta = field_type.as_mut_accessor();
+        fta.set_tp(FieldTypeTp::LongLong);
+
+        test_integration_util(field_type.tp(),
+                              field_type.flag(),
+                              &mut columns, &logical_rows, &expected_res);
+    }
+
+    #[test]
+    fn test_aggr_unsigned_signed_int() {
+        // test unsigned bigint
+        let mut columns = LazyBatchColumnVec::from(vec![{
+            let mut col = LazyBatchColumn::decoded_with_capacity_and_tp(0, EvalType::Int);
+            col.mut_decoded().push_int(None);
+            col.mut_decoded().push_int(Some(-1));
+            col.mut_decoded().push_int(Some(2420174916247255494));
+            col.mut_decoded().push_int(Some(3899490809029152765));
+            col
+        }]);
+        let logical_rows = vec![0, 1, 2, 3];
+        let expected_res = vec![Some(-1), Some(2420174916247255494),];
+        let mut field_type = FieldType::default();
+        let fta = field_type.as_mut_accessor();
+        fta.set_tp(FieldTypeTp::LongLong);
+        fta.set_flag(FieldTypeFlag::UNSIGNED);
+
+        test_integration_util(field_type.tp(), field_type.flag(), &mut columns, &logical_rows, &expected_res);
+        
+        // test signed bigint
+        let mut columns = LazyBatchColumnVec::from(vec![{
+            let mut col = LazyBatchColumn::decoded_with_capacity_and_tp(0, EvalType::Int);
+            col.mut_decoded().push_int(None);
+            col.mut_decoded().push_int(Some(-1));
+            col.mut_decoded().push_int(Some(2420174916247255494));
+            col.mut_decoded().push_int(Some(3899490809029152765));
+            col
+        }]);
+        let logical_rows = vec![0, 1, 2, 3];
+        let expected_res = vec![Some(3899490809029152765), Some(-1),];
+        let mut field_type = FieldType::default();
+        let fta = field_type.as_mut_accessor();
+        fta.set_tp(FieldTypeTp::LongLong);
+
+        test_integration_util(field_type.tp(), field_type.flag(), &mut columns, &logical_rows, &expected_res);
+    }
+
+    fn test_integration_util(field_type: FieldTypeTp,
+                             child_flag: FieldTypeFlag,
+                             columns: &mut LazyBatchColumnVec,
+                             logical_rows: &Vec<usize>,
+                             expected_res: &Vec<Option<Int>>) {
+        let max_parser = AggrFnDefinitionParserExtremum::<Max>::new();
+        let min_parser = AggrFnDefinitionParserExtremum::<Min>::new();
+
+        let mut child_field_type: FieldType = field_type.into();
+        child_field_type.as_mut_accessor()
+            .set_flag(child_flag);
+
+        let max = ExprDefBuilder::aggr_func(ExprType::Max, field_type)
+            .push_child(ExprDefBuilder::column_ref(0, child_field_type.clone()))
+            .build();
+        max_parser.check_supported(&max).unwrap();
+
+        let min = ExprDefBuilder::aggr_func(ExprType::Min, field_type)
+            .push_child(ExprDefBuilder::column_ref(0, child_field_type.clone()))
+            .build();
+        min_parser.check_supported(&min).unwrap();
+
+        let src_schema = [child_field_type];
 
         let mut schema = vec![];
         let mut exp = vec![];
@@ -811,14 +868,14 @@ mod tests {
             .parse(max, &mut ctx, &src_schema, &mut schema, &mut exp)
             .unwrap();
         assert_eq!(schema.len(), 1);
-        assert_eq!(schema[0].as_accessor().tp(), FieldTypeTp::LongLong);
+        assert_eq!(schema[0].as_accessor().tp(), field_type);
         assert_eq!(exp.len(), 1);
 
         let min_fn = min_parser
             .parse(min, &mut ctx, &src_schema, &mut schema, &mut exp)
             .unwrap();
         assert_eq!(schema.len(), 2);
-        assert_eq!(schema[1].as_accessor().tp(), FieldTypeTp::LongLong);
+        assert_eq!(schema[1].as_accessor().tp(), field_type);
         assert_eq!(exp.len(), 2);
 
         let mut ctx = EvalContext::default();
@@ -830,7 +887,7 @@ mod tests {
         // max
         {
             let max_result = exp[0]
-                .eval(&mut ctx, &src_schema, &mut columns, &logical_rows, 6)
+                .eval(&mut ctx, &src_schema, columns, &logical_rows, logical_rows.len())
                 .unwrap();
             let max_result = max_result.vector_value().unwrap();
             let max_slice: ChunkedVecSized<Int> = max_result.as_ref().to_int_vec().into();
@@ -841,7 +898,7 @@ mod tests {
         // min
         {
             let min_result = exp[0]
-                .eval(&mut ctx, &src_schema, &mut columns, &logical_rows, 6)
+                .eval(&mut ctx, &src_schema, columns, &logical_rows, logical_rows.len())
                 .unwrap();
             let min_result = min_result.vector_value().unwrap();
             let min_slice: ChunkedVecSized<Int> = min_result.as_ref().to_int_vec().into();
@@ -849,7 +906,7 @@ mod tests {
             min_state.push_result(&mut ctx, &mut aggr_result).unwrap();
         }
 
-        assert_eq!(aggr_result[0].to_int_vec(), &[Some(99), Some(-1i64),]);
+        assert_eq!(aggr_result[0].to_int_vec(), expected_res.clone());
     }
 
     #[test]
