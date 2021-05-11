cf4ab65fde9383664df39ba986ff395db4e2c779
Song Gao <disxiaofei@163.com>
2021-05-11 15:03:23 +0800

    improve test
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- src/import/sst_service.rs
+++ src/import/sst_service.rs
@@ -498,6 +498,15 @@ where
         self.threads.spawn_ok(buf_driver);
         self.threads.spawn_ok(handle_task);
     }
+
+    fn multi_ingest(
+        &mut self,
+        _: RpcContext<'_>,
+        _: MultiIngestRequest,
+        _: UnarySink<kvproto::import_sstpb::IngestResponse>,
+    ) {
+        unimplemented!()
+    }
 }
 
 // add error statistics from pb error response
