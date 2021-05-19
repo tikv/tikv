de48a8bd3323101a5374fe9534d9cb35a1652a02
qupeng <qupeng@pingcap.com>
2021-05-19 16:01:49 +0800

    fix a typo
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- src/server/server.rs
+++ src/server/server.rs
@@ -90,7 +90,7 @@ impl<T: RaftStoreRouter, S: StoreAddrResolver + 'static, E: Engine> Server<T, S,
             .max_concurrent_stream(cfg.grpc_concurrent_stream)
             .max_receive_message_len(-1)
             .max_send_message_len(-1)
-            .http2_max_ping_strikes(i32::MAX) // For pings without data from clients.
+            .http2_max_ping_strikes(std::i32::MAX) // For pings without data from clients.
             .build_args();
         let grpc_server = {
             let mut sb = ServerBuilder::new(Arc::clone(&env))
