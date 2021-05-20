23c8734073e52e48434ae0dbc6affcd2402d55d7
Brian Anderson <andersrb@gmail.com>
2021-05-19 23:13:54 -0400

    tests: Fix tests
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/server/src/server.rs
+++ components/server/src/server.rs
@@ -195,7 +195,7 @@ struct TiKVEngines<ER: RaftEngine> {
 
 struct Servers<ER: RaftEngine> {
     lock_mgr: LockManager,
-    server: Server<RaftRouter<RocksEngine, ER>, resolve::PdStoreAddrResolver>,
+    server: Server<RaftRouter<RocksEngine, ER>, resolve::PdStoreAddrResolver, RaftKv<RocksEngine, ServerRaftStoreRouter<RocksEngine, ER>>>,
     node: Node<RpcClient, ER>,
     importer: Arc<SSTImporter>,
     cdc_scheduler: tikv_util::worker::Scheduler<cdc::Task>,
