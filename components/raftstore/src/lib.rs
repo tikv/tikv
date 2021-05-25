28bcebe1a8d3c50cd0e867b417abff0737569e0c
qupeng <qupeng@pingcap.com>
2021-05-25 15:37:36 +0800

    address comments
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- components/raftstore/src/lib.rs
+++ components/raftstore/src/lib.rs
@@ -18,7 +18,16 @@ pub mod store;
 pub use self::coprocessor::{RegionInfo, RegionInfoAccessor, SeekRegionCallback};
 pub use self::errors::{DiscardReason, Error, Result};
 
+// With feature protobuf-codec, `bytes::Bytes` is generated for `bytes` in protobuf.
+#[cfg(feature = "protobuf-codec")]
 fn bytes_capacity(b: &bytes::Bytes) -> usize {
-    let range = b.as_ptr_range();
-    range.end as usize - range.start as usize
+    // NOTE: it's correct because in raftstore all `bytes::Bytes`s are immutable,
+    // which means `clear` or similar others will never be called for them.
+    b.len()
+}
+
+// Currently `bytes::Bytes` are not available for prost-codec.
+#[cfg(feature = "prost-codec")]
+fn bytes_capacity(b: &Vec<u8>) -> usize {
+    b.capacity()
 }
