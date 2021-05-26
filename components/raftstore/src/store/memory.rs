02685047779559e2d2b22111a4ad6afcf677918c
5kbpers <tangminghua@pingcap.com>
2021-05-26 22:23:37 +0800

    address comments
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/raftstore/src/store/memory.rs
+++ components/raftstore/src/store/memory.rs
@@ -11,15 +11,3 @@ lazy_static! {
         [(raft_router, [alive, leak]), (apply_router, [alive, leak])]
     );
 }
-
-#[derive(MemoryTraceHelper, Default)]
-pub struct RaftContextTrace {
-    pub write_batch: usize,
-    pub rest: usize,
-}
-
-#[derive(MemoryTraceHelper, Default)]
-pub struct ApplyContextTrace {
-    pub cbs_size: usize,
-    pub rest: usize,
-}
