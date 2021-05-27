09322781e466a061c8c5cba9c1d14bea6ac29483
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-05-27 21:13:35 +0800

    cdc, mvcc: always read old value when cdc requires (#10197) (#10246)
    * cdc, mvcc: always read old value when cdc requires (#10197)
    
    * cdc, mvcc: always read old value when cdc requires
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    * address comments
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    * add more assert messages
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    * address comments
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    Co-authored-by: Neil Shen <overvenus@gmail.com>
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- components/cdc/src/delegate.rs
+++ components/cdc/src/delegate.rs
@@ -651,7 +651,13 @@ impl Delegate {
                 };
                 // validate commit_ts must be greater than the current resolved_ts
                 if let (Some(resolver), Some(commit_ts)) = (&self.resolver, commit_ts) {
-                    assert!(commit_ts > resolver.resolved_ts().unwrap_or_default());
+                    let resolved_ts = resolver.resolved_ts().unwrap_or_default();
+                    assert!(
+                        commit_ts > resolved_ts,
+                        "commit_ts: {:?}, resolved_ts: {:?}",
+                        commit_ts,
+                        resolved_ts
+                    );
                 }
 
                 match rows.get_mut(&row.key) {
