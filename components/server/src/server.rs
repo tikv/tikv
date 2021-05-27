68835a7fffeeae69a4c696e7a2288bd6808509dc
Brian Anderson <andersrb@gmail.com>
2021-05-27 17:16:05 -0400

    server: Decomplexify types
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/server/src/server.rs
+++ components/server/src/server.rs
@@ -195,16 +195,15 @@ struct TiKVEngines<ER: RaftEngine> {
 
 struct Servers<ER: RaftEngine> {
     lock_mgr: LockManager,
-    server: Server<
-        RaftRouter<RocksEngine, ER>,
-        resolve::PdStoreAddrResolver,
-        RaftKv<RocksEngine, ServerRaftStoreRouter<RocksEngine, ER>>,
-    >,
+    server: ServerType<ER>,
     node: Node<RpcClient, ER>,
     importer: Arc<SSTImporter>,
     cdc_scheduler: tikv_util::worker::Scheduler<cdc::Task>,
     cdc_memory_quota: MemoryQuota,
 }
+
+type ServerType<ER> = Server<RaftRouter<RocksEngine, ER>, resolve::PdStoreAddrResolver, RaftKvType<ER>>;
+type RaftKvType<ER> = RaftKv<RocksEngine, ServerRaftStoreRouter<RocksEngine, ER>>;
 
 impl<ER: RaftEngine> TiKVServer<ER> {
     fn init(mut config: TiKvConfig) -> TiKVServer<ER> {
