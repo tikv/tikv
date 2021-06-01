68710715dcfe60fef9d16b7bdf6a445d673c1804
Neil Shen <overvenus@gmail.com>
2021-06-01 16:21:37 +0800

    cherry pick #10271 to release-4.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- components/cdc/tests/integrations/test_cdc.rs
+++ components/cdc/tests/integrations/test_cdc.rs
@@ -17,6 +17,11 @@ use kvproto::kvrpcpb::*;
 use pd_client::PdClient;
 use test_raftstore::sleep_ms;
 use test_raftstore::*;
+<<<<<<< HEAD
+=======
+use tikv_util::HandyRwLock;
+use txn_types::{Key, Lock, LockType};
+>>>>>>> 5f6781e85... cdc: stable cdc old value test (#10271)
 
 use cdc::{Task, Validate};
 
@@ -762,49 +767,106 @@ fn test_old_value_basic() {
     m1.set_op(Op::Put);
     m1.key = k1.clone();
     m1.value = b"v1".to_vec();
+<<<<<<< HEAD
     suite.must_kv_prewrite(1, vec![m1], k1.clone(), 1.into());
     suite.must_kv_commit(1, vec![k1.clone()], 1.into(), 2.into());
+=======
+    let ts1 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_prewrite(1, vec![m1], k1.clone(), ts1);
+    let ts2 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_commit(1, vec![k1.clone()], ts1, ts2);
+>>>>>>> 5f6781e85... cdc: stable cdc old value test (#10271)
     // Rollback
     let mut m2 = Mutation::default();
     m2.set_op(Op::Put);
     m2.key = k1.clone();
     m2.value = b"v2".to_vec();
+<<<<<<< HEAD
     suite.must_kv_prewrite(1, vec![m2], k1.clone(), 3.into());
     suite.must_kv_rollback(1, vec![k1.clone()], 3.into());
+=======
+    let ts3 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_prewrite(1, vec![m2], k1.clone(), ts3);
+    suite.must_kv_rollback(1, vec![k1.clone()], ts3);
+>>>>>>> 5f6781e85... cdc: stable cdc old value test (#10271)
     // Update value
     let mut m3 = Mutation::default();
     m3.set_op(Op::Put);
     m3.key = k1.clone();
     m3.value = vec![b'3'; 5120];
+<<<<<<< HEAD
     suite.must_kv_prewrite(1, vec![m3], k1.clone(), 4.into());
     suite.must_kv_commit(1, vec![k1.clone()], 4.into(), 5.into());
+=======
+    let ts4 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_prewrite(1, vec![m3], k1.clone(), ts4);
+    let ts5 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_commit(1, vec![k1.clone()], ts4, ts5);
+>>>>>>> 5f6781e85... cdc: stable cdc old value test (#10271)
     // Lock
     let mut m4 = Mutation::default();
     m4.set_op(Op::Lock);
     m4.key = k1.clone();
+<<<<<<< HEAD
     suite.must_kv_prewrite(1, vec![m4], k1.clone(), 6.into());
     suite.must_kv_commit(1, vec![k1.clone()], 6.into(), 7.into());
+=======
+    let ts6 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_prewrite(1, vec![m4], k1.clone(), ts6);
+    let ts7 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_commit(1, vec![k1.clone()], ts6, ts7);
+>>>>>>> 5f6781e85... cdc: stable cdc old value test (#10271)
     // Delete value and rollback
     let mut m5 = Mutation::default();
     m5.set_op(Op::Del);
     m5.key = k1.clone();
+<<<<<<< HEAD
     suite.must_kv_prewrite(1, vec![m5], k1.clone(), 8.into());
     suite.must_kv_rollback(1, vec![k1.clone()], 8.into());
+=======
+    let ts8 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_prewrite(1, vec![m5], k1.clone(), ts8);
+    suite.must_kv_rollback(1, vec![k1.clone()], ts8);
+>>>>>>> 5f6781e85... cdc: stable cdc old value test (#10271)
     // Update value
     let mut m6 = Mutation::default();
     m6.set_op(Op::Put);
     m6.key = k1.clone();
     m6.value = b"v6".to_vec();
+<<<<<<< HEAD
     suite.must_kv_prewrite(1, vec![m6], k1.clone(), 10.into());
     suite.must_kv_commit(1, vec![k1.clone()], 10.into(), 11.into());
+=======
+    let ts9 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    let ts10 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_prewrite(1, vec![m6], k1.clone(), ts10);
+    let ts11 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_commit(1, vec![k1.clone()], ts10, ts11);
+>>>>>>> 5f6781e85... cdc: stable cdc old value test (#10271)
     // Delete value in pessimistic txn.
     // In pessimistic txn, CDC must use for_update_ts to read the old value.
     let mut m7 = Mutation::default();
     m7.set_op(Op::PessimisticLock);
     m7.key = k1.clone();
+<<<<<<< HEAD
     suite.must_acquire_pessimistic_lock(1, vec![m7.clone()], k1.clone(), 9.into(), 12.into());
     m7.set_op(Op::Del);
     suite.must_kv_pessimistic_prewrite(1, vec![m7], k1, 9.into(), 12.into());
+=======
+    let ts12 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_acquire_pessimistic_lock(1, vec![m7.clone()], k1.clone(), ts9, ts12);
+    m7.set_op(Op::Del);
+    suite.must_kv_pessimistic_prewrite(1, vec![m7], k1.clone(), ts9, ts12);
+    let ts13 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_commit(1, vec![k1.clone()], ts9, ts13);
+    // Insert value again
+    let mut m8 = Mutation::default();
+    m8.set_op(Op::Insert);
+    m8.key = k1.clone();
+    m8.value = b"v1".to_vec();
+    let ts14 = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_prewrite(1, vec![m8], k1, ts14);
+>>>>>>> 5f6781e85... cdc: stable cdc old value test (#10271)
 
     let mut event_count = 0;
     loop {
