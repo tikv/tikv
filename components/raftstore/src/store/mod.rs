0f5f66632b4da0dd89d150372dc36565afa02a0b
yiwu-arbug <yiwu@pingcap.com>
2021-05-31 19:42:32 -0700

    raftstore: Add hibernate region metrics (#10152)
    * produce metrics
    
    Signed-off-by: Yi Wu <yiwu@pingcap.com>

--- components/raftstore/src/store/mod.rs
+++ components/raftstore/src/store/mod.rs
@@ -30,7 +30,7 @@ pub use self::bootstrap::{
 pub use self::compaction_guard::CompactionGuardGeneratorFactory;
 pub use self::config::Config;
 pub use self::fsm::{DestroyPeerJob, RaftRouter, StoreInfo};
-pub use self::hibernate_state::GroupState;
+pub use self::hibernate_state::{GroupState, HibernateState};
 pub use self::msg::{
     Callback, CasualMessage, ExtCallback, MergeResultKind, PeerMsg, PeerTicks, RaftCommand,
     ReadCallback, ReadResponse, SignificantMsg, StoreMsg, StoreTick, WriteCallback, WriteResponse,
