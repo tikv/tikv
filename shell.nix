c2869d52ed5f3a82275a8b09fe176275ed130c82
iosmanthus <myosmanthustree@gmail.com>
2021-06-08 19:29:28 +0800

    fix compiling issue in nix-shell --pure
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 pkgs.mkShell {
   hardeningDisable = [ "all" ];
   nativeBuildInputs = with pkgs;[ cmake ];
-  buildInputs = with pkgs;[ gnumake rustup protobuf3_8 ];
+  buildInputs = with pkgs;[ git gnumake rustup protobuf3_8 perl ];
   shellHooks = ''
     export PROTOC=${pkgs.protobuf3_8}/bin/protoc
   '';
