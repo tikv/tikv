695438e683bdf0cfc3b764836c703f8d1c50cfce
Brian Anderson <andersrb@gmail.com>
2021-06-09 12:25:02 -0400

    server: Replace RocksEngine with typaram in signal_handler
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/server/src/signal_handler.rs
+++ components/server/src/signal_handler.rs
@@ -4,15 +4,14 @@ pub use self::imp::wait_for_signal;
 
 #[cfg(unix)]
 mod imp {
-    use engine_rocks::RocksEngine;
-    use engine_traits::{Engines, MiscExt, RaftEngine};
+    use engine_traits::{Engines, MiscExt, RaftEngine, KvEngine};
     use libc::c_int;
     use nix::sys::signal::{SIGHUP, SIGINT, SIGTERM, SIGUSR1, SIGUSR2};
     use signal::trap::Trap;
     use tikv_util::metrics;
 
     #[allow(dead_code)]
-    pub fn wait_for_signal<ER: RaftEngine>(engines: Option<Engines<RocksEngine, ER>>) {
+    pub fn wait_for_signal<ER: RaftEngine>(engines: Option<Engines<impl KvEngine, ER>>) {
         let trap = Trap::trap(&[SIGTERM, SIGINT, SIGHUP, SIGUSR1, SIGUSR2]);
         for sig in trap {
             match sig {
@@ -37,8 +36,7 @@ mod imp {
 
 #[cfg(not(unix))]
 mod imp {
-    use engine_rocks::RocksEngine;
-    use engine_traits::Engines;
+    use engine_traits::{Engines, KvEngine};
 
-    pub fn wait_for_signal(_: Option<Engines<RocksEngine, RocksEngine>>) {}
+    pub fn wait_for_signal(_: Option<Engines<impl KvEngine, RocksEngine>>) {}
 }
