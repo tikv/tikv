ab79547f81eef322e1a2ee97585c42a025b4141e
MrCroxx <mrcroxx@outlook.com>
2021-06-09 16:20:35 +0800

    fix toml config serialize.
    Signed-off-by: MrCroxx <mrcroxx@outlook.com>

--- src/config.rs
+++ src/config.rs
@@ -1249,12 +1249,12 @@ pub struct RaftDbConfig {
     pub allow_concurrent_memtable_write: bool,
     pub bytes_per_sync: ReadableSize,
     pub wal_bytes_per_sync: ReadableSize,
+    #[config(skip)]
+    pub gc_on_compaction: bool,
     #[config(submodule)]
     pub defaultcf: RaftDefaultCfConfig,
     #[config(skip)]
     pub titan: TitanDBConfig,
-    #[config(skip)]
-    pub gc_on_compaction: bool,
 }
 
 impl Default for RaftDbConfig {
