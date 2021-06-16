9285fde1821431d993290922c84e819b4e5a2ced
Zhenchi <zhongzc_arch@outlook.com>
2021-06-16 17:30:54 +0800

    agent integration test case 1
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- tests/integrations/resource_metering/mod.rs
+++ tests/integrations/resource_metering/mod.rs
@@ -1,7 +1,9 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
-pub mod test_dynamic_config;
 pub mod test_suite;
+
+pub mod test_agent;
+pub mod test_dynamic_config;
 
 #[cfg(target_os = "linux")]
 mod linux {
@@ -16,5 +18,8 @@ mod linux {
         test_dynamic_config::case_report_interval(&mut ts);
         test_dynamic_config::case_max_resource_groups(&mut ts);
         test_dynamic_config::case_precision(&mut ts);
+
+        // Agent
+        test_agent::case_alter_agent_addr(&mut ts);
     }
 }
