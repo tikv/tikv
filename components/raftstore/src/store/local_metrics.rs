c601a877690e633c56f4bf961529fd2ec906006e
NingLin-P <linningde25@gmail.com>
2021-06-16 21:02:38 +0800

    resolved_ts: refine the resolved ts metric and panels (#10350)
    * refine resolved_ts metrics
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * test
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * add panels
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * add check leader duration'
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * refine check leader metric
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * fix
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * refine resolved ts panels
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * fix name
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    Signed-off-by: tiancaiamao <tiancaiamao@gmail.com>

--- components/raftstore/src/store/local_metrics.rs
+++ components/raftstore/src/store/local_metrics.rs
@@ -351,6 +351,7 @@ pub struct RaftMetrics {
     pub process_ready: LocalHistogram,
     pub append_log: LocalHistogram,
     pub commit_log: LocalHistogram,
+    pub check_leader: LocalHistogram,
     pub leader_missing: Arc<Mutex<HashSet<u64>>>,
     pub invalid_proposal: RaftInvalidProposeMetrics,
 }
@@ -367,6 +368,7 @@ impl Default for RaftMetrics {
                 .local(),
             append_log: PEER_APPEND_LOG_HISTOGRAM.local(),
             commit_log: PEER_COMMIT_LOG_HISTOGRAM.local(),
+            check_leader: CHECK_LEADER_DURATION_HISTOGRAM.local(),
             leader_missing: Arc::default(),
             invalid_proposal: Default::default(),
         }
@@ -382,6 +384,7 @@ impl RaftMetrics {
         self.process_ready.flush();
         self.append_log.flush();
         self.commit_log.flush();
+        self.check_leader.flush();
         self.message_dropped.flush();
         self.invalid_proposal.flush();
         let mut missing = self.leader_missing.lock().unwrap();
