67045349e890c7c702e529cfd3fbe4f008b85c01
Zhenchi <zhongzc_arch@outlook.com>
2021-06-16 11:08:33 +0800

    more tests
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- tests/integrations/resource_metering/test_dynamic_config.rs
+++ tests/integrations/resource_metering/test_dynamic_config.rs
@@ -11,13 +11,19 @@ pub fn case(test_suite: &mut TestSuite) {
     let port = alloc_port();
     test_suite.start_agent_at(port);
 
+    // No config address
     test_suite.cfg_enabled(true);
     test_suite.cfg_failpoint_op_duration(300);
     test_suite.execute_ops(vec!["op1", "op2"]);
     sleep(test_suite.get_current_cfg().report_agent_interval.0);
     assert!(test_suite.fetch_reported_cpu_time().is_empty());
 
+    // No requests
     test_suite.cfg_agent_address(format!("127.0.0.1:{}", port));
+    sleep(test_suite.get_current_cfg().report_agent_interval.0);
+    assert!(test_suite.fetch_reported_cpu_time().is_empty());
+
+    // Process 2 requests
     test_suite.execute_ops(vec!["op1", "op2"]);
     sleep(test_suite.get_current_cfg().report_agent_interval.0);
     let res = test_suite.fetch_reported_cpu_time();
@@ -26,7 +32,15 @@ pub fn case(test_suite: &mut TestSuite) {
     res.contains_key("op1");
     res.contains_key("op2");
 
+    // Empty config address
     test_suite.cfg_agent_address("");
+    test_suite.execute_ops(vec!["op1", "op2"]);
+    sleep(test_suite.get_current_cfg().report_agent_interval.0);
+    assert!(test_suite.fetch_reported_cpu_time().is_empty());
+
+    // Disable recorder
+    test_suite.cfg_enabled(false);
+    test_suite.cfg_agent_address(format!("127.0.0.1:{}", port));
     test_suite.execute_ops(vec!["op1", "op2"]);
     sleep(test_suite.get_current_cfg().report_agent_interval.0);
     assert!(test_suite.fetch_reported_cpu_time().is_empty());
