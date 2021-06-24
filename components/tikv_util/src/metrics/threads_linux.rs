82d77147a790f876a4dccf75f968d503cfc7acff
qupeng <qupeng@pingcap.com>
2021-06-24 20:00:41 +0800

    disable cases about thread I/O stats
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- components/tikv_util/src/metrics/threads_linux.rs
+++ components/tikv_util/src/metrics/threads_linux.rs
@@ -493,7 +493,7 @@ mod tests {
     use super::*;
 
     #[test]
-    // #[ignore] // CI gets 0 as stats.
+    #[ignore] // CI gets 0 as stats.
     fn test_thread_stat_io() {
         let name = "testthreadio";
         let (tx, rx) = sync::mpsc::channel();
@@ -578,7 +578,7 @@ mod tests {
     }
 
     #[test]
-    // #[ignore] // CI gets 0 as stats.
+    #[ignore] // CI gets 0 as stats.
     fn test_thread_io_statistics() {
         let s1 = "testio123";
         let s2 = "test45678";
