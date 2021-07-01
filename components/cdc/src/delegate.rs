a7803fd1e92c1b0b4b2a40e14c23918da853bdc9
ben1009 <liuhe1009@gmail.com>
2021-07-01 10:52:41 +0800

    change Copyright to 2021, fix DOC
    Signed-off-by: ben1009 <liuhe1009@gmail.com>

--- components/cdc/src/delegate.rs
+++ components/cdc/src/delegate.rs
@@ -1,4 +1,4 @@
-// Copyright 2020 TiKV Project Authors. Licensed under Apache-2.0.
+// Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
 use std::mem;
 use std::sync::atomic::{AtomicUsize, Ordering};
