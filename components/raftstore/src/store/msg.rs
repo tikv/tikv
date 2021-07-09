640655aaadcca6cfe4ba2ab8de191de20a51b3b4
gengliqi <gengliqiii@gmail.com>
2021-07-09 10:57:15 +0800

    make format
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- components/raftstore/src/store/msg.rs
+++ components/raftstore/src/store/msg.rs
@@ -390,7 +390,9 @@ pub enum PeerMsg<E: KvEngine> {
     /// that the raft node will not work anymore.
     Tick(PeerTicks),
     /// Result of applying committed entries. The message can't be lost.
-    ApplyRes { res: ApplyTaskRes },
+    ApplyRes {
+        res: ApplyTaskRes,
+    },
     /// Message that can't be lost but rarely created. If they are lost, real bad
     /// things happen like some peers will be considered dead in the group.
     SignificantMsg(SignificantMsg),
