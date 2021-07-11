1be1cad0b72b6149ff86df51e403f17381a4a424
lhy1024 <admin@liudos.us>
2021-07-11 19:55:12 +0800

    fix lint
    Signed-off-by: lhy1024 <admin@liudos.us>

--- components/test_raftstore/src/server.rs
+++ components/test_raftstore/src/server.rs
@@ -441,8 +441,7 @@ impl Simulator for ServerCluster {
         let split_check_runner =
             SplitCheckRunner::new(engines.kv.clone(), router.clone(), coprocessor_host.clone());
         let split_check_scheduler = bg_worker.start("split-check", split_check_runner);
-        let split_config_manager =
-            SplitConfigManager(Arc::new(VersionTrack::new(cfg.split)));
+        let split_config_manager = SplitConfigManager(Arc::new(VersionTrack::new(cfg.split)));
         let auto_split_controller = AutoSplitController::new(split_config_manager);
         node.start(
             engines,
