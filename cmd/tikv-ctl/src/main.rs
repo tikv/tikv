bbfa087ef434e760e4dfbc7b30fb8669c13129c3
Connor <zbk602423539@gmail.com>
2021-07-19 22:34:28 +0800

    fix arg
    Signed-off-by: Connor <zbk602423539@gmail.com>

--- cmd/tikv-ctl/src/main.rs
+++ cmd/tikv-ctl/src/main.rs
@@ -1597,6 +1597,8 @@ fn main() {
                         )
                         .arg(
                             Arg::with_name("promote-learner")
+                                .long("promote-learner")
+                                .takes_value(false)
                                 .required(false)
                                 .help("Promote learner to voter"),
                         )
