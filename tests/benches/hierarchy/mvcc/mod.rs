a7d836c6178739ff9d2c027af5d239cbe4524771
Yilin Chen <sticnarf@gmail.com>
2021-07-22 20:06:14 +0800

    storage: prevent repeated non-pessimistic lock prewrite from writing locks (#10600)
    * storage: prevent repeated non-pessimistic lock prewrite from writing locks
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * only check in retry requests
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * print some more information in logs
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * update kvproto
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * clear txn modifies after confirming the transaction has been committed
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * update kvproto
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * fix build
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * fix build
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * update kvproto
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * fix unit test
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    * clear modifies after detecting repeated index key prewrite
    
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- tests/benches/hierarchy/mvcc/mod.rs
+++ tests/benches/hierarchy/mvcc/mod.rs
@@ -45,6 +45,7 @@ where
             lock_ttl: 0,
             min_commit_ts: TimeStamp::default(),
             need_old_value: false,
+            is_retry_request: false,
         };
         prewrite(
             &mut txn,
@@ -93,6 +94,7 @@ fn mvcc_prewrite<E: Engine, F: EngineFactory<E>>(b: &mut Bencher, config: &Bench
                     lock_ttl: 0,
                     min_commit_ts: TimeStamp::default(),
                     need_old_value: false,
+                    is_retry_request: false,
                 };
                 prewrite(&mut txn, &mut reader, &txn_props, mutation, &None, false).unwrap();
             }
