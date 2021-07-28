202e977c03b17889d16a083614abaaef0e68860d
crazycs <crazycs520@gmail.com>
2021-07-28 14:06:15 +0800

    init
    Signed-off-by: crazycs <crazycs520@gmail.com>

--- components/resource_metering/src/reporter.rs
+++ components/resource_metering/src/reporter.rs
@@ -72,7 +72,7 @@ pub struct ResourceMeteringReporter {
 
 impl ResourceMeteringReporter {
     pub fn new(config: Config, scheduler: Scheduler<Task>, env: Arc<Environment>) -> Self {
-        Self {
+        let mut reporter = Self {
             config,
             env,
             scheduler,
@@ -82,15 +82,19 @@ impl ResourceMeteringReporter {
             records: HashMap::default(),
             others: HashMap::default(),
             find_top_k: Vec::default(),
+        };
+        if reporter.config.should_report() {
+            reporter.init_client();
         }
+        reporter
     }
 
-    pub fn init_client(&mut self, addr: &str) {
+    pub fn init_client(&mut self) {
         let channel = {
             let cb = ChannelBuilder::new(self.env.clone())
                 .keepalive_time(Duration::from_secs(10))
                 .keepalive_timeout(Duration::from_secs(3));
-            cb.connect(addr)
+            cb.connect(&self.config.agent_address)
         };
         self.client = Some(ResourceUsageAgentClient::new(channel));
         if self.cpu_records_collector.is_none() {
@@ -110,13 +114,13 @@ impl Runnable for ResourceMeteringReporter {
                 if !new_config.should_report() {
                     self.client.take();
                     self.cpu_records_collector.take();
+                    self.config = new_config;
                 } else if new_config.agent_address != self.config.agent_address
                     || new_config.enabled != self.config.enabled
                 {
-                    self.init_client(&new_config.agent_address);
+                    self.config = new_config;
+                    self.init_client();
                 }
-
-                self.config = new_config;
             }
             Task::CpuRecords(records) => {
                 let timestamp_secs = records.begin_unix_time_secs;
