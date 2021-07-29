36dea0a19f9af5081dc1cad8fb64b0b4e64b791c
kennytm <kennytm@gmail.com>
2021-07-29 15:50:17 +0800

    cherry pick #10577 to release-5.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- components/server/src/server.rs
+++ components/server/src/server.rs
@@ -27,10 +27,14 @@ use std::{
 use cdc::MemoryQuota;
 use concurrency_manager::ConcurrencyManager;
 use encryption_export::{data_key_manager_from_config, DataKeyManager};
+<<<<<<< HEAD
 use engine_rocks::{
     encryption::get_env as get_encrypted_env, file_system::get_env as get_inspected_env,
     RocksEngine,
 };
+=======
+use engine_rocks::{from_rocks_compression_type, get_env, RocksEngine};
+>>>>>>> 3f0c72a38... sst_importer: always use the bottommost compression (zstd) when writing SSTs (#10577)
 use engine_traits::{
     compaction_job::CompactionJobInfo, CFOptionsExt, ColumnFamilyOptions, Engines, MiscExt,
     RaftEngine, CF_DEFAULT, CF_LOCK, CF_WRITE,
@@ -714,6 +718,7 @@ impl<ER: RaftEngine> TiKVServer<ER> {
         );
 
         let import_path = self.store_path.join("import");
+<<<<<<< HEAD
         let importer = Arc::new(
             SSTImporter::new(
                 &self.config.import,
@@ -722,6 +727,28 @@ impl<ER: RaftEngine> TiKVServer<ER> {
             )
             .unwrap(),
         );
+=======
+        let mut importer = SSTImporter::new(
+            &self.config.import,
+            import_path,
+            self.encryption_key_manager.clone(),
+            self.config.storage.enable_ttl,
+        )
+        .unwrap();
+        for (cf_name, compression_type) in &[
+            (
+                CF_DEFAULT,
+                self.config.rocksdb.defaultcf.bottommost_level_compression,
+            ),
+            (
+                CF_WRITE,
+                self.config.rocksdb.writecf.bottommost_level_compression,
+            ),
+        ] {
+            importer.set_compression_type(cf_name, from_rocks_compression_type(*compression_type));
+        }
+        let importer = Arc::new(importer);
+>>>>>>> 3f0c72a38... sst_importer: always use the bottommost compression (zstd) when writing SSTs (#10577)
 
         let split_check_runner = SplitCheckRunner::new(
             engines.engines.kv.clone(),
