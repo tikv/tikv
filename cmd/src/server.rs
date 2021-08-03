47dbff04be5a7dbce1184c2623349c0fd83b2dce
kennytm <kennytm@gmail.com>
2021-08-04 04:54:29 +0800

    cmd: fix merge conflict
    Signed-off-by: kennytm <kennytm@gmail.com>

--- cmd/src/server.rs
+++ cmd/src/server.rs
@@ -22,16 +22,9 @@ use std::{
 use cdc::MemoryQuota;
 use concurrency_manager::ConcurrencyManager;
 use encryption_export::{data_key_manager_from_config, DataKeyManager};
-<<<<<<< HEAD:cmd/src/server.rs
 use engine_rocks::{
     encryption::get_env as get_encrypted_env, file_system::get_env as get_inspected_env,
-    RocksEngine,
-=======
-use engine_rocks::{from_rocks_compression_type, get_env, RocksEngine};
-use engine_traits::{
-    compaction_job::CompactionJobInfo, CFOptionsExt, ColumnFamilyOptions, Engines, MiscExt,
-    RaftEngine, CF_DEFAULT, CF_LOCK, CF_WRITE,
->>>>>>> 3f0c72a38... sst_importer: always use the bottommost compression (zstd) when writing SSTs (#10577):components/server/src/server.rs
+    from_rocks_compression_type, RocksEngine,
 };
 use engine_traits::{compaction_job::CompactionJobInfo, Engines, RaftEngine, CF_DEFAULT, CF_WRITE};
 use error_code::ErrorCodeExt;
@@ -644,21 +637,10 @@ impl<ER: RaftEngine> TiKVServer<ER> {
         .unwrap_or_else(|e| fatal!("failed to create server: {}", e));
 
         let import_path = self.store_path.join("import");
-<<<<<<< HEAD:cmd/src/server.rs
-        let importer = Arc::new(
-            SSTImporter::new(
-                &self.config.import,
-                import_path,
-                self.encryption_key_manager.clone(),
-            )
-            .unwrap(),
-        );
-=======
         let mut importer = SSTImporter::new(
             &self.config.import,
             import_path,
             self.encryption_key_manager.clone(),
-            self.config.storage.enable_ttl,
         )
         .unwrap();
         for (cf_name, compression_type) in &[
@@ -674,7 +656,6 @@ impl<ER: RaftEngine> TiKVServer<ER> {
             importer.set_compression_type(cf_name, from_rocks_compression_type(*compression_type));
         }
         let importer = Arc::new(importer);
->>>>>>> 3f0c72a38... sst_importer: always use the bottommost compression (zstd) when writing SSTs (#10577):components/server/src/server.rs
 
         let split_check_runner = SplitCheckRunner::new(
             engines.engines.kv.clone(),
