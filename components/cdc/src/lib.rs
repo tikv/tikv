740e43a45719a1bcdf89a8c48004f0945c19ad0e
Neil Shen <overvenus@gmail.com>
2021-08-06 15:01:28 +0800

    cdc: add cdc event size bench
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/cdc/src/lib.rs
+++ components/cdc/src/lib.rs
@@ -16,4 +16,4 @@ pub use channel::{recv_timeout, MemoryQuota};
 pub use endpoint::{CdcTxnExtraScheduler, Endpoint, Task, Validate};
 pub use errors::{Error, Result};
 pub use observer::CdcObserver;
-pub use service::{FeatureGate, Service};
+pub use service::{CdcEvent, FeatureGate, Service};
