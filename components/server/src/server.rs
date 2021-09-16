2a460f2d5af659b10f424562722bd736a0e5df1e
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2021-09-16 18:04:43 +0800

    cdc: Support changing CDC configs dynamically (#10651) (#10686)
    * cdc: Support changing CDC configs dynamically (#10651)
    
    * cdc: Support changing CDC configs dynamically
    
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>
    
    * Fix resize
    
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>
    
    * Fix import error
    
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>
    
    * reslove conflict
    
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>
    
    * resovle conflict
    
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>
    
    * Refine order
    
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>
    
    * Set min_ts_interval
    
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>
    
    * Fmt code
    
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>
    
    Co-authored-by: 二手掉包工程师 <rustin.liu@gmail.com>
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- components/server/src/server.rs
+++ components/server/src/server.rs
@@ -24,7 +24,7 @@ use std::{
     time::Duration,
 };
 
-use cdc::MemoryQuota;
+use cdc::{CdcConfigManager, MemoryQuota};
 use concurrency_manager::ConcurrencyManager;
 use encryption_export::{data_key_manager_from_config, DataKeyManager};
 use engine_rocks::{
@@ -637,10 +637,14 @@ impl<ER: RaftEngine> TiKVServer<ER> {
             cop_read_pools.handle()
         };
 
-        // Register cdc
+        // Register cdc.
         let cdc_ob = cdc::CdcObserver::new(cdc_scheduler.clone());
         cdc_ob.register_to(self.coprocessor_host.as_mut().unwrap());
-        // TODO: register a cdc config manager here to support dynamically change cdc config
+        // Register cdc config.
+        cfg_controller.register(
+            tikv::config::Module::CDC,
+            Box::new(CdcConfigManager(cdc_worker.scheduler())),
+        );
 
         // Create resolved ts worker
         let rts_worker = if self.config.resolved_ts.enable {
