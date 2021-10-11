ab73649b987afad08c597a0e88ae0b86071282e9
joccau <zak.zhao@pingcap.com>
2021-10-12 01:01:41 +0800

    Deal the all cases in match expression
    Signed-off-by: joccau <zak.zhao@pingcap.com>

--- components/external_storage/export/src/export.rs
+++ components/external_storage/export/src/export.rs
@@ -173,6 +173,7 @@ fn create_backend_inner(backend: &Backend) -> io::Result<Box<dyn ExternalStorage
                 return Err(bad_backend(Backend::CloudDynamic(dyn_backend.clone())));
             }
         },
+        Backend::Hdfs(_) => return Err(bad_backend(backend.clone())),
         #[cfg(not(any(feature = "cloud-gcp", feature = "cloud-aws")))]
         _ => return Err(bad_backend(backend.clone())),
     };
