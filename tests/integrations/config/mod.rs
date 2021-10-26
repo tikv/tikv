65f229a787d855fef61aba46d256b204f710f739
gengliqi <gengliqiii@gmail.com>
2021-10-26 19:09:17 +0800

    fix compile error ref #10540
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- tests/integrations/config/mod.rs
+++ tests/integrations/config/mod.rs
@@ -222,6 +222,8 @@ fn test_serde_custom_tikv_config() {
         io_reschedule_concurrent_max_count: 1234,
         io_reschedule_hotpot_duration: ReadableDuration::secs(4321),
         inspect_interval: ReadableDuration::millis(444),
+        store_events_flush_interval: ReadableDuration::millis(1234),
+        raft_msg_flush_interval_us: 2333,
     };
     value.pd = PdConfig::new(vec!["example.com:443".to_owned()]);
     let titan_cf_config = TitanCfConfig {
