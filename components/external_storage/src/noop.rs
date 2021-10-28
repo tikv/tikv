20c8a968e6717ff77d5d98b623cca6f227b81db2
yujuncen <yujuncen@pingcap.com>
2021-10-28 14:36:21 +0800

    make clippy happy
    Signed-off-by: yujuncen <yujuncen@pingcap.com>

--- components/external_storage/src/noop.rs
+++ components/external_storage/src/noop.rs
@@ -4,7 +4,6 @@ use std::marker::Unpin;
 use tokio::io;
 
 use async_trait::async_trait;
-use futures_executor::block_on;
 use futures_io::AsyncRead;
 
 use tokio_util::compat::{FuturesAsyncReadCompatExt, TokioAsyncReadCompatExt};
