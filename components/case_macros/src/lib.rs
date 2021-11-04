3137313ae462f6c8572edec8fb5841c46f21150e
yuqi1129 <yuqi4733@gmail.com>
2021-11-04 11:40:14 +0800

    fix disussion again
    Signed-off-by: yuqi1129 <yuqi4733@gmail.com>

--- components/case_macros/src/lib.rs
+++ components/case_macros/src/lib.rs
@@ -3,18 +3,7 @@
 //! Procedural macros used for case conversion.
 extern crate proc_macro;
 
-use proc_macro::{Group, Ident, Literal, TokenStream, TokenTree};
-
-#[inline]
-fn transform_non_keyword_ident_to_string_liternal<Transform>(
-    ident: &Ident,
-    transform: Transform,
-) -> Literal
-where
-    Transform: FnOnce(String) -> String,
-{
-    Literal::string(&transform(ident.to_string()))
-}
+use proc_macro::{Group, Literal, TokenStream, TokenTree};
 
 macro_rules! transform_idents_in_stream_to_string {
     ($stream:ident, $transform:expr) => {
@@ -22,7 +11,7 @@ macro_rules! transform_idents_in_stream_to_string {
             .into_iter()
             .map(|token_tree| match token_tree {
                 TokenTree::Ident(ref ident) => {
-                    transform_non_keyword_ident_to_string_liternal(ident, $transform).into()
+                    Literal::string(&$transform(ident.to_string())).into()
                 }
                 // find all idents in `TokenGroup` apply and reconstruct the group
                 TokenTree::Group(ref group) => TokenTree::Group(Group::new(
@@ -32,8 +21,7 @@ macro_rules! transform_idents_in_stream_to_string {
                         .into_iter()
                         .map(|group_token_tree| {
                             if let TokenTree::Ident(ref ident) = group_token_tree {
-                                transform_non_keyword_ident_to_string_liternal(ident, $transform)
-                                    .into()
+                                Literal::string(&$transform(ident.to_string())).into()
                             } else {
                                 group_token_tree
                             }
@@ -46,20 +34,13 @@ macro_rules! transform_idents_in_stream_to_string {
     };
 }
 
-fn to_kebab(enum_name: &str) -> String {
-    let mut kebab = String::new();
-    for (i, ch) in enum_name.char_indices() {
-        if i > 0 && ch.is_uppercase() {
-            kebab.push('_');
-        }
-        kebab.push(ch.to_ascii_lowercase());
-    }
-    kebab.replace('_', "-")
+fn to_kebab(s: &str) -> String {
+    to_snake(s).replace('_', "-")
 }
 
-fn to_snake(enum_name: &str) -> String {
+fn to_snake(s: &str) -> String {
     let mut snake = String::new();
-    for (i, ch) in enum_name.char_indices() {
+    for (i, ch) in s.char_indices() {
         if i > 0 && ch.is_uppercase() {
             snake.push('_');
         }
