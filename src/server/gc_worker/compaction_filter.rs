046398d897985f6dd750694799337ab206041c74
Connor <zbk602423539@gmail.com>
2021-11-19 14:29:47 +0800

    cherry pick #11416 to release-5.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- src/server/gc_worker/compaction_filter.rs
+++ src/server/gc_worker/compaction_filter.rs
@@ -22,7 +22,12 @@ use file_system::{IOType, WithIOType};
 use pd_client::{Feature, FeatureGate};
 use prometheus::{local::*, *};
 use raftstore::coprocessor::RegionInfoProvider;
+<<<<<<< HEAD
 use tikv_util::{time::Instant, worker::FutureScheduler};
+=======
+use tikv_util::time::Instant;
+use tikv_util::worker::{ScheduleError, Scheduler};
+>>>>>>> dce2dc811... gc: Fix GC scan effectiveness to avoid OOM (#11416)
 use txn_types::{Key, TimeStamp, WriteRef, WriteType};
 
 use crate::server::gc_worker::{GcConfig, GcTask, GcWorkerConfigManager};
@@ -63,9 +68,10 @@ lazy_static! {
     )
     .unwrap();
     // A counter for errors met by `WriteCompactionFilter`.
-    static ref GC_COMPACTION_FAILURE: IntCounter = register_int_counter!(
+    static ref GC_COMPACTION_FAILURE: IntCounterVec = register_int_counter_vec!(
         "tikv_gc_compaction_failure",
-        "Compaction filter meets failure"
+        "Compaction filter meets failure",
+        &["type"]
     )
     .unwrap();
     // A counter for skip performing GC in compactions.
@@ -306,10 +312,28 @@ impl WriteCompactionFilter {
 
     // `log_on_error` indicates whether to print an error log on scheduling failures.
     // It's only enabled for `GcTask::OrphanVersions`.
+<<<<<<< HEAD
     fn schedule_gc_task(&self, task: GcTask, log_on_error: bool) {
         if let Err(e) = self.gc_scheduler.schedule(task) {
             if log_on_error {
                 error!("compaction filter schedule {} fail", e.0);
+=======
+    fn schedule_gc_task(&self, task: GcTask<RocksEngine>, log_on_error: bool) {
+        match self.gc_scheduler.schedule(task) {
+            Ok(_) => {}
+            Err(e) => {
+                if log_on_error {
+                    error!("compaction filter schedule {} fail", e);
+                }
+                match e {
+                    ScheduleError::Full(_) => {
+                        GC_COMPACTION_FAILURE.with_label_values(&["full"]).inc();
+                    }
+                    ScheduleError::Stopped(_) => {
+                        GC_COMPACTION_FAILURE.with_label_values(&["stopped"]).inc();
+                    }
+                }
+>>>>>>> dce2dc811... gc: Fix GC scan effectiveness to avoid OOM (#11416)
             }
         }
     }
@@ -558,7 +582,7 @@ impl CompactionFilter for WriteCompactionFilter {
             Ok(decision) => decision,
             Err(e) => {
                 warn!("compaction filter meet error: {}", e);
-                GC_COMPACTION_FAILURE.inc();
+                GC_COMPACTION_FAILURE.with_label_values(&["filter"]).inc();
                 self.encountered_errors = true;
                 CompactionFilterDecision::Keep
             }
