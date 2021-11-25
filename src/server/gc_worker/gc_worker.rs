15b82170038ac362bd994eaafc7701952cb76340
Connor1996 <zbk602423539@gmail.com>
2021-11-25 19:29:05 +0800

    fix build
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- src/server/gc_worker/gc_worker.rs
+++ src/server/gc_worker/gc_worker.rs
@@ -1475,12 +1475,10 @@ mod tests {
         let engine = TestEngineBuilder::new().build().unwrap();
         let prefixed_engine = PrefixedEngine(engine.clone());
 
-        let (tx, _rx) = mpsc::channel();
         let cfg = GcConfig::default();
         let mut runner = GcRunner::new(
             prefixed_engine.clone(),
             RaftStoreBlackHole,
-            tx,
             GcWorkerConfigManager(Arc::new(VersionTrack::new(cfg.clone())))
                 .0
                 .tracker("gc-woker".to_owned()),
