bdcd7322bb57754cb5f969bc74250b6cf13641e9
5kbpers <tangminghua@pingcap.com>
2021-12-09 15:45:20 +0800

    renew leader lease after not hibernate
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- components/raftstore/src/store/fsm/peer.rs
+++ components/raftstore/src/store/fsm/peer.rs
@@ -1383,32 +1383,6 @@ where
             self.fsm.has_ready = true;
         }
 
-        // If lease expired, we will send a noop read index to renew lease.
-        if self.fsm.peer.is_leader() {
-            let current_time = *self.ctx.current_time.get_or_insert_with(monotonic_raw_now);
-            let next_heartbeat = current_time
-                + self.ctx.cfg.raft_base_tick_interval() * self.ctx.cfg.raft_heartbeat_ticks as i32;
-            // We need to propose a read index request if current lease can't cover till next tick
-            let need_propose = match self.fsm.peer.leader_lease.inspect(Some(next_heartbeat)) {
-                LeaseState::Expired => {
-                    let max_lease = self.ctx.cfg.raft_store_max_leader_lease();
-                    self.fsm.peer.pending_reads.back().map_or(true, |read| {
-                        // If there are read index whose lease can cover till next tick
-                        // then we don't need to propose a new one
-                        read.propose_time + max_lease < next_heartbeat
-                    })
-                }
-                _ => false,
-            };
-            if need_propose {
-                let (id, dropped) = self.fsm.peer.propose_read_index(None, None);
-                if !dropped {
-                    let read_proposal = ReadIndexRequest::noop(id, current_time);
-                    self.fsm.peer.pending_reads.push_back(read_proposal, true);
-                }
-            }
-        }
-
         self.fsm.peer.mut_store().flush_cache_metrics();
 
         // Keep ticking if there are still pending read requests or this node is within hibernate timeout.
@@ -1416,6 +1390,7 @@ where
             !self.fsm.peer.check_after_tick(self.fsm.hibernate_state.group_state(), res.unwrap()) ||
             (self.fsm.peer.is_leader() && !self.all_agree_to_hibernate())
         {
+            self.try_renew_leader_lease();
             self.register_raft_base_tick();
             // We need pd heartbeat tick to collect down peers and pending peers.
             self.register_pd_heartbeat_tick();
@@ -1424,6 +1399,7 @@ where
 
         // Keep ticking if there are disk full peers for the Region.
         if !self.fsm.peer.disk_full_peers.is_empty() {
+            self.try_renew_leader_lease();
             self.register_raft_base_tick();
             return;
         }
@@ -1489,6 +1465,34 @@ where
                         .get_mut(&region_id)
                         .unwrap();
                     *is_ready = true;
+                }
+            }
+        }
+    }
+
+    fn try_renew_leader_lease(&mut self) {
+        // If lease expired, we will send a noop read index to renew lease.
+        if self.fsm.peer.is_leader() {
+            let current_time = *self.ctx.current_time.get_or_insert_with(monotonic_raw_now);
+            let next_heartbeat = current_time
+                + self.ctx.cfg.raft_base_tick_interval() * self.ctx.cfg.raft_heartbeat_ticks as i32;
+            // We need to propose a read index request if current lease can't cover till next tick
+            let need_propose = match self.fsm.peer.leader_lease.inspect(Some(next_heartbeat)) {
+                LeaseState::Expired => {
+                    let max_lease = self.ctx.cfg.raft_store_max_leader_lease();
+                    self.fsm.peer.pending_reads.back().map_or(true, |read| {
+                        // If there are read index whose lease can cover till next heartbeat
+                        // then we don't need to propose a new one
+                        read.propose_time + max_lease < next_heartbeat
+                    })
+                }
+                _ => false,
+            };
+            if need_propose {
+                let (id, dropped) = self.fsm.peer.propose_read_index(None, None);
+                if !dropped {
+                    let read_proposal = ReadIndexRequest::noop(id, current_time);
+                    self.fsm.peer.pending_reads.push_back(read_proposal, true);
                 }
             }
         }
