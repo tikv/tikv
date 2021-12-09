ce9e96f803c556f1fa9ea337e112713ce91e8ac1
Zhenchi <zhongzc_arch@outlook.com>
2021-12-09 11:39:57 +0800

    rsmeter: tag factory instead of global variable (#11522)
    * rsmeter: tag factory instead of global variable
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * fix build fail
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    fix build failed
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    fix build failed
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    fix build failed
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * remove global collector chan
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * unstable tests
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * stablize tests
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * fix build failed
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * retry register
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * fix build failed
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * add comments
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * arcswap instead
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * rename tag_factory to resource_tag_factory
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * rename CollectorRegistry to CollectorRegHandle
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * mute CI check, close #11229
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>
    
    * rename
    
    Signed-off-by: Zhenchi <zhongzc_arch@outlook.com>

--- tests/integrations/resource_metering/mod.rs
+++ tests/integrations/resource_metering/mod.rs
@@ -2,27 +2,8 @@
 
 pub mod test_suite;
 
+#[cfg(target_os = "linux")]
 pub mod test_dynamic_config;
-pub mod test_receiver;
 
 #[cfg(target_os = "linux")]
-mod linux {
-    use super::*;
-
-    #[test]
-    #[ignore = "the case is too slow, ref #11229"]
-    fn test_resource_metering() {
-        let mut ts = test_suite::TestSuite::new();
-
-        // Dynamic config
-        test_dynamic_config::case_enable(&mut ts);
-        test_dynamic_config::case_report_interval(&mut ts);
-        test_dynamic_config::case_max_resource_groups(&mut ts);
-        test_dynamic_config::case_precision(&mut ts);
-
-        // Receiver
-        test_receiver::case_alter_receiver_addr(&mut ts);
-        test_receiver::case_receiver_blocking(&mut ts);
-        test_receiver::case_receiver_shutdown(&mut ts);
-    }
-}
+pub mod test_receiver;
