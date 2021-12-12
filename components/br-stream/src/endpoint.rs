4169b9d3bbf6da595d06b24ef3a56e01ffbff1f4
Yu Juncen <yu745514916@live.com>
2021-12-12 23:08:54 +0800

    br-stream: basic local temporary file logic
    Signed-off-by: Yu Juncen <yu745514916@live.com>

--- components/br-stream/src/endpoint.rs
+++ components/br-stream/src/endpoint.rs
@@ -10,7 +10,7 @@ use tokio_stream::StreamExt;
 
 use crate::metadata::store::{EtcdStore, MetaStore};
 use crate::metadata::{MetadataClient, MetadataEvent, Task as MetaTask};
-use crate::router::Router;
+use crate::router::{DataFileWithMeta, NewKv, Router, RouterInner};
 use crate::{errors::Result, observer::BackupStreamObserver};
 use kvproto::raft_cmdpb::{CmdType, Request};
 use online_config::ConfigChange;
@@ -54,7 +54,7 @@ impl Endpoint<EtcdStore> {
             }
         };
 
-        let range_router = Router::new();
+        let range_router = Router::new(PathBuf::from(config.streaming_path), scheduler.clone());
 
         if cli.is_none() {
             // unable to connect to etcd
@@ -142,98 +142,36 @@ where
         buf
     }
 
-    // TODO improve the bakcup file name
-    fn backup_file_name(
-        &self,
-        task_name: String,
-        table_id: u64,
-        region_id: u64,
-        cf: &str,
-        t: &str,
-    ) -> String {
-        format!("{}{}{}-{}-{}.log", task_name, table_id, region_id, cf, t)
-    }
-
-    // backup kv event to file.
-    fn backup_file(&self, t: CmdType, key: Vec<u8>, value: Vec<u8>, cf: String) {
-        if self.range_router.key_in_ranges(&key) {
-            // drop the key not in filter
-            return;
-        }
-        if let Some(task) = self.range_router.get_task_by_key(&key) {
-            let cmd_type = if t == CmdType::Put { "put" } else { "delete" };
-            let name = self.backup_file_name(task, 0, 0, &cf, cmd_type);
-            let file = PathBuf::from(self.config.streaming_path.clone()).join(name);
-            let mut file = OpenOptions::new()
-                .write(true)
-                .append(true)
-                .open(file)
-                .unwrap();
-            let bytes = self.encode_event(&key, &value);
-            if let Err(e) = file.write_all(&bytes) {
-                error!("backup stream write file failed"; "error" => ?e);
-            }
-        } else {
-            // TODO handle this error
-            error!("backup stream not found task by given key failed"; "key" => ?key);
-        }
-    }
-
-    fn backup_data(&mut self, requests: Vec<Request>) -> Result<()> {
-        for mut req in requests {
-            match req.get_cmd_type() {
-                CmdType::Put => {
-                    let mut put = req.take_put();
-                    self.backup_file(req.get_cmd_type(), put.take_key(), put.take_value(), put.cf);
-                }
-                CmdType::Delete => {
-                    let mut del = req.take_delete();
-                    self.backup_file(req.get_cmd_type(), del.take_key(), Vec::new(), del.cf);
-                }
-                _ => {
-                    debug!(
-                        "backup stream skip other command";
-                        "command" => ?req,
-                    );
-                }
-            };
-        }
-        Ok(())
-    }
-
-    fn backup_batch(&mut self, batch: CmdBatch) -> Result<()> {
+    fn backup_batch(&self, batch: CmdBatch) {
         let region_id = batch.region_id;
-        for cmd in batch.into_iter(region_id) {
-            let Cmd {
-                index: _,
-                request,
-                mut response,
-            } = cmd;
-            if response.get_header().has_error() {
-                let err_header = response.mut_header().take_error();
-                error!("backup stream parse batch cmd failed"; "error" => ?err_header);
-                // TODO find a proper way to handle all related error
+        let kvs = NewKv::from_cmd_batch(batch, /* TODO */ 0);
+        let router = self.range_router.clone();
+        self.pool.spawn(async move {
+            let mut router = router.lock().await;
+            for kv in kvs {
+                // TODO build a error handle mechanism #error 6
+                if let Err(err) = router.on_event(kv).await {
+                    error!("backup stream failed in backup batch"; "error" => ?err);
+                }
             }
-            if !request.has_admin_request() {
-                self.backup_data(request.requests.into())?;
-            } else {
-                error!("backup stream ignore amdin request for now");
-            }
-        }
-        Ok(())
+        });
     }
 
     // register task ranges
     pub fn on_register(&self, task: MetaTask) {
         if let Some(cli) = self.meta_client.as_ref() {
             let cli = cli.clone();
-            self.pool.block_on(async {
+            let range_router = self.range_router.clone();
+            self.pool.block_on(async move {
                 let task_name = task.info.get_name();
                 match cli.ranges_of_task(task_name).await {
                     Ok(ranges) => {
                         debug!("backup stream register ranges to observer");
                         // TODO implement register ranges
-                        self.range_router.register_ranges(task_name, ranges.inner);
+                        range_router
+                            .lock()
+                            .await
+                            .register_ranges(task_name, ranges.inner);
                     }
                     Err(e) => {
                         error!("backup stream get tasks failed"; "error" => ?e);
@@ -244,12 +182,29 @@ where
         };
     }
 
+    pub async fn do_flush(router: Router, task: String) -> Result<()> {
+        let mut temp_files = {
+            let mut router = router.lock().await;
+            router.take_temporary_files(&task)?
+        };
+        temp_files.flush().await?;
+        let meta = temp_files.generate_metadata()?;
+        // TODO flush the files to external storage
+        info!("flushing data to external storage"; "local_files" => ?meta);
+        Ok(())
+    }
+
+    pub fn on_flush(&self, task: String) {
+        let router = self.range_router.clone();
+        self.pool.spawn(async move {
+            // TODO handle the error
+            let _ = Self::do_flush(router, task).await;
+        });
+    }
+
     pub fn do_backup(&mut self, events: Vec<CmdBatch>) {
         for batch in events {
-            if let Err(e) = self.backup_batch(batch) {
-                // TODO build a error handle mechanism #error 6
-                error!("backup stream failed in backup batch"; "error" => ?e);
-            }
+            self.backup_batch(batch)
         }
     }
 }
@@ -275,6 +230,8 @@ pub enum Task {
     WatchTask(MetaTask),
     BatchEvent(Vec<CmdBatch>),
     ChangeConfig(ConfigChange),
+    /// Flush the task with name.
+    Flush(String),
 }
 
 impl fmt::Debug for Task {
@@ -292,6 +249,7 @@ impl fmt::Debug for Task {
                 .field("name", &"change_config")
                 .field("change", change)
                 .finish(),
+            _ => todo!(),
         }
     }
 }
@@ -313,6 +271,7 @@ where
         match task {
             Task::WatchTask(task) => self.on_register(task),
             Task::BatchEvent(events) => self.do_backup(events),
+            Task::Flush(task) => self.on_flush(task),
             _ => (),
         }
     }
--- components/br-stream/src/router.rs
+++ components/br-stream/src/router.rs
@@ -1,48 +1,147 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 use std::{
-    cell::Cell,
     collections::{BTreeMap, HashMap},
     path::{Path, PathBuf},
+    sync::{Arc, Mutex as StdMutex},
 };
 
-use crate::metadata::store::EtcdStore;
+use crate::{endpoint::Task, errors::Error, metadata::store::EtcdStore};
 
 use super::errors::Result;
-use kvproto::raft_cmdpb::CmdType;
+use file_system::Sha256Reader;
+use kvproto::{
+    brpb::LogFile,
+    raft_cmdpb::{CmdType, RaftCmdRequest, Request},
+};
+use openssl::hash::{Hasher, MessageDigest};
+use raftstore::coprocessor::{Cmd, CmdBatch};
+use slog_global::debug;
 use tidb_query_datatype::codec::table::decode_table_id;
+use tikv::storage::kv::BTreeEngine;
+use tikv_util::{
+    box_err,
+    config::{ReadableSize, MIB},
+    worker::Scheduler,
+};
+use tokio::sync::Mutex;
 use txn_types::{Key, TimeStamp};
 // TODO: maybe replace it with tokio fs.
-use std::fs::File;
-use std::io::Write;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+
+impl NewKv {
+    pub fn from_cmd_batch(mut cmd: CmdBatch, resolved_ts: u64) -> Vec<Self> {
+        let region_id = cmd.region_id;
+        let mut result = vec![];
+        for req in cmd
+            .cmds
+            .into_iter()
+            .filter(|cmd| {
+                if cmd.response.get_header().has_error() {
+                    // Add some log for skipping the error.
+                    false
+                } else if cmd.request.has_admin_request() {
+                    // Add some log for skipping the admin request.
+                    false
+                } else {
+                    true
+                }
+            })
+            .flat_map(|cmd| cmd.request.take_requests().into_iter())
+        {
+            let (key, value, cf) = match req.get_cmd_type() {
+                CmdType::Put => {
+                    let mut put = req.take_put();
+                    (put.take_key(), put.take_value(), put.cf)
+                }
+                CmdType::Delete => {
+                    let mut del = req.take_delete();
+                    (del.take_key(), Vec::new(), del.cf)
+                }
+                _ => {
+                    debug!(
+                        "backup stream skip other command";
+                        "command" => ?req,
+                    );
+                    continue;
+                }
+            };
+            result.push(Self {
+                key,
+                value,
+                cf,
+                region_id,
+                region_resolved_ts: resolved_ts,
+                cmd_type: req.get_cmd_type(),
+            })
+        }
+        result
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Router(Arc<Mutex<RouterInner>>);
+
+impl Router {
+    pub fn new(prefix: PathBuf, scheduler: Scheduler<Task>) -> Self {
+        Self(Arc::new(Mutex::new(RouterInner::new(prefix, scheduler))))
+    }
+}
+
+impl std::ops::Deref for Router {
+    type Target = Mutex<RouterInner>;
+
+    fn deref(&self) -> &Self::Target {
+        Arc::deref(&self.0)
+    }
+}
 
 /// An Router for Backup Stream.
 ///
 /// It works as a table-filter.
 ///   1. route the kv event to different task
 ///   2. filter the kv event not belong to the task
-
 // TODO maybe we should introduce table key from tidb_query_datatype module.
-#[derive(Debug, Default)]
-pub struct Router {
+pub struct RouterInner {
     // TODO find a proper way to record the ranges of table_filter.
     ranges: BTreeMap<KeyRange, TaskRange>,
-    tables: BTreeMap<String, TableRouter>,
+    // TODO replace all map like things with lock free map, to get rid of the Mutex.
+    temp_files_of_task: HashMap<String, TemporaryFiles>,
     prefix: PathBuf,
+
+    scheduler: Scheduler<Task>,
+    /// The size limit of temporary file per task.
+    temp_file_size_limit: u64,
+}
+
+impl std::fmt::Debug for RouterInner {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("RouterInner")
+            .field("ranges", &self.ranges)
+            .field("temp_files_of_task", &self.temp_files_of_task)
+            .field("prefix", &self.prefix)
+            .finish()
+    }
 }
 
 pub struct NewKv {
     key: Vec<u8>,
     value: Vec<u8>,
-    cf: &'static str,
-    region_id: i64,
+    cf: String,
+    region_id: u64,
     region_resolved_ts: u64,
     cmd_type: CmdType,
 }
 
-impl Router {
-    pub fn new() -> Self {
-        Router {
-            ..Default::default()
+impl RouterInner {
+    pub fn new(prefix: PathBuf, scheduler: Scheduler<Task>) -> Self {
+        RouterInner {
+            ranges: BTreeMap::default(),
+            temp_files_of_task: HashMap::default(),
+            prefix,
+            scheduler,
+            // Maybe make it configurable?
+            temp_file_size_limit: 128 * MIB,
         }
     }
     // keep ranges in memory to filter kv events not in these ranges.
@@ -86,26 +185,52 @@ impl Router {
             )
     }
 
-    pub fn on_event(&mut self, kv: NewKv) -> Result<()> {
+    pub async fn on_event(&mut self, kv: NewKv) -> Result<()> {
+        let prefix = &self.prefix;
         if let Some(task) = self.get_task_by_key(&kv.key) {
             let inner_router = self
-                .tables
-                .entry(task)
-                .or_insert_with(|| TableRouter::new(self.prefix.join(&task)));
-            inner_router.on_event(kv)?
+                .temp_files_of_task
+                .entry(task.clone())
+                .or_insert_with(|| TemporaryFiles::new(prefix.join(&task)));
+            let prev_size = inner_router.total_size();
+            inner_router.on_event(kv).await?;
+
+            // When this event make the size of temporary files exceeds the size limit, make a flush.
+            // Note that we only flush if the size is less than the limit before the event,
+            // or we may send multiplied flush requests.
+            if prev_size < self.temp_file_size_limit
+                && inner_router.total_size() > self.temp_file_size_limit
+            {
+                self.scheduler.schedule(Task::Flush(task));
+            }
         }
         Ok(())
+    }
+
+    pub fn take_temporary_files(&mut self, task: &str) -> Result<TemporaryFiles> {
+        self.temp_files_of_task.remove(task).map_or(
+            Err(Error::NoSuchTask {
+                task_name: task.to_owned(),
+            }),
+            |data| Ok(data),
+        )
+    }
+
+    pub fn flush_task(&mut self, task: &str) -> Result<Vec<DataFileWithMeta>> {
+        let collector = self.take_temporary_files(task)?;
+        Ok(collector.generate_metadata()?)
     }
 }
 
 #[derive(Default, Debug)]
-struct TableRouter {
+struct TemporaryFiles {
     temp_dir: PathBuf,
     // (table_id, region_id)
-    files: HashMap<(i64, i64), DataFile>,
+    files: HashMap<(i64, u64), DataFile>,
+    total_size: usize,
 }
 
-impl TableRouter {
+impl TemporaryFiles {
     pub fn new(temp_dir: PathBuf) -> Self {
         Self {
             temp_dir,
@@ -113,58 +238,145 @@ impl TableRouter {
         }
     }
 
-    pub fn on_event(&mut self, kv: NewKv) -> Result<()> {
+    // TODO: make a file-level lock for git rid of the &mut.
+    pub async fn on_event(&mut self, kv: NewKv) -> Result<()> {
         let table_id = decode_table_id(&kv.key).expect("TableRouter: Not table key");
         if !self.files.contains_key(&(table_id, kv.region_id)) {
             let path = self
                 .temp_dir
                 .join(format!("{:08}_{:08}.temp.log", table_id, kv.region_id));
-            let file = File::create(path)?;
-            let data_file = DataFile::new(file, kv.region_id);
+            let data_file = DataFile::new(path, kv.region_id).await?;
             self.files.insert((table_id, kv.region_id), data_file);
         }
-        self.files
-            .get(&(table_id, kv.region_id))
+        self.total_size += self
+            .files
+            .get_mut(&(table_id, kv.region_id))
             .unwrap()
-            .on_event(kv)?;
+            .on_event(kv)
+            .await?;
         Ok(())
+    }
+
+    /// Return the name with path prefix.
+    fn path_to_log_file(table_id: i64, min_ts: i64) -> String {
+        format!(
+            "/v1/t{:12}/{:12}-{}.log",
+            table_id,
+            min_ts,
+            uuid::Uuid::new_v4()
+        )
+    }
+
+    pub fn total_size(&self) -> u64 {
+        self.total_size as _
+    }
+
+    /// Flush all temporary files.
+    pub async fn flush(&mut self) -> Result<()> {
+        futures::future::join_all(self.files.values_mut().map(|f| f.inner.flush()))
+            .await
+            .into_iter()
+            .map(|r| r.map_err(Error::from))
+            .fold(Ok(()), Result::and)
+    }
+
+    pub fn generate_metadata(self) -> Result<Vec<DataFileWithMeta>> {
+        let mut result = Vec::with_capacity(self.files.len());
+        for ((table_id, region_id), data_file) in self.files.into_iter() {
+            let mut file_meta = data_file.generate_metadata()?;
+            file_meta
+                .meta
+                .set_path(Self::path_to_log_file(table_id, file_meta.meta.min_ts));
+            result.push(file_meta)
+        }
+        Ok(result)
     }
 }
 
 struct DataFile {
-    region: i64,
-    min_ts: Cell<TimeStamp>,
-    max_ts: Cell<TimeStamp>,
-    resolved_ts: Cell<TimeStamp>,
+    region: u64,
+    min_ts: TimeStamp,
+    max_ts: TimeStamp,
+    resolved_ts: TimeStamp,
+    sha256: Hasher,
     inner: File,
+    start_key: Vec<u8>,
+    end_key: Vec<u8>,
+    number_of_entries: usize,
+    local_path: PathBuf,
+}
+
+#[derive(Debug)]
+pub struct DataFileWithMeta {
+    pub meta: LogFile,
+    pub local_path: PathBuf,
 }
 
 impl DataFile {
-    fn new(file: File, region_id: i64) -> Self {
-        Self {
+    async fn new(local_path: impl AsRef<Path>, region_id: u64) -> Result<Self> {
+        let sha256 = Hasher::new(MessageDigest::sha256())
+            .map_err(|err| box_err!("openssl hasher failed to init: {}", err).into());
+        Ok(Self {
             region: region_id,
-            min_ts: Cell::new(TimeStamp::max()),
-            max_ts: Cell::new(TimeStamp::zero()),
-            resolved_ts: Cell::new(TimeStamp::zero()),
-            inner: file,
+            min_ts: TimeStamp::max(),
+            max_ts: TimeStamp::zero(),
+            resolved_ts: TimeStamp::zero(),
+            inner: File::create(local_path.as_ref()).await?,
+            sha256,
+            number_of_entries: 0,
+            start_key: vec![],
+            end_key: vec![],
+            local_path: local_path.as_ref().to_owned(),
+        })
+    }
+
+    /// Add a new KV pair to the file, returning its size.
+    async fn on_event(&mut self, mut kv: NewKv) -> Result<usize> {
+        let encoded = super::endpoint::Endpoint::<EtcdStore>::encode_event(&kv.key, &kv.value);
+        let size = encoded.len();
+        let key = Key::from_encoded(std::mem::take(&mut kv.key));
+        let ts = key.decode_ts().expect("key without ts");
+        self.min_ts = self.min_ts.min(ts);
+        self.max_ts = self.max_ts.max(ts);
+        self.resolved_ts = self.resolved_ts.max(kv.region_resolved_ts.into());
+        self.inner.write_all(encoded.as_slice()).await?;
+        self.sha256
+            .update(encoded.as_slice())
+            .map_err(|err| box_err!("openssl hasher failed to update: {}", err).into())?;
+        self.update_key_bound(key.into_encoded());
+        Ok(size)
+    }
+
+    fn update_key_bound(&mut self, key: Vec<u8>) {
+        if self.start_key.is_empty() && self.end_key.is_empty() {
+            self.start_key = key.clone();
+            self.end_key = key;
+        }
+        if self.start_key > key {
+            self.start_key = key;
+        } else if self.end_key < key {
+            self.end_key = key;
         }
     }
 
-    fn on_event(&self, mut kv: NewKv) -> Result<()> {
-        let encoded = super::endpoint::Endpoint::<EtcdStore>::encode_event(&kv.key, &kv.value);
-        let key = Key::from_encoded(std::mem::take(&mut kv.key));
-        let ts = key.decode_ts().expect("key without ts");
-        if ts < self.min_ts.get() {
-            self.min_ts.set(ts)
-        }
-        if ts > self.max_ts.get() {
-            self.max_ts.set(ts)
-        }
-        if kv.region_resolved_ts > self.resolved_ts.get().into_inner() {
-            self.resolved_ts.set(kv.region_resolved_ts.into())
-        }
-        self.inner.write(encoded.as_slice())?;
-        Ok(())
+    fn generate_metadata(mut self) -> Result<DataFileWithMeta> {
+        let mut meta = LogFile::new();
+        meta.set_sha_256(
+            self.sha256
+                .finish()
+                .map(|bytes| bytes.to_vec())
+                .map_err(|err| box_err!("openssl hasher failed to finish: {}", err).into())?,
+        );
+        meta.set_number_of_entries(self.number_of_entries as _);
+        meta.set_max_ts(self.max_ts.into_inner() as _);
+        meta.set_min_ts(self.min_ts.into_inner() as _);
+        meta.set_start_key(std::mem::take(&mut self.start_key));
+        meta.set_end_key(std::mem::take(&mut self.end_key));
+        let file_with_meta = DataFileWithMeta {
+            meta,
+            local_path: self.local_path,
+        };
+        Ok(file_with_meta)
     }
 }
 
@@ -175,6 +387,7 @@ impl std::fmt::Debug for DataFile {
             .field("min_ts", &self.min_ts)
             .field("max_ts", &self.max_ts)
             .field("resolved_ts", &self.resolved_ts)
+            .field("local_path", &self.local_path.display())
             .finish()
     }
 }
@@ -190,11 +403,14 @@ struct TaskRange {
 
 #[cfg(test)]
 mod tests {
+    use tikv_util::worker::dummy_scheduler;
+
     use super::*;
 
     #[test]
     fn test_register() {
-        let mut router = Router::new();
+        let (tx, _) = dummy_scheduler();
+        let mut router = RouterInner::new(PathBuf::new(), tx);
         // -----t1.start-----t1.end-----t2.start-----t2.end------
         // --|------------|----------|------------|-----------|--
         // case1        case2      case3        case4       case5
