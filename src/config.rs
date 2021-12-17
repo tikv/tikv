01b49171ac8a7f5fc18ce8d86534743164bc5ff5
3pointer <luancheng@pingcap.com>
2021-12-17 18:31:32 +0800

    hidden the new config
    Signed-off-by: 3pointer <luancheng@pingcap.com>

--- src/config.rs
+++ src/config.rs
@@ -2209,7 +2209,13 @@ pub struct BackupConfig {
     pub enable_auto_tune: bool,
     pub auto_tune_remain_threads: usize,
     pub auto_tune_refresh_interval: ReadableDuration,
+<<<<<<< HEAD
     pub io_thread_size: usize,
+=======
+    // Do not expose this config to user.
+    #[serde(skip_serializing)]
+    #[online_config(hidden)]
+>>>>>>> 6e558e337 (hidden the new config)
     pub s3_multi_part_size: ReadableSize,
     #[online_config(submodule)]
     pub hadoop: HadoopConfig,
