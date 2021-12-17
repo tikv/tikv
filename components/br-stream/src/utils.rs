43a81a447a17c54512f646b1befbbf6d76f9d9e6
yujuncen <yujuncen@pingcap.com>
2021-12-17 14:54:15 +0800

    br/stream: make clippy happy
    Signed-off-by: yujuncen <yujuncen@pingcap.com>

--- components/br-stream/src/utils.rs
+++ components/br-stream/src/utils.rs
@@ -2,7 +2,7 @@
 use txn_types::Key;
 
 /// wrap a user key with encoded data key.
-pub fn wrap_key(mut v: Vec<u8>) -> Vec<u8> {
+pub fn wrap_key(v: Vec<u8>) -> Vec<u8> {
     // TODO: encode in place.
     let key = Key::from_raw(v.as_slice()).into_encoded();
     key
