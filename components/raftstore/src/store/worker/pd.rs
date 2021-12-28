2dcc565a48b08e1977b5073b1876651d25a03662
disksing <i@disksing.com>
2021-12-28 12:18:31 +0800

    send state from global variable
    Signed-off-by: disksing <i@disksing.com>

--- components/raftstore/src/store/worker/pd.rs
+++ components/raftstore/src/store/worker/pd.rs
@@ -17,7 +17,7 @@ use kvproto::raft_cmdpb::{
     SplitRequest,
 };
 use kvproto::raft_serverpb::{PeerState, RaftMessage, RegionLocalState};
-use kvproto::replication_modepb::RegionReplicationStatus;
+use kvproto::replication_modepb::{RegionReplicationStatus, StoreDrAutoSyncStatus};
 use kvproto::{metapb, pdpb};
 use ordered_float::OrderedFloat;
 use prometheus::local::LocalHistogram;
@@ -136,6 +136,7 @@ where
         stats: pdpb::StoreStats,
         store_info: StoreInfo<EK, ER>,
         send_detailed_report: bool,
+        dr_autosync_status: Option<StoreDrAutoSyncStatus>,
     },
     ReportBatchSplit {
         regions: Vec<metapb::Region>,
@@ -923,6 +924,7 @@ where
         mut stats: pdpb::StoreStats,
         store_info: StoreInfo<EK, ER>,
         send_detailed_report: bool,
+        dr_autosync_status: Option<StoreDrAutoSyncStatus>,
     ) {
         let disk_stats = match fs2::statvfs(store_info.kv_engine.path()) {
             Err(e) => {
@@ -1071,7 +1073,7 @@ where
         let router = self.router.clone();
         let scheduler = self.scheduler.clone();
         let stats_copy = stats.clone();
-        let resp = self.pd_client.store_heartbeat(stats, optional_report, None);
+        let resp = self.pd_client.store_heartbeat(stats, optional_report, dr_autosync_status.clone());
         let f = async move {
             match resp.await {
                 Ok(mut resp) => {
@@ -1084,6 +1086,7 @@ where
                             stats: stats_copy,
                             store_info,
                             send_detailed_report: true,
+                            dr_autosync_status: dr_autosync_status,
                         };
                         if let Err(e) = scheduler.schedule(task) {
                             error!("notify pd failed"; "err" => ?e);
@@ -1117,6 +1120,7 @@ where
                             stats: stats_copy,
                             store_info,
                             send_detailed_report: true,
+                            dr_autosync_status: dr_autosync_status,
                         };
                         if let Err(e) = scheduler.schedule(task) {
                             error!("notify pd failed"; "err" => ?e);
@@ -1665,7 +1669,8 @@ where
                 stats,
                 store_info,
                 send_detailed_report,
-            } => self.handle_store_heartbeat(stats, store_info, send_detailed_report),
+                dr_autosync_status,
+            } => self.handle_store_heartbeat(stats, store_info, send_detailed_report, dr_autosync_status),
             Task::ReportBatchSplit { regions } => self.handle_report_batch_split(regions),
             Task::ValidatePeer { region, peer } => self.handle_validate_peer(region, peer),
             Task::ReadStats { read_stats } => self.handle_read_stats(read_stats),
