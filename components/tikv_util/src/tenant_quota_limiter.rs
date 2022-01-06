0c2def7d5852abf1035ffab2ff63e38a2494bdc8
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2022-01-06 17:54:38 +0800

    wrap Limiter to ReadQuotaLimiter and WriteQuotaLimiter
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- components/tikv_util/src/tenant_quota_limiter.rs
+++ components/tikv_util/src/tenant_quota_limiter.rs
@@ -6,13 +6,42 @@ use super::time::Limiter;
 use std::collections::HashMap;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
+use std::sync::Arc;
 use std::sync::RwLock;
 use std::time::Duration;
 
+pub struct ReadQuotaLimiter(Limiter);
+pub struct WriteQuotaLimiter(Limiter);
+
+impl ReadQuotaLimiter {
+    // millicpu same as the k8s
+    pub fn new(milli_cpu: u32) -> Self {
+        Self(Limiter::new(milli_cpu as f64 * 1000_f64))
+    }
+
+    // Consume read cpu quota
+    // If the quota is not enough, the returned duration will > 0, or return Duration::ZERO
+    pub fn consume_read(&self, time_slice_micro_secs: u32) -> Duration {
+        self.0.consume_duration(time_slice_micro_secs as usize)
+    }
+}
+
+impl WriteQuotaLimiter {
+    pub fn new(speed_limit: u64) -> Self {
+        Self(Limiter::new(speed_limit as f64))
+    }
+
+    // Consume write bytes quota
+    // If the quota is not enough, the returned duration will > 0, or return Duration::ZERO
+    pub fn consume_write(&self, write_bytes: u64) -> Duration {
+        self.0.consume_duration(write_bytes as usize)
+    }
+}
+
 pub struct TenantQuotaLimiter {
     // tenant_id -> limiter
-    tenant_write_limiters: RwLock<HashMap<u32, Limiter>>,
-    tenant_read_limiters: RwLock<HashMap<u32, Limiter>>,
+    tenant_write_limiters: RwLock<HashMap<u32, Arc<WriteQuotaLimiter>>>,
+    tenant_read_limiters: RwLock<HashMap<u32, Arc<ReadQuotaLimiter>>>,
     quota_is_updating: AtomicBool,
 }
 
@@ -31,11 +60,25 @@ impl TenantQuotaLimiter {
         }
     }
 
+    pub fn get_read_quota_limiter(&self, tenant_id: u32) -> Option<Arc<ReadQuotaLimiter>> {
+        if let Some(rlimiter) = self.tenant_read_limiters.read().unwrap().get(&tenant_id) {
+            return Some(Arc::clone(rlimiter));
+        }
+        None
+    }
+
+    pub fn get_write_quota_limiter(&self, tenant_id: u32) -> Option<Arc<WriteQuotaLimiter>> {
+        if let Some(wlimiter) = self.tenant_write_limiters.read().unwrap().get(&tenant_id) {
+            return Some(Arc::clone(wlimiter));
+        }
+        None
+    }
+
     // Consume write bytes quota
     // if quota is not enough, the returned duration will > 0, or return Duration::ZERO
     pub fn consume_write(&self, tenant_id: u32, write_bytes: u64) -> Duration {
         if let Some(limiter) = self.tenant_write_limiters.read().unwrap().get(&tenant_id) {
-            return limiter.consume_duration(write_bytes as usize);
+            return limiter.as_ref().consume_write(write_bytes);
         }
         Duration::ZERO
     }
@@ -44,12 +87,12 @@ impl TenantQuotaLimiter {
     // if quota is not enough, the returned duration will > 0, or return Duration::ZERO
     pub fn consume_read(&self, tenant_id: u32, time_slice_micro_secs: u32) -> Duration {
         if let Some(limiter) = self.tenant_read_limiters.read().unwrap().get(&tenant_id) {
-            return limiter.consume_duration(time_slice_micro_secs as usize);
+            return limiter.as_ref().consume_read(time_slice_micro_secs);
         }
         Duration::ZERO
     }
 
-    // Update quota if there are some changes
+    // Refresh quota if there are some changes
     // tenant_quotas: tenant_id -> (write_bytes_per_sec, read_milli_cpu)
     pub fn refresh_quota(&self, tenant_quotas: Vec<(u32, (u64, u32))>) {
         if self
@@ -65,7 +108,7 @@ impl TenantQuotaLimiter {
                 let rlimiters = self.tenant_read_limiters.read().unwrap();
                 for (idx, quota) in tenant_quotas.iter().enumerate() {
                     if let Some(l) = wlimiters.get(&quota.0) {
-                        if l.speed_limit() as u64 != quota.1.0 {
+                        if l.0.speed_limit() as u64 != quota.1.0 {
                             idxs_write.push(idx);
                         }
                     } else if quota.1.0 > 0 {
@@ -73,7 +116,7 @@ impl TenantQuotaLimiter {
                     }
 
                     if let Some(l) = rlimiters.get(&quota.0) {
-                        if l.speed_limit() as u64 != quota.1.1 as u64 * 1000 {
+                        if l.0.speed_limit() as u64 != quota.1.1 as u64 * 1000 {
                             idxs_read.push(idx)
                         }
                     } else if quota.1.1 > 0 {
@@ -88,8 +131,8 @@ impl TenantQuotaLimiter {
                     let quota = tenant_quotas[idx];
                     let limiter = wlimiters
                         .entry(quota.0)
-                        .or_insert_with(|| Limiter::new(quota.1.0 as f64));
-                    (*limiter).set_speed_limit(quota.1.0 as f64);
+                        .or_insert_with(|| Arc::new(WriteQuotaLimiter::new(quota.1.0)));
+                    (*limiter).0.set_speed_limit(quota.1.0 as f64);
                 }
             }
             if !idxs_read.is_empty() {
@@ -98,8 +141,8 @@ impl TenantQuotaLimiter {
                     let quota = tenant_quotas[idx];
                     let limiter = rlimiters
                         .entry(quota.0)
-                        .or_insert_with(|| Limiter::new(quota.1.0 as f64 * 1000_f64));
-                    (*limiter).set_speed_limit(quota.1.0 as f64 * 1000_f64);
+                        .or_insert_with(|| Arc::new(ReadQuotaLimiter::new(quota.1.1)));
+                    (*limiter).0.set_speed_limit(quota.1.1 as f64 * 1000_f64);
                 }
             }
 
