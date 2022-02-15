eb2d95f558ccee79b563d0fd34a915182e67bf62
Zhigao Tong <tongzhigao@pingcap.com>
2022-02-15 22:04:04 +0800

    Update build.sh
    - check openssl before build process

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ if [[ $(uname -s) == "Darwin" ]]; then
   lib_suffix="dylib"
   # use the openssl 1.1 lib from system
   export OPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR:-$(brew --prefix openssl@1.1)}
+
+  if [[ -z ${OPENSSL_ROOT_DIR} ]]; then
+    echo "Not found openssl installed by Homebrew or env 'OPENSSL_ROOT_DIR', please install openssl by 'brew install openssl@1.1'"
+    exit -1
+  fi
+  
   echo "OPENSSL_ROOT_DIR: ${OPENSSL_ROOT_DIR}"
   export OPENSSL_NO_VENDOR=1
   export OPENSSL_STATIC=1
