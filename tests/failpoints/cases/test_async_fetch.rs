4538f530d637b096716874bdf0acfba9e40200e6
Connor1996 <zbk602423539@gmail.com>
2022-02-28 14:02:59 +0800

    close #11973 ignore async fetch result when the peer is pending removed
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- tests/failpoints/cases/test_async_fetch.rs
+++ tests/failpoints/cases/test_async_fetch.rs
@@ -11,10 +11,9 @@ use test_raftstore::*;
 use tikv_util::config::*;
 
 // Test the case where cache is compacted when one node down to check
-// if it goes well after the node is back online.
-// It's mainly aimmed to test the async fetch raft log mechanism.
+// if it goes well after the node is back online which triggers async fetch.
 #[test]
-fn test_node_cache_compact_with_one_node_down() {
+fn test_node_async_fetch() {
     let count = 3;
     let mut cluster = new_node_cluster(0, count);
 
@@ -102,4 +101,70 @@ fn test_node_cache_compact_with_one_node_down() {
         }
     }
     panic!("cluster is not compacted after inserting 200 entries.");
+}
+
+// Test the case where async fetch is performed while the peer is removed.
+#[test]
+fn test_node_async_fetch_remove_peer() {
+    let count = 5;
+    let mut cluster = new_node_cluster(0, count);
+    cluster.pd_client.disable_default_operator();
+
+    cluster.cfg.raft_store.raft_log_gc_count_limit = 100000;
+    cluster.cfg.raft_store.raft_log_gc_threshold = 50;
+    cluster.cfg.raft_store.raft_log_gc_size_limit = ReadableSize::mb(20);
+    cluster.cfg.raft_store.raft_log_gc_tick_interval = ReadableDuration::millis(100);
+    cluster.cfg.raft_store.raft_log_reserve_max_ticks = 2;
+    cluster.cfg.raft_store.raft_entry_cache_life_time = ReadableDuration::millis(100);
+    cluster.run();
+
+    cluster.must_put(b"k1", b"v1");
+    cluster.must_transfer_leader(1, new_peer(1, 1));
+
+    let mut before_states = HashMap::default();
+
+    for (&id, engines) in &cluster.engines {
+        must_get_equal(engines.kv.as_inner(), b"k1", b"v1");
+        let mut state: RaftApplyState = engines
+            .kv
+            .get_msg_cf(CF_RAFT, &keys::apply_state_key(1))
+            .unwrap()
+            .unwrap_or_default();
+        let state = state.take_truncated_state();
+        // compact should not start
+        assert_eq!(RAFT_INIT_LOG_INDEX, state.get_index());
+        assert_eq!(RAFT_INIT_LOG_TERM, state.get_term());
+        before_states.insert(id, state);
+    }
+
+    cluster.stop_node(5);
+
+    for i in 1..60 {
+        let k = i.to_string().into_bytes();
+        let v = k.clone();
+        cluster.must_put(&k, &v);
+    }
+
+    fail::cfg("worker_async_fetch_raft_log", "pause").unwrap();
+    cluster.run_node(5).unwrap();
+
+    // make sure destroy_peer and on_entries_fetched are called in one batch.
+    cluster.must_transfer_leader(1, new_peer(2, 2));
+    fail::cfg("worker_gc_raft_log", "pause").unwrap();
+    cluster.pd_client.must_remove_peer(1, new_peer(1, 1));
+    fail::cfg("pause_on_peer_collect_message", "pause").unwrap();
+    fail::remove("worker_gc_raft_log");
+    sleep_ms(10);
+    fail::remove("worker_async_fetch_raft_log");
+    sleep_ms(10);
+    fail::remove("pause_on_peer_collect_message");
+
+    cluster.pd_client.must_add_peer(1, new_peer(1, 1));
+
+    // logs should be replicated to node 1 successfully.
+    for i in 1..60 {
+        let k = i.to_string().into_bytes();
+        let v = k.clone();
+        must_get_equal(cluster.engines[&1].kv.as_inner(), &k, &v);
+    }
 }
