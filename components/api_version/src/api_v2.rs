f6c46584e4554fd00be10870816f947a9d98d4c4
pingyu <yuping@pingcap.com>
2022-03-03 16:48:17 +0800

    api_version: address comments.
    Issue Number: ref #11965
    
    Signed-off-by: pingyu <yuping@pingcap.com>

--- components/api_version/src/api_v2.rs
+++ components/api_version/src/api_v2.rs
@@ -1,8 +1,9 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
+use codec::byte::MemComparableByteCodec;
 use engine_traits::Result;
 use tikv_util::codec::number::{self, NumberEncoder};
-use tikv_util::codec::Error;
+use tikv_util::codec::{bytes, Error};
 
 use super::*;
 
@@ -133,24 +134,31 @@ impl APIVersion for APIV2 {
         }
     }
 
-    // `encode_raw_key_owned` has improvement only for APIV1|APIV1TTL, but not for APIV2.
-    // As there is no "in place" implementation available for encoding.
-    fn encode_raw_key_owned(user_key: Vec<u8>, ts: Option<TimeStamp>) -> Key {
-        Self::encode_raw_key(&user_key, ts)
+    fn encode_raw_key_owned(mut user_key: Vec<u8>, ts: Option<TimeStamp>) -> Key {
+        debug_assert_eq!(Self::parse_key_mode(&user_key), KeyMode::Raw);
+        let src_len = user_key.len();
+        let encoded_len = MemComparableByteCodec::encoded_len(src_len);
+        user_key.reserve(encoded_len - src_len + number::U64_SIZE * (ts.is_some() as usize));
+        user_key.resize(encoded_len, 0u8);
+        MemComparableByteCodec::encode_all_in_place(&mut user_key, src_len);
+
+        let encoded_key = Key::from_encoded(user_key);
+        if let Some(ts) = ts {
+            encoded_key.append_ts(ts)
+        } else {
+            encoded_key
+        }
     }
 }
 
 #[inline]
 fn verify_encoded_key(encoded_key: &Key, with_ts: bool) -> bool {
-    let encoded_bytes = &encoded_key.as_encoded()[..];
+    let mut encoded_bytes = &encoded_key.as_encoded()[..];
     if !matches!(APIV2::parse_key_mode(encoded_bytes), KeyMode::Raw) {
         return false;
     }
-    let mut len = encoded_bytes.len();
-    if with_ts {
-        len = len.checked_sub(number::U64_SIZE).unwrap();
-    }
-    len > 0 && (len % (tikv_util::codec::bytes::ENC_GROUP_SIZE + 1) == 0)
+    bytes::decode_bytes(&mut encoded_bytes, false).is_ok()
+        && encoded_bytes.len() == number::U64_SIZE * (with_ts as usize)
 }
 
 #[inline]
