4c3625af0d8ca0bf80fe585cc1d5893fa6655dfa
Yilin Chen <sticnarf@gmail.com>
2022-03-08 18:55:16 +0800

    pick test improvements
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- components/raftstore/src/store/peer.rs
+++ components/raftstore/src/store/peer.rs
@@ -2934,6 +2934,7 @@ where
             Ok(RequestPolicy::ProposeConfChange) => self.propose_conf_change(ctx, &req),
             Err(e) => Err(e),
         };
+        fail_point!("after_propose");
 
         match res {
             Err(e) => {
