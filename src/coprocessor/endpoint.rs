9592ddc8de24178459c07e845d36ae502dd50f04
youjiali1995 <zlwgx1023@gmail.com>
2022-03-16 17:09:58 +0800

    copr: return buckets version for unary call close #12182
    Signed-off-by: youjiali1995 <zlwgx1023@gmail.com>

--- src/coprocessor/endpoint.rs
+++ src/coprocessor/endpoint.rs
@@ -379,6 +379,7 @@ impl<E: Engine> Endpoint<E> {
         tracker.on_snapshot_finished();
         tracker.req_ctx.deadline.check()?;
         tracker.buckets = snapshot.ext().get_buckets();
+        let buckets_version = tracker.buckets.as_ref().map_or(0, |b| b.version);
 
         let mut handler = if tracker.req_ctx.cache_match_version.is_some()
             && tracker.req_ctx.cache_match_version == snapshot.ext().get_data_version()
@@ -422,6 +423,7 @@ impl<E: Engine> Endpoint<E> {
         };
         resp.set_exec_details(exec_details);
         resp.set_exec_details_v2(exec_details_v2);
+        resp.set_latest_buckets_version(buckets_version);
         Ok(resp)
     }
 
