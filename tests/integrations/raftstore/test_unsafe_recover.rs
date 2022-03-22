1e23623877d279bf2bfc5f4710ca2b728027e882
Connor1996 <zbk602423539@gmail.com>
2022-03-22 16:57:10 +0800

    add three nodes test
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- tests/integrations/raftstore/test_unsafe_recover.rs
+++ tests/integrations/raftstore/test_unsafe_recover.rs
@@ -89,7 +89,69 @@ fn test_unsafe_recover_create_region() {
 }
 
 #[test]
-fn test_force_leader() {
+fn test_force_leader_three_nodes() {
+    let mut cluster = new_node_cluster(0, 3);
+    cluster.pd_client.disable_default_operator();
+
+    cluster.run();
+    cluster.must_put(b"k1", b"v1");
+    cluster.must_transfer_leader(1, new_peer(3, 3));
+
+    cluster.stop_node(2);
+    cluster.stop_node(3);
+
+    let put = new_put_cmd(b"k2", b"v2");
+    let mut region = cluster.get_region(b"k2");
+    let req = new_request(region.get_id(), region.take_region_epoch(), vec![put], true);
+    // marjority is lost, can't propose command successfully.
+    assert!(
+        cluster
+            .call_command_on_leader(req, Duration::from_millis(10))
+            .is_err()
+    );
+
+    cluster.enter_force_leader(1, 1, HashSet::from_iter(vec![2, 3]));
+    // remove the peers on failed nodes
+    cluster.pd_client.must_remove_peer(1, new_peer(2, 2));
+    cluster.pd_client.must_remove_peer(1, new_peer(3, 3));
+    let mut region = cluster.get_region(b"k2");
+    // forbid writes in force leader state
+    let put = new_put_cmd(b"k3", b"v3");
+    let req = new_request(region.get_id(), region.take_region_epoch(), vec![put], true);
+    let resp = cluster
+        .call_command_on_leader(req, Duration::from_millis(10))
+        .unwrap();
+    assert_eq!(
+        resp.get_header().get_error().get_recovery_in_progress(),
+        &kvproto::errorpb::RecoveryInProgress {
+            region_id: 1,
+            ..Default::default()
+        }
+    );
+    // forbid reads in force leader state
+    let get = new_get_cmd(b"k1");
+    let req = new_request(region.get_id(), region.take_region_epoch(), vec![get], true);
+    let resp = cluster
+        .call_command_on_leader(req, Duration::from_millis(10))
+        .unwrap();
+    assert_eq!(
+        resp.get_header().get_error().get_recovery_in_progress(),
+        &kvproto::errorpb::RecoveryInProgress {
+            region_id: 1,
+            ..Default::default()
+        }
+    );
+    cluster.exit_force_leader(1, 1);
+
+    // majority is formed, can propose command successfully now
+    cluster.must_put(b"k4", b"v4");
+    assert_eq!(cluster.must_get(b"k2"), None);
+    assert_eq!(cluster.must_get(b"k3"), None);
+    assert_eq!(cluster.must_get(b"k4"), Some(b"v4".to_vec()));
+}
+
+#[test]
+fn test_force_leader_five_nodes() {
     let mut cluster = new_node_cluster(0, 5);
     cluster.pd_client.disable_default_operator();
 
