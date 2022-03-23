1b3b1d6615b7b6b726ea20df09e8ef634d308634
pingyu <yuping@pingcap.com>
2022-03-23 15:29:15 +0800

    fix CI
    Signed-off-by: pingyu <yuping@pingcap.com>

--- components/causal_ts/src/tso.rs
+++ components/causal_ts/src/tso.rs
@@ -298,6 +298,7 @@ impl CausalTsProvider for SimpleTsoProvider {
 #[cfg(test)]
 pub mod tests {
     use super::*;
+    use test_raftstore::TestPdClient;
 
     #[test]
     fn test_tso_batch() {
@@ -345,5 +346,121 @@ pub mod tests {
             let new_size = BatchTsoProvider::calc_new_batch_size(batch_size, used_size, 100);
             assert_eq!(new_size, expected, "case {}", i);
         }
+    }
+
+    #[test]
+    fn test_simple_tso_provider() {
+        let pd_cli = Arc::new(TestPdClient::new(1, false));
+
+        let provider = SimpleTsoProvider::new(pd_cli.clone());
+
+        pd_cli.set_tso(100.into());
+        let ts = provider.get_ts().unwrap();
+        assert_eq!(ts, 101.into(), "ts: {:?}", ts);
+    }
+
+    #[test]
+    fn test_batch_tso_provider() {
+        let pd_cli = Arc::new(TestPdClient::new(1, false));
+        pd_cli.set_tso(1000.into());
+
+        // Set `renew_interval` to 0 to disable background renew. Invoke `flush()` to renew manually.
+        // allocated: [1001, 1100]
+        let provider = block_on(BatchTsoProvider::new_opt(
+            pd_cli.clone(),
+            Duration::ZERO,
+            100,
+        ))
+        .unwrap();
+        assert_eq!(provider.batch_size(), 100);
+        for ts in 1001..=1010u64 {
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+
+        provider.flush().unwrap(); // allocated: [1101, 1200]
+        assert_eq!(provider.batch_size(), 100);
+        // used up
+        pd_cli.trigger_tso_failure(); // make renew fail to verify used-up
+        for ts in 1101..=1200u64 {
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+        assert!(provider.get_ts().is_err());
+
+        provider.flush().unwrap(); // allocated: [1201, 1400]
+        assert_eq!(provider.batch_size(), 200);
+
+        // used < 20%
+        for ts in 1201..=1249u64 {
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+
+        provider.flush().unwrap(); // allocated: [1401, 1500]
+        assert_eq!(provider.batch_size(), 100);
+
+        pd_cli.trigger_tso_failure(); // make renew fail to verify used-up
+        for ts in 1401..=1500u64 {
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+        assert!(provider.get_ts().is_err());
+
+        // renew on used-up
+        for ts in 1501..=2500u64 {
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+    }
+
+    #[test]
+    fn test_batch_tso_provider_on_failure() {
+        let pd_cli = Arc::new(TestPdClient::new(1, false));
+        pd_cli.set_tso(1000.into());
+
+        {
+            pd_cli.trigger_tso_failure();
+            assert!(
+                block_on(BatchTsoProvider::new_opt(
+                    pd_cli.clone(),
+                    Duration::ZERO,
+                    100
+                ))
+                .is_err()
+            );
+        }
+
+        // Set `renew_interval` to 0 to disable background renew. Invoke `flush()` to renew manually.
+        // allocated: [1001, 1100]
+        let provider = block_on(BatchTsoProvider::new_opt(
+            pd_cli.clone(),
+            Duration::ZERO,
+            100,
+        ))
+        .unwrap();
+        assert_eq!(provider.batch_size(), 100);
+        for ts in 1001..=1010u64 {
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+
+        pd_cli.trigger_tso_failure();
+        for ts in 1011..=1020u64 {
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+
+        assert!(provider.flush().is_err());
+        for ts in 1101..=1300u64 {
+            // renew on used-up, allocated: [1101, 1300]
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+
+        pd_cli.trigger_tso_failure();
+        assert!(provider.get_ts().is_err()); // renew fail on used-up
+
+        pd_cli.trigger_tso_failure();
+        assert!(provider.flush().is_err());
+
+        provider.flush().unwrap(); // allocated: [1301, 1700]
+        pd_cli.trigger_tso_failure(); // make renew fail to verify used-up
+        for ts in 1301..=1700u64 {
+            assert_eq!(TimeStamp::from(ts), provider.get_ts().unwrap())
+        }
+        assert!(provider.get_ts().is_err());
     }
 }
