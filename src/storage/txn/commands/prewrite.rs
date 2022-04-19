5fd0b0de5a670fc544710e09cc002c36cb5a9fd7
Yilin Chen <sticnarf@gmail.com>
2022-04-19 15:02:03 +0800

    cherry pick #12369 to release-5.0
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- src/storage/txn/commands/prewrite.rs
+++ src/storage/txn/commands/prewrite.rs
@@ -506,12 +506,11 @@ impl<K: PrewriteKind> Prewriter<K> {
                     txn.guards = Vec::new();
                     final_min_commit_ts = TimeStamp::zero();
                 }
-                e @ Err(MvccError(box MvccErrorInner::KeyIsLocked { .. })) => {
-                    locks.push(
-                        e.map(|_| ())
-                            .map_err(Error::from)
-                            .map_err(StorageError::from),
-                    );
+                Err(MvccError(box MvccErrorInner::KeyIsLocked { .. })) => {
+                    match check_committed_record_on_err(prewrite_result, txn, reader, &key) {
+                        Ok(res) => return Ok(res),
+                        Err(e) => locks.push(Err(e.into())),
+                    }
                 }
                 Err(e) => return Err(Error::from(e)),
             }
@@ -806,7 +805,7 @@ mod tests {
         )
         .err()
         .unwrap();
-        assert_eq!(2, statistic.write.seek);
+        assert_eq!(3, statistic.write.seek);
         match e {
             Error(box ErrorInner::Mvcc(MvccError(box MvccErrorInner::KeyIsLocked(_)))) => (),
             _ => panic!("error type not match"),
@@ -819,7 +818,7 @@ mod tests {
             102,
         )
         .unwrap();
-        assert_eq!(2, statistic.write.seek);
+        assert_eq!(3, statistic.write.seek);
         let e = prewrite(
             &engine,
             &mut statistic,
@@ -1717,4 +1716,214 @@ mod tests {
         let snap = engine.snapshot(Default::default()).unwrap();
         assert!(prewrite_cmd.cmd.process_write(snap, context).is_err());
     }
+<<<<<<< HEAD
+=======
+
+    #[test]
+    fn test_assertion_fail_on_conflicting_index_key() {
+        let engine = crate::storage::TestEngineBuilder::new().build().unwrap();
+
+        // Simulate two transactions that tries to insert the same row with a secondary index, and
+        // the second one canceled the first one (by rolling back its lock).
+
+        let t1_start_ts = TimeStamp::compose(1, 0);
+        let t2_start_ts = TimeStamp::compose(2, 0);
+        let t2_commit_ts = TimeStamp::compose(3, 0);
+
+        // txn1 acquires lock on the row key.
+        must_acquire_pessimistic_lock(&engine, b"row", b"row", t1_start_ts, t1_start_ts);
+        // txn2 rolls it back.
+        let err =
+            must_acquire_pessimistic_lock_err(&engine, b"row", b"row", t2_start_ts, t2_start_ts);
+        assert!(matches!(err, MvccError(box MvccErrorInner::KeyIsLocked(_))));
+        must_check_txn_status(
+            &engine,
+            b"row",
+            t1_start_ts,
+            t2_start_ts,
+            t2_start_ts,
+            false,
+            false,
+            true,
+            |status| status == TxnStatus::PessimisticRollBack,
+        );
+        // And then txn2 acquire continues and finally commits
+        must_acquire_pessimistic_lock(&engine, b"row", b"row", t2_start_ts, t2_start_ts);
+        must_prewrite_put_impl(
+            &engine,
+            b"row",
+            b"value",
+            b"row",
+            &None,
+            t2_start_ts,
+            true,
+            1000,
+            t2_start_ts,
+            1,
+            t2_start_ts.next(),
+            0.into(),
+            false,
+            Assertion::NotExist,
+            AssertionLevel::Strict,
+        );
+        must_prewrite_put_impl(
+            &engine,
+            b"index",
+            b"value",
+            b"row",
+            &None,
+            t2_start_ts,
+            false,
+            1000,
+            t2_start_ts,
+            1,
+            t2_start_ts.next(),
+            0.into(),
+            false,
+            Assertion::NotExist,
+            AssertionLevel::Strict,
+        );
+        must_commit(&engine, b"row", t2_start_ts, t2_commit_ts);
+        must_commit(&engine, b"index", t2_start_ts, t2_commit_ts);
+
+        // Txn1 continues. If the two keys are sent in the single prewrite request, the
+        // AssertionFailed error won't be returned since there are other error.
+        let cm = ConcurrencyManager::new(1.into());
+        let mut stat = Statistics::default();
+        // Two keys in single request:
+        let cmd = PrewritePessimistic::with_defaults(
+            vec![
+                (
+                    Mutation::make_put(Key::from_raw(b"row"), b"value".to_vec()),
+                    true,
+                ),
+                (
+                    Mutation::make_put(Key::from_raw(b"index"), b"value".to_vec()),
+                    false,
+                ),
+            ],
+            b"row".to_vec(),
+            t1_start_ts,
+            t2_start_ts,
+        );
+        let err = prewrite_command(&engine, cm.clone(), &mut stat, cmd).unwrap_err();
+        assert!(matches!(
+            err,
+            Error(box ErrorInner::Mvcc(MvccError(
+                box MvccErrorInner::PessimisticLockNotFound { .. }
+            )))
+        ));
+        // Passing keys in different order gets the same result:
+        let cmd = PrewritePessimistic::with_defaults(
+            vec![
+                (
+                    Mutation::make_put(Key::from_raw(b"index"), b"value".to_vec()),
+                    false,
+                ),
+                (
+                    Mutation::make_put(Key::from_raw(b"row"), b"value".to_vec()),
+                    true,
+                ),
+            ],
+            b"row".to_vec(),
+            t1_start_ts,
+            t2_start_ts,
+        );
+        let err = prewrite_command(&engine, cm, &mut stat, cmd).unwrap_err();
+        assert!(matches!(
+            err,
+            Error(box ErrorInner::Mvcc(MvccError(
+                box MvccErrorInner::PessimisticLockNotFound { .. }
+            )))
+        ));
+
+        // If the two keys are sent in different requests, it would be the client's duty to ignore
+        // the assertion error.
+        let err = must_prewrite_put_err_impl(
+            &engine,
+            b"row",
+            b"value",
+            b"row",
+            &None,
+            t1_start_ts,
+            t1_start_ts,
+            true,
+            0,
+            false,
+            Assertion::NotExist,
+            AssertionLevel::Strict,
+        );
+        assert!(matches!(
+            err,
+            MvccError(box MvccErrorInner::PessimisticLockNotFound { .. })
+        ));
+        let err = must_prewrite_put_err_impl(
+            &engine,
+            b"index",
+            b"value",
+            b"row",
+            &None,
+            t1_start_ts,
+            t1_start_ts,
+            false,
+            0,
+            false,
+            Assertion::NotExist,
+            AssertionLevel::Strict,
+        );
+        assert!(matches!(
+            err,
+            MvccError(box MvccErrorInner::AssertionFailed { .. })
+        ));
+    }
+
+    #[test]
+    fn test_prewrite_committed_encounter_newer_lock() {
+        let engine = TestEngineBuilder::new().build().unwrap();
+        let mut statistics = Statistics::default();
+
+        let k1 = b"k1";
+        let v1 = b"v1";
+        let v2 = b"v2";
+
+        must_prewrite_put_async_commit(&engine, k1, v1, k1, &Some(vec![]), 5, 10);
+        // This commit may actually come from a ResolveLock command
+        must_commit(&engine, k1, 5, 15);
+
+        // Another transaction prewrites
+        must_prewrite_put(&engine, k1, v2, k1, 20);
+
+        // A retried prewrite of the first transaction should be idempotent.
+        let prewrite_cmd = Prewrite::new(
+            vec![Mutation::make_put(Key::from_raw(k1), v1.to_vec())],
+            k1.to_vec(),
+            5.into(),
+            2000,
+            false,
+            1,
+            5.into(),
+            1000.into(),
+            Some(vec![]),
+            false,
+            AssertionLevel::Off,
+            Context::default(),
+        );
+        let context = WriteContext {
+            lock_mgr: &DummyLockManager {},
+            concurrency_manager: ConcurrencyManager::new(20.into()),
+            extra_op: ExtraOp::Noop,
+            statistics: &mut statistics,
+            async_apply_prewrite: false,
+        };
+        let snap = engine.snapshot(Default::default()).unwrap();
+        let res = prewrite_cmd.cmd.process_write(snap, context).unwrap();
+        match res.pr {
+            ProcessResult::PrewriteResult { result } => {
+                assert!(result.locks.is_empty(), "{:?}", result);
+                assert_eq!(result.min_commit_ts, 15.into(), "{:?}", result);
+            }
+            _ => panic!("unexpected result {:?}", res.pr),
+        }
+    }
+>>>>>>> c038c4cfb... txn: make prewrite idempotent after a new lock is written (#12369)
 }
