a27de02e66d8a497651ab71cd316b982796f76f2
Yilin Chen <sticnarf@gmail.com>
2022-04-27 14:06:12 +0800

    *: apply perf flags to record RocksDB perf context in coprocessor
    Signed-off-by: Yilin Chen <sticnarf@gmail.com>

--- components/test_raftstore/src/server.rs
+++ components/test_raftstore/src/server.rs
@@ -25,7 +25,7 @@ use api_version::{dispatch_api_version, KvFormat};
 use collections::{HashMap, HashSet};
 use concurrency_manager::ConcurrencyManager;
 use encryption_export::DataKeyManager;
-use engine_rocks::{PerfLevel, RocksEngine, RocksSnapshot};
+use engine_rocks::{RocksEngine, RocksSnapshot};
 use engine_traits::{Engines, MiscExt};
 use pd_client::PdClient;
 use raftstore::errors::Error as RaftError;
@@ -408,7 +408,6 @@ impl ServerCluster {
             &server_cfg.value().clone(),
             cop_read_pool.handle(),
             concurrency_manager.clone(),
-            PerfLevel::EnableCount,
             res_tag_factory,
             quota_limiter,
         );
