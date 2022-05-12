5808bcfcf968fdb1d9c4b21aad369c8435929fca
glorv <glorvs@163.com>
2022-05-12 11:14:04 +0800

    fix
    Signed-off-by: glorv <glorvs@163.com>

--- components/config_info/config_info_derive/src/lib.rs
+++ components/config_info/config_info_derive/src/lib.rs
@@ -175,7 +175,7 @@ fn build_filed_constructer(
     } = attrs;
     let convert_field = |l: Lit| match l {
         Lit::Str(s) => {
-            quote!(FromStr::from_str(#s).unwrap())
+            quote!(core::str::FromStr::from_str(#s).unwrap())
         }
         s => quote!(Into::into(#s)),
     };
