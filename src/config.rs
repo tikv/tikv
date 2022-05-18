d3fc75f6ebd200ac3af00cf89f01f4739ca03a03
5kbpers <hustmh@gmail.com>
2022-05-18 17:09:13 +0800

    fix tests
    Signed-off-by: 5kbpers <hustmh@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -4953,6 +4953,8 @@ mod tests {
         default_cfg.security.redact_info_log = Some(false);
         default_cfg.coprocessor.region_max_size = Some(default_cfg.coprocessor.region_max_size());
         default_cfg.coprocessor.region_max_keys = Some(default_cfg.coprocessor.region_max_keys());
+        default_cfg.coprocessor.region_split_keys =
+            Some(default_cfg.coprocessor.region_split_keys());
         default_cfg.raft_store.raft_log_gc_size_limit =
             Some(default_cfg.coprocessor.region_split_size * 3 / 4);
         default_cfg.raft_store.raft_log_gc_count_limit =
