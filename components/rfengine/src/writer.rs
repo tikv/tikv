73724699887aa8b13ed2f5853a6c4187b36d62bc
youjiali1995 <zlwgx1023@gmail.com>
2022-05-23 14:39:14 +0800

    rfengine: introduce DmaBuffer
    Signed-off-by: youjiali1995 <zlwgx1023@gmail.com>

--- components/rfengine/src/writer.rs
+++ components/rfengine/src/writer.rs
@@ -1,11 +1,12 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
 use std::{
-    fs,
+    alloc::{self, Layout},
+    cmp, fs,
     fs::File,
-    mem,
     os::unix::prelude::FileExt,
     path::{Path, PathBuf},
+    ptr::NonNull,
 };
 
 use bytes::BufMut;
@@ -14,26 +15,133 @@ use tikv_util::time::Instant;
 
 use crate::{write_batch::RegionBatch, *};
 
-pub const BATCH_HEADER_SIZE: usize = 12;
-pub(crate) const ALIGN_SIZE: usize = 4096;
-pub(crate) const ALIGN_MASK: u64 = 0xffff_f000;
+pub const BATCH_HEADER_SIZE: usize = 4 /* epoch_id */ + 4 /* checksum */ + 4 /* batch_len */;
 pub(crate) const INITIAL_BUF_SIZE: usize = 8 * 1024 * 1024;
 pub(crate) const RECYCLE_DIR: &str = "recycle";
 
-#[repr(C, align(4096))]
-struct AlignTo4K([u8; ALIGN_SIZE]);
+/// `DmaBuffer` is a buffer used for direct I/O that follows the alignment restrictions
+/// on the length and address of user-space buffers.
+///
+/// The typical usage is:
+///
+/// ```ignore
+/// let mut buf = DmaBuffer::new(16*1024);
+/// let data = b"data";
+/// buf.ensure_space(data.len());
+/// let chunk = unsafe { buf.chunk_mut() };
+/// chunk.copy_from_slice(data);
+/// unsafe { buf.advance_mut(data.len()) };
+/// buf.pad_to_align();
+/// write(buf.as_ref());
+/// ```
+pub(crate) struct DmaBuffer {
+    data: NonNull<u8>,
+    layout: Layout,
+    len: usize,
+}
 
-pub fn alloc_aligned(n_bytes: usize) -> Vec<u8> {
-    let n_units = (n_bytes + ALIGN_SIZE - 1) / ALIGN_SIZE;
+unsafe impl Send for DmaBuffer {}
 
-    let mut aligned: Vec<AlignTo4K> = Vec::with_capacity(n_units);
+impl DmaBuffer {
+    const DMA_ALIGN: usize = 4096;
 
-    let ptr = aligned.as_mut_ptr();
-    let cap_units = aligned.capacity();
+    fn new(cap: usize) -> Self {
+        debug_assert!(0 < cap && cap <= isize::MAX as usize);
+        let layout = Layout::from_size_align(cap, Self::DMA_ALIGN)
+            .unwrap()
+            .pad_to_align();
+        let data = unsafe { alloc::alloc(layout) };
+        let data = NonNull::new(data).expect("memory allocation success");
+        Self {
+            data,
+            layout,
+            len: 0,
+        }
+    }
 
-    mem::forget(aligned);
+    fn len(&self) -> usize {
+        self.len
+    }
 
-    unsafe { Vec::from_raw_parts(ptr as *mut u8, 0, cap_units * mem::size_of::<AlignTo4K>()) }
+    fn capacity(&self) -> usize {
+        self.layout.size()
+    }
+
+    /// Shortens the vector, keeping the first `len` elements and dropping
+    /// the rest.
+    ///
+    /// If `len` is greater than the vector's current length, this has no
+    /// effect.
+    fn truncate(&mut self, len: usize) {
+        self.len = cmp::min(self.len, len);
+    }
+
+    /// Ensures enough space for `size`.
+    fn ensure_space(&mut self, size: usize) {
+        if self.capacity() - self.len >= size {
+            return;
+        }
+        let require_cap = self
+            .len
+            .checked_add(size)
+            .expect("capacity shouldn't overflow");
+        let new_cap = cmp::max(self.layout.size() * 2, require_cap);
+        let new_layout = Layout::from_size_align(new_cap, Self::DMA_ALIGN)
+            .unwrap()
+            .pad_to_align();
+        let data = unsafe { alloc::realloc(self.data.as_ptr(), self.layout, new_layout.size()) };
+        self.data = NonNull::new(data).expect("memory allocation success");
+        self.layout = new_layout;
+    }
+
+    /// Pads the length of buf to the alignment. It doesn't pad zeros.
+    fn pad_to_align(&mut self) {
+        self.len = Self::aligned_len(self.len);
+        assert!(self.len <= self.capacity());
+    }
+
+    /// Returns a mutable slice starting at the current position.
+    ///
+    /// This function is unsafe because the returned byte slice may represent uninitialized memory.
+    unsafe fn chunk_mut(&mut self) -> &mut [u8] {
+        &mut std::slice::from_raw_parts_mut(self.data.as_ptr(), self.capacity())[self.len..]
+    }
+
+    /// Advances the internal cursor of the BufMut
+    ///
+    /// The next call to `chunk_mut` will return a slice starting `cnt` bytes
+    /// further into the underlying buf.
+    ///
+    /// This function is unsafe because there is no guarantee that the bytes
+    /// being advanced past have been initialized.
+    unsafe fn advance_mut(&mut self, cnt: usize) {
+        self.len += cnt;
+        assert!(self.len <= self.capacity());
+    }
+
+    pub(crate) fn aligned_len(len: usize) -> usize {
+        len.wrapping_add(Self::DMA_ALIGN - 1) & !(Self::DMA_ALIGN - 1)
+    }
+}
+
+impl Drop for DmaBuffer {
+    fn drop(&mut self) {
+        unsafe {
+            alloc::dealloc(self.data.as_ptr(), self.layout);
+        }
+    }
+}
+
+impl AsRef<[u8]> for DmaBuffer {
+    fn as_ref(&self) -> &[u8] {
+        unsafe { std::slice::from_raw_parts(self.data.as_ptr(), self.len) }
+    }
+}
+
+impl AsMut<[u8]> for DmaBuffer {
+    fn as_mut(&mut self) -> &mut [u8] {
+        unsafe { std::slice::from_raw_parts_mut(self.data.as_ptr(), self.len) }
+    }
 }
 
 pub(crate) struct WALWriter {
@@ -41,18 +149,22 @@ pub(crate) struct WALWriter {
     pub(crate) epoch_id: u32,
     pub(crate) wal_size: usize,
     fd: File,
-    buf: Vec<u8>,
+    buf: DmaBuffer,
     // file_off is always aligned.
     file_off: u64,
 }
 
 impl WALWriter {
     pub(crate) fn new(dir: &Path, epoch_id: u32, wal_size: usize) -> Result<Self> {
-        let wal_size = (wal_size + ALIGN_SIZE - 1) & ALIGN_MASK as usize;
+        let wal_size = DmaBuffer::aligned_len(wal_size);
         let file_path = get_wal_file_path(dir, epoch_id)?;
         let fd = open_direct_file(&file_path, true)?;
-        let mut buf = alloc_aligned(INITIAL_BUF_SIZE);
-        buf.resize(BATCH_HEADER_SIZE, 0);
+        let mut buf = DmaBuffer::new(INITIAL_BUF_SIZE);
+        buf.ensure_space(BATCH_HEADER_SIZE);
+        // Safety: ensured enough space and `flush` will init the header.
+        unsafe {
+            buf.advance_mut(BATCH_HEADER_SIZE);
+        }
         Ok(Self {
             dir: dir.to_path_buf(),
             epoch_id,
@@ -64,48 +176,44 @@ impl WALWriter {
     }
 
     pub(crate) fn seek(&mut self, file_offset: u64) {
-        assert_eq!(file_offset & ALIGN_MASK.reverse_bits(), 0);
+        assert_eq!(file_offset & (DmaBuffer::DMA_ALIGN - 1) as u64, 0);
         self.file_off = file_offset;
     }
 
-    pub(crate) fn reallocate(&mut self) {
-        let new_cap = self.buf.capacity() * 2;
-        let mut new_buf = alloc_aligned(new_cap);
-        new_buf.truncate(0);
-        new_buf.extend_from_slice(self.buf.as_slice());
-        let _ = mem::replace(&mut self.buf, new_buf);
-    }
-
     pub(crate) fn append_region_data(&mut self, region_batch: &RegionBatch) {
-        if self.buf.len() + region_batch.encoded_len() > self.buf.capacity() {
-            self.reallocate();
+        let data_len = region_batch.encoded_len();
+        self.buf.ensure_space(data_len);
+        // Safety: `data_len` is the length of data encoded by `encode_to` and
+        // `ensure_space` ensures enough space.
+        unsafe {
+            region_batch.encode_to(&mut self.buf.chunk_mut());
+            self.buf.advance_mut(data_len);
         }
-        region_batch.encode_to(&mut self.buf);
     }
 
-    pub(crate) fn flush(&mut self) -> Result<bool> {
-        let mut rotated = false;
-        if aligned_len(self.buf.len()) + self.file_off as usize > self.wal_size {
-            self.rotate()?;
-            rotated = true;
-        }
-        let batch = &mut self.buf[..];
+    pub(crate) fn flush(&mut self) -> Result<(usize, bool)> {
+        let data_len = self.buf.len();
+        let batch = self.buf.as_mut();
         let (mut batch_header, batch_payload) = batch.split_at_mut(BATCH_HEADER_SIZE);
         let checksum = crc32fast::hash(batch_payload);
         batch_header.put_u32_le(self.epoch_id);
         batch_header.put_u32_le(checksum);
         batch_header.put_u32_le(batch_payload.len() as u32);
-        self.buf.resize(aligned_len(self.buf.len()), 0);
+        self.buf.pad_to_align();
+
         let timer = Instant::now_coarse();
-        self.fd.write_all_at(&self.buf[..], self.file_off)?;
+        self.fd.write_all_at(self.buf.as_ref(), self.file_off)?;
         ENGINE_WAL_WRITE_DURATION_HISTOGRAM.observe(timer.saturating_elapsed_secs());
         self.file_off += self.buf.len() as u64;
-        self.reset_batch();
-        Ok(rotated)
-    }
-
-    pub(crate) fn reset_batch(&mut self) {
         self.buf.truncate(BATCH_HEADER_SIZE);
+
+        let mut rotated = false;
+        if self.file_off as usize > self.wal_size {
+            self.rotate()?;
+            rotated = true;
+        }
+
+        Ok((data_len, rotated))
     }
 
     fn rotate(&mut self) -> Result<()> {
@@ -124,10 +232,6 @@ impl WALWriter {
         self.file_off = 0;
         Ok(())
     }
-
-    pub(crate) fn buf_size(&mut self) -> usize {
-        self.buf.len()
-    }
 }
 
 pub(crate) fn get_wal_file_path(dir: &Path, epoch_id: u32) -> Result<PathBuf> {
@@ -138,10 +242,6 @@ pub(crate) fn get_wal_file_path(dir: &Path, epoch_id: u32) -> Result<PathBuf> {
         }
     }
     Ok(filename)
-}
-
-pub(crate) fn aligned_len(origin_len: usize) -> usize {
-    ((origin_len + ALIGN_SIZE - 1) as u64 & ALIGN_MASK) as usize
 }
 
 pub(crate) fn find_recycled_file(dir: &Path) -> Result<Option<PathBuf>> {
@@ -155,4 +255,31 @@ pub(crate) fn find_recycled_file(dir: &Path) -> Result<Option<PathBuf>> {
         }
     }
     Ok(recycle_file)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::DmaBuffer;
+
+    #[test]
+    fn test_dma_buffer() {
+        let mut buf = DmaBuffer::new(4095);
+        assert_eq!(buf.layout.size(), DmaBuffer::aligned_len(4095));
+        let addr = buf.data.as_ptr() as usize;
+        assert_eq!(addr, DmaBuffer::aligned_len(addr));
+
+        let data = b"data";
+        for i in 1..=1025 {
+            buf.ensure_space(data.len());
+            let chunk = unsafe { buf.chunk_mut() };
+            chunk[..data.len()].copy_from_slice(data);
+            unsafe { buf.advance_mut(data.len()) };
+            assert_eq!(buf.len(), data.len() * i);
+            assert_eq!(buf.as_ref(), data.repeat(i));
+            assert_eq!(buf.as_mut(), data.repeat(i));
+        }
+        assert_eq!(buf.layout.size(), 8192);
+        let addr = buf.data.as_ptr() as usize;
+        assert_eq!(addr, DmaBuffer::aligned_len(addr));
+    }
 }
