68b56897b3c7349af34fd75a7a2eec773971e258
Jay Lee <BusyJayLee@gmail.com>
2022-06-16 15:50:54 -0700

    raftstorev2: add basic layout
    This is an attempt to reimplement raftstore using the new assumptions
    that peer's range can be overlapped.
    
    Currently, compatability is not considered, though we may think about
    how to migrate from old version by the end of this year.
    
    No concrete implementations is added yet, we may choose reuse
    implementation from v1 or implementing new logic base on actual
    requirement. The principle is do not introduce history debt while reusing
    code as much as possible.
    
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>

---
+++ components/raftstore-v2/src/raft/storage.rs
@@ -0,0 +1,54 @@
+// Copyright 2022 TiKV Project Authors. Licensed under Apache-2.0.
+
+use engine_traits::RaftEngine;
+use raft::{GetEntriesContext, RaftState};
+use raft::eraftpb::{Entry, Snapshot};
+use slog::Logger;
+
+/// A storage for raft.
+pub struct Storage<ER> {
+    engine: ER,
+    logger: Logger,
+}
+
+impl<ER> Storage<ER> {
+    pub fn new(engine: ER, logger: Logger) -> Storage<ER> {
+        Storage { engine, logger }
+    }
+
+    pub fn applied_index(&self) -> u64 {
+        unimplemented!()
+    }
+}
+
+impl<ER: RaftEngine> raft::Storage for Storage<ER> {
+    fn initial_state(&self) -> raft::Result<RaftState> {
+        unimplemented!()
+    }
+
+    fn entries(
+        &self,
+        low: u64,
+        high: u64,
+        max_size: impl Into<Option<u64>>,
+        context: GetEntriesContext,
+    ) -> raft::Result<Vec<Entry>> {
+        unimplemented!()
+    }
+
+    fn term(&self, idx: u64) -> raft::Result<u64> {
+        unimplemented!()
+    }
+
+    fn first_index(&self) -> raft::Result<u64> {
+        unimplemented!()
+    }
+
+    fn last_index(&self) -> raft::Result<u64> {
+        unimplemented!()
+    }
+
+    fn snapshot(&self, request_index: u64, to: u64) -> raft::Result<Snapshot> {
+        unimplemented!()
+    }
+}
