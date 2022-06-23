e8094c4f582ad460509cfb753f4661e59407c1b1
CalvinNeo <calvinneo1995@gmail.com>
2022-06-23 18:24:01 +0800

    test panick
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- tests/failpoints/cases/mod.rs
+++ tests/failpoints/cases/mod.rs
@@ -1,5 +1,7 @@
 // Copyright 2017 TiKV Project Authors. Licensed under Apache-2.0.
 
+mod test_normal;
+mod test_snap;
 mod test_bootstrap;
 mod test_cmd_epoch_checker;
 mod test_conf_change;
--- tests/failpoints/cases/test_normal.rs
+++ tests/failpoints/cases/test_normal.rs
@@ -4,12 +4,13 @@ use mock_engine_store;
 use raftstore::engine_store_ffi::{KVGetStatus, RaftStoreProxyFFI};
 use std::sync::{Arc, RwLock};
 use test_raftstore::*;
+use kvproto::kvrpcpb::ApiVersion;
 
 #[test]
 fn test_normal() {
     let pd_client = Arc::new(TestPdClient::new(0, false));
     let sim = Arc::new(RwLock::new(NodeCluster::new(pd_client.clone())));
-    let mut cluster = Cluster::new(0, 3, sim, pd_client);
+    let mut cluster = Cluster::new(0, 3, sim, pd_client, ApiVersion::V1);
     unsafe {
         test_raftstore::init_cluster_ptr(&cluster);
     }
@@ -20,6 +21,8 @@ fn test_normal() {
     let k = b"k1";
     let v = b"v1";
     cluster.must_put(k, v);
+    std::thread::sleep(std::time::Duration::from_secs(4));
+    return;
     for id in cluster.engines.keys() {
         must_get_equal(&cluster.get_engine(*id), k, v);
         // must_get_equal(db, k, v);
