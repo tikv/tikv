47072fd62b7429a468c3c6aa0759b3545fc7bec7
MyonKeminta <MyonKeminta@users.noreply.github.com>
2022-07-11 20:17:25 +0800

    raftstore: Avoid printing error log in case sending CaptureChange message failed
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- components/raftstore/src/store/msg.rs
+++ components/raftstore/src/store/msg.rs
@@ -635,6 +635,17 @@ impl<EK: KvEngine> fmt::Debug for PeerMsg<EK> {
     }
 }
 
+impl<EK: KvEngine> PeerMsg<EK> {
+    /// For some specific kind of messages, it's actually acceptable if failed to send it by
+    /// `significant_send`. This function determine if the current message is acceptable to fail.
+    pub fn is_send_failure_ignorable(&self) -> bool {
+        match self {
+            PeerMsg::SignificantMsg(SignificantMsg::CaptureChange { .. }) => true,
+            _ => false,
+        }
+    }
+}
+
 pub enum StoreMsg<EK>
 where
     EK: KvEngine,
