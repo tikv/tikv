f4b535f8d0c053c98aa03f0a62c3e4514da7bec7
subains <sunny.bains@pingcap.com>
2022-07-14 15:20:04 -0700

    Fix formatting
    Signed-off-by: subains <sunny.bains@pingcap.com>

--- components/tidb_query_executors/src/fast_hash_aggr_executor.rs
+++ components/tidb_query_executors/src/fast_hash_aggr_executor.rs
@@ -281,7 +281,7 @@ impl<Src: BatchExecutor> AggregationExecutorImpl<Src> for FastHashAggregationImp
 
         let rows_len = input_logical_rows.len();
 
-        if rows_len> 0 && self.n_bytes == 0 {
+        if rows_len > 0 && self.n_bytes == 0 {
             self.alloc_trace(rows_len * size_of::<usize>());
         }
 
