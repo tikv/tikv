820899de52e37c28bcfba011d6edc6a0f6291c06
Willendless <317500141@qq.com>
2022-07-15 22:39:53 -0400

    chore: format
    Signed-off-by: Willendless <317500141@qq.com>

--- components/tidb_query_datatype/src/codec/mysql/duration.rs
+++ components/tidb_query_datatype/src/codec/mysql/duration.rs
@@ -274,9 +274,9 @@ mod parser {
         }
 
         match duration {
-            Some(Ok(duration)) => {
-                ctx.handle_truncate(truncated_parse).map_or(Some(duration), |_| Some(duration))
-            }
+            Some(Ok(duration)) => ctx
+                .handle_truncate(truncated_parse)
+                .map_or(Some(duration), |_| Some(duration)),
             Some(Err(err)) if err.is_overflow() => {
                 if overflow_as_null {
                     return None;
