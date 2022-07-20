3a60a0443badca0f6c9ccb1888b5ce525b439259
Yu Juncen <yujuncen@pingcap.com>
2022-07-20 16:13:07 +0800

    remove unused import
    Signed-off-by: Yu Juncen <yujuncen@pingcap.com>

--- components/backup-stream/src/event_loader.rs
+++ components/backup-stream/src/event_loader.rs
@@ -9,7 +9,7 @@ use std::{
 use engine_traits::{KvEngine, CF_DEFAULT, CF_WRITE};
 use futures::executor::block_on;
 use kvproto::{kvrpcpb::ExtraOp, metapb::Region, raft_cmdpb::CmdType};
-use prometheus::core::Atomic;
+use prometheus::core::Atomi;
 use raftstore::{
     coprocessor::RegionInfoProvider,
     router::RaftStoreRouter,
