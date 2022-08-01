261e7f1441ad56d0e3d7bbd015804a8e20831098
Wenbo Zhang <ethercflow@gmail.com>
2022-08-01 15:44:04 +0800

    Revert "Fix panic when enable titan (#13051)"
    This reverts commit 6c7f6ecf4a999b0c102d442061d0ce0bd8b7c969.
    
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>

--- components/engine_rocks/src/write_batch.rs
+++ components/engine_rocks/src/write_batch.rs
@@ -98,7 +98,7 @@ impl RocksWriteBatchVec {
 impl engine_traits::WriteBatch for RocksWriteBatchVec {
     fn write_opt(&self, opts: &WriteOptions) -> Result<()> {
         let opt: RocksWriteOptions = opts.into();
-        if self.support_write_batch_vec {
+        if self.index > 0 {
             self.get_db()
                 .multi_batch_write(self.as_inner(), &opt.into_raw())
                 .map_err(Error::Engine)
