75a8d415725dfcc57be8658e1ca4309c493da7ba
lhy1024 <admin@liudos.us>
2022-08-02 17:52:06 +0800

    pd-client: pd client should update if the grpc stream sender failed. (#13094) (#13207)
    ref tikv/tikv#12827, ref tikv/tikv#12934, ref tikv/tikv#13094
    
    Signed-off-by: bufferflies <1045931706@qq.com>
    Signed-off-by: lhy1024 <admin@liudos.us>
    
    Co-authored-by: buffer <1045931706@qq.com>

--- components/pd_client/src/client.rs
+++ components/pd_client/src/client.rs
@@ -609,6 +609,9 @@ impl PdClient for RpcClient {
                             if last > last_report {
                                 last_report = last - 1;
                             }
+                            fail::fail_point!("region_heartbeat_send_failed", |_| {
+                                Err(Error::Grpc(grpcio::Error::RemoteStopped))
+                            });
                             Ok((r, WriteFlags::default()))
                         }))
                         .await;
@@ -633,7 +636,8 @@ impl PdClient for RpcClient {
                 .expect("expect region heartbeat sender");
             let ret = sender
                 .unbounded_send(req)
-                .map_err(|e| Error::Other(Box::new(e)));
+                .map_err(|e| Error::StreamDisconnect(e.into_send_error()));
+
             Box::pin(future::ready(ret)) as PdFuture<_>
         };
 
