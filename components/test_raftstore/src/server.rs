0b4231ac9ed76575d0fe4e3b6ba93efac4b50431
Ping Yu <yuping@pingcap.com>
2022-08-08 12:14:46 +0800

    causal_ts: Implement TSO batch list (#12970)
    close tikv/tikv#12794, ref tikv/tikv#12794
    
    Implement TSO batch list to improve tolerance to TSO service fault.
    
    Signed-off-by: pingyu <yuping@pingcap.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- components/test_raftstore/src/server.rs
+++ components/test_raftstore/src/server.rs
@@ -364,7 +364,9 @@ impl ServerCluster {
                 block_on(causal_ts::BatchTsoProvider::new_opt(
                     self.pd_client.clone(),
                     cfg.causal_ts.renew_interval.0,
+                    cfg.causal_ts.available_interval.0,
                     cfg.causal_ts.renew_batch_min_size,
+                    cfg.causal_ts.renew_batch_max_size,
                 ))
                 .unwrap(),
             );
