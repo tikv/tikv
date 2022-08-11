1ffa3034bec416268af751d319c8d7a2bc2ca464
buffer <1045931706@qq.com>
2022-08-11 18:32:49 +0800

    pd-client:  remove `call_option` to avoid  deadlock(RWR). (#13249)
    close tikv/tikv#13191, ref rust-lang/rust#93740
    
    Signed-off-by: bufferflies <1045931706@qq.com>
    
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- components/pd_client/src/util.rs
+++ components/pd_client/src/util.rs
@@ -472,10 +472,17 @@ where
     }
 }
 
+pub fn call_option_inner(inner: &Inner) -> CallOption {
+    inner
+        .target_info()
+        .call_option()
+        .timeout(Duration::from_secs(REQUEST_TIMEOUT))
+}
+
 /// Do a request in synchronized fashion.
 pub fn sync_request<F, R>(client: &Client, mut retry: usize, func: F) -> Result<R>
 where
-    F: Fn(&PdClientStub) -> GrpcResult<R>,
+    F: Fn(&PdClientStub, CallOption) -> GrpcResult<R>,
 {
     loop {
         let ret = {
@@ -483,8 +490,12 @@ where
             // thread which may hold the read lock and wait for PD client thread
             // completing the request and the PD client thread which may block
             // on acquiring the write lock.
-            let client_stub = client.inner.rl().client_stub.clone();
-            func(&client_stub).map_err(Error::Grpc)
+            let (client_stub, option) = {
+                let inner = client.inner.rl();
+                (inner.client_stub.clone(), call_option_inner(&inner))
+            };
+
+            func(&client_stub, option).map_err(Error::Grpc)
         };
         match ret {
             Ok(r) => {
