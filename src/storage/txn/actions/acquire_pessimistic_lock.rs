3265bc0063742f97e00d6d0cddfebf31065a8fd5
TonsnakeLin <lpbgytong@163.com>
2022-08-30 13:30:50 +0800

    modify log format
    Signed-off-by: TonsnakeLin <lpbgytong@163.com>

--- src/storage/txn/actions/acquire_pessimistic_lock.rs
+++ src/storage/txn/actions/acquire_pessimistic_lock.rs
@@ -43,7 +43,7 @@ pub fn acquire_pessimistic_lock<S: Snapshot>(
     ));
     if lock_only_if_exists && !need_value {
         error!(
-            "When acquires a pessimistic lock, Lock_only_if_exists of a pessimistic lock request is set to true,
+            "lock_only_if_exists of a pessimistic lock request is set to true,
             but return_value is not";
             "start_ts" => reader.start_ts,
             "key" => log_wrappers::Value::key(key.as_encoded()),
