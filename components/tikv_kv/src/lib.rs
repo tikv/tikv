c228314da6ced101280519cf7c264c965f6706e3
SpadeA-Tang <u6748471@anu.edu.au>
2022-09-08 16:09:04 +0800

    add comment
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- components/tikv_kv/src/lib.rs
+++ components/tikv_kv/src/lib.rs
@@ -268,9 +268,17 @@ pub trait Engine: Send + Clone + 'static {
     type Local: LocalEngine;
 
     /// Local storage engine.
+    ///
+    /// In the single rocksdb version, the return value is guaranteed to be Some
+    /// where as in the multi-rocksdb version, the None will be returned.
     fn kv_engine(&self) -> Option<Self::Local>;
 
     /// Write modifications into internal local engine directly.
+    ///
+    /// region_modifies records each region's modifications. In the single
+    /// rocksdb version, they are flattened to handle together whereas in the
+    /// multi-rocksdb version, the modifications of each region will be handled
+    /// separately.
     fn modify_on_kv_engine(&self, region_modifies: HashMap<u64, Vec<Modify>>) -> Result<()>;
 
     fn async_snapshot(&self, ctx: SnapContext<'_>, cb: Callback<Self::Snap>) -> Result<()>;
