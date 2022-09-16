4837bed47d57497548fd9414aa6ec985ede77f70
3pointer <luancheng@pingcap.com>
2022-09-16 17:15:37 +0800

    add test
    Signed-off-by: 3pointer <luancheng@pingcap.com>

--- tests/integrations/raftstore/test_snap_recovery.rs
+++ tests/integrations/raftstore/test_snap_recovery.rs
@@ -64,3 +64,49 @@ fn test_check_pending_admin() {
         }
     });
 }
+
+#[test]
+fn test_check_pending_admin() {
+    let mut cluster = new_server_cluster(0, 3);
+    cluster.pd_client.disable_default_operator();
+    cluster.cfg.raft_store.store_io_pool_size = 0;
+
+    cluster.run();
+
+    // write a key to let leader stuck.
+    cluster.must_put(b"k", b"v");
+    must_get_equal(&cluster.get_engine(1), b"k", b"v");
+    must_get_equal(&cluster.get_engine(2), b"k", b"v");
+    must_get_equal(&cluster.get_engine(3), b"k", b"v");
+
+    // add filter to make leader cannot commit apply
+    cluster.add_send_filter(CloneFilterFactory(
+        RegionPacketFilter::new(1, 1)
+            .msg_type(MessageType::MsgAppendResponse)
+            .direction(Direction::Recv),
+    ));
+
+    let router = cluster.sim.wl().get_router(1).unwrap();
+
+    let (tx, mut rx) = futures::channel::mpsc::unbounded();
+
+    router.broadcast_normal(|| {
+        PeerMsg::SignificantMsg(SignificantMsg::SnapshotRecoveryWaitApply(SnapshotRecoveryWaitApplySyncer::new(1, tx.clone())))
+    });
+
+    assert_eq!(rx.recv_timeout(Duration::from_secs(3)), Ok(3));
+
+    // clear filter so we can make pending admin requests finished.
+    cluster.clear_send_filters();
+
+
+    let (tx, mut rx) = futures::channel::mpsc::unbounded();
+    router.broadcast_normal(|| {
+        PeerMsg::SignificantMsg(SignificantMsg::SnapshotRecoveryWaitApply(SnapshotRecoveryWaitApplySyncer::new(1, tx.clone())))
+    });
+
+    futures::executor::block_on(async {
+        let r = rx.next().await;
+        assert_eq!(r, 1);
+    });
+}
