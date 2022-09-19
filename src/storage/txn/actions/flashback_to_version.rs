9b9480d5c3625063ad91ada787271fd8b453fb28
JmPotato <ghzpotato@gmail.com>
2022-09-19 14:41:32 +0800

    Address the comments
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- src/storage/txn/actions/flashback_to_version.rs
+++ src/storage/txn/actions/flashback_to_version.rs
@@ -1,6 +1,5 @@
-// Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
+// Copyright 2022 TiKV Project Authors. Licensed under Apache-2.0.
 
-// #[PerformanceCriticalPath]
 use txn_types::{Key, Lock, LockType, TimeStamp, Write, WriteType};
 
 use crate::storage::{
