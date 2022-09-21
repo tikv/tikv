2b6474b392daaa470d913c68f6783ee8f8f5cb0b
Yilin Chen <sticnarf@gmail.com>
2022-09-21 14:16:47 +0800

    Revert "tikv_util: introduce future channel (#13407)"
    This reverts commit 7382d4eddfe8a0e6001fdc5b7b8540c0a561d034.

--- tests/benches/channel/bench_channel.rs
+++ tests/benches/channel/bench_channel.rs
@@ -113,8 +113,8 @@ fn bench_crossbeam_channel(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_receiver_stream_unbounded_batch(b: &mut Bencher) {
-    let (tx, rx) = mpsc::future::unbounded::<i32>(mpsc::future::WakePolicy::TillReach(8));
+fn bench_receiver_stream_batch(b: &mut Bencher) {
+    let (tx, rx) = mpsc::batch::bounded::<i32>(128, 8);
     for _ in 0..1 {
         let tx1 = tx.clone();
         thread::spawn(move || {
@@ -124,9 +124,12 @@ fn bench_receiver_stream_unbounded_batch(b: &mut Bencher) {
         });
     }
 
-    let rx = mpsc::future::BatchReceiver::new(rx, 32, Vec::new, Vec::push);
-
-    let mut rx = Some(block_on(rx.into_future()).1);
+    let mut rx = Some(mpsc::batch::BatchReceiver::new(
+        rx,
+        32,
+        Vec::new,
+        mpsc::batch::VecCollector,
+    ));
 
     b.iter(|| {
         let mut count = 0;
@@ -147,8 +150,8 @@ fn bench_receiver_stream_unbounded_batch(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_receiver_stream_unbounded_nobatch(b: &mut Bencher) {
-    let (tx, rx) = mpsc::future::unbounded::<i32>(mpsc::future::WakePolicy::Immediately);
+fn bench_receiver_stream(b: &mut Bencher) {
+    let (tx, rx) = mpsc::batch::bounded::<i32>(128, 1);
     for _ in 0..1 {
         let tx1 = tx.clone();
         thread::spawn(move || {
@@ -158,7 +161,7 @@ fn bench_receiver_stream_unbounded_nobatch(b: &mut Bencher) {
         });
     }
 
-    let mut rx = Some(block_on(rx.into_future()).1);
+    let mut rx = Some(rx);
     b.iter(|| {
         let mut count = 0;
         let mut rx1 = rx.take().unwrap();
