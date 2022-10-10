d91c0766efc6cd93618a7daf46e516343018ec62
SpadeA-Tang <u6748471@anu.edu.au>
2022-10-10 18:03:40 +0800

    update
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- tests/failpoints/cases/test_local_read.rs
+++ tests/failpoints/cases/test_local_read.rs
@@ -4,10 +4,12 @@ use std::{sync::Arc, thread, time::Duration};
 
 use grpcio::{ChannelBuilder, Environment};
 use kvproto::{
-    kvrpcpb::{Context, GetRequest, Op},
+    kvrpcpb::{Context, RawGetRequest},
     tikvpb_grpc::TikvClient,
 };
-use test_raftstore::{get_tso, new_mutation, new_peer, new_server_cluster, PeerClient};
+use test_raftstore::{
+    must_get_equal, must_get_none, must_raw_get, must_raw_put, new_peer, new_server_cluster,
+};
 use tikv_util::HandyRwLock;
 
 // The test mocks the situation that just after passing the lease check, even
@@ -19,30 +21,12 @@ fn test_consistency_after_lease_pass() {
     pd_client.disable_default_operator();
     cluster.run();
     let leader = new_peer(1, 1);
-    cluster.must_transfer_leader(1, leader.clone());
+    cluster.must_transfer_leader(1, leader);
 
     // Create clients.
     let env = Arc::new(Environment::new(1));
     let channel = ChannelBuilder::new(Arc::clone(&env)).connect(&cluster.sim.rl().get_addr(1));
     let client = TikvClient::new(channel);
-    let leader_client = PeerClient::new(&cluster, 1, leader);
-
-    let _ = leader_client.must_kv_write(
-        &pd_client,
-        vec![new_mutation(Op::Put, &b"key1"[..], &b"value1"[..])],
-        b"key1".to_vec(),
-    );
-
-    // Ensure the request is executed by the local reader
-    fail::cfg("localreader_before_redirect", "panic").unwrap();
-
-    // Lease read works correctly
-    leader_client.must_kv_read_equal(b"key1".to_vec(), b"value1".to_vec(), get_tso(&pd_client));
-
-    // we pause just after pass the lease check, and then remove the peer. We can
-    // still read the relevant value as we should have already got the snapshot when
-    // passing the lease check.
-    fail::cfg("after_pass_lease_check", "pause").unwrap();
 
     let region = cluster.get_region(&b"key1"[..]);
     let region_id = region.id;
@@ -53,28 +37,40 @@ fn test_consistency_after_lease_pass() {
     ctx.set_peer(leader.clone());
     ctx.set_region_epoch(region.get_region_epoch().clone());
 
-    let mut get_req = GetRequest::default();
+    must_raw_put(&client, ctx.clone(), b"key1".to_vec(), b"value1".to_vec());
+    must_get_equal(&cluster.get_engine(1), b"key1", b"value1");
+
+    // Ensure the request is executed by the local reader
+    fail::cfg("localreader_before_redirect", "panic").unwrap();
+
+    // Lease read works correctly
+    assert_eq!(
+        must_raw_get(&client, ctx.clone(), b"key1".to_vec()).unwrap(),
+        b"value1".to_vec()
+    );
+
+    // we pause just after pass the lease check, and then remove the peer. We can
+    // still read the relevant value as we should have already got the snapshot when
+    // passing the lease check.
+    fail::cfg("after_pass_lease_check", "pause").unwrap();
+
+    let mut get_req = RawGetRequest::default();
     get_req.set_context(ctx);
     get_req.key = b"key1".to_vec();
-    get_req.version = get_tso(&pd_client);
-    let mut receiver = client.kv_get_async(&get_req).unwrap();
+    let mut receiver = client.raw_get_async(&get_req).unwrap();
 
     thread::sleep(Duration::from_millis(200));
 
+    let mut peer = leader.clone();
     cluster.must_transfer_leader(1, new_peer(2, 2));
+    pd_client.must_remove_peer(region_id, leader);
+    peer.id += 1000;
+    fail::cfg("apply_snap_cleanup_range", "pause").unwrap();
+    pd_client.must_add_peer(region_id, peer);
 
-    assert!(
-        !cluster
-            .async_remove_peer(region_id, leader)
-            .unwrap()
-            .recv()
-            .unwrap()
-            .get_header()
-            .has_error()
-    );
-
-    // Wait for data to be cleaned
     thread::sleep(Duration::from_millis(2000));
+    // Wait for data to be cleaned
+    must_get_none(&cluster.get_engine(1), b"key1");
     fail::cfg("after_pass_lease_check", "off").unwrap();
 
     assert_eq!(b"value1", receiver.receive_sync().unwrap().1.get_value());
