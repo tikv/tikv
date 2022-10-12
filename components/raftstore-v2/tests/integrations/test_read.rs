2fbc0d8b4cf694fbebbc8957c835e724740dd806
qi.xu <tonxuqi@outlook.com>
2022-10-13 05:52:11 +0800

    fix local read test case
    Signed-off-by: qi.xu <tonxuqi@outlook.com>

--- components/raftstore-v2/tests/integrations/test_read.rs
+++ components/raftstore-v2/tests/integrations/test_read.rs
@@ -251,16 +251,7 @@ fn test_local_read() {
     request_inner.set_cmd_type(CmdType::Snap);
     req.mut_requests().push(request_inner);
 
-    // FIXME: Get snapshot from local reader, but it will fail as the leader has not
-    // applied in the current term (due to unimplementation of ApplyRes).
-    let resp = block_on(async { router.get_snapshot(req.clone()).await.unwrap_err() });
-    assert!(
-        resp.get_header()
-            .get_error()
-            .get_message()
-            .contains("Fail to get snapshot ")
-    );
-
+    block_on(async { router.get_snapshot(req.clone()).await.unwrap() });
     let res = router.query(region_id, req.clone()).unwrap();
     let resp = res.read().unwrap();
     // The read index will be 0 as the retry process in the `get_snapshot` will
