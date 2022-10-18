8ce2bd8887b7ebaa750267ca210eb2856dd564a1
hillium <yujuncen@pingcap.com>
2022-10-18 12:01:33 +0800

    cargo fix
    Signed-off-by: hillium <yujuncen@pingcap.com>

--- components/backup-stream/src/event_loader.rs
+++ components/backup-stream/src/event_loader.rs
@@ -37,7 +37,7 @@ use crate::{
     errors::{ContextualResultExt, Error, Result},
     metrics,
     router::{ApplyEvent, ApplyEvents, Router},
-    subscription_track::{RegionSubscription, SubscriptionTracer, TwoPhaseResolver},
+    subscription_track::{SubscriptionTracer, TwoPhaseResolver},
     try_send,
     utils::{self, RegionPager},
     Task,
