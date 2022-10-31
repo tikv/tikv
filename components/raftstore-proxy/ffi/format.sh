01454150386e05c978a8970613b2426354d0fd0a
Calvin Neo <CalvinNeo@users.noreply.github.com>
2022-10-31 17:06:29 +0800

    Depend all proxy's crates by workspace (#209)

--- components/raftstore-proxy/ffi/format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-base_path=$(cd $(dirname $0); pwd)
-
-set -xe
-
-clang-format -style google -i ${base_path}/src/RaftStoreProxyFFI/*.h
--- components/raftstore-proxy/ffi/src/RaftStoreProxyFFI/VersionCheck.h
+++
@@ -1,9 +0,0 @@
-// 5.1
-
-#pragma once
-
-#include "@version"
-
-namespace DB {
-constexpr uint32_t RAFT_STORE_PROXY_MAGIC_NUMBER = 0x13579BDF;
-}  // namespace DB
