c63a077d84917dc5628736814c3a3c138398cf60
Calvin Neo <CalvinNeo@users.noreply.github.com>
2022-11-01 16:07:19 +0800

    Fix failed make (#211)

--- ci_check.sh
+++ ci_check.sh
@@ -38,6 +38,7 @@ elif [[ $M == "testnew" ]]; then
     cargo test --package proxy_tests --test proxy server_cluster_test
     # tests based on new-mock-engine-store, for some tests not available for new proxy
     cargo test --package proxy_tests --test proxy proxy
+    make debug
 elif [[ $M == "debug" ]]; then
     # export RUSTC_WRAPPER=~/.cargo/bin/sccache
     export ENGINE_LABEL_VALUE=tiflash
