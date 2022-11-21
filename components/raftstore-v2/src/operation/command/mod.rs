9cb8501bff93ce1c03600b100a1e42475bf8a0a2
joccau <zak.zhao@pingcap.com>
2022-11-21 19:36:19 +0800

    remove duplidate code
    Signed-off-by: joccau <zak.zhao@pingcap.com>

--- components/raftstore-v2/src/operation/command/mod.rs
+++ components/raftstore-v2/src/operation/command/mod.rs
@@ -465,7 +465,6 @@ impl<EK: KvEngine, R: ApplyResReporter> Apply<EK, R> {
                 AdminCmdType::InvalidAdmin => {
                     return Err(box_err!("invalid admin command type"));
                 }
-                AdminCmdType::BatchSwitchWitness => unimplemented!(),
             };
 
             self.push_admin_result(admin_result);
