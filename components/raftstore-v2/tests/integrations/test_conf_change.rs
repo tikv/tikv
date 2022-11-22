03dab3a158bacaa82ed7f4ef5bc167ca1c980895
bufferflies <1045931706@qq.com>
2022-11-22 09:45:16 +0800

    remote msg append by raft tick
    Signed-off-by: bufferflies <1045931706@qq.com>

--- components/raftstore-v2/tests/integrations/test_conf_change.rs
+++ components/raftstore-v2/tests/integrations/test_conf_change.rs
@@ -3,12 +3,9 @@
 use std::{self, time::Duration};
 
 use engine_traits::{OpenOptions, Peekable, TabletFactory};
-use kvproto::{
-    raft_cmdpb::{AdminCmdType, CmdType, Request},
-    raft_serverpb::RaftMessage,
-};
+use kvproto::raft_cmdpb::{AdminCmdType, CmdType, Request};
 use raft::prelude::ConfChangeType;
-use raftstore_v2::router::PeerMsg;
+use raftstore_v2::router::{PeerMsg, PeerTick};
 use tikv_util::store::new_learner_peer;
 
 use crate::cluster::Cluster;
@@ -17,7 +14,6 @@ use crate::cluster::Cluster;
 fn test_simple_change() {
     let cluster = Cluster::with_node_count(2, None);
     let region_id = 2;
-    let learner_peer = new_learner_peer(cluster.node(1).id(), 10);
     let router0 = cluster.router(0);
     let mut req = router0.new_request_for(2);
     let admin_req = req.mut_admin_request();
@@ -54,24 +50,14 @@ fn test_simple_change() {
         meta.raft_status.soft_state.leader_id,
         req.get_header().get_peer().get_id()
     );
+    // Trigger the raft tick to replica the log to the learner and execute the
+    // snapshot task.
+    router0
+        .send(region_id, PeerMsg::Tick(PeerTick::Raft))
+        .unwrap();
+    cluster.dispatch(region_id, vec![]);
 
-    let mut raft_msg = RaftMessage::default();
-    raft_msg.set_region_id(region_id);
-    raft_msg.set_to_peer(learner_peer.clone());
-    raft_msg.set_from_peer(leader_peer.clone());
-
-    let raft_message = raft_msg.mut_message();
-    raft_message.set_msg_type(raft::prelude::MessageType::MsgAppendResponse);
-    raft_message.set_from(leader_peer.id);
-    raft_message.set_to(learner_peer.id);
-    raft_message.set_term(meta.raft_status.hard_state.term);
-    raft_message.set_reject(true);
-    raft_message.set_request_snapshot(1);
-    // trigger the leader send snapshot to the new learner, it will create new
-    // snapshot generate task.
-    let msgs = vec![Box::new(raft_msg)];
-    cluster.dispatch(region_id, msgs);
-
+    // write one kv after snapshot
     let (key, val) = (b"key", b"value");
     let mut write_req = router0.new_request_for(region_id);
     let mut put_req = Request::default();
@@ -79,18 +65,16 @@ fn test_simple_change() {
     put_req.mut_put().set_key(key.to_vec());
     put_req.mut_put().set_value(val.to_vec());
     write_req.mut_requests().push(put_req);
-    // trigger the leader execute snapshot generator task.
     let (msg, _) = PeerMsg::raft_command(write_req.clone());
-    router0.send(2, msg).unwrap();
-
-    // trigger the learner execute persisted log.
+    router0.send(region_id, msg).unwrap();
     std::thread::sleep(Duration::from_millis(1000));
     cluster.dispatch(region_id, vec![]);
 
     let meta = router1
         .must_query_debug_info(region_id, Duration::from_secs(3))
         .unwrap();
-    // the learner truncated index muse be equal the leader applied index
+    // the learner truncated index muse be equal the leader applied index and can
+    // read the new written kv.
     assert_eq!(match_index, meta.raft_apply.truncated_state.index);
     assert!(meta.raft_apply.applied_index >= match_index);
     let tablet_factory = cluster.node(1).tablet_factory();
