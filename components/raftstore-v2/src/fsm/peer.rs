d192187f778ce8af0330ceec4335fe151fbbb51e
tabokie <xy.tao@outlook.com>
2022-11-25 14:39:45 +0800

    tick on start
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- components/raftstore-v2/src/fsm/peer.rs
+++ components/raftstore-v2/src/fsm/peer.rs
@@ -177,6 +177,8 @@ impl<'a, EK: KvEngine, ER: RaftEngine, T: Transport> PeerFsmDelegate<'a, EK, ER,
 
     fn on_start(&mut self) {
         self.schedule_tick(PeerTick::Raft);
+        self.schedule_tick(PeerTick::PdHeartbeat);
+        self.schedule_tick(PeerTick::RaftLogGc);
         if self.fsm.peer.storage().is_initialized() {
             self.fsm.peer.schedule_apply_fsm(self.store_ctx);
         }
