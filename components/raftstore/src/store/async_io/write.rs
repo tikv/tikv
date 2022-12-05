694d181e98762df0062cbad5187bfb04f1e54ab6
tabokie <xy.tao@outlook.com>
2022-12-05 10:34:23 +0800

    add a test and address comments
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- components/raftstore/src/store/async_io/write.rs
+++ components/raftstore/src/store/async_io/write.rs
@@ -27,7 +27,7 @@ use protobuf::Message;
 use raft::eraftpb::Entry;
 use tikv_util::{
     box_err,
-    config::{Tracker, VersionTrack},
+    config::{ReadableSize, Tracker, VersionTrack},
     debug, info, slow_log,
     sys::thread::StdThreadBuildWrapper,
     thd_name,
@@ -54,7 +54,7 @@ const KV_WB_SHRINK_SIZE: usize = 1024 * 1024;
 const KV_WB_DEFAULT_SIZE: usize = 16 * 1024;
 const RAFT_WB_SHRINK_SIZE: usize = 10 * 1024 * 1024;
 const RAFT_WB_DEFAULT_SIZE: usize = 256 * 1024;
-const RAFT_WB_SPLIT_SIZE: usize = 1024 * 1024 * 1024;
+const RAFT_WB_SPLIT_SIZE: usize = ReadableSize::gb(1).0 as usize;
 
 /// Notify the event to the specified region.
 pub trait PersistedNotifier: Clone + Send + 'static {
@@ -361,11 +361,12 @@ where
     EK: KvEngine,
     ER: RaftEngine,
 {
-    // When a single batch becomes too large, we uses multiple batches each contains atomic writes.
+    // When a single batch becomes too large, we uses multiple batches each containing atomic
+    // writes.
     pub raft_wbs: Vec<ER::LogBatch>,
     // Write states once for a region everytime writing to disk.
     // These states only corresponds to entries inside `raft_wbs.last()`. States for other write
-    // batches must be added early.
+    // batches must be inlined early.
     pub raft_states: HashMap<u64, RaftLocalState>,
     pub extra_batch_write: ExtraBatchWrite<EK::WriteBatch>,
     pub state_size: usize,
@@ -373,6 +374,7 @@ where
     pub persisted_cbs: Vec<Box<dyn FnOnce() + Send>>,
     // region_id -> (peer_id, ready_number)
     pub readies: HashMap<u64, (u64, u64)>,
+    pub(crate) raft_wb_split_size: usize,
 }
 
 impl<EK, ER> WriteTaskBatch<EK, ER>
@@ -389,6 +391,7 @@ where
             tasks: vec![],
             persisted_cbs: vec![],
             readies: HashMap::default(),
+            raft_wb_split_size: RAFT_WB_SPLIT_SIZE,
         }
     }
 
@@ -440,28 +443,32 @@ where
             panic!("task is not valid: {:?}", e);
         }
 
-        if self.raft_wbs.last().unwrap().persist_size() >= RAFT_WB_SPLIT_SIZE {
+        if self.raft_wb_split_size > 0
+            && self.raft_wbs.last().unwrap().persist_size() >= self.raft_wb_split_size
+        {
             self.flush_states_to_raft_wb(raft_engine);
-            self.raft_wbs.push(raft_engine.log_batch(16));
+            self.raft_wbs
+                .push(raft_engine.log_batch(RAFT_WB_DEFAULT_SIZE));
         }
+
         let raft_wb = self.raft_wbs.last_mut().unwrap();
+        if let Some(wb) = task.raft_wb.take() {
+            raft_wb.merge(wb).unwrap();
+        }
         raft_wb
             .append(task.region_id, std::mem::take(&mut task.entries))
             .unwrap();
         if let Some((from, to)) = task.cut_logs {
             raft_wb.cut_logs(task.region_id, from, to);
         }
-        if let Some(wb) = task.raft_wb.take() {
-            raft_wb.merge(wb).unwrap();
-        }
 
-        self.state_size += self
-            .extra_batch_write
-            .merge(task.region_id, &mut task.extra_write);
         if let Some(raft_state) = task.raft_state.take()
             && self.raft_states.insert(task.region_id, raft_state).is_none() {
             self.state_size += std::mem::size_of::<RaftLocalState>();
         }
+        self.state_size += self
+            .extra_batch_write
+            .merge(task.region_id, &mut task.extra_write);
 
         if let Some(prev_readies) = self
             .readies
@@ -510,7 +517,8 @@ where
             + self
                 .raft_wbs
                 .iter()
-                .fold(0, |acc, wb| acc + wb.persist_size())
+                .map(|wb| wb.persist_size())
+                .sum::<usize>()
     }
 
     fn before_write_to_db(&mut self, engine: &ER, metrics: &StoreWriteMetrics) {
