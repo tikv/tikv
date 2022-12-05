d66dd544856d9b6e8f60e8ce6c017e4444fbeda2
Xinye Tao <xy.tao@outlook.com>
2022-12-05 17:10:00 +0800

    Apply suggestions from code review
    Signed-off-by: Xinye Tao <xy.tao@outlook.com>
    
    Signed-off-by: Xinye Tao <xy.tao@outlook.com>

--- components/pd_client/src/client_v2.rs
+++ components/pd_client/src/client_v2.rs
@@ -69,6 +69,7 @@ use crate::PdFuture;
 fn request_timeout() -> Duration {
     fail_point!("pd_client_v2_request_timeout", |s| {
         use std::str::FromStr;
+
         use tikv_util::config::ReadableDuration;
         ReadableDuration::from_str(&s.unwrap()).unwrap().0
     });
@@ -411,6 +412,7 @@ async fn reconnect_loop(
     let backoff = (|| {
         fail_point!("pd_client_v2_backoff", |s| {
             use std::str::FromStr;
+
             use tikv_util::config::ReadableDuration;
             ReadableDuration::from_str(&s.unwrap()).unwrap().0
         });
