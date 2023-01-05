3457613ccba2b82943a2caea40fe8d5083745242
hongyunyan <649330952@qq.com>
2023-01-05 13:28:25 +0800

    fix test
    Signed-off-by: hongyunyan <649330952@qq.com>

--- engine_tiflash/src/engine.rs
+++ engine_tiflash/src/engine.rs
@@ -14,11 +14,10 @@ use std::{
 
 use engine_rocks::{RocksDbVector, RocksEngineIterator, RocksSnapshot};
 use engine_traits::{
-    CfOptionsExt, Checkpointable, Checkpointer, DbOptions, DbOptionsExt, Error, IterOptions,
-    Iterable, KvEngine, Peekable, ReadOptions, Result, SyncMutable, CF_DEFAULT,
+    Checkpointable, Checkpointer, Error, IterOptions, Iterable, KvEngine, Peekable, ReadOptions,
+    Result, SyncMutable,
 };
 use rocksdb::{Writable, DB};
-use tikv::config::ConfigurableDb;
 
 use crate::{r2e, util::get_cf_handle};
 
@@ -243,46 +242,6 @@ impl SyncMutable for RocksEngine {
         Ok(())
     }
 }
-// pub type ConfigRes = std::result::Result<(), Box<dyn std::error::Error>>;
-
-// impl ConfigurableDb for RocksEngine {
-//     fn set_db_config(&self, opts: &[(&str, &str)]) -> ConfigRes {
-//         self.set_db_options(opts).map_err(Box::from)
-//     }
-
-//     fn set_cf_config(&self, cf: &str, opts: &[(&str, &str)]) -> ConfigRes {
-//         self.set_options_cf(cf, opts).map_err(Box::from)
-//     }
-
-//     fn set_rate_bytes_per_sec(&self, rate_bytes_per_sec: i64) -> ConfigRes {
-//         let mut opt = self.get_db_options();
-//         opt.set_rate_bytes_per_sec(rate_bytes_per_sec)
-//             .map_err(Box::from)
-//     }
-
-//     fn set_rate_limiter_auto_tuned(&self, auto_tuned: bool) -> ConfigRes {
-//         let mut opt = self.get_db_options();
-//         opt.set_rate_limiter_auto_tuned(auto_tuned)
-//             .map_err(Box::new)?;
-//         // double check the new state
-//         let new_auto_tuned = opt.get_rate_limiter_auto_tuned();
-//         if new_auto_tuned == Some(auto_tuned) {
-//             Ok(())
-//         } else {
-//             Err(engine_traits::Status::with_error(
-//                 engine_traits::Code::IoError,
-//                 "fail to set rate_limiter_auto_tuned",
-//             )
-//             .into())
-//         }
-//     }
-
-//     fn set_shared_block_cache_capacity(&self, capacity: usize) -> ConfigRes {
-//         let opt = self.get_options_cf(CF_DEFAULT).unwrap(); // FIXME unwrap
-//         opt.set_block_cache_capacity(capacity as u64)
-//             .map_err(Box::from)
-//     }
-// }
 
 pub struct TiFlashCheckpointer {}
 
