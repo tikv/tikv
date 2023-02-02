ebe34aa54a22eaa44a4aa7a396acc16d1442e5ec
Calvin Neo <CalvinNeo@users.noreply.github.com>
2023-02-02 16:12:38 +0800

    Update components/raftstore/src/coprocessor/dispatcher.rs
    Co-authored-by: Jay <BusyJay@users.noreply.github.com>
    Signed-off-by: Calvin Neo <CalvinNeo@users.noreply.github.com>

--- components/raftstore/src/coprocessor/dispatcher.rs
+++ components/raftstore/src/coprocessor/dispatcher.rs
@@ -788,7 +788,7 @@ impl<E: KvEngine> CoprocessorHost<E> {
         true
     }
 
-    /// Ignore the message if one of the observers returns false.
+    /// Returns false if the message should not be stepped later.
     pub fn on_raft_message(&self, msg: &RaftMessage) -> bool {
         for observer in &self.registry.message_observers {
             let observer = observer.observer.inner();
