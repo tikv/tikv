4814e7b974343e4ea47cfbdaa8a09e4419e2bcd5
Neil Shen <overvenus@gmail.com>
2023-02-17 16:19:56 +0800

    fix test
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/raftstore/src/store/worker/read.rs
+++ components/raftstore/src/store/worker/read.rs
@@ -2062,14 +2062,18 @@ mod tests {
         cmd.set_requests(vec![req].into());
 
         // A peer can serve read_ts < safe_ts.
+        let safe_ts = TimeStamp::compose(2, 0);
         {
             let mut meta = store_meta.lock().unwrap();
             let delegate = meta.readers.get_mut(&1).unwrap();
-            delegate.read_progress.update_safe_ts(1, 2);
-            assert_eq!(delegate.read_progress.safe_ts(), 2);
+            delegate
+                .read_progress
+                .update_safe_ts(1, safe_ts.into_inner());
+            assert_eq!(delegate.read_progress.safe_ts(), safe_ts.into_inner());
         }
+        let read_ts_1 = TimeStamp::compose(1, 0);
         let mut data = [0u8; 8];
-        (&mut data[..]).encode_u64(1).unwrap();
+        (&mut data[..]).encode_u64(read_ts_1.into_inner()).unwrap();
         header.set_flag_data(data.into());
         cmd.set_header(header.clone());
         let (snap_tx, snap_rx) = channel();
@@ -2091,8 +2095,10 @@ mod tests {
                 .read_progress
                 .update_advance_resolved_ts_notify(notify.clone());
         }
+        // 201ms larger than safe_ts.
+        let read_ts_2 = TimeStamp::compose(safe_ts.physical() + 201, 0);
         let mut data = [0u8; 8];
-        (&mut data[..]).encode_u64(10).unwrap(); // Larger than safe_ts 2.
+        (&mut data[..]).encode_u64(read_ts_2.into_inner()).unwrap();
         header.set_flag_data(data.into());
         cmd.set_header(header.clone());
         let task = RaftCommand::<KvTestSnapshot>::new(
