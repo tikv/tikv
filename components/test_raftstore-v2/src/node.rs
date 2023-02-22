daa8a80ad9f848511c73c5d74c33d0748da0e5aa
SpadeA-Tang <u6748471@anu.edu.au>
2023-02-22 15:11:56 +0800

    remove useless comment
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- components/test_raftstore-v2/src/node.rs
+++ components/test_raftstore-v2/src/node.rs
@@ -105,7 +105,6 @@ impl Transport for ChannelTransport {
             Some(h) => {
                 h.send_raft_msg(msg)?;
                 if is_snapshot {
-                    // should report snapshot finish
                     let _ = core.routers[&from_store].report_snapshot_status(
                         region_id,
                         to_peer_id,
