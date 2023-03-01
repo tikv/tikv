00bd36d7f4deda512c86f4b55801b292340332cd
Jay Lee <BusyJayLee@gmail.com>
2023-03-01 12:10:51 +0800

    pd_client_v2: fix version race
    Version should be updated before broadcast updates, otherwise the
    update will be just ignore.
    
    Signed-off-by: Jay Lee <BusyJayLee@gmail.com>

--- components/pd_client/src/client_v2.rs
+++ components/pd_client/src/client_v2.rs
@@ -245,8 +245,9 @@ impl CachedRawClient {
         let latest_version = {
             let mut latest = self.core.latest.lock().unwrap();
             *latest = self.cache.clone();
+            let v = self.core.version.fetch_add(1, Ordering::Relaxed) + 1;
             let _ = self.core.on_reconnect_tx.send(());
-            self.core.version.fetch_add(1, Ordering::Relaxed) + 1
+            v
         };
         debug_assert!(self.cache_version < latest_version);
         self.cache_version = latest_version;
@@ -266,9 +267,9 @@ impl CachedRawClient {
                 .await
                 .is_ok()
             {
-                if current_version < latest_version.load(Ordering::Acquire) {
-                    return true;
-                }
+        if current_version < latest_version.load(Ordering::Acquire) {
+            return true;
+        }
             } else {
                 return false;
             }
