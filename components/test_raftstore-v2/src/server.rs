82ac84bb485d4cd2f7a2e6714e7170485efb6296
Spade  A <71589810+SpadeA-Tang@users.noreply.github.com>
2023-03-09 10:55:12 +0800

    integration test v2: mvcc resolve lock gc test (#14360)
    ref tikv/tikv#12842
    
    mvcc resolve lock gc test for v2
    
    Signed-off-by: Spade A <u6748471@anu.edu.au>
    
    Co-authored-by: Xinye Tao <xy.tao@outlook.com>
    Co-authored-by: Ti Chi Robot <ti-community-prow-bot@tidb.io>

--- components/test_raftstore-v2/src/server.rs
+++ components/test_raftstore-v2/src/server.rs
@@ -242,6 +242,7 @@ pub struct ServerMeta {
     sim_router: SimulateStoreTransport,
     sim_trans: SimulateServerTransport,
     raw_router: StoreRouter<RocksEngine, RaftTestEngine>,
+    gc_worker: GcWorker<TestRaftKv2>,
     rsmeter_cleanup: Box<dyn FnOnce()>,
 }
 
@@ -624,6 +625,7 @@ impl ServerCluster {
                 node,
                 server,
                 sim_router,
+                gc_worker,
                 sim_trans: simulate_trans,
                 rsmeter_cleanup,
             },
@@ -633,6 +635,10 @@ impl ServerCluster {
             .insert(node_id, concurrency_manager);
 
         Ok(node_id)
+    }
+
+    pub fn get_gc_worker(&self, node_id: u64) -> &GcWorker<TestRaftKv2> {
+        &self.metas.get(&node_id).unwrap().gc_worker
     }
 
     pub fn get_causal_ts_provider(&self, node_id: u64) -> Option<Arc<CausalTsProviderImpl>> {
