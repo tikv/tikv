6b57e224d45e7db59cafa0da984333dd1a0f1eed
joccau <zak.zhao@pingcap.com>
2023-03-17 17:15:30 +0800

    modify code
    Signed-off-by: joccau <zak.zhao@pingcap.com>

--- tests/integrations/config/mod.rs
+++ tests/integrations/config/mod.rs
@@ -788,7 +788,7 @@ fn test_serde_custom_tikv_config() {
         num_threads: 123,
         stream_channel_window: 123,
         import_mode_timeout: ReadableDuration::secs(1453),
-        memory_use_ratio: 0.15,
+        memory_use_ratio: 0.2,
     };
     value.panic_when_unexpected_key_or_data = true;
     value.gc = GcConfig {
