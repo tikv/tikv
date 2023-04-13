bb4871ace713b8c9a101e650fc19d5e704628d64
Neil Shen <overvenus@gmail.com>
2023-04-13 14:32:27 +0800

    server2: fix old value cache miss caused by missing scheduler
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/server/src/server2.rs
+++ components/server/src/server2.rs
@@ -188,6 +188,7 @@ struct TikvServer<ER: RaftEngine> {
     coprocessor_host: Option<CoprocessorHost<RocksEngine>>,
     concurrency_manager: ConcurrencyManager,
     env: Arc<Environment>,
+    cdc_worker: Option<Box<LazyWorker<cdc::Task>>>,
     cdc_scheduler: Option<Scheduler<cdc::Task>>,
     cdc_memory_quota: Option<MemoryQuota>,
     sst_worker: Option<Box<LazyWorker<String>>>,
@@ -324,6 +325,7 @@ where
             coprocessor_host: None,
             concurrency_manager,
             env,
+            cdc_worker: None,
             cdc_scheduler: None,
             cdc_memory_quota: None,
             sst_worker: None,
@@ -564,12 +566,8 @@ where
         self.core.to_stop.push(check_leader_worker);
 
         // Create cdc worker.
-        let cdc_worker = Box::new(LazyWorker::new("cdc"));
+        let mut cdc_worker = self.cdc_worker.take().unwrap();
         let cdc_scheduler = cdc_worker.scheduler();
-        let txn_extra_scheduler = cdc::CdcTxnExtraScheduler::new(cdc_scheduler.clone());
-        engines
-            .engine
-            .set_txn_extra_scheduler(Arc::new(txn_extra_scheduler));
         // Register cdc observer.
         let cdc_ob = cdc::CdcObserver::new(cdc_scheduler.clone());
         cdc_ob.register_to(self.coprocessor_host.as_mut().unwrap());
@@ -580,7 +578,7 @@ where
         );
         // Start cdc endpoint.
         let cdc_memory_quota = MemoryQuota::new(self.core.config.cdc.sink_memory_quota.0 as _);
-        let _cdc_endpoint = cdc::Endpoint::new(
+        let cdc_endpoint = cdc::Endpoint::new(
             self.core.config.server.cluster_id,
             &self.core.config.cdc,
             self.core.config.storage.engine == EngineType::RaftKv2,
@@ -597,15 +595,13 @@ where
             cdc_memory_quota.clone(),
             self.causal_ts_provider.clone(),
         );
-        // TODO: enable cdc.
-        // cdc_worker.start_with_timer(cdc_endpoint);
-        // self.core.to_stop.push(cdc_worker);
-        self.cdc_scheduler = Some(cdc_scheduler);
+        cdc_worker.start_with_timer(cdc_endpoint);
+        self.core.to_stop.push(cdc_worker);
         self.cdc_memory_quota = Some(cdc_memory_quota);
 
         // Create resolved ts.
         if self.core.config.resolved_ts.enable {
-            let rts_worker = Box::new(LazyWorker::new("resolved-ts"));
+            let mut rts_worker = Box::new(LazyWorker::new("resolved-ts"));
             // Register the resolved ts observer
             let resolved_ts_ob = resolved_ts::Observer::new(rts_worker.scheduler());
             resolved_ts_ob.register_to(self.coprocessor_host.as_mut().unwrap());
@@ -616,7 +612,7 @@ where
                     rts_worker.scheduler(),
                 )),
             );
-            let _rts_endpoint = resolved_ts::Endpoint::new(
+            let rts_endpoint = resolved_ts::Endpoint::new(
                 &self.core.config.resolved_ts,
                 rts_worker.scheduler(),
                 self.router.clone().unwrap(),
@@ -626,9 +622,8 @@ where
                 self.env.clone(),
                 self.security_mgr.clone(),
             );
-            // TODO: enable resolved_ts.
-            // rts_worker.start_with_timer(rts_endpoint);
-            // self.core.to_stop.push(rts_worker);
+            rts_worker.start_with_timer(rts_endpoint);
+            self.core.to_stop.push(rts_worker);
         }
 
         let server_config = Arc::new(VersionTrack::new(self.core.config.server.clone()));
@@ -1236,7 +1231,12 @@ impl<CER: ConfiguredRaftEngine> TikvServer<CER> {
         );
         let region_info_accessor = RegionInfoAccessor::new(&mut coprocessor_host);
 
-        let engine = RaftKv2::new(router.clone(), region_info_accessor.region_leaders());
+        let cdc_worker = Box::new(LazyWorker::new("cdc"));
+        let cdc_scheduler = cdc_worker.scheduler();
+        let txn_extra_scheduler = cdc::CdcTxnExtraScheduler::new(cdc_scheduler.clone());
+
+        let mut engine = RaftKv2::new(router.clone(), region_info_accessor.region_leaders());
+        engine.set_txn_extra_scheduler(Arc::new(txn_extra_scheduler));
 
         self.engines = Some(TikvEngines {
             raft_engine,
@@ -1246,6 +1246,8 @@ impl<CER: ConfiguredRaftEngine> TikvServer<CER> {
         self.node = Some(node);
         self.coprocessor_host = Some(coprocessor_host);
         self.region_info_accessor = Some(region_info_accessor);
+        self.cdc_worker = Some(cdc_worker);
+        self.cdc_scheduler = Some(cdc_scheduler);
 
         engines_info
     }
