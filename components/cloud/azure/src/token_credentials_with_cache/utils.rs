963e1389c01f56164f726200e8d6a0442f009e6a
Lucasliang <nkcs_lykx@hotmail.com>
2023-05-09 23:37:25 +0800

    Add extra uts.
    Signed-off-by: Lucasliang <nkcs_lykx@hotmail.com>

--- components/cloud/azure/src/token_credentials_with_cache/utils.rs
+++ components/cloud/azure/src/token_credentials_with_cache/utils.rs
@@ -3,6 +3,7 @@
 use std::sync::{Arc, Mutex, RwLock};
 
 use azure_core::auth::TokenResponse;
+use fail;
 use time::OffsetDateTime;
 
 /// Refresh time to use in seconds
@@ -10,6 +11,12 @@ pub(crate) const DEFAULT_REFRESH_TIME: i64 = 300;
 /// Timeout on refreshing token in seconds
 const DEFAULT_REFRESH_TOKEN_TIMEOUT: i64 = 10;
 
+fn config_default_refresh_token_timeout() -> i64 {
+    fail::fail_point!("mock_token_cache::refresh_timeout", |_| 1);
+    DEFAULT_REFRESH_TOKEN_TIMEOUT
+}
+
+#[derive(Clone)]
 pub(crate) struct TokenCache {
     /// Cached token
     cached_token: Arc<RwLock<Option<TokenResponse>>>,
@@ -28,6 +35,7 @@ impl Default for TokenCache {
 
 impl TokenCache {
     fn is_fresh(&self) -> bool {
+        fail::fail_point!("mock_token_cache::is_fresh", |_| { true });
         if let Some(token) = self.cached_token.read().unwrap().as_ref() {
             let current_time = OffsetDateTime::now_utc().unix_timestamp();
             let expiry_time = current_time + DEFAULT_REFRESH_TIME;
@@ -49,7 +57,8 @@ impl TokenCache {
                     break;
                 }
                 // Timeout to forcely trigger refreshing token.
-                if OffsetDateTime::now_utc().unix_timestamp() - now >= DEFAULT_REFRESH_TOKEN_TIMEOUT
+                if OffsetDateTime::now_utc().unix_timestamp() - now
+                    >= config_default_refresh_token_timeout()
                 {
                     break;
                 }
@@ -79,4 +88,38 @@ pub trait TokenCredentialWithCache {
     fn update_cached_token(&self, token: TokenResponse);
 
     fn get_cached_token(&self) -> TokenResponse;
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{thread, time::Duration};
+
+    use super::*;
+
+    #[test]
+    fn test_concurrently_refresh_token() {
+        let token_cache = TokenCache::default();
+        assert!(!token_cache.is_fresh());
+        let (token1, token2) = (token_cache.clone(), token_cache.clone());
+        let thread1 = thread::spawn(move || {
+            assert!(token1.need_update_token());
+            thread::sleep(Duration::from_secs(2));
+        });
+        let thread2 = thread::spawn(move || {
+            thread::sleep(Duration::from_millis(10));
+            let _f = fail::FailGuard::new("mock_token_cache::refresh_timeout", "return").unwrap();
+            // forcely exit as timeout.
+            assert!(token2.need_update_token());
+        });
+        let token3 = token_cache.clone();
+        let thread3 = thread::spawn(move || {
+            thread::sleep(Duration::from_millis(2023)); // sleep enough
+            // mock that token is refreshed.
+            let _f = fail::FailGuard::new("mock_token_cache::is_fresh", "return").unwrap();
+            assert!(!token3.need_update_token());
+        });
+        thread1.join().unwrap();
+        thread2.join().unwrap();
+        thread3.join().unwrap();
+    }
 }
