82393c23a6ce220f374a2b70574b9faa3b72bb88
Lucasliang <nkcs_lykx@hotmail.com>
2023-05-17 16:15:15 +0800

    Revert "Revert the support on caching TokenCredential."
    This reverts commit d52ff4654224a918667fe8d59cc518844684f282.
    
    Signed-off-by: Lucasliang <nkcs_lykx@hotmail.com>

--- components/cloud/azure/src/lib.rs
+++ components/cloud/azure/src/lib.rs
@@ -1,5 +1,10 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
 mod azblob;
+mod token_credentials_with_cache;
 
 pub use azblob::{AzureStorage, Config};
+pub use token_credentials_with_cache::{
+    certificate_credentials::ClientCertificateCredentialWithCache,
+    secret_credentials::ClientSecretCredentialWithCache,
+};
---
+++ components/cloud/azure/src/token_credentials_with_cache/mod.rs
@@ -0,0 +1,5 @@
+// Copyright 2023 TiKV Project Authors. Licensed under Apache-2.0.
+
+pub mod certificate_credentials;
+pub mod secret_credentials;
+mod utils;
