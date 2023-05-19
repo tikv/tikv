5c9d593578736ca8733c8d61888fb54559aa6ab2
Lucasliang <nkcs_lykx@hotmail.com>
2023-05-19 23:05:30 +0800

    Remove redundant TokenCredentialWithCache as `azure-sdk-for-rust` already supports it.
    Signed-off-by: Lucasliang <nkcs_lykx@hotmail.com>

--- components/cloud/azure/src/lib.rs
+++ components/cloud/azure/src/lib.rs
@@ -1,10 +1,7 @@
 // Copyright 2021 TiKV Project Authors. Licensed under Apache-2.0.
 
 mod azblob;
-mod token_credentials_with_cache;
+mod token_credentials;
 
 pub use azblob::{AzureStorage, Config};
-pub use token_credentials_with_cache::{
-    certificate_credentials::ClientCertificateCredentialWithCache,
-    secret_credentials::ClientSecretCredentialWithCache,
-};
+pub use token_credentials::certificate_credentials::ClientCertificateCredentialExt;
