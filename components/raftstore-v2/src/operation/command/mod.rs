0ea7fedc3ddf98770905a110d23b5612c2dda484
Neil Shen <overvenus@gmail.com>
2023-05-29 20:14:41 +0800

    raftstorev2: avoid unnecessary snapshot after split (#14830)
    close tikv/tikv#14787
    
    raftstorev2: avoid unnecessary snapshot after split
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/raftstore-v2/src/operation/command/mod.rs
+++ components/raftstore-v2/src/operation/command/mod.rs
@@ -68,7 +68,7 @@ mod write;
 pub use admin::{
     merge_source_path, report_split_init_finish, temp_split_path, AdminCmdResult, CatchUpLogs,
     CompactLogContext, MergeContext, RequestHalfSplit, RequestSplit, SplitFlowControl, SplitInit,
-    MERGE_IN_PROGRESS_PREFIX, MERGE_SOURCE_PREFIX, SPLIT_PREFIX,
+    SplitPendingAppend, MERGE_IN_PROGRESS_PREFIX, MERGE_SOURCE_PREFIX, SPLIT_PREFIX,
 };
 pub use control::ProposalControl;
 use pd_client::{BucketMeta, BucketStat};
