727b4f7674cd1953432b92ab4ea3830cda7a8bbc
cfzjywxk <lsswxrxr@163.com>
2023-06-07 21:27:41 +0800

    scheduler: use must_call for async write callback, add assertion for expected path (#14872)
    ref tikv/tikv#14838
    
    Signed-off-by: cfzjywxk <lsswxrxr@163.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- src/storage/txn/scheduler.rs
+++ src/storage/txn/scheduler.rs
@@ -818,6 +818,12 @@ impl<E: Engine, L: LockManager> TxnScheduler<E, L> {
                 Err(e) => {
                     if !Self::is_undetermined_error(&e) {
                         do_wake_up = false;
+                    } else {
+                        panic!(
+                            "undetermined error: {:?} cid={}, tag={}, process
+                        result={:?}",
+                            e, cid, tag, &pr
+                        );
                     }
                     ProcessResult::Failed {
                         err: StorageError::from(e),
@@ -1055,10 +1061,18 @@ impl<E: Engine, L: LockManager> TxnScheduler<E, L> {
             .unwrap();
     }
 
-    fn is_undetermined_error(_e: &tikv_kv::Error) -> bool {
-        // TODO: If there's some cases that `engine.async_write` returns error but it's
-        // still possible that the data is successfully written, return true.
-        false
+    // Return true if raftstore returns error and the underlying write status could
+    // not be decided.
+    fn is_undetermined_error(e: &tikv_kv::Error) -> bool {
+        if let tikv_kv::ErrorInner::Undetermined(err_msg) = &*(e.0) {
+            error!(
+                "undetermined error is encountered, exit the tikv-server msg={:?}",
+                err_msg
+            );
+            true
+        } else {
+            false
+        }
     }
 
     fn early_response(
