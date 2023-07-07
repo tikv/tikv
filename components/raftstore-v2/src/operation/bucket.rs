f2de2ccc1ef3a4ec7cdeabaa886542143831e5ae
Qi Xu <tonyxuqqi@outlook.com>
2023-07-07 12:41:13 -0700

    lint
    Signed-off-by: Qi Xu <tonyxuqqi@outlook.com>

--- components/raftstore-v2/src/operation/bucket.rs
+++ components/raftstore-v2/src/operation/bucket.rs
@@ -11,7 +11,7 @@ use raftstore::{
     coprocessor::RegionChangeEvent,
     store::{util, Bucket, BucketRange, ReadProgress, SplitCheckTask, Transport},
 };
-use slog::{error, info, warn};
+use slog::{error, warn};
 
 use crate::{
     batch::StoreContext,
