8992754b5a30bf4f619800b4fd01222c9f6fe013
lucasliang <nkcs_lykx@hotmail.com>
2023-07-12 20:23:25 +0800

    fix testing errors and remove unnecessary test cases.
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>

--- cmd/tikv-server/src/main.rs
+++ cmd/tikv-server/src/main.rs
@@ -218,11 +218,8 @@ fn main() {
         process::exit(1)
     }
 
-    let (service_event_tx, service_event_rx) = mpsc::unbounded();
     match config.storage.engine {
-        EngineType::RaftKv => server::server::run_tikv(config, service_event_tx, service_event_rx),
-        EngineType::RaftKv2 => {
-            server::server2::run_tikv(config, service_event_tx, service_event_rx)
-        }
+        EngineType::RaftKv => server::server::run_tikv(config),
+        EngineType::RaftKv2 => server::server2::run_tikv(config),
     }
 }
