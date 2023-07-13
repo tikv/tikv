82dea3a6b610a4cd60048ea4a544028cd7fb6832
CalvinNeo <calvinneo1995@gmail.com>
2023-07-13 17:22:58 +0800

    Parse orphan key
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- proxy_scripts/clippy.sh
+++ proxy_scripts/clippy.sh
@@ -4,7 +4,7 @@ export RUST_BACKTRACE=full
 export ENGINE_FEATURES="test-engine-kv-rocksdb test-engine-raft-raft-engine testexport"
 rustup component add clippy
 # TODO We use --manifest-path as a wordaround.
-cargo clippy --features "$ENGINE_FEATURES" --package proxy_ffi --no-deps -- -Dwarnings -A clippy::result_large_err -A clippy::clone_on_copy -A clippy::upper_case_acronyms -A clippy::missing_safety_doc
+cargo clippy --package proxy_ffi --features "$ENGINE_FEATURES" --manifest-path proxy_components/proxy_ffi/Cargo.toml --no-deps -- -Dwarnings -A clippy::result_large_err -A clippy::clone_on_copy -A clippy::upper_case_acronyms -A clippy::missing_safety_doc
 cargo clippy --package engine_store_ffi --features "$ENGINE_FEATURES" --features engine_tiflash/testexport --manifest-path proxy_components/engine_store_ffi/Cargo.toml --no-deps -- -Dwarnings -A clippy::result_large_err -A clippy::clone_on_copy -A clippy::upper_case_acronyms -A clippy::missing_safety_doc
 cargo clippy --package proxy_tests --features "$ENGINE_FEATURES" --features engine_tiflash/testexport --manifest-path proxy_tests/Cargo.toml --no-deps -- -Dwarnings -A clippy::result_large_err -A clippy::needless_borrow -A clippy::clone_on_copy -A clippy::upper_case_acronyms -A clippy::missing_safety_doc
 cargo clippy --package proxy_server --features "$ENGINE_FEATURES" --features engine_tiflash/testexport --manifest-path proxy_components/proxy_server/Cargo.toml --no-deps -- -Dwarnings -A clippy::result_large_err -A clippy::needless_borrow -A clippy::clone_on_copy -A clippy::upper_case_acronyms -A clippy::missing_safety_doc -A clippy::derive_partial_eq_without_eq
