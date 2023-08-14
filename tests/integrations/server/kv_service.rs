326d83c9d4789cde7d2d00d982e0ad37a59c1dd1
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-08-14 19:46:58 +0800

    raftstore: trigger compaction when no valid split key can be found (#15284) (#15301)
    close tikv/tikv#15282
    
    trigger compaction when no valid split key can be found
    
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>
    
    Co-authored-by: SpadeA-Tang <u6748471@anu.edu.au>
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>
    Co-authored-by: Spade  A <71589810+SpadeA-Tang@users.noreply.github.com>

--- tests/integrations/server/kv_service.rs
+++ tests/integrations/server/kv_service.rs
@@ -3,7 +3,7 @@
 use std::{
     char::from_u32,
     path::Path,
-    sync::*,
+    sync::{atomic::AtomicU64, *},
     thread,
     time::{Duration, Instant},
 };
@@ -1213,6 +1213,7 @@ fn test_double_run_node() {
             ConcurrencyManager::new(1.into()),
             CollectorRegHandle::new_for_test(),
             None,
+            Arc::new(AtomicU64::new(0)),
         )
         .unwrap_err();
     assert!(format!("{:?}", e).contains("already started"), "{:?}", e);
