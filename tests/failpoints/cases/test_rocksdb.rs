6e08331a8425c6b62d1d1bda0afbff502e05ab1a
SpadeA-Tang <u6748471@anu.edu.au>
2023-08-16 17:41:01 +0800

    update
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- tests/failpoints/cases/test_rocksdb.rs
+++ tests/failpoints/cases/test_rocksdb.rs
@@ -1,8 +1,12 @@
 // Copyright 2023 TiKV Project Authors. Licensed under Apache-2.0.
 
+use tikv_util::config::ReadableSize;
+
 #[test]
 fn test_write_buffer_manager() {
     use test_raftstore_v2::*;
     let count = 3;
     let mut cluster = new_node_cluster(0, count);
+    cluster.cfg.rocksdb.lock_write_buffer_limit = Some(ReadableSize::kb(1));
+    cluster.cfg.rocksdb.write_buffer_limit = Some(ReadableSize::kb(1));
 }
