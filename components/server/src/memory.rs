f3abf3162ba6a4ccaeba4dbf6404689775fad177
tonyxuqqi <tonyxuqi@outlook.com>
2023-08-17 14:43:42 +0200

    fix metrics
    Signed-off-by: tonyxuqqi <tonyxuqi@outlook.com>

--- components/server/src/memory.rs
+++ components/server/src/memory.rs
@@ -3,7 +3,7 @@
 use std::sync::Arc;
 
 use tikv::server::MEM_TRACE_SUM_GAUGE;
-use tikv_alloc::trace::MemoryTrace;
+use tikv_alloc::{trace::MemoryTrace, Id};
 use tikv_util::time::Instant;
 
 #[derive(Default)]
@@ -13,15 +13,32 @@ pub struct MemoryTraceManager {
 
 impl MemoryTraceManager {
     pub fn flush(&mut self, _now: Instant) {
+        let raft_router = "raft_router".to_string();
+        let apply_router = "apply_router".to_string();
         for provider in &self.providers {
             let provider_name = provider.name();
             let ids = provider.get_children_ids();
             for id in ids {
                 let sub_trace = provider.sub_trace(id);
                 let sub_trace_name = sub_trace.name();
-                MEM_TRACE_SUM_GAUGE
-                    .with_label_values(&[&format!("{}-{}", provider_name, sub_trace_name)])
-                    .set(sub_trace.sum() as i64)
+                if sub_trace_name == raft_router || sub_trace_name == apply_router {
+                    let alive = provider.sub_trace(Id::Name("alive"));
+                    let leak = provider.sub_trace(Id::Name("leak"));
+                    MEM_TRACE_SUM_GAUGE
+                        .with_label_values(&[&format!(
+                            "{}-{}-alive",
+                            provider_name, sub_trace_name
+                        )])
+                        .set(alive.sum() as i64);
+
+                    MEM_TRACE_SUM_GAUGE
+                        .with_label_values(&[&format!("{}-{}-leak", provider_name, sub_trace_name)])
+                        .set(leak.sum() as i64);
+                } else {
+                    MEM_TRACE_SUM_GAUGE
+                        .with_label_values(&[&format!("{}-{}", provider_name, sub_trace_name)])
+                        .set(sub_trace.sum() as i64);
+                }
             }
 
             MEM_TRACE_SUM_GAUGE
