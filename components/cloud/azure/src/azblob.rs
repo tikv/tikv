2bb6c0b77736b78e382383b2b09712802338fa79
Leavrth <jianjun.liao@outlook.com>
2023-08-22 11:58:17 +0800

    show the details of the error occurred when try to get the azure ad token
    Signed-off-by: Leavrth <jianjun.liao@outlook.com>

--- components/cloud/azure/src/azblob.rs
+++ components/cloud/azure/src/azblob.rs
@@ -517,7 +517,7 @@ impl ContainerBuilder for TokenCredContainerBuilder {
                 .token_cred
                 .get_token(&self.token_resource)
                 .await
-                .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, format!("{}", &e)))?;
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, format!("{:?}", &e)))?;
             let blob_service = BlobServiceClient::new(
                 self.account_name.clone(),
                 StorageCredentials::BearerToken(token.token.secret().into()),
@@ -749,7 +749,7 @@ impl AzureStorage {
             azure_core::Result::Ok(chunk)
         };
         let stream = stream::once(
-            t.map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, format!("{}", e))),
+            t.map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, format!("{:?}", e))),
         )
         .boxed()
         .into_async_read();
