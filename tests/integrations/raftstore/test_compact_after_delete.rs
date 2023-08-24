dda3a4145095e12b4b65ddc70d6af87c87777bd8
tabokie <xy.tao@outlook.com>
2023-08-24 07:10:15 +0200

    fix another titan enabled test
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- tests/integrations/raftstore/test_compact_after_delete.rs
+++ tests/integrations/raftstore/test_compact_after_delete.rs
@@ -98,7 +98,8 @@ fn test_node_compact_after_delete_v2() {
     // disable it
     cluster.cfg.raft_store.region_compact_min_redundant_rows = 10000000;
     cluster.cfg.raft_store.region_compact_check_step = Some(2);
-    cluster.cfg.rocksdb.titan.enabled = true;
+    // TODO: v2 doesn't support titan.
+    // cluster.cfg.rocksdb.titan.enabled = true;
     cluster.run();
 
     let region = cluster.get_region(b"");
@@ -169,7 +170,8 @@ fn test_node_compact_after_update_v2() {
     cluster.cfg.raft_store.region_compact_redundant_rows_percent = 40;
     cluster.cfg.raft_store.region_compact_min_redundant_rows = 50;
     cluster.cfg.raft_store.region_compact_check_step = Some(2);
-    cluster.cfg.rocksdb.titan.enabled = true;
+    // TODO: titan is not supported in v2.
+    // cluster.cfg.rocksdb.titan.enabled = true;
     cluster.run();
 
     let region = cluster.get_region(b"");
