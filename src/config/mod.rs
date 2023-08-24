671a4ea33adb7706edde65d5d45d139c774ed56a
SpadeA-Tang <u6748471@anu.edu.au>
2023-08-24 19:15:21 +0800

    fix test
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -5966,6 +5966,9 @@ mod tests {
         default_cfg
             .server
             .optimize_for(default_cfg.coprocessor.region_split_size());
+        default_cfg
+            .raft_store
+            .optimize_for(default_cfg.storage.engine == EngineType::RaftKv2);
         default_cfg.security.redact_info_log = Some(false);
         default_cfg.coprocessor.region_max_size = Some(default_cfg.coprocessor.region_max_size());
         default_cfg.coprocessor.region_max_keys = Some(default_cfg.coprocessor.region_max_keys());
