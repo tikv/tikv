d9f8ed31f958d156ba6e43f33f13253e4e562cac
Neil Shen <overvenus@gmail.com>
2023-08-29 17:03:38 +0800

    This is an automated cherry-pick of #15411
    close tikv/tikv#14864
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- src/config.rs
+++ src/config.rs
@@ -2527,6 +2527,7 @@ pub struct ResolvedTsConfig {
     pub advance_ts_interval: ReadableDuration,
     #[online_config(skip)]
     pub scan_lock_pool_size: usize,
+    pub memory_quota: ReadableSize,
 }
 
 impl ResolvedTsConfig {
@@ -2547,6 +2548,7 @@ impl Default for ResolvedTsConfig {
             enable: true,
             advance_ts_interval: ReadableDuration::secs(1),
             scan_lock_pool_size: 2,
+            memory_quota: ReadableSize::mb(256),
         }
     }
 }
