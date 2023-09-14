70447562becb513b14b9a71e55f4411d52340f88
Yang Zhang <yang.zhang@pingcap.com>
2023-09-13 21:42:09 -0700

    This is an automated cherry-pick of #15578
    close tikv/tikv#15580
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- tests/failpoints/cases/test_unsafe_recovery.rs
+++ tests/failpoints/cases/test_unsafe_recovery.rs
@@ -440,15 +440,24 @@ fn test_unsafe_recovery_demotion_reentrancy() {
     fail::remove("on_handle_apply_store_1");
 }
 
+<<<<<<< HEAD
 #[test]
 fn test_unsafe_recovery_create_destroy_reentrancy() {
     let mut cluster = new_server_cluster(0, 3);
+=======
+#[test_case(test_raftstore::new_node_cluster)]
+fn test_unsafe_recovery_rollback_merge() {
+    let mut cluster = new_cluster(0, 3);
+    cluster.cfg.raft_store.raft_store_max_leader_lease = ReadableDuration::millis(40);
+    cluster.cfg.raft_store.merge_check_tick_interval = ReadableDuration::millis(20);
+>>>>>>> 62c17991fd (unsafe recovery: Enable force leader to rollback merge (#15578))
     cluster.run();
     let nodes = Vec::from_iter(cluster.get_node_ids());
     assert_eq!(nodes.len(), 3);
 
     let pd_client = Arc::clone(&cluster.pd_client);
     pd_client.disable_default_operator();
+<<<<<<< HEAD
     let region = block_on(pd_client.get_region_by_id(1)).unwrap().unwrap();
 
     // Makes the leadership definite.
@@ -468,6 +477,28 @@ fn test_unsafe_recovery_create_destroy_reentrancy() {
     let region1_store0_peer = find_peer(&region1, nodes[0]).unwrap().to_owned();
     pd_client.must_remove_peer(region1.get_id(), region1_store0_peer);
     cluster.must_remove_region(nodes[0], region1.get_id());
+=======
+
+    for i in 0..10 {
+        cluster.must_put(format!("k{}", i).as_bytes(), b"v");
+    }
+
+    // Block merge commit, let go of the merge prepare.
+    fail::cfg("on_schedule_merge_ret_err", "return()").unwrap();
+
+    let region = pd_client.get_region(b"k1").unwrap();
+    cluster.must_split(&region, b"k2");
+
+    let left = pd_client.get_region(b"k1").unwrap();
+    let right = pd_client.get_region(b"k2").unwrap();
+
+    // Makes the leadership definite.
+    let left_peer_2 = find_peer(&left, nodes[2]).unwrap().to_owned();
+    let right_peer_2 = find_peer(&right, nodes[2]).unwrap().to_owned();
+    cluster.must_transfer_leader(left.get_id(), left_peer_2);
+    cluster.must_transfer_leader(right.get_id(), right_peer_2);
+    cluster.must_try_merge(left.get_id(), right.get_id());
+>>>>>>> 62c17991fd (unsafe recovery: Enable force leader to rollback merge (#15578))
 
     // Makes the group lose its quorum.
     cluster.stop_node(nodes[1]);
@@ -475,8 +506,13 @@ fn test_unsafe_recovery_create_destroy_reentrancy() {
     {
         let put = new_put_cmd(b"k2", b"v2");
         let req = new_request(
+<<<<<<< HEAD
             region2.get_id(),
             region2.get_region_epoch().clone(),
+=======
+            region.get_id(),
+            region.get_region_epoch().clone(),
+>>>>>>> 62c17991fd (unsafe recovery: Enable force leader to rollback merge (#15578))
             vec![put],
             true,
         );
@@ -486,11 +522,17 @@ fn test_unsafe_recovery_create_destroy_reentrancy() {
             .unwrap_err();
     }
 
+<<<<<<< HEAD
     cluster.must_enter_force_leader(region2.get_id(), nodes[0], vec![nodes[1], nodes[2]]);
+=======
+    cluster.must_enter_force_leader(left.get_id(), nodes[0], vec![nodes[1], nodes[2]]);
+    cluster.must_enter_force_leader(right.get_id(), nodes[0], vec![nodes[1], nodes[2]]);
+>>>>>>> 62c17991fd (unsafe recovery: Enable force leader to rollback merge (#15578))
 
     // Construct recovery plan.
     let mut plan = pdpb::RecoveryPlan::default();
 
+<<<<<<< HEAD
     let mut create = metapb::Region::default();
     create.set_id(101);
     create.set_end_key(b"random_key1".to_vec());
@@ -513,10 +555,59 @@ fn test_unsafe_recovery_create_destroy_reentrancy() {
     for _ in 0..20 {
         store_report = pd_client.must_get_store_report(nodes[0]);
         if store_report.is_some() {
+=======
+    let left_demote_peers: Vec<metapb::Peer> = left
+        .get_peers()
+        .iter()
+        .filter(|&peer| peer.get_store_id() != nodes[0])
+        .cloned()
+        .collect();
+    let mut left_demote = pdpb::DemoteFailedVoters::default();
+    left_demote.set_region_id(left.get_id());
+    left_demote.set_failed_voters(left_demote_peers.into());
+    let right_demote_peers: Vec<metapb::Peer> = right
+        .get_peers()
+        .iter()
+        .filter(|&peer| peer.get_store_id() != nodes[0])
+        .cloned()
+        .collect();
+    let mut right_demote = pdpb::DemoteFailedVoters::default();
+    right_demote.set_region_id(right.get_id());
+    right_demote.set_failed_voters(right_demote_peers.into());
+    plan.mut_demotes().push(left_demote);
+    plan.mut_demotes().push(right_demote);
+
+    // Triggers the unsafe recovery plan execution.
+    pd_client.must_set_unsafe_recovery_plan(nodes[0], plan.clone());
+    cluster.must_send_store_heartbeat(nodes[0]);
+
+    let mut demoted = false;
+    for _ in 0..10 {
+        let new_left = block_on(pd_client.get_region_by_id(left.get_id()))
+            .unwrap()
+            .unwrap();
+        let new_right = block_on(pd_client.get_region_by_id(right.get_id()))
+            .unwrap()
+            .unwrap();
+        assert_eq!(new_left.get_peers().len(), 3);
+        assert_eq!(new_right.get_peers().len(), 3);
+        demoted = new_left
+            .get_peers()
+            .iter()
+            .filter(|peer| peer.get_store_id() != nodes[0])
+            .all(|peer| peer.get_role() == metapb::PeerRole::Learner)
+            && new_right
+                .get_peers()
+                .iter()
+                .filter(|peer| peer.get_store_id() != nodes[0])
+                .all(|peer| peer.get_role() == metapb::PeerRole::Learner);
+        if demoted {
+>>>>>>> 62c17991fd (unsafe recovery: Enable force leader to rollback merge (#15578))
             break;
         }
         sleep_ms(100);
     }
+<<<<<<< HEAD
     assert_ne!(store_report, None);
     let report = store_report.unwrap();
     let peer_reports = report.get_peer_reports();
@@ -526,4 +617,9 @@ fn test_unsafe_recovery_create_destroy_reentrancy() {
     assert_eq!(reported_region.get_peers().len(), 1);
     assert_eq!(reported_region.get_peers()[0].get_id(), 102);
     fail::remove("on_handle_apply_store_1");
+=======
+    assert_eq!(demoted, true);
+
+    fail::remove("on_schedule_merge_ret_err");
+>>>>>>> 62c17991fd (unsafe recovery: Enable force leader to rollback merge (#15578))
 }
