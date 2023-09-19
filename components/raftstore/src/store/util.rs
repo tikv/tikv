b1512c11ced26a2516b1b91c27e0bef0edaaaf3d
bufferflies <1045931706@qq.com>
2023-09-19 21:28:39 +0800

    bucket split
    Signed-off-by: bufferflies <1045931706@qq.com>

--- components/raftstore/src/store/util.rs
+++ components/raftstore/src/store/util.rs
@@ -159,6 +159,20 @@ pub fn new_empty_snapshot(
     snapshot
 }
 
+pub fn gen_bucket_version(term: u64, current_version: u64) -> u64 {
+    //   term       logical counter
+    // |-----------|-----------|
+    //  high bits     low bits
+    // term: given 10s election timeout, the 32 bit means 1362 year running time
+    let current_version_term = current_version >> 32;
+    let bucket_version: u64 = if current_version_term == term {
+        current_version + 1
+    } else {
+        term << 32
+    };
+    bucket_version
+}
+
 const STR_CONF_CHANGE_ADD_NODE: &str = "AddNode";
 const STR_CONF_CHANGE_REMOVE_NODE: &str = "RemoveNode";
 const STR_CONF_CHANGE_ADDLEARNER_NODE: &str = "AddLearner";
