e3e6124c00f9dba71fbb9d39012122fa96a325b9
husharp <jinhao.hu@pingcap.com>
2023-10-07 10:32:59 +0800

    rebase
    Signed-off-by: husharp <jinhao.hu@pingcap.com>

--- src/storage/txn/commands/flashback_to_version_read_phase.rs
+++ src/storage/txn/commands/flashback_to_version_read_phase.rs
@@ -216,7 +216,7 @@ impl<S: Snapshot> ReadCommand<S> for FlashbackToVersionReadPhase {
                     }));
                 }
                 if next_write_key == self.start_key {
-                    info!("flashback to version fake prewrite"; "key_to_commit" => %key_to_commit, "end_key" => ?self.end_key, 
+                    info!("flashback to version fake prewrite"; "key_to_commit" => %next_write_key, "end_key" => ?self.end_key, 
                     "version" => self.version, "region_id" => self.ctx.get_region_id(), "start_ts" => self.start_ts, "commit_ts" => self.commit_ts);
                     // The start key from the client is actually a range which is used to limit the
                     // upper bound of this flashback when scanning data, so it may not be a real
