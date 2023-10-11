f006b93c96f95afc58c82805495327eb432c3eaf
glorv <glorvs@163.com>
2023-10-11 15:46:45 +0800

    add a unit test
    Signed-off-by: glorv <glorvs@163.com>

--- src/coprocessor/dag/mod.rs
+++ src/coprocessor/dag/mod.rs
@@ -209,3 +209,43 @@ fn handle_qe_stream_response(
         },
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use anyhow::anyhow;
+    use protobuf::Message;
+    use tidb_query_common::error::{Error as CommonError, EvaluateError, StorageError};
+
+    use super::*;
+    use crate::coprocessor::Error;
+
+    #[test]
+    fn test_handle_qe_response() {
+        // Ok Response
+        let ok_res = Ok((SelectResponse::default(), None));
+        let res = handle_qe_response(ok_res, true, Some(1)).unwrap();
+        assert!(res.can_be_cached);
+        assert_eq!(res.get_cache_last_version(), 1);
+        let mut select_res = SelectResponse::new();
+        Message::merge_from_bytes(&mut select_res, res.get_data()).unwrap();
+        assert!(!select_res.has_error());
+
+        // Storage Error
+        let storage_err = CommonError::from(StorageError(anyhow!("unknown")));
+        let res = handle_qe_response(Err(storage_err), false, None);
+        assert!(matches!(res, Err(Error::Other(_))));
+
+        // Evaluate Error
+        let err = CommonError::from(EvaluateError::DeadlineExceeded);
+        let res = handle_qe_response(Err(err.into()), false, None);
+        assert!(matches!(res, Err(Error::DeadlineExceeded)));
+
+        let err = CommonError::from(EvaluateError::InvalidCharacterString {
+            charset: "test".into(),
+        });
+        let res = handle_qe_response(Err(err.into()), false, None).unwrap();
+        let mut select_res = SelectResponse::new();
+        Message::merge_from_bytes(&mut select_res, res.get_data()).unwrap();
+        assert_eq!(select_res.get_error().get_code(), 1300);
+    }
+}
