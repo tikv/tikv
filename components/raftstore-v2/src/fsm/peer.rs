6e826308b9ca246ee5572bcdd24e6b26fd19c156
tonyxuqqi <tonyxuqi@outlook.com>
2023-10-16 12:28:57 -0700

    add more metrics for slow commit log diagnostics (#15716)
    ref tikv/tikv#15175
    
    Add more metrics for slow commit log duration investigation. 
    In this PR, it adds raft message process wait duration and exposes raft
    message recv by store counter. Together with raft-engine write duration,
    we can further narrow reason of the commit log duration. With this PR,
    we still cannot tell if the slowness comes from network or
    raft-client's (grpc client).
    
    Signed-off-by: tonyxuqqi <tonyxuqi@outlook.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- components/raftstore-v2/src/fsm/peer.rs
+++ components/raftstore-v2/src/fsm/peer.rs
@@ -247,8 +247,10 @@ impl<'a, EK: KvEngine, ER: RaftEngine, T: Transport> PeerFsmDelegate<'a, EK, ER,
     pub fn on_msgs(&mut self, peer_msgs_buf: &mut Vec<PeerMsg>) {
         for msg in peer_msgs_buf.drain(..) {
             match msg {
-                PeerMsg::RaftMessage(msg) => {
-                    self.fsm.peer.on_raft_message(self.store_ctx, msg);
+                PeerMsg::RaftMessage(msg, send_time) => {
+                    self.fsm
+                        .peer
+                        .on_raft_message(self.store_ctx, msg, send_time);
                 }
                 PeerMsg::RaftQuery(cmd) => {
                     self.on_receive_command(cmd.send_time, cmd.ch.read_tracker());
