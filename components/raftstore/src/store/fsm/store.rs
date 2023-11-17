30b578fdf20c1fd0d73a00ca26d57adb0242652f
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2023-11-17 15:21:47 +0800

    raftstore: gc uninitialized stale peer after merge (#15934) (#15939)
    close tikv/tikv#15919
    
    A "stale peer" refers to a peer that still exists on a TiKV node but
    has been removed from the raft group, typically through a confchange
    operation. TiKV performs regular checks and validations on its peers
    to ensure that no such stale peer exists.
    
    However, the current stale peer check is not enough when dealing with
    uninitialized stale peers that its region has been merged. These
    uninitialized stale peers are left indefinitely, consuming CPU, memory,
    and blocking resolved ts.
    
    This commit introduces an in-memory state for peers whose raft messages
    create uninitialized stale peers. The stale peer then sends a
    MsgCheckStalePeer message to the corresponding peer, validating whether
    it should be removed.
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>
    
    Co-authored-by: Neil Shen <overvenus@gmail.com>
    Co-authored-by: tonyxuqqi <tonyxuqi@outlook.com>
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- components/raftstore/src/store/fsm/store.rs
+++ components/raftstore/src/store/fsm/store.rs
@@ -674,6 +674,8 @@ where
             "region_id" => region_id,
             "current_region_epoch" => ?cur_epoch,
             "msg_type" => ?msg_type,
+            "to_peer_id" => ?from_peer.get_id(),
+            "to_peer_store_id" => ?from_peer.get_store_id(),
         );
 
         self.raft_metrics.message_dropped.stale_msg.inc();
@@ -692,6 +694,8 @@ where
             error!(?e;
                 "send gc message failed";
                 "region_id" => region_id,
+                "to_peer_id" => ?from_peer.get_id(),
+                "to_peer_store_id" => ?from_peer.get_store_id(),
             );
         }
     }
@@ -2376,6 +2380,7 @@ impl<'a, EK: KvEngine, ER: RaftEngine, T: Transport> StoreFsmDelegate<'a, EK, ER
             self.ctx.engines.clone(),
             region_id,
             target.clone(),
+            msg.get_from_peer().clone(),
         )?;
 
         // WARNING: The checking code must be above this line.
