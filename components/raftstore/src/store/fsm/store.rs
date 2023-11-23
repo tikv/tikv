19e4b0a44a9d66edbd8e88c21f0a3b93ce6e4ec3
lucasliang <nkcs_lykx@hotmail.com>
2023-11-23 10:31:16 +0800

    Polish codes.
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>

--- components/raftstore/src/store/fsm/store.rs
+++ components/raftstore/src/store/fsm/store.rs
@@ -2844,16 +2844,17 @@ impl<'a, EK: KvEngine, ER: RaftEngine, T: Transport> StoreFsmDelegate<'a, EK, ER
     fn on_wake_up_regions(&self, abnormal_stores: Vec<u64>) {
         info!("try to wake up all hibernated regions in this store";
             "to_all" => abnormal_stores.is_empty());
+        let store_id = self.ctx.store_id();
         let meta = self.ctx.store_meta.lock().unwrap();
-        for region_id in meta.regions.keys() {
-            let region = &meta.regions[region_id];
+
+        for (region_id, region) in &meta.regions {
             // Check whether the current region is not found on abnormal stores. If so,
             // this region is not the target to be awaken.
-            if !region_on_stores(region, &abnormal_stores) {
+            if !region_on_stores(&region, &abnormal_stores) {
                 continue;
             }
             let peer = {
-                match find_peer(region, self.ctx.store_id()) {
+                match find_peer(&region, store_id) {
                     None => continue,
                     Some(p) => p.clone(),
                 }
