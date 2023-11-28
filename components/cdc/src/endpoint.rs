cbfb177f76e542d89978f29eb4c5a4168fc68011
Neil Shen <overvenus@gmail.com>
2023-11-28 18:23:43 +0800

    Apply suggestions from code review
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/cdc/src/endpoint.rs
+++ components/cdc/src/endpoint.rs
@@ -671,7 +671,7 @@ impl<T: 'static + RaftStoreRouter<E>, E: KvEngine> Endpoint<T, E> {
             scan_task_counter.fetch_sub(1, Ordering::Relaxed);
         });
         if scan_task_count + 1 > self.config.incremental_scan_concurrency_limit as isize {
-            warn!("cdc rejects registration, too many scan tasks";
+            debug!("cdc rejects registration, too many scan tasks";
                 "region_id" => region_id,
                 "conn_id" => ?conn_id,
                 "scan_task_count" => scan_task_count,
