24903885eb534d92e937d1a3f0d583b965055e22
SpadeA-Tang <u6748471@anu.edu.au>
2023-12-07 11:46:21 +0800

    u
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- components/engine_traits/src/memory_engine.rs
+++ components/engine_traits/src/memory_engine.rs
@@ -2,11 +2,14 @@
 
 use std::fmt::Debug;
 
-use crate::{Iterable, WriteBatchExt};
+use crate::{Iterable, WriteBatchExt, Snapshot};
 
 /// A memory enigne works as a region cache caching some regions to improve the
 /// read performance.
 pub trait MemoryEngine:
     WriteBatchExt + Iterable + Debug + Clone + Unpin + Send + Sync + 'static
 {
+    type Snapshot: Snapshot;
+    
+    fn snapshot(&self, region_id: u64, read_ts: u64) -> Self::Snapshot;
 }
