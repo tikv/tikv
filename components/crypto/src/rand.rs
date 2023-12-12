4a5288a82b9ce18358d1d6a7444be279b71330a9
Neil Shen <overvenus@gmail.com>
2023-12-12 16:38:35 +0800

    Update components/crypto/src/rand.rs
    Co-authored-by: lucasliang <nkcs_lykx@hotmail.com>
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/crypto/src/rand.rs
+++ components/crypto/src/rand.rs
@@ -4,14 +4,14 @@
 
 use openssl::{error::ErrorStack, rand};
 
-/// Return a random u64.
-pub fn rand_u64() -> Result<u64, ErrorStack> {
-    let mut rand_id = [0u8; 8];
-    rand::rand_bytes(&mut rand_id)?;
-    Ok(u64::from_ne_bytes(rand_id))
-}
-
 /// Fill buffer with cryptographically strong pseudo-random bytes.
 pub fn rand_bytes(buf: &mut [u8]) -> Result<(), ErrorStack> {
     rand::rand_bytes(buf)
+}
+
+/// Return a random u64.
+pub fn rand_u64() -> Result<u64, ErrorStack> {
+    let mut rand_id = [0u8; 8];
+    rand_bytes(&mut rand_id)?;
+    Ok(u64::from_ne_bytes(rand_id))
 }
