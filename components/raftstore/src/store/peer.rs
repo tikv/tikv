17c34da9388873a4cf0adf616077e742eee69fa2
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-02-01 07:03:24 +0800

    raftstore: check last heartbeat time before doing conf change remove node (#16174) (#16416)
    close tikv/tikv#15799
    
    Check the last heartbeat time before doing remove node operation. It defines 8*heartbeat interval as the threshold of slow peer. And if the remove node operation will lead to at least half of the peers are slow, then the remove node operation will fail.
    
    Signed-off-by: Qi Xu <tonyxuqqi@outlook.com>
    Signed-off-by: tonyxuqqi <tonyxuqi@outlook.com>
    
    Co-authored-by: Qi Xu <tonyxuqqi@outlook.com>
    Co-authored-by: tonyxuqqi <tonyxuqi@outlook.com>

--- components/raftstore/src/store/peer.rs
+++ components/raftstore/src/store/peer.rs
@@ -4740,6 +4740,7 @@ where
             changes.as_ref(),
             &cc,
             self.is_in_force_leader(),
+            &self.peer_heartbeats,
         )?;
 
         ctx.raft_metrics.propose.conf_change.inc();
