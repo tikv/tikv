4453976cb434eb199cbdff0c1ca0a66a78ccfb55
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-02-18 17:07:54 +0800

    engine: calculate table properties correctly for Titan (#16320) (#16332)
    close tikv/tikv#16319
    
    Signed-off-by: qupeng <qupeng@pingcap.com>
    
    Co-authored-by: qupeng <qupeng@pingcap.com>
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- src/storage/kv/test_engine_builder.rs
+++ src/storage/kv/test_engine_builder.rs
@@ -127,7 +127,9 @@ impl TestEngineBuilder {
                 _ => (*cf, RocksCfOptions::default()),
             })
             .collect();
-        let engine = RocksEngine::new(&path, None, cfs_opts, self.io_rate_limiter)?;
+        let resources = cfg_rocksdb.build_resources(Default::default());
+        let db_opts = cfg_rocksdb.build_opt(&resources, EngineType::RaftKv);
+        let engine = RocksEngine::new(&path, Some(db_opts), cfs_opts, self.io_rate_limiter)?;
         Ok(engine)
     }
 }
