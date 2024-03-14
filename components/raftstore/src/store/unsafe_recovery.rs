15121f69060ce46ff65d4106ad55236395ca9585
glorv <glorvs@163.com>
2024-03-14 14:35:58 +0800

    make apply before persist compatible with online unsafe recovery
    Signed-off-by: glorv <glorvs@163.com>

--- components/raftstore/src/store/unsafe_recovery.rs
+++ components/raftstore/src/store/unsafe_recovery.rs
@@ -165,6 +165,10 @@ pub enum ForceLeaderState {
         failed_stores: HashSet<u64>,
         ticks: usize,
     },
+    WaitForceCompact {
+        syncer: UnsafeRecoveryForceLeaderSyncer,
+        failed_stores: HashSet<u64>,
+    },
     PreForceLeader {
         syncer: UnsafeRecoveryForceLeaderSyncer,
         failed_stores: HashSet<u64>,
