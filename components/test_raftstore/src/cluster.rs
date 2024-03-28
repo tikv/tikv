62b31322a689492473b35a59a821294f6df860d8
cfzjywxk <cfzjywxk@gmail.com>
2024-03-28 14:41:57 +0800

    fix clippy
    Signed-off-by: cfzjywxk <cfzjywxk@gmail.com>

--- components/test_raftstore/src/cluster.rs
+++ components/test_raftstore/src/cluster.rs
@@ -1372,8 +1372,7 @@ where
                     .get_region()
                     .get_peers()
                     .iter()
-                    .filter(|p| p.get_id() == peer_id)
-                    .next()
+                    .find(|p| p.get_id() == peer_id)
                     .unwrap();
                 if peer.role == role {
                     return;
