c460303a5914809dde29b62e444c1712f1e11af8
SpadeA-Tang <u6748471@anu.edu.au>
2024-03-28 11:14:46 +0800

    hot_data
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- components/region_cache_memory_engine/src/memory_controller.rs
+++ components/region_cache_memory_engine/src/memory_controller.rs
@@ -1,6 +1,8 @@
 // Copyright 2024 TiKV Project Authors. Licensed under Apache-2.0.
 
-use std::sync::atomic::{self, AtomicBool, AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+
+use crossbeam::utils::CachePadded;
 
 use crate::{engine::SkiplistEngine, write_batch::NODE_OVERHEAD_SIZE_EXPECTATION};
 
@@ -13,6 +15,14 @@ pub(crate) enum MemoryUsage {
     HardLimitReached(usize),
 }
 
+#[derive(Debug)]
+struct HotData {
+    // Allocated memory for keys and values (node overhead is not included)
+    allocated: AtomicUsize,
+    // The number of writes that are buffered but not yet written.
+    pending_node_count: AtomicUsize,
+}
+
 /// MemoryController is used to control the memory usage of the region cache
 /// memory engine. The memory consumption is calculated by the allocated memory
 /// for keys and values, and the overhead of the skiplist where the overhead is
@@ -20,14 +30,10 @@ pub(crate) enum MemoryUsage {
 /// `NODE_OVERHEAD_SIZE_EXPECTATION`.
 #[derive(Debug)]
 pub struct MemoryController {
-    // Allocated memory for keys and values (node overhead is not included)
-    allocated: AtomicUsize,
+    hot_data: CachePadded<HotData>,
     soft_limit_threshold: usize,
     hard_limit_threshold: usize,
     memory_checking: AtomicBool,
-
-    // The number of writes that are buffered but not yet written.
-    pending_node_count: AtomicUsize,
     skiplist_engine: SkiplistEngine,
 }
 
@@ -38,25 +44,33 @@ impl MemoryController {
         skiplist_engine: SkiplistEngine,
     ) -> Self {
         Self {
+            hot_data: CachePadded::new(HotData {
+                allocated: AtomicUsize::new(0),
+                pending_node_count: AtomicUsize::new(0),
+            }),
             soft_limit_threshold,
             hard_limit_threshold,
-            allocated: AtomicUsize::new(0),
-            pending_node_count: AtomicUsize::new(0),
             memory_checking: AtomicBool::new(false),
             skiplist_engine,
         }
     }
 
     pub(crate) fn acquire(&self, n: usize) -> MemoryUsage {
-        let pending_count = self.pending_node_count.fetch_add(1, Ordering::Relaxed) + 1;
+        let pending_count = self
+            .hot_data
+            .pending_node_count
+            .fetch_add(1, Ordering::Relaxed)
+            + 1;
         let node_count = self.skiplist_engine.node_count();
 
-        let mem_usage = self.allocated.fetch_add(n, Ordering::Relaxed)
+        let mem_usage = self.hot_data.allocated.fetch_add(n, Ordering::Relaxed)
             + n
             + (node_count + pending_count) * NODE_OVERHEAD_SIZE_EXPECTATION;
         if mem_usage >= self.hard_limit_threshold {
-            self.allocated.fetch_sub(n, Ordering::Relaxed);
-            self.pending_node_count.fetch_sub(1, Ordering::Relaxed);
+            self.hot_data.allocated.fetch_sub(n, Ordering::Relaxed);
+            self.hot_data
+                .pending_node_count
+                .fetch_sub(1, Ordering::Relaxed);
             return MemoryUsage::HardLimitReached(mem_usage - n - NODE_OVERHEAD_SIZE_EXPECTATION);
         }
 
@@ -68,11 +82,13 @@ impl MemoryController {
     }
 
     pub(crate) fn on_node_written(&self, counts: usize) {
-        self.pending_node_count.fetch_sub(counts, Ordering::Relaxed);
+        self.hot_data
+            .pending_node_count
+            .fetch_sub(counts, Ordering::Relaxed);
     }
 
     pub(crate) fn release(&self, n: usize) {
-        self.allocated.fetch_sub(n, Ordering::Relaxed);
+        self.hot_data.allocated.fetch_sub(n, Ordering::Relaxed);
     }
 
     #[inline]
@@ -83,11 +99,6 @@ impl MemoryController {
     #[inline]
     pub(crate) fn soft_limit_threshold(&self) -> usize {
         self.soft_limit_threshold
-    }
-
-    #[inline]
-    pub(crate) fn hard_limit_threshold(&self) -> usize {
-        self.hard_limit_threshold
     }
 
     #[inline]
@@ -102,8 +113,9 @@ impl MemoryController {
 
     #[inline]
     pub(crate) fn mem_usage(&self) -> usize {
-        self.allocated.load(Ordering::Relaxed)
-            + (self.skiplist_engine.node_count() + self.pending_node_count.load(Ordering::Relaxed))
+        self.hot_data.allocated.load(Ordering::Relaxed)
+            + (self.skiplist_engine.node_count()
+                + self.hot_data.pending_node_count.load(Ordering::Relaxed))
                 * NODE_OVERHEAD_SIZE_EXPECTATION
     }
 }
@@ -126,10 +138,10 @@ mod tests {
         assert_eq!(mc.acquire(200), MemoryUsage::HardLimitReached(734));
         mc.release(50);
         assert_eq!(mc.mem_usage(), 684);
-        assert_eq!(mc.pending_node_count.load(Ordering::Relaxed), 4);
+        assert_eq!(mc.hot_data.pending_node_count.load(Ordering::Relaxed), 4);
 
         mc.on_node_written(4);
-        assert_eq!(mc.pending_node_count.load(Ordering::Relaxed), 0);
+        assert_eq!(mc.hot_data.pending_node_count.load(Ordering::Relaxed), 0);
         assert_eq!(mc.mem_usage(), 300);
 
         let guard = &epoch::pin();
