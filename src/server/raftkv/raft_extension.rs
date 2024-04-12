ecdd673be54d3293fb4934a420f4523d1397bf82
y_static_y@sina.com <y_static_y@sina.com>
2024-04-12 15:12:54 +0800

    forbid consistency check on keyspace region
    Signed-off-by: y_static_y@sina.com <y_static_y@sina.com>

--- src/server/raftkv/raft_extension.rs
+++ src/server/raftkv/raft_extension.rs
@@ -160,13 +160,12 @@ where
         Box::pin(async move {
             let meta: RegionMeta = region.await?;
 
-            // If the region is in keyspace range，it not support consistency check now.
+            // Don't support check consistency on keyspace region now.
             let startkey=meta.region_state.start_key;
             let endkey=meta.region_state.end_key;
             if ApiV2::get_u32_keyspace_id_by_key(startkey.as_slice()).is_some()||ApiV2::get_u32_keyspace_id_by_key(endkey.as_slice()).is_some(){
                 return Err(raftstore::Error::Other(From::from(format!(
-                    "snapshot of {:?} not exists.",
-                    startkey
+                    "Don't support check consistency on keyspace region now."
                 ))).into());
             }
 
