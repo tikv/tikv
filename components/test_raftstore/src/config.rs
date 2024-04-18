dc963b2929bb0301a632c7577d895bfc91b50731
glorv <glorvs@163.com>
2024-04-18 18:04:37 +0800

    This is an automated cherry-pick of #16874
    close tikv/tikv#16871
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- components/test_raftstore/src/config.rs
+++ components/test_raftstore/src/config.rs
@@ -4,10 +4,39 @@ use std::ops::{Deref, DerefMut};
 
 use tikv::config::TiKvConfig;
 
-#[derive(Clone)]
 pub struct Config {
+<<<<<<< HEAD
     pub tikv: TiKvConfig,
+=======
+    // temp dir to store the persisted configuration.
+    // We use a temp dir to ensure the original `common-test.toml` won't be
+    // changed by online config.
+    pub cfg_dir: Option<tempfile::TempDir>,
+    pub tikv: TikvConfig,
+>>>>>>> b6a029e3c0 (test: use a temp dir to store the test cluster configuration (#16874))
     pub prefer_mem: bool,
+}
+
+impl Config {
+    pub fn new(mut tikv: TikvConfig, prefer_mem: bool) -> Self {
+        let cfg_dir = test_util::temp_dir("test-cfg", prefer_mem);
+        tikv.cfg_path = cfg_dir.path().join("tikv.toml").display().to_string();
+        Self {
+            cfg_dir: Some(cfg_dir),
+            tikv,
+            prefer_mem,
+        }
+    }
+}
+
+impl Clone for Config {
+    fn clone(&self) -> Self {
+        Self {
+            cfg_dir: None,
+            tikv: self.tikv.clone(),
+            prefer_mem: self.prefer_mem,
+        }
+    }
 }
 
 impl Deref for Config {
