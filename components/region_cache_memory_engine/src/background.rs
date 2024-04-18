595fd0d27dc22708df2c0335f10a685e279efba5
Spade  A <71589810+SpadeA-Tang@users.noreply.github.com>
2024-04-18 11:30:07 +0800

    In-memory engine: fix missing memory record when snapshot loading (#16850)
    ref tikv/tikv#16141
    
    fix missing memory record when snapshot loading
    
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

--- components/region_cache_memory_engine/src/background.rs
+++ components/region_cache_memory_engine/src/background.rs
@@ -9,7 +9,8 @@ use crossbeam::{
 };
 use engine_rocks::RocksSnapshot;
 use engine_traits::{
-    CacheRange, IterOptions, Iterable, Iterator, RangeHintService, CF_DEFAULT, CF_WRITE, DATA_CFS,
+    CacheRange, IterOptions, Iterable, Iterator, RangeHintService, SnapshotMiscExt, CF_DEFAULT,
+    CF_WRITE, DATA_CFS,
 };
 use parking_lot::RwLock;
 use pd_client::RpcClient;
@@ -26,7 +27,7 @@ use yatp::Remote;
 use crate::{
     engine::{RangeCacheMemoryEngineCore, SkiplistHandle},
     keys::{decode_key, encode_key, encoding_for_filter, InternalBytes, InternalKey, ValueType},
-    memory_controller::MemoryController,
+    memory_controller::{MemoryController, MemoryUsage},
     metrics::{
         GC_FILTERED_STATIC, RANGE_CACHE_MEMORY_USAGE, RANGE_GC_TIME_HISTOGRAM,
         RANGE_LOAD_TIME_HISTOGRAM,
@@ -35,6 +36,7 @@ use crate::{
     region_label::{
         LabelRule, RegionLabelAddedCb, RegionLabelRulesManager, RegionLabelServiceBuilder,
     },
+    write_batch::RangeCacheWriteBatchEntry,
 };
 
 /// Try to extract the key and `u64` timestamp from `encoded_key`.
@@ -446,6 +448,23 @@ impl BackgroundRunnerCore {
             .write()
             .mut_range_manager()
             .on_delete_ranges(ranges);
+        #[cfg(test)]
+        flush_epoch();
+    }
+}
+
+// Flush epoch and pin enough times to make the delayed operations be executed
+#[cfg(test)]
+pub(crate) fn flush_epoch() {
+    {
+        let guard = &epoch::pin();
+        guard.flush();
+    }
+    // Local epoch tries to advance the global epoch every 128 pins. When global
+    // epoch advances, the operations(here, means delete) in the older epoch can be
+    // executed.
+    for _ in 0..128 {
+        let _ = &epoch::pin();
     }
 }
 
@@ -513,6 +532,10 @@ impl Runnable for BackgroundRunner {
     fn run(&mut self, task: Self::Task) {
         match task {
             BackgroundTask::Gc(t) => {
+                info!(
+                    "start a new round of gc for range cache engine";
+                    "safe_point" => t.safe_point,
+                );
                 let mut core = self.core.clone();
                 if let Some(ranges) = core.ranges_for_gc() {
                     let f = async move {
@@ -547,35 +570,76 @@ impl Runnable for BackgroundRunner {
                         }
 
                         if canceled {
+                            info!(
+                                "snapshot load canceled due to memory reaching soft limit";
+                                "range" => ?range,
+                            );
                             core.on_snapshot_load_canceled(range);
                             continue;
                         }
-                        let start = Instant::now();
-                        for &cf in DATA_CFS {
-                            let guard = &epoch::pin();
-                            let handle = skiplist_engine.cf_handle(cf);
-                            match snap.iterator_opt(cf, iter_opt.clone()) {
-                                Ok(mut iter) => {
-                                    iter.seek_to_first().unwrap();
-                                    while iter.valid().unwrap() {
-                                        // use 0 sequence number here as the kv is clearly
-                                        // visible
-                                        let mut encoded_key =
-                                            encode_key(iter.key(), 0, ValueType::Value);
-                                        let mut val =
-                                            InternalBytes::from_vec(iter.value().to_vec());
-                                        encoded_key
-                                            .set_memory_controller(core.memory_controller.clone());
-                                        val.set_memory_controller(core.memory_controller.clone());
-                                        handle.insert(encoded_key, val, guard);
-                                        iter.next().unwrap();
+
+                        let snapshot_load = || -> bool {
+                            for &cf in DATA_CFS {
+                                let handle = skiplist_engine.cf_handle(cf);
+                                let seq = snap.sequence_number();
+                                let guard = &epoch::pin();
+                                match snap.iterator_opt(cf, iter_opt.clone()) {
+                                    Ok(mut iter) => {
+                                        iter.seek_to_first().unwrap();
+                                        while iter.valid().unwrap() {
+                                            // use the sequence number from RocksDB snapshot here as
+                                            // the kv is clearly visible
+                                            let mut encoded_key =
+                                                encode_key(iter.key(), seq, ValueType::Value);
+                                            let mut val =
+                                                InternalBytes::from_vec(iter.value().to_vec());
+
+                                            let mem_size =
+                                                RangeCacheWriteBatchEntry::calc_put_entry_size(
+                                                    iter.key(),
+                                                    val.as_bytes(),
+                                                );
+
+                                            // todo(SpadeA): we can batch acquire the memory size
+                                            // here.
+                                            if let MemoryUsage::HardLimitReached(n) =
+                                                core.memory_controller.acquire(mem_size)
+                                            {
+                                                warn!(
+                                                    "stop loading snapshot due to memory reaching hard limit";
+                                                    "range" => ?range,
+                                                    "memory_usage(MB)" => ReadableSize(n as u64).as_mb_f64(),
+                                                );
+                                                return false;
+                                            }
+
+                                            encoded_key.set_memory_controller(
+                                                core.memory_controller.clone(),
+                                            );
+                                            val.set_memory_controller(
+                                                core.memory_controller.clone(),
+                                            );
+                                            handle.insert(encoded_key, val, guard);
+                                            iter.next().unwrap();
+                                        }
+                                    }
+                                    Err(e) => {
+                                        error!("creating rocksdb iterator failed"; "cf" => cf, "err" => %e);
+                                        return false;
                                     }
                                 }
-                                Err(e) => {
-                                    error!("creating rocksdb iterator failed"; "cf" => cf, "err" => %e);
-                                }
                             }
+                            true
+                        };
+
+                        let start = Instant::now();
+                        if !snapshot_load() {
+                            // snapshot load failed, we should clear the dirty data
+                            core.delete_ranges(&[range.clone()]);
+                            core.on_snapshot_load_canceled(range);
+                            continue;
                         }
+
                         if core.on_snapshot_load_finished(range.clone()) {
                             let duration = start.saturating_elapsed();
                             RANGE_LOAD_TIME_HISTOGRAM.observe(duration.as_secs_f64());
@@ -781,12 +845,13 @@ pub mod tests {
     use crossbeam::epoch;
     use engine_rocks::util::new_engine;
     use engine_traits::{
-        CacheRange, RangeCacheEngine, SyncMutable, CF_DEFAULT, CF_WRITE, DATA_CFS,
+        CacheRange, IterOptions, Iterable, Iterator, RangeCacheEngine, SyncMutable, CF_DEFAULT,
+        CF_LOCK, CF_WRITE, DATA_CFS,
     };
     use keys::{data_key, DATA_MAX_KEY, DATA_MIN_KEY};
     use pd_client::PdClient;
     use tempfile::Builder;
-    use tikv_util::config::ReadableDuration;
+    use tikv_util::config::{ReadableDuration, ReadableSize};
     use txn_types::{Key, TimeStamp, Write, WriteType};
 
     use super::{Filter, PdRangeHintService};
@@ -794,14 +859,15 @@ pub mod tests {
         background::BackgroundRunner,
         engine::{SkiplistEngine, SkiplistHandle},
         keys::{
-            construct_key, construct_value, encode_key, encode_seek_key, encoding_for_filter,
-            InternalBytes, ValueType,
+            construct_key, construct_user_key, construct_value, encode_key, encode_seek_key,
+            encoding_for_filter, InternalBytes, ValueType,
         },
         memory_controller::MemoryController,
         region_label::{
             region_label_meta_client,
             tests::{add_region_label_rule, new_region_label_rule, new_test_server_and_client},
         },
+        write_batch::RangeCacheWriteBatchEntry,
         RangeCacheEngineConfig, RangeCacheMemoryEngine,
     };
 
@@ -816,10 +882,10 @@ pub mod tests {
         write_cf: &SkiplistHandle,
         mem_controller: Arc<MemoryController>,
     ) {
-        let write_k = Key::from_raw(key)
+        let raw_write_k = Key::from_raw(key)
             .append_ts(TimeStamp::new(commit_ts))
             .into_encoded();
-        let mut write_k = encode_key(&write_k, seq_num, ValueType::Value);
+        let mut write_k = encode_key(&raw_write_k, seq_num, ValueType::Value);
         write_k.set_memory_controller(mem_controller.clone());
         let write_v = Write::new(
             WriteType::Put,
@@ -833,16 +899,24 @@ pub mod tests {
         let mut val = InternalBytes::from_vec(write_v.as_ref().to_bytes());
         val.set_memory_controller(mem_controller.clone());
         let guard = &epoch::pin();
+        let _ = mem_controller.acquire(RangeCacheWriteBatchEntry::calc_put_entry_size(
+            &raw_write_k,
+            val.as_bytes(),
+        ));
         write_cf.insert(write_k, val, guard);
 
         if !short_value {
-            let default_k = Key::from_raw(key)
+            let raw_default_k = Key::from_raw(key)
                 .append_ts(TimeStamp::new(start_ts))
                 .into_encoded();
-            let mut default_k = encode_key(&default_k, seq_num + 1, ValueType::Value);
+            let mut default_k = encode_key(&raw_default_k, seq_num + 1, ValueType::Value);
             default_k.set_memory_controller(mem_controller.clone());
             let mut val = InternalBytes::from_vec(value.to_vec());
-            val.set_memory_controller(mem_controller);
+            val.set_memory_controller(mem_controller.clone());
+            let _ = mem_controller.acquire(RangeCacheWriteBatchEntry::calc_put_entry_size(
+                &raw_default_k,
+                val.as_bytes(),
+            ));
             default_cf.insert(default_k, val, guard);
         }
     }
@@ -854,15 +928,19 @@ pub mod tests {
         write_cf: &SkiplistHandle,
         mem_controller: Arc<MemoryController>,
     ) {
-        let write_k = Key::from_raw(key)
+        let raw_write_k = Key::from_raw(key)
             .append_ts(TimeStamp::new(ts))
             .into_encoded();
-        let mut write_k = encode_key(&write_k, seq_num, ValueType::Value);
+        let mut write_k = encode_key(&raw_write_k, seq_num, ValueType::Value);
         write_k.set_memory_controller(mem_controller.clone());
         let write_v = Write::new(WriteType::Delete, TimeStamp::new(ts), None);
         let mut val = InternalBytes::from_vec(write_v.as_ref().to_bytes());
-        val.set_memory_controller(mem_controller);
+        val.set_memory_controller(mem_controller.clone());
         let guard = &epoch::pin();
+        let _ = mem_controller.acquire(RangeCacheWriteBatchEntry::calc_put_entry_size(
+            &raw_write_k,
+            val.as_bytes(),
+        ));
         write_cf.insert(write_k, val, guard);
     }
 
@@ -873,15 +951,19 @@ pub mod tests {
         write_cf: &SkiplistHandle,
         mem_controller: Arc<MemoryController>,
     ) {
-        let write_k = Key::from_raw(key)
+        let raw_write_k = Key::from_raw(key)
             .append_ts(TimeStamp::new(ts))
             .into_encoded();
-        let mut write_k = encode_key(&write_k, seq_num, ValueType::Value);
+        let mut write_k = encode_key(&raw_write_k, seq_num, ValueType::Value);
         write_k.set_memory_controller(mem_controller.clone());
         let write_v = Write::new(WriteType::Rollback, TimeStamp::new(ts), None);
         let mut val = InternalBytes::from_vec(write_v.as_ref().to_bytes());
-        val.set_memory_controller(mem_controller);
+        val.set_memory_controller(mem_controller.clone());
         let guard = &epoch::pin();
+        let _ = mem_controller.acquire(RangeCacheWriteBatchEntry::calc_put_entry_size(
+            &raw_write_k,
+            val.as_bytes(),
+        ));
         write_cf.insert(write_k, val, guard);
     }
 
@@ -1527,5 +1609,111 @@ pub mod tests {
         }
 
         pd_server.stop();
+    }
+
+    #[test]
+    fn test_snapshot_load_reaching_limit() {
+        let mut config = RangeCacheEngineConfig::config_for_test();
+        config.soft_limit_threshold = Some(ReadableSize(1000));
+        config.hard_limit_threshold = Some(ReadableSize(1500));
+        let mut engine = RangeCacheMemoryEngine::new(&config);
+        let path = Builder::new()
+            .prefix("test_snapshot_load_reaching_limit")
+            .tempdir()
+            .unwrap();
+        let path_str = path.path().to_str().unwrap();
+        let rocks_engine = new_engine(path_str, DATA_CFS).unwrap();
+        engine.set_disk_engine(rocks_engine.clone());
+        let mem_controller = engine.memory_controller();
+
+        let range1 = CacheRange::new(construct_user_key(1), construct_user_key(3));
+        // Memory for one put is 17(key) + 3(val) + 8(Seqno) + 16(Memory controller in
+        // key and val) + 96(Node overhead) = 140
+        let key = construct_key(1, 10);
+        rocks_engine.put_cf(CF_DEFAULT, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_LOCK, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_WRITE, &key, b"val").unwrap();
+
+        let key = construct_key(2, 10);
+        rocks_engine.put_cf(CF_DEFAULT, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_LOCK, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_WRITE, &key, b"val").unwrap();
+        // After loading range1, the memory usage should be 140*6=840
+
+        let range2 = CacheRange::new(construct_user_key(3), construct_user_key(5));
+        let key = construct_key(3, 10);
+        rocks_engine.put_cf(CF_DEFAULT, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_LOCK, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_WRITE, &key, b"val").unwrap();
+
+        let key = construct_key(4, 10);
+        rocks_engine.put_cf(CF_DEFAULT, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_LOCK, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_WRITE, &key, b"val").unwrap();
+        // 840*2 > hard limit 1500, so the load will fail and the loaded keys should be
+        // removed
+
+        let range3 = CacheRange::new(construct_user_key(5), construct_user_key(6));
+        let key = construct_key(5, 10);
+        rocks_engine.put_cf(CF_DEFAULT, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_LOCK, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_WRITE, &key, b"val").unwrap();
+        // Memory usage reaches 1260
+
+        let range4 = CacheRange::new(construct_user_key(6), construct_user_key(7));
+        let key = construct_key(6, 10);
+        rocks_engine.put_cf(CF_DEFAULT, &key, b"val").unwrap();
+        rocks_engine.put_cf(CF_LOCK, &key, b"val").unwrap();
+        // Although the memory is enough for loading range4, it is alreay reaching soft
+        // limit at begin.
+
+        for r in [&range1, &range2, &range3, &range4] {
+            engine.load_range(r.clone()).unwrap();
+            engine.prepare_for_apply(r);
+        }
+
+        // ensure all ranges are finshed
+        {
+            let mut count = 0;
+            while count < 20 {
+                {
+                    let core = engine.core.read();
+                    let range_manager = core.range_manager();
+                    if range_manager.pending_ranges.is_empty()
+                        && range_manager.pending_ranges_loading_data.is_empty()
+                    {
+                        break;
+                    }
+                }
+                std::thread::sleep(Duration::from_millis(100));
+                count += 1;
+            }
+        }
+
+        let verify = |range: CacheRange, exist, expect_count| {
+            if exist {
+                let snap = engine.snapshot(range.clone(), 10, u64::MAX).unwrap();
+                let mut count = 0;
+                for cf in DATA_CFS {
+                    let mut iter = IterOptions::default();
+                    iter.set_lower_bound(&range.start, 0);
+                    iter.set_upper_bound(&range.end, 0);
+                    let mut iter = snap.iterator_opt(cf, iter).unwrap();
+                    let _ = iter.seek_to_first();
+                    while iter.valid().unwrap() {
+                        let _ = iter.next();
+                        count += 1;
+                    }
+                }
+                assert_eq!(count, expect_count);
+            } else {
+                engine.snapshot(range, 10, 10).unwrap_err();
+            }
+        };
+        verify(range1, true, 6);
+        verify(range2, false, 0);
+        verify(range3, true, 3);
+        verify(range4, false, 0);
+        assert_eq!(mem_controller.mem_usage(), 1260);
     }
 }
