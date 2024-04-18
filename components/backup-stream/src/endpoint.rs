ba3f249301c962a6d7224fa14435d88ff1e5e901
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-04-18 08:13:36 +0800

    log_backup: make a more rusty `CallbackWaitGroup` (#16740) (#16756)
    close tikv/tikv#16739
    
    This make `CallbackWaitGroup` returns an equivalent future of the `BoxFuture` returned by `wait`. Also this fixed where a stale notify may also resolve the future.
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    Signed-off-by: hillium <yu745514916@live.com>
    
    Co-authored-by: 山岚 <36239017+YuJuncen@users.noreply.github.com>
    Co-authored-by: hillium <yu745514916@live.com>
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- components/backup-stream/src/endpoint.rs
+++ components/backup-stream/src/endpoint.rs
@@ -62,7 +62,7 @@ use crate::{
     subscription_manager::{RegionSubscriptionManager, ResolvedRegions},
     subscription_track::{Ref, RefMut, ResolveResult, SubscriptionTracer},
     try_send,
-    utils::{self, CallbackWaitGroup, StopWatch, Work},
+    utils::{self, FutureWaitGroup, StopWatch, Work},
 };
 
 const SLOW_EVENT_THRESHOLD: f64 = 120.0;
@@ -1070,7 +1070,7 @@ where
     }
 
     pub fn do_backup(&self, events: Vec<CmdBatch>) {
-        let wg = CallbackWaitGroup::new();
+        let wg = FutureWaitGroup::new();
         for batch in events {
             self.backup_batch(batch, wg.clone().work());
         }
