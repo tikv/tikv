419dacd0c4023b038506934e3ecc76f841789d10
山岚 <36239017+YuJuncen@users.noreply.github.com>
2024-04-29 11:38:57 +0800

    pd_client: make `update_service_safe_point` returns error when stale (#16911)
    close tikv/tikv#16910
    
    Let `update_service_safe_point` return an error when the provided service gc safepoint is stale.
    
    Signed-off-by: hillium <yujuncen@pingcap.com>
    Signed-off-by: Yu Juncen <yu745514916@live.com>

--- tests/integrations/pd/test_rpc_client_legacy.rs
+++ tests/integrations/pd/test_rpc_client_legacy.rs
@@ -689,3 +689,32 @@ fn test_cluster_version() {
     emit_heartbeat();
     assert!(feature_gate.can_enable(feature_c));
 }
+
+#[test]
+fn test_update_service_gc_safepoint() {
+    let server = MockServer::new(1);
+    let eps = server.bind_addrs();
+
+    let client = new_client(eps, None);
+    let update_gc_safepoint = |x| {
+        block_on(client.update_service_safe_point("test".to_owned(), x, Duration::from_secs(1)))
+    };
+
+    #[track_caller]
+    fn assert_is_unsafe_safepoint(res: pd_client::Result<()>, current_min: u64, safe_point: u64) {
+        match res {
+            Err(pd_client::Error::UnsafeServiceGcSafePoint {
+                requested,
+                current_minimal,
+            }) => {
+                assert_eq!(requested.into_inner(), safe_point);
+                assert_eq!(current_minimal.into_inner(), current_min);
+            }
+            _ => panic!("the error is {:?}", res),
+        }
+    }
+    update_gc_safepoint(42.into()).unwrap();
+    assert_is_unsafe_safepoint(update_gc_safepoint(41.into()), 42, 41);
+    update_gc_safepoint(43.into()).unwrap();
+    assert_is_unsafe_safepoint(update_gc_safepoint(42.into()), 43, 42);
+}
