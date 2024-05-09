f0983cc637e158a9bcc65c401cf25c97280e8fad
gengliqi <gengliqiii@gmail.com>
2024-05-09 14:56:36 +0800

    make clippy happy
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- components/tidb_query_expr/src/impl_math.rs
+++ components/tidb_query_expr/src/impl_math.rs
@@ -1673,7 +1673,7 @@ mod tests {
                 .push_param(f)
                 .push_param(t)
                 .evaluate::<Bytes>(ScalarFuncSig::Conv);
-            assert!(got.is_err());
+            got.unwrap_err();
         }
     }
 
