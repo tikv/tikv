0b700bf9ca8fb46d46fd5b9f277d3a17e29c7d1c
山岚 <36239017+YuJuncen@users.noreply.github.com>
2024-05-16 13:22:13 +0800

    This is an automated cherry-pick of #17021
    close tikv/tikv#17020
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- components/backup-stream/tests/failpoints/mod.rs
+++ components/backup-stream/tests/failpoints/mod.rs
@@ -393,4 +393,18 @@ mod all {
             std::iter::once(enc_key.as_encoded().as_slice()),
         )
     }
+
+    #[test]
+    fn failed_to_get_task_when_pausing() {
+        let suite = SuiteBuilder::new_named("resume_error").nodes(1).build();
+        suite.must_register_task(1, "resume_error");
+        let mcli = suite.get_meta_cli();
+        run_async_test(mcli.pause("resume_error")).unwrap();
+        suite.sync();
+        fail::cfg("failed_to_get_task", "1*return").unwrap();
+        run_async_test(mcli.resume("resume_error")).unwrap();
+        suite.sync();
+        // Make sure our suite doesn't panic.
+        suite.sync();
+    }
 }
