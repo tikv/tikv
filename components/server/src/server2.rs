1fdc48373311a0a8c5c5df3ef4bdcd6974de36ad
Neil Shen <overvenus@gmail.com>
2024-05-17 15:44:44 +0800

    This is an automated cherry-pick of #16662
    close tikv/tikv#16653
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- components/server/src/server2.rs
+++ components/server/src/server2.rs
@@ -756,6 +756,17 @@ where
 
         let node = self.node.as_ref().unwrap();
 
+        // Create coprocessor endpoint.
+        let copr = coprocessor::Endpoint::new(
+            &server_config.value(),
+            cop_read_pool_handle,
+            self.concurrency_manager.clone(),
+            resource_tag_factory,
+            self.quota_limiter.clone(),
+            self.resource_manager.clone(),
+        );
+        let copr_config_manager = copr.config_manager();
+
         self.snap_mgr = Some(snap_mgr.clone());
         // Create server
         let server = Server::new(
@@ -763,14 +774,7 @@ where
             &server_config,
             &self.security_mgr,
             storage,
-            coprocessor::Endpoint::new(
-                &server_config.value(),
-                cop_read_pool_handle,
-                self.concurrency_manager.clone(),
-                resource_tag_factory,
-                self.quota_limiter.clone(),
-                self.resource_manager.clone(),
-            ),
+            copr,
             coprocessor_v2::Endpoint::new(&self.core.config.coprocessor_v2),
             self.resolver.clone().unwrap(),
             Either::Right(snap_mgr.clone()),
@@ -789,6 +793,7 @@ where
                 server.get_snap_worker_scheduler(),
                 server_config.clone(),
                 server.get_grpc_mem_quota().clone(),
+                copr_config_manager,
             )),
         );
 
