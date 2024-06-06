d7639ca0626ecbb43faf44337580498679349ac7
MyonKeminta <MyonKeminta@users.noreply.github.com>
2024-06-06 18:39:22 +0800

    Fix the problem that CDC and log-backup didn't use advance-ts-interval to limit the timeout of check_leader
    Signed-off-by: MyonKeminta <MyonKeminta@users.noreply.github.com>

--- components/backup-stream/tests/suite.rs
+++ components/backup-stream/tests/suite.rs
@@ -38,7 +38,7 @@ use tempfile::TempDir;
 use test_pd_client::TestPdClient;
 use test_raftstore::{new_server_cluster, Cluster, ServerCluster};
 use test_util::retry;
-use tikv::config::BackupStreamConfig;
+use tikv::config::{BackupStreamConfig, ResolvedTsConfig};
 use tikv_util::{
     codec::{
         number::NumberEncoder,
@@ -383,6 +383,7 @@ impl Suite {
             id,
             self.meta_store.clone(),
             cfg,
+            ResolvedTsConfig::default(),
             worker.scheduler(),
             ob,
             regions,
