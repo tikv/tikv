93c8a677e54ce70842030f5947d333f2c5c9336e
Spade  A <71589810+SpadeA-Tang@users.noreply.github.com>
2024-06-18 20:01:17 +0800

    In-memory engine: set disk engine when start (#17165)
    ref tikv/tikv#16141
    
    set disk engine when start
    
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- components/region_cache_memory_engine/src/engine.rs
+++ components/region_cache_memory_engine/src/engine.rs
@@ -562,7 +562,17 @@ impl RangeCacheEngine for RangeCacheMemoryEngine {
 
     type DiskEngine = RocksEngine;
     fn set_disk_engine(&mut self, disk_engine: Self::DiskEngine) {
-        self.rocks_engine = Some(disk_engine);
+        self.rocks_engine = Some(disk_engine.clone());
+        if let Err(e) = self
+            .bg_worker_manager()
+            .schedule_task(BackgroundTask::SetRocksEngine(disk_engine))
+        {
+            error!(
+                "schedule set rocks_engine failed";
+                "err" => ?e,
+            );
+            assert!(tikv_util::thread_group::is_shutdown(!cfg!(test)));
+        }
     }
 
     type RangeHintService = PdRangeHintService;
