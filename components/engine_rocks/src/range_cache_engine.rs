a1ddb411eb9f6be99207743c50944f7aa0c370e2
Spade  A <71589810+SpadeA-Tang@users.noreply.github.com>
2024-06-19 15:56:48 +0800

    In-memory engine: avoid using snapshot cache (#16863)
    ref tikv/tikv#16141
    
    avoid using snapshot cache
    
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>

---
+++ components/engine_rocks/src/range_cache_engine.rs
@@ -0,0 +1,11 @@
+// Copyright 2024 TiKV Project Authors. Licensed under Apache-2.0.
+
+use engine_traits::RangeCacheEngineExt;
+
+use crate::RocksEngine;
+
+impl RangeCacheEngineExt for RocksEngine {
+    fn range_cache_engine_enabled(&self) -> bool {
+        false
+    }
+}
