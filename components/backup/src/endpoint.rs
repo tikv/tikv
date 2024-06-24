d503fb5547c3c7fca5a5e8c72b5f3af6aefbb85e
Jianjun Liao <jianjun.liao@outlook.com>
2024-06-24 13:25:54 +0800

    make format & clippy
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- components/backup/src/endpoint.rs
+++ components/backup/src/endpoint.rs
@@ -1961,7 +1961,7 @@ pub mod tests {
     }
 
     fn fake_empty_marker() -> Vec<super::BackupRange> {
-        return vec![super::BackupRange{
+        vec![super::BackupRange {
             start_key: None,
             end_key: None,
             region: Region::new(),
@@ -1969,7 +1969,7 @@ pub mod tests {
             codec: KeyValueCodec::new(false, ApiVersion::V1, ApiVersion::V1),
             cf: "",
             uses_replica_read: false,
-        }];
+        }]
     }
 
     #[test]
