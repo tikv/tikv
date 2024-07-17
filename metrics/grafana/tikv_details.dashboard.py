73a5b5466011c566e43095c743c5001ffdd535c2
lucasliang <nkcs_lykx@hotmail.com>
2024-07-17 12:25:03 +0800

    metrics: fix incorrect unit of grpc source requests' duration.
    This pr is used to fix the incorrect format of the unit in the panel
    of `grpc source requests' duration`.
    
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>

--- metrics/grafana/tikv_details.dashboard.py
+++ metrics/grafana/tikv_details.dashboard.py
@@ -1096,7 +1096,9 @@ def gRPC() -> RowPanel:
             graph_panel(
                 title="gRPC request sources duration",
                 description="The duration of different sources of gRPC request",
-                yaxes=yaxes(left_format=UNITS.MICRO_SECONDS),
+                yaxes=yaxes(
+                    left_format=UNITS.NANO_SECONDS, right_format=UNITS.NANO_SECONDS
+                ),
                 lines=False,
                 stack=True,
                 targets=[
