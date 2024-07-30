3c16d9d491322f2ea65f59e5c31f77ec74275444
“EricZequan” <zequany33@gmail.com>
2024-07-30 17:46:10 +0800

    add vector functions
    Signed-off-by: “EricZequan” <zequany33@gmail.com>

--- components/tidb_query_datatype/src/codec/mysql/vector.rs
+++ components/tidb_query_datatype/src/codec/mysql/vector.rs
@@ -1,19 +1,33 @@
 // Copyright 2024 TiKV Project Authors. Licensed under Apache-2.0.
 
 use codec::prelude::*;
-use ordered_float::OrderedFloat;
+use ordered_float::NotNan;
 
 use crate::codec::Result;
 
 // TODO: Implement generic version
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct VectorFloat32 {
-    pub value: Vec<OrderedFloat<f32>>,
+    pub value: Vec<NotNan<f32>>,
 }
 
 impl VectorFloat32 {
-    pub fn new(value: Vec<OrderedFloat<f32>>) -> Self {
-        Self { value }
+    pub fn new(value: Vec<f32>) -> Result<Self> {
+        // Check again that NaN and ±Inf is not contained
+        for v in &value {
+            if v.is_nan() {
+                return Err(box_err!("NaN not allowed in vector"));
+            }
+            if v.is_infinite() {
+                return Err(box_err!("infinite value not allowed in vector"));
+            }
+        }
+        Ok(Self {
+            value: value
+                .into_iter()
+                .map(|v| NotNan::<f32>::new(v).unwrap())
+                .collect(),
+        })
     }
 
     pub fn as_ref(&self) -> VectorFloat32Ref<'_> {
@@ -56,14 +70,13 @@ pub trait VectorFloat32Decoder: NumberDecoder {
         }
 
         if self.bytes().is_empty() {
-            return Ok(VectorFloat32Ref::new(&[]));
+            return VectorFloat32Ref::new(&[]);
         }
         let n = self.read_u32_le()? as usize;
         let data_size = n * 4;
         let data = self.read_bytes(data_size)?;
-        let data_in_f32 =
-            unsafe { std::slice::from_raw_parts(data.as_ptr() as *const OrderedFloat<f32>, n) };
-        Ok(VectorFloat32Ref::new(data_in_f32))
+        let data_in_f32 = unsafe { std::slice::from_raw_parts(data.as_ptr() as *const f32, n) };
+        VectorFloat32Ref::new(data_in_f32)
     }
 
     // `read_vector_float32` decodes value encoded by `write_vector_float32` before.
@@ -79,12 +92,26 @@ impl<T: BufferReader> VectorFloat32Decoder for T {}
 #[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct VectorFloat32Ref<'a> {
     // Referred value
-    value: &'a [OrderedFloat<f32>],
+    value: &'a [NotNan<f32>],
 }
 
 impl<'a> VectorFloat32Ref<'a> {
-    pub fn new(value: &[OrderedFloat<f32>]) -> VectorFloat32Ref<'_> {
-        VectorFloat32Ref { value }
+    pub fn new(value: &[f32]) -> Result<VectorFloat32Ref<'_>> {
+        // Check again that NaN and ±Inf is not contained
+        for v in value {
+            if v.is_nan() {
+                return Err(box_err!("NaN not allowed in vector"));
+            }
+            if v.is_infinite() {
+                return Err(box_err!("infinite value not allowed in vector"));
+            }
+        }
+        let value_in_notnan: &[NotNan<f32>] = unsafe {
+            std::slice::from_raw_parts(value.as_ptr() as *const NotNan<f32>, value.len())
+        };
+        Ok(VectorFloat32Ref {
+            value: value_in_notnan,
+        })
     }
 
     pub fn encoded_len(&self) -> usize {
@@ -103,6 +130,101 @@ impl<'a> VectorFloat32Ref<'a> {
 
     pub fn is_empty(&self) -> bool {
         self.len() == 0
+    }
+
+    fn primitive_value(&self) -> &[f32] {
+        unsafe { std::slice::from_raw_parts(self.value.as_ptr() as *const f32, self.value.len()) }
+    }
+
+    fn check_dims(&self, b: VectorFloat32Ref<'a>) -> Result<()> {
+        if self.len() != b.len() {
+            return Err(box_err!(
+                "vectors have different dimensions: {} and {}",
+                self.len(),
+                b.len()
+            ));
+        }
+        Ok(())
+    }
+
+    pub fn l2_squared_distance(&self, b: VectorFloat32Ref<'a>) -> Result<f64> {
+        self.check_dims(b)?;
+        let mut distance: f32 = 0.0;
+
+        // Note: Must NOT calculate over va[i].value and vb[i].value directly (which is
+        // a &[NotNan]), because it will panic if the calculated result is NaN.
+        // See https://docs.rs/ordered-float/latest/ordered_float/struct.NotNan.html
+        let va = self.primitive_value();
+        let vb = b.primitive_value();
+        for i in 0..self.len() {
+            // Hope this can be vectorized.
+            let diff = va[i] - vb[i];
+            distance += diff * diff;
+        }
+        Ok(distance as f64)
+    }
+
+    pub fn l2_distance(&self, b: VectorFloat32Ref<'a>) -> Result<f64> {
+        Ok(self.l2_squared_distance(b)?.sqrt())
+    }
+
+    pub fn inner_product(&self, b: VectorFloat32Ref<'a>) -> Result<f64> {
+        self.check_dims(b)?;
+        let mut distance: f32 = 0.0;
+        let va = self.primitive_value();
+        let vb = b.primitive_value();
+        for i in 0..self.len() {
+            // Hope this can be vectorized.
+            distance += va[i] * vb[i];
+        }
+        Ok(distance as f64)
+    }
+
+    pub fn cosine_distance(&self, b: VectorFloat32Ref<'a>) -> Result<f64> {
+        self.check_dims(b)?;
+        let mut distance: f32 = 0.0;
+        let mut norma: f32 = 0.0;
+        let mut normb: f32 = 0.0;
+        let va = self.primitive_value();
+        let vb = b.primitive_value();
+        for i in 0..self.len() {
+            // Hope this can be vectorized.
+            distance += va[i] * vb[i];
+            norma += va[i] * va[i];
+            normb += vb[i] * vb[i];
+        }
+        let similarity = (distance as f64) / ((norma as f64).sqrt() * (normb as f64).sqrt());
+        if similarity.is_nan() {
+            // Divide by zero
+            return Ok(std::f64::NAN);
+        }
+        let similarity = similarity.clamp(-1.0, 1.0);
+        Ok(1.0 - similarity)
+    }
+
+    pub fn l1_distance(&self, b: VectorFloat32Ref<'a>) -> Result<f64> {
+        self.check_dims(b)?;
+        let mut distance: f32 = 0.0;
+        let va = self.primitive_value();
+        let vb = b.primitive_value();
+        for i in 0..self.len() {
+            // Hope this can be vectorized.
+            let diff = va[i] - vb[i];
+            distance += diff.abs();
+        }
+        Ok(distance as f64)
+    }
+
+    pub fn l2_norm(&self) -> f64 {
+        // Note: We align the impl with pgvector: Only l2_norm use double
+        // precision during calculation.
+        let mut norm: f64 = 0.0;
+        let va = self.primitive_value();
+        for i in 0..self.len() {
+            // Hope this can be vectorized.
+            norm += va[i] as f64 * va[i] as f64;
+        }
+        norm.sqrt()
     }
 }
 
@@ -153,20 +275,41 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_nan_inf() {
+        let v = VectorFloat32::new(vec![1.0, std::f32::NAN]);
+        v.unwrap_err();
+
+        let v = VectorFloat32::new(vec![1.0, std::f32::INFINITY]);
+        v.unwrap_err();
+
+        let v = VectorFloat32::new(vec![1.0, std::f32::NEG_INFINITY]);
+        v.unwrap_err();
+
+        let v = VectorFloat32Ref::new(&[1.0, std::f32::NAN]);
+        v.unwrap_err();
+
+        let v = VectorFloat32Ref::new(&[1.0, std::f32::INFINITY]);
+        v.unwrap_err();
+
+        let v = VectorFloat32Ref::new(&[1.0, std::f32::NEG_INFINITY]);
+        v.unwrap_err();
+    }
+
+    #[test]
     fn test_to_string() {
-        let v = VectorFloat32::new(vec![OrderedFloat(1.0), OrderedFloat(2.0)]);
+        let v = VectorFloat32::new(vec![1.0, 2.0]).unwrap();
         assert_eq!("[1,2]", v.to_string());
 
-        let v = VectorFloat32::new(vec![OrderedFloat(1.1), OrderedFloat(2.2)]);
+        let v = VectorFloat32::new(vec![1.1, 2.2]).unwrap();
         assert_eq!("[1.1,2.2]", v.to_string());
 
-        let v = VectorFloat32::new(vec![]);
+        let v = VectorFloat32::new(vec![]).unwrap();
         assert_eq!("[]", v.to_string());
     }
 
     #[test]
     fn test_encode() {
-        let v = VectorFloat32::new(vec![OrderedFloat(1.1), OrderedFloat(2.2)]);
+        let v = VectorFloat32::new(vec![1.1, 2.2]).unwrap();
         let mut encoded = Vec::new();
         encoded.write_vector_float32(v.as_ref()).unwrap();
         assert_eq!(
--- components/tidb_query_expr/src/impl_vec.rs
+++ components/tidb_query_expr/src/impl_vec.rs
@@ -15,3 +15,271 @@ fn vec_as_text(a: VectorFloat32Ref, writer: BytesWriter) -> Result<BytesGuard> {
 fn vec_dims(arg: VectorFloat32Ref) -> Result<Option<Int>> {
     Ok(Some(arg.len() as Int))
 }
+
+#[rpn_fn]
+#[inline]
+fn vec_l1_distance(a: VectorFloat32Ref, b: VectorFloat32Ref) -> Result<Option<Real>> {
+    // TiKV does not support NaN. This turns NaN into null
+    Ok(Real::new(a.l1_distance(b)?).ok())
+}
+
+#[rpn_fn]
+#[inline]
+fn vec_l2_distance(a: VectorFloat32Ref, b: VectorFloat32Ref) -> Result<Option<Real>> {
+    // TiKV does not support NaN. This turns NaN into null
+    Ok(Real::new(a.l2_distance(b)?).ok())
+}
+
+#[rpn_fn]
+#[inline]
+fn vec_negative_inner_product(a: VectorFloat32Ref, b: VectorFloat32Ref) -> Result<Option<Real>> {
+    // TiKV does not support NaN. This turns NaN into null
+    Ok(Real::new(a.inner_product(b)? * -1.0).ok())
+}
+
+#[rpn_fn]
+#[inline]
+fn vec_cosine_distance(a: VectorFloat32Ref, b: VectorFloat32Ref) -> Result<Option<Real>> {
+    // TiKV does not support NaN. This turns NaN into null
+    Ok(Real::new(a.cosine_distance(b)?).ok())
+}
+
+#[rpn_fn]
+#[inline]
+fn vec_l2_norm(a: VectorFloat32Ref) -> Result<Option<Real>> {
+    // TiKV does not support NaN. This turns NaN into null
+    Ok(Real::new(a.l2_norm()).ok())
+}
+
+#[cfg(test)]
+mod tests {
+    use tipb::ScalarFuncSig;
+
+    use super::*;
+    use crate::types::test_util::RpnFnScalarEvaluator;
+
+    fn real_eq(a: Real, b: Real) -> bool {
+        if a.is_nan() {
+            return b.is_nan();
+        }
+        if a.is_infinite() {
+            return b.is_infinite() && (a.is_sign_positive() == b.is_sign_positive());
+        }
+        (a.into_inner() - b.into_inner()).abs() < 1e-6
+    }
+
+    fn option_real_eq(a: Option<Real>, b: Option<Real>) -> bool {
+        match (a, b) {
+            (Some(a), Some(b)) => real_eq(a, b),
+            (None, None) => true,
+            _ => false,
+        }
+    }
+
+    // Test cases are ported from pgvector: https://github.com/pgvector/pgvector/blob/master/test/expected/functions.out
+    // Copyright (c) 1996-2023, PostgreSQL Global Development Group
+
+    #[test]
+    fn test_dims() {
+        let cases = vec![
+            (vec![], Some(0)),
+            (vec![1.0, 2.0], Some(2)),
+            (vec![1.0, 2.0, 3.0], Some(3)),
+        ];
+        for (arg, expected_output) in cases {
+            let arg = VectorFloat32::new(arg).unwrap();
+            let output: Option<Int> = RpnFnScalarEvaluator::new()
+                .push_param(arg)
+                .evaluate(ScalarFuncSig::VecDimsSig)
+                .unwrap();
+            assert_eq!(output, expected_output);
+        }
+    }
+
+    #[test]
+    fn test_l2_norm() {
+        let cases = vec![
+            (vec![], Some(0.0)),
+            (vec![3.0, 4.0], Some(5.0)),
+            (vec![0.0, 1.0], Some(1.0)),
+        ];
+
+        for (arg, expected_output) in cases {
+            let arg = VectorFloat32::new(arg).unwrap();
+            let output: Option<Real> = RpnFnScalarEvaluator::new()
+                .push_param(arg)
+                .evaluate(ScalarFuncSig::VecL2NormSig)
+                .unwrap();
+            if expected_output.is_none() {
+                assert!(output.is_none(), "expected None, got {:?}", output);
+            } else {
+                assert!(
+                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
+                    "expected {:?}, got {:?}",
+                    expected_output,
+                    output
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_l2_distance() {
+        let ok_cases = vec![
+            (vec![0.0, 0.0], vec![3.0, 4.0], Some(5.0)),
+            (vec![0.0, 0.0], vec![0.0, 1.0], Some(1.0)),
+            (vec![3e38], vec![-3e38], Some(f64::INFINITY)),
+        ];
+        for (arg1, arg2, expected_output) in ok_cases {
+            let arg1 = VectorFloat32::new(arg1).unwrap();
+            let arg2 = VectorFloat32::new(arg2).unwrap();
+            let output: Option<Real> = RpnFnScalarEvaluator::new()
+                .push_param(arg1)
+                .push_param(arg2)
+                .evaluate(ScalarFuncSig::VecL2DistanceSig)
+                .unwrap();
+            if expected_output.is_none() {
+                assert!(output.is_none(), "expected None, got {:?}", output);
+            } else {
+                assert!(
+                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
+                    "expected {:?}, got {:?}",
+                    expected_output,
+                    output
+                );
+            }
+        }
+
+        let err_cases = vec![(vec![1.0, 2.0], vec![3.0])];
+        for (arg1, arg2) in err_cases {
+            let arg1 = VectorFloat32::new(arg1).unwrap();
+            let arg2 = VectorFloat32::new(arg2).unwrap();
+            let output: Result<Option<Real>> = RpnFnScalarEvaluator::new()
+                .push_param(arg1)
+                .push_param(arg2)
+                .evaluate(ScalarFuncSig::VecL2DistanceSig);
+            assert!(output.is_err(), "expected error, got {:?}", output);
+        }
+    }
+
+    #[test]
+    fn test_negative_inner_product() {
+        let ok_cases = vec![
+            (vec![1.0, 2.0], vec![3.0, 4.0], Some(-11.0)),
+            (vec![3e38], vec![3e38], Some(f64::NEG_INFINITY)),
+        ];
+        for (arg1, arg2, expected_output) in ok_cases {
+            let arg1 = VectorFloat32::new(arg1).unwrap();
+            let arg2 = VectorFloat32::new(arg2).unwrap();
+            let output: Option<Real> = RpnFnScalarEvaluator::new()
+                .push_param(arg1)
+                .push_param(arg2)
+                .evaluate(ScalarFuncSig::VecNegativeInnerProductSig)
+                .unwrap();
+            if expected_output.is_none() {
+                assert!(output.is_none(), "expected None, got {:?}", output);
+            } else {
+                assert!(
+                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
+                    "expected {:?}, got {:?}",
+                    expected_output,
+                    output
+                );
+            }
+        }
+
+        let err_cases = vec![(vec![1.0, 2.0], vec![3.0])];
+        for (arg1, arg2) in err_cases {
+            let arg1 = VectorFloat32::new(arg1).unwrap();
+            let arg2 = VectorFloat32::new(arg2).unwrap();
+            let output: Result<Option<Real>> = RpnFnScalarEvaluator::new()
+                .push_param(arg1)
+                .push_param(arg2)
+                .evaluate(ScalarFuncSig::VecNegativeInnerProductSig);
+            assert!(output.is_err(), "expected error, got {:?}", output);
+        }
+    }
+
+    #[test]
+    fn test_cosine_distance() {
+        let ok_cases = vec![
+            (vec![1.0, 2.0], vec![2.0, 4.0], Some(0.0)),
+            (vec![1.0, 2.0], vec![0.0, 0.0], None), // NaN turns to NULL
+            (vec![1.0, 1.0], vec![1.0, 1.0], Some(0.0)),
+            (vec![1.0, 0.0], vec![0.0, 2.0], Some(1.0)),
+            (vec![1.0, 1.0], vec![-1.0, -1.0], Some(2.0)),
+            (vec![1.0, 1.0], vec![1.1, 1.1], Some(0.0)),
+            (vec![1.0, 1.0], vec![-1.1, -1.1], Some(2.0)),
+            (vec![3e38], vec![3e38], None), // NaN turns to NULL
+        ];
+        for (arg1, arg2, expected_output) in ok_cases {
+            let arg1 = VectorFloat32::new(arg1).unwrap();
+            let arg2 = VectorFloat32::new(arg2).unwrap();
+            let output: Option<Real> = RpnFnScalarEvaluator::new()
+                .push_param(arg1)
+                .push_param(arg2)
+                .evaluate(ScalarFuncSig::VecCosineDistanceSig)
+                .unwrap();
+            if expected_output.is_none() {
+                assert!(output.is_none(), "expected None, got {:?}", output);
+            } else {
+                assert!(
+                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
+                    "expected {:?}, got {:?}",
+                    expected_output,
+                    output
+                );
+            }
+        }
+
+        let err_cases = vec![(vec![1.0, 2.0], vec![3.0])];
+        for (arg1, arg2) in err_cases {
+            let arg1 = VectorFloat32::new(arg1).unwrap();
+            let arg2 = VectorFloat32::new(arg2).unwrap();
+            let output: Result<Option<Real>> = RpnFnScalarEvaluator::new()
+                .push_param(arg1)
+                .push_param(arg2)
+                .evaluate(ScalarFuncSig::VecCosineDistanceSig);
+            assert!(output.is_err(), "expected error, got {:?}", output);
+        }
+    }
+
+    #[test]
+    fn test_l1_distance() {
+        let ok_cases = vec![
+            (vec![0.0, 0.0], vec![3.0, 4.0], Some(7.0)),
+            (vec![0.0, 0.0], vec![0.0, 1.0], Some(1.0)),
+            (vec![3e38], vec![-3e38], Some(f64::INFINITY)),
+        ];
+        for (arg1, arg2, expected_output) in ok_cases {
+            let arg1 = VectorFloat32::new(arg1).unwrap();
+            let arg2 = VectorFloat32::new(arg2).unwrap();
+            let output: Option<Real> = RpnFnScalarEvaluator::new()
+                .push_param(arg1)
+                .push_param(arg2)
+                .evaluate(ScalarFuncSig::VecL1DistanceSig)
+                .unwrap();
+            if expected_output.is_none() {
+                assert!(output.is_none(), "expected None, got {:?}", output);
+            } else {
+                assert!(
+                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
+                    "expected {:?}, got {:?}",
+                    expected_output,
+                    output
+                );
+            }
+        }
+
+        let err_cases = vec![(vec![1.0, 2.0], vec![3.0])];
+        for (arg1, arg2) in err_cases {
+            let arg1 = VectorFloat32::new(arg1).unwrap();
+            let arg2 = VectorFloat32::new(arg2).unwrap();
+            let output: Result<Option<Real>> = RpnFnScalarEvaluator::new()
+                .push_param(arg1)
+                .push_param(arg2)
+                .evaluate(ScalarFuncSig::VecL1DistanceSig);
+            assert!(output.is_err(), "expected error, got {:?}", output);
+        }
+    }
+}
