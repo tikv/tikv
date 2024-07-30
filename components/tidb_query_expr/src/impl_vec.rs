b48795fc1e03bbafb5dc6f9e4ca39f7ff8c7d7de
“EricZequan” <zequany33@gmail.com>
2024-07-30 18:09:09 +0800

    Fix vector precision
    Signed-off-by: “EricZequan” <zequany33@gmail.com>

--- components/tidb_query_expr/src/impl_vec.rs
+++ components/tidb_query_expr/src/impl_vec.rs
@@ -58,24 +58,6 @@ mod tests {
     use super::*;
     use crate::types::test_util::RpnFnScalarEvaluator;
 
-    fn real_eq(a: Real, b: Real) -> bool {
-        if a.is_nan() {
-            return b.is_nan();
-        }
-        if a.is_infinite() {
-            return b.is_infinite() && (a.is_sign_positive() == b.is_sign_positive());
-        }
-        (a.into_inner() - b.into_inner()).abs() < 1e-6
-    }
-
-    fn option_real_eq(a: Option<Real>, b: Option<Real>) -> bool {
-        match (a, b) {
-            (Some(a), Some(b)) => real_eq(a, b),
-            (None, None) => true,
-            _ => false,
-        }
-    }
-
     // Test cases are ported from pgvector: https://github.com/pgvector/pgvector/blob/master/test/expected/functions.out
     // Copyright (c) 1996-2023, PostgreSQL Global Development Group
 
@@ -110,16 +92,7 @@ mod tests {
                 .push_param(arg)
                 .evaluate(ScalarFuncSig::VecL2NormSig)
                 .unwrap();
-            if expected_output.is_none() {
-                assert!(output.is_none(), "expected None, got {:?}", output);
-            } else {
-                assert!(
-                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
-                    "expected {:?}, got {:?}",
-                    expected_output,
-                    output
-                );
-            }
+            assert_eq!(output, expected_output.map(|x| Real::new(x).unwrap()));
         }
     }
 
@@ -138,16 +111,7 @@ mod tests {
                 .push_param(arg2)
                 .evaluate(ScalarFuncSig::VecL2DistanceSig)
                 .unwrap();
-            if expected_output.is_none() {
-                assert!(output.is_none(), "expected None, got {:?}", output);
-            } else {
-                assert!(
-                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
-                    "expected {:?}, got {:?}",
-                    expected_output,
-                    output
-                );
-            }
+            assert_eq!(output, expected_output.map(|x| Real::new(x).unwrap()));
         }
 
         let err_cases = vec![(vec![1.0, 2.0], vec![3.0])];
@@ -176,16 +140,7 @@ mod tests {
                 .push_param(arg2)
                 .evaluate(ScalarFuncSig::VecNegativeInnerProductSig)
                 .unwrap();
-            if expected_output.is_none() {
-                assert!(output.is_none(), "expected None, got {:?}", output);
-            } else {
-                assert!(
-                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
-                    "expected {:?}, got {:?}",
-                    expected_output,
-                    output
-                );
-            }
+            assert_eq!(output, expected_output.map(|x| Real::new(x).unwrap()));
         }
 
         let err_cases = vec![(vec![1.0, 2.0], vec![3.0])];
@@ -220,16 +175,7 @@ mod tests {
                 .push_param(arg2)
                 .evaluate(ScalarFuncSig::VecCosineDistanceSig)
                 .unwrap();
-            if expected_output.is_none() {
-                assert!(output.is_none(), "expected None, got {:?}", output);
-            } else {
-                assert!(
-                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
-                    "expected {:?}, got {:?}",
-                    expected_output,
-                    output
-                );
-            }
+            assert_eq!(output, expected_output.map(|x| Real::new(x).unwrap()));
         }
 
         let err_cases = vec![(vec![1.0, 2.0], vec![3.0])];
@@ -259,16 +205,7 @@ mod tests {
                 .push_param(arg2)
                 .evaluate(ScalarFuncSig::VecL1DistanceSig)
                 .unwrap();
-            if expected_output.is_none() {
-                assert!(output.is_none(), "expected None, got {:?}", output);
-            } else {
-                assert!(
-                    option_real_eq(output, expected_output.map(|x| Real::new(x).unwrap())),
-                    "expected {:?}, got {:?}",
-                    expected_output,
-                    output
-                );
-            }
+            assert_eq!(output, expected_output.map(|x| Real::new(x).unwrap()));
         }
 
         let err_cases = vec![(vec![1.0, 2.0], vec![3.0])];
