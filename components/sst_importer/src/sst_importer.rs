daa0fc22f632e94d3aa010c5f4ac7635e3188c7f
lance6716 <lance6716@gmail.com>
2024-08-06 14:30:30 +0800

    fix UT
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- components/sst_importer/src/sst_importer.rs
+++ components/sst_importer/src/sst_importer.rs
@@ -2680,7 +2680,7 @@ mod tests {
             db,
         );
 
-        let path = importer.dir.join(&meta).unwrap();
+        let path = importer.dir.join_for_read(&meta).unwrap();
         assert!(!file_system::file_exists(path.save));
 
         match result {
