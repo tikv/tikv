a2a81a9af7956485762b7faf7e60e0f3a154c78e
Jianjun Liao <36503113+Leavrth@users.noreply.github.com>
2024-08-20 15:25:42 +0800

    backup-stream: clean the `pause-guard-gc-safepoint` when unregister the log task (#17317)
    close tikv/tikv#17316
    
    clean `pause-guard-gc-safepoint` when unregister the log task
    
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- components/backup-stream/src/endpoint.rs
+++ components/backup-stream/src/endpoint.rs
@@ -685,18 +685,7 @@ where
         );
 
         let task_name = task.info.get_name().to_owned();
-        // clean the safepoint created at pause(if there is)
-        self.pool.spawn(root!("load_initial_task";
-            self.pd_client
-                .update_service_safe_point(
-                    self.pause_guard_id_for_task(task.info.get_name()),
-                    TimeStamp::zero(),
-                    Duration::new(0, 0),
-                )
-                .map(|r| {
-                    r.map_err(|err| Error::from(err).report("removing safe point for pausing"))
-                })
-        ));
+        self.clean_pause_guard_id_for_task(&task_name);
         self.pool.block_on(async move {
             let task_name_clone = task.info.get_name().to_owned();
             let run = async move {
@@ -738,6 +727,21 @@ where
         metrics::update_task_status(TaskStatus::Running, &task_name);
     }
 
+    // clean the safepoint created at pause(if there is)
+    fn clean_pause_guard_id_for_task(&self, task_name: &str) {
+        self.pool.spawn(root!("unregister_task";
+        self.pd_client
+            .update_service_safe_point(
+                self.pause_guard_id_for_task(task_name),
+                TimeStamp::zero(),
+                Duration::new(0, 0),
+            )
+            .map(|r| {
+                r.map_err(|err| Error::from(err).report("removing safe point for pausing"))
+            })
+        ));
+    }
+
     fn pause_guard_id_for_task(&self, task: &str) -> String {
         format!("{}-{}-pause-guard", task, self.store_id)
     }
@@ -772,9 +776,10 @@ where
         }
     }
 
-    pub fn on_unregister(&self, task: &str) -> Option<StreamBackupTaskInfo> {
-        let info = self.unload_task(task);
-        self.remove_metrics_after_unregister(task);
+    pub fn on_unregister(&self, task_name: &str) -> Option<StreamBackupTaskInfo> {
+        let info = self.unload_task(task_name);
+        self.clean_pause_guard_id_for_task(task_name);
+        self.remove_metrics_after_unregister(task_name);
         info
     }
 
