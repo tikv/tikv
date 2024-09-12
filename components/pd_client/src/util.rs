cb84f955af2f001825880639b819f07a1b08eac4
glorv <glorvs@163.com>
2024-09-12 17:49:50 +0800

    fix lint
    Signed-off-by: glorv <glorvs@163.com>

--- components/pd_client/src/util.rs
+++ components/pd_client/src/util.rs
@@ -951,7 +951,6 @@ pub fn check_resp_header(header: &ResponseHeader) -> Result<()> {
         ErrorType::DuplicatedEntry | ErrorType::EntryNotFound => Err(box_err!(err.get_message())),
         ErrorType::Unknown => Err(box_err!(err.get_message())),
         ErrorType::InvalidValue => Err(box_err!(err.get_message())),
-        ErrorType::RegionsNotContainAllKeyRange => Err(box_err!(err.get_message())),
         ErrorType::GlobalConfigNotFound => panic!("unexpected error {:?}", err),
         // It will not happen, because we don't call `batch_scan_regions` in TiKV.
         ErrorType::RegionsNotContainAllKeyRange => Err(box_err!(err.get_message())),
