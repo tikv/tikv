8ec1aae464f9afe3eb9ad7b44369055efd3917a0
Bisheng Huang <hbisheng@gmail.com>
2024-09-12 17:40:10 +0800

    raftstore: fix panic due to stale peer handling snapshot
    The commit fixes a panic in TiKV that occurs in a rare scenario that involves region splits and immediate peer removal.
    
    When a region splits, the new peer on a follower can be created in two ways:
    1. By receiving a Raft message from the new region (`fn maybe_create_peer`).
    2. By applying the split operation locally (`fn on_ready_split_region`).
    Depending on timing, a new peer might first be created by a Raft message and then again when the split is applied. This is a known situation. When it happens, the second peer replaces the first, and the first peer is dicarded. However, the discarded peer may continue processing existing messages, leading to unexpected states.
    
    The panic can be reproduced with the following sequence of events:
    1. The first peer is created by a Raft message and is waiting for a Raft snapshot.
    2. The second peer (of the same reason) is created by `on_ready_split_region` when the split operation is applied, replacing the first peer and closing its mailbox (as expected).
    3. The second peer is immediately removed (a rare situation). This removes the region metadata.
    4. The first peer continues processing the Raft snapshot message, expecting the metadata of the region to exist, causing the panic.
    
    Signed-off-by: Bisheng Huang <hbisheng@gmail.com>

--- tests/failpoints/cases/test_split_region.rs
+++ tests/failpoints/cases/test_split_region.rs
@@ -552,6 +552,88 @@ fn test_split_not_to_split_existing_tombstone_region() {
     must_get_equal(&cluster.get_engine(2), b"k1", b"v1");
 }
 
+#[test]
+fn test_snap_handling_after_peer_is_replaced_by_split_and_removed() {
+    // The following diagram represents the final state of the test:
+    //
+    //                    ┌───────────┐  ┌───────────┐  ┌───────────┐
+    //                    │           │  │           │  │           │
+    //      Region 1      │ Peer 1    │  │ Peer 2    │  │ Peer 3    │
+    //      (-∞, k2)      │           │  │           │  │           │
+    // ───────────────────┼───────────┼──┼───────────┼──┼───────────┼──
+    //                    │           │  │           │  │           │
+    //      Region 1000   │ Peer 1001 │  │ Peer 1003 │  │ Peer 1002 │
+    //      [k2, +∞)      │           │  │           │  │           │
+    //                    └───────────┘  └───────────┘  └───────────┘
+    //                       Store 1        Store 2        Store 3
+    //
+    //
+    // In this test, there is a split operation and Peer 1003 will be created
+    // twice (by raft message and by split). The new Peer 1003 will replace the
+    // old Peer 1003 and but it will be immediately removed. This test verifies
+    // that TiKV would not panic if the old Peer 1003 continues to process a
+    // snapshot message.
+
+    let mut cluster = new_node_cluster(0, 3);
+    configure_for_merge(&mut cluster.cfg);
+    cluster.cfg.raft_store.right_derive_when_split = true;
+    cluster.cfg.raft_store.store_batch_system.max_batch_size = Some(1);
+    cluster.cfg.raft_store.store_batch_system.pool_size = 2;
+    cluster.cfg.raft_store.apply_batch_system.max_batch_size = Some(1);
+    cluster.cfg.raft_store.apply_batch_system.pool_size = 2;
+    let pd_client = Arc::clone(&cluster.pd_client);
+    pd_client.disable_default_operator();
+
+    fail::cfg("on_raft_gc_log_tick", "return()").unwrap();
+    let r1 = cluster.run_conf_change();
+    // Add Peer 3
+    pd_client.must_add_peer(r1, new_peer(3, 3));
+    assert_eq!(r1, 1);
+
+    // Pause the snapshot apply of Peer 2.
+    let before_check_snapshot_1_2_fp = "before_check_snapshot_1_2";
+    fail::cfg(before_check_snapshot_1_2_fp, "pause").unwrap();
+    // Add Peer 2
+    pd_client.must_add_peer(r1, new_peer(2, 2));
+
+    cluster.must_put(b"k1", b"v1");
+    cluster.must_put(b"k2", b"v2");
+
+    // Before the split, pause the snapshot apply of Peer 1003.
+    let before_check_snapshot_1000_2_fp = "before_check_snapshot_1000_2";
+    fail::cfg(before_check_snapshot_1000_2_fp, "pause").unwrap();
+
+    // Split the region into Region 1 and Region 1000. Peer 1003 will be created
+    // for the first time when it receives a raft message from Peer 1001, but it
+    // will remain uninitialized, waiting for the snapshot.
+    let region = pd_client.get_region(b"k1").unwrap();
+    cluster.must_split(&region, b"k2");
+
+    cluster.must_put(b"k22", b"v22");
+
+    // Check that Store 2 doesn't have any data yet.
+    must_get_none(&cluster.get_engine(2), b"k1");
+
+    // Unblock Peer 2. It will proceed to apply the split operation, which
+    // creates Peer 1003 for the second time and replaces the old Peer 1003.
+    fail::remove(before_check_snapshot_1_2_fp);
+
+    // Verify that data can be accessed from the new Peer 1003.
+    must_get_equal(&cluster.get_engine(2), b"k1", b"v1");
+
+    // Immediately remove the new Peer 1003. This removes the region metadata.
+    let left = pd_client.get_region(b"k1").unwrap();
+    let left_peer_2 = find_peer(&left, 2).cloned().unwrap();
+    pd_client.must_remove_peer(left.get_id(), left_peer_2);
+    must_get_none(&cluster.get_engine(2), b"k1");
+
+    // Unblock the old Peer 1003. When it continues to process the snapshot
+    // message, it would expect the region metadata to exist, causing a panic if
+    // #17469 is not fixed.
+    fail::remove(before_check_snapshot_1000_2_fp);
+    must_get_none(&cluster.get_engine(2), b"k1");
+}
+
 // TiKV uses memory lock to control the order between spliting and creating
 // new peer. This case test if tikv continues split if the peer is destroyed
 // after memory lock check.
