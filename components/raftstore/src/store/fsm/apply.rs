14ab849c520c450fafbd52db37c6a10a8161eba0
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-09-24 19:22:40 +0800

    batch-system: Reduce the memory usage of peers' message channel  (#17326) (#17567)
    close tikv/tikv#16229
    
    Reduce the memory usage of peers' message channel
    
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>
    
    Co-authored-by: lucasliang <nkcs_lykx@hotmail.com>
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- components/raftstore/src/store/fsm/apply.rs
+++ components/raftstore/src/store/fsm/apply.rs
@@ -918,7 +918,8 @@ where
     /// All of messages that need to continue to be handled after
     /// the source peer has applied its logs and pending entries
     /// are all handled.
-    pending_msgs: Vec<Msg<EK>>,
+    #[allow(clippy::vec_box)]
+    pending_msgs: Vec<Box<Msg<EK>>>,
 
     /// Cache heap size for itself.
     heap_size: Option<usize>,
@@ -2864,11 +2865,11 @@ where
             fail_point!("before_handle_catch_up_logs_for_merge");
             // Sends message to the source peer fsm and pause `exec_commit_merge` process
             let logs_up_to_date = Arc::new(AtomicU64::new(0));
-            let msg = SignificantMsg::CatchUpLogs(CatchUpLogs {
+            let msg = Box::new(SignificantMsg::CatchUpLogs(CatchUpLogs {
                 target_region_id: self.region_id(),
                 merge: merge.to_owned(),
                 logs_up_to_date: logs_up_to_date.clone(),
-            });
+            }));
             ctx.notifier
                 .notify_one(source_region_id, PeerMsg::SignificantMsg(msg));
             return Ok((
@@ -3771,10 +3772,13 @@ where
     },
 }
 
-impl<EK: KvEngine> ResourceMetered for Msg<EK> {
+impl<EK: KvEngine> ResourceMetered for Box<Msg<EK>> {
     fn consume_resource(&self, resource_ctl: &Arc<ResourceController>) -> Option<String> {
-        match self {
-            Msg::Apply { apply, .. } => {
+        if !resource_ctl.is_customized() {
+            return None;
+        }
+        match **self {
+            Msg::Apply { ref apply, .. } => {
                 let mut dominant_group = "".to_owned();
                 let mut max_write_bytes = 0;
                 for cached_entries in &apply.entries {
@@ -3907,7 +3911,7 @@ where
     EK: KvEngine,
 {
     delegate: ApplyDelegate<EK>,
-    receiver: Receiver<Msg<EK>>,
+    receiver: Receiver<Box<Msg<EK>>>,
     mailbox: Option<BasicMailbox<ApplyFsm<EK>>>,
 }
 
@@ -3917,12 +3921,14 @@ where
 {
     fn from_peer<ER: RaftEngine>(
         peer: &Peer<EK, ER>,
-    ) -> (LooseBoundedSender<Msg<EK>>, Box<ApplyFsm<EK>>) {
+    ) -> (LooseBoundedSender<Box<Msg<EK>>>, Box<ApplyFsm<EK>>) {
         let reg = Registration::new(peer);
         ApplyFsm::from_registration(reg)
     }
 
-    fn from_registration(reg: Registration) -> (LooseBoundedSender<Msg<EK>>, Box<ApplyFsm<EK>>) {
+    fn from_registration(
+        reg: Registration,
+    ) -> (LooseBoundedSender<Box<Msg<EK>>>, Box<ApplyFsm<EK>>) {
         let (tx, rx) = loose_bounded(usize::MAX);
         let delegate = ApplyDelegate::from_registration(reg);
         (
@@ -4093,13 +4099,11 @@ where
             self.destroy(ctx);
             ctx.notifier.notify_one(
                 self.delegate.region_id(),
-                PeerMsg::ApplyRes {
-                    res: TaskRes::Destroy {
-                        region_id: self.delegate.region_id(),
-                        peer_id: self.delegate.id(),
-                        merge_from_snapshot: d.merge_from_snapshot,
-                    },
-                },
+                PeerMsg::ApplyRes(Box::new(TaskRes::Destroy {
+                    region_id: self.delegate.region_id(),
+                    peer_id: self.delegate.id(),
+                    merge_from_snapshot: d.merge_from_snapshot,
+                })),
             );
         }
     }
@@ -4163,7 +4167,7 @@ where
             .store(region_id, Ordering::SeqCst);
         // To trigger the target apply fsm
         if let Some(mailbox) = ctx.router.mailbox(catch_up_logs.target_region_id) {
-            let _ = mailbox.force_send(Msg::Noop);
+            let _ = mailbox.force_send(Box::new(Msg::Noop));
         } else {
             error!(
                 "failed to get mailbox, are we shutting down?";
@@ -4350,7 +4354,8 @@ where
         }
     }
 
-    fn handle_tasks(&mut self, apply_ctx: &mut ApplyContext<EK>, msgs: &mut Vec<Msg<EK>>) {
+    #[allow(clippy::vec_box)]
+    fn handle_tasks(&mut self, apply_ctx: &mut ApplyContext<EK>, msgs: &mut Vec<Box<Msg<EK>>>) {
         let mut drainer = msgs.drain(..);
         let mut batch_apply = None;
         loop {
@@ -4365,7 +4370,7 @@ where
             };
 
             if batch_apply.is_some() {
-                match &msg {
+                match *msg {
                     Msg::Apply { .. } => (),
                     _ => {
                         self.handle_apply(apply_ctx, batch_apply.take().unwrap());
@@ -4378,7 +4383,7 @@ where
                 }
             }
 
-            match msg {
+            match *msg {
                 Msg::Apply { start, mut apply } => {
                     let apply_wait = start.saturating_elapsed();
                     apply_ctx.apply_wait.observe(apply_wait.as_secs_f64());
@@ -4399,7 +4404,9 @@ where
                         } else {
                             self.handle_apply(apply_ctx, batch_apply.take().unwrap());
                             if let Some(ref mut state) = self.delegate.yield_state {
-                                state.pending_msgs.push(Msg::Apply { start, apply });
+                                state
+                                    .pending_msgs
+                                    .push(Box::new(Msg::Apply { start, apply }));
                                 state.pending_msgs.extend(drainer);
                                 break;
                             }
@@ -4445,7 +4452,7 @@ impl<EK> Fsm for ApplyFsm<EK>
 where
     EK: KvEngine,
 {
-    type Message = Msg<EK>;
+    type Message = Box<Msg<EK>>;
 
     #[inline]
     fn is_stopped(&self) -> bool {
@@ -4552,7 +4559,8 @@ pub struct ApplyPoller<EK>
 where
     EK: KvEngine,
 {
-    msg_buf: Vec<Msg<EK>>,
+    #[allow(clippy::vec_box)]
+    msg_buf: Vec<Box<Msg<EK>>>,
     apply_ctx: ApplyContext<EK>,
     messages_per_tick: usize,
     cfg_tracker: Tracker<Config>,
@@ -4784,9 +4792,9 @@ where
     EK: KvEngine,
 {
     pub fn schedule_task(&self, region_id: u64, msg: Msg<EK>) {
-        let reg = match self.try_send(region_id, msg) {
+        let reg = match self.try_send(region_id, Box::new(msg)) {
             Either::Left(Ok(())) => return,
-            Either::Left(Err(TrySendError::Disconnected(msg))) | Either::Right(msg) => match msg {
+            Either::Left(Err(TrySendError::Disconnected(msg))) | Either::Right(msg) => match *msg {
                 Msg::Registration(reg) => reg,
                 Msg::Apply { mut apply, .. } => {
                     info!(
@@ -5112,8 +5120,8 @@ mod tests {
     impl<EK: KvEngine> Notifier<EK> for TestNotifier<EK> {
         fn notify(&self, apply_res: Vec<ApplyRes<EK::Snapshot>>) {
             for r in apply_res {
-                let res = TaskRes::Apply(r);
-                let _ = self.tx.send(PeerMsg::ApplyRes { res });
+                let res = Box::new(TaskRes::Apply(r));
+                let _ = self.tx.send(PeerMsg::ApplyRes(res));
             }
         }
         fn notify_one(&self, _: u64, msg: PeerMsg<EK>) {
@@ -5320,10 +5328,7 @@ mod tests {
         E: KvEngine,
     {
         match receiver.recv_timeout(Duration::from_secs(3)) {
-            Ok(PeerMsg::ApplyRes {
-                res: TaskRes::Apply(res),
-                ..
-            }) => res,
+            Ok(PeerMsg::ApplyRes(box TaskRes::Apply(res))) => res,
             e => panic!("unexpected res {:?}", e),
         }
     }
@@ -5471,10 +5476,7 @@ mod tests {
             ],
         );
         let apply_res = match rx.recv_timeout(Duration::from_secs(3)) {
-            Ok(PeerMsg::ApplyRes {
-                res: TaskRes::Apply(res),
-                ..
-            }) => res,
+            Ok(PeerMsg::ApplyRes(box TaskRes::Apply(res))) => res,
             e => panic!("unexpected apply result: {:?}", e),
         };
         let apply_state_key = keys::apply_state_key(2);
@@ -5505,12 +5507,9 @@ mod tests {
 
         router.schedule_task(2, Msg::destroy(2, false));
         let (region_id, peer_id) = match rx.recv_timeout(Duration::from_secs(3)) {
-            Ok(PeerMsg::ApplyRes {
-                res: TaskRes::Destroy {
-                    region_id, peer_id, ..
-                },
-                ..
-            }) => (region_id, peer_id),
+            Ok(PeerMsg::ApplyRes(box TaskRes::Destroy {
+                region_id, peer_id, ..
+            })) => (region_id, peer_id),
             e => panic!("expected destroy result, but got {:?}", e),
         };
         assert_eq!(peer_id, 1);
@@ -7773,9 +7772,9 @@ mod tests {
             .unwrap();
         assert_ne!(initial_state.get_applied_index(), 0);
         match apply_res_rx.recv_timeout(Duration::from_secs(3)) {
-            Ok(PeerMsg::ApplyRes {
-                res: TaskRes::Apply(apply_res),
-            }) => assert_eq!(apply_res.apply_state, initial_state),
+            Ok(PeerMsg::ApplyRes(box TaskRes::Apply(apply_res))) => {
+                assert_eq!(apply_res.apply_state, initial_state)
+            }
             e => panic!("unexpected result: {:?}", e),
         }
         index_id += 1;
@@ -7807,9 +7806,9 @@ mod tests {
             .unwrap()
             .unwrap();
         match apply_res_rx.recv_timeout(Duration::from_secs(3)) {
-            Ok(PeerMsg::ApplyRes {
-                res: TaskRes::Apply(apply_res),
-            }) => assert_eq!(apply_res.apply_state, apply_state),
+            Ok(PeerMsg::ApplyRes(box TaskRes::Apply(apply_res))) => {
+                assert_eq!(apply_res.apply_state, apply_state)
+            }
             e => panic!("unexpected result: {:?}", e),
         }
         assert!(
