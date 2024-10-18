c0c10c761bc4edab4ee0da47719dcebbe680dbef
hhwyt <hhwyt1@gmail.com>
2024-10-18 16:12:32 +0800

    raftstore: fix typo
    Signed-off-by: hhwyt <hhwyt1@gmail.com>

--- components/raftstore/src/store/fsm/peer.rs
+++ components/raftstore/src/store/fsm/peer.rs
@@ -3694,7 +3694,7 @@ where
     // `MsgTransferLeader`.
     fn on_transfer_leader_msg(&mut self, msg: &eraftpb::Message, peer_disk_usage: DiskUsage) {
         info!(
-            "received transferring leader";
+            "received transfer leader";
             "region_id" => self.fsm.region_id(),
             "peer_id" => self.fsm.peer.peer_id(),
             "from" => msg.get_from(),
