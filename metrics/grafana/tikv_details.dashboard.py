f0713b6070b8b606f0d077221df353684a1178a4
Spade  A <71589810+SpadeA-Tang@users.noreply.github.com>
2024-10-25 15:57:53 +0800

    In-memory engine: display put/delete operations per second (#17695)
    ref tikv/tikv#16141
    
    display put/delete operations per second
    
    Signed-off-by: SpadeA-Tang <u6748471@anu.edu.au>
    Signed-off-by: SpadeA-Tang <tangchenjie1210@gmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- metrics/grafana/tikv_details.dashboard.py
+++ metrics/grafana/tikv_details.dashboard.py
@@ -4375,6 +4375,25 @@ def InMemoryEngine() -> RowPanel:
     layout.row(
         [
             graph_panel(
+                title="OPS",
+                description="Operation per second for cf",
+                yaxes=yaxes(left_format=UNITS.OPS_PER_SEC),
+                targets=[
+                    target(
+                        expr=expr_sum_rate(
+                            "tikv_in_memory_engine_kv_operations",
+                            by_labels=["instance", "type"],
+                        ),
+                        legend_format="{{type}}-{{instance}}",
+                        additional_groupby=True,
+                    ),
+                ],
+            )
+        ]
+    )
+    layout.row(
+        [
+            graph_panel(
                 title="Region Cache Hit",
                 description="Count of region cache hit",
                 targets=[
