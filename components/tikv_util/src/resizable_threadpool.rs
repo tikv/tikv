9a62450fb075fc3d8215ef3c3aad61b7cc686d29
RidRisR <79858083+RidRisR@users.noreply.github.com>
2024-11-07 08:50:20 +0100

    first try
    Signed-off-by: RidRisR <79858083+RidRisR@users.noreply.github.com>

--- components/tikv_util/src/resizable_threadpool.rs
+++ components/tikv_util/src/resizable_threadpool.rs
@@ -3,80 +3,65 @@
 use std::sync::{Arc, RwLock};
 
 use futures::Future;
-use tokio::{io::Result as TokioResult, runtime::Runtime};
-
-/// DaemonRuntime is a "background" runtime, which contains "daemon" tasks:
-/// any task spawn into it would run until finish even the runtime isn't
-/// referenced.
-pub struct DaemonRuntime(Option<Runtime>);
-
-impl DaemonRuntime {
-    /// spawn a daemon task to the runtime.
-    pub fn spawn(self: &Arc<Self>, f: impl Future<Output = ()> + Send + 'static) {
-        let wkr = self.clone();
-        self.0.as_ref().unwrap().spawn(async move {
-            f.await;
-            drop(wkr)
-        });
-    }
-
-    /// create a daemon runtime from some runtime.
-    pub fn from_runtime(rt: Runtime) -> Arc<Self> {
-        Arc::new(Self(Some(rt)))
-    }
-}
-
-impl Drop for DaemonRuntime {
-    fn drop(&mut self) {
-        if let Some(runtime) = self.0.take() {
-            runtime.shutdown_background();
-        }
-    }
-}
+use std::sync::Mutex;
+use tokio::{io::Result as TokioResult, runtime::{Runtime, Handle}};
 
 pub struct ResizableRuntime {
     pub size: usize,
     thread_name: String,
-    pool: Option<Arc<DaemonRuntime>>,
+    pool: Arc<Mutex<Option<Handle>>>, // Use Arc<Mutex> for thread-safe updates
+    all_pools: Vec<Runtime>, // Keep track of all pools
     replace_pool_rule: Box<dyn Fn(usize, &str) -> TokioResult<Runtime> + Send + Sync>,
     after_adjust: Box<dyn Fn(usize) + Send + Sync>,
 }
 
 impl ResizableRuntime {
     pub fn new(
+        thread_size: usize,
         thread_name: &str,
         replace_pool_rule: Box<dyn Fn(usize, &str) -> TokioResult<Runtime> + Send + Sync>,
         after_adjust: Box<dyn Fn(usize) + Send + Sync>,
     ) -> Self {
-        ResizableRuntime {
+        let mut ret = ResizableRuntime {
             size: 0,
             thread_name: thread_name.to_owned(),
-            pool: None,
+            pool: Arc::new(Mutex::new(None)),
+            all_pools: Vec::new(),
             replace_pool_rule,
             after_adjust,
-        }
+        };
+
+        ret.adjust_with(thread_size);
+        ret
     }
 
     pub fn spawn<Fut>(&self, fut: Fut)
     where
         Fut: Future<Output = ()> + Send + 'static,
     {
-        self.pool
-            .as_ref()
-            .expect("ResizableRuntime: please call adjust_with() before spawn()")
-            .spawn(fut);
+        let pool_guard = self.pool.lock().unwrap();
+        if let Some(pool) = pool_guard.as_ref() {
+            pool.spawn(fut);
+        } else {
+            panic!("ResizableRuntime: please call adjust_with() before spawn()");
+        }
     }
 
-    /// Lazily adjust the thread pool's size
     pub fn adjust_with(&mut self, new_size: usize) {
         if self.size == new_size {
             return;
         }
-        // TODO: after tokio supports adjusting thread pool size(https://github.com/tokio-rs/tokio/issues/3329),
-        //   adapt it.
-        let pool = (self.replace_pool_rule)(new_size, &self.thread_name)
+
+        let new_pool = (self.replace_pool_rule)(new_size, &self.thread_name)
             .expect("failed to create tokio runtime for backup worker.");
-        self.pool = Some(DaemonRuntime::from_runtime(pool));
+        let handle = new_pool.handle().clone();
+        self.all_pools.push(new_pool);
+
+        {
+            let mut pool_guard = self.pool.lock().unwrap();
+            *pool_guard = Some(handle);
+        }
+
         self.size = new_size;
         (self.after_adjust)(new_size);
     }
@@ -85,13 +70,18 @@ impl ResizableRuntime {
     where
         F: Future,
     {
-        self.pool
-            .as_ref()
-            .expect("ResizableRuntime: please call adjust_with() before block_on()")
-            .0
-            .as_ref()
-            .unwrap()
-            .block_on(f)
+        let pool_guard = self.pool.lock().unwrap();
+        if let Some(pool) = pool_guard.as_ref() {
+            pool.block_on(f)
+        } else {
+            panic!("ResizableRuntime: please call adjust_with() before block_on()");
+        }
+    }
+}
+
+impl Drop for ResizableRuntime {
+    fn drop(&mut self) {
+        self.all_pools.clear(); // Drop all previously used pools
     }
 }
 
@@ -152,8 +142,7 @@ mod test {
             COUNTER.store(new_size, Ordering::SeqCst);
         };
         let mut threads =
-            ResizableRuntime::new("test", Box::new(replace_pool_rule), Box::new(after_adjust));
-        threads.adjust_with(4);
+            ResizableRuntime::new(4,"test", Box::new(replace_pool_rule), Box::new(after_adjust));
         assert_eq!(COUNTER.load(Ordering::SeqCst), 4);
         threads.adjust_with(8);
         assert_eq!(COUNTER.load(Ordering::SeqCst), 8);
