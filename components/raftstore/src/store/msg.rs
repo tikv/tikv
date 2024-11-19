049a78d293ef4f22a60c29d25c5deb86a28d4a0b
lucasliang <nkcs_lykx@hotmail.com>
2024-11-19 18:03:32 +0800

    raftstore: `campaign` newly created regions in time after `Split` (#17625)
    close tikv/tikv#12410
    
    This pr make the `campaign` of the newly splitted regions triggered in time, when the leadership of the parent region is stable after `on_role_changed`.
    
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- components/raftstore/src/store/msg.rs
+++ components/raftstore/src/store/msg.rs
@@ -554,6 +554,17 @@ where
     CheckPendingAdmin(UnboundedSender<CheckAdminResponse>),
 }
 
+/// Campaign type for triggering a Raft campaign.
+#[derive(Debug, Clone, Copy)]
+pub enum CampaignType {
+    /// Forcely campaign to be the leader.
+    ForceLeader,
+    /// Campaign triggered by the leader of a parent region. It's used to make
+    /// the new splitted peer campaign to get votes.
+    /// Only if the parent region has valid leader, will it be safe to do that.
+    UnsafeSplitCampaign,
+}
+
 /// Message that will be sent to a peer.
 ///
 /// These messages are not significant and can be dropped occasionally.
@@ -648,7 +659,8 @@ pub enum CasualMessage<EK: KvEngine> {
     },
 
     // Trigger raft to campaign which is used after exiting force leader
-    Campaign,
+    // or make new splitted peers campaign to get votes.
+    Campaign(CampaignType),
     // Trigger loading pending region for in_memory_engine,
     InMemoryEngineLoadRegion {
         region_id: u64,
@@ -724,7 +736,9 @@ impl<EK: KvEngine> fmt::Debug for CasualMessage<EK> {
                 "SnapshotApplied, peer_id={}, tombstone={}",
                 peer_id, tombstone
             ),
-            CasualMessage::Campaign => write!(fmt, "Campaign"),
+            CasualMessage::Campaign(_) => {
+                write!(fmt, "Campaign")
+            }
             CasualMessage::InMemoryEngineLoadRegion { region_id, .. } => write!(
                 fmt,
                 "[region={}] try load in memory region cache",
