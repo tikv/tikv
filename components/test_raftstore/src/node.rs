53b5a321898efb8036c87bd8424408e2e044ff59
lucasliang <nkcs_lykx@hotmail.com>
2024-12-09 18:25:50 +0800

    Use small intervals.
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>

--- components/test_raftstore/src/node.rs
+++ components/test_raftstore/src/node.rs
@@ -324,7 +324,7 @@ impl<EK: KvEngine> Simulator<EK> for NodeCluster<EK> {
             let engines = engines.clone();
             let data_dir = PathBuf::from(engines.kv.path());
             let snap_mgr = snap_mgr.clone();
-            bg_worker.spawn_interval_task(std::time::Duration::from_millis(100), move || {
+            bg_worker.spawn_interval_task(std::time::Duration::from_millis(1000), move || {
                 let snap_size = snap_mgr.get_total_snap_size().unwrap();
                 let kv_size = engines
                     .kv
