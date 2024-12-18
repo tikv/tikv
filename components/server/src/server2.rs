7ef4aa9549f136cabdac7e5177f39fe16631b514
lucasliang <nkcs_lykx@hotmail.com>
2024-12-18 18:02:33 +0800

    server: polish the logging when updating disk status. (#18025)
    ref tikv/tikv#17939
    
    Polish the logging when periodically updating the disk status.
    
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>
    
    Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

--- components/server/src/server2.rs
+++ components/server/src/server2.rs
@@ -1171,6 +1171,10 @@ where
         let snap_mgr = self.snap_mgr.clone().unwrap();
         let reserve_space = disk::get_disk_reserved_space();
         let reserve_raft_space = disk::get_raft_disk_reserved_space();
+        let need_update_disk_status = reserve_space != 0 || reserve_raft_space != 0;
+        if !need_update_disk_status {
+            info!("ignore updating disk status as no reserve space is set");
+        }
         let raft_engine = self.engines.as_ref().unwrap().raft_engine.clone();
         let tablet_registry = self.tablet_registry.clone().unwrap();
         let raft_path = raft_engine.get_engine_path().to_string();
@@ -1252,9 +1256,7 @@ where
                     );
                 }
                 // Update disk status if disk space checker is enabled.
-                if reserve_space == 0 && reserve_raft_space == 0 {
-                    info!("ignore updating disk status as no reserve space is set");
-                } else {
+                if need_update_disk_status {
                     disk::set_disk_status(cur_disk_status);
                 }
                 // Update disk capacity, used size and available size.
