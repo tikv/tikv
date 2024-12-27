45ec99c081acc77e236099ee0e48c1688353eecf
ekexium <eke@fastmail.com>
2024-12-27 16:37:12 +0800

    concurrency_manager: double check via PD TSO before reporting error of invalid max-ts update (#18057)
    close tikv/tikv#18055
    
    concurrency_manager: double check via PD TSO before reporting error of invalid max-ts update
    
    Signed-off-by: ekexium <eke@fastmail.com>

--- components/server/src/server.rs
+++ components/server/src/server.rs
@@ -28,10 +28,7 @@ use backup_stream::{
 };
 use causal_ts::CausalTsProviderImpl;
 use cdc::CdcConfigManager;
-use concurrency_manager::{
-    ConcurrencyManager, DEFAULT_MAX_TS_DRIFT_ALLOWANCE, DEFAULT_MAX_TS_SYNC_INTERVAL,
-    LIMIT_VALID_TIME_MULTIPLIER,
-};
+use concurrency_manager::{ActionOnInvalidMaxTs, ConcurrencyManager, DEFAULT_MAX_TS_DRIFT_ALLOWANCE, DEFAULT_MAX_TS_SYNC_INTERVAL, LIMIT_VALID_TIME_MULTIPLIER};
 use engine_rocks::{
     from_rocks_compression_type, RocksCompactedEvent, RocksEngine, RocksStatistics,
 };
@@ -117,7 +114,7 @@ use tikv::{
         config::EngineType,
         config_manager::StorageConfigManger,
         kv::LocalTablets,
-        mvcc::{MvccConsistencyCheckObserver, TimeStamp},
+        mvcc::MvccConsistencyCheckObserver,
         txn::{
             flow_controller::{EngineFlowController, FlowController},
             txn_status_cache::TxnStatusCache,
@@ -408,6 +405,9 @@ where
         let concurrency_manager = ConcurrencyManager::new_with_config(
             latest_ts,
             DEFAULT_MAX_TS_SYNC_INTERVAL * LIMIT_VALID_TIME_MULTIPLIER,
+            ActionOnInvalidMaxTs::Log,
+            Some(pd_client.clone()),
+            DEFAULT_MAX_TS_DRIFT_ALLOWANCE,
         );
 
         // use different quota for front-end and back-end requests
@@ -1172,12 +1172,11 @@ where
             .spawn_interval_async_task(max_ts_sync_interval, move || {
                 let cm = cm.clone();
                 let pd_client = pd_client.clone();
-                let allowance_ms = DEFAULT_MAX_TS_DRIFT_ALLOWANCE.as_millis() as u64;
 
                 async move {
                     let pd_tso = pd_client.get_tso().await;
                     if let Ok(ts) = pd_tso {
-                        cm.set_max_ts_limit(TimeStamp::compose(ts.physical() + allowance_ms, 0));
+                        cm.set_max_ts_limit(ts);
                     } else {
                         warn!("failed to get tso from pd in background, the max_ts validity check could be skipped");
                     }
