de15c1b00017c1f9799d377d0d3bf4b831c022df
hhwyt <hhwyt1@gmail.com>
2025-01-20 12:24:30 +0800

    rafstore: use keys::next_key
    Signed-off-by: hhwyt <hhwyt1@gmail.com>

--- src/server/gc_worker/compaction_filter.rs
+++ src/server/gc_worker/compaction_filter.rs
@@ -598,13 +598,11 @@ impl WriteCompactionFilter {
                                 .unwrap()
                                 .as_encoded()
                                 .clone();
-                            // Here, we can directly use [`smallest_key`, `largest_key`) for
-                            // locking. Since for the same key, the
-                            // `largest_key` here has a timestamp appended
-                            // at the end, while the locking for SST
-                            // ingestion uses `Region.start_key` without a
-                            // timestamp, an overlap exists, ensuring mutual exclusion.
-                            Some(engine.ingest_latch.acquire(smallest_key, largest_key))
+                            Some(
+                                engine
+                                    .ingest_latch
+                                    .acquire(smallest_key, keys::next_key(&largest_key)),
+                            )
                         }
                         None => None,
                     };
