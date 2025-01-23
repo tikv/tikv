tikv/tikv

commit e36cdcf039172a8587118694982a5618211ccc27

cdc: filter events with the observed range before load old values (#17656)
close tikv/tikv#16601, close tikv/tikv#17620

cdc: filter events with the observed range before load old values

Signed-off-by: qupeng <qupeng@pingcap.com>

components/cdc/src/delegate.rs
@@ -578,7 +578,6 @@ impl Delegate {
         request_id: u64,
         entries: Vec<Option<KvEntry>>,
         filter_loop: bool,
-        observed_range: &ObservedRange,
     ) -> Result<Vec<CdcEvent>> {
         let entries_len = entries.len();
         let mut rows = vec![Vec::with_capacity(entries_len)];
@@ -596,9 +595,6 @@ impl Delegate {
                     lock,
                     old_value,
                 })) => {
-                    if !observed_range.contains_encoded_key(&lock.0) {
-                        continue;
-                    }
                     let l = Lock::parse(&lock.1).unwrap();
                     if decode_lock(lock.0, l, &mut row, &mut _has_value) {
                         continue;
@@ -612,9 +608,6 @@ impl Delegate {
                     write,
                     old_value,
                 })) => {
-                    if !observed_range.contains_encoded_key(&write.0) {
-                        continue;
-                    }
                     if decode_write(write.0, &write.1, &mut row, &mut _has_value, false) {
                         continue;
                     }

components/cdc/src/initializer.rs
@@ -59,15 +59,6 @@ use crate::{
     Error, Result, Task,
 };
 
-struct ScanStat {
-    // Fetched bytes to the scanner.
-    emit: usize,
-    // Bytes from the device, `None` if not possible to get it.
-    disk_read: Option<usize>,
-    // Perf delta for RocksDB.
-    perf_delta: ReadPerfContext,
-}
-
 pub(crate) enum KvEntry {
     TxnEntry(TxnEntry),
     RawKvEntry(KvPair),
@@ -115,7 +106,7 @@ impl<E: KvEngine> Initializer<E> {
         cdc_handle: T,
         concurrency_semaphore: Arc<Semaphore>,
         memory_quota: Arc<MemoryQuota>,
-    ) -> Result<()> {
+    ) -> Result<InitializeStats> {
         fail_point!("cdc_before_initialize");
         let _permit = concurrency_semaphore.acquire().await;
 
@@ -170,7 +161,7 @@ impl<E: KvEngine> Initializer<E> {
         &mut self,
         mut resp: ReadResponse<impl EngineSnapshot>,
         memory_quota: Arc<MemoryQuota>,
-    ) -> Result<()> {
+    ) -> Result<InitializeStats> {
         if let Some(region_snapshot) = resp.snapshot {
             let region = region_snapshot.get_region().clone();
             assert_eq!(self.region_id, region.get_id());
@@ -192,7 +183,7 @@ impl<E: KvEngine> Initializer<E> {
         snap: S,
         region: Region,
         memory_quota: Arc<MemoryQuota>,
-    ) -> Result<()> {
+    ) -> Result<InitializeStats> {
         CDC_SCAN_TASKS.with_label_values(&["ongoing"]).inc();
         defer!(CDC_SCAN_TASKS.with_label_values(&["ongoing"]).dec());
 
@@ -201,7 +192,7 @@ impl<E: KvEngine> Initializer<E> {
         let observe_id = self.observe_id;
         let conn_id = self.conn_id;
         let kv_api = self.kv_api;
-        let on_cancel = || -> Result<()> {
+        let on_cancel = || -> Result<InitializeStats> {
             info!("cdc async incremental scan canceled";
                 "region_id" => region_id,
                 "downstream_id" => ?downstream_id,
@@ -272,6 +263,7 @@ impl<E: KvEngine> Initializer<E> {
             DownstreamState::Initializing | DownstreamState::Stopped
         ));
 
+        let mut stats = InitializeStats::default();
         while !done {
             // When downstream_state is Stopped, it means the corresponding
             // delegate is stopped. The initialization can be safely canceled.
@@ -280,7 +272,9 @@ impl<E: KvEngine> Initializer<E> {
             }
             let cursors = old_value_cursors.as_mut();
             let resolver = resolver.as_mut();
-            let entries = self.scan_batch(&mut scanner, cursors, resolver).await?;
+            let entries = self
+                .scan_batch(&mut scanner, cursors, resolver, &mut stats)
+                .await?;
             if let Some(None) = entries.last() {
                 // If the last element is None, it means scanning is finished.
                 done = true;
@@ -310,7 +304,7 @@ impl<E: KvEngine> Initializer<E> {
 
         CDC_SCAN_DURATION_HISTOGRAM.observe(takes.as_secs_f64());
         CDC_SCAN_SINK_DURATION_HISTOGRAM.observe(duration_to_sec(sink_time));
-        Ok(())
+        Ok(stats)
     }
 
     // It's extracted from `Initializer::scan_batch` to avoid becoming an
@@ -321,7 +315,7 @@ impl<E: KvEngine> Initializer<E> {
         scanner: &mut Scanner<S>,
         mut old_value_cursors: Option<&mut OldValueCursors<S::Iter>>,
         entries: &mut Vec<Option<KvEntry>>,
-    ) -> Result<ScanStat> {
+    ) -> Result<InitializeStats> {
         let mut read_old_value = |v: &mut OldValue, stats: &mut Statistics| -> Result<()> {
             let (wc, dc) = match old_value_cursors {
                 Some(ref mut x) => (&mut x.write, &mut x.default),
@@ -336,21 +330,28 @@ impl<E: KvEngine> Initializer<E> {
             Ok(())
         };
 
+        let mut stats = InitializeStats::default();
+
         // This code block shouldn't be switched to other threads.
         let mut total_bytes = 0;
         let mut total_size = 0;
         let perf_instant = ReadPerfInstant::new();
         let inspector = self_thread_inspector().ok();
         let old_io_stat = inspector.as_ref().and_then(|x| x.io_stat().unwrap_or(None));
-        let mut stats = Statistics::default();
         while total_bytes <= self.max_scan_batch_bytes && total_size < self.max_scan_batch_size {
             total_size += 1;
             match scanner {
                 Scanner::TxnKvScanner(scanner) => match scanner.next_entry()? {
                     Some(mut entry) => {
-                        read_old_value(entry.old_value(), &mut stats)?;
-                        total_bytes += entry.size();
-                        entries.push(Some(KvEntry::TxnEntry(entry)));
+                        let key = match entry {
+                            TxnEntry::Prewrite { ref lock, .. } => &lock.0,
+                            TxnEntry::Commit { ref write, .. } => &write.0,
+                        };
+                        if self.observed_range.contains_encoded_key(key) {
+                            read_old_value(entry.old_value(), &mut stats.old_value)?;
+                            total_bytes += entry.size();
+                            entries.push(Some(KvEntry::TxnEntry(entry)));
+                        }
                     }
                     None => {
                         entries.push(None);
@@ -374,19 +375,17 @@ impl<E: KvEngine> Initializer<E> {
                 }
             }
         }
-        flush_oldvalue_stats(&stats, TAG_INCREMENTAL_SCAN);
+        flush_oldvalue_stats(&stats.old_value, TAG_INCREMENTAL_SCAN);
         let new_io_stat = inspector.as_ref().and_then(|x| x.io_stat().unwrap_or(None));
-        let disk_read = match (old_io_stat, new_io_stat) {
+
+        stats.scan.emit = total_bytes;
+        stats.scan.disk_read = match (old_io_stat, new_io_stat) {
             (Some(s1), Some(s2)) => Some((s2.read - s1.read) as usize),
             _ => None,
         };
-        let perf_delta = perf_instant.delta();
-        let emit = total_bytes;
-        Ok(ScanStat {
-            emit,
-            disk_read,
-            perf_delta,
-        })
+        stats.scan.perf_delta = perf_instant.delta();
+
+        Ok(stats)
     }
 
     async fn scan_batch<S: Snapshot>(
@@ -394,22 +393,22 @@ impl<E: KvEngine> Initializer<E> {
         scanner: &mut Scanner<S>,
         old_value_cursors: Option<&mut OldValueCursors<S::Iter>>,
         resolver: Option<&mut Resolver>,
+        stats: &mut InitializeStats,
     ) -> Result<Vec<Option<KvEntry>>> {
         let mut entries = Vec::with_capacity(self.max_scan_batch_size);
-        let ScanStat {
-            emit,
-            disk_read,
-            perf_delta,
-        } = self.do_scan(scanner, old_value_cursors, &mut entries)?;
+        let delta_stats = self.do_scan(scanner, old_value_cursors, &mut entries)?;
+        stats.add(&delta_stats);
 
-        TLS_CDC_PERF_STATS.with(|x| *x.borrow_mut() += perf_delta);
+        TLS_CDC_PERF_STATS.with(|x| *x.borrow_mut() += delta_stats.scan.perf_delta);
         tls_flush_perf_stats();
-        if let Some(bytes) = disk_read {
+        if let Some(bytes) = delta_stats.scan.disk_read {
             CDC_SCAN_DISK_READ_BYTES.inc_by(bytes as _);
             self.scan_speed_limiter.consume(bytes).await;
         }
-        CDC_SCAN_BYTES.inc_by(emit as _);
-        self.fetch_speed_limiter.consume(emit as _).await;
+        CDC_SCAN_BYTES.inc_by(delta_stats.scan.emit as _);
+        self.fetch_speed_limiter
+            .consume(delta_stats.scan.emit as _)
+            .await;
 
         if let Some(resolver) = resolver {
             // Track the locks.
@@ -437,7 +436,6 @@ impl<E: KvEngine> Initializer<E> {
             self.request_id,
             entries,
             self.filter_loop,
-            &self.observed_range,
         )?;
         if done {
             let (cb, fut) = tikv_util::future::paired_future_callback();
@@ -515,6 +513,8 @@ impl<E: KvEngine> Initializer<E> {
     }
 
     fn ts_filter_is_helpful<S: Snapshot>(&self, snap: &S) -> bool {
+        fail_point!("ts_filter_is_helpful_always_true", |_| true);
+
         if self.ts_filter_ratio < f64::EPSILON {
             return false;
         }
@@ -563,6 +563,33 @@ impl<E: KvEngine> Initializer<E> {
     ) -> Option<u64> {
         prop.get(field.as_bytes())
             .and_then(|mut x| number::decode_u64(&mut x).ok())
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct InitializeStats {
+    pub old_value: Statistics,
+    pub scan: ScanStats,
+}
+
+#[derive(Default, Debug)]
+pub struct ScanStats {
+    // Fetched bytes to the scanner.
+    emit: usize,
+    // Bytes from the device, `None` if not possible to get it.
+    disk_read: Option<usize>,
+    // Perf delta for RocksDB.
+    perf_delta: ReadPerfContext,
+}
+
+impl InitializeStats {
+    fn add(&mut self, other: &InitializeStats) {
+        self.old_value.add(&other.old_value);
+        self.scan.emit += other.scan.emit;
+        if let Some(x) = self.scan.disk_read.as_mut() {
+            *x += other.scan.disk_read.unwrap_or_default();
+        }
+        self.scan.perf_delta += other.scan.perf_delta;
     }
 }
 
@@ -714,12 +741,14 @@ mod tests {
             total_bytes += v.len();
             let ts = TimeStamp::new(i as _);
             must_prewrite_put(&mut engine, k, v, k, ts);
-            let txn_locks = expected_locks.entry(ts).or_insert_with(|| {
-                let mut txn_locks = TxnLocks::default();
-                txn_locks.sample_lock = Some(k.to_vec().into());
-                txn_locks
-            });
-            txn_locks.lock_count += 1;
+            if i < 90 {
+                let txn_locks = expected_locks.entry(ts).or_insert_with(|| {
+                    let mut txn_locks = TxnLocks::default();
+                    txn_locks.sample_lock = Some(k.to_vec().into());
+                    txn_locks
+                });
+                txn_locks.lock_count += 1;
+            }
         }
 
         let region = Region::default();

components/cdc/src/endpoint.rs
@@ -59,7 +59,7 @@ use txn_types::{TimeStamp, TxnExtra, TxnExtraScheduler};
 use crate::{
     channel::{CdcEvent, SendError},
     delegate::{on_init_downstream, Delegate, Downstream, DownstreamId, DownstreamState},
-    initializer::Initializer,
+    initializer::{InitializeStats, Initializer},
     metrics::*,
     old_value::{OldValueCache, OldValueCallback},
     service::{validate_kv_api, Conn, ConnId, FeatureGate},
@@ -160,6 +160,7 @@ type InitCallback = Box<dyn FnOnce() + Send>;
 pub enum Validate {
     Region(u64, Box<dyn FnOnce(Option<&Delegate>) + Send>),
     OldValueCache(Box<dyn FnOnce(&OldValueCache) + Send>),
+    InitializeStats(Box<dyn FnOnce(InitializeStats) + Send>),
 }
 
 pub enum Task {
@@ -287,6 +288,7 @@ impl fmt::Debug for Task {
             Task::Validate(validate) => match validate {
                 Validate::Region(region_id, _) => de.field("region_id", &region_id).finish(),
                 Validate::OldValueCache(_) => de.finish(),
+                Validate::InitializeStats(_) => de.finish(),
             },
             Task::ChangeConfig(change) => de
                 .field("type", &"change_config")
@@ -402,6 +404,9 @@ pub struct Endpoint<T, E, S> {
     resolved_region_count: usize,
     unresolved_region_count: usize,
     warn_resolved_ts_repeat_count: usize,
+
+    // Validate statistics of the next incremental scan. Only for tests.
+    validate_next_initialize_stats: Option<Box<dyn FnOnce(InitializeStats) + Send>>,
 }
 
 impl<T: 'static + CdcHandle<E>, E: KvEngine, S: StoreRegionMeta> Endpoint<T, E, S> {
@@ -505,6 +510,8 @@ impl<T: 'static + CdcHandle<E>, E: KvEngine, S: StoreRegionMeta> Endpoint<T, E, 
             warn_resolved_ts_repeat_count: WARN_RESOLVED_TS_COUNT_THRESHOLD,
             current_ts: TimeStamp::zero(),
             causal_ts_provider,
+
+            validate_next_initialize_stats: None,
         };
         ep.register_min_ts_event(leader_resolver, Instant::now());
         ep
@@ -843,14 +850,18 @@ impl<T: 'static + CdcHandle<E>, E: KvEngine, S: StoreRegionMeta> Endpoint<T, E, 
         let cdc_handle = self.cdc_handle.clone();
         let concurrency_semaphore = self.scan_concurrency_semaphore.clone();
         let memory_quota = self.sink_memory_quota.clone();
+        let validate_initialize_stats = self.validate_next_initialize_stats.take();
         self.workers.spawn(async move {
             CDC_SCAN_TASKS.with_label_values(&["total"]).inc();
             match init
                 .initialize(change_cmd, cdc_handle, concurrency_semaphore, memory_quota)
                 .await
             {
-                Ok(()) => {
+                Ok(stats) => {
                     CDC_SCAN_TASKS.with_label_values(&["finish"]).inc();
+                    if let Some(validate) = validate_initialize_stats {
+                        validate(stats);
+                    }
                 }
                 Err(e) => {
                     CDC_SCAN_TASKS.with_label_values(&["abort"]).inc();
@@ -1318,6 +1329,9 @@ impl<T: 'static + CdcHandle<E>, E: KvEngine, S: StoreRegionMeta + Send> Runnable
                 }
                 Validate::OldValueCache(validate) => {
                     validate(&self.old_value_cache);
+                }
+                Validate::InitializeStats(validate) => {
+                    self.validate_next_initialize_stats = Some(validate);
                 }
             },
             Task::ChangeConfig(change) => self.on_change_cfg(change),

components/cdc/tests/failpoints/test_endpoint.rs
@@ -9,12 +9,16 @@ use std::{
 use api_version::{test_kv_format_impl, KvFormat};
 use causal_ts::CausalTsProvider;
 use cdc::{recv_timeout, Delegate, OldValueCache, Task, Validate};
+use engine_traits::{
+    IterOptions, Iterable, Iterator, MiscExt, Mutable, WriteBatch, WriteBatchExt, WriteOptions,
+    CF_DEFAULT, CF_WRITE,
+};
 use futures::{executor::block_on, sink::SinkExt};
 use grpcio::{ChannelBuilder, Environment, WriteFlags};
 use kvproto::{cdcpb::*, kvrpcpb::*, tikvpb_grpc::TikvClient};
 use pd_client::PdClient;
 use test_raftstore::*;
-use tikv_util::{debug, worker::Scheduler, HandyRwLock};
+use tikv_util::{debug, keybuilder::KeyBuilder, worker::Scheduler, HandyRwLock};
 use txn_types::{Key, TimeStamp};
 
 use crate::{new_event_feed, new_event_feed_v2, ClientReceiver, TestSuite, TestSuiteBuilder};
@@ -655,4 +659,93 @@ fn test_delegate_fail_during_incremental_scan() {
     let mut recver = recv.replace(None).unwrap();
     recv_timeout(&mut recver, Duration::from_secs(1)).unwrap_err();
     recv.replace(Some(recver));
+}
+
+#[test]
+fn test_cdc_load_unnecessary_old_value() {
+    let mut suite = TestSuite::new(1, ApiVersion::V1);
+    let region = suite.cluster.get_region(&[]);
+    let rid = region.id;
+    let engine = suite.cluster.get_engine(1);
+
+    let start_tso = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    let pk = format!("key_{:05}", 0).into_bytes();
+    let mut mutations = Vec::with_capacity(1000);
+    let mut keys = Vec::with_capacity(1000);
+    for i in 0..1000 {
+        let key = format!("key_{:05}", i).into_bytes();
+        keys.push(key.clone());
+
+        let mut mutation = Mutation::default();
+        mutation.set_op(Op::Put);
+        mutation.key = key;
+        mutation.value = vec![b'x'; 16];
+        mutations.push(mutation);
+    }
+    suite.must_kv_prewrite(rid, mutations, pk, start_tso);
+
+    let commit_tso = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    suite.must_kv_commit(rid, keys, start_tso, commit_tso);
+    engine.flush_cf(CF_WRITE, true).unwrap();
+
+    for cf in &[CF_WRITE, CF_DEFAULT] {
+        let mut wb = suite.cluster.get_engine(1).write_batch();
+        let mut count = 0;
+
+        let start = KeyBuilder::from_vec(vec![b'z'], 0, 0);
+        let end = KeyBuilder::from_vec(vec![b'z' + 1], 0, 0);
+        let iter_opts = IterOptions::new(Some(start), Some(end), false);
+        let mut iter = engine.iterator_opt(cf, iter_opts).unwrap();
+        let mut valid = iter.seek_to_first().unwrap();
+
+        // skip some keys.
+        while valid && count < 2 {
+            count += 1;
+            valid = iter.next().unwrap();
+        }
+        while valid {
+            count += 1;
+            let key = iter.key();
+            wb.delete_cf(cf, key).unwrap();
+            valid = iter.next().unwrap();
+        }
+        assert!(count == 0 || count == 1000);
+        wb.write_opt(&WriteOptions::default()).unwrap();
+        engine.flush_cf(cf, true).unwrap();
+    }
+
+    let scheduler = suite.endpoints.values().next().unwrap().scheduler();
+    let (tx, rx) = std::sync::mpsc::sync_channel(1);
+    scheduler
+        .schedule(Task::Validate(Validate::InitializeStats(Box::new(
+            move |stats| tx.send(stats).unwrap(),
+        ))))
+        .unwrap();
+
+    fail::cfg("ts_filter_is_helpful_always_true", "return(0)").unwrap();
+    let (mut req_tx, _, receive_event) = new_event_feed_v2(suite.get_region_cdc_client(rid));
+    let mut req = suite.new_changedata_request(rid);
+    req.request_id = 100;
+    req.checkpoint_ts = commit_tso.into_inner() - 1;
+    req.set_start_key(Key::from_raw(b"aa").into_encoded());
+    req.set_end_key(Key::from_raw(b"ab").into_encoded());
+    block_on(req_tx.send((req.clone(), WriteFlags::default()))).unwrap();
+
+    let events = receive_event(false).events.to_vec();
+    assert_eq!(events.len(), 1, "{:?}", events);
+    match events[0].event.as_ref().unwrap() {
+        Event_oneof_event::Entries(es) => {
+            assert!(es.entries.len() == 1);
+            assert_eq!(es.entries[0].get_type(), EventLogType::Initialized);
+        }
+        _ => unreachable!(),
+    }
+
+    let stats = rx.recv().unwrap().old_value.write;
+    assert_eq!(stats.seek_tombstone, 0);
+    assert_eq!(stats.next_tombstone, 0);
+    assert_eq!(stats.prev_tombstone, 0);
+
+    fail::remove("ts_filter_is_helpful_always_true");
+    suite.stop();
 }

commit ba5c0a0de34276e3980cc42caa87a3c4dbf7f39b

cdc: fix the panic introduced by #17656 (#18143)
close tikv/tikv#18142

cdc: fix the panic introduced by #17656

Signed-off-by: qupeng <qupeng@pingcap.com>

Co-authored-by: ti-chi-bot[bot] <108142056+ti-chi-bot[bot]@users.noreply.github.com>

components/cdc/src/delegate.rs
@@ -578,6 +578,7 @@ impl Delegate {
         request_id: u64,
         entries: Vec<Option<KvEntry>>,
         filter_loop: bool,
+        observed_range: &ObservedRange,
     ) -> Result<Vec<CdcEvent>> {
         let entries_len = entries.len();
         let mut rows = vec![Vec::with_capacity(entries_len)];
@@ -595,6 +596,9 @@ impl Delegate {
                     lock,
                     old_value,
                 })) => {
+                    if !observed_range.contains_encoded_key(&lock.0) {
+                        continue;
+                    }
                     let l = Lock::parse(&lock.1).unwrap();
                     if decode_lock(lock.0, l, &mut row, &mut _has_value) {
                         continue;
@@ -608,6 +612,9 @@ impl Delegate {
                     write,
                     old_value,
                 })) => {
+                    if !observed_range.contains_encoded_key(&write.0) {
+                        continue;
+                    }
                     if decode_write(write.0, &write.1, &mut row, &mut _has_value, false) {
                         continue;
                     }

components/cdc/src/initializer.rs
@@ -362,8 +362,8 @@ impl<E: KvEngine> Initializer<E> {
                         if self.observed_range.contains_encoded_key(key) {
                             read_old_value(entry.old_value(), &mut stats.old_value)?;
                             total_bytes += entry.size();
-                            entries.push(Some(KvEntry::TxnEntry(entry)));
                         }
+                        entries.push(Some(KvEntry::TxnEntry(entry)));
                     }
                     None => {
                         entries.push(None);
@@ -448,6 +448,7 @@ impl<E: KvEngine> Initializer<E> {
             self.request_id,
             entries,
             self.filter_loop,
+            &self.observed_range,
         )?;
         if done {
             let (cb, fut) = tikv_util::future::paired_future_callback();
@@ -753,14 +754,12 @@ mod tests {
             total_bytes += v.len();
             let ts = TimeStamp::new(i as _);
             must_prewrite_put(&mut engine, k, v, k, ts);
-            if i < 90 {
-                let txn_locks = expected_locks.entry(ts).or_insert_with(|| {
-                    let mut txn_locks = TxnLocks::default();
-                    txn_locks.sample_lock = Some(k.to_vec().into());
-                    txn_locks
-                });
-                txn_locks.lock_count += 1;
-            }
+            let txn_locks = expected_locks.entry(ts).or_insert_with(|| {
+                let mut txn_locks = TxnLocks::default();
+                txn_locks.sample_lock = Some(k.to_vec().into());
+                txn_locks
+            });
+            txn_locks.lock_count += 1;
         }
 
         let region = Region::default();

components/cdc/tests/integrations/test_cdc.rs
@@ -15,7 +15,7 @@ use tikv::server::DEFAULT_CLUSTER_ID;
 use tikv_util::{config::ReadableDuration, HandyRwLock};
 use txn_types::{Key, Lock, LockType};
 
-use crate::{new_event_feed, TestSuite, TestSuiteBuilder};
+use crate::{new_event_feed, new_event_feed_v2, TestSuite, TestSuiteBuilder};
 
 #[test]
 fn test_cdc_basic() {
@@ -2731,4 +2731,57 @@ fn test_cdc_filter_key_range() {
     }
 
     suite.stop();
+}
+
+#[test]
+fn test_partial_subscription_build_resolver() {
+    let cluster = new_server_cluster(0, 2);
+    cluster.pd_client.disable_default_operator();
+    let mut suite = TestSuiteBuilder::new().cluster(cluster).build();
+    let rid = suite.cluster.get_region(&[]).id;
+
+    let start_tso = block_on(suite.cluster.pd_client.get_tso()).unwrap();
+    let pk = format!("key_{:05}", 0).into_bytes();
+    let mut mutations = Vec::with_capacity(1000);
+    let mut keys = Vec::with_capacity(1000);
+    for i in 0..1 {
+        let key = format!("key_{:05}", i).into_bytes();
+        keys.push(key.clone());
+
+        let mut mutation = Mutation::default();
+        mutation.set_op(Op::Put);
+        mutation.key = key;
+        mutation.value = vec![b'x'; 16];
+        mutations.push(mutation);
+    }
+    suite.must_kv_prewrite(rid, mutations, pk, start_tso);
+
+    // Subscription a range without any locks.
+    let (mut req_tx, _, receive_event) = new_event_feed_v2(suite.get_region_cdc_client(rid));
+    let mut req = suite.new_changedata_request(rid);
+    req.request_id = 100;
+    req.checkpoint_ts = start_tso.into_inner() - 1;
+    req.set_start_key(Key::from_raw(b"m").into_encoded());
+    req.set_end_key(Key::from_raw(b"z").into_encoded());
+    block_on(req_tx.send((req.clone(), WriteFlags::default()))).unwrap();
+    println!("send request ok");
+
+    // The region subscription can be initialized correctly.
+    let events = receive_event(false).events.to_vec();
+    assert_eq!(events.len(), 1, "{:?}", events);
+    match events[0].event.as_ref().unwrap() {
+        Event_oneof_event::Entries(es) => {
+            assert!(es.entries.len() == 1);
+            assert_eq!(es.entries[0].get_type(), EventLogType::Initialized);
+        }
+        _ => unreachable!(),
+    }
+
+    sleep_ms(1000);
+
+    // However, ResolvedTs shouldn't be advanced incorrectly.
+    let event = receive_event(true);
+    assert!(event.events.is_empty());
+    assert!(event.has_resolved_ts());
+    assert_eq!(event.get_resolved_ts().ts, start_tso.into_inner());
 }