21ac1beb42400a9e8b4209a1e912c2a912c27e6b
Yang Zhang <yang.zhang@pingcap.com>
2025-02-14 00:07:26 -0800

    Add test
    Signed-off-by: Yang Zhang <yang.zhang@pingcap.com>

--- components/raftstore-v2/src/operation/unsafe_recovery/force_leader.rs
+++ components/raftstore-v2/src/operation/unsafe_recovery/force_leader.rs
@@ -4,6 +4,7 @@ use std::mem;
 
 use collections::HashSet;
 use engine_traits::{KvEngine, RaftEngine};
+use kvproto::metapb;
 use raft::{eraftpb::MessageType, StateRole, Storage};
 use raftstore::store::{
     util::LeaseState, ForceLeaderState, UnsafeRecoveryForceLeaderSyncer, UnsafeRecoveryState,
@@ -338,13 +339,14 @@ impl<EK: KvEngine, ER: RaftEngine> Peer<EK, ER> {
         let region = self.region();
         let mut replicated_idx = self.raft_group().raft.raft_log.persisted;
         for (peer_id, p) in self.raft_group().raft.prs().iter() {
-            let store_id = region
+            let peer = region
                 .get_peers()
                 .iter()
                 .find(|p| p.get_id() == *peer_id)
-                .unwrap()
-                .get_store_id();
-            if failed_stores.contains(&store_id) {
+                .unwrap();
+            if failed_stores.contains(&peer.get_store_id())
+                || peer.get_role() != metapb::PeerRole::Voter
+            {
                 continue;
             }
             if replicated_idx > p.matched {
