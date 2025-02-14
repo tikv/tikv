101707b1e9f6400cf146e2fb97b05721c33e38d2
mittalrishabh <mittalrishabh@gmail.com>
2025-02-14 00:52:58 -0800

    Update peer.rs
    Signed-off-by: mittalrishabh <mittalrishabh@gmail.com>
    Signed-off-by: rishabh_mittal <mittalrishabh@gmail.com>

--- components/raftstore/src/store/peer.rs
+++ components/raftstore/src/store/peer.rs
@@ -6073,7 +6073,7 @@ where
             txn_ext: self.txn_ext.clone(),
         }) {
             error!(
-                "failed to update max ts";
+                "failed to update max_ts";
                 "err" => ?e,
             );
         }
