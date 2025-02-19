fdc3d445c5be36dee17c282e6e6456e4f81e61ef
you06 <you1474600@gmail.com>
2025-02-19 16:59:12 +0900

    fix clippy
    Signed-off-by: you06 <you1474600@gmail.com>

--- src/storage/mvcc/reader/reader.rs
+++ src/storage/mvcc/reader/reader.rs
@@ -124,7 +124,7 @@ impl<S: EngineSnapshot> SnapshotReader<S> {
     }
 
     #[inline(always)]
-    pub fn setup_with_hint_items<T>(&mut self, items: &mut Vec<T>, key_of: fn(&T) -> &Key) {
+    pub fn setup_with_hint_items<T>(&mut self, items: &mut [T], key_of: fn(&T) -> &Key) {
         // enable scan mode if there are multiple items, so that we don't need to seek
         // for every key.
         if items.len() > 1 {
