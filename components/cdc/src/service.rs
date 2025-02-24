86e4421ca414d2a1a0921ca199d2a4ae41102623
wlwilliamx <wlwilliamx@gmail.com>
2025-02-24 17:14:30 +0800

    cdc: ensure `Deregister::Conn` is always triggered
    Signed-off-by: wlwilliamx <wlwilliamx@gmail.com>

--- components/cdc/src/service.rs
+++ components/cdc/src/service.rs
@@ -455,10 +455,6 @@ impl Service {
             while let Some(request) = stream.try_next().await? {
                 Self::handle_request(&scheduler, &peer, request, conn_id)?;
             }
-            let deregister = Deregister::Conn(conn_id);
-            if let Err(e) = scheduler.schedule(Task::Deregister(deregister)) {
-                error!("cdc deregister failed"; "error" => ?e, "conn_id" => ?conn_id);
-            }
             Ok::<(), String>(())
         };
 
@@ -468,6 +464,11 @@ impl Service {
                 warn!("cdc receive failed"; "error" => ?e, "downstream" => peer, "conn_id" => ?conn_id);
             } else {
                 info!("cdc receive closed"; "downstream" => peer, "conn_id" => ?conn_id);
+            }
+
+            let deregister = Deregister::Conn(conn_id);
+            if let Err(e) = scheduler.schedule(Task::Deregister(deregister)) {
+                error!("cdc deregister failed"; "error" => ?e, "conn_id" => ?conn_id);
             }
         });
 
