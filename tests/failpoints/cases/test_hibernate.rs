f3ae3aaf8e2d1110fad94905d379e5f55ed6a6b3
lucasliang <nkcs_lykx@hotmail.com>
2025-03-05 13:12:07 +0800

    This is an automated cherry-pick of #18236
    close tikv/tikv#18233
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- tests/failpoints/cases/test_hibernate.rs
+++ tests/failpoints/cases/test_hibernate.rs
@@ -40,7 +40,8 @@ fn test_break_leadership_on_restart() {
     // Peer 3 will:
     // 1. steps a heartbeat message from its leader and then ticks 1 time.
     // 2. ticks a peer_stale_state_check, which will change state from Idle to
-    // PreChaos. 3. continues to tick until it hibernates totally.
+    //    PreChaos.
+    // 3. continues to tick until it hibernates totally.
     let (tx, rx) = mpsc::sync_channel(128);
     fail::cfg_callback("on_raft_base_tick_idle", move || tx.send(0).unwrap()).unwrap();
     let mut raft_msg = RaftMessage::default();
@@ -82,6 +83,134 @@ fn test_break_leadership_on_restart() {
     rx.recv_timeout(Duration::from_secs(2)).unwrap_err();
 }
 
+<<<<<<< HEAD
+=======
+#[test]
+fn test_restart_peer_busy_on_apply() {
+    let mut cluster = new_node_cluster(0, 3);
+    let base_tick_ms = 50;
+    cluster.cfg.raft_store.raft_base_tick_interval = ReadableDuration::millis(base_tick_ms);
+    cluster.cfg.raft_store.raft_heartbeat_ticks = 2;
+    cluster.cfg.raft_store.raft_election_timeout_ticks = 10;
+    // So the random election timeout will always be 10, which makes the case more
+    // stable.
+    cluster.cfg.raft_store.raft_min_election_timeout_ticks = 10;
+    cluster.cfg.raft_store.raft_max_election_timeout_ticks = 11;
+    // Set a fairy small leader transfer log gap
+    cluster.cfg.raft_store.leader_transfer_max_log_lag = 10;
+    cluster.cfg.raft_store.min_pending_apply_region_count = 1;
+    configure_for_hibernate(&mut cluster.cfg);
+    cluster.pd_client.disable_default_operator();
+    let r = cluster.run_conf_change();
+    cluster.pd_client.must_add_peer(r, new_peer(2, 1002));
+    cluster.pd_client.must_add_peer(r, new_peer(3, 1003));
+
+    cluster.must_put(b"k1", b"v1");
+    must_get_equal(&cluster.get_engine(2), b"k1", b"v1");
+    must_get_equal(&cluster.get_engine(3), b"k1", b"v1");
+
+    // Make a log gap on Peer 1003.
+    cluster.stop_node(3);
+    for _ in 0..=11 {
+        cluster.must_put(b"k2", b"v2");
+    }
+
+    // Pause the applying processing in Peer 3 to make a log gap.
+    fail::cfg("on_handle_apply_1003", "pause").unwrap();
+    // Restart the node 3 and check the Peer 1003 is under applying stage.
+    cluster.run_node(3).unwrap();
+
+    // Wait for a while to make Peer 1003 enter `hibernate` state and
+    // check the node is still busy on applying.
+    thread::sleep(Duration::from_millis(base_tick_ms * 30));
+    // Check hibernated.
+    let (tx, rx) = mpsc::sync_channel(128);
+    fail::cfg_callback("on_raft_base_tick_idle", move || tx.send(0).unwrap()).unwrap();
+    let mut raft_msg = RaftMessage::default();
+    raft_msg.region_id = 1;
+    raft_msg.set_from_peer(new_peer(1, 1));
+    raft_msg.set_to_peer(new_peer(3, 1003));
+    raft_msg.mut_region_epoch().version = 1;
+    raft_msg.mut_region_epoch().conf_ver = 3;
+    raft_msg.mut_message().msg_type = MessageType::MsgHeartbeat;
+    raft_msg.mut_message().from = 1;
+    raft_msg.mut_message().to = 1003;
+    raft_msg.mut_message().term = 6;
+    let router = cluster.sim.rl().get_router(3).unwrap();
+    router.send_raft_message(raft_msg).unwrap();
+    assert_eq!(rx.recv_timeout(Duration::from_secs(1)).unwrap(), 0);
+    fail::remove("on_raft_base_tick_idle");
+    cluster.must_send_store_heartbeat(3);
+    thread::sleep(Duration::from_millis(base_tick_ms));
+    let stats = cluster.pd_client.get_store_stats(3).unwrap();
+    assert!(stats.is_busy);
+
+    // Recover the applying processing on Peer 1003 and wait for a while, then
+    // the region will be hibernated.
+    fail::remove("on_handle_apply_1003");
+    thread::sleep(Duration::from_millis(base_tick_ms * 10));
+    cluster.must_send_store_heartbeat(3);
+    thread::sleep(Duration::from_millis(base_tick_ms));
+    let stats = cluster.pd_client.get_store_stats(3).unwrap();
+    assert!(!stats.is_busy);
+}
+
+#[test]
+fn test_forcely_awaken_hibenrate_regions() {
+    let mut cluster = new_node_cluster(0, 3);
+    let base_tick_ms = 50;
+    cluster.cfg.raft_store.raft_base_tick_interval = ReadableDuration::millis(base_tick_ms);
+    cluster.cfg.raft_store.raft_heartbeat_ticks = 2;
+    cluster.cfg.raft_store.raft_election_timeout_ticks = 10;
+    // So the random election timeout will always be 10, which makes the case more
+    // stable.
+    cluster.cfg.raft_store.raft_min_election_timeout_ticks = 10;
+    cluster.cfg.raft_store.raft_max_election_timeout_ticks = 11;
+    configure_for_hibernate(&mut cluster.cfg);
+    cluster.pd_client.disable_default_operator();
+    let r = cluster.run_conf_change();
+    cluster.pd_client.must_add_peer(r, new_peer(2, 2));
+    cluster.pd_client.must_add_peer(r, new_peer(3, 3));
+
+    cluster.must_put(b"k1", b"v1");
+    must_get_equal(&cluster.get_engine(2), b"k1", b"v1");
+    must_get_equal(&cluster.get_engine(3), b"k1", b"v1");
+
+    // Wait until all peers of region 1 hibernate.
+    thread::sleep(Duration::from_millis(base_tick_ms * 30));
+
+    // Firstly, send `CheckPeerStaleState` message to trigger the check.
+    let router = cluster.sim.rl().get_router(3).unwrap();
+    router
+        .send(1, PeerMsg::Tick(PeerTick::CheckPeerStaleState))
+        .unwrap();
+
+    // Secondly, forcely send `MsgRegionWakeUp` message for awakening hibernated
+    // regions.
+    let (tx, rx) = mpsc::sync_channel(128);
+    fail::cfg_callback("on_raft_base_tick_chaos", move || {
+        tx.send(base_tick_ms).unwrap()
+    })
+    .unwrap();
+    let mut message = RaftMessage::default();
+    message.region_id = 1;
+    message.set_from_peer(new_peer(3, 3));
+    message.set_to_peer(new_peer(3, 3));
+    message.mut_region_epoch().version = 1;
+    message.mut_region_epoch().conf_ver = 3;
+    let mut msg = ExtraMessage::default();
+    msg.set_type(ExtraMessageType::MsgRegionWakeUp);
+    msg.forcely_awaken = true;
+    message.set_extra_msg(msg);
+    router.send_raft_message(message).unwrap();
+    assert_eq!(
+        rx.recv_timeout(Duration::from_secs(1)).unwrap(),
+        base_tick_ms
+    );
+    fail::remove("on_raft_base_tick_chaos");
+}
+
+>>>>>>> 6ec92c8579 (raftstore: avoid early hibernate if pending on applying logs when restart (#18236))
 // This case creates a cluster with 3 TiKV instances, and then wait all peers
 // hibernate.
 //
