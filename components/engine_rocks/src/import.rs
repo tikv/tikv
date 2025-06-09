bfefccfaa7ac4bdfb3dad12d7be56e0f04cd4231
Neil Shen <overvenus@gmail.com>
2025-06-09 16:54:43 +0800

    sst_importer: allow write during ingesting sst (#18514)
    ref tikv/tikv#18081
    
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/engine_rocks/src/import.rs
+++ components/engine_rocks/src/import.rs
@@ -17,6 +17,7 @@ impl ImportExt for RocksEngine {
         cf_name: &str,
         files: &[&str],
         range: Option<Range<'_>>,
+        force_allow_write: bool,
     ) -> Result<()> {
         // Acquire latch to prevent concurrency with compaction-filter operations
         // when using RocksDB IngestExternalFileOptions.allow_write = true.
@@ -29,7 +30,7 @@ impl ImportExt for RocksEngine {
         let cf = util::get_cf_handle(self.as_inner(), cf_name)?;
         let mut opts = RocksIngestExternalFileOptions::new();
         opts.move_files(true);
-        let allow_write = range.is_some();
+        let allow_write = range.is_some() || force_allow_write;
         opts.allow_write(allow_write);
         if allow_write {
             INGEST_EXTERNAL_FILE_ALLOW_WRITE_COUNTER
@@ -153,6 +154,7 @@ mod tests {
             CF_DEFAULT,
             &[p1.to_str().unwrap(), p2.to_str().unwrap()],
             None,
+            false, // force_allow_write
         )
         .unwrap();
     }
