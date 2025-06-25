b53e5f79efe8873719ecae2c7904651d30e09e3c
lucasliang <nkcs_lykx@hotmail.com>
2025-06-25 18:50:48 +0800

    *: remove the logging for "sst ingest is too slow" to avoid latency jitters. (#18590)
    close tikv/tikv#18549
    
    Removes the logging for "sst ingest is too slow" to avoid latency jitters.
    
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>

--- components/engine_rocks/src/misc.rs
+++ components/engine_rocks/src/misc.rs
@@ -334,24 +334,6 @@ impl MiscExt for RocksEngine {
         Ok(false)
     }
 
-    fn get_sst_key_ranges(&self, cf: &str, level: usize) -> Result<Vec<(Vec<u8>, Vec<u8>)>> {
-        let handle = util::get_cf_handle(self.as_inner(), cf)?;
-        let ret = self
-            .as_inner()
-            .get_column_family_meta_data(handle)
-            .get_level(level)
-            .get_files()
-            .iter()
-            .map(|sst_meta| {
-                (
-                    sst_meta.get_smallestkey().to_vec(),
-                    sst_meta.get_largestkey().to_vec(),
-                )
-            })
-            .collect();
-        Ok(ret)
-    }
-
     fn get_engine_used_size(&self) -> Result<u64> {
         let mut used_size: u64 = 0;
         for cf in self.cf_names() {
@@ -515,8 +497,7 @@ impl MiscExt for RocksEngine {
 #[cfg(test)]
 mod tests {
     use engine_traits::{
-        ALL_CFS, CompactExt, DeleteStrategy, Iterable, Iterator, ManualCompactionOptions, Mutable,
-        SyncMutable, WriteBatchExt,
+        ALL_CFS, DeleteStrategy, Iterable, Iterator, Mutable, SyncMutable, WriteBatchExt,
     };
     use tempfile::Builder;
 
@@ -776,79 +757,6 @@ mod tests {
         )
         .unwrap();
         check_data(&db, &[cf], kvs_left.as_slice());
-    }
-
-    #[test]
-    fn test_get_sst_key_ranges() {
-        let path = Builder::new()
-            .prefix("test_get_sst_key_ranges")
-            .tempdir()
-            .unwrap();
-        let path_str = path.path().to_str().unwrap();
-
-        let mut opts = RocksDbOptions::default();
-        opts.create_if_missing(true);
-        opts.enable_multi_batch_write(true);
-
-        let mut cf_opts = RocksCfOptions::default();
-        // Prefix extractor(trim the timestamp at tail) for write cf.
-        cf_opts
-            .set_prefix_extractor(
-                "FixedSuffixSliceTransform",
-                crate::util::FixedSuffixSliceTransform::new(8),
-            )
-            .unwrap_or_else(|err| panic!("{:?}", err));
-        // Create prefix bloom filter for memtable.
-        cf_opts.set_memtable_prefix_bloom_size_ratio(0.1_f64);
-        cf_opts.set_level_compaction_dynamic_level_bytes(false);
-        let cf = "default";
-        let db = new_engine_opt(path_str, opts, vec![(cf, cf_opts)]).unwrap();
-        let mut wb = db.write_batch();
-        let kvs: Vec<(&[u8], &[u8])> = vec![
-            (b"k1", b"v1"),
-            (b"k2", b"v2"),
-            (b"k6", b"v3"),
-            (b"k7", b"v4"),
-        ];
-
-        for &(k, v) in kvs.as_slice() {
-            wb.put_cf(cf, k, v).unwrap();
-        }
-        wb.write().unwrap();
-
-        db.flush_cf(cf, true).unwrap();
-        let sst_range = db.get_sst_key_ranges(cf, 0).unwrap();
-        let expected = vec![(b"k1".to_vec(), b"k7".to_vec())];
-        assert_eq!(sst_range, expected);
-
-        let mut wb = db.write_batch();
-        let kvs: Vec<(&[u8], &[u8])> = vec![(b"k3", b"v1"), (b"k4", b"v2"), (b"k8", b"v3")];
-
-        for &(k, v) in kvs.as_slice() {
-            wb.put_cf(cf, k, v).unwrap();
-        }
-        wb.write().unwrap();
-
-        db.flush_cf(cf, true).unwrap();
-        let sst_range = db.get_sst_key_ranges(cf, 0).unwrap();
-        let expected = vec![
-            (b"k3".to_vec(), b"k8".to_vec()),
-            (b"k1".to_vec(), b"k7".to_vec()),
-        ];
-        assert_eq!(sst_range, expected);
-
-        db.compact_range_cf(
-            cf,
-            None,
-            None,
-            ManualCompactionOptions::new(false, 1, false),
-        )
-        .unwrap();
-        let sst_range = db.get_sst_key_ranges(cf, 0).unwrap();
-        assert_eq!(sst_range.len(), 0);
-        let sst_range = db.get_sst_key_ranges(cf, 1).unwrap();
-        let expected = vec![(b"k1".to_vec(), b"k8".to_vec())];
-        assert_eq!(sst_range, expected);
     }
 
     #[test]
