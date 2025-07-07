2d4ba9f434e8716e4ef5ee655230bdc4bdeec1a7
lucasliang <nkcs_lykx@hotmail.com>
2025-07-07 15:57:14 +0800

    Address conflicts.
    Signed-off-by: lucasliang <nkcs_lykx@hotmail.com>

--- components/hybrid_engine/src/misc.rs
+++ components/hybrid_engine/src/misc.rs
@@ -52,10 +52,6 @@ where
         self.disk_engine().ingest_maybe_slowdown_writes(cf)
     }
 
-    fn get_sst_key_ranges(&self, cf: &str, level: usize) -> Result<Vec<(Vec<u8>, Vec<u8>)>> {
-        self.disk_engine().get_sst_key_ranges(cf, level)
-    }
-
     fn get_engine_used_size(&self) -> Result<u64> {
         self.disk_engine().get_engine_used_size()
     }
