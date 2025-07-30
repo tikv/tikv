b2c46f08f3cfcf329baade12b8520bc051bb7402
DogDu <du.wen.hao@outlook.com>
2025-07-30 15:45:51 +0800

    close tikv/tikv#18756
    maybe related tikv/tikv#18671
    
    fix dangling pointer.
    
    make sure `_mutex_guard` released earlier than `Arc<Mutex<()>>`.
    
    Signed-off-by: DogDu <du.wen.hao@outlook.com>

--- components/tikv_util/src/range_latch.rs
+++ components/tikv_util/src/range_latch.rs
@@ -3,6 +3,7 @@ use std::{
         BTreeMap,
         Bound::{Excluded, Unbounded},
     },
+    mem::ManuallyDrop,
     sync::{Arc, Mutex},
 };
 /// A structure used to manage range-based latch with mutual exclusion.
@@ -96,15 +97,16 @@ impl RangeLatch {
 
                 // Now acquire the latch after releasing the write guard
                 let mutex_guard = mutex.lock().unwrap();
-                // Safety: `_mutex_guard` is declared before `handle` in `KeyHandleGuard`.
-                // So the mutex guard will be released earlier than the `Arc<KeyHandle>`.
+                // Safety: `_mutex_guard` is declared before `handle` in `RangeLatchGuard`.
+                // So the mutex guard will be released earlier than the `Arc<RangeLatch>`.
                 // Then we can make sure the mutex guard doesn't point to released memory.
+                // Also, `_mutex_guard` points to the `Mutex<()>`, make sure it will be dropped before the `Arc<Mutex<()>>` in `drop`.
                 #[allow(clippy::missing_transmute_annotations)]
                 let mutex_guard = unsafe { std::mem::transmute(mutex_guard) };
 
                 return RangeLatchGuard {
                     start_key,
-                    _mutex_guard: mutex_guard,
+                    _mutex_guard: ManuallyDrop::new(mutex_guard),
                     handle: self,
                 };
             }
@@ -126,7 +128,7 @@ pub struct RangeLatchGuard<'a> {
     ///
     /// This field must be declared before `handle` so it will be dropped before
     /// `handle`.
-    _mutex_guard: std::sync::MutexGuard<'a, ()>,
+    _mutex_guard: ManuallyDrop<std::sync::MutexGuard<'a, ()>>,
     /// Holds a reference to RangeLatch to release the latch when the guard is
     /// dropped.
     handle: &'a RangeLatch,
@@ -134,7 +136,11 @@ pub struct RangeLatchGuard<'a> {
 
 impl Drop for RangeLatchGuard<'_> {
     fn drop(&mut self) {
+        // Safety: we call `ManuallyDrop::drop` to drop the mutex guard once and only once.
+        // So `_mutex_guard` will be released earlier than the `Arc<Mutex<()>>`.
+        unsafe { ManuallyDrop::drop(&mut self._mutex_guard) };
         let mut range_latches = self.handle.range_latches.lock().unwrap();
+        // `range_latches.remove(&self.start_key);` will cause `Arc<Mutex()>>` dropped.
         range_latches.remove(&self.start_key);
     }
 }
@@ -168,6 +174,7 @@ mod tests {
         drop(guard1);
         drop(guard2);
         drop(guard3);
+        drop(latch);
     }
 
     #[test]
