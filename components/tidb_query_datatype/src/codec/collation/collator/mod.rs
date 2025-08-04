a55e4f56c35808373afc4fcfc1ebc77ff076eb61
mohangjie <mohangjie1995@gmail.com>
2025-08-04 21:17:53 +0800

    collation: fix wrong compare result for unicode collate
    Signed-off-by: mohangjie <mohangjie1995@gmail.com>

--- components/tidb_query_datatype/src/codec/collation/collator/mod.rs
+++ components/tidb_query_datatype/src/codec/collation/collator/mod.rs
@@ -262,6 +262,23 @@ mod tests {
                     Ordering::Equal,
                 ],
             ),
+            (
+                "ʩ".as_bytes(),
+                "F".as_bytes(),
+                [
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                    Ordering::Greater,
+                ]
+            )
         ];
 
         for (sa, sb, expected) in cases {
