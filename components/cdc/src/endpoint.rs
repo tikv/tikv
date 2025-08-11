eaf1a07c7c0b29beb795ed3f16ea46c1a12b7eae
3AceShowHand <jinl1037@hotmail.com>
2025-08-11 14:09:20 +0800

    some fix
    Signed-off-by: 3AceShowHand <jinl1037@hotmail.com>

--- components/cdc/src/endpoint.rs
+++ components/cdc/src/endpoint.rs
@@ -1177,7 +1177,7 @@ impl<T: 'static + CdcHandle<E>, E: KvEngine, S: StoreRegionMeta> Endpoint<T, E, 
                     Ok(_) | Err(ScheduleError::Stopped(_)) => (),
                     // Must schedule `MinTS` event otherwise resolved ts can not
                     // advance normally.
-                    Err(e) => panic!("cdc failed to schedule min ts event"; "error" => ?e),
+                    Err(e) => panic!("cdc failed to schedule min ts event, {:?}", e),
                 }
             }
         };
