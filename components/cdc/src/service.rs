2865a9893cfddf62fff3347a71eca196eded9a1c
3AceShowHand <jinl1037@hotmail.com>
2025-08-26 23:00:58 +0800

    close the sink when exit
    Signed-off-by: 3AceShowHand <jinl1037@hotmail.com>

--- components/cdc/src/service.rs
+++ components/cdc/src/service.rs
@@ -10,7 +10,11 @@ use std::{
 
 use collections::{HashMap, HashMapEntry};
 use crossbeam::atomic::AtomicCell;
-use futures::{compat::Stream01CompatExt, stream::{StreamExt, TryStreamExt}, SinkExt};
+use futures::{
+    SinkExt,
+    compat::Stream01CompatExt,
+    stream::{StreamExt, TryStreamExt},
+};
 use grpcio::{DuplexSink, RequestStream, RpcContext, RpcStatus, RpcStatusCode};
 use kvproto::{
     cdcpb::{
@@ -513,7 +517,7 @@ impl Service {
                         warn!("cdc send failed"; "error" => ?e, "downstream" => peer, "conn_id" => ?conn_id);
                         let status = RpcStatus::with_message(RpcStatusCode::UNKNOWN,  format!("{:?}", e));
                         sink.fail(status).await.unwrap_or_else(|e| {
-                            error!("cdc failed to send error"; "error" => ?e, "downstream" => peer, "conn_id" => ?conn_id);
+                            error!("cdc failed to send error"; "error" => ?e, "conn_id" => ?conn_id);
                         });
                     } else {
                         info!("cdc send closed"; "downstream" => peer, "conn_id" => ?conn_id);
