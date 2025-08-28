b426ca0704e1482758657209e8f33f1e128bd137
okJiang <819421878@qq.com>
2025-08-28 22:30:16 +0800

    raftstore: support network dimension for SlowScore (#18525)
    ref tikv/tikv#18797
    
    raftstore: support network dimension for SlowScore
    
    Signed-off-by: okjiang <819421878@qq.com>
    Signed-off-by: 3AceShowHand <jinl1037@hotmail.com>

--- components/raftstore/src/store/worker/pd.rs
+++ components/raftstore/src/store/worker/pd.rs
@@ -650,6 +650,7 @@ where
     collect_tick_interval: Duration,
     inspect_latency_interval: Duration,      // for raft mount path
     inspect_kvdb_latency_interval: Duration, // for kvdb mount path
+    inspect_network_interval: Duration,
 }
 
 impl<T> StatsMonitor<T>
@@ -660,6 +661,7 @@ where
         interval: Duration,
         inspect_latency_interval: Duration,
         inspect_kvdb_latency_interval: Duration,
+        inspect_network_interval: Duration,
         reporter: T,
     ) -> Self {
         StatsMonitor {
@@ -683,6 +685,7 @@ where
             ),
             inspect_latency_interval,
             inspect_kvdb_latency_interval,
+            inspect_network_interval,
         }
     }
 
@@ -720,6 +723,9 @@ where
                 .div_duration_f64(tick_interval) as u64;
         let update_kvdisk_latency_stats_interval =
             self.inspect_kvdb_latency_interval
+                .div_duration_f64(tick_interval) as u64;
+        let update_network_latency_stats_interval =
+            self.inspect_network_interval
                 .div_duration_f64(tick_interval) as u64;
 
         let (timer_tx, timer_rx) = mpsc::channel();
@@ -786,6 +792,9 @@ where
                     }
                     if is_enable_tick(timer_cnt, update_kvdisk_latency_stats_interval) {
                         reporter.update_latency_stats(timer_cnt, InspectFactor::KvDisk);
+                    }
+                    if is_enable_tick(timer_cnt, update_network_latency_stats_interval) {
+                        reporter.update_latency_stats(timer_cnt, InspectFactor::Network);
                     }
                     timer_cnt += 1;
                 }
@@ -976,15 +985,17 @@ where
             interval,
             cfg.inspect_interval.0,
             cfg.inspect_kvdb_interval.0,
+            cfg.inspect_network_interval.0,
             WrappedScheduler(scheduler.clone()),
         );
         if let Err(e) = stats_monitor.start(auto_split_controller, collector_reg_handle) {
             error!("failed to start stats collector, error = {:?}", e);
         }
 
-        let health_reporter_config = RaftstoreReporterConfig {
+        let health_reporter_config: RaftstoreReporterConfig = RaftstoreReporterConfig {
             inspect_interval: cfg.inspect_interval.0,
             inspect_kvdb_interval: cfg.inspect_kvdb_interval.0,
+            inspect_network_interval: cfg.inspect_network_interval.0,
 
             unsensitive_cause: cfg.slow_trend_unsensitive_cause,
             unsensitive_result: cfg.slow_trend_unsensitive_result,
@@ -1317,8 +1328,16 @@ where
         self.store_stat.region_bytes_read.flush();
         self.store_stat.region_keys_read.flush();
 
-        let slow_score = self.health_reporter.get_slow_score();
-        stats.set_slow_score(slow_score as u64);
+        stats.set_slow_score(self.health_reporter.get_disk_slow_score() as u64);
+        // Filter out network slow scores equal to 1 to reduce message volume
+        let network_scores = self
+            .health_reporter
+            .get_network_slow_score()
+            .into_iter()
+            .filter(|(_, score)| *score != 1)
+            .collect();
+        stats.set_network_slow_scores(network_scores);
+
         let (rps, slow_trend_pb) = self
             .health_reporter
             .update_slow_trend(all_query_num, Instant::now());
@@ -2042,6 +2061,7 @@ where
         }
         let id = slow_score_tick_result.tick_id;
         let scheduler = self.scheduler.clone();
+
         let inspector = {
             match factor {
                 InspectFactor::RaftDisk => {
@@ -2102,6 +2122,19 @@ where
                         }
                     }),
                 ),
+                InspectFactor::Network => {
+                    let duration = std::collections::HashMap::<u64, Duration>::new();
+
+                    for (store_id, network_duration) in &duration {
+                        STORE_INSPECT_NETWORK_DURATION_HISTOGRAM
+                            .with_label_values(&[&store_id.to_string()])
+                            .observe(tikv_util::time::duration_to_sec(*network_duration));
+                    }
+
+                    self.health_reporter.record_network_duration(id, duration);
+
+                    return;
+                }
             }
         };
         let msg = StoreMsg::LatencyInspect {
@@ -2110,7 +2143,7 @@ where
             inspector,
         };
         if let Err(e) = self.router.send_control(msg) {
-            warn!("pd worker send latency inspecter failed"; "err" => ?e);
+            warn!("pd worker send latency inspector failed"; "err" => ?e);
         }
     }
 }
@@ -2368,7 +2401,7 @@ where
                 factor,
                 duration,
             } => {
-                self.health_reporter.record_raftstore_duration(
+                self.health_reporter.record_duration(
                     id,
                     factor,
                     duration,
@@ -2651,6 +2684,7 @@ mod tests {
                     Duration::from_secs(interval),
                     Duration::from_secs(interval),
                     Duration::default(),
+                    Duration::default(),
                     WrappedScheduler(scheduler),
                 );
                 if let Err(e) = stats_monitor.start(
@@ -2894,6 +2928,7 @@ mod tests {
         let mut stats_monitor = StatsMonitor::new(
             Duration::from_secs(interval),
             Duration::from_secs(interval),
+            Duration::default(),
             Duration::default(),
             WrappedScheduler(pd_worker.scheduler()),
         );
