d2590aba4014e8c5efdfa063fd08fbfc4a6bcc52
yibin87 <huyibin@pingcap.com>
2025-09-02 14:53:36 +0800

    Add network stats in topsql
    Signed-off-by: yibin87 <huyibin@pingcap.com>

--- components/resource_metering/src/lib.rs
+++ components/resource_metering/src/lib.rs
@@ -21,7 +21,8 @@ pub use model::*;
 pub use recorder::{
     CollectorGuard, CollectorId, CollectorRegHandle,
     ConfigChangeNotifier as RecorderConfigChangeNotifier, CpuRecorder, Recorder, RecorderBuilder,
-    SummaryRecorder, init_recorder, record_read_keys, record_write_keys,
+    SummaryRecorder, init_recorder, record_read_keys, record_write_keys, record_network_in_bytes,
+    record_network_out_bytes,
 };
 use recorder::{LocalStorage, LocalStorageRef, STORAGE};
 pub use reporter::{
