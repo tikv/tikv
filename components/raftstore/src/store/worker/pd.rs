460366a196b3e9534500db24268c8b4800bde155
yibin87 <huyibin@pingcap.com>
2025-09-02 17:31:03 +0800

    Little update
    Signed-off-by: yibin87 <huyibin@pingcap.com>

--- components/raftstore/src/store/worker/pd.rs
+++ components/raftstore/src/store/worker/pd.rs
@@ -2818,6 +2818,10 @@ mod tests {
                             cpu_time: 10,
                             read_keys: 0,
                             write_keys: 0,
+                            network_in_bytes: 0,
+                            network_out_bytes: 0,
+                            logical_read_bytes: 0,
+                            logical_write_bytes: 0,
                         },
                     );
                     records
@@ -2968,6 +2972,10 @@ mod tests {
                             cpu_time: 111,
                             read_keys: 1,
                             write_keys: 0,
+                            network_in_bytes: 0,
+                            network_out_bytes: 0,
+                            logical_read_bytes: 0,
+                            logical_write_bytes: 0,
                         },
                     );
                     records
