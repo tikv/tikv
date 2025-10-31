2d039a85ad058ed7a5e96574104ba8f97a5b59a1
山岚 <36239017+YuJuncen@users.noreply.github.com>
2025-10-31 14:36:55 +0800

    compact_log_backup: read meta from checkpoint (#19068)
    close tikv/tikv#19069
    
    This PR makes `compact-log-backup` fills the migration with subcompactions skipped by checkpoint.
    
    Signed-off-by: hillium <yu745514916@live.com>
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- components/compact-log-backup/src/execute/mod.rs
+++ components/compact-log-backup/src/execute/mod.rs
@@ -33,6 +33,7 @@ use super::{
 use crate::{
     compaction::{exec::SubcompactionExecArg, SubcompactionResult},
     errors::{Result, TraceResultExt},
+    execute::hooking::SubcompactionSkippedCtx,
     util, ErrorKind,
 };
 
@@ -203,7 +204,7 @@ impl Execution {
 
             let c = c?;
             let cid = CId(id);
-            let skip = Cell::new(false);
+            let skip = Cell::new(None);
             let cx = SubcompactionStartCtx {
                 subc: &c,
                 load_stat_diff: &lstat,
@@ -211,7 +212,9 @@ impl Execution {
                 skip: &skip,
             };
             hooks.before_a_subcompaction_start(cid, cx);
-            if skip.get() {
+            if let Some(reason) = skip.get() {
+                let skipped_cx = SubcompactionSkippedCtx { subc: &c, reason };
+                hooks.on_subcompaction_skipped(skipped_cx).await;
                 continue;
             }
 
