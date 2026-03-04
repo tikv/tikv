405a99bfdc54cfb41bde98313a9bfdb7f3e8b80a
Chen <chen.han@pingcap.com>
2026-03-04 16:02:05 +0800

    add log-backup config for gcsv2
    Signed-off-by: Chen <chen.han@pingcap.com>

--- components/backup/src/endpoint.rs
+++ components/backup/src/endpoint.rs
@@ -730,6 +730,7 @@ pub struct Endpoint<E: Engine, R: RegionInfoProvider + Clone + 'static> {
     io_pool: Runtime,
     tablets: LocalTablets<E::Local>,
     config_manager: ConfigManager,
+    gcs_v2_enable: bool,
     concurrency_manager: ConcurrencyManager,
     soft_limit_keeper: SoftLimitKeeper,
     api_version: ApiVersion,
@@ -895,6 +896,7 @@ impl<E: Engine, R: RegionInfoProvider + Clone + 'static> Endpoint<E, R> {
         region_info: R,
         tablets: LocalTablets<E::Local>,
         config: BackupConfig,
+        gcs_v2_enable: bool,
         concurrency_manager: ConcurrencyManager,
         api_version: ApiVersion,
         causal_ts_provider: Option<Arc<CausalTsProviderImpl>>,
@@ -919,6 +921,7 @@ impl<E: Engine, R: RegionInfoProvider + Clone + 'static> Endpoint<E, R> {
             io_pool: rt,
             soft_limit_keeper,
             config_manager,
+            gcs_v2_enable,
             concurrency_manager,
             api_version,
             causal_ts_provider,
@@ -933,7 +936,7 @@ impl<E: Engine, R: RegionInfoProvider + Clone + 'static> Endpoint<E, R> {
     fn get_config(&self) -> BackendConfig {
         BackendConfig {
             s3_multi_part_size: self.config_manager.0.read().unwrap().s3_multi_part_size.0 as usize,
-            gcs_v2_enable: self.config_manager.0.read().unwrap().gcs_v2_enable,
+            gcs_v2_enable: self.gcs_v2_enable,
             hdfs_config: HdfsConfig {
                 hadoop_home: self.config_manager.0.read().unwrap().hadoop.home.clone(),
                 linux_user: self
@@ -1511,6 +1514,7 @@ pub mod tests {
                     sst_max_size: ReadableSize::mb(144),
                     ..Default::default()
                 },
+                true,
                 concurrency_manager,
                 api_version,
                 causal_ts_provider,
