b282aede80c38164a2caa2e66704f19fba7b3206
zhangjinpeng87 <zzzhangjinpeng@gmail.com>
2026-03-12 18:33:39 -0700

    restore shared lock test helper
    Signed-off-by: zhangjinpeng87 <zzzhangjinpeng@gmail.com>

--- src/storage/mvcc/mod.rs
+++ src/storage/mvcc/mod.rs
@@ -903,4 +903,14 @@ pub mod tests {
             expect
         );
     }
+
+    pub fn must_load_shared_lock<E: Engine>(engine: &mut E, key: &[u8]) -> SharedLocks {
+        let snapshot = engine.snapshot(Default::default()).unwrap();
+        let mut reader = MvccReader::new(snapshot, None, true);
+        let lock_or_shared = reader.load_lock(&Key::from_raw(key)).unwrap().unwrap();
+        match lock_or_shared {
+            Either::Right(shared_locks) => shared_locks,
+            Either::Left(_) => panic!("Expected SharedLocks, got Lock"),
+        }
+    }
 }
