79ab6ed0a45abc2d8088eea000f5836cdbc88a69
zhangjinpeng87 <zzzhangjinpeng@gmail.com>
2026-03-19 18:54:08 -0700

    format: fix engine rocks test formatting
    Signed-off-by: zhangjinpeng87 <zzzhangjinpeng@gmail.com>

--- components/engine_rocks/src/engine.rs
+++ components/engine_rocks/src/engine.rs
@@ -435,10 +435,8 @@ mod tests {
                     prop::collection::vec(prop::num::u8::ANY, 0..value_size)
                 )
                     .prop_map(|(k, v)| Operation::Put(k, v)),
-                prop::collection::vec(prop::num::u8::ANY, 0..key_size)
-                    .prop_map(Operation::Get),
-                prop::collection::vec(prop::num::u8::ANY, 0..key_size)
-                    .prop_map(Operation::Delete),
+                prop::collection::vec(prop::num::u8::ANY, 0..key_size).prop_map(Operation::Get),
+                prop::collection::vec(prop::num::u8::ANY, 0..key_size).prop_map(Operation::Delete),
                 (
                     prop::collection::vec(prop::num::u8::ANY, 0..key_size),
                     0..10usize
