6209bc0dc2641993a55d3e816639345e282efd7d
lhy1024 <lhylhy1024@gmail.com>
2026-05-19 15:02:02 +0800

    raftstore: support split reason for release-8.5-20251208-v8.5.4 (#19599)
    ref tikv/tikv#19013, ref tikv/tikv#19014, ref tikv/tikv#19221\n\nBackport split-reason support to the v8.5.4 release branch.
    
    This PR is rebased onto tikv:release-8.5-20251208-v8.5.4 at b2ff5c6c3 and contains:
    - kvproto update to pingcap/kvproto:release-8.5-20251208-v8.5.4#ca8835c, which includes pingcap/kvproto#1464
    - #19212: support split reason and disable auto split on release-8.5
    - #19223: use SplitReason in SplitCheckerHost on release-8.5\n\nSigned-off-by: lhy1024 <admin@liudos.us>\nSigned-off-by: HunDunDM <hundundm@gmail.com>\nSigned-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>\n\nCo-authored-by: 混沌DM <hundundm@gmail.com>

--- components/raftstore/src/store/worker/split_check.rs
+++ components/raftstore/src/store/worker/split_check.rs
@@ -16,7 +16,7 @@ use file_system::{IoType, WithIoType};
 use itertools::Itertools;
 use kvproto::{
     metapb::{Region, RegionEpoch},
-    pdpb::CheckPolicy,
+    pdpb::{CheckPolicy, SplitReason},
 };
 use online_config::{ConfigChange, OnlineConfig};
 use pd_client::{BucketMeta, BucketStat};
@@ -376,7 +376,7 @@ where
         region: Region,
         start_key: Option<Vec<u8>>,
         end_key: Option<Vec<u8>>,
-        auto_split: bool,
+        split_reason: SplitReason,
         policy: CheckPolicy,
         bucket_ranges: Option<Vec<BucketRange>>,
     },
@@ -405,7 +405,11 @@ where
             region,
             start_key: None,
             end_key: None,
-            auto_split,
+            split_reason: if auto_split {
+                SplitReason::Size
+            } else {
+                SplitReason::Admin
+            },
             policy,
             bucket_ranges,
         }
@@ -415,7 +419,7 @@ where
         region: Region,
         start_key: Option<Vec<u8>>,
         end_key: Option<Vec<u8>>,
-        auto_split: bool,
+        split_reason: SplitReason,
         policy: CheckPolicy,
         bucket_ranges: Option<Vec<BucketRange>>,
     ) -> Self {
@@ -423,7 +427,7 @@ where
             region,
             start_key,
             end_key,
-            auto_split,
+            split_reason,
             policy,
             bucket_ranges,
         }
@@ -440,15 +444,15 @@ where
                 region,
                 start_key,
                 end_key,
-                auto_split,
+                split_reason,
                 ..
             } => write!(
                 f,
-                "[split check worker] Split Check Task for {}, start_key: {:?}, end_key: {:?}, auto_split: {:?}",
+                "[split check worker] Split Check Task for {}, start_key: {:?}, end_key: {:?}, split_reason: {:?}",
                 region.get_id(),
                 start_key,
                 end_key,
-                auto_split
+                split_reason
             ),
             Task::ChangeConfig(_) => write!(f, "[split check worker] Change Config Task"),
             Task::CompactedEvent { .. } => {
@@ -602,7 +606,7 @@ impl<EK: KvEngine, S: StoreHandle> Runner<EK, S> {
         region: &Region,
         start_key: Option<Vec<u8>>,
         end_key: Option<Vec<u8>>,
-        auto_split: bool,
+        reason: SplitReason,
         policy: CheckPolicy,
         bucket_ranges: Option<Vec<BucketRange>>,
     ) {
@@ -644,7 +648,7 @@ impl<EK: KvEngine, S: StoreHandle> Runner<EK, S> {
         CHECK_SPILT_COUNTER.all.inc();
         let mut host = self
             .coprocessor
-            .new_split_checker_host(region, tablet, auto_split, policy);
+            .new_split_checker_host(region, tablet, reason, policy);
 
         if host.skip() {
             debug!("skip split check";
@@ -746,8 +750,13 @@ impl<EK: KvEngine, S: StoreHandle> Runner<EK, S> {
             );
 
             let region_epoch = region.get_region_epoch().clone();
+            let source = match reason {
+                SplitReason::Size => "split_checker_by_size",
+                SplitReason::Load => "split_checker_by_load",
+                _ => "split_checker_by_admin",
+            };
             self.router
-                .ask_split(region_id, region_epoch, split_keys, "split checker".into());
+                .ask_split(region_id, region_epoch, split_keys, source.into());
             CHECK_SPILT_COUNTER.success.inc();
         } else {
             debug!(
@@ -973,14 +982,14 @@ where
                 region,
                 start_key,
                 end_key,
-                auto_split,
+                split_reason,
                 policy,
                 bucket_ranges,
             } => self.check_split_and_bucket(
                 &region,
                 start_key,
                 end_key,
-                auto_split,
+                split_reason,
                 policy,
                 bucket_ranges,
             ),
@@ -1004,7 +1013,10 @@ where
                     let mut host = self.coprocessor.new_split_checker_host(
                         &region,
                         tablet,
-                        false,
+                        // Only estimates and refreshes region bucket information.
+                        // No split keys are generated and no split is triggered.
+                        // Treated as an Admin operation.
+                        SplitReason::Admin,
                         CheckPolicy::Approximate,
                     );
                     if let Err(e) = self.approximate_check_bucket(tablet, &region, &mut host, None)
