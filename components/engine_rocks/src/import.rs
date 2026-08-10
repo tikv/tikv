cbf98df7711b84fe3ad1de68f4a0cb756905fee4
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2026-08-11 03:08:52 +0800

    engine_rocks: temporarily disable `allow_write` during SST ingestion (#19906) (#19914)
    close tikv/tikv#19891\n\nTemporarily disable RocksDB `allow_write` for external SST ingestion to prevent the global sequence number from moving backwards when ingestion races with foreground writes.
    
    See https://github.com/tikv/tikv/issues/19891#issuecomment-5129839846 for more information.\n\nSigned-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>\nSigned-off-by: Liqi Geng <gengliqiii@gmail.com>\n\nCo-authored-by: Liqi Geng <gengliqiii@gmail.com>

--- components/engine_rocks/src/import.rs
+++ components/engine_rocks/src/import.rs
@@ -9,6 +9,9 @@ use crate::{
     engine::RocksEngine, perf_context_metrics::INGEST_EXTERNAL_FILE_ALLOW_WRITE_COUNTER, r2e, util,
 };
 
+// Temporarily disabled due to https://github.com/tikv/tikv/issues/19891.
+const ENABLE_INGEST_ALLOW_WRITE: bool = false;
+
 impl ImportExt for RocksEngine {
     type IngestExternalFileOptions = RocksIngestExternalFileOptions;
 
@@ -31,7 +34,7 @@ impl ImportExt for RocksEngine {
         let mut opts = RocksIngestExternalFileOptions::new();
         opts.move_files(true);
         opts.set_write_global_seqno(false);
-        let allow_write = range.is_some() || force_allow_write;
+        let allow_write = ENABLE_INGEST_ALLOW_WRITE && (range.is_some() || force_allow_write);
         opts.allow_write(allow_write);
         if allow_write {
             INGEST_EXTERNAL_FILE_ALLOW_WRITE_COUNTER
