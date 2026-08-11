ee480ba571ddcd84644f3b2af43762818f70e7c2
Liqi Geng <gengliqiii@gmail.com>
2026-08-11 17:40:28 +0800

    This is an automated cherry-pick of #19975
    close tikv/tikv#19954
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>

--- components/engine_rocks/src/import.rs
+++ components/engine_rocks/src/import.rs
@@ -13,9 +13,6 @@ use crate::{
     r2e, util,
 };
 
-// Temporarily disabled due to https://github.com/tikv/tikv/issues/19891.
-const ENABLE_INGEST_ALLOW_WRITE: bool = false;
-
 impl ImportExt for RocksEngine {
     type IngestExternalFileOptions = RocksIngestExternalFileOptions;
 
@@ -37,8 +34,12 @@ impl ImportExt for RocksEngine {
         let cf = util::get_cf_handle(self.as_inner(), cf_name)?;
         let mut opts = RocksIngestExternalFileOptions::new();
         opts.move_files(true);
+<<<<<<< HEAD
         opts.set_write_global_seqno(false);
         let allow_write = ENABLE_INGEST_ALLOW_WRITE && (range.is_some() || force_allow_write);
+=======
+        let allow_write = range.is_some() || force_allow_write;
+>>>>>>> 08f2329ab3 (engine_rocks: re-enable allow_write for external SST ingestion (#19975))
         opts.allow_write(allow_write);
         if allow_write {
             INGEST_EXTERNAL_FILE_ALLOW_WRITE_COUNTER
